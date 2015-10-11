2f4f90743555b54c281c1746e57066ec269e9aa3
Dmitriy Novozhilov <dimon2x@gmail.com>
2015-10-11 19:38:40 +0300

    Added Edu project group

--- src/EduProjectTemplateFactory.java
+++ src/EduProjectTemplateFactory.java
@@ -4,16 +4,16 @@ import com.intellij.platform.ProjectTemplatesFactory;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class KtEduProjectTemplateFactory extends ProjectTemplatesFactory {
+public class EduProjectTemplateFactory extends ProjectTemplatesFactory {
     @NotNull
     @Override
     public String[] getGroups() {
-        return new String[0];
+        return new String[] {"Edu"};
     }
 
     @NotNull
     @Override
     public ProjectTemplate[] createTemplates(@Nullable String group, WizardContext context) {
-        return new ProjectTemplate[0];
+        return EduProjectTemplate.EP_NAME.getExtensions();
     }
 }
