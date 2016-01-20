a6bed3cd21289a1b7599f5bb8184684feb0b1223
liana.bakradze <liana.bakradze@jetbrains.com>
2016-01-20 17:16:56 +0300

    do not hide util folder in other types of projects

--- src/com/jetbrains/edu/kotlin/KotlinStudyTreeStructureProvider.java
+++ src/com/jetbrains/edu/kotlin/KotlinStudyTreeStructureProvider.java
@@ -2,7 +2,9 @@ package com.jetbrains.edu.kotlin;
 
 import com.intellij.ide.projectView.ViewSettings;
 import com.intellij.ide.util.treeView.AbstractTreeNode;
+import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiDirectory;
+import com.jetbrains.edu.learning.StudyTaskManager;
 import com.jetbrains.edu.learning.projectView.StudyTreeStructureProvider;
 import org.jetbrains.annotations.NotNull;
 
@@ -19,6 +21,13 @@ public class KotlinStudyTreeStructureProvider extends StudyTreeStructureProvider
                                                ViewSettings settings) {
 
         Collection<AbstractTreeNode> old_nodes = super.modify(parent, children, settings);
+        Project project = parent.getProject();
+        if (project == null) {
+            return old_nodes;
+        }
+        if (StudyTaskManager.getInstance(project).getCourse() == null) {
+            return old_nodes;
+        }
         Collection<AbstractTreeNode> nodes = new ArrayList<AbstractTreeNode>();
         for (AbstractTreeNode node: old_nodes) {
             if (node.getValue() instanceof PsiDirectory) {
