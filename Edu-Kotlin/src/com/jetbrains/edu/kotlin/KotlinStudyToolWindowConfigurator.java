531d9224ddf3653214e1da65bf89132b6d3bf04e
Liana Bakradze <liana.bakradze@jetbrains.com>
2016-02-08 15:52:12 +0300

    made project generation for java work

--- Edu-Kotlin/src/com/jetbrains/edu/kotlin/KotlinStudyToolWindowConfigurator.java
+++ Edu-Kotlin/src/com/jetbrains/edu/kotlin/KotlinStudyToolWindowConfigurator.java
@@ -1,22 +1,16 @@
 package com.jetbrains.edu.kotlin;
 
 import com.intellij.openapi.actionSystem.DefaultActionGroup;
-import com.intellij.openapi.fileEditor.FileEditorManager;
-import com.intellij.openapi.fileEditor.FileEditorManagerEvent;
 import com.intellij.openapi.fileEditor.FileEditorManagerListener;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.util.containers.HashMap;
 import com.jetbrains.edu.courseFormat.Course;
-import com.jetbrains.edu.courseFormat.Task;
-import com.jetbrains.edu.courseFormat.TaskFile;
 import com.jetbrains.edu.learning.StudyTaskManager;
 import com.jetbrains.edu.learning.StudyToolWindowConfigurator;
-import com.jetbrains.edu.learning.StudyUtils;
 import com.jetbrains.edu.learning.actions.*;
 import com.jetbrains.edu.learning.ui.StudyToolWindow;
+import com.jetbrains.edu.utils.EduIntellijUtils;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 
@@ -47,50 +41,7 @@ public class KotlinStudyToolWindowConfigurator implements StudyToolWindowConfigu
     public FileEditorManagerListener getFileEditorManagerListener(@NotNull final Project project, 
                                                                   @NotNull final StudyToolWindow studyToolWindow) {
 
-        return new FileEditorManagerListener() {
-
-            private static final String EMPTY_TASK_TEXT = "Please, open any task Ыto see task description";
-
-            @Override
-            public void fileOpened(@NotNull FileEditorManager source, @NotNull VirtualFile file) {
-                Task task = getTask(file);
-                setTaskText(task, file.getParent());
-            }
-
-            @Override
-            public void fileClosed(@NotNull FileEditorManager source, @NotNull VirtualFile file) {
-                studyToolWindow.setTaskText(EMPTY_TASK_TEXT);
-            }
-
-            @Override
-            public void selectionChanged(@NotNull FileEditorManagerEvent event) {
-                VirtualFile file = event.getNewFile();
-                if (file != null) {
-                    Task task = getTask(file);
-                    setTaskText(task, file.getParent());
-                }
-            }
-
-            @Nullable
-            private Task getTask(@NotNull VirtualFile file) {
-                TaskFile taskFile = StudyUtils.getTaskFile(project, file);
-                if (taskFile != null) {
-                    return taskFile.getTask();
-                }
-                else {
-                    return null;
-                }
-            }
-
-            private void setTaskText(@Nullable final Task task, @Nullable final VirtualFile taskDirectory) {
-                String text = StudyUtils.getTaskTextFromTask(task, taskDirectory);
-                if (text == null) {
-                    studyToolWindow.setTaskText(EMPTY_TASK_TEXT);
-                    return;
-                }
-                studyToolWindow.setTaskText(text);
-            }
-        };
+        return EduIntellijUtils.getFileEditorManagerListener(studyToolWindow, project);
     }
 
     @NotNull
