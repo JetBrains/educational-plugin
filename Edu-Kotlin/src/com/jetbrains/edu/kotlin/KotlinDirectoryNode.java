be748c45c064f9cf5ef60a0db5cfc084c9b003cb
liana.bakradze <liana.bakradze@jetbrains.com>
2016-02-15 12:25:29 +0300

    fix navigation for kotlin nodes

--- Edu-Kotlin/src/com/jetbrains/edu/kotlin/KotlinDirectoryNode.java
+++ Edu-Kotlin/src/com/jetbrains/edu/kotlin/KotlinDirectoryNode.java
@@ -1,10 +1,20 @@
 package com.jetbrains.edu.kotlin;
 
+import com.intellij.ide.projectView.ProjectView;
 import com.intellij.ide.projectView.ViewSettings;
+import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiDirectory;
+import com.intellij.psi.PsiElement;
+import com.jetbrains.edu.EduNames;
+import com.jetbrains.edu.courseFormat.Task;
+import com.jetbrains.edu.courseFormat.TaskFile;
+import com.jetbrains.edu.learning.StudyUtils;
 import com.jetbrains.edu.learning.projectView.StudyDirectoryNode;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Map;
 
 public class KotlinDirectoryNode extends StudyDirectoryNode {
     public KotlinDirectoryNode(@NotNull Project project, PsiDirectory value, ViewSettings viewSettings) {
@@ -13,7 +23,49 @@ public class KotlinDirectoryNode extends StudyDirectoryNode {
 
     @Override
     public void navigate(boolean requestFocus) {
-        //TODO: implement
-        //super.navigate(requestFocus);
+        final String myValueName = myValue.getName();
+        if (myValueName.contains(EduNames.TASK)) {
+            PsiDirectory src = myValue.findSubdirectory("src");
+            if (src == null) {
+                return;
+            }
+            TaskFile taskFile = null;
+            VirtualFile virtualFile =  null;
+            for (PsiElement child : src.getChildren()) {
+                VirtualFile childFile = child.getContainingFile().getVirtualFile();
+                taskFile = StudyUtils.getTaskFile(myProject, childFile);
+                if (taskFile != null) {
+                    virtualFile = childFile;
+                    break;
+                }
+            }
+            if (taskFile != null) {
+                VirtualFile taskDir = virtualFile.getParent();
+                Task task = taskFile.getTask();
+                for (VirtualFile openFile : FileEditorManager.getInstance(myProject).getOpenFiles()) {
+                    FileEditorManager.getInstance(myProject).closeFile(openFile);
+                }
+                VirtualFile child = null;
+                Map<String, TaskFile> taskFiles = task.getTaskFiles();
+                for (Map.Entry<String, TaskFile> entry: taskFiles.entrySet()) {
+                    VirtualFile file = taskDir.findChild(entry.getKey());
+                    if (file != null) {
+                        FileEditorManager.getInstance(myProject).openFile(file, true);
+                    }
+                    if (!entry.getValue().getAnswerPlaceholders().isEmpty()) {
+                        child = file;
+                    }
+                }
+                if (child != null) {
+                    ProjectView.getInstance(myProject).select(child, child, false);
+                    FileEditorManager.getInstance(myProject).openFile(child, true);
+                } else {
+                    VirtualFile[] children = taskDir.getChildren();
+                    if (children.length > 0) {
+                        ProjectView.getInstance(myProject).select(children[0], children[0], false);
+                    }
+                }
+            }
+        }
     }
 }
