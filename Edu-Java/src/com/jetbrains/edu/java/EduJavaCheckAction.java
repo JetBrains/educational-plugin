d33bd3c9b3ef66cce644656e707b5522b8f74733
Liana Bakradze <liana.bakradze@jetbrains.com>
2016-02-24 15:45:43 +0300

    task checker for java implemented

--- Edu-Java/src/com/jetbrains/edu/java/EduJavaCheckAction.java
+++ Edu-Java/src/com/jetbrains/edu/java/EduJavaCheckAction.java
@@ -1,12 +1,31 @@
 package com.jetbrains.edu.java;
 
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.jetbrains.edu.learning.actions.StudyCheckAction;
+import com.intellij.execution.application.ApplicationConfiguration;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.module.ModuleUtilCore;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.jetbrains.edu.learning.StudyState;
+import com.jetbrains.edu.utils.EduCheckAction;
+import com.jetbrains.edu.utils.EduIntelliJNames;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-class EduJavaCheckAction extends StudyCheckAction {
+class EduJavaCheckAction extends EduCheckAction {
+    @Nullable
     @Override
-    public void actionPerformed(@NotNull AnActionEvent e) {
-       //TODO: implement
+    protected VirtualFile getTestsFile(@NotNull StudyState studyState) {
+        VirtualFile taskFileVF = studyState.getVirtualFile();
+        String testFileName = taskFileVF.getNameWithoutExtension() + "Test" + ".java";
+        return taskFileVF.getParent().findChild(testFileName);
+    }
+
+
+    protected void setProcessParameters(Project project, ApplicationConfiguration configuration,
+                                      VirtualFile taskFileVF, @NotNull VirtualFile testsFile) {
+        configuration.setMainClassName(EduIntelliJNames.TEST_RUNNER_CLASS);
+        Module module = ModuleUtilCore.findModuleForFile(taskFileVF, project);
+        configuration.setModule(module);
+        configuration.setProgramParameters(testsFile.getNameWithoutExtension());
     }
 }
