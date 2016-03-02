f2901668bf0206d0ba5a71fef9fc5467fa6431e4
Liana Bakradze <liana.bakradze@jetbrains.com>
2016-03-02 15:39:42 +0300

    find tests for files without placeholders

--- Edu-Java/src/com/jetbrains/edu/java/EduJavaCheckAction.java
+++ Edu-Java/src/com/jetbrains/edu/java/EduJavaCheckAction.java
@@ -4,6 +4,7 @@ import com.intellij.execution.application.ApplicationConfiguration;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleUtilCore;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.jetbrains.edu.learning.StudyState;
 import com.jetbrains.edu.utils.EduCheckAction;
@@ -11,13 +12,29 @@ import com.jetbrains.edu.utils.EduIntelliJNames;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Set;
+
 class EduJavaCheckAction extends EduCheckAction {
     @Nullable
     @Override
     protected VirtualFile getTestsFile(@NotNull StudyState studyState) {
+        //typically people invoke checker from task files with placeholders
         VirtualFile taskFileVF = studyState.getVirtualFile();
+        VirtualFile taskDir = studyState.getTaskDir();
         String testFileName = taskFileVF.getNameWithoutExtension() + "Test" + ".java";
-        return taskFileVF.getParent().findChild(testFileName);
+        VirtualFile virtualFile =  taskDir.findChild(testFileName);
+        if (virtualFile != null) {
+            return virtualFile;
+        }
+        Set<String> fileNames = studyState.getTask().getTaskFiles().keySet();
+        for (String name : fileNames) {
+            String testName = FileUtil.getNameWithoutExtension(name) + "Test" + ".java";
+            VirtualFile child = taskDir.findChild(testName);
+            if (child != null) {
+                return child;
+            }
+        }
+        return null;
     }
 
 
