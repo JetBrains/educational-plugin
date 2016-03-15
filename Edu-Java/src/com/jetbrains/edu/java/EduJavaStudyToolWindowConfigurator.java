b10ce55b9d686b14e6050303533b616e5659a1b9
Valentina Kiryushkina <valentina.kiryushkina@jetbrains.com>
2016-03-15 20:27:59 +0300

    Update java configurator according to the new api

--- Edu-Java/src/com/jetbrains/edu/java/EduJavaStudyToolWindowConfigurator.java
+++ Edu-Java/src/com/jetbrains/edu/java/EduJavaStudyToolWindowConfigurator.java
@@ -2,12 +2,16 @@ package com.jetbrains.edu.java;
 
 import com.intellij.openapi.actionSystem.DefaultActionGroup;
 import com.intellij.openapi.project.Project;
+import com.jetbrains.edu.learning.StudyBasePluginConfigurator;
+import com.jetbrains.edu.learning.StudyPluginConfigurator;
 import com.jetbrains.edu.learning.courseFormat.Course;
 import com.jetbrains.edu.learning.StudyBaseToolWindowConfigurator;
 import com.jetbrains.edu.learning.StudyTaskManager;
+import com.jetbrains.edu.learning.settings.ModifiableSettingsPanel;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-public class EduJavaStudyToolWindowConfigurator extends StudyBaseToolWindowConfigurator {
+public class EduJavaStudyToolWindowConfigurator extends StudyBasePluginConfigurator {
     @NotNull
     @Override
     public DefaultActionGroup getActionGroup(Project project) {
@@ -37,5 +41,11 @@ public class EduJavaStudyToolWindowConfigurator extends StudyBaseToolWindowConfi
     @Override
     public String getLanguageScriptUrl() {
         return getClass().getResource("/code_mirror/clike.js").toExternalForm();
+    }
+
+    @Nullable
+    @Override
+    public ModifiableSettingsPanel getSettingsPanel() {
+        return null;
     }
 }
