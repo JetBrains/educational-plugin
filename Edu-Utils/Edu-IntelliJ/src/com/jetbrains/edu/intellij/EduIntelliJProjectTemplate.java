ec0ba0965518382720f7ce903fc54097f5edff50
Liana.Bakradze <liana.bakradze@jetbrains.com>
2017-02-16 19:12:38 +0300

    remove Edu-Intellij plugin (it's now merged into student)

--- Edu-Utils/Edu-IntelliJ/src/com/jetbrains/edu/intellij/EduIntelliJProjectTemplate.java
+++
@@ -1,8 +0,0 @@
-package com.jetbrains.edu.intellij;
-
-import com.intellij.openapi.extensions.ExtensionPointName;
-import com.intellij.platform.ProjectTemplate;
-
-public interface EduIntelliJProjectTemplate extends ProjectTemplate {
-    ExtensionPointName<EduIntelliJProjectTemplate> EP_NAME = ExtensionPointName.create("com.jetbrains.edu.intellij.eduIntelliJProjectTemplate");
-}
