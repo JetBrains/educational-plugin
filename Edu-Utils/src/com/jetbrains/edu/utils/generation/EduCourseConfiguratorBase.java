1b043589a44efa5051d4f18da77b3534c9e2dacf
Liana.Bakradze <liana.bakradze@jetbrains.com>
2017-03-03 15:30:10 +0300

    course creator project template and module builder moved to course-creator plugin

--- Edu-Utils/src/com/jetbrains/edu/utils/generation/EduCourseConfiguratorBase.java
+++ Edu-Utils/src/com/jetbrains/edu/utils/generation/EduCourseConfiguratorBase.java
@@ -1,10 +1,15 @@
 package com.jetbrains.edu.utils.generation;
 
+import com.intellij.codeInsight.daemon.impl.quickfix.OrderEntryFix;
+import com.intellij.execution.junit.JUnitExternalLibraryDescriptor;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.module.ModifiableModuleModel;
+import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleWithNameAlreadyExists;
 import com.intellij.openapi.options.ConfigurationException;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.ExternalLibraryDescriptor;
+import com.intellij.openapi.roots.ModuleRootModificationUtil;
 import com.jetbrains.edu.learning.courseFormat.Course;
 import com.jetbrains.edu.learning.courseGeneration.StudyProjectGenerator;
 import com.jetbrains.edu.learning.intellij.EduCourseConfigurator;
@@ -14,9 +19,20 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
 
 public class EduCourseConfiguratorBase implements EduCourseConfigurator {
+
   private static final Logger LOG = Logger.getInstance(EduCourseConfiguratorBase.class);
+
+  @Override
+  public void configureModule(@NotNull Module module) {
+    ExternalLibraryDescriptor descriptor = JUnitExternalLibraryDescriptor.JUNIT4;
+    List<String> defaultRoots = descriptor.getLibraryClassesRoots();
+    final List<String> urls = OrderEntryFix.refreshAndConvertToUrls(defaultRoots);
+    ModuleRootModificationUtil.addModuleLibrary(module, descriptor.getPresentableName(), urls, Collections.emptyList());
+  }
 
   @Override
   public void createCourseFromCourseInfo(@NotNull ModifiableModuleModel moduleModel, @NotNull Project project, @NotNull StudyProjectGenerator generator, @NotNull CourseInfo selectedCourse, @Nullable String moduleDir) {
