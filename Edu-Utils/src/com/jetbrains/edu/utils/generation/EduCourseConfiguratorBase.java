9cf0ac020efca42dc60beb190157b6c6b66e13c6
liana.bakradze <liana.bakradze@jetbrains.com>
2017-03-13 19:09:52 +0300

    CourseInfo mooved to course format

--- Edu-Utils/src/com/jetbrains/edu/utils/generation/EduCourseConfiguratorBase.java
+++ Edu-Utils/src/com/jetbrains/edu/utils/generation/EduCourseConfiguratorBase.java
@@ -13,7 +13,7 @@ import com.intellij.openapi.roots.ModuleRootModificationUtil;
 import com.jetbrains.edu.learning.courseFormat.Course;
 import com.jetbrains.edu.learning.courseGeneration.StudyProjectGenerator;
 import com.jetbrains.edu.learning.intellij.EduCourseConfigurator;
-import com.jetbrains.edu.learning.stepic.CourseInfo;
+import com.jetbrains.edu.learning.courseFormat.CourseInfo;
 import org.jdom.JDOMException;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
