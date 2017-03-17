776a860286e5b52fc3fab9cd593943f006310d6f
Liana.Bakradze <liana.bakradze@jetbrains.com>
2017-03-17 15:19:05 +0300

    update to use new EduPluginConfigurator extension (EDU-895)

--- src/com/jetbrains/edu/utils/EduCCCreationUtils.java
+++ src/com/jetbrains/edu/utils/EduCCCreationUtils.java
@@ -1,4 +1,4 @@
-package com.jetbrains.edu.utils.creation;
+package com.jetbrains.edu.utils;
 
 import com.intellij.ide.IdeView;
 import com.intellij.ide.util.newProjectWizard.AbstractProjectWizard;
@@ -9,19 +9,42 @@ import com.intellij.openapi.module.ModuleManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.ui.configuration.actions.NewModuleAction;
 import com.intellij.psi.PsiDirectory;
-import com.jetbrains.edu.coursecreator.creation.CCTaskCreator;
 import com.jetbrains.edu.learning.courseFormat.Course;
+import com.jetbrains.edu.learning.courseFormat.Lesson;
 import com.jetbrains.edu.learning.courseFormat.StudyItem;
 import com.jetbrains.edu.learning.courseFormat.Task;
-import com.jetbrains.edu.utils.EduIntelliJNames;
+import com.jetbrains.edu.utils.generation.EduLessonModuleBuilder;
 import com.jetbrains.edu.utils.generation.EduTaskModuleBuilder;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class EduCCTaskCreatorBase implements CCTaskCreator {
+public class EduCCCreationUtils {
+  private EduCCCreationUtils() {
+  }
 
-  @Override
-  public PsiDirectory createTask(@NotNull Project project, @NotNull StudyItem item, @Nullable IdeView view, @NotNull PsiDirectory parentDirectory, @NotNull Course course) {
+  public static PsiDirectory createLesson(@NotNull Project project, @NotNull StudyItem item, @NotNull PsiDirectory parentDirectory) {
+    NewModuleAction newModuleAction = new NewModuleAction();
+    String courseDirPath = parentDirectory.getVirtualFile().getPath();
+    Lesson lesson = (Lesson) item;
+    Module utilModule = ModuleManager.getInstance(project).findModuleByName(EduIntelliJNames.UTIL);
+    if (utilModule == null) {
+      return null;
+    }
+    newModuleAction.createModuleFromWizard(project, null, new AbstractProjectWizard("", project, "") {
+      @Override
+      public StepSequence getSequence() {
+        return null;
+      }
+
+      @Override
+      public ProjectBuilder getProjectBuilder() {
+        return new EduLessonModuleBuilder(courseDirPath, lesson, utilModule);
+      }
+    });
+    return null;
+  }
+
+  public static PsiDirectory createTask(@NotNull Project project, @NotNull StudyItem item, @Nullable IdeView view, @NotNull PsiDirectory parentDirectory, @NotNull Course course) {
     String lessonDirName = parentDirectory.getName();
     NewModuleAction newModuleAction = new NewModuleAction();
     Module lessonModule = ModuleManager.getInstance(project).findModuleByName(lessonDirName);
@@ -43,4 +66,5 @@ public class EduCCTaskCreatorBase implements CCTaskCreator {
     });
     return null;
   }
+
 }
