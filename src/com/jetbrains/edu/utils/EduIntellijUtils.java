6bd83ad8f1f6ca732a1c0593a79b8a2e86ca1824
liana.bakradze <liana.bakradze@jetbrains.com>
2017-03-19 12:12:19 +0300

    extract common code from Java and Kotlin configurators (EDU-895)

--- src/com/jetbrains/edu/utils/EduIntellijUtils.java
+++ src/com/jetbrains/edu/utils/EduIntellijUtils.java
@@ -1,22 +1,35 @@
 package com.jetbrains.edu.utils;
 
+import com.intellij.ide.IdeView;
 import com.intellij.ide.fileTemplates.FileTemplate;
 import com.intellij.ide.fileTemplates.FileTemplateManager;
 import com.intellij.ide.fileTemplates.FileTemplateUtil;
 import com.intellij.ide.projectView.actions.MarkRootActionBase;
+import com.intellij.ide.util.newProjectWizard.AbstractProjectWizard;
+import com.intellij.ide.util.newProjectWizard.StepSequence;
+import com.intellij.ide.util.projectWizard.ProjectBuilder;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.module.Module;
+import com.intellij.openapi.module.ModuleManager;
 import com.intellij.openapi.module.ModuleUtilCore;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.ContentEntry;
 import com.intellij.openapi.roots.ModifiableRootModel;
 import com.intellij.openapi.roots.ModuleRootManager;
+import com.intellij.openapi.roots.ui.configuration.actions.NewModuleAction;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiManager;
 import com.intellij.util.PathUtil;
 import com.intellij.util.io.ZipUtil;
+import com.jetbrains.edu.coursecreator.settings.CCSettings;
+import com.jetbrains.edu.learning.StudyUtils;
+import com.jetbrains.edu.learning.core.EduNames;
+import com.jetbrains.edu.learning.courseFormat.Course;
+import com.jetbrains.edu.learning.courseFormat.StudyItem;
+import com.jetbrains.edu.learning.courseFormat.Task;
+import com.jetbrains.edu.utils.generation.EduTaskModuleBuilder;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -109,4 +122,46 @@ public class EduIntellijUtils {
         }
 
     }
+
+  public static PsiDirectory createTask(@NotNull Project project, @NotNull StudyItem item, @Nullable IdeView view, @NotNull PsiDirectory parentDirectory,
+                                        @Nullable String taskFileName, @Nullable String testFileName) {
+    String lessonDirName = parentDirectory.getName();
+    NewModuleAction newModuleAction = new NewModuleAction();
+    Module lessonModule = ModuleManager.getInstance(project).findModuleByName(lessonDirName);
+    Module utilModule = ModuleManager.getInstance(project).findModuleByName(EduIntelliJNames.UTIL);
+    if (lessonModule == null || utilModule == null) {
+      return null;
+    }
+    Task task = (Task) item;
+    newModuleAction.createModuleFromWizard(project, null, new AbstractProjectWizard("", project, "") {
+      @Override
+      public StepSequence getSequence() {
+        return null;
+      }
+
+      @Override
+      public ProjectBuilder getProjectBuilder() {
+        return new EduTaskModuleBuilder(parentDirectory.getVirtualFile().getPath(), lessonDirName, task, utilModule) {
+          @Override
+          protected void createTask(Project project, Course course, VirtualFile src) throws IOException {
+            PsiDirectory psiDirectory = PsiManager.getInstance(project).findDirectory(src);
+            if (psiDirectory == null) {
+              return;
+            }
+            String taskDescriptionFileName = StudyUtils.getTaskDescriptionFileName(CCSettings.getInstance().useHtmlAsDefaultTaskFormat());
+            StudyUtils.createFromTemplate(project, psiDirectory, taskDescriptionFileName, view, false);
+            createIfNotNull(project, psiDirectory, taskFileName, view);
+            createIfNotNull(project, psiDirectory, testFileName, view);
+          }
+        };
+      }
+    });
+    return parentDirectory.findSubdirectory(EduNames.LESSON + task.getLesson().getIndex() + "-" + EduNames.TASK + task.getIndex());
+  }
+
+  private static void createIfNotNull(@NotNull Project project, @NotNull PsiDirectory psiDirectory, @Nullable String fileName, @Nullable IdeView view) {
+    if (fileName != null) {
+      StudyUtils.createFromTemplate(project, psiDirectory, fileName, view, false);
+    }
+  }
 }
---
+++ src/com/jetbrains/edu/utils/EduPluginConfiguratorBase.java
@@ -0,0 +1,105 @@
+package com.jetbrains.edu.utils;
+
+import com.intellij.codeInsight.daemon.impl.quickfix.OrderEntryFix;
+import com.intellij.execution.junit.JUnitExternalLibraryDescriptor;
+import com.intellij.ide.IdeView;
+import com.intellij.ide.util.newProjectWizard.AbstractProjectWizard;
+import com.intellij.ide.util.newProjectWizard.StepSequence;
+import com.intellij.ide.util.projectWizard.ProjectBuilder;
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.module.ModifiableModuleModel;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.module.ModuleManager;
+import com.intellij.openapi.module.ModuleWithNameAlreadyExists;
+import com.intellij.openapi.options.ConfigurationException;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.ExternalLibraryDescriptor;
+import com.intellij.openapi.roots.ModuleRootModificationUtil;
+import com.intellij.openapi.roots.ui.configuration.actions.NewModuleAction;
+import com.intellij.openapi.util.io.FileUtilRt;
+import com.intellij.psi.PsiDirectory;
+import com.jetbrains.edu.learning.EduPluginConfigurator;
+import com.jetbrains.edu.learning.core.EduNames;
+import com.jetbrains.edu.learning.courseFormat.Course;
+import com.jetbrains.edu.learning.courseFormat.Lesson;
+import com.jetbrains.edu.learning.courseFormat.StudyItem;
+import com.jetbrains.edu.utils.generation.EduLessonModuleBuilder;
+import com.jetbrains.edu.utils.generation.EduModuleBuilderUtils;
+import org.jdom.JDOMException;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
+public abstract class EduPluginConfiguratorBase implements EduPluginConfigurator {
+  @Override
+  public PsiDirectory createLesson(@NotNull Project project, @NotNull StudyItem item, @Nullable IdeView view, @NotNull PsiDirectory parentDirectory) {
+    NewModuleAction newModuleAction = new NewModuleAction();
+    String courseDirPath = parentDirectory.getVirtualFile().getPath();
+    Lesson lesson = (Lesson) item;
+    Module utilModule = ModuleManager.getInstance(project).findModuleByName(EduIntelliJNames.UTIL);
+    if (utilModule == null) {
+      return null;
+    }
+    newModuleAction.createModuleFromWizard(project, null, new AbstractProjectWizard("", project, "") {
+      @Override
+      public StepSequence getSequence() {
+        return null;
+      }
+
+      @Override
+      public ProjectBuilder getProjectBuilder() {
+        return new EduLessonModuleBuilder(courseDirPath, lesson, utilModule);
+      }
+    });
+    return parentDirectory.findSubdirectory(EduNames.LESSON + lesson.getIndex());
+  }
+
+  @Override
+  public PsiDirectory createTask(@NotNull Project project, @NotNull StudyItem item, @Nullable IdeView view, @NotNull PsiDirectory parentDirectory, @NotNull Course course) {
+    return EduIntellijUtils.createTask(project, item, view, parentDirectory, null, null);
+  }
+
+  @NotNull
+  @Override
+  public String getLanguageScriptUrl() {
+    return getClass().getResource("/code_mirror/clike.js").toExternalForm();
+  }
+
+  @NotNull
+  @Override
+  public String getDefaultHighlightingMode() {
+    return "text/x-java";
+  }
+
+  @Override
+  public boolean excludeFromArchive(@NotNull File pathname) {
+    String name = pathname.getName();
+    return "out".equals(name) || ".idea".equals(name) || "iml".equals(FileUtilRt.getExtension(name)) || EduIntelliJNames.TEST_RUNNER_FILE.equals(name);
+  }
+
+
+  @Override
+  public void configureModule(@NotNull Module module) {
+    ExternalLibraryDescriptor descriptor = JUnitExternalLibraryDescriptor.JUNIT4;
+    List<String> defaultRoots = descriptor.getLibraryClassesRoots();
+    final List<String> urls = OrderEntryFix.refreshAndConvertToUrls(defaultRoots);
+    ModuleRootModificationUtil.addModuleLibrary(module, descriptor.getPresentableName(), urls, Collections.emptyList());
+  }
+
+
+  @Override
+  public void createCourseModuleContent(@NotNull ModifiableModuleModel moduleModel,
+                                        @NotNull Project project,
+                                        @NotNull Course course,
+                                        @Nullable String moduleDir) {
+    try {
+      EduModuleBuilderUtils.createCourseModuleContent(moduleModel, project, course, moduleDir);
+    } catch (IOException | ModuleWithNameAlreadyExists | ConfigurationException | JDOMException e) {
+      Logger.getInstance(EduPluginConfiguratorBase.class).error(e);
+    }
+  }
+}
