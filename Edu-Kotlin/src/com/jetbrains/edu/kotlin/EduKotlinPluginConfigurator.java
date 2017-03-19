a3bd4bf25c08884c0da0d6c5f90ae65bd9748854
liana.bakradze <liana.bakradze@jetbrains.com>
2017-03-19 12:12:19 +0300

    extract common code from Java and Kotlin configurators (EDU-895)

--- Edu-Kotlin/src/com/jetbrains/edu/kotlin/EduKotlinPluginConfigurator.java
+++ Edu-Kotlin/src/com/jetbrains/edu/kotlin/EduKotlinPluginConfigurator.java
@@ -1,45 +1,23 @@
 package com.jetbrains.edu.kotlin;
 
-import com.intellij.codeInsight.daemon.impl.quickfix.OrderEntryFix;
-import com.intellij.execution.junit.JUnitExternalLibraryDescriptor;
-import com.intellij.ide.IdeView;
 import com.intellij.openapi.actionSystem.ActionManager;
 import com.intellij.openapi.actionSystem.DefaultActionGroup;
-import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.module.ModifiableModuleModel;
 import com.intellij.openapi.module.Module;
-import com.intellij.openapi.module.ModuleWithNameAlreadyExists;
-import com.intellij.openapi.options.ConfigurationException;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.roots.ExternalLibraryDescriptor;
-import com.intellij.openapi.roots.ModuleRootModificationUtil;
 import com.intellij.openapi.startup.StartupManager;
 import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.PsiDirectory;
-import com.jetbrains.edu.learning.EduPluginConfigurator;
 import com.jetbrains.edu.learning.actions.StudyCheckAction;
 import com.jetbrains.edu.learning.actions.StudyFillPlaceholdersAction;
 import com.jetbrains.edu.learning.actions.StudyShowHintAction;
 import com.jetbrains.edu.learning.core.EduNames;
-import com.jetbrains.edu.learning.courseFormat.Course;
-import com.jetbrains.edu.learning.courseFormat.StudyItem;
-import com.jetbrains.edu.utils.EduCCCreationUtils;
-import com.jetbrains.edu.utils.generation.EduModuleBuilderUtils;
-import org.jdom.JDOMException;
+import com.jetbrains.edu.utils.EduPluginConfiguratorBase;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Collections;
-import java.util.List;
-
-public class EduKotlinPluginConfigurator implements EduPluginConfigurator {
+public class EduKotlinPluginConfigurator extends EduPluginConfiguratorBase {
 
   static final String LEGACY_TESTS_KT = "tests.kt";
   static final String TESTS_KT = "Tests.kt";
-  private static final Logger LOG = Logger.getInstance(EduKotlinPluginConfigurator.class);
 
   @NotNull
   @Override
@@ -48,37 +26,9 @@ public class EduKotlinPluginConfigurator implements EduPluginConfigurator {
   }
 
   @Override
-  public PsiDirectory createLesson(@NotNull Project project, @NotNull StudyItem item, @Nullable IdeView view, @NotNull PsiDirectory parentDirectory) {
-    return EduCCCreationUtils.createLesson(project, item, parentDirectory);
-  }
-
-  @Override
-  public PsiDirectory createTask(@NotNull Project project, @NotNull StudyItem item, @Nullable IdeView view, @NotNull PsiDirectory parentDirectory, @NotNull Course course) {
-    return EduCCCreationUtils.createTask(project, item, view, parentDirectory, null, null);
-  }
-
-  @Override
-  public boolean excludeFromArchive(File pathname) {
-    String name = pathname.getName();
-    return "out".equals(name) || ".idea".equals(name);
-  }
-
-  @Override
   public boolean isTestFile(VirtualFile file) {
     String name = file.getName();
     return TESTS_KT.equals(name) || LEGACY_TESTS_KT.equals(name) || name.contains(FileUtil.getNameWithoutExtension(TESTS_KT)) && name.contains(EduNames.SUBTASK_MARKER);
-  }
-
-  @NotNull
-  @Override
-  public String getDefaultHighlightingMode() {
-    return "text/x-java";
-  }
-
-  @NotNull
-  @Override
-  public String getLanguageScriptUrl() {
-    return getClass().getResource("/code_mirror/clike.js").toExternalForm();
   }
 
   @Override
@@ -89,7 +39,7 @@ public class EduKotlinPluginConfigurator implements EduPluginConfigurator {
   @NotNull
   @Override
   public DefaultActionGroup getTaskDescriptionActionGroup() {
-    DefaultActionGroup taskDescriptionActionGroup = EduPluginConfigurator.super.getTaskDescriptionActionGroup();
+    DefaultActionGroup taskDescriptionActionGroup = super.getTaskDescriptionActionGroup();
     taskDescriptionActionGroup.remove(ActionManager.getInstance().getAction(StudyShowHintAction.ACTION_ID));
     StudyFillPlaceholdersAction fillPlaceholdersAction = new StudyFillPlaceholdersAction();
     fillPlaceholdersAction.getTemplatePresentation().setIcon(EduKotlinIcons.FILL_PLACEHOLDERS_ICON);
@@ -100,23 +50,8 @@ public class EduKotlinPluginConfigurator implements EduPluginConfigurator {
 
   @Override
   public void configureModule(@NotNull Module module) {
-    ExternalLibraryDescriptor descriptor = JUnitExternalLibraryDescriptor.JUNIT4;
-    List<String> defaultRoots = descriptor.getLibraryClassesRoots();
-    final List<String> urls = OrderEntryFix.refreshAndConvertToUrls(defaultRoots);
-    ModuleRootModificationUtil.addModuleLibrary(module, descriptor.getPresentableName(), urls, Collections.emptyList());
+    super.configureModule(module);
     Project project = module.getProject();
     StartupManager.getInstance(project).runWhenProjectIsInitialized(() -> EduKotlinLibConfigurator.configureLib(project));
-  }
-
-  @Override
-  public void createCourseModuleContent(@NotNull ModifiableModuleModel moduleModel,
-                                        @NotNull Project project,
-                                        @NotNull Course course,
-                                        @Nullable String moduleDir) {
-    try {
-      EduModuleBuilderUtils.createCourseModuleContent(moduleModel, project, course, moduleDir);
-    } catch (IOException | ModuleWithNameAlreadyExists | ConfigurationException | JDOMException e) {
-      LOG.error(e);
-    }
   }
 }
