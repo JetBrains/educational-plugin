454f3836021305d7b90adc8f7f6d157ed84c49a9
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2017-06-01 14:19:33 +0300

    simplified course project generation

--- Edu-Utils/Edu-Kotlin/src/com/jetbrains/edu/kotlin/android/EduKotlinAndroidCourseProjectGenerator.java
+++ Edu-Utils/Edu-Kotlin/src/com/jetbrains/edu/kotlin/android/EduKotlinAndroidCourseProjectGenerator.java
@@ -1,15 +1,18 @@
 package com.jetbrains.edu.kotlin.android;
 
 import com.intellij.facet.ui.ValidationResult;
-import com.intellij.ide.util.newProjectWizard.AbstractProjectWizard;
-import com.intellij.ide.util.newProjectWizard.StepSequence;
-import com.intellij.ide.util.projectWizard.ProjectBuilder;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.module.Module;
+import com.intellij.openapi.module.ModuleManager;
+import com.intellij.openapi.project.DumbModePermission;
+import com.intellij.openapi.project.DumbService;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.roots.ui.configuration.actions.NewModuleAction;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.platform.DirectoryProjectGenerator;
+import com.jetbrains.edu.learning.EduPluginConfigurator;
+import com.jetbrains.edu.learning.StudyUtils;
 import com.jetbrains.edu.learning.courseFormat.Course;
+import com.jetbrains.edu.learning.intellij.generation.EduProjectGenerator;
 import com.jetbrains.edu.learning.newproject.EduCourseProjectGenerator;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
@@ -38,17 +41,15 @@ class EduKotlinAndroidCourseProjectGenerator implements EduCourseProjectGenerato
 
       @Override
       public void generateProject(@NotNull Project project, @NotNull VirtualFile baseDir, @Nullable Object settings, @NotNull Module module) {
-        new NewModuleAction().createModuleFromWizard(project, null, new AbstractProjectWizard("", project, baseDir.getPath()) {
-          @Override
-          public StepSequence getSequence() {
-            return null;
-          }
+        EduProjectGenerator generator = new EduProjectGenerator();
+        generator.setSelectedCourse(myCourse);
+        generator.generateProject(project, project.getBaseDir());
 
-          @Override
-          public ProjectBuilder getProjectBuilder() {
-            return new EduKotlinAndroidModuleBuilder(myCourse);
-          }
-        });
+        EduPluginConfigurator.INSTANCE.
+            forLanguage(myCourse.getLanguageById()).createCourseModuleContent(ModuleManager.getInstance(project).getModifiableModel(),
+            project, myCourse, project.getBasePath());
+        ApplicationManager.getApplication().invokeLater(() -> DumbService.allowStartingDumbModeInside(DumbModePermission.MAY_START_BACKGROUND,
+            () -> ApplicationManager.getApplication().runWriteAction(() -> StudyUtils.registerStudyToolWindow(myCourse, project))));
       }
 
       @NotNull
