8586471066feb5d290c1bd6b6cd61cd379b1c81f
Liana.Bakradze <liana.bakradze@jetbrains.com>
2017-07-03 17:08:05 +0300

    set jdk when creating kotlin course

--- Edu-Utils/Edu-Kotlin/src/com/jetbrains/edu/kotlin/EduKotlinCourseProjectGenerator.java
+++ Edu-Utils/Edu-Kotlin/src/com/jetbrains/edu/kotlin/EduKotlinCourseProjectGenerator.java
@@ -4,11 +4,24 @@ import com.intellij.facet.ui.ValidationResult;
 import com.intellij.ide.util.newProjectWizard.AbstractProjectWizard;
 import com.intellij.ide.util.newProjectWizard.StepSequence;
 import com.intellij.ide.util.projectWizard.ProjectBuilder;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.module.Module;
+import com.intellij.openapi.options.ConfigurationException;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.project.ProjectManager;
+import com.intellij.openapi.projectRoots.JavaSdkType;
+import com.intellij.openapi.projectRoots.SdkType;
+import com.intellij.openapi.roots.ProjectRootManager;
+import com.intellij.openapi.roots.ui.configuration.JdkComboBox;
+import com.intellij.openapi.roots.ui.configuration.ProjectStructureConfigurable;
 import com.intellij.openapi.roots.ui.configuration.actions.NewModuleAction;
+import com.intellij.openapi.roots.ui.configuration.projectRoot.ProjectSdksModel;
+import com.intellij.openapi.ui.FixedSizeButton;
+import com.intellij.openapi.ui.LabeledComponent;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.platform.DirectoryProjectGenerator;
+import com.intellij.ui.ComboboxWithBrowseButton;
 import com.jetbrains.edu.learning.courseFormat.Course;
 import com.jetbrains.edu.learning.newproject.EduCourseProjectGenerator;
 import org.jetbrains.annotations.Nls;
@@ -17,9 +30,25 @@ import org.jetbrains.annotations.Nullable;
 import org.jetbrains.kotlin.idea.KotlinIcons;
 
 import javax.swing.*;
+import java.awt.*;
 
 class EduKotlinCourseProjectGenerator implements EduCourseProjectGenerator {
+  private static final Logger LOG = Logger.getInstance(EduKotlinCourseProjectGenerator.class);
   private Course myCourse;
+  private JdkComboBox myJdkComboBox;
+  private ProjectSdksModel myModel;
+
+  @Nullable
+  @Override
+  public LabeledComponent<JComponent> getLanguageSettingsComponent(@NotNull Course selectedCourse) {
+    myModel = ProjectStructureConfigurable.getInstance(ProjectManager.getInstance().getDefaultProject()).getProjectJdksModel();
+    myJdkComboBox = new JdkComboBox(myModel, sdkTypeId -> sdkTypeId instanceof JavaSdkType && !((JavaSdkType)sdkTypeId).isDependent(), sdk -> true, sdkTypeId -> sdkTypeId instanceof JavaSdkType && !((JavaSdkType)sdkTypeId).isDependent(), true);
+    ComboboxWithBrowseButton comboboxWithBrowseButton = new ComboboxWithBrowseButton(myJdkComboBox);
+    FixedSizeButton setupButton = comboboxWithBrowseButton.getButton();
+    myJdkComboBox.setSetupButton(setupButton, null, myModel, (JdkComboBox.JdkComboBoxItem) myJdkComboBox.getModel().getSelectedItem(), null, false);
+    return LabeledComponent.create(comboboxWithBrowseButton, "Jdk", BorderLayout.WEST);
+  }
+
   @NotNull
   @Override
   public DirectoryProjectGenerator getDirectoryProjectGenerator() {
@@ -50,6 +79,7 @@ class EduKotlinCourseProjectGenerator implements EduCourseProjectGenerator {
             return new EduKotlinKoansModuleBuilder();
           }
         });
+        setJdk(project);
       }
 
       @NotNull
@@ -58,12 +88,33 @@ class EduKotlinCourseProjectGenerator implements EduCourseProjectGenerator {
         return ValidationResult.OK;
       }
     };
+
+  }
+
+  private void setJdk(@NotNull Project project) {
+    JdkComboBox.JdkComboBoxItem selectedItem = myJdkComboBox.getSelectedItem();
+    if (selectedItem instanceof JdkComboBox.SuggestedJdkItem) {
+      SdkType type = ((JdkComboBox.SuggestedJdkItem)selectedItem).getSdkType();
+      String path = ((JdkComboBox.SuggestedJdkItem)selectedItem).getPath();
+      myModel.addSdk(type, path, sdk -> {
+        myJdkComboBox.reloadModel(new JdkComboBox.ActualJdkComboBoxItem(sdk), project);
+        myJdkComboBox.setSelectedJdk(sdk);
+      });
+    }
+    try {
+      myModel.apply();
+    } catch (ConfigurationException e) {
+      LOG.error(e);
+    }
+    ApplicationManager.getApplication().runWriteAction(() -> {
+      ProjectRootManager.getInstance(project).setProjectSdk(myJdkComboBox.getSelectedJdk());
+    });
   }
 
   @Nullable
   @Override
   public Object getProjectSettings() {
-    return null;
+    return new Object();
   }
 
   @Override
