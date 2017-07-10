09acc81b3a9f69704fcd233810148f5ac9906bc7
liana.bakradze <liana.bakradze@jetbrains.com>
2017-07-10 13:09:12 +0300

    minor cleanup

--- Edu-Utils/src/com/jetbrains/edu/utils/EduIntellijUtils.java
+++ Edu-Utils/src/com/jetbrains/edu/utils/EduIntellijUtils.java
@@ -6,15 +6,12 @@ import com.intellij.ide.IdeView;
 import com.intellij.ide.fileTemplates.FileTemplate;
 import com.intellij.ide.fileTemplates.FileTemplateManager;
 import com.intellij.ide.fileTemplates.FileTemplateUtil;
-import com.intellij.ide.projectView.actions.MarkRootActionBase;
 import com.intellij.ide.util.newProjectWizard.AbstractProjectWizard;
 import com.intellij.ide.util.newProjectWizard.StepSequence;
 import com.intellij.ide.util.projectWizard.ProjectBuilder;
-import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleManager;
-import com.intellij.openapi.module.ModuleUtilCore;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.*;
 import com.intellij.openapi.roots.ui.configuration.actions.NewModuleAction;
@@ -33,7 +30,6 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
-import java.io.FilenameFilter;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
@@ -56,12 +52,7 @@ public class EduIntellijUtils {
           LOG.info("Failed to create courses dir");
         } else {
           try {
-            ZipUtil.extract(jarFile, pluginBaseDir, new FilenameFilter() {
-              @Override
-              public boolean accept(File dir, String name) {
-                return name.equals(courseName);
-              }
-            });
+            ZipUtil.extract(jarFile, pluginBaseDir, (dir, name) -> name.equals(courseName));
           } catch (IOException e) {
             LOG.info("Failed to extract default course", e);
           }
@@ -72,50 +63,12 @@ public class EduIntellijUtils {
     return new File(jarPath, "courses");
   }
 
-  private static void commitAndSaveModel(final ModifiableRootModel model) {
-    ApplicationManager.getApplication().runWriteAction(new Runnable() {
-      @Override
-      public void run() {
-        model.commit();
-        model.getProject().save();
-      }
-    });
-  }
-
-  @Nullable
-  private static ModifiableRootModel getModel(@NotNull VirtualFile dir, @NotNull Project project) {
-    final Module module = ModuleUtilCore.findModuleForFile(dir, project);
-    if (module == null) {
-      LOG.info("Module for " + dir.getPath() + " was not found");
-      return null;
-    }
-    return ModuleRootManager.getInstance(module).getModifiableModel();
-  }
-
-  static void markDirAsSourceRoot(@NotNull final VirtualFile dir, @NotNull final Project project) {
-    final ModifiableRootModel model = getModel(dir, project);
-    if (model == null) {
-      return;
-    }
-    final ContentEntry entry = MarkRootActionBase.findContentEntry(model, dir);
-    if (entry == null) {
-      LOG.info("Content entry for " + dir.getPath() + " was not found");
-      return;
-    }
-    entry.addSourceFolder(dir, false);
-    commitAndSaveModel(model);
-  }
-
 
   public static void addTemplate(@NotNull final Project project, @NotNull VirtualFile baseDir, @NotNull @NonNls final String templateName) {
     final FileTemplate template = FileTemplateManager.getInstance(project).getInternalTemplate(templateName);
     final PsiDirectory projectDir = PsiManager.getInstance(project).findDirectory(baseDir);
     if (projectDir == null) return;
     try {
-//            PsiDirectory utilDir = projectDir.findSubdirectory("util");
-//            if (utilDir == null) {
-//                utilDir = projectDir.createSubdirectory("util");
-//            }
       FileTemplateUtil.createFromTemplate(template, templateName, null, projectDir);
     } catch (Exception exception) {
       LOG.error("Failed to create from file template ", exception);
