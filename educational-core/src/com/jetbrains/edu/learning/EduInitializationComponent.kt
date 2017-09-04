48b88c68782977804fa40308830aa51933fec3da
Liana.Bakradze <liana.bakradze@jetbrains.com>
2017-09-04 18:41:24 +0300

    EDU-1042 cosmetic improvements
    prettier info message, do not check every time if old plugins are enabled

--- educational-core/src/com/jetbrains/edu/learning/EduInitializationComponent.kt
+++ educational-core/src/com/jetbrains/edu/learning/EduInitializationComponent.kt
@@ -2,10 +2,14 @@ package com.jetbrains.edu.learning
 
 import com.intellij.ide.plugins.PluginManager
 import com.intellij.ide.plugins.PluginManagerCore
+import com.intellij.ide.util.PropertiesComponent
 import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.ApplicationNamesInfo
 import com.intellij.openapi.components.ApplicationComponent
 import com.intellij.openapi.extensions.PluginId
 import com.intellij.openapi.ui.Messages
+import com.intellij.openapi.util.text.StringUtil
+import java.util.*
 
 
 class EduInitializationComponent : ApplicationComponent {
@@ -23,26 +27,44 @@ class EduInitializationComponent : ApplicationComponent {
                 "com.jetbrains.python.edu.core",
                 "com.jetbrains.edu.core"
         )
+        const val CONFLICTING_PLUGINS_DISABLED = "Educational.conflictingPluginsDisabled"
     }
 
     override fun initComponent() {
-        if (disablePlugins()) {
-            Messages.showInfoMessage("IDE will be restarted", "Restart")
+        if (PropertiesComponent.getInstance().isValueSet(CONFLICTING_PLUGINS_DISABLED)) {
+            return
+        }
+        var disabledPlugins = disablePlugins()
+        if (disabledPlugins.isNotEmpty()) {
+            disabledPlugins = disabledPlugins.map { name -> "'$name'" }
+            val ideName = ApplicationNamesInfo.getInstance().fullProductName
+            val multiplePluginsDisabled = disabledPlugins.size != 1
+
+            val names = if (multiplePluginsDisabled) StringUtil.join(disabledPlugins, ", ") else disabledPlugins[0]
+            val ending = if (multiplePluginsDisabled) "s" else ""
+            val verb = if (multiplePluginsDisabled) "were" else "was"
+            val restartInfo = if (ApplicationManager.getApplication().isRestartCapable) "$ideName will be restarted" else "Restart $ideName"
+
+            val message = "Conflicting plugin$ending $names $verb disabled. $restartInfo in order to apply changes"
+
+            Messages.showInfoMessage(message, restartInfo)
             ApplicationManager.getApplication().restart()
         }
+
     }
 
 
-    private fun disablePlugins(): Boolean {
-        var restartNeeded = false
+    private fun disablePlugins(): List<String> {
+        val disabledPlugins = ArrayList<String>()
         for (id in IDS) {
             val plugin = PluginManager.getPlugin(PluginId.getId(id))
             plugin ?: continue
             if (plugin.isEnabled) {
+                disabledPlugins.add(plugin.name)
                 PluginManagerCore.disablePlugin(id)
-                restartNeeded = true
             }
         }
-        return restartNeeded
+        PropertiesComponent.getInstance().setValue(CONFLICTING_PLUGINS_DISABLED, "true")
+        return disabledPlugins
     }
 }
