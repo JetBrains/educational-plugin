eea8a121cabab8cd76a843dc5447d2aa2bdd12e3
Liana.Bakradze <liana.bakradze@jetbrains.com>
2017-09-28 19:12:20 +0300

    EDU-1047 rules to make headers prettier in swing panels
    (cherry picked from commit e04c83d)

--- educational-core/src/com/jetbrains/edu/learning/ui/StudySwingToolWindow.java
+++ educational-core/src/com/jetbrains/edu/learning/ui/StudySwingToolWindow.java
@@ -22,12 +22,12 @@ import com.intellij.ui.BrowserHyperlinkListener;
 import com.intellij.ui.ColorUtil;
 import com.intellij.ui.components.JBScrollPane;
 import com.intellij.util.ui.UIUtil;
+import com.intellij.util.ui.JBUI;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
-import javax.swing.text.html.HTMLDocument;
 import javax.swing.text.html.HTMLEditorKit;
+import javax.swing.text.html.StyleSheet;
 import java.awt.*;
 
 public class StudySwingToolWindow extends StudyToolWindow {
@@ -42,22 +42,35 @@ public class StudySwingToolWindow extends StudyToolWindow {
     myTaskTextPane = new JTextPane();
     final JBScrollPane scrollPane = new JBScrollPane(myTaskTextPane);
     myTaskTextPane.setContentType(new HTMLEditorKit().getContentType());
+
     final EditorColorsScheme editorColorsScheme = EditorColorsManager.getInstance().getGlobalScheme();
     int fontSize = editorColorsScheme.getEditorFontSize();
     final String fontName = editorColorsScheme.getEditorFontName();
     final Font font = new Font(fontName, Font.PLAIN, fontSize);
-    String bodyRule = "body { font-family: " + font.getFamily() + "; " +
-                      "font-size: " + font.getSize() + "pt; }" +
-                      "pre {font-family: Courier; display: inline; ine-height: 50px; padding-top: 5px; padding-bottom: 5px; padding-left: 5px; background-color:"
-                      + ColorUtil.toHex(ColorUtil.dimmer(UIUtil.getPanelBackground())) + ";}" +
-                      "code {font-family: Courier; display: flex; float: left; background-color:"
-                      + ColorUtil.toHex(ColorUtil.dimmer(UIUtil.getPanelBackground())) + ";}";
-    ((HTMLDocument)myTaskTextPane.getDocument()).getStyleSheet().addRule(bodyRule);
+    String dimmedColor = ColorUtil.toHex(ColorUtil.dimmer(UIUtil.getPanelBackground()));
+    int size = font.getSize();
+    String bodyRule = String.format("body { font-family: %s; font-size: %dpt; }", font.getFamily(), size);
+    String preRule = String.format("pre {font-family: Courier; font-size: %dpt; " +
+      "display: inline; ine-height: 50px; padding-top: 5px; padding-bottom: 5px; " +
+      "padding-left: 5px; background-color:%s;}", fontSize, dimmedColor);
+    String codeRule = String.format("code {font-family: Courier; font-size:%dpt; display: flex; " +
+      "float: left; background-color: %s;}", fontSize, dimmedColor);
+    String sizeRule = String.format("h1 { font-size: %dpt; } h2 { font-size: %fpt; }", 2 * fontSize, 1.5 * fontSize);
+    HTMLEditorKit htmlEditorKit = UIUtil.getHTMLEditorKit(false);
+
+    StyleSheet styleSheet = htmlEditorKit.getStyleSheet();
+    styleSheet.addRule(bodyRule);
+    styleSheet.addRule(preRule);
+    styleSheet.addRule(codeRule);
+    styleSheet.addRule(sizeRule);
+
+    myTaskTextPane.setEditorKit(htmlEditorKit);
+
     myTaskTextPane.setEditable(false);
     if (!UIUtil.isUnderDarcula()) {
       myTaskTextPane.setBackground(EditorColorsManager.getInstance().getGlobalScheme().getDefaultBackground());
     }
-    myTaskTextPane.setBorder(new EmptyBorder(20, 20, 0, 10));
+    myTaskTextPane.setBorder(JBUI.Borders.empty(20, 20, 0, 10));
     myTaskTextPane.addHyperlinkListener(BrowserHyperlinkListener.INSTANCE);
     return scrollPane;
   }
