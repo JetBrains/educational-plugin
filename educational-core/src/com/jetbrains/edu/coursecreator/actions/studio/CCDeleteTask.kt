5a9dbe7efa4f4f4717924fd8d93ce7185fdfef9a
Liana.Bakradze <liana.bakradze@jetbrains.com>
2017-10-15 13:12:12 +0300

    EDU-1065 delete lesson action
    default delete handler doesn't delete modules in project view in AS
    
    (cherry picked from commit 590a483)

--- educational-core/src/com/jetbrains/edu/coursecreator/actions/studio/CCDeleteTask.kt
+++ educational-core/src/com/jetbrains/edu/coursecreator/actions/studio/CCDeleteTask.kt
@@ -1,34 +1,14 @@
 package com.jetbrains.edu.coursecreator.actions.studio
 
-import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.actionSystem.CommonDataKeys
-import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.project.DumbAwareAction
-import com.jetbrains.edu.coursecreator.CCUtils
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
 import com.jetbrains.edu.coursecreator.CCVirtualFileListener
 import com.jetbrains.edu.learning.StudyUtils
+import com.jetbrains.edu.learning.courseFormat.Course
+import com.jetbrains.edu.learning.courseFormat.StudyItem
 
-class CCDeleteTask : DumbAwareAction("Delete Task") {
-  override fun actionPerformed(e: AnActionEvent?) {
-    val dataContext = e?.dataContext!!
-    val project = e.project
-    val virtualFile = CommonDataKeys.VIRTUAL_FILE.getData(dataContext)!!
-    val task = StudyUtils.getTask(project!!, virtualFile)!!
-    CCVirtualFileListener.deleteTask(task.lesson.course, virtualFile)
-    ApplicationManager.getApplication().runWriteAction({ virtualFile.delete(CCDeleteTask::class.java) })
-  }
+class CCDeleteTask : DeleteStudyItem("Delete Task") {
+  override fun getStudyItem(project: Project, file: VirtualFile): StudyItem? = StudyUtils.getTask(project, file)
 
-  override fun update(e: AnActionEvent?) {
-    e?:return
-    val presentation = e.presentation
-    presentation.isEnabledAndVisible = false
-    val dataContext = e.dataContext
-    val project = e.project?:return
-    if (CCUtils.isCourseCreator(project).not()) {
-      return
-    }
-    val virtualFile = CommonDataKeys.VIRTUAL_FILE.getData(dataContext)?:return
-    StudyUtils.getTask(project, virtualFile)?:return
-    presentation.isEnabledAndVisible = true
-  }
+  override fun deleteItem(course: Course, file: VirtualFile, project: Project) = CCVirtualFileListener.deleteTask(course, file)
 }
