90c5b0eea7f3377212ffb3b4bbdd3e34646494c2
Valentina Kiryushkina <valentina.kiryushkina@jetbrains.com>
2017-10-27 14:03:47 +0300

    EDU-1124 EduTools for AS: deleting task removes all next tasks
    The problem was that after fix of EDU-1093, we deleted study item twice.
    First in DeleteStudyItem action, and then in corresponding method of
    CCVirtualFileListener

--- educational-core/src/com/jetbrains/edu/coursecreator/actions/studio/CCDeleteTask.kt
+++ educational-core/src/com/jetbrains/edu/coursecreator/actions/studio/CCDeleteTask.kt
@@ -2,13 +2,9 @@ package com.jetbrains.edu.coursecreator.actions.studio
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
-import com.jetbrains.edu.coursecreator.CCVirtualFileListener
 import com.jetbrains.edu.learning.StudyUtils
-import com.jetbrains.edu.learning.courseFormat.Course
 import com.jetbrains.edu.learning.courseFormat.StudyItem
 
 class CCDeleteTask : DeleteStudyItem("Delete Task") {
   override fun getStudyItem(project: Project, file: VirtualFile): StudyItem? = StudyUtils.getTask(project, file)
-
-  override fun deleteItem(course: Course, file: VirtualFile, project: Project) = CCVirtualFileListener.deleteTask(course, file)
 }
