5e248df8bf47ab7c994ca228e33566943d44593a
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2017-11-03 15:45:49 +0300

    correct package for Edu-Python module

---
+++ Edu-Python/src/com/jetbrains/edu/python/learning/PyStudyInitialConfigurator.java
@@ -0,0 +1,50 @@
+package com.jetbrains.edu.python.learning;
+
+import com.intellij.codeInsight.CodeInsightSettings;
+import com.intellij.ide.util.PropertiesComponent;
+import com.intellij.openapi.application.PathManager;
+import com.intellij.openapi.components.ApplicationComponent;
+import com.intellij.openapi.fileTypes.FileTypeManager;
+import com.intellij.openapi.project.ex.ProjectManagerEx;
+import com.intellij.openapi.util.io.FileUtil;
+import com.intellij.util.messages.MessageBus;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+
+@SuppressWarnings({"UtilityClassWithoutPrivateConstructor", "UtilityClassWithPublicConstructor"})
+public class PyStudyInitialConfigurator implements ApplicationComponent {
+  @NonNls private static final String CONFIGURED_V40 = "StudyPyCharm.InitialConfiguration40";
+
+  /**
+   * @noinspection UnusedParameters
+   */
+  public PyStudyInitialConfigurator(MessageBus bus,
+                                    CodeInsightSettings codeInsightSettings,
+                                    final PropertiesComponent propertiesComponent,
+                                    FileTypeManager fileTypeManager,
+                                    final ProjectManagerEx projectManager) {
+    if (!propertiesComponent.getBoolean(CONFIGURED_V40)) {
+      final File courses = new File(PathManager.getConfigPath(), "courses");
+      FileUtil.delete(courses);
+      propertiesComponent.setValue(CONFIGURED_V40, "true");
+    }
+  }
+
+  @Override
+  public void initComponent() {
+
+  }
+
+  @Override
+  public void disposeComponent() {
+
+  }
+
+  @NotNull
+  @Override
+  public String getComponentName() {
+    return "Educational.InitialConfigurator";
+  }
+}
