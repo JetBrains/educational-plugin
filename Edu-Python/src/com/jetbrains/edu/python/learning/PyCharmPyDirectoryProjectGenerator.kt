6dc95404c625f45017a9b800d4f517434bfa5fad
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2017-11-03 15:45:49 +0300

    correct package for Edu-Python module

---
+++ Edu-Python/src/com/jetbrains/edu/python/learning/PyCharmPyDirectoryProjectGenerator.kt
@@ -0,0 +1,10 @@
+package com.jetbrains.edu.python.learning
+
+import com.intellij.openapi.projectRoots.Sdk
+import com.jetbrains.edu.learning.courseFormat.Course
+import com.jetbrains.python.configuration.PyConfigurableInterpreterList
+
+internal class PyCharmPyDirectoryProjectGenerator(course: Course) : PyDirectoryProjectGenerator(course) {
+
+  override fun getAllSdks(): List<Sdk> = PyConfigurableInterpreterList.getInstance(null).allPythonSdks
+}
