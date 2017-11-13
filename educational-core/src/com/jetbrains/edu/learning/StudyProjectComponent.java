2c81e0dc47b92da48c3d921e15810d5a499da979
Liana.Bakradze <liana.bakradze@jetbrains.com>
2017-11-13 13:19:27 +0300

    fix tests: no project view in unit test mode

--- educational-core/src/com/jetbrains/edu/learning/StudyProjectComponent.java
+++ educational-core/src/com/jetbrains/edu/learning/StudyProjectComponent.java
@@ -87,7 +87,11 @@ public class StudyProjectComponent implements ProjectComponent {
 
     StartupManager.getInstance(myProject).runWhenProjectIsInitialized(
       () -> {
-        ProjectView.getInstance(myProject).changeView(ProjectViewPane.ID);
+
+        if (!ApplicationManager.getApplication().isUnitTestMode()) {
+          selectProjectView();
+        }
+
         Course course = StudyTaskManager.getInstance(myProject).getCourse();
         if (course == null) {
           LOG.warn("Opened project is with null course");
@@ -126,6 +130,16 @@ public class StudyProjectComponent implements ProjectComponent {
         }
       }
     });
+  }
+
+  private void selectProjectView() {
+    ProjectView projectView = ProjectView.getInstance(myProject);
+    if (projectView != null) {
+      projectView.changeView(ProjectViewPane.ID);
+    }
+    else {
+      LOG.warn("Failed to select Project View");
+    }
   }
 
   private void loadSolutionsFromStepik(@NotNull Course course) {
