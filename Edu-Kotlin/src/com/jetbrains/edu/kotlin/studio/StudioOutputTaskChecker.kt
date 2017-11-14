e592f5d89c7cd31a854e73ff4367da477fca6cf3
Natalia Selezneva <natalia.selezneva@jetbrains.com>
2017-11-14 09:17:51 +0300

    Implement Check Action for kotlin output tasks in Android Studio

---
+++ Edu-Kotlin/src/com/jetbrains/edu/kotlin/studio/StudioOutputTaskChecker.kt
@@ -0,0 +1,41 @@
+package com.jetbrains.edu.kotlin.studio
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VfsUtil
+import com.jetbrains.edu.kotlin.KtTaskChecker.FAILED_TO_LAUNCH
+import com.jetbrains.edu.learning.checker.CheckResult
+import com.jetbrains.edu.learning.checker.OutputTaskChecker
+import com.jetbrains.edu.learning.checker.TestsOutputParser
+import com.jetbrains.edu.learning.courseFormat.CheckStatus
+import com.jetbrains.edu.learning.courseFormat.tasks.OutputTask
+import com.jetbrains.edu.learning.courseFormat.tasks.Task
+
+class StudioOutputTaskChecker : StudioTaskCheckerBase() {
+    override fun isAccepted(task: Task) = task is OutputTask && super.isAccepted(task)
+
+    override fun check(task: Task, project: Project): CheckResult {
+        val mainClassName = getMainClassName(project) ?: return FAILED_TO_LAUNCH
+        val cmd = generateGradleCommandLine(
+                project,
+                "${getGradleProjectName(task)}:run",
+                "$MAIN_CLASS_PROPERTY_PREFIX$mainClassName"
+        ) ?: return FAILED_TO_LAUNCH
+
+        val output = getProcessOutput(cmd.createProcess(), cmd.commandLineString)
+                .postProcessOutput()
+                .takeIf { it.isNotBlank() } ?: "<no output>"
+
+        val outputFile = task.getTaskDir(project)
+                ?.parent
+                ?.findChild("test")
+                ?.findChild(OutputTaskChecker.OUTPUT_PATTERN_NAME)
+                ?: return FAILED_TO_LAUNCH
+
+        val expectedOutput = VfsUtil.loadText(outputFile).postProcessOutput()
+        if (expectedOutput != output) {
+            return CheckResult(CheckStatus.Failed, "Expected output:\n$expectedOutput \nActual output:\n$output")
+        }
+
+        return CheckResult(CheckStatus.Solved, TestsOutputParser.CONGRATULATIONS)
+    }
+}
