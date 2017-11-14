3883ed15d5855b54630d89d35fa17673afbcdb21
Natalia Selezneva <natalia.selezneva@jetbrains.com>
2017-11-14 09:14:10 +0300

    Implement Run Action for theory tasks
    (cherry picked from commit 7c82abf)

--- educational-core/src/com/jetbrains/edu/learning/checker/TheoryTaskChecker.kt
+++ educational-core/src/com/jetbrains/edu/learning/checker/TheoryTaskChecker.kt
@@ -1,6 +1,10 @@
 package com.jetbrains.edu.learning.checker
 
+import com.intellij.execution.ProgramRunnerUtil
+import com.intellij.execution.executors.DefaultRunExecutor
 import com.intellij.openapi.project.Project
+import com.jetbrains.edu.learning.checker.CheckUtils.NOT_RUNNABLE_MESSAGE
+import com.jetbrains.edu.learning.checker.CheckUtils.createDefaultRunConfiguration
 import com.jetbrains.edu.learning.courseFormat.CheckStatus
 import com.jetbrains.edu.learning.courseFormat.tasks.Task
 import com.jetbrains.edu.learning.courseFormat.tasks.TheoryTask
@@ -10,7 +14,16 @@ class TheoryTaskChecker : TaskChecker() {
 
     override fun onTaskSolved(task: Task, project: Project, message: String) {}
 
-    override fun check(task: Task, project: Project) = CheckResult(CheckStatus.Solved, "")
+    override fun check(task: Task, project: Project): CheckResult {
+        val configuration = createDefaultRunConfiguration(project)
+        @Suppress("FoldInitializerAndIfToElvis")
+        if (configuration == null) {
+            return CheckResult(CheckStatus.Unchecked, NOT_RUNNABLE_MESSAGE)
+        }
+
+        ProgramRunnerUtil.executeConfiguration(configuration, DefaultRunExecutor.getRunExecutorInstance())
+        return CheckResult(CheckStatus.Solved, "")
+    }
 
     override fun checkOnRemote(task: Task, project: Project) = check(task, project)
 }
