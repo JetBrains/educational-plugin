ec2b88e98a9865d56396af23d88c15fe013c3392
liana.bakradze <liana.bakradze@jetbrains.com>
2017-11-15 17:29:44 +0300

    EDU-1172 extract package for new project generation

--- Edu-Python/src/com/jetbrains/edu/python/learning/pycharm/PyCharmPyDirectoryProjectGenerator.kt
+++ Edu-Python/src/com/jetbrains/edu/python/learning/pycharm/PyCharmPyDirectoryProjectGenerator.kt
@@ -3,7 +3,7 @@ package com.jetbrains.edu.python.learning.pycharm
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.projectRoots.Sdk
 import com.jetbrains.edu.learning.courseFormat.Course
-import com.jetbrains.edu.python.learning.PyDirectoryProjectGenerator
+import com.jetbrains.edu.python.learning.newproject.PyDirectoryProjectGenerator
 import com.jetbrains.python.configuration.PyConfigurableInterpreterList
 
 internal class PyCharmPyDirectoryProjectGenerator(course: Course) : PyDirectoryProjectGenerator(course) {
