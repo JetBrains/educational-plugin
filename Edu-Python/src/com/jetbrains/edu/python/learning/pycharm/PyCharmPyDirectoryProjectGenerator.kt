51048da03779dd5db27f9c5bb20639baa6594876
liana.bakradze <liana.bakradze@jetbrains.com>
2017-11-15 17:27:04 +0300

    EDU-1172 extract package for pycharm specific code
    (cherry picked from commit 960befe)

--- Edu-Python/src/com/jetbrains/edu/python/learning/pycharm/PyCharmPyDirectoryProjectGenerator.kt
+++ Edu-Python/src/com/jetbrains/edu/python/learning/pycharm/PyCharmPyDirectoryProjectGenerator.kt
@@ -1,10 +1,11 @@
-package com.jetbrains.edu.python.learning
+package com.jetbrains.edu.python.learning.pycharm
 
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.options.ConfigurationException
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.projectRoots.Sdk
 import com.jetbrains.edu.learning.courseFormat.Course
+import com.jetbrains.edu.python.learning.PyDirectoryProjectGenerator
 import com.jetbrains.python.configuration.PyConfigurableInterpreterList
 
 internal class PyCharmPyDirectoryProjectGenerator(course: Course) : PyDirectoryProjectGenerator(course) {
--- Edu-Python/src/com/jetbrains/edu/python/learning/pycharm/PyCharmPyEduPluginConfigurator.kt
+++ Edu-Python/src/com/jetbrains/edu/python/learning/pycharm/PyCharmPyEduPluginConfigurator.kt
@@ -1,18 +1,19 @@
-package com.jetbrains.edu.python.learning
+package com.jetbrains.edu.python.learning.pycharm
 
 import com.intellij.util.PlatformUtils
 import com.jetbrains.edu.learning.EduPluginConfigurator
 import com.jetbrains.edu.learning.courseFormat.Course
 import com.jetbrains.edu.learning.newproject.EduCourseProjectGenerator
+import com.jetbrains.edu.python.learning.PyEduPluginConfigurator
 import com.jetbrains.python.newProject.PyNewProjectSettings
 
 class PyCharmPyEduPluginConfigurator : PyEduPluginConfigurator() {
 
   override fun getLanguageSettings(): EduPluginConfigurator.LanguageSettings<PyNewProjectSettings> =
-          PyCharmPyLanguageSettings()
+    PyCharmPyLanguageSettings()
 
   override fun getEduCourseProjectGenerator(course: Course): EduCourseProjectGenerator<PyNewProjectSettings>? =
-          PyCharmPyDirectoryProjectGenerator(course)
+    PyCharmPyDirectoryProjectGenerator(course)
 
   override fun isEnabled(): Boolean = PlatformUtils.isPyCharm() || PlatformUtils.isCLion()
 }
