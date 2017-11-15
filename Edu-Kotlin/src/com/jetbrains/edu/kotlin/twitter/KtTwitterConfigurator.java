01181088762aa6a4ef68d799166970811f65d5f2
Liana.Bakradze <liana.bakradze@jetbrains.com>
2017-11-15 15:51:38 +0300

    drop KotlinUtils
    we have too many util classes and this one had only one method with only two usages
    
    (cherry picked from commit 4119892)

--- Edu-Kotlin/src/com/jetbrains/edu/kotlin/twitter/KtTwitterConfigurator.java
+++ Edu-Kotlin/src/com/jetbrains/edu/kotlin/twitter/KtTwitterConfigurator.java
@@ -5,6 +5,7 @@ import com.jetbrains.edu.learning.StudyTaskManager;
 import com.jetbrains.edu.learning.StudyTwitterPluginConfigurator;
 import com.jetbrains.edu.learning.core.EduNames;
 import com.jetbrains.edu.learning.courseFormat.Course;
+import com.jetbrains.edu.learning.courseFormat.Lesson;
 import com.jetbrains.edu.learning.courseFormat.StudyStatus;
 import com.jetbrains.edu.learning.courseFormat.tasks.Task;
 import com.jetbrains.edu.learning.twitter.StudyTwitterUtils;
@@ -57,7 +58,7 @@ public class KtTwitterConfigurator implements StudyTwitterPluginConfigurator {
             return ktTwitterSettings.askToTweet()
                     && solvedTask.getStatus() == StudyStatus.Solved
                     && (statusBeforeCheck == StudyStatus.Unchecked || statusBeforeCheck == StudyStatus.Failed)
-                    && KotlinUtils.calculateTaskNumber(solvedTask) % 8 == 0;
+                    && calculateTaskNumber(solvedTask) % 8 == 0;
         }
         return false;
     }
@@ -79,5 +80,19 @@ public class KtTwitterConfigurator implements StudyTwitterPluginConfigurator {
         if (instance == null) return false;
         Course course = instance.getCourse();
         return course != null && EduNames.PYCHARM.equals(course.getCourseType()) && "kotlin".equals(course.getLanguage());
+    }
+
+    public static int calculateTaskNumber(@NotNull final Task solvedTask) {
+        Lesson lesson = solvedTask.getLesson();
+        Course course = lesson.getCourse();
+        int solvedTaskNumber = 0;
+        for (Lesson currentLesson: course.getLessons()) {
+            for (Task task: currentLesson.getTaskList()) {
+                if(task.getStatus() == StudyStatus.Solved) {
+                    solvedTaskNumber++;
+                }
+            }
+        }
+        return solvedTaskNumber;
     }
 }
