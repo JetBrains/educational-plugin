d98add4cd7acf1ee092ff228d930892de1adcf03
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2017-11-20 13:36:05 +0300

    EDU-1046: show interpreter version in 'Change Course Info' dialog only for python
    (cherry picked from commit 3156554)

--- Edu-Python/src/com/jetbrains/edu/python/coursecreator/PyCCChangeCourseInfo.java
+++ Edu-Python/src/com/jetbrains/edu/python/coursecreator/PyCCChangeCourseInfo.java
@@ -20,32 +20,35 @@ public class PyCCChangeCourseInfo extends CCChangeCourseInfo {
   }
 
   protected void setVersion(Course course, CCNewProjectPanel panel) {
-    String version = panel.getLanguageVersion();
-    String language = PythonLanguage.getInstance().getID();
-    if (version != null && !ALL_VERSIONS.equals(version)) {
-      language += " " + version;
+    if (PythonLanguage.getInstance().equals(course.getLanguageById())) {
+      String version = panel.getLanguageVersion();
+      String language = PythonLanguage.getInstance().getID();
+      if (version != null && !ALL_VERSIONS.equals(version)) {
+        language += " " + version;
+      }
+      course.setLanguage(language);
     }
-    course.setLanguage(language);
   }
 
   protected void setupLanguageLevels(Course course, CCNewProjectPanel panel) {
-    JLabel languageLevelLabel = panel.getLanguageLevelLabel();
-    languageLevelLabel.setText("Python:");
-    languageLevelLabel.setVisible(true);
-    ComboBox<String> languageLevelCombobox = panel.getLanguageLevelCombobox();
-    languageLevelCombobox.addItem(ALL_VERSIONS);
-    languageLevelCombobox.addItem(PYTHON_3);
-    languageLevelCombobox.addItem(PYTHON_2);
-    for (LanguageLevel level : LanguageLevel.values()) {
-      languageLevelCombobox.addItem(level.toString());
-    }
-    languageLevelCombobox.setVisible(true);
-    final String version = course.getLanguageVersion();
-    if (version != null) {
-      languageLevelCombobox.setSelectedItem(version);
-    }
-    else {
-      languageLevelCombobox.setSelectedItem(ALL_VERSIONS);
+    if (PythonLanguage.getInstance().equals(course.getLanguageById())) {
+      JLabel languageLevelLabel = panel.getLanguageLevelLabel();
+      languageLevelLabel.setText("Python:");
+      languageLevelLabel.setVisible(true);
+      ComboBox<String> languageLevelCombobox = panel.getLanguageLevelCombobox();
+      languageLevelCombobox.addItem(ALL_VERSIONS);
+      languageLevelCombobox.addItem(PYTHON_3);
+      languageLevelCombobox.addItem(PYTHON_2);
+      for (LanguageLevel level : LanguageLevel.values()) {
+        languageLevelCombobox.addItem(level.toString());
+      }
+      languageLevelCombobox.setVisible(true);
+      final String version = course.getLanguageVersion();
+      if (version != null) {
+        languageLevelCombobox.setSelectedItem(version);
+      } else {
+        languageLevelCombobox.setSelectedItem(ALL_VERSIONS);
+      }
     }
   }
 }
