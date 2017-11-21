2b8ba17e0a768558108e9bb5e288adf839f71391
Valentina Kiryushkina <valentina.kiryushkina@jetbrains.com>
2017-11-21 15:20:22 +0300

    EDU-1180 Remove obsolete code-mirror language scripts

--- Edu-Kotlin/src/com/jetbrains/edu/kotlin/KtLanguageDecorator.kt
+++ Edu-Kotlin/src/com/jetbrains/edu/kotlin/KtLanguageDecorator.kt
@@ -5,7 +5,7 @@ import org.jetbrains.kotlin.idea.KotlinIcons
 import javax.swing.Icon
 
 class KtLanguageDecorator : EduLanguageDecorator {
-  override fun getLanguageScriptUrl(): String = javaClass.getResource("/code_mirror/clike.js").toExternalForm()
+  override fun getLanguageScriptUrl(): String = javaClass.classLoader.getResource("/code-mirror/clike.js").toExternalForm()
   override fun getDefaultHighlightingMode(): String = "text/x-java"
   override fun getLogo(): Icon = KotlinIcons.SMALL_LOGO
 }
