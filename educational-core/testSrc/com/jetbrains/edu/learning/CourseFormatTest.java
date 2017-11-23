5aa538e11660349745e60d1ba302ce40bf268398
Ekaterina Tuzova <ekaterina.tuzova@jetbrains.com>
2017-11-23 14:34:35 +0300

    more tests
    (cherry picked from commit 2968918)

--- educational-core/testSrc/com/jetbrains/edu/learning/CourseFormatTest.java
+++ educational-core/testSrc/com/jetbrains/edu/learning/CourseFormatTest.java
@@ -8,24 +8,28 @@ import com.jetbrains.edu.learning.courseFormat.Course;
 import com.jetbrains.edu.learning.courseFormat.Lesson;
 import com.jetbrains.edu.learning.courseFormat.tasks.EduTask;
 import com.jetbrains.edu.learning.courseFormat.tasks.Task;
+import com.jetbrains.edu.learning.courseFormat.tasks.TaskWithSubtasks;
 import com.jetbrains.edu.learning.stepic.StepicNames;
 import org.jetbrains.annotations.NotNull;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestName;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
-import static junit.framework.TestCase.assertFalse;
-import static junit.framework.TestCase.assertNull;
-import static junit.framework.TestCase.assertTrue;
+import static junit.framework.TestCase.*;
 import static org.junit.Assert.assertNotNull;
 
 
 public class CourseFormatTest {
+  @Rule
+  public TestName name = new TestName();
+
   @Test
   public void testAdditionalMaterialsLesson() throws IOException {
-    final Course course = getCourseFromJson("course.json");
+    final Course course = getCourseFromJson();
     final List<Lesson> lessons = course.getLessons(true);
     final Lesson additional = lessons.stream().
         filter(lesson -> lesson.getName().equals(EduNames.ADDITIONAL_MATERIALS)).findFirst().orElse(null);
@@ -37,7 +41,7 @@ public class CourseFormatTest {
 
   @Test
   public void testPycharmToEduTask() throws IOException {
-    final Course course = getCourseFromJson("pycharmToEdu.json");
+    final Course course = getCourseFromJson();
     final List<Lesson> lessons = course.getLessons();
     assertFalse("No lessons found", lessons.isEmpty());
     final Lesson lesson = lessons.get(0);
@@ -46,7 +50,22 @@ public class CourseFormatTest {
     assertTrue(taskList.get(0) instanceof EduTask);
   }
 
-  private static Course getCourseFromJson(@NotNull final String fileName) throws IOException {
+  @Test
+  public void testSubtask() throws IOException {
+    final Course course = getCourseFromJson();
+    final List<Lesson> lessons = course.getLessons();
+    assertFalse("No lessons found", lessons.isEmpty());
+    final Lesson lesson = lessons.get(0);
+    final List<Task> taskList = lesson.getTaskList();
+    assertFalse("No tasks found", taskList.isEmpty());
+    final Task task = taskList.get(0);
+    assertTrue(task instanceof TaskWithSubtasks);
+    TaskWithSubtasks taskWithSubtasks = (TaskWithSubtasks) task;
+    assertEquals(1, taskWithSubtasks.getLastSubtaskIndex());
+  }
+
+  private Course getCourseFromJson() throws IOException {
+    final String fileName = getTestFile();
     String courseJson = FileUtil.loadFile(new File(getTestDataPath(), fileName));
 
     Gson gson = new GsonBuilder()
@@ -60,5 +79,13 @@ public class CourseFormatTest {
   @NotNull
   private static String getTestDataPath() {
     return FileUtil.join("testData/format");
+  }
+
+  @NotNull
+  private String getTestFile() {
+    final String methodName = name.getMethodName();
+    String fileName = methodName.substring("test".length());
+    fileName = Character.toLowerCase(fileName.charAt(0)) + fileName.substring(1);
+    return fileName + ".json";
   }
 }
