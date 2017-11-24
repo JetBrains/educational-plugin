f0d59eec8de8f87cb4caa9257d87680193b23ebd
Ekaterina Tuzova <ekaterina.tuzova@jetbrains.com>
2017-11-24 17:58:56 +0300

    test refresh current file action
    (cherry picked from commit 1174fc0)

---
+++ educational-core/testSrc/com/jetbrains/edu/learning/actions/RefreshTaskTest.java
@@ -0,0 +1,46 @@
+package com.jetbrains.edu.learning.actions;
+
+import com.jetbrains.edu.learning.EduTestCase;
+import com.jetbrains.edu.learning.StudyTaskManager;
+import com.jetbrains.edu.learning.courseFormat.Course;
+import com.jetbrains.edu.learning.courseFormat.Lesson;
+
+import java.io.IOException;
+
+public class RefreshTaskTest extends EduTestCase {
+
+  public void testRefreshTask() {
+    configureByTaskFile(1, 1, "taskFile1.txt");
+    myFixture.type("test");
+    myFixture.testAction(new RefreshTaskFileAction());
+
+    assertEquals("Look! There <placeholder hint=\"my hint\">i<caret>s</placeholder> placeholder.",
+        myFixture.getDocument(myFixture.getFile()).getText());
+  }
+
+  public void testCaretOutside() {
+    configureByTaskFile(1, 2, "taskFile2.txt");
+    myFixture.type("test");
+    myFixture.testAction(new RefreshTaskFileAction());
+
+    assertEquals("Look!<caret> There <placeholder hint=\"my hint\">is</placeholder> placeholder.",
+        myFixture.getDocument(myFixture.getFile()).getText());
+  }
+
+  @Override
+  protected void createCourse() throws IOException {
+    myFixture.copyDirectoryToProject("lesson1", "lesson1");
+    Course course = new Course();
+    course.setName("Edu test course");
+    StudyTaskManager.getInstance(myFixture.getProject()).setCourse(course);
+
+    Lesson lesson1 = createLesson(1);
+    course.addLesson(lesson1);
+    course.initCourse(false);
+  }
+
+  @Override
+  protected String getTestDataPath() {
+    return super.getTestDataPath() + "/actions/refresh";
+  }
+}
