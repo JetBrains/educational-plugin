45147a53131ca11870c497f7fda03f595d25a425
Ekaterina Tuzova <ekaterina.tuzova@jetbrains.com>
2017-11-24 20:08:31 +0300

    extract hint component from task toolwindow
    - there is no need to create full task description toolwindow to show only hint
    - added several tests on showHint
    
    (cherry picked from commit 2f7f82c)

---
+++ educational-core/testSrc/com/jetbrains/edu/learning/actions/ShowHintTest.java
@@ -0,0 +1,98 @@
+package com.jetbrains.edu.learning.actions;
+
+import com.jetbrains.edu.learning.EduState;
+import com.jetbrains.edu.learning.EduTestCase;
+import com.jetbrains.edu.learning.EduUtils;
+import com.jetbrains.edu.learning.StudyTaskManager;
+import com.jetbrains.edu.learning.courseFormat.AnswerPlaceholder;
+import com.jetbrains.edu.learning.courseFormat.Course;
+import com.jetbrains.edu.learning.courseFormat.Lesson;
+import com.jetbrains.edu.learning.ui.AnswerPlaceholderHint;
+import com.jetbrains.edu.learning.ui.HintComponent;
+
+import java.io.IOException;
+
+public class ShowHintTest extends EduTestCase {
+  public void testOneHint() {
+    configureByTaskFile(1, 1, "taskFile1.txt");
+    EduState eduState = new EduState(EduUtils.getSelectedStudyEditor(myFixture.getProject()));
+    int offset = myFixture.getCaretOffset();
+    AnswerPlaceholder answerPlaceholder = eduState.getTaskFile().getAnswerPlaceholder(offset);
+
+    final AnswerPlaceholderHint hint = ShowHintAction.getHint(myFixture.getProject(), answerPlaceholder);
+    final HintComponent hintContent = hint.getHintComponent();
+    final String text = hintContent.getText();
+    assertEquals("my hint", text.trim());
+  }
+
+  public void testOutsidePlaceholder() {
+    configureByTaskFile(1, 2, "taskFile2.txt");
+    EduState eduState = new EduState(EduUtils.getSelectedStudyEditor(myFixture.getProject()));
+    int offset = myFixture.getCaretOffset();
+    AnswerPlaceholder answerPlaceholder = eduState.getTaskFile().getAnswerPlaceholder(offset);
+
+    final AnswerPlaceholderHint hint = ShowHintAction.getHint(myFixture.getProject(), answerPlaceholder);
+    final HintComponent hintContent = hint.getHintComponent();
+    final String text = hintContent.getText();
+    assertEquals(AnswerPlaceholderHint.Companion.getNO_PLACEHOLDER_MESSAGE(), text.trim());
+  }
+
+  public void testSecondHints() {
+    configureByTaskFile(1, 3, "taskFile3.txt");
+    EduState eduState = new EduState(EduUtils.getSelectedStudyEditor(myFixture.getProject()));
+    int offset = myFixture.getCaretOffset();
+    AnswerPlaceholder answerPlaceholder = eduState.getTaskFile().getAnswerPlaceholder(offset);
+
+    final AnswerPlaceholderHint hint = ShowHintAction.getHint(myFixture.getProject(), answerPlaceholder);
+    final AnswerPlaceholderHint.GoForward goForward = hint.getGoForwardAction();
+    myFixture.testAction(goForward);
+    final HintComponent hintContent = hint.getHintComponent();
+    final String text = hintContent.getText();
+    assertEquals("my hint2", text.trim());
+  }
+
+  public void testGoBackwardHints() {
+    configureByTaskFile(1, 3, "taskFile3.txt");
+    EduState eduState = new EduState(EduUtils.getSelectedStudyEditor(myFixture.getProject()));
+    int offset = myFixture.getCaretOffset();
+    AnswerPlaceholder answerPlaceholder = eduState.getTaskFile().getAnswerPlaceholder(offset);
+
+    final AnswerPlaceholderHint hint = ShowHintAction.getHint(myFixture.getProject(), answerPlaceholder);
+    final AnswerPlaceholderHint.GoForward goForward = hint.getGoForwardAction();
+    myFixture.testAction(goForward);
+    myFixture.testAction(hint.getGoBackwardAction());
+    final HintComponent hintContent = hint.getHintComponent();
+    final String text = hintContent.getText();
+    assertEquals("my hint1", text.trim());
+  }
+
+  public void testNoHints() {
+    configureByTaskFile(1, 4, "taskFile4.txt");
+    EduState eduState = new EduState(EduUtils.getSelectedStudyEditor(myFixture.getProject()));
+    int offset = myFixture.getCaretOffset();
+    AnswerPlaceholder answerPlaceholder = eduState.getTaskFile().getAnswerPlaceholder(offset);
+
+    final AnswerPlaceholderHint hint = ShowHintAction.getHint(myFixture.getProject(), answerPlaceholder);
+    final HintComponent hintContent = hint.getHintComponent();
+    final String text = hintContent.getText();
+    assertEquals(AnswerPlaceholderHint.Companion.getHINTS_NOT_AVAILABLE(), text.trim());
+  }
+
+  @Override
+  protected void createCourse() throws IOException {
+    myFixture.copyDirectoryToProject("lesson1", "lesson1");
+    Course course = new Course();
+    course.setLanguage("JAVA");
+    course.setName("Edu test course");
+    StudyTaskManager.getInstance(myFixture.getProject()).setCourse(course);
+
+    Lesson lesson1 = createLesson(1, 4);
+    course.addLesson(lesson1);
+    course.initCourse(false);
+  }
+
+  @Override
+  protected String getTestDataPath() {
+    return super.getTestDataPath() + "/actions/showHint";
+  }
+}
