141c30f198868d5d07537ad7045fda33b8de2e01
Ekaterina Tuzova <ekaterina.tuzova@jetbrains.com>
2017-11-27 15:28:46 +0300

    fix unpack course action for module-based projects
    TODO:
    - remove CCFromCourseArchive completely
    - make fields in unpack dialog unmodifiable
    
    (cherry picked from commit 20c3cc6)

---
+++ educational-core/src/com/jetbrains/edu/coursecreator/actions/CCUnpackCourseArchive.kt
@@ -0,0 +1,26 @@
+package com.jetbrains.edu.coursecreator.actions
+
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.fileChooser.FileChooser
+import com.intellij.openapi.fileChooser.FileChooserDescriptor
+import com.intellij.openapi.project.DumbAwareAction
+import com.intellij.openapi.ui.Messages
+import com.jetbrains.edu.coursecreator.ui.CCUnpackCourseDialog
+import com.jetbrains.edu.learning.EduUtils
+
+class CCUnpackCourseArchive : DumbAwareAction("Unpack Course From Archive") {
+  override fun actionPerformed(e: AnActionEvent) {
+    val project = e.getData(CommonDataKeys.PROJECT)
+    val descriptor = FileChooserDescriptor(true, true, true, true,
+                                           true, false)
+    val virtualFile = FileChooser.chooseFile(descriptor, project, null) ?: return
+    val course = EduUtils.getLocalCourse(virtualFile.path)
+    if (course == null) {
+      Messages.showErrorDialog("This course is incompatible with current version", "Failed to Unpack Course")
+      return
+    }
+
+    CCUnpackCourseDialog(course).show()
+  }
+}
