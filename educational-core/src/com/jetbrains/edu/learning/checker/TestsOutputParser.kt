5feb4560f1a431b804da2bb6f467dae33b4b784a
Natalia Selezneva <natalia.selezneva@jetbrains.com>
2017-11-29 11:08:32 +0300

    Check task should fail when there is compilation error in java file

--- educational-core/src/com/jetbrains/edu/learning/checker/TestsOutputParser.kt
+++ educational-core/src/com/jetbrains/edu/learning/checker/TestsOutputParser.kt
@@ -1,7 +1,9 @@
 package com.jetbrains.edu.learning.checker
 
 import com.intellij.execution.process.ProcessOutput
-import com.jetbrains.edu.learning.checker.CheckUtils.*
+import com.intellij.openapi.diagnostic.Logger
+import com.jetbrains.edu.learning.checker.CheckUtils.COMPILATION_FAILED_MESSAGE
+import com.jetbrains.edu.learning.checker.CheckUtils.STUDY_PREFIX
 
 
 object TestsOutputParser {
@@ -10,50 +12,50 @@ object TestsOutputParser {
     private const val CONGRATS_MESSAGE = "CONGRATS_MESSAGE "
     const val CONGRATULATIONS = "Congratulations!"
 
+    private val LOG = Logger.getInstance(TestsOutputParser::class.qualifiedName!!)
+
     class TestsOutput(val isSuccess: Boolean, val message: String)
 
     @JvmStatic
     fun getTestsOutput(processOutput: ProcessOutput, isAdaptive: Boolean): TestsOutput {
         //gradle prints compilation failures to error stream
-        if (hasCompilationErrors(processOutput)) {
+        if (CheckUtils.hasCompilationErrors(processOutput)) {
+            LOG.info(processOutput.stderr)
             return TestsOutput(false, COMPILATION_FAILED_MESSAGE)
         }
+
+        val lines = processOutput.stdoutLines.filter { it.startsWith(STUDY_PREFIX) }
         var congratulations = CONGRATULATIONS
-        val lines = processOutput.stdoutLines
         for ((i, line) in lines.withIndex()) {
-            if (line.startsWith(STUDY_PREFIX)) {
-                if (line.contains(TEST_OK)) {
-                    continue
-                }
+            if (line.contains(TEST_OK)) {
+                continue
+            }
 
-                if (line.contains(CONGRATS_MESSAGE)) {
-                    congratulations = line.substringAfter(CONGRATS_MESSAGE)
-                }
+            if (line.contains(CONGRATS_MESSAGE)) {
+                congratulations = line.substringAfter(CONGRATS_MESSAGE)
+            }
 
-                if (line.contains(TEST_FAILED)) {
-                    if (!isAdaptive) {
-                        return TestsOutput(false, line.substringAfter(TEST_FAILED))
-                    }
-                    else {
-                        val builder = StringBuilder(line.substringAfter(TEST_FAILED) + "\n")
-                        for (j in i + 1 until lines.size) {
-                            val failedTextLine = lines[j]
-                            if (!failedTextLine.contains(STUDY_PREFIX) || !failedTextLine.contains(CONGRATS_MESSAGE)) {
-                                builder.append(failedTextLine)
-                                builder.append("\n")
-                            }
-                            else {
-                                break
-                            }
+            if (line.contains(TEST_FAILED)) {
+                if (!isAdaptive) {
+                    return TestsOutput(false, line.substringAfter(TEST_FAILED))
+                }
+                else {
+                    val builder = StringBuilder(line.substringAfter(TEST_FAILED) + "\n")
+                    for (j in i + 1 until lines.size) {
+                        val failedTextLine = lines[j]
+                        if (!failedTextLine.contains(STUDY_PREFIX) || !failedTextLine.contains(CONGRATS_MESSAGE)) {
+                            builder.append(failedTextLine)
+                            builder.append("\n")
                         }
-                        return TestsOutput(false, builder.toString())
+                        else {
+                            break
+                        }
                     }
+                    return TestsOutput(false, builder.toString())
                 }
             }
         }
 
         return TestsOutput(true, congratulations)
     }
-
-    private fun hasCompilationErrors(processOutput: ProcessOutput) = processOutput.stderrLines.any { it.contains(BUILD_FAILED) }
 }
