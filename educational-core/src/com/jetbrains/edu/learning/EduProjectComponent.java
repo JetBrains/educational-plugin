73dd5537c87c6dc708ea5381a5208dca8a377251
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2017-11-29 15:35:59 +0300

    EDU-1196: simplify code in EduProjectComponent
    (cherry picked from commit 76f0e7f)

--- educational-core/src/com/jetbrains/edu/learning/EduProjectComponent.java
+++ educational-core/src/com/jetbrains/edu/learning/EduProjectComponent.java
@@ -31,8 +31,10 @@ import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.startup.StartupManager;
 import com.intellij.openapi.util.Pair;
-import com.intellij.openapi.util.io.FileUtil;
-import com.intellij.openapi.vfs.*;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.vfs.VirtualFileAdapter;
+import com.intellij.openapi.vfs.VirtualFileEvent;
+import com.intellij.openapi.vfs.VirtualFileManager;
 import com.intellij.openapi.wm.StatusBar;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowManager;
@@ -50,10 +52,6 @@ import com.jetbrains.edu.learning.courseGeneration.GeneratorUtils;
 import com.jetbrains.edu.learning.editor.EduEditorFactoryListener;
 import com.jetbrains.edu.learning.statistics.EduUsagesCollector;
 import com.jetbrains.edu.learning.stepic.*;
-import com.jetbrains.edu.learning.stepic.StepicConnector;
-import com.jetbrains.edu.learning.stepic.StepicNames;
-import com.jetbrains.edu.learning.stepic.StepicUserWidget;
-import com.jetbrains.edu.learning.stepic.StepikSolutionsLoader;
 import com.jetbrains.edu.learning.ui.taskDescription.TaskDescriptionToolWindow;
 import com.jetbrains.edu.learning.ui.taskDescription.TaskDescriptionToolWindowFactory;
 import org.jetbrains.annotations.NotNull;
@@ -401,34 +399,16 @@ public class EduProjectComponent implements ProjectComponent {
     @Override
     public void fileCreated(@NotNull VirtualFileEvent event) {
       if (myProject.isDisposed()) return;
+      if (!isStudentProject(myProject)) return;
       final VirtualFile createdFile = event.getFile();
-      final VirtualFile taskDir = EduUtils.getTaskDir(createdFile);
-      final Course course = StudyTaskManager.getInstance(myProject).getCourse();
-      if (course == null || !course.isStudy()) {
-        return;
-      }
-      if (taskDir != null && taskDir.getName().contains(EduNames.TASK)) {
-        int taskIndex = EduUtils.getIndex(taskDir.getName(), EduNames.TASK);
-        final VirtualFile lessonDir = taskDir.getParent();
-        if (lessonDir != null && lessonDir.getName().contains(EduNames.LESSON)) {
-          int lessonIndex = EduUtils.getIndex(lessonDir.getName(), EduNames.LESSON);
-          List<Lesson> lessons = course.getLessons();
-          if (EduUtils.indexIsValid(lessonIndex, lessons)) {
-            final Lesson lesson = lessons.get(lessonIndex);
-            final List<Task> tasks = lesson.getTaskList();
-            if (EduUtils.indexIsValid(taskIndex, tasks)) {
-              final Task task = tasks.get(taskIndex);
-              final TaskFile taskFile = new TaskFile();
-              taskFile.initTaskFile(task, false);
-              taskFile.setUserCreated(true);
-              final String name = FileUtil.getRelativePath(taskDir.getPath(), createdFile.getPath(), '/');
-              taskFile.name = name;
-              //TODO: put to other steps as well
-              task.getTaskFiles().put(name, taskFile);
-            }
-          }
-        }
-      }
+      final Task task = getTaskForFile(myProject, createdFile);
+      if (task == null) return;
+      final TaskFile taskFile = new TaskFile();
+      taskFile.initTaskFile(task, false);
+      taskFile.setUserCreated(true);
+      final String name = pathRelativeToTask(createdFile);
+      taskFile.name = name;
+      task.getTaskFiles().put(name, taskFile);
     }
   }
 
