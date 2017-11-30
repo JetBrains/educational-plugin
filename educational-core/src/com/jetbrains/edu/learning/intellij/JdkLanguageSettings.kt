6f259101dac806bc4525db1d718c8c7f8aee733a
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2017-11-30 19:31:49 +0300

    EDU-1194: correctly update sdks after project creation
    (cherry picked from commit 8d75629)

--- educational-core/src/com/jetbrains/edu/learning/intellij/JdkLanguageSettings.kt
+++ educational-core/src/com/jetbrains/edu/learning/intellij/JdkLanguageSettings.kt
@@ -2,13 +2,13 @@ package com.jetbrains.edu.learning.intellij
 
 import com.intellij.openapi.project.ProjectManager
 import com.intellij.openapi.projectRoots.JavaSdkType
+import com.intellij.openapi.projectRoots.Sdk
 import com.intellij.openapi.projectRoots.SdkTypeId
 import com.intellij.openapi.roots.ui.configuration.JdkComboBox
 import com.intellij.openapi.roots.ui.configuration.ProjectStructureConfigurable
 import com.intellij.openapi.roots.ui.configuration.projectRoot.ProjectSdksModel
 import com.intellij.openapi.ui.LabeledComponent
 import com.intellij.openapi.util.Condition
-import com.intellij.openapi.util.Conditions
 import com.intellij.ui.ComboboxWithBrowseButton
 import com.jetbrains.edu.learning.EduCourseBuilder
 import com.jetbrains.edu.learning.courseFormat.Course
@@ -17,12 +17,18 @@ import javax.swing.JComponent
 
 internal class JdkLanguageSettings : EduCourseBuilder.LanguageSettings<JdkProjectSettings> {
 
-  private val myModel: ProjectSdksModel = ProjectStructureConfigurable.getInstance(ProjectManager.getInstance().defaultProject).projectJdksModel
+  private val myModel: ProjectSdksModel = createSdkModel()
   private var myJdkSettings: JdkProjectSettings = JdkProjectSettings(myModel, null)
+
+  private fun createSdkModel(): ProjectSdksModel {
+    val project = ProjectManager.getInstance().defaultProject
+    return ProjectStructureConfigurable.getInstance(project).projectJdksModel.apply { reset(project) }
+  }
 
   override fun getLanguageSettingsComponent(course: Course): LabeledComponent<JComponent> {
     val sdkTypeFilter = Condition<SdkTypeId> { sdkTypeId -> sdkTypeId is JavaSdkType && !(sdkTypeId as JavaSdkType).isDependent }
-    val jdkComboBox = JdkComboBox(myModel, sdkTypeFilter, Conditions.alwaysTrue(), sdkTypeFilter, true)
+    val sdkFilter = Condition<Sdk> { sdk -> sdkTypeFilter.value(sdk.sdkType) }
+    val jdkComboBox = JdkComboBox(myModel, sdkTypeFilter, sdkFilter, sdkTypeFilter, true)
     val comboboxWithBrowseButton = ComboboxWithBrowseButton(jdkComboBox)
     val setupButton = comboboxWithBrowseButton.button
     jdkComboBox.setSetupButton(setupButton, null, myModel, jdkComboBox.selectedItem, null, false)
