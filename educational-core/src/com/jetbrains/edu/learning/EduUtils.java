ea120352b6ad1a0729c721085947bc1f16c54d2e
Ekaterina Tuzova <ekaterina.tuzova@jetbrains.com>
2017-12-04 12:56:52 +0300

    update featured courses link
    (cherry picked from commit 1ce961a)

--- educational-core/src/com/jetbrains/edu/learning/EduUtils.java
+++ educational-core/src/com/jetbrains/edu/learning/EduUtils.java
@@ -107,7 +107,7 @@ import static com.jetbrains.edu.learning.navigation.NavigationUtils.navigateToTa
 
 public class EduUtils {
 
-  private static final String PROMOTED_COURSES_LINK = "https://raw.githubusercontent.com/JetBrains/educational-plugin/master/educational-core/resources/featured_courses.txt";
+  private static final String PROMOTED_COURSES_LINK = "https://raw.githubusercontent.com/JetBrains/educational-plugin/master/featured_courses.txt";
 
   private EduUtils() {
   }
