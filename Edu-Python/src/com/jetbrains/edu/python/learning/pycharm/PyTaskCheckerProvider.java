5bc4bf84ddc6b68c5ea063830c9eecdf9490bc74
Natalia Selezneva <natalia.selezneva@jetbrains.com>
2017-12-07 09:45:06 +0300

    Implement PyTaskCheckerProvider

--- Edu-Python/src/com/jetbrains/edu/python/learning/pycharm/PyTaskCheckerProvider.java
+++ Edu-Python/src/com/jetbrains/edu/python/learning/pycharm/PyTaskCheckerProvider.java
@@ -1,29 +1,32 @@
 package com.jetbrains.edu.python.learning.pycharm;
 
 import com.intellij.openapi.project.Project;
+import com.jetbrains.edu.learning.checker.OutputTaskChecker;
 import com.jetbrains.edu.learning.checker.TaskChecker;
 import com.jetbrains.edu.learning.checker.TaskCheckerProvider;
+import com.jetbrains.edu.learning.checker.TheoryTaskChecker;
 import com.jetbrains.edu.learning.courseFormat.tasks.EduTask;
 import com.jetbrains.edu.learning.courseFormat.tasks.OutputTask;
 import com.jetbrains.edu.learning.courseFormat.tasks.TheoryTask;
+import com.jetbrains.edu.python.learning.checker.PyTaskChecker;
 import org.jetbrains.annotations.NotNull;
 
 public class PyTaskCheckerProvider implements TaskCheckerProvider {
     @NotNull
     @Override
     public TaskChecker<EduTask> getEduTaskChecker(@NotNull EduTask task, @NotNull Project project) {
-        return null;
+        return new PyTaskChecker(task, project);
     }
 
     @NotNull
     @Override
     public TaskChecker<OutputTask> getOutputTaskChecker(@NotNull OutputTask task, @NotNull Project project) {
-        return null;
+        return new OutputTaskChecker(task, project);
     }
 
     @NotNull
     @Override
     public TaskChecker<TheoryTask> getTheoryTaskChecker(@NotNull TheoryTask task, @NotNull Project project) {
-        return null;
+        return new TheoryTaskChecker(task, project);
     }
 }
