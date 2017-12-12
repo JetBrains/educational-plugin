77ad4c22823e6cab0749b6b5103a00e781d32063
Ekaterina Tuzova <ekaterina.tuzova@jetbrains.com>
2017-12-12 13:50:50 +0300

    merge fix

--- educational-core/src/com/jetbrains/edu/learning/statistics/PostFeedbackCheckListener.kt
+++ educational-core/src/com/jetbrains/edu/learning/statistics/PostFeedbackCheckListener.kt
@@ -2,13 +2,12 @@ package com.jetbrains.edu.learning.statistics
 
 import com.intellij.openapi.project.Project
 import com.jetbrains.edu.learning.checker.CheckListener
-import com.jetbrains.edu.learning.checker.CheckResult
 import com.jetbrains.edu.learning.courseFormat.tasks.Task
 import com.jetbrains.edu.learning.ui.taskDescription.TaskDescriptionToolWindow
 
 
 class PostFeedbackCheckListener : CheckListener {
-  override fun afterCheck(project: Project, task: Task, result: CheckResult) {
+  override fun afterCheck(project: Project, task: Task) {
     if (isFeedbackAsked()) {
       return
     }
