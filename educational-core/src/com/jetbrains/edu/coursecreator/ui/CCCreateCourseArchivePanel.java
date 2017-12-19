a579343f2ea119fd34c1dff8d6e1b88646e5ee42
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2017-12-19 17:22:47 +0300

    EDU-1216: save last archive location

--- educational-core/src/com/jetbrains/edu/coursecreator/ui/CCCreateCourseArchivePanel.java
+++ educational-core/src/com/jetbrains/edu/coursecreator/ui/CCCreateCourseArchivePanel.java
@@ -1,13 +1,16 @@
 package com.jetbrains.edu.coursecreator.ui;
 
 import com.intellij.icons.AllIcons;
+import com.intellij.ide.util.PropertiesComponent;
 import com.intellij.openapi.fileChooser.FileChooserDescriptor;
 import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.TextFieldWithBrowseButton;
 import com.intellij.openapi.util.io.FileUtil;
+import com.jetbrains.edu.coursecreator.actions.CCCreateCourseArchive;
 import com.jetbrains.edu.learning.EduNames;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import java.awt.*;
@@ -31,7 +34,7 @@ public class CCCreateCourseArchivePanel extends JPanel {
     myDlg = dlg;
     String sanitizedName = FileUtil.sanitizeFileName(name);
     myNameField.setText(sanitizedName.startsWith("_") ? EduNames.COURSE : sanitizedName);
-    myLocationField.setText(project.getBasePath());
+    myLocationField.setText(getArchiveLocation(project));
     FileChooserDescriptor descriptor = FileChooserDescriptorFactory.createSingleFolderDescriptor();
     myLocationField.addBrowseFolderListener("Choose Location Folder", null, project, descriptor);
     myLocationField.addActionListener(new ActionListener() {
@@ -64,5 +67,11 @@ public class CCCreateCourseArchivePanel extends JPanel {
 
   public String getLocationPath() {
     return myLocationField.getText();
+  }
+
+  @Nullable
+  private static String getArchiveLocation(@NotNull Project project) {
+    String location = PropertiesComponent.getInstance(project).getValue(CCCreateCourseArchive.LAST_ARCHIVE_LOCATION);
+    return location == null ? project.getBasePath() : location;
   }
 }
