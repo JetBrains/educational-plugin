f5472bd560f0241cda1cb07875e728731ff5410f
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2017-12-22 17:08:11 +0300

    fix EDU-1248 KtOptionsProvider throws exception if no Kotlin project opened
    made EduConfigurable project configurable
    
    (cherry picked from commit eca1b54)

--- educational-core/src/com/jetbrains/edu/learning/settings/OptionsProvider.java
+++ educational-core/src/com/jetbrains/edu/learning/settings/OptionsProvider.java
@@ -1,6 +1,9 @@
 package com.jetbrains.edu.learning.settings;
 
-import com.intellij.openapi.options.UnnamedConfigurable;
+import com.intellij.openapi.extensions.ExtensionPointName;
+import com.intellij.openapi.options.Configurable;
+import com.intellij.openapi.options.ConfigurableEP;
 
-public interface OptionsProvider extends UnnamedConfigurable {
+public interface OptionsProvider extends Configurable {
+  ExtensionPointName<ConfigurableEP<OptionsProvider>> EP_NAME = ExtensionPointName.create("Educational.optionsProvider");
 }
