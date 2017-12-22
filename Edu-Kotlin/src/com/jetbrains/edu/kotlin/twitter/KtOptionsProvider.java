eca1b544bd8847e24d66c14df196d590bad78d1c
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2017-12-22 17:08:11 +0300

    fix EDU-1248 KtOptionsProvider throws exception if no Kotlin project opened
    made EduConfigurable project configurable

--- Edu-Kotlin/src/com/jetbrains/edu/kotlin/twitter/KtOptionsProvider.java
+++ Edu-Kotlin/src/com/jetbrains/edu/kotlin/twitter/KtOptionsProvider.java
@@ -1,60 +1,68 @@
 package com.jetbrains.edu.kotlin.twitter;
+
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.project.ProjectManager;
 import com.intellij.ui.components.JBCheckBox;
-import com.jetbrains.edu.learning.StudyTaskManager;
-import com.jetbrains.edu.learning.twitter.TwitterPluginConfigurator;
 import com.jetbrains.edu.learning.EduUtils;
+import com.jetbrains.edu.learning.StudyTaskManager;
 import com.jetbrains.edu.learning.settings.OptionsProvider;
+import com.jetbrains.edu.learning.twitter.TwitterPluginConfigurator;
+import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 
 
 public class KtOptionsProvider implements OptionsProvider {
-  private KtTwitterSettings twitterSettings;
+  private final Project myProject;
+  private KtTwitterSettings myTwitterSettings;
   private JBCheckBox myAskToTweetCheckBox;
   private JPanel myPanel;
   private boolean myIsModified = false;
 
-  KtOptionsProvider() {
-    Project[] openProjects = ProjectManager.getInstance().getOpenProjects();
-    for (Project project : openProjects) {
-      if (StudyTaskManager.getInstance(project).getCourse() != null) {
-        TwitterPluginConfigurator twitterConfigurator = EduUtils.getTwitterConfigurator(project);
-        if (twitterConfigurator != null) {
-          twitterSettings = KtTwitterSettings.getInstance(project);
-          myAskToTweetCheckBox.setSelected(twitterSettings.askToTweet());
-          break;
-        }
-      }
+  KtOptionsProvider(@NotNull final Project project) {
+    myProject = project;
+    myTwitterSettings = KtTwitterSettings.getInstance(myProject);
+    TwitterPluginConfigurator twitterConfigurator = EduUtils.getTwitterConfigurator(myProject);
+    if (twitterConfigurator != null) {
+      myAskToTweetCheckBox.setSelected(myTwitterSettings.askToTweet());
     }
     myAskToTweetCheckBox.addActionListener(e -> myIsModified = true);
   }
 
   @Override
   public void apply() {
-    twitterSettings.setAskToTweet(myAskToTweetCheckBox.isSelected());
+    myTwitterSettings.setAskToTweet(myAskToTweetCheckBox.isSelected());
   }
 
   @Override
   public void reset() {
-    twitterSettings.setAskToTweet(true);
+    myTwitterSettings.setAskToTweet(true);
   }
 
   @Override
   public void disposeUIResources() {
-
   }
 
   @Nullable
   @Override
   public JComponent createComponent() {
-    return myPanel;
+    final boolean hasCourse = StudyTaskManager.getInstance(myProject).getCourse() != null;
+    TwitterPluginConfigurator twitterConfigurator = EduUtils.getTwitterConfigurator(myProject);
+    if (hasCourse && twitterConfigurator != null) {
+      return myPanel;
+    }
+    return null;
   }
 
   @Override
   public boolean isModified() {
     return myIsModified;
+  }
+
+  @Nls
+  @Override
+  public String getDisplayName() {
+    return "Twitter Settings";
   }
 }
--- educational-core/src/com/jetbrains/edu/learning/settings/OptionsProviderEP.java
+++
@@ -1,9 +0,0 @@
-package com.jetbrains.edu.learning.settings;
-
-import com.intellij.openapi.extensions.ExtensionPointName;
-import com.intellij.openapi.options.ConfigurableEP;
-
-public class OptionsProviderEP extends ConfigurableEP<OptionsProvider> {
-  public static final ExtensionPointName<OptionsProviderEP>
-    EP_NAME = ExtensionPointName.create("Educational.optionsProvider");
-}
