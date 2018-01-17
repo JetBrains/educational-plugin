0ca2e4cdfa1a41a4deb1bd57e016276485ee3f25
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2018-01-17 19:06:56 +0300

    do not wrap code in html in case we have attributes in tag
    (cherry picked from commit 12ac23a)
    
    (cherry picked from commit 3892eb1)

--- educational-core/src/com/jetbrains/edu/learning/EduUtils.java
+++ educational-core/src/com/jetbrains/edu/learning/EduUtils.java
@@ -568,7 +568,8 @@ public class EduUtils {
 
   public static String convertToHtml(@NotNull String content) {
     ArrayList<String> lines = ContainerUtil.newArrayList(content.split("\n|\r|\r\n"));
-    if ((content.contains("<h") && content.contains("</h")) || (content.contains("<code>") && content.contains("</code>"))) {
+    if ((content.contains("<h") && content.contains("</h")) ||
+        ((content.contains("<code>") || content.contains("<code ")) && content.contains("</code>"))) {
       return content;
     }
     MarkdownUtil.replaceHeaders(lines);
