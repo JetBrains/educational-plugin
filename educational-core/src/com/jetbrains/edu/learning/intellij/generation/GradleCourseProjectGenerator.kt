4a15ad73a6e733beff0eff57142fa5daa6cbe2da
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2018-01-26 15:07:24 +0300

    EDU-1281 & EDU-1308: create course structure before gradle import
    (cherry picked from commit 90c15ae)

--- educational-core/src/com/jetbrains/edu/learning/intellij/generation/GradleCourseProjectGenerator.kt
+++ educational-core/src/com/jetbrains/edu/learning/intellij/generation/GradleCourseProjectGenerator.kt
@@ -1,20 +1,13 @@
 package com.jetbrains.edu.learning.intellij.generation
 
-import com.intellij.ide.impl.NewProjectUtil
-import com.intellij.ide.util.newProjectWizard.AddModuleWizard
-import com.intellij.openapi.application.WriteAction
 import com.intellij.openapi.application.runWriteAction
 import com.intellij.openapi.diagnostic.Logger
-import com.intellij.openapi.externalSystem.service.project.manage.ProjectDataManager
 import com.intellij.openapi.options.ConfigurationException
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.projectRoots.Sdk
 import com.intellij.openapi.roots.ProjectRootManager
 import com.intellij.openapi.roots.ui.configuration.JdkComboBox
 import com.intellij.openapi.util.Ref
-import com.intellij.openapi.util.io.FileUtil
-import com.intellij.openapi.vfs.LocalFileSystem
-import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.util.Consumer
 import com.jetbrains.edu.coursecreator.CCUtils
@@ -26,49 +19,12 @@ import com.jetbrains.edu.learning.courseGeneration.GeneratorUtils
 import com.jetbrains.edu.learning.intellij.GradleCourseBuilderBase
 import com.jetbrains.edu.learning.intellij.JdkProjectSettings
 import com.jetbrains.edu.learning.newproject.CourseProjectGenerator
-import org.jetbrains.plugins.gradle.service.project.wizard.GradleProjectImportBuilder
-import org.jetbrains.plugins.gradle.service.project.wizard.GradleProjectImportProvider
-import java.io.File
 import java.io.IOException
 
 open class GradleCourseProjectGenerator(
   private val myCourseBuilder: GradleCourseBuilderBase,
   course: Course
 ) : CourseProjectGenerator<JdkProjectSettings>(course) {
-
-  override fun createProject(location: String, projectSettings: Any): Project? {
-    val locationFile = File(FileUtil.toSystemDependentName(location))
-    if (!locationFile.exists() && !locationFile.mkdirs()) return null
-
-    val baseDir = WriteAction.compute<VirtualFile, RuntimeException> {
-      LocalFileSystem.getInstance().refreshAndFindFileByIoFile(locationFile)
-    }
-    if (baseDir == null) {
-      LOG.error("Couldn't find '$locationFile' in VFS")
-      return null
-    }
-    VfsUtil.markDirtyAndRefresh(false, true, true, baseDir)
-
-    val isGradleFilesCreated = WriteAction.compute<Boolean, RuntimeException> {
-      try {
-        EduGradleModuleGenerator.createProjectGradleFiles(location, locationFile.name, myCourseBuilder.buildGradleTemplateName)
-        true
-      } catch (e: IOException) {
-        LOG.error("Failed to generate project with gradle", e)
-        false
-      }
-    }
-    if (!isGradleFilesCreated) return null
-
-    val projectDataManager = ProjectDataManager.getInstance()
-    val gradleProjectImportBuilder = GradleProjectImportBuilder(projectDataManager)
-    val gradleProjectImportProvider = GradleProjectImportProvider(gradleProjectImportBuilder)
-    val wizard = AddModuleWizard(null, baseDir.path, gradleProjectImportProvider)
-    val project = NewProjectUtil.createFromWizard(wizard, null) ?: return null
-
-    createCourseStructure(project, baseDir, projectSettings as JdkProjectSettings)
-    return project
-  }
 
   override fun createCourseStructure(project: Project, baseDir: VirtualFile, settings: JdkProjectSettings) {
     runWriteAction {
@@ -81,6 +37,7 @@ open class GradleCourseProjectGenerator(
           lesson.addTask(task)
           myCourseBuilder.initNewTask(task)
         }
+        EduGradleModuleGenerator.createProjectGradleFiles(project.basePath!!, project.name, myCourseBuilder.buildGradleTemplateName)
         GeneratorUtils.createCourse(myCourse, baseDir)
 
         setJdk(project, settings)
