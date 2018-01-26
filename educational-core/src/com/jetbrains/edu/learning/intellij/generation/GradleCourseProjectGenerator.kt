6118fc8afe0781beca4a8853fd4ad62990fbb315
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2018-01-26 18:26:22 +0300

    EDU-1281: don't create two root modules
    (cherry picked from commit 8d295ec)

--- educational-core/src/com/jetbrains/edu/learning/intellij/generation/GradleCourseProjectGenerator.kt
+++ educational-core/src/com/jetbrains/edu/learning/intellij/generation/GradleCourseProjectGenerator.kt
@@ -5,6 +5,7 @@ import com.intellij.openapi.application.runWriteAction
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.options.ConfigurationException
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.modifyModules
 import com.intellij.openapi.projectRoots.Sdk
 import com.intellij.openapi.roots.ProjectRootManager
 import com.intellij.openapi.roots.ui.configuration.JdkComboBox
@@ -28,7 +29,27 @@ open class GradleCourseProjectGenerator(
 ) : CourseProjectGenerator<JdkProjectSettings>(course) {
 
   override fun createCourseStructure(project: Project, baseDir: VirtualFile, settings: JdkProjectSettings) {
+    // Hack!
+    // We rename all modules (really only root module because new project has only one root module)
+    // to have names which are expected by gradle importer.
+    //
+    // We do these hacky things to avoid the following situation:
+    // If project dir contains some symbols (' ', '/', '\', ':', '<', '>', '"', '?', '*', '|')  in its name (for example, `Awesome Course`)
+    // then after project creation we will get `Awesome Course` root module.
+    // But gradle importer won't find it because it expects `Awesome_Course` module
+    // and it'll create new root module.
+    // After project reopening we will get an exception because of two modules with same content.
+    //
+    // Note we don't create gradle project from the beginning
+    // because it is much slower and prevents showing project content at the beginning
+    project.modifyModules {
+      for (module in modules) {
+        renameModule(module, sanitizeName(module.name))
+      }
+    }
+
     PropertiesComponent.getInstance(project).setValue(SHOW_UNLINKED_GRADLE_POPUP, false, true)
+
     runWriteAction {
       try {
         GeneratorUtils.initializeCourse(project, myCourse)
@@ -80,5 +101,12 @@ open class GradleCourseProjectGenerator(
     // Unfortunately, org.jetbrains.plugins.gradle.service.project.GradleStartupActivity#SHOW_UNLINKED_GRADLE_POPUP is private
     // so create own const
     private const val SHOW_UNLINKED_GRADLE_POPUP = "show.inlinked.gradle.project.popup"
+
+    private val INVALID_SYMBOLS = "[ /\\\\:<>\"?*|]".toRegex()
+
+    /**
+     * Replaces ' ', '/', '\', ':', '<', '>', '"', '?', '*', '|' symbols by '_'
+     */
+    private fun sanitizeName(name: String): String = name.replace(INVALID_SYMBOLS, "_")
   }
 }
