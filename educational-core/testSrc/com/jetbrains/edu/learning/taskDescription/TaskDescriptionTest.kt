7ca3dfd584a0e78588c40dd7402d1c8692cfd2de
liana.bakradze <liana.bakradze@jetbrains.com>
2018-02-01 13:31:07 +0300

    EDU-1251 Allow referring IDE shortcuts in task descriptions

--- educational-core/testSrc/com/jetbrains/edu/learning/taskDescription/TaskDescriptionTest.kt
+++ educational-core/testSrc/com/jetbrains/edu/learning/taskDescription/TaskDescriptionTest.kt
@@ -1,21 +1,27 @@
-package com.jetbrains.edu.learning
+package com.jetbrains.edu.learning.taskDescription
 
 import com.intellij.ide.BrowserUtil
 import com.intellij.openapi.fileTypes.PlainTextLanguage
+import com.intellij.openapi.keymap.KeymapManager
+import com.intellij.openapi.keymap.impl.KeymapManagerImpl
 import com.intellij.openapi.util.io.FileUtil
+import com.jetbrains.edu.learning.EduNames
+import com.jetbrains.edu.learning.EduTestCase
+import com.jetbrains.edu.learning.EduUtils
+import com.jetbrains.edu.learning.StudyTaskManager
 import com.jetbrains.edu.learning.courseFormat.Course
 import com.jetbrains.edu.learning.ui.taskDescription.BrowserWindow
 import org.jsoup.Jsoup
 import java.io.File
 import java.io.IOException
 
-class TaskDescriptionToolWindowTest : EduTestCase() {
+class TaskDescriptionTest : EduTestCase() {
   fun testSimpleImg() {
-    doTest()
+    doTestImage()
   }
 
   fun testImgInsideOtherTags() {
-    doTest()
+    doTestImage()
   }
 
   fun testWebImage() {
@@ -35,7 +41,7 @@ class TaskDescriptionToolWindowTest : EduTestCase() {
     initialImgElements.zip(processedImageElements).map { assert(it.first.attr("src") == it.second.attr("src") ) }
   }
 
-  private fun doTest() {
+  private fun doTestImage() {
     configureByTaskFile(1, 1, "taskFile1.txt")
     val name = getTestName(true) + ".html"
     val fileText = FileUtil.loadFile(File(testDataPath, name))
@@ -46,6 +52,37 @@ class TaskDescriptionToolWindowTest : EduTestCase() {
     val imageElements = document.getElementsByTag("img")
     imageElements
       .map { assert(BrowserUtil.isAbsoluteURL(it.attr("src"))) }
+  }
+
+  fun testShortcutRendering() {
+    val taskText = "You can use &shortcut:OverrideMethods; to override methods"
+    val taskTextWithShortcuts = "You can use Ctrl+O to override methods"
+    doTestShortcut(taskText, taskTextWithShortcuts)
+  }
+
+  fun testSeveralShortcutsRendering() {
+    val taskText = "You can use &shortcut:OverrideMethods; to override methods. One more useful shortcut: &shortcut:GotoAction;"
+    val taskTextWithShortcuts = "You can use Ctrl+O to override methods. One more useful shortcut: Ctrl+Shift+A"
+    doTestShortcut(taskText, taskTextWithShortcuts)
+  }
+
+  fun testShortcutInsideTag() {
+    val taskText = "You can use <code>&shortcut:OverrideMethods;</code> to override methods. One more useful shortcut: &shortcut:GotoAction;"
+    val taskTextWithShortcuts = "You can use Ctrl+O to override methods. One more useful shortcut: Ctrl+Shift+A"
+    doTestShortcut(taskText, taskTextWithShortcuts)
+  }
+
+  private fun doTestShortcut(taskText: String, taskTextWithShortcuts: String, keymapName: String = "Default for XWin") {
+    configureByTaskFile(1, 1, "taskFile1.txt")
+    val task = EduUtils.getCurrentTask(project)!!
+    task.taskTexts.clear()
+    task.addTaskText(EduNames.TASK, taskText)
+    val oldActiveKeymap = KeymapManager.getInstance().activeKeymap
+    val keymapManager = KeymapManager.getInstance() as KeymapManagerImpl
+    keymapManager.activeKeymap = keymapManager.getKeymap(keymapName)
+    val taskDescription = task.getTaskDescription(task.getTaskDir(project))
+    assertEquals(taskTextWithShortcuts, taskDescription!!.getBody())
+    keymapManager.activeKeymap = oldActiveKeymap
   }
 
   @Throws(IOException::class)
@@ -65,4 +102,7 @@ class TaskDescriptionToolWindowTest : EduTestCase() {
   override fun getTestDataPath(): String {
     return super.getTestDataPath() + "/toolWindow"
   }
+
+  private fun String.getBody() = Jsoup.parse(this).getElementsByTag("body").text()
+
 }
