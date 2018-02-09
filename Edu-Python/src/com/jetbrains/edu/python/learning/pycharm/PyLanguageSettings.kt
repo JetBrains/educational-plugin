ce904e1789e515c74fca75f56ad29e5bd7966e78
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2018-02-09 19:29:52 +0300

    fix broken Browse courses (removed API)

--- Edu-Python/src/com/jetbrains/edu/python/learning/pycharm/PyLanguageSettings.kt
+++ Edu-Python/src/com/jetbrains/edu/python/learning/pycharm/PyLanguageSettings.kt
@@ -5,14 +5,27 @@ import com.intellij.ui.ComboboxWithBrowseButton
 import com.intellij.util.containers.ContainerUtil
 import com.jetbrains.edu.python.learning.newproject.PyLanguageSettings
 import com.jetbrains.python.configuration.PyConfigurableInterpreterList
-import com.jetbrains.python.configuration.VirtualEnvProjectFilter
 import com.jetbrains.python.newProject.steps.PythonSdkChooserCombo
+import com.jetbrains.python.sdk.PythonSdkAdditionalData
+import com.jetbrains.python.sdk.PythonSdkType
 
 internal class PyLanguageSettings : PyLanguageSettings() {
 
   override fun getInterpreterComboBox(fakeSdk: Sdk?): ComboboxWithBrowseButton {
     val registeredSdks = PyConfigurableInterpreterList.getInstance(null).allPythonSdks
-    VirtualEnvProjectFilter.removeAllAssociated(registeredSdks)
+    registeredSdks.removeIf { it ->
+      if (it != null && PythonSdkType.isVirtualEnv(it)) {
+        val data = it.sdkAdditionalData as PythonSdkAdditionalData?
+        if (data != null) {
+          val path = data.associatedProjectPath
+          if (path != null) {
+            return@removeIf true
+          }
+        }
+      }
+      false
+    }
+
     val sdks = if (fakeSdk != null) ContainerUtil.prepend(registeredSdks, fakeSdk) else registeredSdks
     val sdkChooser = PythonSdkChooserCombo(null, sdks, null) { true }
     sdkChooser.addChangedListener {
