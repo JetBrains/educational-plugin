65701ff21467b21dad6c57d26388d3cb206bb73e
Ekaterina Tuzova <ekaterina.tuzova@jetbrains.com>
2018-02-09 14:09:20 +0300

    EDU-1315 Open first task automatically after opening course
    (cherry picked from commit 89521eb)

--- educational-core/src/com/jetbrains/edu/learning/intellij/generation/GradleCourseProjectGenerator.kt
+++ educational-core/src/com/jetbrains/edu/learning/intellij/generation/GradleCourseProjectGenerator.kt
@@ -51,6 +51,7 @@ open class GradleCourseProjectGenerator(
   }
 
   override fun afterProjectGenerated(project: Project, projectSettings: JdkProjectSettings) {
+    super.afterProjectGenerated(project, projectSettings)
     setJdk(project, projectSettings)
   }
 
