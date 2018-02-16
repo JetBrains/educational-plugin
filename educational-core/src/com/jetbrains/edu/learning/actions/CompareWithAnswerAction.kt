3ec5dd6c8afd0e356d84e45a5cec677c87c523b4
Ekaterina Tuzova <ekaterina.tuzova@jetbrains.com>
2018-02-16 16:20:37 +0300

    EDU-1340 Do not allow to "apply changes" from compare with answer action
    (cherry picked from commit 33eaff0)

--- educational-core/src/com/jetbrains/edu/learning/actions/CompareWithAnswerAction.kt
+++ educational-core/src/com/jetbrains/edu/learning/actions/CompareWithAnswerAction.kt
@@ -9,75 +9,76 @@ import com.intellij.openapi.editor.Document
 import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.openapi.project.Project
 import com.jetbrains.edu.learning.EduState
-import com.jetbrains.edu.learning.StudyTaskManager
 import com.jetbrains.edu.learning.EduUtils
+import com.jetbrains.edu.learning.StudyTaskManager
 
 class CompareWithAnswerAction : DumbAwareAction("Compare with Answer", "Compare your solution with answer", AllIcons.Diff.Diff) {
-    companion object {
-        @JvmField val ACTION_ID = "Educational.CompareWithAnswer"
+  companion object {
+    const val ACTION_ID = "Educational.CompareWithAnswer"
+  }
+
+  override fun actionPerformed(e: AnActionEvent) {
+    val project = e.project ?: return
+
+    val studyEditor = EduUtils.getSelectedStudyEditor(project)
+    val studyState = EduState(studyEditor)
+    if (!studyState.isValid) {
+      return
     }
 
-    override fun actionPerformed(e: AnActionEvent) {
-        val project = e.project ?: return
+    val virtualFile = studyState.virtualFile ?: return
+    val document = studyEditor?.editor?.document ?: return
 
-        val studyState = EduState(EduUtils.getSelectedStudyEditor(project))
-        if (!studyState.isValid) {
-            return
+    val taskFileContent = DiffContentFactory.getInstance().create(project, document.immutableCharSequence.toString())
+    val answerText = getFileTextWithAnswers(project, document)
+    val answerFileName = "answer." + virtualFile.extension
+    val answerContent = DiffContentFactory.getInstance().create(answerText, virtualFile.fileType)
+
+    val request = SimpleDiffRequest(
+      "Compare your solution with answer",
+      taskFileContent, answerContent,
+      virtualFile.name, answerFileName)
+
+    DiffManager.getInstance().showDiff(project, request)
+  }
+
+  private fun getFileTextWithAnswers(project: Project, myDocument: Document): String {
+    val fullAnswer = StringBuilder(myDocument.text)
+
+    val studyState = EduState(EduUtils.getSelectedStudyEditor(project))
+    studyState.taskFile.activePlaceholders
+      .sortedBy { it.offset }
+      .reversed()
+      .forEach { placeholder ->
+        placeholder.possibleAnswer?.let { answer ->
+          fullAnswer.replace(placeholder.offset, placeholder.offset + placeholder.realLength, answer)
         }
+      }
 
-        val myVirtualFile = studyState.virtualFile ?: return
-        val myFileContent = DiffContentFactory.getInstance().create(project, myVirtualFile)
+    return fullAnswer.toString()
+  }
 
-        val myDocument = EduUtils.getSelectedEditor(project)?.document ?: return
-        val answerText = getFileTextWithAnswers(project, myDocument)
-        val answerFileName = "answer." + myVirtualFile.extension
-        val answerContent = DiffContentFactory.getInstance().create(answerText, myVirtualFile.fileType)
-
-        val request = SimpleDiffRequest(
-                "Compare your solution with answer",
-                myFileContent, answerContent,
-                myVirtualFile.name, answerFileName)
-
-        DiffManager.getInstance().showDiff(project, request)
+  override fun update(e: AnActionEvent?) {
+    EduUtils.updateAction(e!!)
+    val project = e.project
+    if (project != null) {
+      val course = StudyTaskManager.getInstance(project).course
+      val presentation = e.presentation
+      if (course != null && !course.isStudy) {
+        presentation.isEnabled = false
+        presentation.isVisible = true
+        return
+      }
+      val studyEditor = EduUtils.getSelectedStudyEditor(project)
+      val studyState = EduState(studyEditor)
+      if (!studyState.isValid) {
+        presentation.isEnabledAndVisible = false
+        return
+      }
+      val taskFile = studyState.taskFile
+      if (taskFile.activePlaceholders.isEmpty()) {
+        presentation.isEnabledAndVisible = false
+      }
     }
-
-    private fun getFileTextWithAnswers(project: Project, myDocument: Document): String {
-        val fullAnswer = StringBuilder(myDocument.text)
-
-        val studyState = EduState(EduUtils.getSelectedStudyEditor(project))
-        studyState.taskFile.activePlaceholders
-                .sortedBy { it.offset }
-                .reversed()
-                .forEach { placeholder ->
-                    placeholder.possibleAnswer?.let { answer ->
-                        fullAnswer.replace(placeholder.offset, placeholder.offset + placeholder.realLength, answer)
-                    }
-                }
-
-        return fullAnswer.toString()
-    }
-
-    override fun update(e: AnActionEvent?) {
-        EduUtils.updateAction(e!!)
-        val project = e.project
-        if (project != null) {
-            val course = StudyTaskManager.getInstance(project).course
-            val presentation = e.presentation
-            if (course != null && !course.isStudy) {
-                presentation.isEnabled = false
-                presentation.isVisible = true
-                return
-            }
-            val studyEditor = EduUtils.getSelectedStudyEditor(project)
-            val studyState = EduState(studyEditor)
-            if (!studyState.isValid) {
-                presentation.isEnabledAndVisible = false
-                return
-            }
-            val taskFile = studyState.taskFile
-            if (taskFile.activePlaceholders.isEmpty()) {
-                presentation.isEnabledAndVisible = false
-            }
-        }
-    }
+  }
 }
