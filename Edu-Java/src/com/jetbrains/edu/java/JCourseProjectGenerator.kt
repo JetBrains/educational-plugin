e6e8e5c3b71dc278f2b841f97d3fd86735d7f172
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2018-02-20 23:32:00 +0300

    don't set code style settings in unit tests

--- Edu-Java/src/com/jetbrains/edu/java/JCourseProjectGenerator.kt
+++ Edu-Java/src/com/jetbrains/edu/java/JCourseProjectGenerator.kt
@@ -1,6 +1,7 @@
 package com.jetbrains.edu.java
 
 import com.intellij.lang.java.JavaLanguage
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.Project
 import com.intellij.psi.codeStyle.CodeStyleSettingsManager
 import com.jetbrains.edu.learning.courseFormat.Course
@@ -12,9 +13,12 @@ class JCourseProjectGenerator(builder: GradleCourseBuilderBase,course: Course) :
 
   override fun afterProjectGenerated(project: Project, projectSettings: JdkProjectSettings) {
     super.afterProjectGenerated(project, projectSettings)
-    val indentOptions = CodeStyleSettingsManager.getInstance().currentSettings.
-                                                                  getCommonSettings(JavaLanguage.INSTANCE).indentOptions
-    indentOptions?.TAB_SIZE = 2
-    indentOptions?.INDENT_SIZE = 2
+    if (!ApplicationManager.getApplication().isUnitTestMode) {
+      // BACKCOMPAT: 2017.3
+      @Suppress("DEPRECATION")
+      val indentOptions = CodeStyleSettingsManager.getInstance().currentSettings.getCommonSettings(JavaLanguage.INSTANCE).indentOptions
+      indentOptions?.TAB_SIZE = 2
+      indentOptions?.INDENT_SIZE = 2
+    }
   }
 }
