322f3a94b80303c4b6e09752f87cc294751d20cc
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2018-02-27 15:49:49 +0300

    test course progress on check

--- educational-core/testSrc/com/jetbrains/edu/learning/CourseViewTest.kt
+++ educational-core/testSrc/com/jetbrains/edu/learning/CourseViewTest.kt
@@ -2,15 +2,24 @@
 package com.jetbrains.edu.learning
 
 import com.intellij.ide.projectView.ProjectView
+import com.intellij.lang.LanguageExtensionPoint
+import com.intellij.lang.annotation.Annotator
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.extensions.ExtensionPointName
 import com.intellij.openapi.fileTypes.PlainTextLanguage
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Disposer
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.testFramework.*
 import com.jetbrains.edu.learning.actions.CheckAction
+import com.jetbrains.edu.learning.checker.CheckResult
+import com.jetbrains.edu.learning.checker.TaskChecker
+import com.jetbrains.edu.learning.checker.TaskCheckerProvider
+import com.jetbrains.edu.learning.courseFormat.CheckStatus
 import com.jetbrains.edu.learning.courseFormat.Course
+import com.jetbrains.edu.learning.courseFormat.tasks.EduTask
+import com.jetbrains.edu.learning.courseFormat.tasks.Task
 import com.jetbrains.edu.learning.navigation.NavigationUtils
 import com.jetbrains.edu.learning.projectView.CourseViewPane
 import junit.framework.TestCase
@@ -25,6 +34,7 @@ class CourseViewTest : EduTestCase() {
   override fun setUp() {
     super.setUp()
     myCourse?.language = PlainTextLanguage.INSTANCE.id
+    registerPlainTextConfigurator()
     ProjectViewTestUtil.setupImpl(project, true)
   }
 
@@ -150,6 +160,31 @@ class CourseViewTest : EduTestCase() {
     pane.createComponent()
     Disposer.register(project, pane)
     return pane
+  }
+
+  private fun registerPlainTextConfigurator() {
+    val extension = LanguageExtensionPoint<Annotator>()
+    extension.language = PlainTextLanguage.INSTANCE.id
+    extension.implementationClass = PlainTextConfigurator::class.java.name
+    PlatformTestUtil.registerExtension(
+      ExtensionPointName.create(EduConfigurator.EP_NAME), extension, myFixture.testRootDisposable)
+  }
+
+  class PlainTextConfigurator : EduConfigurator<Unit> {
+    override fun getCourseBuilder() = object : EduCourseBuilder<Unit> {
+      override fun createTaskContent(project: Project, task: Task, parentDirectory: VirtualFile, course: Course): VirtualFile? = null
+      override fun getLanguageSettings(): EduCourseBuilder.LanguageSettings<Unit> = EduCourseBuilder.LanguageSettings { Unit }
+    }
+
+    override fun getTestFileName() = "test.txt"
+
+    override fun excludeFromArchive(name: String) = false
+
+    override fun getTaskCheckerProvider() = TaskCheckerProvider{ task, project -> object: TaskChecker<EduTask>(task, project) {
+      override fun check(): CheckResult {
+        return CheckResult(CheckStatus.Solved, "")
+      }
+    }}
   }
 
   override fun getTestDataPath(): String {
