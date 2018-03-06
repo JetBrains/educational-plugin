3b9a151603c90432c46f2515515776f6ee85e771
Liana Bakradze <liana.bakradze@jetbrains.com>
2018-03-06 11:13:56 +0300

    even more logs to track flaky tests

--- Edu-Java/src/com/jetbrains/edu/java/JTaskCheckerProvider.kt
+++ Edu-Java/src/com/jetbrains/edu/java/JTaskCheckerProvider.kt
@@ -1,10 +1,13 @@
 package com.jetbrains.edu.java
 
+import com.intellij.codeInsight.runner.JavaMainMethodProvider
 import com.intellij.execution.JavaExecutionUtil
+import com.intellij.openapi.extensions.Extensions
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiClass
 import com.intellij.psi.PsiManager
+import com.intellij.psi.PsiMethod
 import com.intellij.psi.util.PsiMethodUtil
 import com.intellij.psi.util.PsiTreeUtil
 import com.jetbrains.edu.learning.checker.gradle.GradleTaskCheckerProvider
@@ -23,7 +26,7 @@ class JTaskCheckerProvider : GradleTaskCheckerProvider() {
             println("Class found: ${clazz.text}")
         }
         val mainClass = classes.find { psiClass ->
-            PsiMethodUtil.MAIN_CLASS.value(psiClass) && PsiMethodUtil.hasMainMethod(psiClass)
+            isMainClass(psiClass) && hasMainMethod(psiClass)
         }
         if (mainClass == null) {
             println("Main class in ${psiFile.name} not found")
@@ -35,5 +38,34 @@ class JTaskCheckerProvider : GradleTaskCheckerProvider() {
             println("Runtime qualified name not found")
         }
         return runtimeQualifiedName
+    }
+
+    private fun isMainClass(psiClass: PsiClass?): Boolean {
+        val isMainClass = PsiMethodUtil.MAIN_CLASS.value(psiClass)
+        println("isMainClass=$isMainClass")
+        return isMainClass
+    }
+
+    fun hasMainMethod(psiClass: PsiClass): Boolean {
+        val providers = Extensions.getExtensions(JavaMainMethodProvider.EP_NAME)
+        if (providers.isEmpty()) {
+            println("No providers")
+        }
+        for (provider in providers) {
+            if (provider.isApplicable(psiClass)) {
+                return provider.hasMainMethod(psiClass)
+            }
+        }
+        return findMainMethod(psiClass.findMethodsByName("main", true)) != null
+    }
+
+    private fun findMainMethod(mainMethods: Array<PsiMethod>): PsiMethod? {
+        for (mainMethod in mainMethods) {
+            println("Main method: ${mainMethod.text}")
+            val isMainMethod = PsiMethodUtil.isMainMethod(mainMethod)
+            println("isMainMethod: $isMainMethod")
+            if (isMainMethod) return mainMethod
+        }
+        return null
     }
 }
