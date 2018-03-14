40b354b9ee5c08e6f08c1952461f5e0a68932892
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2018-03-15 00:50:23 +0300

    create plain text course project generator
    (cherry picked from commit 7de5fc9)

--- educational-core/testSrc/com/jetbrains/edu/learning/PlainTextConfigurator.kt
+++ educational-core/testSrc/com/jetbrains/edu/learning/PlainTextConfigurator.kt
@@ -14,26 +14,28 @@ import com.jetbrains.edu.learning.courseFormat.CheckStatus
 import com.jetbrains.edu.learning.courseFormat.Course
 import com.jetbrains.edu.learning.courseFormat.tasks.EduTask
 import com.jetbrains.edu.learning.courseFormat.tasks.Task
+import com.jetbrains.edu.learning.newproject.CourseProjectGenerator
 
 class PlainTextConfigurator : EduConfigurator<Unit> {
 
-  override fun getCourseBuilder() = object : EduCourseBuilder<Unit> {
-    override fun createTaskContent(project: Project, task: Task, parentDirectory: VirtualFile, course: Course): VirtualFile? = null
-    override fun getLanguageSettings(): EduCourseBuilder.LanguageSettings<Unit> = EduCourseBuilder.LanguageSettings { }
-  }
-
+  override fun getCourseBuilder() = PlainTextCourseBuilder()
   override fun getTestFileName() = "test.txt"
-
   override fun excludeFromArchive(name: String) = false
 
   override fun getTaskCheckerProvider() = TaskCheckerProvider { task, project ->
     object : TaskChecker<EduTask>(task, project) {
-      override fun check(): CheckResult {
-        return CheckResult(CheckStatus.Solved, "")
-      }
+      override fun check(): CheckResult = CheckResult(CheckStatus.Solved, "")
     }
   }
 }
+
+class PlainTextCourseBuilder : EduCourseBuilder<Unit> {
+  override fun createTaskContent(project: Project, task: Task, parentDirectory: VirtualFile, course: Course): VirtualFile? = null
+  override fun getLanguageSettings(): EduCourseBuilder.LanguageSettings<Unit> = EduCourseBuilder.LanguageSettings { }
+  override fun getCourseProjectGenerator(course: Course): CourseProjectGenerator<Unit> = PlainTextCourseGenerator(this, course)  
+}
+
+class PlainTextCourseGenerator(builder: EduCourseBuilder<Unit>, course: Course) : CourseProjectGenerator<Unit>(builder, course)
 
 fun registerPlainTextConfigurator(disposable: Disposable) {
   val extension = LanguageExtensionPoint<EduConfigurator<*>>()
