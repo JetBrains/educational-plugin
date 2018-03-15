c1419956a3287c7a2070d7e0a6057936344b2d33
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2018-03-15 18:15:19 +0300

    move course dsl from 'EduTestCase'
    (cherry picked from commit 6622995)

--- educational-core/testSrc/com/jetbrains/edu/learning/EduTestCase.kt
+++ educational-core/testSrc/com/jetbrains/edu/learning/EduTestCase.kt
@@ -11,28 +11,22 @@ import com.intellij.openapi.fileEditor.impl.FileEditorProviderManagerImpl
 import com.intellij.openapi.fileTypes.PlainTextLanguage
 import com.intellij.openapi.util.Disposer
 import com.intellij.openapi.vfs.VfsUtilCore
+import com.intellij.testFramework.LightPlatformTestCase
 import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCase
 import com.intellij.ui.docking.DockContainer
 import com.intellij.ui.docking.DockManager
 import com.jetbrains.edu.coursecreator.CCTestCase
-import com.jetbrains.edu.learning.courseFormat.*
-import com.jetbrains.edu.learning.courseFormat.ext.sourceDir
+import com.jetbrains.edu.learning.courseFormat.Course
+import com.jetbrains.edu.learning.courseFormat.Lesson
+import com.jetbrains.edu.learning.courseFormat.TaskFile
 import com.jetbrains.edu.learning.courseFormat.tasks.EduTask
 import com.jetbrains.edu.learning.courseFormat.tasks.Task
-import com.jetbrains.edu.learning.courseFormat.tasks.TheoryTask
 import java.io.IOException
-import java.util.regex.Pattern
 
 abstract class EduTestCase : LightPlatformCodeInsightFixtureTestCase() {
   private lateinit var myManager: FileEditorManagerImpl
   private lateinit var myOldManager: FileEditorManager
   private lateinit var myOldDockContainers: Set<DockContainer>
-
-  companion object {
-    private val OPENING_TAG: Pattern = Pattern.compile("<p>")
-    private val CLOSING_TAG: Pattern = Pattern.compile("</p>")
-  }
-
 
   @Throws(Exception::class)
   override fun setUp() {
@@ -120,147 +114,16 @@ abstract class EduTestCase : LightPlatformCodeInsightFixtureTestCase() {
     return "testData"
   }
 
-  fun course(name: String = "Test Course", language: Language = PlainTextLanguage.INSTANCE, buildCourse: CourseBuilder.() -> Unit): Course {
-    val builder = CourseBuilder()
-    builder.withName(name)
-    builder.buildCourse()
-    val course = builder.course
-    course.language = language.id
-    StudyTaskManager.getInstance(myFixture.project).course = course
-    createCourseFiles(course)
-    return course
-  }
-
-  private fun createCourseFiles(course: Course) {
-    for (lesson in course.lessons) {
-      val lessonDirName = EduNames.LESSON + lesson.index
-      for (task in lesson.taskList) {
-        val taskDirName = EduNames.TASK + task.index
-        val sourceDir = if (course.sourceDir.isNullOrEmpty()) "" else "${course.sourceDir}/"
-        for (taskFile in task.getTaskFiles().values) {
-          val path = "${myFixture.project.baseDir.path}/$lessonDirName/$taskDirName/$sourceDir${taskFile.name}"
-          myFixture.tempDirFixture.createFile(path, taskFile.text)
-        }
-      }
-    }
-  }
-
-  class CourseBuilder {
-    val course: Course = Course()
-
-    fun withName(name: String) {
-      course.name = name
-    }
-
-    fun lesson(name: String? = null, buildLesson: LessonBuilder.() -> Unit) {
-      val lessonBuilder = LessonBuilder(course)
-      val nextLessonIndex = course.lessons.size + 1
-      lessonBuilder.withName(name ?: EduNames.LESSON + nextLessonIndex)
-      lessonBuilder.buildLesson()
-      val lesson = lessonBuilder.lesson
-      course.addLesson(lesson)
-      lesson.index = course.lessons.size
-    }
-  }
-
-  class LessonBuilder(val course: Course) {
-    val lesson = Lesson()
-
-    init {
-      lesson.course = course
-    }
-
-    fun withName(name: String) {
-      lesson.name = name
-    }
-
-    fun task(task: Task, name: String? = null, buildTask: TaskBuilder.() -> Unit) {
-      val taskBuilder = TaskBuilder(lesson, task)
-      val nextTaskIndex = lesson.taskList.size + 1
-      taskBuilder.withName(name?: EduNames.TASK + nextTaskIndex)
-      taskBuilder.buildTask()
-      lesson.addTask(taskBuilder.task)
-      taskBuilder.task.index = lesson.taskList.size
-    }
-
-    fun eduTask(name: String? = null, buildTask: TaskBuilder.() -> Unit) = task(EduTask(), name, buildTask)
-
-    fun theoryTask(name: String? = null, text: String, buildTask: TaskBuilder.()-> Unit) = task(TheoryTask(), name, buildTask)
-  }
-
-  class TaskBuilder(val lesson: Lesson, val task: Task) {
-    init {
-      task.lesson = lesson
-    }
-    fun withName(name: String) {
-      task.name = name
-    }
-
-    fun taskFile(name: String, text: String, buildTaskFile: (TaskFileBuilder.() -> Unit)? = null) {
-      val taskFileBuilder = TaskFileBuilder(task)
-      taskFileBuilder.withName(name)
-      val textBuffer = StringBuilder(text)
-      val placeholders = extractPlaceholdersFromText(textBuffer)
-      taskFileBuilder.withText(textBuffer.toString())
-      taskFileBuilder.withPlaceholders(placeholders)
-      if (buildTaskFile != null) {
-        taskFileBuilder.buildTaskFile()
-      }
-      val taskFile = taskFileBuilder.taskFile
-      taskFile.task = task
-      task.addTaskFile(taskFile)
-    }
-
-    private fun extractPlaceholdersFromText(text: StringBuilder): List<AnswerPlaceholder> {
-      val openingMatcher = OPENING_TAG.matcher(text)
-      val closingMatcher = CLOSING_TAG.matcher(text)
-      val placeholders = mutableListOf<AnswerPlaceholder>()
-      var pos = 0
-      while (openingMatcher.find(pos)) {
-        val answerPlaceholder = AnswerPlaceholder()
-        val answerPlaceholderSubtaskInfo = AnswerPlaceholderSubtaskInfo()
-        answerPlaceholder.subtaskInfos[0] = answerPlaceholderSubtaskInfo
-        answerPlaceholder.offset = openingMatcher.start()
-        if (!closingMatcher.find(openingMatcher.end())) {
-          LOG.error("No matching closing tag found")
-        }
-        answerPlaceholder.length = closingMatcher.start() - openingMatcher.end()
-        placeholders.add(answerPlaceholder)
-        text.delete(closingMatcher.start(), closingMatcher.end())
-        text.delete(openingMatcher.start(), openingMatcher.end())
-        pos = answerPlaceholder.offset + answerPlaceholder.realLength
-      }
-      return placeholders
-    }
-  }
-
-  class TaskFileBuilder(val task: Task) {
-    val taskFile = TaskFile()
-
-    init {
-      taskFile.task = task
-    }
-    fun withName(name: String) {
-      taskFile.name = name
-    }
-
-    fun withText(text: String) {
-      taskFile.text = text
-    }
-
-    fun withPlaceholders(placeholders: List<AnswerPlaceholder>) {
-      for (placeholder in placeholders) {
-        placeholder.taskFile = taskFile
-        taskFile.addAnswerPlaceholder(placeholder)
-      }
-    }
-
-    fun placeholder(index: Int, taskText: String = "type here") {
-      val answerPlaceholder = taskFile.answerPlaceholders[index]
-      answerPlaceholder.taskText = taskText
+  fun course(
+    name: String = "Test Course",
+    language: Language = PlainTextLanguage.INSTANCE,
+    settings: Any = Unit,
+    buildCourse: CourseBuilder.() -> Unit
+  ): Course {
+    return course(name, language, buildCourse).apply {
+      createCourseFiles(project, LightPlatformTestCase.getSourceRoot(), settings)
     }
   }
 
   fun getCourse(): Course = StudyTaskManager.getInstance(project).course!!
-
 }
