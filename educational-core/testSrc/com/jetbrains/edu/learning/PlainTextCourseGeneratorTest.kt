acc6161f8823c39255eb27f7a0526b446e609b7c
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2018-03-15 18:15:44 +0300

    test course dsl

---
+++ educational-core/testSrc/com/jetbrains/edu/learning/PlainTextCourseGeneratorTest.kt
@@ -0,0 +1,42 @@
+package com.jetbrains.edu.learning
+
+import com.intellij.testFramework.LightPlatformTestCase
+
+class PlainTextCourseGeneratorTest : EduTestCase() {
+
+  fun `test course structure creation`() {
+    course {
+      lesson {
+        eduTask {
+          taskFile("Fizz.kt")
+        }
+        eduTask {
+          taskFile("Buzz.kt")
+        }
+      }
+      lesson {
+        eduTask {
+          taskFile("FizzBuzz.kt")
+        }
+      }
+    }
+
+    val expectedFileTree = fileTree {
+      dir("lesson1") {
+        dir("task1") {
+          file("Fizz.kt")
+        }
+        dir("task2") {
+          file("Buzz.kt")
+        }
+      }
+      dir("lesson2") {
+        dir("task1") {
+          file("FizzBuzz.kt")
+        }
+      }
+    }
+
+    expectedFileTree.assertEquals(LightPlatformTestCase.getSourceRoot())
+  }
+}
