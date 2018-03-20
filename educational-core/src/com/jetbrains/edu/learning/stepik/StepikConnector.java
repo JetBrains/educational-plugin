bc12eed6ab2d9e4b92738110684f261e0baebff7
Valentina Kiryushkina <valentina.kiryushkina@jetbrains.com>
2018-03-20 20:06:31 +0300

    EDU-1372 Sort units not only by ids (the order we get them from Stepik), but also by sections
    (cherry picked from commit 0f05272)
    
    (cherry picked from commit dae455d)

--- educational-core/src/com/jetbrains/edu/learning/stepik/StepikConnector.java
+++ educational-core/src/com/jetbrains/edu/learning/stepik/StepikConnector.java
@@ -420,6 +420,7 @@ public class StepikConnector {
   @NotNull
   private static List<Lesson> sortLessonsByUnits(List<Unit> units, List<Lesson> lessons) {
     HashMap<Integer, Lesson> idToLesson = new HashMap<>();
+    units.sort(Comparator.comparingInt(unit -> unit.section));
     for (Lesson lesson : lessons) {
       idToLesson.put(lesson.getId(), lesson);
     }
@@ -430,7 +431,6 @@ public class StepikConnector {
     }
     return sorted;
   }
-
   private static List<Lesson> getLessonsFromUnits(RemoteCourse remoteCourse, String[] unitIds) throws IOException {
     final ProgressIndicator progressIndicator = ProgressManager.getInstance().getProgressIndicator();
     final List<Lesson> lessons = new ArrayList<>();
