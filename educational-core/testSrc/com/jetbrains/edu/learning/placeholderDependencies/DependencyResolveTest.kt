71a34d3fc24ea3006b94a6220c9d77729d2387e7
Liana Bakradze <liana.bakradze@jetbrains.com>
2018-03-21 08:56:35 +0300

    fixup: add test for backslash

--- educational-core/testSrc/com/jetbrains/edu/learning/placeholderDependencies/DependencyResolveTest.kt
+++ educational-core/testSrc/com/jetbrains/edu/learning/placeholderDependencies/DependencyResolveTest.kt
@@ -91,6 +91,34 @@ class DependencyResolveTest : EduTestCase() {
     checkPlaceholder(11, 8, targetPlaceholder)
   }
 
+  fun `test resolve path with backslash`() {
+    courseWithFiles {
+      lesson {
+        eduTask {
+          taskFile("com/edu/test/Task.kt", """
+          |def f():
+          |  <p>print(1)</p>
+        """.trimMargin("|"))
+        }
+      }
+      lesson {
+        eduTask {
+          taskFile("Task.kt", """
+          |def <p>foo</p>():
+          |  <p>type here</p>
+          """.trimMargin("|")) {
+            placeholder(1, dependency = "lesson1#task1#com\\edu\\test\\Task.kt#1")
+          }
+        }
+      }
+    }
+    val course = getCourse()
+    val placeholderDependency = findPlaceholder(1, 0, "Task.kt", 1).placeholderDependency
+
+    val targetPlaceholder = placeholderDependency!!.resolve(course)!!
+    checkPlaceholder(11, 8, targetPlaceholder)
+  }
+
   private fun checkPlaceholder(expectedOffset: Int, expectedLength: Int, actualPlaceholder: AnswerPlaceholder) {
     val actualOffset = actualPlaceholder.offset
     val actualLength = actualPlaceholder.realLength
