58230153574b215d520e011472bf0c8174c4bdb0
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2018-03-21 14:40:14 +0300

    EDU-1362: introduce TaskDiff and use it to apply diff between framework tasks
    (cherry picked from commit 7d0d0b3)

--- educational-core/src/com/jetbrains/edu/learning/courseFormat/FrameworkLesson.kt
+++ educational-core/src/com/jetbrains/edu/learning/courseFormat/FrameworkLesson.kt
@@ -1,15 +1,99 @@
 package com.jetbrains.edu.learning.courseFormat
 
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.util.xmlb.annotations.Transient
 import com.jetbrains.edu.learning.EduNames
+import com.jetbrains.edu.learning.courseFormat.ext.sourceDir
+import com.jetbrains.edu.learning.courseFormat.ext.testDir
 import com.jetbrains.edu.learning.courseFormat.tasks.Task
+import com.jetbrains.edu.learning.courseGeneration.*
 
 class FrameworkLesson : Lesson() {
 
   @Transient
   var currentTaskIndex: Int = 0
 
+  /**
+   * Contains diffs between neighbor tasks.
+   * [myDiffs]`[i]` is diff list between [taskList]`[i]` and [taskList]`[i - 1]`.
+   * [myDiffs]`[0]` is supposed to be empty.
+   */
+  @Transient
+  private var myDiffs: List<List<TaskDiff>>? = null
+
+  override fun initLesson(course: Course?, isRestarted: Boolean) {
+    super.initLesson(course, isRestarted)
+    myDiffs = List(taskList.size) { index ->
+      if (index == 0) return@List emptyList<TaskDiff>()
+      val task = taskList[index]
+      val prevTask = taskList[index - 1]
+      calculateDiffs(prevTask, task)
+    }
+  }
+
   override fun getTask(name: String): Task? {
     return if (name == EduNames.TASK) taskList.getOrNull(currentTaskIndex) else null
+  }
+
+  fun prepareNextTask(project: Project, taskDir: VirtualFile) {
+    currentTaskIndex++
+    myDiffs?.get(currentTaskIndex)?.forEach { diff -> diff.apply(project, taskDir) }
+  }
+
+  fun preparePrevTask(project: Project, taskDir: VirtualFile) {
+    myDiffs?.get(currentTaskIndex)?.forEach { diff -> diff.revert(project, taskDir) }
+    currentTaskIndex--
+  }
+
+  private fun calculateDiffs(prevTask: Task, nextTask: Task): List<TaskDiff> {
+    val diffs = mutableListOf<TaskDiff>()
+    diffs += calculateDiffs(
+      prevTask.taskFiles,
+      nextTask.taskFiles,
+      course.sourceDir,
+      add = ::addTaskFile,
+      remove = ::removeTaskFile,
+      change = ::changeTaskFile
+    )
+    diffs += calculateDiffs(
+      prevTask.testsText,
+      nextTask.testsText,
+      course.testDir,
+      add = ::addFile,
+      remove = ::removeFile,
+      change = ::changeFile
+    )
+    diffs += calculateDiffs(
+      prevTask.additionalFiles,
+      nextTask.additionalFiles,
+      add = ::addFile,
+      remove = ::removeFile,
+      change = ::changeFile
+    )
+    return diffs
+  }
+}
+
+private inline fun <T> calculateDiffs(
+  prevItems: Map<String, T>,
+  nextItems: Map<String, T>,
+  pathPrefix: String? = null,
+  add: (String, T) -> TaskDiff,
+  remove: (String, T) -> TaskDiff,
+  change: (String, T, T) -> TaskDiff
+): List<TaskDiff> {
+  val allItems = prevItems.keys + nextItems.keys
+  return allItems.mapNotNull { path ->
+    val prevItem = prevItems[path]
+    val nextItem = nextItems[path]
+    val resultPath = if (pathPrefix.isNullOrEmpty()) path else "$pathPrefix/$path"
+    when {
+      prevItem == null && nextItem != null -> add(resultPath, nextItem)
+      prevItem != null && nextItem == null -> remove(resultPath, prevItem)
+      // TODO: implement `equals` for `TaskFile`
+      prevItem != null && nextItem != null && prevItem != nextItem -> change(resultPath, prevItem, nextItem)
+      else -> null
+    }
   }
 }
---
+++ educational-core/src/com/jetbrains/edu/learning/courseGeneration/TaskDiff.kt
@@ -0,0 +1,108 @@
+package com.jetbrains.edu.learning.courseGeneration
+
+import com.intellij.openapi.application.runUndoTransparentWriteAction
+import com.intellij.openapi.application.runWriteAction
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+import com.jetbrains.edu.learning.courseFormat.TaskFile
+import com.jetbrains.edu.learning.courseFormat.ext.getDocument
+import java.io.IOException
+
+/**
+ * Represent difference between two neighbor tasks in framework lesson.
+ *
+ * Note, initial state and state after sequential invocation of `apply` and `revert` can differ
+ */
+interface TaskDiff {
+  fun apply(project: Project, baseDir: VirtualFile)
+  fun revert(project: Project, baseDir: VirtualFile)
+}
+
+fun addFile(path: String, text: String): TaskDiff = FileAdded(path, text)
+fun removeFile(path: String, text: String): TaskDiff = FileRemoved(path, text)
+fun changeFile(path: String, prevText: String, nextText: String): TaskDiff =
+  ItemChanged(removeFile(path, prevText), addFile(path, nextText))
+
+fun addTaskFile(path: String, taskFile: TaskFile): TaskDiff = TaskFileAdded(path, taskFile)
+fun removeTaskFile(path: String, taskFile: TaskFile): TaskDiff = TaskFileRemoved(path, taskFile)
+fun changeTaskFile(path: String, prevTaskFile: TaskFile, nextTaskFile: TaskFile): TaskDiff =
+  ItemChanged(removeTaskFile(path, prevTaskFile), addTaskFile(path, nextTaskFile))
+
+private open class ReversedTaskDiff(private val diff: TaskDiff) : TaskDiff {
+  override fun apply(project: Project, baseDir: VirtualFile) = diff.revert(project, baseDir)
+  override fun revert(project: Project, baseDir: VirtualFile) = diff.apply(project, baseDir)
+}
+
+private class ItemChanged(private val remove: TaskDiff, private val add: TaskDiff) : TaskDiff {
+  override fun apply(project: Project, baseDir: VirtualFile) {
+    remove.apply(project, baseDir)
+    add.apply(project, baseDir)
+  }
+
+  override fun revert(project: Project, baseDir: VirtualFile) {
+    add.revert(project, baseDir)
+    remove.revert(project, baseDir)
+  }
+}
+
+private class FileAdded(
+  private val path: String,
+  private val text: String
+) : TaskDiff {
+  override fun apply(project: Project, baseDir: VirtualFile) {
+    try {
+      GeneratorUtils.createChildFile(baseDir, path, text)
+    } catch (e: IOException) {
+      LOG.error("Failed to create file `${baseDir.path}/$path`", e)
+    }
+  }
+
+  override fun revert(project: Project, baseDir: VirtualFile) {
+    runWriteAction {
+      try {
+        baseDir.findFileByRelativePath(path)?.delete(FileAdded::class.java)
+      } catch (e: IOException) {
+        LOG.error("Failed to delete file `${baseDir.path}/$path`", e)
+      }
+    }
+  }
+
+  companion object {
+    private val LOG: Logger = Logger.getInstance(FileAdded::class.java)
+  }
+}
+
+private class FileRemoved(path: String, text: String) : ReversedTaskDiff(FileAdded(path, text))
+
+private class TaskFileAdded(
+  path: String,
+  private val taskFile: TaskFile
+) : TaskDiff {
+
+  private val add: FileAdded = FileAdded(path, taskFile.text)
+
+  override fun apply(project: Project, baseDir: VirtualFile) {
+    add.apply(project, baseDir)
+    restoreAnswers(project)
+  }
+
+  override fun revert(project: Project, baseDir: VirtualFile) {
+    add.revert(project, baseDir)
+  }
+
+  private fun restoreAnswers(project: Project) {
+    for (placeholder in taskFile.answerPlaceholders) {
+      val studentAnswer = placeholder.studentAnswer
+      if (studentAnswer != null) {
+        val startOffset = placeholder.initialState.offset
+        val endOffset = startOffset + placeholder.initialState.length
+        runUndoTransparentWriteAction {
+          taskFile.getDocument(project)?.replaceString(startOffset, endOffset, studentAnswer)
+        }
+      }
+    }
+  }
+}
+
+private class TaskFileRemoved(path: String, taskFile: TaskFile) : ReversedTaskDiff(TaskFileAdded(path, taskFile))
