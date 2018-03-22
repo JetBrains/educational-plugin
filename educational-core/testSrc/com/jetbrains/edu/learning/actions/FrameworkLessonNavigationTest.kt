3bbc7ffa676766bafe9b32710ac5603c29959a34
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2018-03-22 15:09:03 +0300

    EDU-1362: improve navigation tests for framework lessons
    (cherry picked from commit 6cb1869)

--- educational-core/testSrc/com/jetbrains/edu/learning/actions/FrameworkLessonNavigationTest.kt
+++ educational-core/testSrc/com/jetbrains/edu/learning/actions/FrameworkLessonNavigationTest.kt
@@ -3,72 +3,122 @@ package com.jetbrains.edu.learning.actions
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.testFramework.LightPlatformTestCase
 import com.jetbrains.edu.learning.*
-import com.jetbrains.edu.learning.actions.NavigationAction.NEXT
-import com.jetbrains.edu.learning.actions.NavigationAction.PREV
+import com.jetbrains.edu.learning.courseFormat.CheckStatus
 import com.jetbrains.edu.learning.courseFormat.Course
+import com.jetbrains.edu.learning.courseFormat.ext.dirName
+import com.jetbrains.edu.learning.courseFormat.tasks.Task
 
 class FrameworkLessonNavigationTest : EduTestCase() {
 
   private val rootDir: VirtualFile get() = LightPlatformTestCase.getSourceRoot()
 
-  fun `test next`() = doTest("lesson1/task/file-1.txt", NEXT) {
-    dir("lesson1") {
-      dir("task") {
-        file("file-1.txt")
-        file("file-2.txt")
+  fun `test next`() {
+    val course = createFrameworkCourse()
+    val task = course.getLesson("lesson1")?.getTask("task1") ?: error("")
+    task.openTaskFileInEditor(rootDir, "fizz.kt", 0)
+    myFixture.type("\"Fizz\"")
+    task.status = CheckStatus.Solved
+    myFixture.testAction(NextTaskAction())
+
+    val fileTree = fileTree {
+      dir("lesson1") {
+        dir("task") {
+          file("fizz.kt", """
+            fn fizz() = "Fizz"
+          """)
+          file("buzz.kt", """
+            fn buzz() = TODO()
+          """)
+        }
       }
     }
+    fileTree.assertEquals(rootDir, myFixture)
   }
 
-  fun `test next next`() = doTest("lesson1/task/file-1.txt", NEXT, NEXT) {
-    dir("lesson1") {
-      dir("task") {
-        file("file-3.txt")
+  fun `test next next`() {
+    val course = createFrameworkCourse()
+    val task = course.getLesson("lesson1")?.getTask("task1") ?: error("")
+    task.openTaskFileInEditor(rootDir, "fizz.kt", 0)
+    myFixture.type("\"Fizz\"")
+    task.status = CheckStatus.Solved
+    myFixture.testAction(NextTaskAction())
+
+    val task2 = course.getLesson("lesson1")?.getTask("task2") ?: error("")
+    task2.openTaskFileInEditor(rootDir, "buzz.kt", 0)
+    myFixture.type("\"Buzz\"")
+    task2.status = CheckStatus.Solved
+    myFixture.testAction(NextTaskAction())
+
+    val fileTree = fileTree {
+      dir("lesson1") {
+        dir("task") {
+          file("fizzBuzz.kt", """
+            fn fizzBuzz() = "Fizz" + "Buzz"
+          """)
+        }
       }
     }
+    fileTree.assertEquals(rootDir, myFixture)
   }
 
-  fun `test next prev`() = doTest("lesson1/task/file-1.txt", NEXT, PREV) {
-    dir("lesson1") {
-      dir("task") {
-        file("file-1.txt")
+  fun `test next prev`() {
+    val course = createFrameworkCourse()
+    val task = course.getLesson("lesson1")?.getTask("task1") ?: error("")
+    task.openTaskFileInEditor(rootDir, "fizz.kt", 0)
+    myFixture.type("\"Fizz\"")
+    task.status = CheckStatus.Solved
+    myFixture.testAction(NextTaskAction())
+
+    myFixture.testAction(PreviousTaskAction())
+
+    val fileTree = fileTree {
+      dir("lesson1") {
+        dir("task") {
+          file("fizz.kt", """
+            fn fizz() = "Fizz"
+          """)
+        }
       }
     }
-  }
-
-  private fun doTest(fileToOpen: String, vararg actions: NavigationAction, block: FileTreeBuilder.() -> Unit) {
-    createFrameworkCourse()
-    val file = rootDir.findFileByRelativePath(fileToOpen) ?: error("Can't find `$fileToOpen` file")
-    myFixture.openFileInEditor(file)
-
-    for (action in actions) {
-      myFixture.testAction(action.action())
-    }
-
-    fileTree(block).assertEquals(rootDir)
+    fileTree.assertEquals(rootDir, myFixture)
   }
 
   private fun createFrameworkCourse(): Course = courseWithFiles {
     lesson(isFramework = true) {
       eduTask {
-        taskFile("file-1.txt", "")
+        taskFile("fizz.kt", """
+          fn fizz() = <p>TODO()</p>
+        """)
       }
       eduTask {
-        taskFile("file-1.txt", "")
-        taskFile("file-2.txt", "")
+        taskFile("fizz.kt", """
+          fn fizz() = <p>TODO()</p>
+        """) {
+          placeholder(0, dependency = "lesson1#task1#fizz.kt#1")
+        }
+        taskFile("buzz.kt", """
+          fn buzz() = <p>TODO()</p>
+        """)
       }
       eduTask {
-        taskFile("file-3.txt", "")
+        taskFile("fizzBuzz.kt", """
+          fn fizzBuzz() = <p>TODO()</p> + <p>TODO()</p>
+        """) {
+          placeholder(0, dependency = "lesson1#task2#fizz.kt#1")
+          placeholder(1, dependency = "lesson1#task2#buzz.kt#1")
+        }
       }
     }
   }
-}
 
-private enum class NavigationAction {
-  NEXT, PREV;
-
-  fun action(): TaskNavigationAction = when (this) {
-    NEXT -> NextTaskAction()
-    PREV -> PreviousTaskAction()
+  private fun Task.openTaskFileInEditor(baseDir: VirtualFile, taskFilePath: String, placeholderIndex: Int? = null) {
+    val taskFile = getTaskFile(taskFilePath) ?: error("Can't find task file `$taskFilePath` in `$name`")
+    val path = "lesson${lesson.index}/$dirName/$taskFilePath"
+    val file = baseDir.findFileByRelativePath(path) ?: error("Can't find `$path` file")
+    myFixture.openFileInEditor(file)
+    if (placeholderIndex != null) {
+      val placeholder = taskFile.answerPlaceholders[placeholderIndex]
+      myFixture.editor.selectionModel.setSelection(placeholder.offset, placeholder.offset + placeholder.realLength)
+    }
   }
 }
