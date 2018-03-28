966b3bfae909bb8f3126a705a67400b7cd11baac
Liana.Bakradze <liana.bakradze@jetbrains.com>
2018-03-28 18:09:18 +0300

    cleanup: remove author comment
    (cherry picked from commit 6971ad9)

--- educational-core/src/com/jetbrains/edu/learning/ui/taskDescription/EduCondition.java
+++ educational-core/src/com/jetbrains/edu/learning/ui/taskDescription/EduCondition.java
@@ -5,10 +5,6 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Condition;
 import com.jetbrains.edu.learning.EduUtils;
 
-/**
- * author: liana
- * data: 7/29/14.
- */
 public class EduCondition implements Condition<Project>, DumbAware {
   @Override
   public boolean value(Project project) {
