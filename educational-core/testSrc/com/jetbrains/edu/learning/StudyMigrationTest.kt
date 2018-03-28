47e94011efb7a73837aa7dd08dcda618303becc0
Liana.Bakradze <liana.bakradze@jetbrains.com>
2018-03-28 17:01:41 +0300

    convert migration test to Kotlin
    (cherry picked from commit 16c2d78)

---
+++ educational-core/testSrc/com/jetbrains/edu/learning/StudyMigrationTest.kt
@@ -0,0 +1,40 @@
+package com.jetbrains.edu.learning
+
+import com.intellij.openapi.util.JDOMUtil
+import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCase
+import com.intellij.util.loadElement
+import com.jetbrains.edu.learning.serialization.SerializationUtils
+import java.nio.file.Paths
+
+class StudyMigrationTest : LightPlatformCodeInsightFixtureTestCase() {
+
+  fun testFromThirdToForth() = doTest(3)
+
+  fun testAdaptive45() = doTest(4)
+
+  fun testSubtasks45() = doTest(4)
+
+  fun testTheory35To4() = doTest(4)
+
+  fun testTheory351To4() = doTest(4)
+
+  fun testPycharmToEdu() = doTest(7)
+
+  private fun doTest(version: Int) {
+    val name = getTestName(true)
+    val before = Paths.get(testDataPath).resolve("$name.xml")
+    val after = Paths.get(testDataPath).resolve("$name.after.xml")
+    val element = loadElement(before)
+    var converted = element
+    when (version) {
+      1 -> converted = SerializationUtils.Xml.convertToSecondVersion(project, element)
+      3 -> converted = SerializationUtils.Xml.convertToFourthVersion(project, element)
+      4 -> converted = SerializationUtils.Xml.convertToFifthVersion(project, element)
+      7 -> converted = SerializationUtils.Xml.convertToSeventhVersion(project, element)
+    }
+    assertTrue(JDOMUtil.areElementsEqual(converted, loadElement(after)))
+  }
+
+  override fun getTestDataPath() = "testData/migration"
+
+}
