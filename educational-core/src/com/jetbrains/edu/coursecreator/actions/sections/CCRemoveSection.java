c952946f2cd33bc3791f0f035c5fc32d9c7cb44b
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2018-03-30 17:56:17 +0300

    update add/remove section actions
    CCAddSection/CCRemoveSection
    
    (cherry picked from commit f0a959d)

--- educational-core/src/com/jetbrains/edu/coursecreator/actions/sections/CCRemoveSection.java
+++ educational-core/src/com/jetbrains/edu/coursecreator/actions/sections/CCRemoveSection.java
@@ -2,16 +2,26 @@ package com.jetbrains.edu.coursecreator.actions.sections;
 
 import com.intellij.ide.projectView.ProjectView;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.actionSystem.Presentation;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.DumbAwareAction;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Computable;
+import com.intellij.openapi.vfs.VfsUtil;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.jetbrains.edu.coursecreator.CCUtils;
 import com.jetbrains.edu.learning.StudyTaskManager;
 import com.jetbrains.edu.learning.courseFormat.Course;
+import com.jetbrains.edu.learning.courseFormat.Lesson;
 import com.jetbrains.edu.learning.courseFormat.Section;
 
+import java.io.IOException;
+import java.util.List;
+
 public class CCRemoveSection extends DumbAwareAction {
+  protected static final Logger LOG = Logger.getInstance(CCRemoveSection.class);
   public static final String TITLE = "Unwrap Section";
 
   public CCRemoveSection() {
@@ -24,30 +34,86 @@ public class CCRemoveSection extends DumbAwareAction {
     if (project == null) {
       return;
     }
-    final Object[] selectedItems = PlatformDataKeys.SELECTED_ITEMS.getData(e.getDataContext());
+    final VirtualFile[] selectedFiles = e.getData(CommonDataKeys.VIRTUAL_FILE_ARRAY);
     final Course course = StudyTaskManager.getInstance(project).getCourse();
     if (course == null) {
       return;
     }
-    if (selectedItems != null && selectedItems.length == 1 && selectedItems[0] instanceof Section) {
-      final Section selectedSection = (Section)selectedItems[0];
-      course.removeSection(selectedSection);
+    if (selectedFiles == null || selectedFiles.length != 1) {
+      return;
+    }
+
+    final VirtualFile file = selectedFiles[0];
+    final Section section = course.getSection(file.getName());
+    if (section == null) {
+      return;
+    }
+    if (removeSectionDir(file, project.getBaseDir())) {
+      final List<Lesson> lessonsFromSection = section.getLessons();
+      final int sectionIndex = section.getIndex();
+      course.removeSection(section);
+
+      for (Lesson lesson : lessonsFromSection) {
+        lesson.setIndex(lesson.getIndex() + sectionIndex - 1);
+        lesson.setSection(null);
+      }
+      for (Lesson lesson : course.getLessons()) {
+        if (lesson.getIndex() > sectionIndex) {
+          lesson.setIndex(lesson.getIndex() + lessonsFromSection.size() - 1);
+        }
+      }
+
+      course.addLessons(lessonsFromSection);
+      course.sortItems();
     }
 
     ProjectView.getInstance(project).refresh();
+  }
+
+  private boolean removeSectionDir(VirtualFile file, VirtualFile courseDir) {
+    return ApplicationManager.getApplication().runWriteAction(new Computable<Boolean>() {
+      @Override
+      public Boolean compute() {
+        final VirtualFile[] children = VfsUtil.getChildren(file);
+        for (VirtualFile child : children) {
+          try {
+            child.move(this, courseDir);
+          }
+          catch (IOException e) {
+            LOG.error("Failed to move lesson " + child.getName() + " out of section");
+            return false;
+          }
+        }
+        try {
+          file.delete(this);
+        }
+        catch (IOException e) {
+          LOG.error("Failed to delete section " + file.getName());
+          return false;
+        }
+        return true;
+      }
+    });
   }
 
   @Override
   public void update(AnActionEvent e) {
     Project project = e.getProject();
     Presentation presentation = e.getPresentation();
-    if (project != null && CCUtils.isCourseCreator(project)) {
-      final Object[] selectedItems = PlatformDataKeys.SELECTED_ITEMS.getData(e.getDataContext());
-      if (selectedItems != null && selectedItems.length == 1 && selectedItems[0] instanceof Section) {
-        presentation.setEnabledAndVisible(true);
-        return;
+    presentation.setEnabledAndVisible(false);
+    if (project == null) return;
+    final Course course = StudyTaskManager.getInstance(project).getCourse();
+    if (course == null) {
+      return;
+    }
+    if (CCUtils.isCourseCreator(project)) {
+      final VirtualFile[] selectedFiles = e.getData(CommonDataKeys.VIRTUAL_FILE_ARRAY);
+      if (selectedFiles != null && selectedFiles.length == 1) {
+        final Section section = course.getSection(selectedFiles[0].getName());
+        if (section != null) {
+          presentation.setEnabledAndVisible(true);
+        }
       }
     }
-    presentation.setEnabledAndVisible(false);
   }
 }
