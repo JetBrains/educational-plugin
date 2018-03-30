9fc8c869fa6ffba62a8efc0702fad082812af64c
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2018-03-30 17:56:17 +0300

    update add/remove section actions
    CCAddSection/CCRemoveSection
    
    (cherry picked from commit f0a959d)

--- educational-core/src/com/jetbrains/edu/coursecreator/actions/sections/CCAddSection.java
+++ educational-core/src/com/jetbrains/edu/coursecreator/actions/sections/CCAddSection.java
@@ -1,17 +1,33 @@
 package com.jetbrains.edu.coursecreator.actions.sections;
 
+import com.intellij.ide.projectView.ProjectView;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.actionSystem.Presentation;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.DumbAwareAction;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.ui.NonEmptyInputValidator;
+import com.intellij.openapi.util.Computable;
+import com.intellij.openapi.vfs.VfsUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.jetbrains.edu.coursecreator.CCUtils;
+import com.jetbrains.edu.learning.EduUtils;
 import com.jetbrains.edu.learning.StudyTaskManager;
 import com.jetbrains.edu.learning.courseFormat.Course;
+import com.jetbrains.edu.learning.courseFormat.Lesson;
+import com.jetbrains.edu.learning.courseFormat.Section;
 import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class CCAddSection extends DumbAwareAction {
+  protected static final Logger LOG = Logger.getInstance(CCAddSection.class);
+
   public static final String TITLE = "Wrap With Section";
   @NonNls private static final String SECTION = "Section";
 
@@ -33,29 +49,88 @@ public class CCAddSection extends DumbAwareAction {
     if (course == null) {
       return;
     }
-    //final List<Section> sections = course.getSections();
-    //final List<Integer> lessonsInSections =
-    //  sections.stream().map(section -> section.lessonIndexes).flatMap(lessonIndexes -> lessonIndexes.stream()).collect(Collectors.toList());
-    //final ArrayList<Integer> lessonsToWrap = new ArrayList<>();
-    //for (VirtualFile file : virtualFiles) {
-    //  final Lesson lesson = course.getLesson(file.getName());
-    //  if (lesson != null && !lessonsInSections.contains(lesson.getIndex())) {
-    //    lessonsToWrap.add(lesson.getIndex());
-    //  }
-    //}
-    //if (lessonsToWrap.isEmpty()) {
-    //  return;
-    //}
-    //final int index = sections.size() + 1;
-    //final String sectionName = Messages.showInputDialog("Enter Section Name", SECTION, null,
-    //                                                    SECTION.toLowerCase() + index, new NonEmptyInputValidator());
-    //if (sectionName != null) {
-    //  final Section section = new Section();
-    //  section.setTitle(sectionName);
-    //  section.lessonIndexes.addAll(lessonsToWrap);
-    //  course.addSections(Collections.singletonList(section));
-    //  ProjectView.getInstance(project).refresh();
-    //}
+
+    final ArrayList<Lesson> lessonsToWrap = new ArrayList<>();
+    final ArrayList<VirtualFile> lessonDirsToWrap = new ArrayList<>();
+    for (VirtualFile file : virtualFiles) {
+      final Lesson lesson = course.getLesson(file.getName());
+      if (lesson != null) {
+        lessonsToWrap.add(lesson);
+        lessonDirsToWrap.add(file);
+      }
+    }
+    if (lessonsToWrap.isEmpty()) {
+      return;
+    }
+
+    final int sectionIndex = course.getSections().size() + 1;
+    final String sectionName = Messages.showInputDialog("Enter Section Name", SECTION, null,
+                                                        SECTION.toLowerCase() + sectionIndex, new NonEmptyInputValidator());
+    if (sectionName == null) {
+      return;
+    }
+
+    lessonsToWrap.sort(EduUtils.INDEX_COMPARATOR);
+    final int minIndex = lessonsToWrap.get(0).getIndex();
+    final int maxIndex = lessonsToWrap.get(lessonsToWrap.size() - 1).getIndex();
+
+    final VirtualFile sectionDir = createSectionDir(project, sectionName);
+    if (sectionDir != null) {
+      final Section section = new Section();
+      section.setIndex(minIndex);
+      section.setName(sectionName);
+      section.addLessons(lessonsToWrap);
+
+      int lessonIndex = 1;
+      for (Lesson lesson : lessonsToWrap) {
+        lesson.setIndex(lessonIndex);
+        lesson.setSection(section);
+        lessonIndex += 1;
+      }
+      moveLessons(lessonDirsToWrap, sectionDir);
+
+      for (Lesson lesson : course.getLessons()) {
+        if (lessonsToWrap.contains(lesson)) {
+          course.removeLesson(lesson);
+        }
+        else if (lesson.getIndex() > maxIndex){
+          lesson.setIndex(lesson.getIndex() - lessonsToWrap.size() + 1);
+        }
+      }
+      course.addItem(section, section.getIndex()-1);
+      ProjectView.getInstance(project).refresh();
+    }
+  }
+
+  private void moveLessons(@NotNull final ArrayList<VirtualFile> lessonDirsToWrap, @NotNull final VirtualFile sectionDir) {
+    ApplicationManager.getApplication().runWriteAction(new Runnable() {
+      @Override
+      public void run() {
+        for (VirtualFile file : lessonDirsToWrap) {
+          try {
+            file.move(this, sectionDir);
+          }
+          catch (IOException e1) {
+            LOG.error("Failed to move lessons to the new section");
+          }
+        }
+      }
+    });
+  }
+
+  private static VirtualFile createSectionDir(Project project, String sectionName) {
+    return ApplicationManager.getApplication().runWriteAction(new Computable<VirtualFile>() {
+      @Override
+      public VirtualFile compute() {
+        try {
+          return VfsUtil.createDirectoryIfMissing(project.getBaseDir(), sectionName);
+        }
+        catch (IOException e1) {
+          LOG.error("Failed to create directory for section " + sectionName);
+        }
+        return null;
+      }
+    });
   }
 
   @Override
@@ -74,25 +149,15 @@ public class CCAddSection extends DumbAwareAction {
     if (course == null) {
       return;
     }
-    //final List<Section> sections = course.getSections();
-    //final List<Integer> lessonsInSections =
-    //  sections.stream().map(section -> section.lessonIndexes).flatMap(lessonIndexes -> lessonIndexes.stream()).collect(Collectors.toList());
-    //final Object[] selectedItems = PlatformDataKeys.SELECTED_ITEMS.getData(e.getDataContext());
-    //if (selectedItems != null) {
-    //  for (Object item : selectedItems) {
-    //    if (item instanceof Section) {
-    //      return;
-    //    }
-    //    else if (item instanceof PsiDirectory) {
-    //      final Lesson lesson = course.getLesson(((PsiDirectory)item).getName());
-    //      if (lesson != null) {
-    //        if (lessonsInSections.contains(lesson.getIndex())) {
-    //          return;
-    //        }
-    //        presentation.setEnabledAndVisible(true);
-    //      }
-    //    }
-    //  }
-    //}
+    final ArrayList<Lesson> lessonsToWrap = new ArrayList<>();
+    for (VirtualFile file : virtualFiles) {
+      final Lesson lesson = course.getLesson(file.getName());
+      if (lesson != null) {
+        lessonsToWrap.add(lesson);
+      }
+    }
+    if (!lessonsToWrap.isEmpty()) {
+      presentation.setEnabledAndVisible(true);
+    }
   }
 }
