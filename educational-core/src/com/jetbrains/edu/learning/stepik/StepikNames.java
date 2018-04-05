674302454abf055bc340685f4e53b27326e2fc70
Valentina Kiryushkina <valentina.kiryushkina@jetbrains.com>
2018-04-05 16:05:32 +0300

    EDU-1306 Add JetBrains user as admin to courses posted from edu

--- educational-core/src/com/jetbrains/edu/learning/stepik/StepikNames.java
+++ educational-core/src/com/jetbrains/edu/learning/stepik/StepikNames.java
@@ -31,4 +31,5 @@ public class StepikNames {
   public static final String OAUTH_SERVICE_NAME = "edu/stepik/oauth";
   public static final String EXTERNAL_REDIRECT_URL = "https://example.com";
   public static final String PYCHARM_ADDITIONAL = "PyCharm additional materials";
+  public static final String MEMBERS = "/members";
 }
