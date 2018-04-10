ebffa8d0529ba64247c31587ee5d97b6eaa39691
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2018-04-10 16:03:24 +0300

    EDU-1400: add 'Versions.md' doc

--- educational-core/src/com/jetbrains/edu/learning/EduVersions.kt
+++ educational-core/src/com/jetbrains/edu/learning/EduVersions.kt
@@ -2,5 +2,6 @@
 
 package com.jetbrains.edu.learning
 
+// If you change version of any format, add point about it in `docs/Versions.md`
 const val XML_FORMAT_VERSION: Int = 9
 const val JSON_FORMAT_VERSION: Int = 5
