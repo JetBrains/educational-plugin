dc9cedae904e0f75a2558e667de4e2806f6c7e0f
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2018-04-25 12:39:00 +0300

    EDU-1410: handle task description file renaming
    (cherry picked from commit 1e5c820)

--- educational-core/src/com/jetbrains/edu/coursecreator/handlers/CCLessonRenameHandler.java
+++ educational-core/src/com/jetbrains/edu/coursecreator/handlers/CCLessonRenameHandler.java
@@ -3,7 +3,7 @@ package com.jetbrains.edu.coursecreator.handlers;
 import com.intellij.ide.TitledHandler;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.PsiDirectory;
+import com.intellij.psi.PsiFileSystemItem;
 import com.jetbrains.edu.learning.EduNames;
 import com.jetbrains.edu.learning.EduUtils;
 import com.jetbrains.edu.learning.courseFormat.Course;
@@ -12,15 +12,15 @@ import org.jetbrains.annotations.NotNull;
 
 public class CCLessonRenameHandler extends CCRenameHandler implements TitledHandler {
   @Override
-  protected boolean isAvailable(@NotNull Project project, @NotNull VirtualFile dir) {
-    return EduUtils.isLessonDirectory(project, dir);
+  protected boolean isAvailable(@NotNull Project project, @NotNull VirtualFile file) {
+    return EduUtils.isLessonDirectory(project, file);
   }
 
   @Override
-  protected void rename(@NotNull Project project, @NotNull Course course, @NotNull PsiDirectory directory) {
-    Lesson lesson = EduUtils.getLesson(directory.getVirtualFile(), course);
+  protected void rename(@NotNull Project project, @NotNull Course course, @NotNull PsiFileSystemItem item) {
+    Lesson lesson = EduUtils.getLesson(item.getVirtualFile(), course);
     if (lesson != null) {
-      processRename(lesson, EduNames.LESSON, course, project, directory.getVirtualFile());
+      processRename(lesson, EduNames.LESSON, course, project, item.getVirtualFile());
     }
   }
 
--- educational-core/src/com/jetbrains/edu/coursecreator/handlers/CCSectionRenameHandler.java
+++ educational-core/src/com/jetbrains/edu/coursecreator/handlers/CCSectionRenameHandler.java
@@ -4,6 +4,7 @@ import com.intellij.ide.TitledHandler;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiDirectory;
+import com.intellij.psi.PsiFileSystemItem;
 import com.jetbrains.edu.learning.EduNames;
 import com.jetbrains.edu.learning.StudyTaskManager;
 import com.jetbrains.edu.learning.courseFormat.Course;
@@ -12,16 +13,18 @@ import org.jetbrains.annotations.NotNull;
 
 public class CCSectionRenameHandler extends CCRenameHandler implements TitledHandler {
   @Override
-  protected boolean isAvailable(@NotNull Project project, @NotNull VirtualFile dir) {
+  protected boolean isAvailable(@NotNull Project project, @NotNull VirtualFile file) {
     Course course = StudyTaskManager.getInstance(project).getCourse();
     if (course == null) {
       return false;
     }
-    return course.getSection(dir.getName()) != null;
+    return file.isDirectory() && course.getSection(file.getName()) != null;
   }
 
   @Override
-  protected void rename(@NotNull Project project, @NotNull Course course, @NotNull PsiDirectory directory) {
+  protected void rename(@NotNull Project project, @NotNull Course course, @NotNull PsiFileSystemItem item) {
+    if (!(item instanceof PsiDirectory)) return;
+    PsiDirectory directory = (PsiDirectory)item;
     Section section = course.getSection(directory.getName());
     if (section != null) {
       processRename(section, EduNames.SECTION, course, project, directory.getVirtualFile());
