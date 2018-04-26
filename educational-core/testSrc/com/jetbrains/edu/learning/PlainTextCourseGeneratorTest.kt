bdcf014ad5b6b5d0c2d3f0785553b63618c84a17
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2018-04-26 13:57:04 +0300

    add course creation tests
    (cherry picked from commit d02734b)

--- educational-core/testSrc/com/jetbrains/edu/learning/PlainTextCourseGeneratorTest.kt
+++ educational-core/testSrc/com/jetbrains/edu/learning/PlainTextCourseGeneratorTest.kt
@@ -1,6 +1,7 @@
 package com.jetbrains.edu.learning
 
 import com.intellij.testFramework.LightPlatformTestCase
+import com.jetbrains.edu.coursecreator.CCUtils
 
 class PlainTextCourseGeneratorTest : EduTestCase() {
 
@@ -21,7 +22,7 @@ class PlainTextCourseGeneratorTest : EduTestCase() {
       }
     }
 
-    val expectedFileTree = fileTree {
+    checkFileTree {
       dir("lesson1") {
         dir("task1") {
           file("Fizz.kt")
@@ -36,8 +37,6 @@ class PlainTextCourseGeneratorTest : EduTestCase() {
         }
       }
     }
-
-    expectedFileTree.assertEquals(LightPlatformTestCase.getSourceRoot())
   }
 
   fun `test course with framework lesson structure creation`() {
@@ -57,7 +56,7 @@ class PlainTextCourseGeneratorTest : EduTestCase() {
       }
     }
 
-    val expectedFileTree = fileTree {
+    checkFileTree {
       dir("lesson1") {
         dir("task") {
           file("Fizz.kt")
@@ -69,6 +68,183 @@ class PlainTextCourseGeneratorTest : EduTestCase() {
         }
       }
     }
-    expectedFileTree.assertEquals(LightPlatformTestCase.getSourceRoot())
+  }
+
+  fun `test course with sections creation`() {
+    courseWithFiles {
+      section {
+        lesson {
+          eduTask {
+            taskFile("Fizz.kt")
+          }
+          eduTask {
+            taskFile("Buzz.kt")
+          }
+        }
+      }
+      lesson {
+        eduTask {
+          taskFile("FizzBuzz.kt")
+        }
+      }
+    }
+
+    checkFileTree {
+      dir("section1") {
+        dir("lesson1") {
+          dir("task1") {
+            file("Fizz.kt")
+          }
+          dir("task2") {
+            file("Buzz.kt")
+          }
+        }
+      }
+      dir("lesson1") {
+        dir("task1") {
+          file("FizzBuzz.kt")
+        }
+      }
+    }
+  }
+
+  fun `test course creation in CC mode`() {
+    courseWithFiles(courseMode = CCUtils.COURSE_MODE) {
+      section {
+        lesson {
+          eduTask {
+            taskFile("Fizz.kt")
+          }
+          eduTask {
+            taskFile("Buzz.kt")
+          }
+        }
+      }
+      lesson {
+        eduTask {
+          taskFile("FizzBuzz.kt")
+        }
+      }
+    }
+
+    checkFileTree {
+      dir("section1") {
+        dir("lesson1") {
+          dir("task1") {
+            file("Fizz.kt")
+            file("task.html")
+          }
+          dir("task2") {
+            file("Buzz.kt")
+            file("task.html")
+          }
+        }
+      }
+      dir("lesson1") {
+        dir("task1") {
+          file("FizzBuzz.kt")
+          file("task.html")
+        }
+      }
+    }
+  }
+
+  fun `test course with sections creation in CC mode`() {
+    courseWithFiles(courseMode = CCUtils.COURSE_MODE) {
+      section {
+        lesson {
+          eduTask {
+            taskFile("Fizz.kt")
+          }
+          eduTask {
+            taskFile("Buzz.kt")
+          }
+        }
+      }
+      lesson {
+        eduTask {
+          taskFile("FizzBuzz.kt")
+        }
+      }
+    }
+
+    checkFileTree {
+      dir("section1") {
+        dir("lesson1") {
+          dir("task1") {
+            file("Fizz.kt")
+            file("task.html")
+          }
+          dir("task2") {
+            file("Buzz.kt")
+            file("task.html")
+          }
+        }
+      }
+      dir("lesson1") {
+        dir("task1") {
+          file("FizzBuzz.kt")
+          file("task.html")
+        }
+      }
+    }
+  }
+
+  fun `test creation of course with sections and without top level lessons in CC mode`() {
+    courseWithFiles(courseMode = CCUtils.COURSE_MODE) {
+      section {
+        lesson {
+          eduTask {
+            taskFile("Fizz.kt")
+          }
+          eduTask {
+            taskFile("Buzz.kt")
+          }
+        }
+        lesson {
+          eduTask {
+            taskFile("FizzBuzz.kt")
+          }
+        }
+      }
+    }
+
+    checkFileTree {
+      dir("section1") {
+        dir("lesson1") {
+          dir("task1") {
+            file("Fizz.kt")
+            file("task.html")
+          }
+          dir("task2") {
+            file("Buzz.kt")
+            file("task.html")
+          }
+        }
+        dir("lesson2") {
+          dir("task1") {
+            file("FizzBuzz.kt")
+            file("task.html")
+          }
+        }
+      }
+    }
+  }
+
+  fun `test empty course creation`() {
+    courseWithFiles(courseMode = CCUtils.COURSE_MODE) {}
+
+    checkFileTree {
+      dir("lesson1") {
+        dir("task1") {
+          file("Task.txt")
+          file("task.html")
+        }
+      }
+    }
+  }
+
+  private fun checkFileTree(block: FileTreeBuilder.() -> Unit) {
+    fileTree(block).assertEquals(LightPlatformTestCase.getSourceRoot())
   }
 }
