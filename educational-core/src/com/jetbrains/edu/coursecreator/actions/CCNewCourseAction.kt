de8fc122262797fe49666078490c720b40713c4e
Liana.Bakradze <liana.bakradze@jetbrains.com>
2018-04-27 15:23:24 +0300

    update create new course icon

--- educational-core/src/com/jetbrains/edu/coursecreator/actions/CCNewCourseAction.kt
+++ educational-core/src/com/jetbrains/edu/coursecreator/actions/CCNewCourseAction.kt
@@ -1,11 +1,11 @@
 package com.jetbrains.edu.coursecreator.actions
 
+import com.intellij.icons.AllIcons
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.jetbrains.edu.coursecreator.ui.CCNewCourseDialog
-import icons.EducationalCoreIcons
 
-class CCNewCourseAction : AnAction("Create New Course", "Create new educational course", EducationalCoreIcons.CourseAction) {
+class CCNewCourseAction : AnAction("Create New Course", "Create new educational course", AllIcons.General.Add) {
 
   override fun actionPerformed(e: AnActionEvent) {
     CCNewCourseDialog("Create Course", "Create").show()
