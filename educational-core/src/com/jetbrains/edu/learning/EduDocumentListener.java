7ba584b802f83629b263699ced9095065b104d90
Ekaterina Tuzova <ekaterina.tuzova@jetbrains.com>
2018-04-29 12:49:23 +0100

    Handle selection deletion overlapping answer placeholder

--- educational-core/src/com/jetbrains/edu/learning/EduDocumentListener.java
+++ educational-core/src/com/jetbrains/edu/learning/EduDocumentListener.java
@@ -53,21 +53,24 @@ public class EduDocumentListener implements DocumentListener {
 
     for (AnswerPlaceholder placeholder : myTaskFile.getAnswerPlaceholders()) {
       int placeholderStart = placeholder.getOffset();
+      int placeholderEnd = placeholder.getEndOffset();
+
+      int changeForPlaceholder = getChangeForPlaceholder(offset, change, placeholder);
+
       if (placeholderStart - 1 == offset && fragment.toString().isEmpty() && oldFragment.toString().startsWith("\n")) {
         placeholderStart -= 1;
       }
 
       if (placeholderStart > offset) {
-        placeholderStart += change;
+        placeholderStart += changeForPlaceholder;
       }
-      int placeholderEnd = placeholder.getEndOffset();
       if (placeholderEnd == offset) {
         if (e.getOldLength() != 1 && e.getNewLength() != 0) {
-          placeholderEnd += change;
+          placeholderEnd += changeForPlaceholder;
         }
       }
-      else if (placeholderEnd > offset ) {
-        placeholderEnd += change;
+      else if (placeholderEnd > offset) {
+        placeholderEnd += placeholderStart >= offset ? change : changeForPlaceholder;
       }
       if (placeholderStart == offset && oldFragment.toString().isEmpty() && fragment.toString().startsWith("\n")) {
         placeholderStart += 1;
@@ -78,6 +81,21 @@ public class EduDocumentListener implements DocumentListener {
       assert placeholderStart >= 0;
       updatePlaceholder(placeholder, document, placeholderStart, length);
     }
+  }
+
+  private static int getChangeForPlaceholder(int offset, int change, @NotNull final AnswerPlaceholder placeholder) {
+    int placeholderStart = placeholder.getOffset();
+    int placeholderEnd = placeholder.getEndOffset();
+    int changeForPlaceholder = change;
+    if (change < 0) {
+      if (offset - change > placeholderEnd && offset < placeholderEnd) {
+        changeForPlaceholder = offset - placeholderEnd;
+      }
+      else if (offset - change > placeholderStart && offset < placeholderStart) {
+        changeForPlaceholder = offset - placeholderStart;
+      }
+    }
+    return changeForPlaceholder;
   }
 
   protected void updatePlaceholder(@NotNull AnswerPlaceholder answerPlaceholder,
