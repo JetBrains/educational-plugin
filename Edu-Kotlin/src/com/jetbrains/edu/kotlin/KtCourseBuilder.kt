4f1cdbfba460b11eba1b895be038cbaf3c730f93
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2018-05-15 19:09:23 +0300

    insert the latest kotlin plugin version in build.gradle

--- Edu-Kotlin/src/com/jetbrains/edu/kotlin/KtCourseBuilder.kt
+++ Edu-Kotlin/src/com/jetbrains/edu/kotlin/KtCourseBuilder.kt
@@ -1,15 +1,27 @@
 package com.jetbrains.edu.kotlin
 
+import com.intellij.util.text.VersionComparatorUtil
 import com.jetbrains.edu.learning.intellij.GradleCourseBuilderBase
+import com.jetbrains.edu.learning.pluginVersion
 
 open class KtCourseBuilder : GradleCourseBuilderBase() {
 
   override val buildGradleTemplateName: String = KOTLIN_BUILD_GRADLE_TEMPLATE_NAME
+
+  override val buildGradleVariables: Map<String, String>
+    get() = super.buildGradleVariables + Pair("KOTLIN_VERSION", kotlinPluginVersion)
 
   override fun getTaskTemplateName(): String = KtConfigurator.TASK_KT
   override fun getTestTemplateName(): String = KtConfigurator.TESTS_KT
 
   companion object {
     private const val KOTLIN_BUILD_GRADLE_TEMPLATE_NAME = "kotlin-build.gradle"
+    private const val KOTLIN_PLUGIN_ID = "org.jetbrains.kotlin"
+    private const val DEFAULT_KOTLIN_VERSION = "1.2.41"
+
+    private val kotlinPluginVersion: String get() {
+      val kotlinPluginVersion = pluginVersion(KOTLIN_PLUGIN_ID)?.takeWhile { it != '-' } ?: DEFAULT_KOTLIN_VERSION
+      return VersionComparatorUtil.max(kotlinPluginVersion, DEFAULT_KOTLIN_VERSION)
+    }
   }
 }
