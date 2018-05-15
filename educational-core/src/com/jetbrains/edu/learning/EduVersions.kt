6a0adcac466f420314cb24abeeb210f00eae9893
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2018-05-15 19:09:23 +0300

    insert the latest kotlin plugin version in build.gradle
    (cherry picked from commit 4f1cdbf)

--- educational-core/src/com/jetbrains/edu/learning/EduVersions.kt
+++ educational-core/src/com/jetbrains/edu/learning/EduVersions.kt
@@ -2,6 +2,11 @@
 
 package com.jetbrains.edu.learning
 
+import com.intellij.ide.plugins.PluginManager
+import com.intellij.openapi.extensions.PluginId
+
 // If you change version of any format, add point about it in `docs/Versions.md`
 const val XML_FORMAT_VERSION: Int = 9
 const val JSON_FORMAT_VERSION: Int = 5
+
+fun pluginVersion(pluginId: String): String? = PluginManager.getPlugin(PluginId.getId(pluginId))?.version
