1f696638c05573f0b6b1ba929d088c48cebf0455
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2018-05-15 16:13:11 +0300

    EDU-1425: correctly create non empty course in CC mode with framework lessons
    (cherry picked from commit f28bac4)

--- educational-core/testSrc/com/jetbrains/edu/learning/PlainTextCourseGeneratorTest.kt
+++ educational-core/testSrc/com/jetbrains/edu/learning/PlainTextCourseGeneratorTest.kt
@@ -70,6 +70,43 @@ class PlainTextCourseGeneratorTest : EduTestCase() {
     }
   }
 
+  fun `test course with framework lesson structure creation in CC mode`() {
+    courseWithFiles(courseMode = CCUtils.COURSE_MODE) {
+      frameworkLesson {
+        eduTask {
+          taskFile("Fizz.kt")
+        }
+        eduTask {
+          taskFile("Buzz.kt")
+        }
+      }
+      lesson {
+        eduTask {
+          taskFile("FizzBuzz.kt")
+        }
+      }
+    }
+
+    checkFileTree {
+      dir("lesson1") {
+        dir("task1") {
+          file("Fizz.kt")
+          file("task.html")
+        }
+        dir("task2") {
+          file("Buzz.kt")
+          file("task.html")
+        }
+      }
+      dir("lesson2") {
+        dir("task1") {
+          file("FizzBuzz.kt")
+          file("task.html")
+        }
+      }
+    }
+  }
+
   fun `test course with sections creation`() {
     courseWithFiles {
       section {
