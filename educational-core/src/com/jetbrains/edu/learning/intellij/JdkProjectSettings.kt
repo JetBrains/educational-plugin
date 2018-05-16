df27e8ec01144c839742120f7e748cfbcb21aed3
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2018-05-16 17:51:03 +0300

    added Scala support

--- educational-core/src/com/jetbrains/edu/learning/intellij/JdkProjectSettings.kt
+++ educational-core/src/com/jetbrains/edu/learning/intellij/JdkProjectSettings.kt
@@ -5,7 +5,7 @@ import com.intellij.openapi.roots.ui.configuration.JdkComboBox
 import com.intellij.openapi.roots.ui.configuration.ProjectStructureConfigurable
 import com.intellij.openapi.roots.ui.configuration.projectRoot.ProjectSdksModel
 
-data class JdkProjectSettings(val model: ProjectSdksModel, val jdkItem: JdkComboBox.JdkComboBoxItem?) {
+open class JdkProjectSettings(val model: ProjectSdksModel, val jdkItem: JdkComboBox.JdkComboBoxItem?) {
   companion object {
     fun emptySettings(): JdkProjectSettings {
       val configurable = ProjectStructureConfigurable.getInstance(ProjectManager.getInstance().defaultProject)
