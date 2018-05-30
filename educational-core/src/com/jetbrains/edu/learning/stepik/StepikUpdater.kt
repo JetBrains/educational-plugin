aecf30d4a0717303b1af543c5e8447040a1324cc
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2018-05-30 16:15:19 +0300

    EDU-1493: convert StepikUpdater to kotlin. step 2

--- educational-core/src/com/jetbrains/edu/learning/stepik/StepikUpdater.kt
+++ educational-core/src/com/jetbrains/edu/learning/stepik/StepikUpdater.kt
@@ -1,96 +1,94 @@
-package com.jetbrains.edu.learning.stepik;
+package com.jetbrains.edu.learning.stepik
 
-import com.intellij.ide.AppLifecycleListener;
-import com.intellij.ide.util.PropertiesComponent;
-import com.intellij.notification.Notification;
-import com.intellij.notification.NotificationType;
-import com.intellij.openapi.application.Application;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.util.ActionCallback;
-import com.intellij.openapi.util.Ref;
-import com.intellij.openapi.util.text.StringUtil;
-import com.intellij.util.Alarm;
-import com.intellij.util.text.DateFormatUtil;
-import com.jetbrains.edu.learning.InitializationComponent;
-import com.jetbrains.edu.learning.EduSettings;
-import com.jetbrains.edu.learning.courseFormat.Course;
-import com.jetbrains.edu.learning.courseFormat.RemoteCourse;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.ide.AppLifecycleListener
+import com.intellij.ide.util.PropertiesComponent
+import com.intellij.notification.Notification
+import com.intellij.notification.NotificationType
+import com.intellij.openapi.application.Application
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.util.ActionCallback
+import com.intellij.openapi.util.Ref
+import com.intellij.util.Alarm
+import com.intellij.util.text.DateFormatUtil
+import com.jetbrains.edu.learning.InitializationComponent
+import com.jetbrains.edu.learning.EduSettings
+import com.jetbrains.edu.learning.courseFormat.Course
+import com.jetbrains.edu.learning.courseFormat.RemoteCourse
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.ArrayList
+import java.util.Date
 
-public class StepikUpdater {
-  private static final long CHECK_INTERVAL = DateFormatUtil.DAY;
+class StepikUpdater(application: Application) {
 
-  private final Runnable myCheckRunnable = () -> updateCourseList().doWhenDone(() -> queueNextCheck(CHECK_INTERVAL));
-  private final Alarm myCheckForUpdatesAlarm = new Alarm(Alarm.ThreadToUse.SWING_THREAD);
+  private val myCheckRunnable = Runnable { updateCourseList().doWhenDone { queueNextCheck(CHECK_INTERVAL) } }
+  private val myCheckForUpdatesAlarm = Alarm(Alarm.ThreadToUse.SWING_THREAD)
 
-  public StepikUpdater(@NotNull Application application) {
-    scheduleCourseListUpdate(application);
+  init {
+    scheduleCourseListUpdate(application)
   }
 
-  public void scheduleCourseListUpdate(Application application) {
+  private fun scheduleCourseListUpdate(application: Application) {
     if (!checkNeeded()) {
-      return;
+      return
     }
-    application.getMessageBus().connect(application).subscribe(AppLifecycleListener.TOPIC, new AppLifecycleListener() {
-      @Override
-      public void appFrameCreated(String[] commandLineArgs, @NotNull Ref<Boolean> willOpenProject) {
+    application.messageBus.connect(application).subscribe(AppLifecycleListener.TOPIC, object : AppLifecycleListener {
+      override fun appFrameCreated(commandLineArgs: Array<String>?, willOpenProject: Ref<Boolean>) {
 
-        long timeToNextCheck = EduSettings.getInstance().getLastTimeChecked() + CHECK_INTERVAL - System.currentTimeMillis();
+        val timeToNextCheck = EduSettings.getInstance().lastTimeChecked + CHECK_INTERVAL - System.currentTimeMillis()
         if (timeToNextCheck <= 0) {
-          myCheckRunnable.run();
+          myCheckRunnable.run()
         }
         else {
-          queueNextCheck(timeToNextCheck);
+          queueNextCheck(timeToNextCheck)
         }
       }
-    });
+    })
   }
 
-  private static ActionCallback updateCourseList() {
-    ActionCallback callback = new ActionCallback();
-    ApplicationManager.getApplication().executeOnPooledThread(() -> {
-      final List<Course> courses = StepikConnector.getCourses(null);
-      EduSettings.getInstance().setLastTimeChecked(System.currentTimeMillis());
+  private fun queueNextCheck(interval: Long) {
+    myCheckForUpdatesAlarm.addRequest(myCheckRunnable, interval)
+  }
 
-      if (!courses.isEmpty()) {
-        List<Course> updated = new ArrayList<>();
-        for (Course course : courses) {
-          if (course instanceof RemoteCourse && ((RemoteCourse)course).getUpdateDate().
-                                                after(new Date(EduSettings.getInstance().getLastTimeChecked()))) {
-            updated.add(course);
+  companion object {
+    private val CHECK_INTERVAL = DateFormatUtil.DAY
+
+    private fun updateCourseList(): ActionCallback {
+      val callback = ActionCallback()
+      ApplicationManager.getApplication().executeOnPooledThread {
+        val courses = StepikConnector.getCourses(null)
+        EduSettings.getInstance().lastTimeChecked = System.currentTimeMillis()
+
+        if (!courses.isEmpty()) {
+          val updated = ArrayList<Course>()
+          for (course in courses) {
+            if (course is RemoteCourse && course.updateDate.after(
+                Date(EduSettings.getInstance().lastTimeChecked))) {
+              updated.add(course)
+            }
           }
+          if (updated.isEmpty()) return@executeOnPooledThread
+          val message: String
+          val title: String
+          if (updated.size == 1) {
+            message = updated[0].name
+            title = "New course available"
+          } else {
+            title = "New courses available"
+            message = updated.joinToString(separator = ", ") { it.name }
+          }
+          val notification = Notification("New.course", title, message, NotificationType.INFORMATION)
+          notification.notify(null)
         }
-        if (updated.isEmpty()) return;
-        final String message;
-        final String title;
-        if (updated.size() == 1) {
-          message = updated.get(0).getName();
-          title = "New course available";
-        }
-        else {
-          title = "New courses available";
-          message = StringUtil.join(updated, Course::getName, ", ");
-        }
-        final Notification notification = new Notification("New.course", title, message, NotificationType.INFORMATION);
-        notification.notify(null);
       }
-    });
-    return callback;
-  }
-
-  private void queueNextCheck(long interval) {
-    myCheckForUpdatesAlarm.addRequest(myCheckRunnable, interval);
-  }
-
-  private static boolean checkNeeded() {
-    if (!PropertiesComponent.getInstance().isValueSet(InitializationComponent.CONFLICTING_PLUGINS_DISABLED)) {
-      return false;
+      return callback
     }
-    long timeToNextCheck = EduSettings.getInstance().getLastTimeChecked() + CHECK_INTERVAL - System.currentTimeMillis();
-    return timeToNextCheck <= 0;
+
+    private fun checkNeeded(): Boolean {
+      if (!PropertiesComponent.getInstance().isValueSet(InitializationComponent.CONFLICTING_PLUGINS_DISABLED)) {
+        return false
+      }
+      val timeToNextCheck = EduSettings.getInstance().lastTimeChecked + CHECK_INTERVAL - System.currentTimeMillis()
+      return timeToNextCheck <= 0
+    }
   }
 }
