9e5793251b5da23bec667fa5c8614cd570f1341d
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2018-06-01 18:59:37 +0300

    EDU-1493: extract new courses notifier ui into separate file.
    Also allow setting mock ui for testing

--- educational-core/src/com/jetbrains/edu/learning/update/NewCoursesNotifier.kt
+++ educational-core/src/com/jetbrains/edu/learning/update/NewCoursesNotifier.kt
@@ -1,51 +1,48 @@
 package com.jetbrains.edu.learning.update
 
+import com.google.common.annotations.VisibleForTesting
 import com.intellij.ide.AppLifecycleListener
 import com.intellij.ide.util.PropertiesComponent
-import com.intellij.notification.Notification
-import com.intellij.notification.NotificationListener
-import com.intellij.notification.NotificationType
-import com.intellij.openapi.application.Application
+import com.intellij.openapi.Disposable
 import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.ui.DialogWrapper
+import com.intellij.openapi.components.ApplicationComponent
+import com.intellij.openapi.extensions.Extensions
 import com.intellij.openapi.util.ActionCallback
 import com.intellij.openapi.util.Ref
 import com.intellij.util.Alarm
 import com.intellij.util.text.DateFormatUtil
-import com.intellij.util.ui.JBUI
-import com.jetbrains.edu.learning.EduConfigurator
+import com.jetbrains.edu.learning.EduCoursesProvider
 import com.jetbrains.edu.learning.EduSettings
 import com.jetbrains.edu.learning.InitializationComponent
-import com.jetbrains.edu.learning.courseFormat.Course
 import com.jetbrains.edu.learning.courseFormat.RemoteCourse
-import com.jetbrains.edu.learning.courseFormat.ext.configurator
-import com.jetbrains.edu.learning.newproject.ui.CoursePanel
-import com.jetbrains.edu.learning.stepik.StepikConnector
+import com.jetbrains.edu.learning.isUnitTestMode
+import org.jetbrains.annotations.TestOnly
 import java.util.*
-import javax.swing.JComponent
+import java.util.concurrent.atomic.AtomicInteger
 
-class NewCoursesNotifier(application: Application) {
+class NewCoursesNotifier : ApplicationComponent, Disposable {
 
-  private val myCheckRunnable = Runnable { updateCourseList().doWhenDone { queueNextCheck(CHECK_INTERVAL) } }
-  private val myCheckForUpdatesAlarm = Alarm(Alarm.ThreadToUse.SWING_THREAD)
+  private val myCheckRunnable = Runnable { updateCourseList().doWhenDone { queueNextCheck(ourCheckInterval) } }
+  private val myCheckForUpdatesAlarm = Alarm(Alarm.ThreadToUse.POOLED_THREAD, this)
 
-  init {
-    scheduleCourseListUpdate(application)
-  }
-
-  private fun scheduleCourseListUpdate(application: Application) {
+  override fun initComponent() {
     if (!checkNeeded()) return
 
-    application.messageBus.connect(application).subscribe(AppLifecycleListener.TOPIC, object : AppLifecycleListener {
+    ApplicationManager.getApplication().messageBus.connect().subscribe(AppLifecycleListener.TOPIC, object : AppLifecycleListener {
       override fun appFrameCreated(commandLineArgs: Array<String>?, willOpenProject: Ref<Boolean>) {
-        val timeToNextCheck = EduSettings.getInstance().lastTimeChecked + CHECK_INTERVAL - System.currentTimeMillis()
-        if (timeToNextCheck <= 0) {
-          myCheckRunnable.run()
-        } else {
-          queueNextCheck(timeToNextCheck)
-        }
+        scheduleCourseListUpdate()
       }
     })
+  }
+
+  @VisibleForTesting
+  fun scheduleCourseListUpdate() {
+    val timeToNextCheck = EduSettings.getInstance().lastTimeChecked + ourCheckInterval - System.currentTimeMillis()
+    return if (timeToNextCheck <= 0) {
+      myCheckRunnable.run()
+    } else {
+      queueNextCheck(timeToNextCheck)
+    }
   }
 
   private fun queueNextCheck(interval: Long) {
@@ -56,21 +53,16 @@ class NewCoursesNotifier(application: Application) {
     val callback = ActionCallback()
 
     ApplicationManager.getApplication().executeOnPooledThread {
-      val courses = StepikConnector.getCourses(null)
-      EduSettings.getInstance().lastTimeChecked = System.currentTimeMillis()
+      val courses = Extensions.getExtensions(EduCoursesProvider.EP_NAME).flatMap(EduCoursesProvider::loadCourses)
 
       val updated = courses.filterIsInstance<RemoteCourse>()
         .filter { it.updateDate.after(Date(EduSettings.getInstance().lastTimeChecked)) }
       if (!updated.isEmpty()) {
-        val title = if (updated.size == 1) "New course available" else "New courses available"
-        val message = updated.joinToString(separator = " <br> ") { "<a href=\"${it.id}\">${it.name}</a>" }
-        val notification = Notification("New.course", title, message, NotificationType.INFORMATION, NotificationListener { notification, e ->
-          notification.expire()
-          val course = updated.find { it.id.toString() == e.description } ?: return@NotificationListener
-          val configurator = course.configurator ?: return@NotificationListener
-          SingleCourseDialog(course, configurator).show()
-        })
-        notification.notify(null)
+        showNewCoursesNotification(updated)
+      }
+      EduSettings.getInstance().lastTimeChecked = System.currentTimeMillis()
+      if (isUnitTestMode) {
+        INVOCATION_COUNTER.incrementAndGet()
       }
       callback.setDone()
     }
@@ -81,45 +73,25 @@ class NewCoursesNotifier(application: Application) {
     if (!PropertiesComponent.getInstance().isValueSet(InitializationComponent.CONFLICTING_PLUGINS_DISABLED)) {
       return false
     }
-    val timeToNextCheck = EduSettings.getInstance().lastTimeChecked + CHECK_INTERVAL - System.currentTimeMillis()
+    val timeToNextCheck = EduSettings.getInstance().lastTimeChecked + ourCheckInterval - System.currentTimeMillis()
     return timeToNextCheck <= 0
   }
 
-  companion object {
-    private const val CHECK_INTERVAL = DateFormatUtil.DAY
+  override fun dispose() {}
+
+  @TestOnly
+  fun setNewCheckInterval(newInterval: Long): Long {
+    val oldValue = ourCheckInterval
+    ourCheckInterval = newInterval
+    return oldValue
   }
 
-  private class SingleCourseDialog(
-    private val myCourse: Course,
-    private val myConfigurator: EduConfigurator<*>
-  ) : DialogWrapper(true) {
+  @TestOnly
+  fun invocationNumber(): Int = INVOCATION_COUNTER.get()
 
-    private val myPanel: CoursePanel = CoursePanel(/*isIndependent = */ true, /*isLocationFieldNeeded = */ true).apply {
-      preferredSize = JBUI.size(WIDTH, HEIGHT)
-      minimumSize = JBUI.size(WIDTH, HEIGHT)
-    }
+  companion object {
+    private var ourCheckInterval = DateFormatUtil.DAY
 
-    init {
-      title = "Create Course"
-      setOKButtonText("Create")
-      myPanel.bindCourse(myCourse)
-      init()
-    }
-
-    override fun createCenterPanel(): JComponent = myPanel
-
-    override fun doOKAction() {
-      val settings = myPanel.projectSettings
-      val location = myPanel.locationString ?: error("Location should be not null")
-      myConfigurator.courseBuilder
-        .getCourseProjectGenerator(myCourse)
-        ?.doCreateCourseProject(location, settings)
-      close(OK_EXIT_CODE)
-    }
-
-    companion object {
-      private const val WIDTH: Int = 370
-      private const val HEIGHT: Int = 330
-    }
+    private val INVOCATION_COUNTER: AtomicInteger = AtomicInteger()
   }
 }
