e8e08b6c090ad8ecfeee4490f13c3e69ce7c9250
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2018-06-06 23:22:57 +0300

    EDU-1493: add 'EduCoursesProvider.loadAllCourses' method
    (cherry picked from commit 9531e50)

--- educational-core/src/com/jetbrains/edu/learning/EduCoursesProvider.java
+++ educational-core/src/com/jetbrains/edu/learning/EduCoursesProvider.java
@@ -1,10 +1,15 @@
 package com.jetbrains.edu.learning;
 
 import com.intellij.openapi.extensions.ExtensionPointName;
+import com.intellij.openapi.extensions.Extensions;
 import com.jetbrains.edu.learning.courseFormat.Course;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.jetbrains.edu.learning.OpenApiExtKt.checkIsBackgroundThread;
 
 public interface EduCoursesProvider {
 
@@ -18,4 +23,16 @@ public interface EduCoursesProvider {
    */
   @NotNull
   List<Course> loadCourses();
+
+  /**
+   * Loads courses from all available providers
+   *
+   * @return list of loaded courses
+   */
+  static List<Course> loadAllCourses() {
+    checkIsBackgroundThread();
+    return Arrays.stream(Extensions.getExtensions(EP_NAME))
+      .flatMap(provider -> provider.loadCourses().stream())
+      .collect(Collectors.toList());
+  }
 }
