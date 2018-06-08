24f13026a134c042ab78af8de51ad08d9bc77919
Valentina Kiryushkina <valentina.kiryushkina@jetbrains.com>
2018-06-08 19:27:40 +0300

    Use unit update date to check if lesson info was changed
    (cherry picked from commit 913a6c1decaa17f298d602ea39834d5a4fab1a5e)

--- educational-core/src/com/jetbrains/edu/learning/courseFormat/Lesson.java
+++ educational-core/src/com/jetbrains/edu/learning/courseFormat/Lesson.java
@@ -11,6 +11,7 @@ import com.jetbrains.edu.learning.EduUtils;
 import com.jetbrains.edu.learning.courseFormat.tasks.*;
 import com.jetbrains.edu.learning.stepik.StepikConnector;
 import com.jetbrains.edu.learning.stepik.StepikNames;
+import com.jetbrains.edu.learning.stepik.StepikWrappers;
 import kotlin.collections.CollectionsKt;
 import one.util.streamex.StreamEx;
 import org.jetbrains.annotations.NotNull;
@@ -149,12 +150,17 @@ public class Lesson extends StudyItem {
     Lesson lessonInfo = StepikConnector.getLessonFromServer(myId);
     if (lessonInfo == null) return true;
     if (lessonInfo.myUpdateDate == null) return true;
+
+    StepikWrappers.Unit unit = StepikConnector.getUnit(unitId);
+    if (unit == null) return true;
+    if (unit.getUpdateDate() == null) return true;
+
     if (myUpdateDate == null) return false;
     if (lessonInfo.steps.size() != taskList.size()) return false;
     for (Task task : taskList) {
       if (!task.isUpToDate()) return false;
     }
-    return !lessonInfo.myUpdateDate.after(myUpdateDate);
+    return !lessonInfo.myUpdateDate.after(myUpdateDate) && !unit.getUpdateDate().after(myUpdateDate);
   }
 
   public boolean isAdditional() {
