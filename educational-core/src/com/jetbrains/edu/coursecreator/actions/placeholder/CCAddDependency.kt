520633833109a6f0e3c396d64d0dc15eca8079e7
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2018-06-14 13:48:44 +0300

    EDU-785: add possibility to set placeholder dependency visibility in UI

--- educational-core/src/com/jetbrains/edu/coursecreator/actions/placeholder/CCAddDependency.kt
+++ educational-core/src/com/jetbrains/edu/coursecreator/actions/placeholder/CCAddDependency.kt
@@ -1,50 +1,19 @@
 package com.jetbrains.edu.coursecreator.actions.placeholder
 
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.ui.InputValidator
-import com.intellij.openapi.ui.InputValidatorEx
-import com.intellij.openapi.ui.Messages
 import com.intellij.ui.EditorNotifications
 import com.jetbrains.edu.learning.courseFormat.AnswerPlaceholder
 import com.jetbrains.edu.learning.courseFormat.AnswerPlaceholderDependency
 import com.jetbrains.edu.learning.courseFormat.tasks.Task
 
-
 class CCAddDependency : CCAnswerPlaceholderAction(null, "Adds/Edits dependency on another answer placeholder") {
   override fun performAnswerPlaceholderAction(state: CCState) {
     val answerPlaceholder = state.answerPlaceholder ?: return
-    val validator: InputValidator = object : InputValidatorEx {
-      private var errorText: String? = null
-      override fun getErrorText(inputString: String): String? = errorText
 
-      override fun checkInput(inputString: String): Boolean {
-        return try {
-          val dependency = AnswerPlaceholderDependency.create(answerPlaceholder, inputString)
-          if (dependency == null) {
-            errorText = "invalid dependency"
-            return false
-          }
-          errorText = null
-          true
-        }
-        catch (e: AnswerPlaceholderDependency.InvalidDependencyException) {
-          errorText = e.customMessage
-          false
-        }
-      }
+    val (dependencyPath, isVisible) = CCDependencyDialog(state.project, answerPlaceholder).showAndGetResult() ?: return
 
-      override fun canClose(inputString: String): Boolean = errorText == null
-    }
-    val dependency = Messages.showInputDialog(state.project, "", getActionName(answerPlaceholder), null, getInitialValue(answerPlaceholder), validator, null)
-    if (dependency.isNullOrEmpty()) {
-      return
-    }
-    answerPlaceholder.placeholderDependency = AnswerPlaceholderDependency.create(answerPlaceholder, dependency!!)
+    answerPlaceholder.placeholderDependency = AnswerPlaceholderDependency.create(answerPlaceholder, dependencyPath, isVisible)
     EditorNotifications.getInstance(state.project).updateNotifications(state.file.virtualFile)
-  }
-
-  private fun getInitialValue(answerPlaceholder: AnswerPlaceholder) : String {
-    return if (answerPlaceholder.placeholderDependency != null) answerPlaceholder.placeholderDependency.toString() else "lesson1#task1#path/task.txt#1"
   }
 
 
@@ -60,8 +29,11 @@ class CCAddDependency : CCAnswerPlaceholderAction(null, "Adds/Edits dependency o
     e.presentation.isEnabledAndVisible = true
   }
 
-  private fun getActionName(answerPlaceholder: AnswerPlaceholder) =
-    "${if (answerPlaceholder.placeholderDependency == null) "Add" else "Edit"} Dependency"
+  companion object {
+    @JvmStatic
+    fun getActionName(answerPlaceholder: AnswerPlaceholder) =
+      "${if (answerPlaceholder.placeholderDependency == null) "Add" else "Edit"} Dependency"
+  }
 }
 
 private val Task.isFirstInCourse: Boolean
