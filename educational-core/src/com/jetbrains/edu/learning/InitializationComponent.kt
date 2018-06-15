f0029cda361c6e6bbb3c7ddfe92e437d7d97e72c
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2018-06-15 17:22:18 +0300

    EDU-1522 & EA-121465: check required plugins are enabled before course creation in 'BrowseCourses' dialog

--- educational-core/src/com/jetbrains/edu/learning/InitializationComponent.kt
+++ educational-core/src/com/jetbrains/edu/learning/InitializationComponent.kt
@@ -4,11 +4,9 @@ import com.intellij.ide.plugins.PluginManager
 import com.intellij.ide.plugins.PluginManagerCore
 import com.intellij.ide.util.PropertiesComponent
 import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.application.ApplicationNamesInfo
 import com.intellij.openapi.components.ApplicationComponent
 import com.intellij.openapi.editor.EditorFactory
 import com.intellij.openapi.extensions.PluginId
-import com.intellij.openapi.ui.Messages
 import com.intellij.openapi.util.text.StringUtil
 import com.jetbrains.edu.learning.editor.EduEditorFactoryListener
 import com.jetbrains.edu.learning.update.NewCoursesNotifier
@@ -32,18 +30,13 @@ class InitializationComponent : ApplicationComponent {
         var disabledPlugins = disablePlugins()
         if (disabledPlugins.isNotEmpty()) {
             disabledPlugins = disabledPlugins.map { name -> "'$name'" }
-            val ideName = ApplicationNamesInfo.getInstance().fullProductName
             val multiplePluginsDisabled = disabledPlugins.size != 1
 
             val names = if (multiplePluginsDisabled) StringUtil.join(disabledPlugins, ", ") else disabledPlugins[0]
             val ending = if (multiplePluginsDisabled) "s" else ""
             val verb = if (multiplePluginsDisabled) "were" else "was"
-            val restartInfo = if (ApplicationManager.getApplication().isRestartCapable) "$ideName will be restarted" else "Restart $ideName"
 
-            val message = "Conflicting plugin$ending $names $verb disabled. $restartInfo in order to apply changes"
-
-            Messages.showInfoMessage(message, restartInfo)
-            ApplicationManager.getApplication().restart()
+            restartIDE("Conflicting plugin$ending $names $verb disabled")
         } else {
             PropertiesComponent.getInstance().setValue(CONFLICTING_PLUGINS_DISABLED, "true")
             newCoursesNotifier.scheduleNotification()
@@ -53,8 +46,7 @@ class InitializationComponent : ApplicationComponent {
     private fun disablePlugins(): List<String> {
         val disabledPlugins = ArrayList<String>()
         for (id in IDS) {
-            val plugin = PluginManager.getPlugin(PluginId.getId(id))
-            plugin ?: continue
+            val plugin = PluginManager.getPlugin(PluginId.getId(id)) ?: continue
             if (plugin.isEnabled) {
                 disabledPlugins.add(plugin.name)
                 PluginManagerCore.disablePlugin(id)
