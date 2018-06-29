7b7c16007ffcc683b42e2f00d105ed6d680b5a9d
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2018-06-29 15:44:10 +0300

    generalize gradle config files creation

--- educational-core/src/com/jetbrains/edu/learning/intellij/GradleCourseBuilderBase.kt
+++ educational-core/src/com/jetbrains/edu/learning/intellij/GradleCourseBuilderBase.kt
@@ -15,8 +15,19 @@ import org.jetbrains.plugins.gradle.util.GradleConstants
 abstract class GradleCourseBuilderBase : EduCourseBuilder<JdkProjectSettings> {
 
   abstract val buildGradleTemplateName: String
+  open val settingGradleTemplateName: String = GradleConstants.SETTINGS_FILE_NAME
 
-  open val buildGradleVariables: Map<String, String> by lazy { mapOf("GRADLE_VERSION" to EduGradleUtils.gradleVersion()) }
+  /**
+   * Map from config file name which should be created in project to template file name
+   */
+  open val configMap: Map<String, String>
+    get() = mapOf(GradleConstants.DEFAULT_SCRIPT_NAME to buildGradleTemplateName,
+                  GradleConstants.SETTINGS_FILE_NAME to settingGradleTemplateName)
+
+  open fun getConfigVariables(project: Project): Map<String, String> {
+    return mapOf("GRADLE_VERSION" to EduGradleUtils.gradleVersion(),
+                 "PROJECT_NAME" to project.name)
+  }
 
   override fun refreshProject(project: Project) {
     ExternalSystemUtil.refreshProjects(project, GradleConstants.SYSTEM_ID, true, ProgressExecutionMode.MODAL_SYNC)
