26e858c27ba435953386d56860f929dc0ce849fd
Valentina Kiryushkina <valentina.kiryushkina@jetbrains.com>
2018-07-03 20:03:43 +0300

    EDU-1549 Update position of other tasks when pushing or updating only one task
    We have to update positions of other tasks if task was added in the middle of its
    position was changed.
    Unobvious points:
    
    * To calculate the right position of task to post we have to find it
    position among pushed tasks only.
    
    * We have to update underlying tasks if task was pushed in the middle. But
    we need to filter out task that are already on Stepik. To set correct position
    we iterate over pushed task, as their position in the list of pushed tasks differs
    from index
    
    * If task was moved we have to update positions for tasks that are between
    current and previous positions of moved tasks. New positions are calculated
    in the same manner as in previous case
    
    (cherry picked from commit fa12f78)

--- educational-core/src/com/jetbrains/edu/coursecreator/actions/stepik/CCPushTask.java
+++ educational-core/src/com/jetbrains/edu/coursecreator/actions/stepik/CCPushTask.java
@@ -18,8 +18,14 @@ import com.jetbrains.edu.learning.courseFormat.Lesson;
 import com.jetbrains.edu.learning.courseFormat.RemoteCourse;
 import com.jetbrains.edu.learning.courseFormat.StepikChangeStatus;
 import com.jetbrains.edu.learning.courseFormat.tasks.Task;
+import com.jetbrains.edu.learning.stepik.StepikConnector;
 import com.jetbrains.edu.learning.stepik.StepikNames;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.jetbrains.edu.coursecreator.stepik.CCStepikConnector.*;
 
 public class CCPushTask extends DumbAwareAction {
   public CCPushTask() {
@@ -96,32 +102,118 @@ public class CCPushTask extends DumbAwareAction {
       @Override
       public void run(@NotNull ProgressIndicator indicator) {
         indicator.setText("Uploading task to " + StepikNames.STEPIK_URL);
-        if (task.getStepId() <= 0) {
-          boolean isPosted = CCStepikConnector.postTask(project, task, lesson.getId());
-          if (isPosted) {
-            CCStepikConnector.showNotification(project, "Task " + task.getName() + " uploaded",
-                                               CCStepikConnector
-                                                 .openOnStepikAction("/lesson/" + task.getLesson().getId() + "/step/" + task.getIndex())
-            );
-          }
-          else {
-            CCStepikConnector.showErrorNotification(project, "Error uploading task", "Task " + task.getName() + "wasn't uploaded");
-          }
+        boolean toPost = task.getStepId() <= 0;
+        if (toPost) {
+          postNewTask(project, task, lesson);
         }
         else {
-          boolean isPosted = CCStepikConnector.updateTask(project, task);
-          task.setStepikChangeStatus(StepikChangeStatus.UP_TO_DATE);
-          if (isPosted) {
-            int lessonId = task.getLesson().getId();
-            CCStepikConnector.showNotification(project, "Task " + task.getName() + " updated",
-                                               CCStepikConnector.openOnStepikAction("/lesson/" + lessonId + "/step/" + task.getIndex())
-            );
-          }
-          else {
-            CCStepikConnector.showErrorNotification(project, "Error updating task", "Task " + task.getName() + "wasn't updated");
-          }
+          updateTask(task, lesson, project);
         }
       }
     });
+  }
+
+  private static void postNewTask(Project project, Task task, Lesson lesson) {
+    int position = stepikPosition(task, lesson);
+    boolean isPosted = postTask(project, task, lesson, position);
+    if (!isPosted) {
+      showErrorNotification(project, "Error uploading task", "Task " + task.getName() + "wasn't uploaded");
+      return;
+    }
+
+
+    // if task was added in the middle we have to update positions of underlying pushed tasks
+    boolean isLast = task.getIndex() == lesson.taskList.size();
+    if (!isLast) {
+      List<Task> postedTasks = getPostedTasks(lesson);
+      List<Task> underlyingTasks = postedTasks.subList(position, postedTasks.size());
+      updateTasksPositions(project, position + 1, underlyingTasks);
+    }
+
+    task.setStepikChangeStatus(StepikChangeStatus.UP_TO_DATE);
+    showNotification(project, "Task " + task.getName() + " uploaded",
+                     openOnStepikAction("/lesson/" + lesson.getId() + "/step/" + task.getIndex()));
+  }
+
+
+  private static List<Task> getPostedTasks(Lesson lesson) {
+    return lesson.taskList.stream().filter(t -> t.getId() > 0).collect(Collectors.toList());
+  }
+
+  private static void updateTask(Task task, Lesson lesson, Project project) {
+    int position = stepikPosition(task, lesson);
+    int positionOnServer = StepikConnector.getTaskPosition(task.getId());
+
+    boolean isPosted = updateTask(task, project, position);
+    if (!isPosted) {
+      showErrorNotification(project, "Error updating task", "Task " + task.getName() + "wasn't updated");
+      return;
+    }
+
+
+    // if task position was changed we had to update affected tasks.
+    // Position was changed for tasks that are between current and previous task position
+    if (position != positionOnServer) {
+      List<Task> postedTasks = getPostedTasks(lesson);
+      boolean movedUp = position - positionOnServer < 0;
+      if (movedUp) {
+        List<Task> underlyingTasks = postedTasks.subList(position, positionOnServer);
+        updateTasksPositions(project, position + 1, underlyingTasks);
+      }
+      else {
+        List<Task> higherTasks = postedTasks.subList(positionOnServer - 1, position - 1);
+        updateTasksPositions(project, position - 1, higherTasks);
+      }
+    }
+
+    task.setStepikChangeStatus(StepikChangeStatus.UP_TO_DATE);
+    showNotification(project, "Task " + task.getName() + " updated",
+                     openOnStepikAction("/lesson/" + lesson.getId() + "/step/" + task.getIndex()));
+  }
+
+  private static boolean postTask(Project project, Task task, Lesson lesson, int position) {
+    Task taskCopy = task.copy();
+    taskCopy.setIndex(position);
+    taskCopy.setLesson(lesson);
+    boolean isPosted = CCStepikConnector.postTask(project, taskCopy, lesson.getId());
+    if (isPosted) {
+      task.setStepId(taskCopy.getStepId());
+      return true;
+    }
+
+    return false;
+  }
+
+  private static boolean updateTask(Task task, Project project, int position) {
+    Task taskCopy = task.copy();
+    taskCopy.setIndex(position);
+    taskCopy.setLesson(task.getLesson());
+    return CCStepikConnector.updateTask(project, taskCopy);
+  }
+
+  private static void updateTasksPositions(@NotNull Project project, int initialPosition, List<Task> tasksToUpdate) {
+    int position = initialPosition;
+    for (Task task : tasksToUpdate) {
+      if (task.getId() == 0) continue;
+      updateTask(task, project, position++);
+    }
+  }
+
+  /**
+   * Calculates estimated task position on Stepik by counting pushed tasks only.
+   */
+  private static int stepikPosition(@NotNull Task taskToUpdate, @NotNull Lesson lesson) {
+    int position = 1;
+    for (Task task : lesson.taskList) {
+      if (task.getName().equals(taskToUpdate.getName())) {
+        break;
+      }
+
+      if (task.getId() > 0) {
+        position++;
+      }
+    }
+
+    return position;
   }
 }
