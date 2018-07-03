010bf93678ec6f9e8e5bcc388429d899b2bd6038
Liana.Bakradze <liana.bakradze@jetbrains.com>
2018-07-03 14:36:37 +0300

    EDU-1554 show config files in course view

--- educational-core/src/com/jetbrains/edu/coursecreator/configuration/YamlFormatSynchronizer.kt
+++ educational-core/src/com/jetbrains/edu/coursecreator/configuration/YamlFormatSynchronizer.kt
@@ -3,6 +3,11 @@ package com.jetbrains.edu.coursecreator.configuration
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+import com.jetbrains.edu.coursecreator.configuration.YamlFormatSettings.COURSE_CONFIG
+import com.jetbrains.edu.coursecreator.configuration.YamlFormatSettings.LESSON_CONFIG
+import com.jetbrains.edu.coursecreator.configuration.YamlFormatSettings.SECTION_CONFIG
+import com.jetbrains.edu.coursecreator.configuration.YamlFormatSettings.TASK_CONFIG
 import com.jetbrains.edu.learning.StudyTaskManager
 import com.jetbrains.edu.learning.courseFormat.*
 import com.jetbrains.edu.learning.courseFormat.tasks.Task
@@ -20,10 +25,10 @@ object YamlFormatSynchronizer {
       return
     }
     val fileName = when (item) {
-      is Course -> YamlFormatSettings.COURSE_CONFIG
-      is Section -> YamlFormatSettings.SECTION_CONFIG
-      is Lesson -> YamlFormatSettings.LESSON_CONFIG
-      is Task -> YamlFormatSettings.TASK_CONFIG
+      is Course -> COURSE_CONFIG
+      is Section -> SECTION_CONFIG
+      is Lesson -> LESSON_CONFIG
+      is Task -> TASK_CONFIG
       else -> error("Unknown StudyItem type: ${item.javaClass.name}")
     }
     val dir = item.getDir(project)
@@ -53,5 +58,11 @@ object YamlFormatSynchronizer {
       }
       true
     })
+  }
+
+  @JvmStatic
+  fun isConfigFile(file: VirtualFile): Boolean {
+    val name = file.name
+    return COURSE_CONFIG == name || LESSON_CONFIG == name || TASK_CONFIG == name || SECTION_CONFIG == name
   }
 }
