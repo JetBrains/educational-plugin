0ca2191107b92ccc7512469cb53c1ce0a3ec8bf1
Liana Bakradze <liana.bakradze@jetbrains.com>
2018-07-04 16:30:33 +0300

    EDU-1554 register MixIns and serialize course to config files

--- educational-core/src/com/jetbrains/edu/coursecreator/configuration/YamlFormatSynchronizer.kt
+++ educational-core/src/com/jetbrains/edu/coursecreator/configuration/YamlFormatSynchronizer.kt
@@ -1,20 +1,60 @@
 package com.jetbrains.edu.coursecreator.configuration
 
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.PropertyNamingStrategy
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
+import com.fasterxml.jackson.dataformat.yaml.YAMLGenerator
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.editor.Document
+import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.jetbrains.edu.coursecreator.configuration.YamlFormatSettings.COURSE_CONFIG
 import com.jetbrains.edu.coursecreator.configuration.YamlFormatSettings.LESSON_CONFIG
 import com.jetbrains.edu.coursecreator.configuration.YamlFormatSettings.SECTION_CONFIG
 import com.jetbrains.edu.coursecreator.configuration.YamlFormatSettings.TASK_CONFIG
+import com.jetbrains.edu.coursecreator.configuration.mixins.*
 import com.jetbrains.edu.learning.StudyTaskManager
 import com.jetbrains.edu.learning.courseFormat.*
+import com.jetbrains.edu.learning.courseFormat.tasks.EduTask
 import com.jetbrains.edu.learning.courseFormat.tasks.Task
 
 
 object YamlFormatSynchronizer {
   private val LOG = Logger.getInstance(YamlFormatSynchronizer.javaClass)
+
+  private val MAPPER: ObjectMapper by lazy {
+    val yamlFactory = YAMLFactory()
+    yamlFactory.disable(YAMLGenerator.Feature.WRITE_DOC_START_MARKER)
+    yamlFactory.enable(YAMLGenerator.Feature.MINIMIZE_QUOTES)
+    yamlFactory.enable(YAMLGenerator.Feature.LITERAL_BLOCK_STYLE)
+
+    val mapper = ObjectMapper(yamlFactory)
+    mapper.registerKotlinModule()
+    mapper.propertyNamingStrategy = PropertyNamingStrategy.SNAKE_CASE
+    mapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY)
+    mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+    addMixIns(mapper)
+
+    mapper
+  }
+
+  private fun addMixIns(mapper: ObjectMapper) {
+    mapper.addMixIn(Course::class.java, CourseYamlMixin::class.java)
+    mapper.addMixIn(Section::class.java, SectionYamlMixin::class.java)
+    mapper.addMixIn(Lesson::class.java, LessonYamlMixin::class.java)
+    mapper.addMixIn(Task::class.java, TaskYamlMixin::class.java)
+    mapper.addMixIn(EduTask::class.java, EduTaskYamlMixin::class.java)
+    mapper.addMixIn(TaskFile::class.java, TaskFileYamlMixin::class.java)
+    mapper.addMixIn(AnswerPlaceholder::class.java, AnswerPlaceholderYamlMixin::class.java)
+    mapper.addMixIn(AnswerPlaceholderDependency::class.java, AnswerPlaceholderDependencyYamlMixin::class.java)
+  }
+
+  private fun VirtualFile.getDocument(): Document? = FileDocumentManager.getInstance().getDocument(this)
 
   @JvmStatic
   fun saveItem(item: StudyItem, project: Project) {
@@ -37,7 +77,9 @@ object YamlFormatSynchronizer {
       return
     }
     ApplicationManager.getApplication().runWriteAction {
-      dir.findOrCreateChildData(YamlFormatSynchronizer.javaClass, fileName)
+      val file = dir.findOrCreateChildData(YamlFormatSynchronizer.javaClass, fileName)
+      val document = file.getDocument() ?: return@runWriteAction
+      document.setText(MAPPER.writeValueAsString(item))
     }
   }
 
@@ -56,6 +98,7 @@ object YamlFormatSynchronizer {
       for (task in lesson.getTaskList()) {
         saveItem(task, project)
       }
+      saveItem(lesson, project)
       true
     })
   }
