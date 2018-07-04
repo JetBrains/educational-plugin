8553a56971812c2040d865a1754e4cd644b4c7fa
Liana Bakradze <liana.bakradze@jetbrains.com>
2018-07-04 16:29:25 +0300

    EDU-1554 mixins for edu tasks

---
+++ educational-core/src/com/jetbrains/edu/coursecreator/configuration/mixins/AnswerPlaceholderDependencyYamlUtil.kt
@@ -0,0 +1,55 @@
+package com.jetbrains.edu.coursecreator.configuration.mixins
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonPropertyOrder
+import com.fasterxml.jackson.databind.annotation.JsonSerialize
+import com.fasterxml.jackson.databind.util.StdConverter
+
+@Suppress("UNUSED_PARAMETER", "unused") // used for yaml serialization
+@JsonAutoDetect(getterVisibility = JsonAutoDetect.Visibility.NONE,
+                isGetterVisibility = JsonAutoDetect.Visibility.NONE,
+                fieldVisibility = JsonAutoDetect.Visibility.NONE)
+@JsonPropertyOrder("section", "lesson", "task", "file", "placeholder", "is_visible")
+abstract class AnswerPlaceholderDependencyYamlMixin {
+  @JsonProperty("section")
+  private var mySectionName: String? = null
+
+  @JsonProperty("lesson")
+  private lateinit var myLessonName: String
+
+  @JsonProperty("task")
+  private lateinit var myTaskName: String
+
+  @JsonProperty("file")
+  private lateinit var myFileName: String
+
+  @JsonProperty("placeholder")
+  @JsonSerialize(converter = PlaceholderIndexConverter::class)
+  private var myPlaceholderIndex: Int = -1
+
+  @JsonProperty("is_visible")
+  @JsonInclude(JsonInclude.Include.CUSTOM, valueFilter = DefaultTrueFilter::class)
+  private var myIsVisible = true
+
+}
+
+/**
+ * Filters default true values for boolean properties
+ *
+ * See {@link com.fasterxml.jackson.annotation.JsonInclude.Include.CUSTOM} for information why it works
+ */
+@Suppress("EqualsOrHashCode")
+class DefaultTrueFilter {
+  override fun equals(other: Any?): Boolean {
+    if (other is Boolean && false == other) {
+      return false;
+    }
+    return true
+  }
+}
+
+class PlaceholderIndexConverter: StdConverter<Int, Int>() {
+  override fun convert(index: Int) = index + 1
+}
