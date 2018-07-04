4c68a7538a4a4163824d306b27b6ef264407f2d5
Liana Bakradze <liana.bakradze@jetbrains.com>
2018-07-04 16:29:25 +0300

    EDU-1554 mixins for edu tasks

---
+++ educational-core/src/com/jetbrains/edu/coursecreator/configuration/mixins/TaskFileYamlUtil.kt
@@ -0,0 +1,17 @@
+package com.jetbrains.edu.coursecreator.configuration.mixins
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.jetbrains.edu.learning.courseFormat.AnswerPlaceholder
+
+@Suppress("UNUSED_PARAMETER") // used for yaml serialization
+@JsonAutoDetect(getterVisibility = JsonAutoDetect.Visibility.NONE,
+                isGetterVisibility = JsonAutoDetect.Visibility.NONE,
+                fieldVisibility = JsonAutoDetect.Visibility.NONE)
+abstract class TaskFileYamlMixin {
+  @JsonProperty("name")
+  private lateinit var name: String
+
+  @JsonProperty("placeholders")
+  private lateinit var myAnswerPlaceholders: List<AnswerPlaceholder>
+}
