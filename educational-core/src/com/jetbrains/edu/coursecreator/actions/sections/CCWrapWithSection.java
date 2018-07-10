f05f00d65901a6e318146208e977c3adc95dc2c3
Liana.Bakradze <liana.bakradze@jetbrains.com>
2018-07-10 16:56:27 +0300

    EDU-1554 handle section changing events correctly

--- educational-core/src/com/jetbrains/edu/coursecreator/actions/sections/CCWrapWithSection.java
+++ educational-core/src/com/jetbrains/edu/coursecreator/actions/sections/CCWrapWithSection.java
@@ -60,6 +60,7 @@ public class CCWrapWithSection extends DumbAwareAction {
     Section section = CCUtils.wrapIntoSection(project, course, lessonsToWrap, sectionName);
     if (section == null) return;
     YamlFormatSynchronizer.saveItem(section);
+    YamlFormatSynchronizer.saveItem(course);
     ProjectView.getInstance(project).refresh();
     StepikCourseChangeHandler.contentChanged(course);
   }
