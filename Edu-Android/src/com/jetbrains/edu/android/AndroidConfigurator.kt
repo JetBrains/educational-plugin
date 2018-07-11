a55e4581a5e26673fc4f1208cf517083a7952c72
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2018-07-11 17:25:51 +0300

    EDU-1096 & EDU-1097: create experimental feature for support of android courses

--- Edu-Android/src/com/jetbrains/edu/android/AndroidConfigurator.kt
+++ Edu-Android/src/com/jetbrains/edu/android/AndroidConfigurator.kt
@@ -1,8 +1,11 @@
 package com.jetbrains.edu.android
 
+import com.intellij.openapi.application.Experiments
+import com.jetbrains.edu.learning.EduExperimentalFeatures
 import com.jetbrains.edu.learning.checker.TaskCheckerProvider
 import com.jetbrains.edu.learning.intellij.GradleConfiguratorBase
 import com.jetbrains.edu.learning.intellij.GradleCourseBuilderBase
+import com.jetbrains.edu.learning.isUnitTestMode
 
 class AndroidConfigurator : GradleConfiguratorBase() {
 
@@ -18,4 +21,6 @@ class AndroidConfigurator : GradleConfiguratorBase() {
   override fun getTestFileName(): String = "ExampleUnitTest.kt"
 
   override fun getTaskCheckerProvider(): TaskCheckerProvider = taskCheckerProvider
+
+  override fun isEnabled(): Boolean = Experiments.isFeatureEnabled(EduExperimentalFeatures.ANDROID_COURSES) || isUnitTestMode
 }
