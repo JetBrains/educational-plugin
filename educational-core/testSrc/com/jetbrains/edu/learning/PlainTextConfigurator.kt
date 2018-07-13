0b54c67f364b3065b779c7649ea803df5826f1dc
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2018-07-13 18:23:16 +0300

    EDU-1521: improve file listener logic
    Currently, when new virtual file listener is added to course
    we check if it in task folder and add it as task file.
    But for almost all languages it's incorrect behaviour
    because task files must be only in source folder (for example, `src` for kotlin)
    but current implementation puts any file into task as task file
    even it's located in task root and should be "additional"
    
    These changes support all file types: task, test and additional

--- educational-core/testSrc/com/jetbrains/edu/learning/PlainTextConfigurator.kt
+++ educational-core/testSrc/com/jetbrains/edu/learning/PlainTextConfigurator.kt
@@ -4,6 +4,7 @@ import com.intellij.lang.LanguageExtensionPoint
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.extensions.ExtensionPointName
 import com.intellij.openapi.fileTypes.PlainTextLanguage
+import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.testFramework.PlatformTestUtil
 import com.jetbrains.edu.learning.checker.CheckResult
 import com.jetbrains.edu.learning.checker.TaskChecker
@@ -16,7 +17,8 @@ import com.jetbrains.edu.learning.newproject.CourseProjectGenerator
 class PlainTextConfigurator : EduConfigurator<Unit> {
 
   override fun getCourseBuilder() = PlainTextCourseBuilder()
-  override fun getTestFileName() = "test.txt"
+  override fun getTestFileName() = "Tests.txt"
+  override fun isTestFile(file: VirtualFile): Boolean = file.name == testFileName
 
   override fun getTaskCheckerProvider() = TaskCheckerProvider { task, project ->
     object : TaskChecker<EduTask>(task, project) {
