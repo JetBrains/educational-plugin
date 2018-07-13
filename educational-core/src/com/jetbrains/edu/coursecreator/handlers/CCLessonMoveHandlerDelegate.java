86016a28ada8f387aa09458aeb4859aeafe839ba
Liana Bakradze <liana.bakradze@jetbrains.com>
2018-07-13 16:18:38 +0300

    EDU-1554 update yaml files on move/rename

--- educational-core/src/com/jetbrains/edu/coursecreator/handlers/CCLessonMoveHandlerDelegate.java
+++ educational-core/src/com/jetbrains/edu/coursecreator/handlers/CCLessonMoveHandlerDelegate.java
@@ -18,6 +18,7 @@ import com.intellij.psi.PsiReference;
 import com.intellij.refactoring.move.MoveCallback;
 import com.intellij.refactoring.move.MoveHandlerDelegate;
 import com.jetbrains.edu.coursecreator.CCUtils;
+import com.jetbrains.edu.coursecreator.configuration.YamlFormatSynchronizer;
 import com.jetbrains.edu.coursecreator.stepik.StepikCourseChangeHandler;
 import com.jetbrains.edu.coursecreator.ui.CCMoveStudyItemDialog;
 import com.jetbrains.edu.learning.EduNames;
@@ -145,6 +146,8 @@ public class CCLessonMoveHandlerDelegate extends MoveHandlerDelegate {
       }
     });
     ProjectView.getInstance(project).refresh();
+    YamlFormatSynchronizer.saveItem(targetContainer);
+    YamlFormatSynchronizer.saveItem(sourceContainer);
   }
 
   protected int getDelta(@NotNull Project project, @NotNull StudyItem targetItem) {
