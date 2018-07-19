e1a5defa19868bdc08d690ca537c7e5e69d1b477
Liana.Bakradze <liana.bakradze@jetbrains.com>
2018-07-19 13:38:22 +0300

    EDU-1550 Action for updating additional materials

---
+++ educational-core/src/com/jetbrains/edu/coursecreator/actions/stepik/UpdateAdditionalMaterials.kt
@@ -0,0 +1,38 @@
+package com.jetbrains.edu.coursecreator.actions.stepik
+
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.progress.ProgressIndicator
+import com.intellij.openapi.progress.ProgressManager
+import com.intellij.openapi.progress.Task
+import com.intellij.openapi.project.DumbAwareAction
+import com.jetbrains.edu.coursecreator.stepik.CCStepikConnector
+import com.jetbrains.edu.learning.StudyTaskManager
+import com.jetbrains.edu.learning.courseFormat.RemoteCourse
+
+
+class UpdateAdditionalMaterials: DumbAwareAction("Update Additional Materials") {
+  override fun actionPerformed(e: AnActionEvent?) {
+    val project = e?.project ?: return
+    val course = StudyTaskManager.getInstance(project).course ?: return
+    if (course !is RemoteCourse) {
+      return
+    }
+    ProgressManager.getInstance().run(object : Task.Modal(project, "Updating Additional Materials", false) {
+      override fun run(indicator: ProgressIndicator) {
+        indicator.isIndeterminate = false
+        CCStepikConnector.updateAdditionalMaterials(project, course.id)
+      }
+    })
+  }
+
+  override fun update(e: AnActionEvent?) {
+    val presentation = e?.presentation ?: return
+    presentation.isEnabledAndVisible = false
+    val project = e.project ?: return
+    val course = StudyTaskManager.getInstance(project).course ?: return
+    if (course !is RemoteCourse || course.isStudy) {
+      return
+    }
+    presentation.isEnabledAndVisible = true
+  }
+}
