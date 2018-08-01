8af4f6f2e60d26d7ce3c51f5b341a6c355b62ac3
Liana.Bakradze <liana.bakradze@jetbrains.com>
2018-08-01 16:06:36 +0300

    EDU-1588 lesson and section yaml deserialization

--- educational-core/src/com/jetbrains/edu/coursecreator/configuration/mixins/SectionYamlUtil.kt
+++ educational-core/src/com/jetbrains/edu/coursecreator/configuration/mixins/SectionYamlUtil.kt
@@ -1,18 +1,40 @@
-@file:JvmName("LessonYamlUtil")
+@file:JvmName("SectionYamlUtil")
 
 package com.jetbrains.edu.coursecreator.configuration.mixins
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder
 import com.fasterxml.jackson.databind.annotation.JsonSerialize
+import com.jetbrains.edu.coursecreator.configuration.InvalidYamlFormatException
+import com.jetbrains.edu.learning.courseFormat.Section
 import com.jetbrains.edu.learning.courseFormat.StudyItem
+private const val CONTENT = "content"
 
 @Suppress("UNUSED_PARAMETER") // used for yaml serialization
 @JsonAutoDetect(getterVisibility = JsonAutoDetect.Visibility.NONE,
                 isGetterVisibility = JsonAutoDetect.Visibility.NONE,
                 fieldVisibility = JsonAutoDetect.Visibility.NONE)
+@JsonDeserialize(builder = SectionBuilder::class)
 abstract class SectionYamlMixin {
-  @JsonProperty("content")
+  @JsonProperty(CONTENT)
   @JsonSerialize(contentConverter = StudyItemConverter::class)
   private lateinit var items: List<StudyItem>
+}
+
+@JsonPOJOBuilder(withPrefix = "")
+private class SectionBuilder(@JsonProperty(CONTENT) val content: List<String?>) {
+  @Suppress("unused") //used for deserialization
+  private fun build(): Section {
+    val section = Section()
+    val items = content.map {
+      if (it == null) {
+        throw InvalidYamlFormatException("Unnamed item")
+      }
+      TitledStudyItem(it)
+    }
+    section.items = items
+    return section
+  }
 }
