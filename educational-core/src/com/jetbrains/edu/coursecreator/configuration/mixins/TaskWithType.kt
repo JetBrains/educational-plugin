54f96179ed7398b73c9374b77ec5dea17f053d30
Liana.Bakradze <liana.bakradze@jetbrains.com>
2018-08-01 16:06:36 +0300

    EDU-1588 lesson and section yaml deserialization

---
+++ educational-core/src/com/jetbrains/edu/coursecreator/configuration/mixins/TaskWithType.kt
@@ -0,0 +1,12 @@
+package com.jetbrains.edu.coursecreator.configuration.mixins
+
+import com.jetbrains.edu.learning.courseFormat.tasks.Task
+
+/**
+ * Placeholder to deserialize task type, should be replaced with actual task later
+ */
+class TaskWithType(title: String): Task(title) {
+  override fun getTaskType(): String {
+    throw NotImplementedError()
+  }
+}
