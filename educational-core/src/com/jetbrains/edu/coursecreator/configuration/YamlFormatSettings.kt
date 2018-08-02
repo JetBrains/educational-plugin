86e75fd9c17e448032bc9ed271eca81ed7c2b27a
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2018-08-02 12:15:22 +0300

    Move all experimental feature ids into one place

--- educational-core/src/com/jetbrains/edu/coursecreator/configuration/YamlFormatSettings.kt
+++ educational-core/src/com/jetbrains/edu/coursecreator/configuration/YamlFormatSettings.kt
@@ -1,15 +1,13 @@
 package com.jetbrains.edu.coursecreator.configuration
 
 import com.intellij.openapi.application.Experiments
-
+import com.jetbrains.edu.learning.EduExperimentalFeatures
 
 object YamlFormatSettings {
-  private const val FEATURE_ID = "edu.course.creator.yaml"
-
   const val COURSE_CONFIG = "course-info.yaml"
   const val SECTION_CONFIG = "section-info.yaml"
   const val LESSON_CONFIG = "lesson-info.yaml"
   const val TASK_CONFIG = "task-info.yaml"
 
-  fun isDisabled() = !Experiments.isFeatureEnabled(FEATURE_ID);
+  fun isDisabled() = !Experiments.isFeatureEnabled(EduExperimentalFeatures.YAML_FORMAT)
 }
