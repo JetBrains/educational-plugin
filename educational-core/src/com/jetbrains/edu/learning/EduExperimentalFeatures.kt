ee0472434a007a24597b376f62a0eba570a3cfdb
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2018-08-02 12:15:22 +0300

    Move all experimental feature ids into one place

--- educational-core/src/com/jetbrains/edu/learning/EduExperimentalFeatures.kt
+++ educational-core/src/com/jetbrains/edu/learning/EduExperimentalFeatures.kt
@@ -1,5 +1,6 @@
 package com.jetbrains.edu.learning
 
 object EduExperimentalFeatures {
+  const val YAML_FORMAT = "edu.course.creator.yaml"
   const val ANDROID_COURSES = "com.jetbrains.edu.android"
 }
