38e632eb53f0cca695b443eb1ef6d6c00cc259ef
phoenix-asv <40440882+phoenix-asv@users.noreply.github.com>
2018-08-03 23:11:55 +0300

    serialization annotations: task files

---
+++ edu-server-integration/src/com/jetbrains/edu/jbserver/TaskFileMixin.kt
@@ -0,0 +1,32 @@
+package com.jetbrains.edu.jbserver
+
+import com.fasterxml.jackson.annotation.*
+import com.jetbrains.edu.learning.courseFormat.TaskFile
+import com.jetbrains.edu.learning.courseFormat.AnswerPlaceholder
+import com.jetbrains.edu.learning.courseFormat.AnswerPlaceholderDependency
+
+
+abstract class TaskFileMixin {
+  @JsonProperty("name") lateinit var name : String
+  @JsonProperty("text") lateinit var text : String
+  @JsonProperty("placeholders") lateinit var myAnswerPlaceholders : List<AnswerPlaceholder>
+}
+
+
+abstract class AnswerPlaceholderMixin {
+  @JsonProperty("offset") var myOffset : Int = -1
+  @JsonProperty("length") var myLength : Int = -1
+  @JsonProperty("dependency") lateinit var myPlaceholderDependency : AnswerPlaceholderDependency
+  @JsonProperty("hints") lateinit var myHints : List<String>
+  @JsonProperty("possible_answer") lateinit var myPossibleAnswer : String
+  @JsonProperty("placeholder_text") lateinit var myPlaceholderText : String
+}
+
+
+abstract class AnswerPlaceholderDependencyMixin {
+  @JsonProperty("section") lateinit var mySectionName : String
+  @JsonProperty("lesson") lateinit var myLessonName : String
+  @JsonProperty("task") lateinit var myTaskName : String
+  @JsonProperty("file") lateinit var myFileName : String
+  @JsonProperty("placeholder") var myPlaceholderIndex : Int = -1
+}
