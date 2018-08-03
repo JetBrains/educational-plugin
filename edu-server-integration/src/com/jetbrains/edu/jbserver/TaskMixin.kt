b935bdf9046178a2f0c65ed6c71778345c424d49
phoenix-asv <40440882+phoenix-asv@users.noreply.github.com>
2018-08-03 23:09:30 +0300

    serialization annotations: tasks

---
+++ edu-server-integration/src/com/jetbrains/edu/jbserver/TaskMixin.kt
@@ -0,0 +1,74 @@
+package com.jetbrains.edu.jbserver
+
+import com.fasterxml.jackson.annotation.*
+import com.jetbrains.edu.learning.courseFormat.tasks.*
+import com.jetbrains.edu.learning.courseFormat.TaskFile
+import com.jetbrains.edu.learning.courseFormat.DescriptionFormat
+
+
+@JsonTypeInfo(
+  use = JsonTypeInfo.Id.NAME,
+  include = JsonTypeInfo.As.PROPERTY,
+  property = "type"
+)
+@JsonSubTypes(*arrayOf(
+  JsonSubTypes.Type(value = TheoryTask::class, name = "theory"),
+  JsonSubTypes.Type(value = IdeTask::class, name = "ide"),
+  JsonSubTypes.Type(value = OutputTask::class, name = "output"),
+  JsonSubTypes.Type(value = EduTask::class, name = "edu"),
+  JsonSubTypes.Type(value = CodeTask::class, name = "code"),
+  JsonSubTypes.Type(value = ChoiceTask::class, name = "choice")
+))
+@JsonIgnoreProperties(value = *arrayOf(
+  "format", "last_modified"
+))
+abstract class TaskMixIn {
+
+  @JsonProperty("id")
+  var myStepId: Int = 0
+
+  @JsonProperty("description")
+  lateinit var descriptionText: String
+
+  @JsonProperty("description_format")
+  lateinit var descriptionFormat: DescriptionFormat
+
+  @JsonProperty("task_files")
+  lateinit var taskFiles: HashMap<String, TaskFile>
+
+  @JsonProperty("test_files")
+  lateinit var testsText: HashMap<String, TaskFile>
+
+}
+
+
+@JsonTypeName("theory") // M
+abstract class TheoryTaskMixIn
+
+
+@JsonTypeName("ide")
+abstract class IdeTaskMixIn
+
+
+@JsonTypeName("output") // M
+abstract class OutputTaskMixIn
+
+
+@JsonTypeName("edu") // M
+abstract class EduTaskMixIn
+
+
+@JsonTypeName("code")
+abstract class CodeTaskMixIn
+
+
+@JsonTypeName("choice")
+abstract class ChoiceTaskMixIn {
+
+  @JsonProperty("choice_variants")
+  lateinit var myChoiceVariants: String
+
+  @JsonProperty("is_multichoice")
+  var myIsMultipleChoice: Boolean = true
+
+}
