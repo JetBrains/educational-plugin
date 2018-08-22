f9c66e9b80831a7730ae1974c1904a608b98c8f2
phoenix-asv <40440882+phoenix-asv@users.noreply.github.com>
2018-08-22 17:11:39 +0300

    integration: cc create course

---
+++ educational-core/src/com/jetbrains/edu/jbserver/CCUploadCourse.kt
@@ -0,0 +1,26 @@
+package com.jetbrains.edu.jbserver
+
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.project.DumbAwareAction
+import com.jetbrains.edu.learning.StudyTaskManager
+import com.jetbrains.edu.learning.courseFormat.EduCourse
+
+
+class CCUploadCourse : DumbAwareAction("&Upload Course to EduServer", "Upload Course to EduServer", null) {
+
+  override fun actionPerformed(e: AnActionEvent) {
+    val project = e.getData(CommonDataKeys.PROJECT) ?: return
+    val course = StudyTaskManager.getInstance(project).course ?: return
+    val eduCourse = if (course is EduCourse) course else EduCourse(course)
+    if (eduCourse.courseId == 0) {
+      // First upload
+      ServerClient.createCourse(eduCourse)
+      StudyTaskManager.getInstance(project).course = eduCourse
+    } else {
+      // Update upload
+      TODO("Update not implemented")
+    }
+  }
+
+}
