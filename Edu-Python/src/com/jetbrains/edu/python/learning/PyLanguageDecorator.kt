7355aa8350b3ae623bed57b7b33b4ff17145e7c5
Nikita Voloshin <nik.volosh@yandex.ru>
2018-08-23 16:04:05 +0300

    EDU-1561: Implemented PyCharm support

--- Edu-Python/src/com/jetbrains/edu/python/learning/PyLanguageDecorator.kt
+++ Edu-Python/src/com/jetbrains/edu/python/learning/PyLanguageDecorator.kt
@@ -4,7 +4,7 @@ import com.jetbrains.edu.learning.EduLanguageDecorator
 import icons.PythonIcons
 import javax.swing.Icon
 
-class PyLanguageDecorator : EduLanguageDecorator {
+open class PyLanguageDecorator : EduLanguageDecorator {
   override fun getLanguageScriptUrl(): String = javaClass.classLoader.getResource("/code-mirror/python.js").toExternalForm()
   override fun getDefaultHighlightingMode(): String = "python"
   override fun getLogo(): Icon = PythonIcons.Python.Python_logo
---
+++ Edu-Python/src/com/jetbrains/edu/python/learning/checkio/pycharm/PyCheckiOCourseProjectGenerator.kt
@@ -0,0 +1,15 @@
+package com.jetbrains.edu.python.learning.checkio.pycharm
+
+import com.intellij.openapi.project.Project
+import com.jetbrains.edu.learning.courseFormat.Course
+import com.jetbrains.edu.python.learning.PyCourseBuilder
+import com.jetbrains.edu.python.learning.checkio.newProject.PyCheckiOCourseProjectGenerator
+import com.jetbrains.edu.python.learning.pycharm.PyCourseProjectGenerator
+import com.jetbrains.python.newProject.PyNewProjectSettings
+
+internal class PyCheckiOCourseProjectGenerator(builder: PyCourseBuilder, course: Course) : PyCheckiOCourseProjectGenerator(builder, course) {
+  private val myPyCourseProjectGenerator = PyCourseProjectGenerator(builder, course)
+
+  override fun afterProjectGenerated(project: Project, settings: PyNewProjectSettings) =
+    myPyCourseProjectGenerator.afterProjectGenerated(project, settings)
+}
