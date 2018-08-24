8c6ef0eb8d908298ff77dd4e15287d6984d270cb
Nikita Voloshin <nik.volosh@yandex.ru>
2018-08-24 12:08:58 +0300

    Implemented js.CheckiO API

--- Edu-JavaScript/src/com/jetbrains/edu/javascript/learning/checkio/utils/JsCheckiONames.java
+++ Edu-JavaScript/src/com/jetbrains/edu/javascript/learning/checkio/utils/JsCheckiONames.java
@@ -7,6 +7,7 @@ public final class JsCheckiONames {
 
   public static final String JS_CHECKIO = "Js " + CHECKIO;
 
+  public static final String JS_CHECKIO_API_HOST = "https://js.checkio.org";
   public static final String JS_CHECKIO_OAUTH_SERVICE_NAME = CHECKIO_OAUTH_SERVICE_NAME + "/js";
   public static final String JS_CHECKIO_OAUTH_SERVICE_PATH = CHECKIO_OAUTH_SERVICE_PATH + "/js";
 }
