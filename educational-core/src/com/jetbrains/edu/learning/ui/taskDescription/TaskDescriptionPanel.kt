734f94336c519a1b6e552467d67b51f2fc6be897
Liana Bakradze <liana.bakradze@jetbrains.com>
2018-08-27 22:37:45 +0300

    finally correct placement of refresh and comment icons

--- educational-core/src/com/jetbrains/edu/learning/ui/taskDescription/TaskDescriptionPanel.kt
+++ educational-core/src/com/jetbrains/edu/learning/ui/taskDescription/TaskDescriptionPanel.kt
@@ -3,16 +3,15 @@ package com.jetbrains.edu.learning.ui.taskDescription
 import com.intellij.icons.AllIcons
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.actionSystem.*
-import com.intellij.openapi.actionSystem.impl.ActionButton
 import com.intellij.openapi.editor.colors.EditorColorsManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.SimpleToolWindowPanel
 import com.intellij.openapi.wm.ToolWindowManager
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBScrollPane
+import com.intellij.ui.components.panels.HorizontalLayout
 import com.intellij.util.ui.AsyncProcessIcon
 import com.intellij.util.ui.JBUI
-import com.intellij.util.ui.UIUtil
 import com.jetbrains.edu.learning.actions.CheckAction
 import com.jetbrains.edu.learning.actions.RefreshTaskFileAction
 import java.awt.BorderLayout
@@ -99,23 +98,33 @@ class TaskDescriptionPanel : SimpleToolWindowPanel(true, true), DataProvider, Di
     }
     val refreshAction = ActionManager.getInstance().getAction(RefreshTaskFileAction.ACTION_ID)
     val toolbar1 = ActionManager.getInstance().createActionToolbar(ActionPlaces.UNKNOWN, DefaultActionGroup(
-       refreshAction, commentAction), true)
+       commentAction), true)
     toolbar1.layoutPolicy = ActionToolbar.NOWRAP_LAYOUT_POLICY
     toolbar1.adjustTheSameSize(true)
 //    val component1 = ActionButton(refreshAction, refreshAction.templatePresentation.clone(), ActionPlaces.UNKNOWN, ActionToolbar.DEFAULT_MINIMUM_BUTTON_SIZE)
     //TODO: compensate depending on OS
 //    component1.border = JBUI.Borders.empty(3, 0, 3, 0)
+
+    val toolbar2 = ActionManager.getInstance().createActionToolbar(ActionPlaces.UNKNOWN, DefaultActionGroup(
+      refreshAction), true)
+    toolbar2.layoutPolicy = ActionToolbar.NOWRAP_LAYOUT_POLICY
+    toolbar2.adjustTheSameSize(true)
+    val component2 = toolbar2.component
+    component2.border = JBUI.Borders.empty(0, 0, 0, 0)
+//    component2.insets.set(5, 0, -5, 0)
+//    component2.minimumSize = JBUI.size(20, 20)
+
     val component1 = toolbar1.component
-    component1.border = JBUI.Borders.empty(5, 0, 0, 0)
-    component1.insets.set(5, 0, -5, 0)
-    component1.minimumSize = JBUI.size(20, 20)
-    for (actionButton in UIUtil.findComponentsOfType(component1, ActionButton::class.java)) {
-      actionButton.insets.set(0, 0, 0, 0)
-      actionButton.border = JBUI.Borders.empty()
-    }
+    component1.border = JBUI.Borders.empty(0, 0, 0, 0)
+//    component1.insets.set(5, 0, -5, 0)
+//    component1.minimumSize = JBUI.size(20, 20)
+
+    val actionsPanel = JPanel(HorizontalLayout(0))
+    actionsPanel.add(component1)
+    actionsPanel.add(component2)
 
 
-    checkPanel.add(component1, BorderLayout.EAST)
+    checkPanel.add(actionsPanel, BorderLayout.EAST)
 
     checkPanel.border = JBUI.Borders.empty(16, 0, 0, 0)
 //    checkPanel.background = JBColor.RED
