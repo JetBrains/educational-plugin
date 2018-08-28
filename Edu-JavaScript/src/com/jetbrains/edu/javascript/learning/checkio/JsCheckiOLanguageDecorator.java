1b457924fff52a25257f3e33a406b9ebabaf056f
Nikita Voloshin <nik.volosh@yandex.ru>
2018-08-28 18:28:24 +0300

    Removed TODO

--- Edu-JavaScript/src/com/jetbrains/edu/javascript/learning/checkio/JsCheckiOLanguageDecorator.java
+++ Edu-JavaScript/src/com/jetbrains/edu/javascript/learning/checkio/JsCheckiOLanguageDecorator.java
@@ -22,6 +22,6 @@ public class JsCheckiOLanguageDecorator implements EduLanguageDecorator {
   @NotNull
   @Override
   public Icon getLogo() {
-    return EducationalCoreIcons.CheckiO; // TODO
+    return EducationalCoreIcons.CheckiO;
   }
 }
