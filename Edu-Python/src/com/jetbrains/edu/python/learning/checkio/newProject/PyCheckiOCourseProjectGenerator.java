c2b4a07063bdd3e2cc7cdc176549958bdf1cc65f
Nikita Voloshin <nik.volosh@yandex.ru>
2018-08-28 18:27:49 +0300

    EDU-1561: Refactored checkio courses generators. Common logic was extracted to `CheckiOCourseGenerationUtils`

--- Edu-Python/src/com/jetbrains/edu/python/learning/checkio/newProject/PyCheckiOCourseProjectGenerator.java
+++ Edu-Python/src/com/jetbrains/edu/python/learning/checkio/newProject/PyCheckiOCourseProjectGenerator.java
@@ -1,11 +1,10 @@
 package com.jetbrains.edu.python.learning.checkio.newProject;
 
-import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.jetbrains.edu.learning.checkio.CheckiOCourseContentGenerator;
 import com.jetbrains.edu.learning.checkio.courseFormat.CheckiOCourse;
-import com.jetbrains.edu.learning.checkio.courseFormat.CheckiOStation;
+import com.jetbrains.edu.learning.checkio.utils.CheckiOCourseGenerationUtils;
 import com.jetbrains.edu.learning.courseFormat.Course;
 import com.jetbrains.edu.python.learning.PyCourseBuilder;
 import com.jetbrains.edu.python.learning.checkio.connectors.PyCheckiOApiConnector;
@@ -13,10 +12,9 @@ import com.jetbrains.edu.python.learning.newproject.PyCourseProjectGenerator;
 import com.jetbrains.python.PythonFileType;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.List;
-
 public class PyCheckiOCourseProjectGenerator extends PyCourseProjectGenerator {
-  private static final Logger LOG = Logger.getInstance(PyCheckiOCourseProjectGenerator.class);
+  private final CheckiOCourseContentGenerator myContentGenerator =
+    new CheckiOCourseContentGenerator(PythonFileType.INSTANCE, PyCheckiOApiConnector.getInstance());
 
   public PyCheckiOCourseProjectGenerator(@NotNull PyCourseBuilder builder,
                                          @NotNull Course course) {
@@ -30,20 +28,6 @@ public class PyCheckiOCourseProjectGenerator extends PyCourseProjectGenerator {
 
   @Override
   protected boolean beforeProjectGenerated() {
-    try {
-      final CheckiOCourseContentGenerator contentGenerator =
-        new CheckiOCourseContentGenerator(PythonFileType.INSTANCE, PyCheckiOApiConnector.getInstance());
-
-      final List<CheckiOStation> stations = contentGenerator.getStationsFromServerUnderProgress();
-
-      stations.forEach(((CheckiOCourse) myCourse)::addStation);
-      return true;
-    }
-    catch (Exception e) {
-      // Notifications aren't able to be shown during course generating process,
-      // so we just log the error and return false
-      LOG.warn(e);
-      return false;
-    }
+    return CheckiOCourseGenerationUtils.generateCourseFromServerUnderProgress(myContentGenerator, (CheckiOCourse) myCourse);
   }
 }
