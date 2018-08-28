aa129e3336ad57fe21f55e3c20f2b63652a0cb49
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2018-08-28 20:40:41 +0300

    separate ext for itemContainer

--- educational-core/src/com/jetbrains/edu/learning/stepik/courseFormat/ext/StepikItemContainerExt.kt
+++ educational-core/src/com/jetbrains/edu/learning/stepik/courseFormat/ext/StepikItemContainerExt.kt
@@ -4,10 +4,6 @@ package com.jetbrains.edu.learning.stepik.courseFormat.ext
 
 import com.jetbrains.edu.learning.courseFormat.ItemContainer
 import com.jetbrains.edu.learning.courseFormat.Lesson
-import com.jetbrains.edu.learning.courseFormat.Section
-import com.jetbrains.edu.learning.courseFormat.StudyItem
-import com.jetbrains.edu.learning.courseFormat.tasks.Task
-import com.jetbrains.edu.learning.stepik.courseFormat.StepikCourse
 
 fun ItemContainer.getLesson(lessonId: Int): Lesson? {
   return items.filterIsInstance(Lesson::class.java).firstOrNull { item -> item.id == lessonId }
