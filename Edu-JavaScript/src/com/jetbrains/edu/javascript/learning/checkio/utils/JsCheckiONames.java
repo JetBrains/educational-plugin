e6b01e66a3881a96ebcefbd4ecf53a046bc4721f
Nikita Voloshin <nik.volosh@yandex.ru>
2018-08-28 18:28:14 +0300

    Refactored CheckiO string constants

--- Edu-JavaScript/src/com/jetbrains/edu/javascript/learning/checkio/utils/JsCheckiONames.java
+++ Edu-JavaScript/src/com/jetbrains/edu/javascript/learning/checkio/utils/JsCheckiONames.java
@@ -7,12 +7,14 @@ public final class JsCheckiONames {
 
   public static final String JS_CHECKIO = "Js " + CHECKIO;
 
-  public static final String JS_CHECKIO_API_HOST = "https://js.checkio.org";
+  public static final String JS_CHECKIO_URL = "js." + CHECKIO_URL;
+
+  public static final String JS_CHECKIO_API_HOST = "https://" + JS_CHECKIO_URL;
   public static final String JS_CHECKIO_OAUTH_SERVICE_NAME = CHECKIO_OAUTH_SERVICE_NAME + "/js";
   public static final String JS_CHECKIO_OAUTH_SERVICE_PATH = CHECKIO_OAUTH_SERVICE_PATH + "/js";
 
   public static final String JS_CHECKIO_LANGUAGE = "CheckiO-JavaScript";
   public static final String JS_CHECKIO_INTERPRETER = "js-node";
 
-  public static final String JS_CHECKIO_TEST_FORM_TARGET_URL = "https://js.checkio.org/mission/check-html-output/";
+  public static final String JS_CHECKIO_TEST_FORM_TARGET_URL = JS_CHECKIO_API_HOST + CHECKIO_TEST_FORM_TARGET_PATH + "/";
 }
