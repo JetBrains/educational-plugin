d60abfc2fe37076cf2a3b6f381187aed392c2bba
Nikita Voloshin <nik.volosh@yandex.ru>
2018-08-29 15:41:51 +0300

    Fixed bug with showing Js CheckiO course without experimental feature enable

--- Edu-JavaScript/src/com/jetbrains/edu/javascript/learning/checkio/JsCheckiOCourseProvider.java
+++ Edu-JavaScript/src/com/jetbrains/edu/javascript/learning/checkio/JsCheckiOCourseProvider.java
@@ -2,6 +2,7 @@ package com.jetbrains.edu.javascript.learning.checkio;
 
 import com.jetbrains.edu.javascript.learning.checkio.utils.JsCheckiONames;
 import com.jetbrains.edu.learning.CoursesProvider;
+import com.jetbrains.edu.learning.EduConfiguratorManager;
 import com.jetbrains.edu.learning.checkio.courseFormat.CheckiOCourse;
 import com.jetbrains.edu.learning.courseFormat.Course;
 import org.jetbrains.annotations.NotNull;
@@ -13,6 +14,10 @@ public class JsCheckiOCourseProvider implements CoursesProvider {
   @NotNull
   @Override
   public List<Course> loadCourses() {
-    return Collections.singletonList(new CheckiOCourse(JsCheckiONames.JS_CHECKIO, JsCheckiONames.JS_CHECKIO_LANGUAGE));
+    final Course course = new CheckiOCourse(JsCheckiONames.JS_CHECKIO, JsCheckiONames.JS_CHECKIO_LANGUAGE);
+    if (EduConfiguratorManager.forLanguage(course.getLanguageById()) != null) {
+      return Collections.singletonList(course);
+    }
+    return Collections.emptyList();
   }
 }
