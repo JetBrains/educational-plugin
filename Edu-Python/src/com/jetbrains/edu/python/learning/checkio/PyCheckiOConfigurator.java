db08bf4903dbfc8aa0f1e6eb64db2fc09a6aa9b2
Nikita Voloshin <nik.volosh@yandex.ru>
2018-08-29 17:45:10 +0300

    Removed redundant classes related to task checking

--- Edu-Python/src/com/jetbrains/edu/python/learning/checkio/PyCheckiOConfigurator.java
+++ Edu-Python/src/com/jetbrains/edu/python/learning/checkio/PyCheckiOConfigurator.java
@@ -1,12 +1,16 @@
 package com.jetbrains.edu.python.learning.checkio;
 
+import com.intellij.openapi.project.Project;
 import com.jetbrains.edu.learning.EduCourseBuilder;
+import com.jetbrains.edu.learning.checker.TaskChecker;
 import com.jetbrains.edu.learning.checker.TaskCheckerProvider;
 import com.jetbrains.edu.learning.checkio.CheckiOConnectorProvider;
+import com.jetbrains.edu.learning.checkio.checker.CheckiOTaskChecker;
 import com.jetbrains.edu.learning.checkio.connectors.CheckiOOAuthConnector;
+import com.jetbrains.edu.learning.courseFormat.tasks.EduTask;
 import com.jetbrains.edu.python.learning.PyConfigurator;
-import com.jetbrains.edu.python.learning.checkio.checker.PyCheckiOTaskCheckerProvider;
 import com.jetbrains.edu.python.learning.checkio.connectors.PyCheckiOOAuthConnector;
+import com.jetbrains.edu.python.learning.checkio.utils.PyCheckiONames;
 import com.jetbrains.python.newProject.PyNewProjectSettings;
 import icons.EducationalCoreIcons;
 import org.jetbrains.annotations.NotNull;
@@ -25,7 +29,19 @@ public class PyCheckiOConfigurator extends PyConfigurator implements CheckiOConn
   @NotNull
   @Override
   public TaskCheckerProvider getTaskCheckerProvider() {
-    return new PyCheckiOTaskCheckerProvider();
+    return new TaskCheckerProvider() {
+      @NotNull
+      @Override
+      public TaskChecker<EduTask> getEduTaskChecker(@NotNull EduTask task, @NotNull Project project) {
+        return new CheckiOTaskChecker(
+          task,
+          project,
+          PyCheckiOOAuthConnector.getInstance(),
+          PyCheckiONames.PY_CHECKIO_INTERPRETER,
+          PyCheckiONames.PY_CHECKIO_TEST_FORM_TARGET_URL
+        );
+      }
+    };
   }
 
   @NotNull
