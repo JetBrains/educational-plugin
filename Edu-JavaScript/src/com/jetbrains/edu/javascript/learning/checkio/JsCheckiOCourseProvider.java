8302a820f045a295e6ab78405fefa33c8032d4df
Nikita Voloshin <nik.volosh@yandex.ru>
2018-08-29 16:14:16 +0300

    Changed way to check if course is available

--- Edu-JavaScript/src/com/jetbrains/edu/javascript/learning/checkio/JsCheckiOCourseProvider.java
+++ Edu-JavaScript/src/com/jetbrains/edu/javascript/learning/checkio/JsCheckiOCourseProvider.java
@@ -1,8 +1,9 @@
 package com.jetbrains.edu.javascript.learning.checkio;
 
+import com.intellij.openapi.application.Experiments;
 import com.jetbrains.edu.javascript.learning.checkio.utils.JsCheckiONames;
 import com.jetbrains.edu.learning.CoursesProvider;
-import com.jetbrains.edu.learning.EduConfiguratorManager;
+import com.jetbrains.edu.learning.EduExperimentalFeatures;
 import com.jetbrains.edu.learning.checkio.courseFormat.CheckiOCourse;
 import com.jetbrains.edu.learning.courseFormat.Course;
 import org.jetbrains.annotations.NotNull;
@@ -14,10 +15,8 @@ public class JsCheckiOCourseProvider implements CoursesProvider {
   @NotNull
   @Override
   public List<Course> loadCourses() {
-    final Course course = new CheckiOCourse(JsCheckiONames.JS_CHECKIO, JsCheckiONames.JS_CHECKIO_LANGUAGE);
-    if (EduConfiguratorManager.forLanguage(course.getLanguageById()) != null) {
-      return Collections.singletonList(course);
-    }
-    return Collections.emptyList();
+    return Experiments.isFeatureEnabled(EduExperimentalFeatures.JAVASCRIPT_COURSES)
+           ? Collections.singletonList(new CheckiOCourse(JsCheckiONames.JS_CHECKIO, JsCheckiONames.JS_CHECKIO_LANGUAGE))
+           : Collections.emptyList();
   }
 }
