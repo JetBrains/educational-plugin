3ebe923746864be0065139a325f9d0774f3b4e79
Nikita Voloshin <nik.volosh@yandex.ru>
2018-08-30 14:30:53 +0300

    Changed js checkio experimental feature id

--- educational-core/src/com/jetbrains/edu/learning/EduExperimentalFeatures.kt
+++ educational-core/src/com/jetbrains/edu/learning/EduExperimentalFeatures.kt
@@ -3,6 +3,6 @@ package com.jetbrains.edu.learning
 object EduExperimentalFeatures {
   const val YAML_FORMAT = "edu.course.creator.yaml"
   const val ANDROID_COURSES = "com.jetbrains.edu.android"
+  const val JAVASCRIPT_COURSES = "edu.checkio.js"
   const val SPLIT_EDITOR = "edu.course.creator.split.editor"
-  const val JAVASCRIPT_COURSES = "com.jetbrains.edu.javascript.learning.checkio"
 }
