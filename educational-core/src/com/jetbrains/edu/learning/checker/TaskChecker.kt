d504b23a19830e40e6b0b7c573fef5df5186f963
Liana.Bakradze <liana.bakradze@jetbrains.com>
2018-08-31 14:42:10 +0300

    don't show popups

--- educational-core/src/com/jetbrains/edu/learning/checker/TaskChecker.kt
+++ educational-core/src/com/jetbrains/edu/learning/checker/TaskChecker.kt
@@ -1,21 +1,17 @@
 package com.jetbrains.edu.learning.checker
 
-import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.ui.MessageType
 import com.jetbrains.edu.learning.courseFormat.CheckStatus
 import com.jetbrains.edu.learning.courseFormat.tasks.Task
 
 open class TaskChecker<out T: Task>(@JvmField val task: T,@JvmField val project: Project) {
     open fun onTaskSolved(message: String) {
-        ApplicationManager.getApplication()
-                .invokeLater { CheckUtils.showTestResultPopUp(message, MessageType.INFO.popupBackground, project) }
+
     }
 
     open fun onTaskFailed(message: String) {
-        ApplicationManager.getApplication()
-                .invokeLater { CheckUtils.showTestResultPopUp(message, MessageType.ERROR.popupBackground, project) }
+
     }
 
     open fun check() =
