376d67a274e83ffec72da19072a70c3118b11378
Liana Bakradze <liana.bakradze@jetbrains.com>
2018-09-10 19:37:33 +0300

    EDU-1627 Coursera: ask for credentials only if there are not saved in settings

--- educational-core/src/com/jetbrains/edu/learning/coursera/CourseraTaskChecker.kt
+++ educational-core/src/com/jetbrains/edu/learning/coursera/CourseraTaskChecker.kt
@@ -3,11 +3,12 @@ package com.jetbrains.edu.learning.coursera
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.ui.Messages
-import com.intellij.openapi.util.Ref
+import com.intellij.openapi.ui.DialogBuilder
 import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.openapi.vfs.VfsUtilCore
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.ui.components.JBTextField
+import com.intellij.ui.layout.*
 import com.jetbrains.edu.learning.EduUtils
 import com.jetbrains.edu.learning.checker.CheckResult
 import com.jetbrains.edu.learning.checker.remote.RemoteTaskChecker
@@ -22,22 +23,19 @@ import org.apache.http.entity.StringEntity
 import org.apache.http.impl.client.HttpClientBuilder
 
 class CourseraTaskChecker : RemoteTaskChecker {
+  private val checkWithoutCredentials = CheckResult(CheckStatus.Unchecked, "Can't check on remote without credentials")
   override fun canCheck(project: Project, task: Task) =
     EduUtils.isStudentProject(project) && CourseraNames.COURSE_TYPE == task.course.courseType
 
+
   override fun check(project: Project, task: Task): CheckResult {
-    //TODO: don't ask for email and token every time
-    val email = Ref<String>()
-    val secret = Ref<String>()
-    ApplicationManager.getApplication().invokeAndWait {
-      email.set(Messages.showInputDialog("Email:", "", null))
-      if (email.get() == null) {
-        return@invokeAndWait
+    val courseraSettings = CourseraSettings.getInstance()
+    if (!courseraSettings.hasFullCredentials()) {
+      askToEnterCredentials()
+
+      if (!courseraSettings.hasFullCredentials()) {
+        return checkWithoutCredentials
       }
-      secret.set(Messages.showInputDialog("Coursera Token:", "", null))
-    }
-    if (email.get() == null || secret.get() == null) {
-      return CheckResult(CheckStatus.Unchecked, "Can't check on remote without credentials")
     }
 
     val taskDir = task.getDir(project) ?: error("No directory for task ${task.name}")
@@ -49,7 +47,8 @@ class CourseraTaskChecker : RemoteTaskChecker {
       val file = it.value.getVirtualFile(project) ?: error("VirtualFile for ${it.key} not found")
       Base64.encodeBase64String(VfsUtilCore.loadBytes(file))
     }
-    val submission = Submission(assignmentKey, email.get(), secret.get(), mapOf(Pair(partId, Part(ObjectMapper().writeValueAsString(output)))))
+    val submission = Submission(assignmentKey, courseraSettings.email!!, courseraSettings.token!!,
+                                mapOf(Pair(partId, Part(ObjectMapper().writeValueAsString(output)))))
     val json = ObjectMapper().writeValueAsString(submission)
 
     val client = HttpClientBuilder.create().build()
@@ -62,8 +61,31 @@ class CourseraTaskChecker : RemoteTaskChecker {
     return CheckResult.FAILED_TO_CHECK
   }
 
+  private fun askToEnterCredentials() {
+    val courseraSettings = CourseraSettings.getInstance()
+
+    val emailField = JBTextField(courseraSettings.email)
+    val tokenField = JBTextField(courseraSettings.token)
+    val credentialsPanel = panel {
+      row("Email:") { emailField(growPolicy = GrowPolicy.MEDIUM_TEXT) }
+      row("Token:") { tokenField(growPolicy = GrowPolicy.MEDIUM_TEXT) }
+    }
+    var refusedToProvidedCredentials = false
+
+    ApplicationManager.getApplication().invokeAndWait {
+      refusedToProvidedCredentials = !DialogBuilder().centerPanel(credentialsPanel).showAndGet()
+    }
+
+    if (!refusedToProvidedCredentials) {
+      CourseraSettings.getInstance().email = emailField.text
+      CourseraSettings.getInstance().token = tokenField.text
+    }
+  }
+
   private fun VirtualFile.getValueFromChildFile(fileName: String): String {
     val file = this.findChild(fileName) ?: error("$fileName not found in ${this.path}")
     return VfsUtil.loadText(file)
   }
+
+  private fun CourseraSettings.hasFullCredentials() = email.isNotEmpty() && token.isNotEmpty()
 }
