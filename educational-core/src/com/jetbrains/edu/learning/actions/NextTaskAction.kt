e8f663d6a402ff4b8045d60907a44a00d2da53ec
Liana Bakradze <liana.bakradze@jetbrains.com>
2018-09-13 18:29:46 +0300

    EDU-1639 make actions always enabled on check panel

--- educational-core/src/com/jetbrains/edu/learning/actions/NextTaskAction.kt
+++ educational-core/src/com/jetbrains/edu/learning/actions/NextTaskAction.kt
@@ -1,14 +1,25 @@
 package com.jetbrains.edu.learning.actions
 
 import com.intellij.icons.AllIcons
+import com.intellij.openapi.actionSystem.AnActionEvent
 import com.jetbrains.edu.learning.courseFormat.tasks.Task
 import com.jetbrains.edu.learning.navigation.NavigationUtils
+import com.jetbrains.edu.learning.ui.taskDescription.check.CheckPanel
 
 class NextTaskAction : TaskNavigationAction("Next Task", "Navigate to the next task", AllIcons.Actions.Forward) {
 
   override fun getTargetTask(sourceTask: Task): Task? = NavigationUtils.nextTask(sourceTask)
   override fun getActionId(): String = ACTION_ID
   override fun getShortcuts(): Array<String>? = arrayOf(SHORTCUT)
+
+  override fun update(e: AnActionEvent) {
+    if (CheckPanel.ACTION_PLACE == e.place) {
+      //action is being added only in valid context
+      //no project in event in this case, so just enable it
+      return
+    }
+    super.update(e)
+  }
 
   companion object {
     const val ACTION_ID = "Educational.NextTask"
