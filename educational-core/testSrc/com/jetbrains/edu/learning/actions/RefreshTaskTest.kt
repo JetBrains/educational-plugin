c317f48b26da7ef29be7d7fe3979899acb194572
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2018-09-13 14:55:00 +0300

    test revert multiple files for task

---
+++ educational-core/testSrc/com/jetbrains/edu/learning/actions/RefreshTaskTest.kt
@@ -0,0 +1,77 @@
+package com.jetbrains.edu.learning.actions
+
+import com.intellij.psi.PsiDocumentManager
+import com.jetbrains.edu.learning.EduTestCase
+import com.jetbrains.edu.learning.StudyTaskManager
+import java.io.IOException
+
+class RefreshTaskTest : EduTestCase() {
+
+  fun testRefreshTask() {
+    configureByTaskFile(1, 1, "taskFile1.txt")
+    myFixture.editor.caretModel.moveToOffset(13)
+    myFixture.type("test")
+    myFixture.testAction(RevertTaskAction())
+
+    assertEquals("Look! There is placeholder.", myFixture.getDocument(myFixture.file).text)
+  }
+
+  fun testCaretOutside() {
+    configureByTaskFile(1, 2, "taskFile2.txt")
+    myFixture.editor.caretModel.moveToOffset(4)
+    myFixture.type("test")
+    myFixture.testAction(RevertTaskAction())
+    assertEquals("Look! There is placeholder.", myFixture.getDocument(myFixture.file).text)
+  }
+
+  fun testMultipleFiles() {
+    configureByTaskFile(1, 3, "taskFile1.txt")
+    myFixture.editor.caretModel.moveToOffset(5)
+    myFixture.type("test")
+    configureByTaskFile(1, 3, "taskFile2.txt")
+    myFixture.editor.caretModel.moveToOffset(5)
+    myFixture.type("test")
+    PsiDocumentManager.getInstance(project).commitAllDocuments()
+    myFixture.testAction(RevertTaskAction())
+    val fileName = "lesson1/task3/taskFile1.txt"
+    val fileName2 = "lesson1/task3/taskFile2.txt"
+    val file1 = myFixture.findFileInTempDir(fileName)
+    val file2 = myFixture.findFileInTempDir(fileName2)
+    myFixture.openFileInEditor(file1)
+    assertEquals("Look! There is my placeholder.", myFixture.editor.document.text)
+    myFixture.openFileInEditor(file2)
+    assertEquals("Look! There is placeholder.", myFixture.editor.document.text)
+  }
+
+  @Throws(IOException::class)
+  override fun createCourse() {
+
+    StudyTaskManager.getInstance(myFixture.project).course = courseWithFiles {
+      lesson {
+        eduTask {
+          taskFile(
+            "taskFile1.txt", """
+          Look! There <p>is</p> placeholder.
+        """
+          )
+        }
+        eduTask {
+          taskFile("taskFile2.txt", """
+          Look! There <p>is</p> placeholder.
+        """
+          )
+        }
+        eduTask {
+            taskFile("taskFile1.txt", """
+          Look! There <p>is</p> my placeholder.
+        """)
+            taskFile("taskFile2.txt", """
+          Look! There <p>is</p> placeholder.
+        """
+            )
+        }
+      }
+    }
+  }
+
+}
