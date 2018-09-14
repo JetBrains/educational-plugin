e8777e109c81ab9143d20f9ad540e0a016a917be
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2018-09-14 17:42:53 +0300

    EDU-1637: test task creation from stepik step

--- educational-core/src/com/jetbrains/edu/learning/serialization/converter/TaskRoots.kt
+++ educational-core/src/com/jetbrains/edu/learning/serialization/converter/TaskRoots.kt
@@ -7,7 +7,9 @@ val LANGUAGE_TASK_ROOTS: Map<String, TaskRoots> = mapOf(
   EduNames.KOTLIN to TaskRoots("src", "test"),
   EduNames.JAVA to TaskRoots("src", "test"),
   EduNames.SCALA to TaskRoots("src", "test"),
-  EduNames.ANDROID to TaskRoots("src/main", "src/test")
+  EduNames.ANDROID to TaskRoots("src/main", "src/test"),
+  // For test purposes
+  "FakeGradleBasedLanguage" to TaskRoots("src", "test")
 )
 
 data class TaskRoots(val taskFilesRoot: String, val testFilesRoot: String)
---
+++ educational-core/testSrc/com/jetbrains/edu/learning/stepik/StepikTaskBuilderTest.kt
@@ -0,0 +1,65 @@
+package com.jetbrains.edu.learning.stepik
+
+import com.intellij.lang.Language
+import com.intellij.openapi.fileTypes.PlainTextLanguage
+import com.intellij.openapi.util.Key
+import com.intellij.openapi.util.io.FileUtil
+import com.jetbrains.edu.learning.EduNames
+import com.jetbrains.edu.learning.EduTestCase
+import com.jetbrains.edu.learning.configurators.FakeGradleBasedLanguage
+import com.jetbrains.edu.learning.courseFormat.RemoteCourse
+import com.jetbrains.edu.learning.courseFormat.tasks.*
+import com.jetbrains.edu.learning.stepik.StepikWrappers.StepContainer
+import org.hamcrest.CoreMatchers
+import org.hamcrest.Matcher
+import org.junit.Assert.assertThat
+import java.io.File
+
+class StepikTaskBuilderTest : EduTestCase() {
+
+  private val dataFileName: String get() = getTestName(true).trim().replace(" ", "_") + ".json"
+
+  override fun getTestDataPath(): String = "testData/stepikTaskBuilder"
+
+  fun `test theory task`() = doTest<TheoryTask>()
+  fun `test choice task`() = doTest<ChoiceTask>()
+  fun `test code task`() = doTest<CodeTask>()
+  fun `test edu task`() = doTest<EduTask>()
+  fun `test edu theory task`() = doTest<TheoryTask>()
+  fun `test output task`() = doTest<OutputTask>()
+  fun `test ide task`() = doTest<IdeTask>()
+
+  private inline fun <reified T : Task> doTest() {
+    for (language in listOf(PlainTextLanguage.INSTANCE, FakeGradleBasedLanguage)) {
+      val response = loadResponse()
+      val params: Map<Key<*>, Any> = mapOf(StepikConnector.COURSE_LANGUAGE to language.id)
+      val stepSource = StepikClient.deserializeStepikResponse(StepContainer::class.java, response, params).steps[0]
+
+      val course = RemoteCourse()
+      course.language = language.id
+      val task = StepikTaskBuilder(course, stepSource, -1, -1).createTask(stepSource.block.name) ?: error("")
+
+      assertInstanceOf(task, T::class.java)
+
+      assertTrue(task.taskFiles.isNotEmpty())
+      for ((path, taskFile) in task.taskFiles) {
+        val pathMatcher = createPathMatcher("${EduNames.SRC}/", language)
+        assertThat(path, pathMatcher)
+        assertThat(taskFile.name, pathMatcher)
+      }
+      for ((path, _) in task.testsText) {
+        assertThat(path, createPathMatcher("${EduNames.TEST}/", language))
+      }
+    }
+  }
+
+  private fun createPathMatcher(basePath: String, language: Language): Matcher<String> {
+    var pathMatcher = CoreMatchers.startsWith(basePath)
+    if (language !is FakeGradleBasedLanguage) {
+      pathMatcher = CoreMatchers.not(pathMatcher)
+    }
+    return pathMatcher
+  }
+
+  private fun loadResponse(): String = FileUtil.loadFile(File(testDataPath, dataFileName))
+}
