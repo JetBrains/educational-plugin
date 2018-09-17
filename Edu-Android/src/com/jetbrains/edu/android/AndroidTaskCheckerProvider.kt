725d53d96d1f5b144ef9980e26eebbad137c13bc
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2018-09-17 15:51:28 +0300

    EDU-1620: implement checker for android courses

--- Edu-Android/src/com/jetbrains/edu/android/AndroidTaskCheckerProvider.kt
+++ Edu-Android/src/com/jetbrains/edu/android/AndroidTaskCheckerProvider.kt
@@ -2,8 +2,13 @@ package com.jetbrains.edu.android
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
+import com.jetbrains.edu.learning.checker.gradle.GradleEduTaskChecker
 import com.jetbrains.edu.learning.checker.gradle.GradleTaskCheckerProvider
+import com.jetbrains.edu.learning.courseFormat.tasks.EduTask
 
 class AndroidTaskCheckerProvider : GradleTaskCheckerProvider() {
+
+  override fun getEduTaskChecker(task: EduTask, project: Project): GradleEduTaskChecker = AndroidChecker(task, project)
+
   override fun mainClassForFile(project: Project, file: VirtualFile): String? = null
 }
