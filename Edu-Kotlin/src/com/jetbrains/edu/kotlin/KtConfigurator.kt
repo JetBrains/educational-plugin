41968f773b4de5dfc2728d5bfc5f6496045ec7e7
Liana.Bakradze <liana.bakradze@jetbrains.com>
2018-09-17 20:01:43 +0300

    EDU-1647 refactoring: use CoursesProvider for bundled courses

--- Edu-Kotlin/src/com/jetbrains/edu/kotlin/KtConfigurator.kt
+++ Edu-Kotlin/src/com/jetbrains/edu/kotlin/KtConfigurator.kt
@@ -1,9 +1,7 @@
 package com.jetbrains.edu.kotlin
 
 import com.intellij.ide.fileTemplates.FileTemplateManager
-import com.intellij.openapi.util.io.FileUtil
 import com.jetbrains.edu.kotlin.checker.KtTaskCheckerProvider
-import com.jetbrains.edu.learning.EduUtils
 import com.jetbrains.edu.learning.gradle.GradleConfiguratorBase
 
 open class KtConfigurator : GradleConfiguratorBase() {
@@ -14,11 +12,6 @@ open class KtConfigurator : GradleConfiguratorBase() {
 
   override fun getTestFileName(): String = TESTS_KT
 
-  override fun getBundledCoursePaths(): List<String> {
-    val bundledCourseRoot = EduUtils.getBundledCourseRoot(DEFAULT_COURSE_NAME, KtConfigurator::class.java)
-    return listOf(FileUtil.join(bundledCourseRoot.absolutePath, DEFAULT_COURSE_NAME))
-  }
-
   override fun getTaskCheckerProvider() = KtTaskCheckerProvider()
 
   override fun getMockTemplate(): String {
@@ -26,8 +19,6 @@ open class KtConfigurator : GradleConfiguratorBase() {
   }
 
   companion object {
-    const val DEFAULT_COURSE_NAME = "Kotlin Koans.zip"
-
     const val TESTS_KT = "Tests.kt"
     const val TASK_KT = "Task.kt"
     const val MOCK_KT = "Mock.kt"
---
+++ educational-core/src/com/jetbrains/edu/learning/courseLoading/BundledCoursesProvider.kt
@@ -0,0 +1,61 @@
+package com.jetbrains.edu.learning.courseLoading
+
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.util.io.FileUtil
+import com.intellij.util.PathUtil
+import com.intellij.util.io.ZipUtil
+import com.jetbrains.edu.learning.CoursesProvider
+import com.jetbrains.edu.learning.EduUtils.getLocalCourse
+import com.jetbrains.edu.learning.courseFormat.Course
+import org.jetbrains.annotations.NonNls
+import java.io.File
+import java.io.IOException
+
+abstract class BundledCoursesProvider : CoursesProvider {
+  override fun loadCourses(): List<Course> {
+    val courses = mutableListOf<Course>()
+    for (path in getBundledCoursesPaths()) {
+      val localCourse = getLocalCourse(path)
+      if (localCourse == null) {
+        LOG.error("Failed to import local course form $path")
+        continue
+      }
+      courses.add(localCourse)
+    }
+    return courses
+  }
+
+  private fun getBundledCoursesPaths(): List<String> {
+    return getBundledCoursesNames().map { FileUtil.join(getBundledCourseRoot(it, javaClass).absolutePath, it) }
+  }
+
+  private fun getBundledCourseRoot(courseName: String, clazz: Class<*>): File {
+    @NonNls val jarPath = PathUtil.getJarPathForClass(clazz)
+    if (jarPath.endsWith(".jar")) {
+      val jarFile = File(jarPath)
+      val pluginBaseDir = jarFile.parentFile
+      val coursesDir = File(pluginBaseDir, "courses")
+
+      if (!coursesDir.exists()) {
+        if (!coursesDir.mkdir()) {
+          LOG.info("Failed to create courses dir")
+          return coursesDir
+        }
+      }
+      try {
+        ZipUtil.extract(jarFile, pluginBaseDir) { _, name -> name == courseName }
+      }
+      catch (e: IOException) {
+        LOG.info("Failed to extract default course", e)
+      }
+      return coursesDir
+    }
+    return File(jarPath, "courses")
+  }
+
+  protected abstract fun getBundledCoursesNames(): List<String>
+
+  companion object {
+    private val LOG = Logger.getInstance(BundledCoursesProvider::class.java)
+  }
+}
