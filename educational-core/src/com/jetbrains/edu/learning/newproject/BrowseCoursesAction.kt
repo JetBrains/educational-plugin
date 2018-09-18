c7c87e0f50ddefaaf762e5e41281652af6a30b18
Liana Bakradze <liana.bakradze@jetbrains.com>
2018-09-18 12:55:44 +0300

    EDU-1647 Reuse CoursesProvider to load courses under progress

--- educational-core/src/com/jetbrains/edu/learning/newproject/BrowseCoursesAction.kt
+++ educational-core/src/com/jetbrains/edu/learning/newproject/BrowseCoursesAction.kt
@@ -2,13 +2,14 @@ package com.jetbrains.edu.learning.newproject
 
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.DumbAwareAction
+import com.jetbrains.edu.learning.CoursesProvider
 import com.jetbrains.edu.learning.courseLoading.CourseLoader
 import com.jetbrains.edu.learning.newproject.ui.BrowseCoursesDialog
 
 class BrowseCoursesAction : DumbAwareAction("Browse Courses", "Browse list of available courses", null) {
 
   override fun actionPerformed(e: AnActionEvent) {
-    val courses = CourseLoader.getCourseInfosUnderProgress() ?: return
+    val courses = CourseLoader.getCourseInfosUnderProgress { CoursesProvider.loadAllCourses() } ?: return
     BrowseCoursesDialog(courses).show()
   }
 }
