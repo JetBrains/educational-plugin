3a4cb644265c5343364f1887d80178218ba924f4
Liana Bakradze <liana.bakradze@jetbrains.com>
2018-09-18 12:31:54 +0300

    EDU-1647 Start Coursera Assignment: show list of available assignments instead of dialog to select local zip

--- educational-core/src/com/jetbrains/edu/learning/coursera/StartCourseraAssignment.kt
+++ educational-core/src/com/jetbrains/edu/learning/coursera/StartCourseraAssignment.kt
@@ -1,11 +1,52 @@
 package com.jetbrains.edu.learning.coursera
 
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.project.DumbAwareAction
+import com.intellij.openapi.util.io.FileUtil
+import com.intellij.platform.templates.github.DownloadUtil
+import com.jetbrains.edu.learning.EduUtils
 import com.jetbrains.edu.learning.courseFormat.Course
+import com.jetbrains.edu.learning.newproject.ui.BrowseCoursesDialog
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.net.URL
+import java.nio.charset.StandardCharsets
 
-class StartCourseraAssignment : ImportLocalCourseAction("Start Coursera Assignment") {
+class StartCourseraAssignment : DumbAwareAction("Start Coursera Assignment") {
+  override fun actionPerformed(e: AnActionEvent) {
+    val courses = mutableListOf<Course>()
+    for (link in getCourseLinks()) {
+      val tempFile = FileUtil.createTempFile("coursera-zip", null)
+      DownloadUtil.downloadAtomically(null, link, tempFile)
+      val localCourse = EduUtils.getLocalCourse(tempFile.absolutePath)
+      if (localCourse == null) {
+        LOG.error("Failed to get local course from $link")
+        continue
+      }
+      courses.add(localCourse)
+    }
+    BrowseCoursesDialog(courses).show()
+  }
 
-  override fun initCourse(course: Course) {
-    super.initCourse(course)
-    course.courseType = CourseraNames.COURSE_TYPE
+  private fun getCourseLinks(): List<String> {
+    try {
+      val url = URL(LINK)
+      val conn = url.openConnection()
+      BufferedReader(InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8)).use { reader ->
+        return reader.readLines().asSequence().map { s: String -> s.split("#")[0].trim() }.filter { it.isNotEmpty() }.toList()
+      }
+    }
+    catch (e: IOException) {
+      LOG.warn("Failed to get courses from $LINK")
+    }
+    return emptyList()
+  }
+
+  companion object {
+    //TODO: change to master
+    private val LOG = Logger.getInstance(StartCourseraAssignment::class.java)
+    private const val LINK = "https://raw.githubusercontent.com/JetBrains/educational-plugin/liana/coursera-assignmnets-list/coursera-assignmnets.txt"
   }
 }
