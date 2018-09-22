2dbd27c87bbbafc0fb62e2c410c76b75cb0ce66b
Liana Bakradze <liana.bakradze@jetbrains.com>
2018-09-22 12:24:17 +0300

    EDU-1639 add Peek Answer link and placeholder for Show Full Output link

--- educational-core/src/com/jetbrains/edu/learning/ui/taskDescription/check/CheckDetailsPanel.kt
+++ educational-core/src/com/jetbrains/edu/learning/ui/taskDescription/check/CheckDetailsPanel.kt
@@ -1,10 +1,14 @@
 package com.jetbrains.edu.learning.ui.taskDescription.check
 
+import com.intellij.openapi.actionSystem.ActionManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.text.StringUtil
+import com.intellij.ui.JBColor
+import com.intellij.ui.components.labels.ActionLink
 import com.intellij.util.ui.JBUI
 import com.jetbrains.edu.learning.EduSettings
 import com.jetbrains.edu.learning.EduUtils
+import com.jetbrains.edu.learning.actions.CompareWithAnswerAction
 import com.jetbrains.edu.learning.checker.CheckResult
 import com.jetbrains.edu.learning.ui.taskDescription.JavaFxToolWindow
 import com.jetbrains.edu.learning.ui.taskDescription.SwingToolWindow
@@ -19,10 +23,25 @@ class CheckDetailsPanel(project: Project, checkResult: CheckResult) : JPanel(Bor
     messagePanel.preferredSize = JBUI.size(100, 100)
     add(messagePanel, BorderLayout.CENTER)
 
+    val peekSolution = LightColoredActionLink("Peek Solution...", CompareWithAnswerAction.ACTION_ID)
+    val linksPanel = JPanel(BorderLayout())
+    add(linksPanel, BorderLayout.SOUTH)
+
+    linksPanel.add(peekSolution, BorderLayout.CENTER)
+
     var message = checkResult.details ?: checkResult.message
     if (message.length > 150) {
       message = message.substring(0, 150) + "..."
+      linksPanel.add(LightColoredActionLink("Show Full Output...", CompareWithAnswerAction.ACTION_ID), BorderLayout.NORTH)
     }
     taskTextTw.setText(StringUtil.escapeXml(message))
+
+  }
+
+  private class LightColoredActionLink(text: String, actionId: String): ActionLink(text, ActionManager.getInstance().getAction(actionId)) {
+    init {
+      setNormalColor(JBColor(0x6894C6, 0x5C84C9))
+      border = JBUI.Borders.empty(16, 0, 0, 0)
+    }
   }
 }
