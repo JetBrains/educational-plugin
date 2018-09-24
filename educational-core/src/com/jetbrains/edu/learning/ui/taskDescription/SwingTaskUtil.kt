305443f6f5259a2b5992ea9debafb4522e05df0d
Valentina Kiryushkina <valentina.kiryushkina@jetbrains.com>
2018-09-24 18:22:24 +0300

    style check panel
    Here I switched from setting styles in HTMLEditorKit to creating templates
    with <style> tag. Looks like I can't style body from HTMLEditorKit as it
    has default settings, for example for a fonts size.

--- educational-core/src/com/jetbrains/edu/learning/ui/taskDescription/SwingTaskUtil.kt
+++ educational-core/src/com/jetbrains/edu/learning/ui/taskDescription/SwingTaskUtil.kt
@@ -2,16 +2,19 @@
 
 package com.jetbrains.edu.learning.ui.taskDescription
 
-import com.intellij.openapi.editor.colors.EditorColorsManager
+import com.intellij.openapi.diagnostic.Logger.getInstance
 import com.intellij.openapi.ui.VerticalFlowLayout
+import com.intellij.openapi.util.SystemInfo
+import com.intellij.openapi.util.io.StreamUtil
+import com.intellij.refactoring.changeClassSignature.ChangeClassSignatureDialog
 import com.intellij.ui.ColorUtil
 import com.intellij.util.ui.JBUI
 import com.intellij.util.ui.UIUtil
 import com.jetbrains.edu.learning.courseFormat.tasks.ChoiceTask
 import com.jetbrains.edu.learning.courseFormat.tasks.Task
-import java.awt.Font
 import java.awt.event.ItemEvent
 import java.awt.event.ItemListener
+import java.io.IOException
 import javax.swing.*
 import javax.swing.text.html.HTMLEditorKit
 
@@ -20,6 +23,7 @@ private const val RIGHT_INSET = 10
 private const val TOP_INSET = 15
 private const val BOTTOM_INSET = 10
 
+private val LOG = getInstance(ChangeClassSignatureDialog::class.java)
 
 fun Task?.createSpecificPanel(): JPanel? {
   val choiceTask = this as? ChoiceTask ?: return null
@@ -83,37 +87,65 @@ fun createTextPaneWithStyleSheet(): JTextPane {
   val textPane = JTextPane()
   textPane.contentType = HTMLEditorKit().contentType
 
-  val editorColorsScheme = EditorColorsManager.getInstance().globalScheme
-  val font = Font(editorColorsScheme.editorFontName, Font.PLAIN, editorColorsScheme.editorFontSize)
-  val dimmedColor = ColorUtil.toHex(ColorUtil.dimmer(UIUtil.getPanelBackground()))
-  val fontSize = font.size
-
-  val bodyRule = "body { font-family: ${font.family}; font-size: ${fontSize}pt; }"
-  val preRule = """
-    pre {
-      font-family: Courier;
-      font-size: $fontSize;
-      display: inline;
-      line-height: 50px;
-      padding-top: 5px;
-      padding-bottom: 5px;
-      padding-left: 5px;
-      background-color:$dimmedColor;
-    }
-  """.trimIndent()
-  val codeRule = "code {font-family: Courier; font-size:${fontSize}pt; display: flex; float: left; background-color: $dimmedColor;}"
-  val headersRule = "h1 { font-size: ${2 * fontSize}pt; } h2 { font-size: ${1.5 * fontSize}pt; }"
-
   val htmlEditorKit = UIUtil.getHTMLEditorKit(false)
-  val styleSheet = htmlEditorKit.styleSheet
-
-  for (rule in listOf(bodyRule, preRule, codeRule, headersRule)) {
-    styleSheet.addRule(rule)
-  }
 
   textPane.editorKit = htmlEditorKit
   textPane.isEditable = false
 
   textPane.background = TaskDescriptionView.getTaskDescriptionBackgroundColor()
   return textPane
+}
+
+fun textWithStyles(content: String): String {
+
+  val classLoader = BrowserWindow::class.java.classLoader
+  val templateName = when {
+    SystemInfo.isMac -> if (UIUtil.isUnderDarcula()) "mac_dark.html" else "mac.html"
+    SystemInfo.isLinux -> if (UIUtil.isUnderDarcula()) "win_dark.html" else "win.html"
+    else -> if (UIUtil.isUnderDarcula()) "linux_dark.html" else "linux.html"
+  }
+  var templateText = loadTemplateText(classLoader, templateName)
+
+  if (templateText == null) {
+    LOG.warn("Code mirror template is null")
+    return content
+  }
+
+  val bodyFontSize = bodyFontSize()
+  val codeFontSize = codeFontSize()
+
+  val bodyLineHeight = bodyLineHeight()
+  val codeLineHeight = codeLineHeight()
+
+  val dimmedColor = ColorUtil.toHex(ColorUtil.dimmer(UIUtil.getPanelBackground()))
+
+  templateText = templateText.replace("\${body_font_size}", bodyFontSize.toString())
+  templateText = templateText.replace("\${code_font_size}", codeFontSize.toString())
+  templateText = templateText.replace("\${body_line_height}", bodyLineHeight.toString())
+  templateText = templateText.replace("\${code_line_height}", codeLineHeight.toString())
+  templateText = templateText.replace("\${dimmedColor}", dimmedColor)
+  templateText = templateText.replace("\${content}", content)
+
+  return templateText
+}
+
+private fun loadTemplateText(classLoader: ClassLoader, templateName: String): String? {
+  var templateText: String? = null
+  val stream = classLoader.getResourceAsStream("/style/swingTemplates/$templateName")
+  try {
+    templateText = StreamUtil.readText(stream, "utf-8")
+  }
+  catch (e: IOException) {
+    LOG.warn(e.message)
+  }
+  finally {
+    try {
+      stream.close()
+    }
+    catch (e: IOException) {
+      LOG.warn(e.message)
+    }
+
+  }
+  return templateText
 }
--- educational-core/src/com/jetbrains/edu/learning/ui/taskDescription/SwingToolWindow.java
+++ educational-core/src/com/jetbrains/edu/learning/ui/taskDescription/SwingToolWindow.java
@@ -42,6 +42,8 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.Enumeration;
 
+import static com.jetbrains.edu.learning.ui.taskDescription.SwingTaskUtil.textWithStyles;
+
 public class SwingToolWindow extends TaskDescriptionToolWindow {
   private static final Logger LOG = Logger.getInstance(SwingToolWindow.class);
   private static final String HINT_PROTOCOL = "hint://";
@@ -97,7 +99,7 @@ public class SwingToolWindow extends TaskDescriptionToolWindow {
 
   public void setText(@NotNull String text) {
     String wrappedText = wrapHints(text);
-    myTaskTextPane.setText(wrappedText);
+    myTaskTextPane.setText(textWithStyles(wrappedText));
   }
 
   @Override
