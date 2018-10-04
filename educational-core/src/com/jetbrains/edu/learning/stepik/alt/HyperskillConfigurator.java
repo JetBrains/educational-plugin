70de812e8fc2938521c8ef121d015a84847528c2
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2018-10-04 21:56:04 +0300

    reuse remote check

--- educational-core/src/com/jetbrains/edu/learning/stepik/alt/HyperskillConfigurator.java
+++ educational-core/src/com/jetbrains/edu/learning/stepik/alt/HyperskillConfigurator.java
@@ -1,9 +1,13 @@
 package com.jetbrains.edu.learning.stepik.alt;
 
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.jetbrains.edu.learning.checker.TaskCheckerProvider;
+import com.jetbrains.edu.learning.checker.gradle.GradleTaskCheckerProvider;
 import com.jetbrains.edu.learning.gradle.GradleConfiguratorBase;
 import com.jetbrains.edu.learning.gradle.GradleCourseBuilderBase;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class HyperskillConfigurator extends GradleConfiguratorBase {
   private final HyperskillCourseBuilder myCourseBuilder = new HyperskillCourseBuilder();
@@ -23,6 +27,12 @@ public class HyperskillConfigurator extends GradleConfiguratorBase {
   @NotNull
   @Override
   public TaskCheckerProvider getTaskCheckerProvider() {
-    return null;
+    return new GradleTaskCheckerProvider() {
+      @Nullable
+      @Override
+      protected String mainClassForFile(@NotNull Project project, @NotNull VirtualFile file) {
+        return null;
+      }
+    };
   }
 }
