ddb35a3821d0aa506e6279cbdaf716ea2ebe2e05
Ekaterina Tuzova <ekaterina.tuzova@jetbrains.com>
2018-10-06 15:04:22 +0300

    hyperskill language decorator

---
+++ Edu-Java/src/com/jetbrains/edu/java/learning/stepik/alt/JHyperskillLanguageDecorator.kt
@@ -0,0 +1,9 @@
+package com.jetbrains.edu.java.learning.stepik.alt
+
+import com.jetbrains.edu.java.learning.JLanguageDecorator
+import icons.EducationalCoreIcons
+import javax.swing.Icon
+
+class JHyperskillLanguageDecorator : JLanguageDecorator() {
+  override fun getLogo(): Icon = EducationalCoreIcons.Hyperskill
+}
