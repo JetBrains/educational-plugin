6c27c679e18a523e1efd38320eb31e0d20aee719
Ekaterina Tuzova <ekaterina.tuzova@jetbrains.com>
2018-10-08 21:23:57 +0200

    stages instead of stage topics for project

--- educational-core/src/com/jetbrains/edu/learning/stepik/alt/HyperskillStepikConnector.kt
+++ educational-core/src/com/jetbrains/edu/learning/stepik/alt/HyperskillStepikConnector.kt
@@ -3,11 +3,24 @@ package com.jetbrains.edu.learning.stepik.alt
 import com.intellij.lang.Language
 import com.jetbrains.edu.learning.courseFormat.FeedbackLink
 import com.jetbrains.edu.learning.courseFormat.Lesson
+import com.jetbrains.edu.learning.stepik.StepikConnector
 import com.jetbrains.edu.learning.stepik.StepikConnector.*
 import com.jetbrains.edu.learning.stepik.StepikNames
 import com.jetbrains.edu.learning.stepik.StepikWrappers
 
 const val LINK_TEMPLATE = HYPERSKILL_URL + "learn/lesson/"
+
+fun getLesson(lessonId: Int, language: Language): Lesson? {
+  val lesson = StepikConnector.getLesson(lessonId)
+  val stepIds = lesson.steps.map { stepId -> stepId.toString() }.toTypedArray()
+  val allStepSources = StepikConnector.getStepSources(stepIds, language.displayName)
+  val tasks = StepikConnector.getTasks(language, stepIds, allStepSources)
+  tasks.forEach { task ->
+    task.feedbackLink = FeedbackLink(LINK_TEMPLATE + lesson.id)
+  }
+  lesson.taskList.addAll(tasks)
+  return lesson
+}
 
 fun getLessons(lessonIds: List<String>, languageId: String): List<Lesson> {
   val lessons = multipleRequestToStepik<StepikWrappers.LessonContainer>(
