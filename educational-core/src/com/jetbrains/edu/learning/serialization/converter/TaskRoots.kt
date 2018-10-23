e176a0cfe32a0092871455d6c136294b139df240
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2018-10-23 17:50:58 +0300

    remove comment

--- educational-core/src/com/jetbrains/edu/learning/serialization/converter/TaskRoots.kt
+++ educational-core/src/com/jetbrains/edu/learning/serialization/converter/TaskRoots.kt
@@ -7,8 +7,6 @@ val LANGUAGE_TASK_ROOTS: Map<String, TaskRoots> = mapOf(
   EduNames.KOTLIN to TaskRoots("src", "test"),
   EduNames.JAVA to TaskRoots("src", "test"),
   EduNames.SCALA to TaskRoots("src", "test"),
-  // TODO: ask Senya if we need this. Migrate if so.
-  // EduNames.ANDROID to TaskRoots("src/main", "src/test"),
   // For test purposes
   "FakeGradleBasedLanguage" to TaskRoots("src", "test")
 )
