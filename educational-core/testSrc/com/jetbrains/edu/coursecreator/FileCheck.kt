ffd08da81b8b1272f8375d5750853e5f655d30de
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2018-10-24 18:50:19 +0300

    EDU-1696: minor refactoring of existing tests

---
+++ educational-core/testSrc/com/jetbrains/edu/coursecreator/FileCheck.kt
@@ -0,0 +1,40 @@
+package com.jetbrains.edu.coursecreator
+
+import com.jetbrains.edu.learning.courseFormat.tasks.Task
+
+enum class FileSetKind(val fileSetName: String) {
+  TASK_FILES("task files"),
+  TEST_FILES("test files"),
+  ADDITIONAL_FILES("additional files"),
+  ALL_FILES("files");
+
+  fun fileSet(task: Task): Set<String> = when (this) {
+    TASK_FILES -> task.taskFiles.keys
+    TEST_FILES -> task.testsText.keys
+    ADDITIONAL_FILES -> task.additionalFiles.keys
+    ALL_FILES -> task.taskFiles.keys + task.testsText.keys + task.additionalFiles.keys
+  }
+}
+
+data class FileCheck(
+  val task: Task,
+  val path: String,
+  val kind: FileSetKind,
+  val shouldContain: Boolean
+) {
+  fun invert(): FileCheck = copy(shouldContain = !shouldContain)
+  fun check() {
+    if (shouldContain) {
+      check(path in kind.fileSet(task)) {
+        "`$path` should be in ${kind.fileSetName} of `${task.name}` task"
+      }
+    } else {
+      check(path !in kind.fileSet(task)) {
+        "`$path` shouldn't be in ${kind.fileSetName} of `${task.name}` task"
+      }
+    }
+  }
+}
+
+infix fun Pair<String, FileSetKind>.`in`(task: Task): FileCheck = FileCheck(task, first, second, true)
+infix fun Pair<String, FileSetKind>.notIn(task: Task): FileCheck = FileCheck(task, first, second, false)
