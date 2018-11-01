b278bb2c9f36c3271156b0714b5e9ed16e848d00
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2018-11-01 15:35:19 +0300

    serialize course type to json

--- educational-core/src/com/jetbrains/edu/learning/EduVersions.kt
+++ educational-core/src/com/jetbrains/edu/learning/EduVersions.kt
@@ -4,4 +4,4 @@ package com.jetbrains.edu.learning
 
 // If you change version of any format, add point about it in `docs/Versions.md`
 const val XML_FORMAT_VERSION: Int = 11
-const val JSON_FORMAT_VERSION: Int = 7
+const val JSON_FORMAT_VERSION: Int = 8
---
+++ educational-core/src/com/jetbrains/edu/learning/serialization/converter/json/local/To8VersionLocalCourseConverter.kt
@@ -0,0 +1,22 @@
+package com.jetbrains.edu.learning.serialization.converter.json.local
+
+import com.google.gson.JsonObject
+import com.jetbrains.edu.learning.EduNames
+import com.jetbrains.edu.learning.serialization.SerializationUtils.Json.COURSE_TYPE
+import com.jetbrains.edu.learning.serialization.SerializationUtils.Json.PROGRAMMING_LANGUAGE
+import com.jetbrains.edu.learning.serialization.converter.json.JsonLocalCourseConverter
+
+class To8VersionLocalCourseConverter : JsonLocalCourseConverter {
+
+  override fun convert(localCourse: JsonObject): JsonObject {
+    val language = localCourse.getAsJsonPrimitive(PROGRAMMING_LANGUAGE)?.asString ?: ""
+    var courseType = EduNames.PYCHARM
+    if ("edu-android" == language) {
+      localCourse.addProperty(PROGRAMMING_LANGUAGE, EduNames.KOTLIN)
+      courseType = EduNames.ANDROID
+    }
+    localCourse.addProperty(COURSE_TYPE, courseType)
+
+    return localCourse
+  }
+}
