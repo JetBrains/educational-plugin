bbc9b6c8450257dccfa2e30bb9ef830d9ee25b8f
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2018-11-06 13:35:17 +0300

    specific configurator for kt coursera

---
+++ Edu-Kotlin/src/com/jetbrains/edu/kotlin/coursera/CourseraKtConfigurator.kt
@@ -0,0 +1,7 @@
+package com.jetbrains.edu.kotlin.coursera
+
+import com.jetbrains.edu.kotlin.KtConfigurator
+
+open class CourseraKtConfigurator : KtConfigurator() {
+  override fun isCourseCreatorEnabled() = false
+}
