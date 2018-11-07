c93ba5c6df28eed1b03de9711e902841d8fb3636
Liana Bakradze <liana.bakradze@jetbrains.com>
2018-11-07 17:41:09 +0300

    show CheckiO check results in CheckDetailsView

--- Edu-Python/src/com/jetbrains/edu/python/learning/checkio/checker/PyCheckiOTaskChecker.java
+++ Edu-Python/src/com/jetbrains/edu/python/learning/checkio/checker/PyCheckiOTaskChecker.java
@@ -1,11 +1,11 @@
 package com.jetbrains.edu.python.learning.checkio.checker;
 
-import com.intellij.icons.AllIcons;
 import com.intellij.openapi.application.ex.ApplicationUtil;
 import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.project.Project;
 import com.jetbrains.edu.learning.checker.CheckResult;
 import com.jetbrains.edu.learning.checker.TaskChecker;
+import com.jetbrains.edu.learning.checker.details.CheckDetailsView;
 import com.jetbrains.edu.learning.checkio.CheckiOCourseContentGenerator;
 import com.jetbrains.edu.learning.checkio.CheckiOCourseUpdater;
 import com.jetbrains.edu.learning.checkio.courseFormat.CheckiOCourse;
@@ -15,16 +15,9 @@ import com.jetbrains.edu.learning.courseFormat.tasks.EduTask;
 import com.jetbrains.edu.python.learning.checkio.connectors.PyCheckiOApiConnector;
 import com.jetbrains.edu.python.learning.checkio.connectors.PyCheckiOOAuthConnector;
 import com.jetbrains.python.PythonFileType;
-import javafx.embed.swing.JFXPanel;
 import org.jetbrains.annotations.NotNull;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-
 public class PyCheckiOTaskChecker extends TaskChecker<EduTask> {
-  private static final String TEST_RESULTS_ID = "checkioTestResults";
 
   public PyCheckiOTaskChecker(@NotNull EduTask task, @NotNull Project project) {
     super(task, project);
@@ -67,7 +60,7 @@ public class PyCheckiOTaskChecker extends TaskChecker<EduTask> {
         ApplicationUtil.runWithCheckCanceled(missionCheck, indicator);
 
       if (checkResult.getStatus() != CheckStatus.Unchecked) {
-        showTestResultPanel(missionCheck.getBrowserPanel());
+        CheckDetailsView.getInstance(project).showJavaFXResult("CheckiO Response", missionCheck.getBrowserPanel());
       }
 
       return checkResult;
@@ -76,56 +69,5 @@ public class PyCheckiOTaskChecker extends TaskChecker<EduTask> {
       LOG.warn(e.getMessage());
       return CheckResult.FAILED_TO_CHECK;
     }
-  }
-
-  private void showTestResultPanel(@NotNull JFXPanel browserPanel) {
-    final JPanel testResultPanel = new JPanel(new BorderLayout());
-
-    testResultPanel.add(createBackButtonUI(), BorderLayout.PAGE_START);
-    testResultPanel.add(createBrowserWindowUI(browserPanel));
-
-    //TODO: integrate checkio into new task description panel
-    //final TaskDescriptionToolWindow toolWindow = EduUtils.getStudyToolWindow(project);
-    //
-    //if (toolWindow != null) {
-    //
-    //  toolWindow.getContentPanel().add(TEST_RESULTS_ID, testResultPanel);
-    //  toolWindow.showPanelById();
-    //}
-  }
-
-  private static JComponent createBrowserWindowUI(@NotNull JFXPanel browserPanel) {
-    final JPanel browserWindowPanel = new JPanel();
-
-    browserWindowPanel.setLayout(new BoxLayout(browserWindowPanel, BoxLayout.PAGE_AXIS));
-    browserWindowPanel.add(browserPanel);
-
-    return browserWindowPanel;
-  }
-
-  private JComponent createBackButtonUI() {
-    final JLabel label = new JLabel("Back to task description", AllIcons.Vcs.Arrow_left, SwingConstants.LEFT);
-
-    final JPanel buttonPanel = new JPanel(new BorderLayout());
-    buttonPanel.add(Box.createRigidArea(new Dimension(0, 10)), BorderLayout.PAGE_START);
-    buttonPanel.add(label, BorderLayout.WEST);
-    buttonPanel.add(Box.createRigidArea(new Dimension(0, 10)), BorderLayout.PAGE_END);
-
-    buttonPanel.addMouseListener(new MouseAdapter() {
-      @Override
-      public void mouseClicked(MouseEvent e) {
-        showTaskInfoPanel();
-      }
-    });
-
-    return buttonPanel;
-  }
-
-  private void showTaskInfoPanel() {
-    //TODO: integrate checkio into new task description panel
-    //final TaskDescriptionToolWindow descriptionToolWindow = EduUtils.getStudyToolWindow(project);
-    //if (descriptionToolWindow != null) {
-    //  descriptionToolWindow.showPanelById();
-    //}
   }
 }
