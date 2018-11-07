8023ac2aec531ad0075531c5f4982c2c52005c32
Liana Bakradze <liana.bakradze@jetbrains.com>
2018-11-07 14:35:51 +0300

    use new Check Details tool window to show full output

---
+++ educational-core/src/com/jetbrains/edu/learning/checker/details/CheckDetailsView.kt
@@ -0,0 +1,43 @@
+package com.jetbrains.edu.learning.checker.details
+
+import com.intellij.execution.impl.ConsoleViewImpl
+import com.intellij.execution.ui.ConsoleViewContentType
+import com.intellij.openapi.components.ServiceManager
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.wm.ToolWindow
+import com.intellij.openapi.wm.ToolWindowManager
+import com.jetbrains.edu.learning.EduUtils
+
+class CheckDetailsView(val project: Project) {
+
+  companion object {
+    @JvmStatic
+    fun getInstance(project: Project): CheckDetailsView {
+      if (!EduUtils.isStudyProject(project)) {
+        error("Attempt to get CheckDetailsView for non-edu project")
+      }
+      return ServiceManager.getService(project, CheckDetailsView::class.java)
+    }
+  }
+
+  fun showOutput(message: String) {
+    printToConsole("Output", message, ConsoleViewContentType.ERROR_OUTPUT)
+  }
+
+  private fun getToolWindow(): ToolWindow {
+    return ToolWindowManager.getInstance(project).getToolWindow(CheckDetailsToolWindowFactory.ID)
+           ?: error("CheckDetails tool window not found")
+  }
+
+  private fun printToConsole(title: String, message: String, contentType: ConsoleViewContentType) {
+    val toolWindow = getToolWindow()
+    val contentManager = toolWindow.contentManager
+    contentManager.removeAllContents(true)
+    val consoleView = ConsoleViewImpl(project, true)
+    val content = contentManager.factory.createContent(consoleView.component, title, false)
+    contentManager.addContent(content)
+    consoleView.print(message, contentType)
+    toolWindow.setAvailable(true, null)
+    toolWindow.show(null)
+  }
+}
