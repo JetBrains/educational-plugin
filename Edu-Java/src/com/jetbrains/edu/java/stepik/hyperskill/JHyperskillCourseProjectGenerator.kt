7fa336905b64dc51962749f52c93c14f7678097d
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2018-11-08 20:18:37 +0300

    get project under progress

--- Edu-Java/src/com/jetbrains/edu/java/stepik/hyperskill/JHyperskillCourseProjectGenerator.kt
+++ Edu-Java/src/com/jetbrains/edu/java/stepik/hyperskill/JHyperskillCourseProjectGenerator.kt
@@ -1,6 +1,9 @@
 package com.jetbrains.edu.java.stepik.hyperskill
 
 import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.progress.ProgressIndicator
+import com.intellij.openapi.progress.ProgressManager
+import com.intellij.openapi.progress.Task
 import com.jetbrains.edu.learning.courseFormat.Course
 import com.jetbrains.edu.learning.courseFormat.FrameworkLesson
 import com.jetbrains.edu.learning.gradle.GradleCourseBuilderBase
@@ -16,27 +19,32 @@ class JHyperskillCourseProjectGenerator(builder: GradleCourseBuilderBase,
 
   override fun beforeProjectGenerated(): Boolean {
     return try {
-      val language = myCourse.languageById
-      val hyperskillAccount = HyperskillSettings.INSTANCE.account
-      if (hyperskillAccount == null) {
-        LOG.error("User is not logged in to the Hyperskill")
-        return false
-      }
-      val userInfo = hyperskillAccount.userInfo
-      val hyperskillProject = userInfo.hyperskillProject
-      if (hyperskillProject == null) {
-        LOG.error("User didn't choose project on hyperskill")
-        return false
-      }
-      val lessonId = hyperskillProject.lesson
-      val projectId = hyperskillProject.id
+      ProgressManager.getInstance().run(object : Task.WithResult<Boolean, Exception>(null, "Loading course", true) {
+        override fun compute(indicator: ProgressIndicator): Boolean {
+          val language = myCourse.languageById
+          val hyperskillAccount = HyperskillSettings.INSTANCE.account
+          if (hyperskillAccount == null) {
+            LOG.error("User is not logged in to the Hyperskill")
+            return false
+          }
+          val userInfo = hyperskillAccount.userInfo
+          val hyperskillProject = userInfo.hyperskillProject
+          if (hyperskillProject == null) {
+            LOG.error("User didn't choose project on hyperskill")
+            return false
+          }
+          val lessonId = hyperskillProject.lesson
+          val projectId = hyperskillProject.id
 
-      val stages = HyperskillConnector.getStages(projectId) ?: return false
-      val lesson = getLesson(lessonId, language, stages) ?: return false
-      lesson.name = hyperskillProject.title.removePrefix(PROJECT_PREFIX)
+          val stages = HyperskillConnector.getStages(projectId) ?: return false
+          val lesson = getLesson(lessonId, language, stages) ?: return false
+          lesson.name = hyperskillProject.title.removePrefix(PROJECT_PREFIX)
 
-      myCourse.addLesson(FrameworkLesson(lesson))
-      true
+          myCourse.addLesson(FrameworkLesson(lesson))
+          return true
+        }
+      })
+
     }
     catch (e: Exception) {
       LOG.warn(e)
