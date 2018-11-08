795aed010066fc174a97156d9b53612ab0f1cd8e
Liana Bakradze <liana.bakradze@jetbrains.com>
2018-11-08 13:02:37 +0300

    EDU-1722 mock CheckDeatils for tests

---
+++ educational-core/src/com/jetbrains/edu/learning/checker/details/CheckDetailsViewImpl.kt
@@ -0,0 +1,57 @@
+package com.jetbrains.edu.learning.checker.details
+
+import com.intellij.execution.impl.ConsoleViewImpl
+import com.intellij.execution.ui.ConsoleViewContentType
+import com.intellij.openapi.application.runInEdt
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.wm.ToolWindow
+import com.intellij.openapi.wm.ToolWindowManager
+import javafx.embed.swing.JFXPanel
+import javax.swing.JComponent
+
+class CheckDetailsViewImpl(val project: Project) : CheckDetailsView() {
+
+  override fun showOutput(message: String) {
+    printToConsole("Output", message, ConsoleViewContentType.NORMAL_OUTPUT)
+  }
+
+  override fun showCompilationResults(message: String) {
+    printToConsole("Compilation Results", message, ConsoleViewContentType.ERROR_OUTPUT)
+  }
+
+  private fun getToolWindow(): ToolWindow {
+    return ToolWindowManager.getInstance(project).getToolWindow(CheckDetailsToolWindowFactory.ID)
+           ?: error("CheckDetails tool window not found")
+  }
+
+  private fun createContentAndShowToolWindow(component: JComponent, title: String) {
+    val toolWindow = getToolWindow()
+    val contentManager = toolWindow.contentManager
+    contentManager.removeAllContents(true)
+    val content = contentManager.factory.createContent(component, title, false)
+    contentManager.addContent(content)
+    toolWindow.setAvailable(true, null)
+    toolWindow.show(null)
+  }
+
+  private fun printToConsole(title: String, message: String, contentType: ConsoleViewContentType) {
+    val consoleView = ConsoleViewImpl(project, true)
+    consoleView.print(message, contentType)
+    runInEdt {
+      createContentAndShowToolWindow(consoleView.component, title)
+    }
+  }
+
+  override fun showJavaFXResult(title: String, panel: JFXPanel) {
+    runInEdt {
+      createContentAndShowToolWindow(panel, title)
+    }
+  }
+
+  override fun clear() {
+    val toolWindow = getToolWindow()
+    runInEdt {
+      toolWindow.contentManager.removeAllContents(true)
+    }
+  }
+}
---
+++ educational-core/src/com/jetbrains/edu/learning/checker/details/MockCheckDetailsView.kt
@@ -0,0 +1,17 @@
+package com.jetbrains.edu.learning.checker.details
+
+import javafx.embed.swing.JFXPanel
+
+class MockCheckDetailsView : CheckDetailsView() {
+  override fun showOutput(message: String) {
+  }
+
+  override fun showCompilationResults(message: String) {
+  }
+
+  override fun showJavaFXResult(title: String, panel: JFXPanel) {
+  }
+
+  override fun clear() {
+  }
+}
