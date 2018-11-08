7aec27292f883ff5df7fd4d69b90796a9c2af08e
Liana Bakradze <liana.bakradze@jetbrains.com>
2018-11-08 13:02:37 +0300

    mock CheckDeatils for tests

--- educational-core/src/com/jetbrains/edu/learning/checker/details/CheckDetailsView.kt
+++ educational-core/src/com/jetbrains/edu/learning/checker/details/CheckDetailsView.kt
@@ -1,67 +1,25 @@
 package com.jetbrains.edu.learning.checker.details
 
-import com.intellij.execution.impl.ConsoleViewImpl
-import com.intellij.execution.ui.ConsoleViewContentType
-import com.intellij.openapi.application.runInEdt
 import com.intellij.openapi.components.ServiceManager
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.wm.ToolWindow
-import com.intellij.openapi.wm.ToolWindowManager
 import com.jetbrains.edu.learning.EduUtils
 import javafx.embed.swing.JFXPanel
-import javax.swing.JComponent
 
-class CheckDetailsView(val project: Project) {
+abstract class CheckDetailsView {
 
-  fun showOutput(message: String) {
-    printToConsole("Output", message, ConsoleViewContentType.NORMAL_OUTPUT)
-  }
+  abstract fun showOutput(message: String)
 
-  fun showCompilationResults(message: String) {
-    printToConsole("Compilation Results", message, ConsoleViewContentType.ERROR_OUTPUT)
-  }
+  abstract fun showCompilationResults(message: String)
 
-  private fun getToolWindow(): ToolWindow {
-    return ToolWindowManager.getInstance(project).getToolWindow(CheckDetailsToolWindowFactory.ID)
-           ?: error("CheckDetails tool window not found")
-  }
+  abstract fun showJavaFXResult(title: String, panel: JFXPanel)
 
-  private fun createContentAndShowToolWindow(component: JComponent, title: String) {
-    val toolWindow = getToolWindow()
-    val contentManager = toolWindow.contentManager
-    contentManager.removeAllContents(true)
-    val content = contentManager.factory.createContent(component, title, false)
-    contentManager.addContent(content)
-    toolWindow.setAvailable(true, null)
-    toolWindow.show(null)
-  }
-
-  private fun printToConsole(title: String, message: String, contentType: ConsoleViewContentType) {
-    val consoleView = ConsoleViewImpl(project, true)
-    consoleView.print(message, contentType)
-    runInEdt {
-      createContentAndShowToolWindow(consoleView.component, title)
-    }
-  }
-
-  fun showJavaFXResult(title: String, panel: JFXPanel) {
-    runInEdt {
-      createContentAndShowToolWindow(panel, title)
-    }
-  }
-
-  fun clear() {
-    val toolWindow = getToolWindow()
-    runInEdt {
-      toolWindow.contentManager.removeAllContents(true)
-    }
-  }
+  abstract fun clear()
 
   companion object {
     @JvmStatic
     fun getInstance(project: Project): CheckDetailsView {
       if (!EduUtils.isStudyProject(project)) {
-        error("Attempt to get CheckDetailsView for non-edu project")
+        error("Attempt to get CheckDetailsViewImpl for non-edu project")
       }
       return ServiceManager.getService(project, CheckDetailsView::class.java)
     }
