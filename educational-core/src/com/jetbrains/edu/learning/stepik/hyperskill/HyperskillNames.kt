2cf68a3a6fd800f4618d7d48ab4a8e654f48c886
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2018-11-08 18:57:08 +0300

    hyperskill client id

--- educational-core/src/com/jetbrains/edu/learning/stepik/hyperskill/HyperskillNames.kt
+++ educational-core/src/com/jetbrains/edu/learning/stepik/hyperskill/HyperskillNames.kt
@@ -8,7 +8,7 @@ const val HYPERSKILL_PROJECTS_URL = "https://hyperskill.org/projects/"
 private val port = BuiltInServerManager.getInstance().port
 val REDIRECT_URI = "http://localhost:$port/api/edu/hyperskill/oauth"
 
-var CLIENT_ID = HyperskillOAuthBundle.valueOrDefault("client_id", "")
+var CLIENT_ID = HyperskillOAuthBundle.valueOrDefault("clientId", "")
 val AUTHORISATION_CODE_URL = "https://hyperskill.org/oauth2/authorize/?" +
                              "client_id=$CLIENT_ID&redirect_uri=$REDIRECT_URI&grant_type=code&scope=read+write&response_type=code"
 
