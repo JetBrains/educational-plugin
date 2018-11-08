bc8a9399be75f0e9184f10cd271a17babf79a8ab
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2018-11-08 20:18:37 +0300

    get project under progress

--- educational-core/src/com/jetbrains/edu/learning/stepik/hyperskill/HyperskillStepikConnector.kt
+++ educational-core/src/com/jetbrains/edu/learning/stepik/hyperskill/HyperskillStepikConnector.kt
@@ -1,16 +1,24 @@
 package com.jetbrains.edu.learning.stepik.hyperskill
 
 import com.intellij.lang.Language
+import com.intellij.openapi.progress.ProgressManager
 import com.jetbrains.edu.learning.courseFormat.Lesson
 import com.jetbrains.edu.learning.courseFormat.TaskFile
 import com.jetbrains.edu.learning.courseFormat.tasks.EduTask
 import com.jetbrains.edu.learning.stepik.StepikConnector
 
 fun getLesson(lessonId: Int, language: Language, stages: List<HyperskillStage>): Lesson? {
+  val progressIndicator = ProgressManager.getInstance().progressIndicator
   val lesson = StepikConnector.getLesson(lessonId)
+  progressIndicator?.checkCanceled()
+  progressIndicator?.text2 = "Loading project steps"
   val stepIds = lesson.steps.map { stepId -> stepId.toString() }.toTypedArray()
   val allStepSources = StepikConnector.getStepSources(stepIds, language.baseLanguage?.id)
+  progressIndicator?.checkCanceled()
+  progressIndicator?.text2 = "Loading tasks"
   val tasks = StepikConnector.getTasks(language, stepIds, allStepSources)
+  progressIndicator?.checkCanceled()
+  progressIndicator?.text2 = "Loading topics"
   val convertedTasks = mutableListOf<EduTask>()
   for ((index, task) in tasks.withIndex()) {
     val stage = stages[index]
