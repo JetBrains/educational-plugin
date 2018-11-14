45570ab088b180ec86ae290126addbd77d027309
Liana Bakradze <liana.bakradze@jetbrains.com>
2018-11-14 14:09:14 +0300

    fix CheckiO Python task checker

--- educational-core/src/com/jetbrains/edu/learning/checkio/checker/CheckiOTaskChecker.java
+++ educational-core/src/com/jetbrains/edu/learning/checkio/checker/CheckiOTaskChecker.java
@@ -1,28 +1,18 @@
 package com.jetbrains.edu.learning.checkio.checker;
 
-import com.intellij.icons.AllIcons;
 import com.intellij.openapi.application.ex.ApplicationUtil;
 import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.project.Project;
-import com.intellij.ui.components.JBLabel;
-import com.intellij.util.ui.JBUI;
 import com.jetbrains.edu.learning.checker.CheckResult;
 import com.jetbrains.edu.learning.checker.TaskChecker;
+import com.jetbrains.edu.learning.checker.details.CheckDetailsView;
 import com.jetbrains.edu.learning.checkio.connectors.CheckiOOAuthConnector;
 import com.jetbrains.edu.learning.courseFormat.CheckStatus;
 import com.jetbrains.edu.learning.courseFormat.tasks.EduTask;
-import javafx.embed.swing.JFXPanel;
 import org.jetbrains.annotations.NotNull;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-
 public class CheckiOTaskChecker extends TaskChecker<EduTask> {
-  private static final String TEST_RESULTS_ID = "checkioTestResults";
-
   private final CheckiOMissionCheck myMissionCheck;
 
   public CheckiOTaskChecker(
@@ -51,7 +41,7 @@ public class CheckiOTaskChecker extends TaskChecker<EduTask> {
         ApplicationUtil.runWithCheckCanceled(myMissionCheck, ProgressManager.getInstance().getProgressIndicator());
 
       if (checkResult.getStatus() != CheckStatus.Unchecked) {
-        showTestResultPanel(myMissionCheck.getBrowserPanel());
+        CheckDetailsView.getInstance(project).showJavaFXResult("CheckiO Response", myMissionCheck.getBrowserPanel());
       }
 
       return checkResult;
@@ -60,53 +50,5 @@ public class CheckiOTaskChecker extends TaskChecker<EduTask> {
       LOG.warn(e.getMessage());
       return CheckResult.FAILED_TO_CHECK;
     }
-  }
-
-  private void showTestResultPanel(@NotNull JFXPanel browserPanel) {
-    //final JPanel testResultPanel = new JPanel(new BorderLayout());
-    //
-    //testResultPanel.add(createBackButtonUI(), BorderLayout.PAGE_START);
-    //testResultPanel.add(createBrowserWindowUI(browserPanel));
-    //
-    //final TaskDescriptionToolWindow toolWindow = EduUtils.getStudyToolWindow(project);
-    //
-    //if (toolWindow != null) {
-    //  toolWindow.getContentPanel().add(TEST_RESULTS_ID, testResultPanel);
-    //  toolWindow.showPanelById(TEST_RESULTS_ID);
-    //}
-  }
-
-  private static JComponent createBrowserWindowUI(@NotNull JFXPanel browserPanel) {
-    final JPanel browserWindowPanel = new JPanel();
-
-    browserWindowPanel.setLayout(new BoxLayout(browserWindowPanel, BoxLayout.PAGE_AXIS));
-    browserWindowPanel.add(browserPanel);
-
-    return browserWindowPanel;
-  }
-
-  private JComponent createBackButtonUI() {
-    final JLabel label = new JBLabel("Back to task description", AllIcons.Vcs.Arrow_left, SwingConstants.LEFT);
-
-    final JPanel buttonPanel = new JPanel(new BorderLayout());
-    buttonPanel.add(Box.createRigidArea(JBUI.size(0, 10)), BorderLayout.PAGE_START);
-    buttonPanel.add(label, BorderLayout.WEST);
-    buttonPanel.add(Box.createRigidArea(JBUI.size(0, 10)), BorderLayout.PAGE_END);
-
-    buttonPanel.addMouseListener(new MouseAdapter() {
-      @Override
-      public void mouseClicked(MouseEvent e) {
-        showTaskInfoPanel();
-      }
-    });
-
-    return buttonPanel;
-  }
-
-  private void showTaskInfoPanel() {
-    //final TaskDescriptionToolWindow descriptionToolWindow = EduUtils.getStudyToolWindow(project);
-    //if (descriptionToolWindow != null) {
-    //  descriptionToolWindow.showPanelById(TaskDescriptionToolWindow.TASK_INFO_ID);
-    //}
   }
 }
