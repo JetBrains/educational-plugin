072ad008cb78e02e6e99d6bbb54833b75a9fd148
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2018-11-15 14:55:43 +0300

    EDU-1731: Save last import location

--- educational-core/src/com/jetbrains/edu/learning/actions/ImportLocalCourseAction.kt
+++ educational-core/src/com/jetbrains/edu/learning/actions/ImportLocalCourseAction.kt
@@ -1,10 +1,13 @@
 package com.jetbrains.edu.learning.actions
 
+import com.intellij.ide.util.PropertiesComponent
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.fileChooser.FileChooser
 import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.openapi.ui.Messages
+import com.intellij.openapi.vfs.LocalFileSystem
 import com.intellij.openapi.vfs.VfsUtil
+import com.intellij.openapi.vfs.VirtualFile
 import com.jetbrains.edu.learning.EduUtils
 import com.jetbrains.edu.learning.courseFormat.Course
 import com.jetbrains.edu.learning.newproject.LocalCourseFileChooser
@@ -13,10 +16,11 @@ import com.jetbrains.edu.learning.statistics.EduUsagesCollector
 
 open class ImportLocalCourseAction(text: String = "Import Local Course") : DumbAwareAction(text) {
   override fun actionPerformed(e: AnActionEvent) {
-    FileChooser.chooseFile(LocalCourseFileChooser, null, VfsUtil.getUserHomeDir()) { file ->
+    FileChooser.chooseFile(LocalCourseFileChooser, null, importLocation()) { file ->
       val fileName = file.path
       val course = EduUtils.getLocalCourse(fileName)
       if (course != null) {
+        saveLastImportLocation(file)
         initCourse(course)
         EduUsagesCollector.courseArchiveImported()
         JoinCourseDialog(course).show()
@@ -29,5 +33,22 @@ open class ImportLocalCourseAction(text: String = "Import Local Course") : DumbA
 
   protected open fun initCourse(course: Course) {
     course.isFromZip = true
+  }
+
+  companion object {
+    private const val LAST_IMPORT_LOCATION = "Edu.LastImportLocation"
+
+    @JvmStatic
+    fun importLocation(): VirtualFile? {
+      val defaultDir = VfsUtil.getUserHomeDir()
+      val lastImportLocation = PropertiesComponent.getInstance().getValue(LAST_IMPORT_LOCATION) ?: return defaultDir
+      return LocalFileSystem.getInstance().findFileByPath(lastImportLocation) ?: defaultDir
+    }
+
+    @JvmStatic
+    fun saveLastImportLocation(file: VirtualFile) {
+      val location = if (!file.isDirectory) file.parent ?: return else file
+      PropertiesComponent.getInstance().setValue(LAST_IMPORT_LOCATION, location.path)
+    }
   }
 }
