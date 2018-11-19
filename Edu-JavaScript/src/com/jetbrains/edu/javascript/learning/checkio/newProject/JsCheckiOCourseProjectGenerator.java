20b6d99eda8fcd71e6d3fda3d50afc4c4ebb3692
Liana.Bakradze <liana.bakradze@jetbrains.com>
2018-11-19 20:00:44 +0300

    EDU-1744 CheckiO JS: automatically enable coding assistance for Node

--- Edu-JavaScript/src/com/jetbrains/edu/javascript/learning/checkio/newProject/JsCheckiOCourseProjectGenerator.java
+++ Edu-JavaScript/src/com/jetbrains/edu/javascript/learning/checkio/newProject/JsCheckiOCourseProjectGenerator.java
@@ -1,6 +1,13 @@
 package com.jetbrains.edu.javascript.learning.checkio.newProject;
 
+import com.intellij.javascript.nodejs.interpreter.NodeJsInterpreter;
+import com.intellij.javascript.nodejs.interpreter.NodeJsInterpreterManager;
+import com.intellij.javascript.nodejs.library.core.NodeCoreLibraryConfigurator;
+import com.intellij.javascript.nodejs.settings.NodeSettingsConfigurable;
 import com.intellij.lang.javascript.JavaScriptFileType;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.application.ModalityState;
+import com.intellij.openapi.options.ShowSettingsUtil;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.jetbrains.edu.javascript.learning.JsNewProjectSettings;
@@ -12,6 +19,7 @@ import com.jetbrains.edu.learning.checkio.utils.CheckiOCourseGenerationUtils;
 import com.jetbrains.edu.learning.courseFormat.Course;
 import com.jetbrains.edu.learning.newproject.CourseProjectGenerator;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class JsCheckiOCourseProjectGenerator extends CourseProjectGenerator<JsNewProjectSettings> {
   private final CheckiOCourseContentGenerator myContentGenerator =
@@ -30,5 +38,30 @@ public class JsCheckiOCourseProjectGenerator extends CourseProjectGenerator<JsNe
   @Override
   protected boolean beforeProjectGenerated() {
     return CheckiOCourseGenerationUtils.getCourseFromServerUnderProgress(myContentGenerator, (CheckiOCourse) myCourse);
+  }
+
+  @Override
+  protected void afterProjectGenerated(@NotNull Project project, @NotNull JsNewProjectSettings projectSettings) {
+    VirtualFile requestor = project.getBaseDir();
+    NodeJsInterpreter interpreter = NodeJsInterpreterManager.getInstance(project).getInterpreter();
+    if (interpreter == null) {
+      showSettings(project, requestor);
+    }
+    else {
+      ModalityState modalityState = ModalityState.current();
+      interpreter.provideCachedVersionOrFetch(version -> ApplicationManager.getApplication().invokeLater(() -> {
+        if (version != null) {
+          NodeCoreLibraryConfigurator configurator = NodeCoreLibraryConfigurator.getInstance(project);
+          configurator.configureAndAssociateWithProject(interpreter, version, null);
+        }
+        else {
+          showSettings(project, requestor);
+        }
+      }, modalityState, project.getDisposed()));
+    }
+  }
+
+  private static void showSettings(@NotNull Project project, @Nullable VirtualFile requestor) {
+    ShowSettingsUtil.getInstance().editConfigurable(project, new NodeSettingsConfigurable(project, requestor, true));
   }
 }
