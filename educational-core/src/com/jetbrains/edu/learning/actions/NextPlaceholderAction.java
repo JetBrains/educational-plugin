2c012e76e291535f3ec51606c4d8802a125214d2
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2018-11-21 17:27:29 +0300

    cleanup

--- educational-core/src/com/jetbrains/edu/learning/actions/NextPlaceholderAction.java
+++ educational-core/src/com/jetbrains/edu/learning/actions/NextPlaceholderAction.java
@@ -10,7 +10,7 @@ import org.jetbrains.annotations.Nullable;
 import java.util.List;
 
 /**
- * move caret to next task window
+ * move caret to next answer placeholder
  */
 public class NextPlaceholderAction extends PlaceholderNavigationAction {
   public static final String ACTION_ID = "Educational.NextPlaceholder";
