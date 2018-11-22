03a973792d65fc4e38bde97d20434027be344b62
Liana.Bakradze <liana.bakradze@jetbrains.com>
2018-11-22 12:55:10 +0300

    EDU-1734 CheckiO JS: do not allow to start project without NodeJS interpreter

--- Edu-JavaScript/src/com/jetbrains/edu/javascript/learning/JsLanguageSettings.java
+++ Edu-JavaScript/src/com/jetbrains/edu/javascript/learning/JsLanguageSettings.java
@@ -1,14 +1,53 @@
 package com.jetbrains.edu.javascript.learning;
 
+import com.intellij.javascript.nodejs.interpreter.NodeInterpreterUtil;
+import com.intellij.javascript.nodejs.interpreter.NodeJsInterpreter;
+import com.intellij.javascript.nodejs.interpreter.NodeJsInterpreterField;
+import com.intellij.javascript.nodejs.interpreter.NodeJsInterpreterManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.project.ProjectManager;
+import com.intellij.openapi.ui.LabeledComponent;
 import com.jetbrains.edu.learning.LanguageSettings;
+import com.jetbrains.edu.learning.courseFormat.Course;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Collections;
+import java.util.List;
 
 public class JsLanguageSettings extends LanguageSettings<JsNewProjectSettings> {
   private final JsNewProjectSettings mySettings = new JsNewProjectSettings();
+  private final NodeJsInterpreterField myInterpreterField;
+
+  public JsLanguageSettings() {
+    Project defaultProject = ProjectManager.getInstance().getDefaultProject();
+    myInterpreterField = new NodeJsInterpreterField(defaultProject, false) {
+      @Override
+      public boolean isDefaultProjectInterpreterField() {
+        return true;
+      }
+    };
+    myInterpreterField.setInterpreterRef(NodeJsInterpreterManager.getInstance(defaultProject).getInterpreterRef());
+  }
 
   @NotNull
   @Override
   public JsNewProjectSettings getSettings() {
     return mySettings;
+  }
+
+  @NotNull
+  @Override
+  public List<LabeledComponent<JComponent>> getLanguageSettingsComponents(@NotNull Course course) {
+    return Collections.singletonList(LabeledComponent.create(myInterpreterField, "NodeJS", BorderLayout.WEST));
+  }
+
+  @Nullable
+  @Override
+  public String validate() {
+    NodeJsInterpreter interpreter = myInterpreterField.getInterpreter();
+    return NodeInterpreterUtil.validateAndGetErrorMessage(interpreter);
   }
 }
