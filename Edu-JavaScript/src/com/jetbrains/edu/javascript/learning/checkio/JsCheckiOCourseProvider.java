e1a85bfcbf979813518855ac9faefe7a17a8e46f
Liana Bakradze <liana.bakradze@jetbrains.com>
2018-11-27 11:17:56 +0300

    enable JS support by default

--- Edu-JavaScript/src/com/jetbrains/edu/javascript/learning/checkio/JsCheckiOCourseProvider.java
+++ Edu-JavaScript/src/com/jetbrains/edu/javascript/learning/checkio/JsCheckiOCourseProvider.java
@@ -1,10 +1,8 @@
 package com.jetbrains.edu.javascript.learning.checkio;
 
 import com.intellij.lang.javascript.JavascriptLanguage;
-import com.intellij.openapi.application.Experiments;
 import com.jetbrains.edu.javascript.learning.checkio.utils.JsCheckiONames;
 import com.jetbrains.edu.learning.CoursesProvider;
-import com.jetbrains.edu.learning.EduExperimentalFeatures;
 import com.jetbrains.edu.learning.checkio.courseFormat.CheckiOCourse;
 import com.jetbrains.edu.learning.courseFormat.Course;
 import org.jetbrains.annotations.NotNull;
@@ -16,8 +14,6 @@ public class JsCheckiOCourseProvider implements CoursesProvider {
   @NotNull
   @Override
   public List<Course> loadCourses() {
-    return Experiments.isFeatureEnabled(EduExperimentalFeatures.JAVASCRIPT_COURSES)
-           ? Collections.singletonList(new CheckiOCourse(JsCheckiONames.JS_CHECKIO, JavascriptLanguage.INSTANCE.getID()))
-           : Collections.emptyList();
+    return Collections.singletonList(new CheckiOCourse(JsCheckiONames.JS_CHECKIO, JavascriptLanguage.INSTANCE.getID()));
   }
 }
--- educational-core/src/com/jetbrains/edu/learning/EduExperimentalFeatures.kt
+++ educational-core/src/com/jetbrains/edu/learning/EduExperimentalFeatures.kt
@@ -3,6 +3,5 @@ package com.jetbrains.edu.learning
 object EduExperimentalFeatures {
   const val YAML_FORMAT = "edu.course.creator.yaml"
   const val ANDROID_COURSES = "com.jetbrains.edu.android"
-  const val JAVASCRIPT_COURSES = "edu.checkio.js"
   const val SPLIT_EDITOR = "edu.course.creator.split.editor"
 }
