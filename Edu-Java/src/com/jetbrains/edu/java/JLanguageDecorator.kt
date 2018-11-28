df1ad8a0fac9d52acb273a6588618dcd5f73f782
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2018-11-28 16:48:58 +0300

    Remove redundant methods for `EduLanguageDecorator`.
    Also, drop redundant language decorators

--- Edu-Java/src/com/jetbrains/edu/java/JLanguageDecorator.kt
+++
@@ -1,8 +0,0 @@
-package com.jetbrains.edu.java
-
-import com.jetbrains.edu.learning.EduLanguageDecorator
-
-open class JLanguageDecorator : EduLanguageDecorator {
-  override fun getLanguageScriptUrl(): String = javaClass.classLoader.getResource("/code-mirror/clike.js").toExternalForm()
-  override fun getDefaultHighlightingMode(): String = "text/x-java"
-}
--- Edu-Scala/src/com/jetbrains/edu/scala/ScalaLanguageDecorator.kt
+++
@@ -1,8 +0,0 @@
-package com.jetbrains.edu.scala
-
-import com.jetbrains.edu.learning.EduLanguageDecorator
-
-class ScalaLanguageDecorator : EduLanguageDecorator {
-  override fun getLanguageScriptUrl(): String = javaClass.classLoader.getResource("/code-mirror/clike.js").toExternalForm()
-  override fun getDefaultHighlightingMode(): String = "text/x-scala"
-}
