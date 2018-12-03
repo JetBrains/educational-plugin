0f31b71216778e34343f3bbc439f39f251f1d3e8
Valentina Kiryushkina <valentina.kiryushkina@jetbrains.com>
2018-12-03 18:10:08 +0300

    EDU-1756 Show project notification instead of check button popup

--- educational-core/src/com/jetbrains/edu/learning/stepik/hyperskill/HyperskillCheckListener.kt
+++ educational-core/src/com/jetbrains/edu/learning/stepik/hyperskill/HyperskillCheckListener.kt
@@ -1,20 +1,22 @@
 package com.jetbrains.edu.learning.stepik.hyperskill
 
+import com.intellij.notification.Notification
+import com.intellij.notification.NotificationType
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.ui.MessageType
 import com.jetbrains.edu.learning.checker.CheckListener
 import com.jetbrains.edu.learning.checker.CheckResult
 import com.jetbrains.edu.learning.courseFormat.tasks.Task
 import com.jetbrains.edu.learning.projectView.ProgressUtil
-import com.jetbrains.edu.learning.ui.taskDescription.TaskDescriptionView
 
 class HyperskillCheckListener : CheckListener {
   override fun afterCheck(project: Project, task: Task, result: CheckResult) {
     val course = task.lesson.course
     val (solved, total) = ProgressUtil.countProgress(course)
     if (solved == total) {
-      TaskDescriptionView.getInstance(project).showBalloon("Congratulations! You finished this project. " +
-                                       "Visit <a href=\"https://hyperskill.org/\">Hyperskill</a> to choose new project.", MessageType.INFO)
+      val notification = Notification("hyperskill", "Well done!", "Congratulations! You finished this project. " +
+                                                                  "Visit <a href=\"https://hyperskill.org/\">Hyperskill</a> to choose new project.",
+                                      NotificationType.INFORMATION)
+      notification.notify(project)
     }
   }
 }
