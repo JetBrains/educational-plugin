523f55cf3cd2c45efe6b9344af4b1d5451730b60
Valentina Kiryushkina <valentina.kiryushkina@jetbrains.com>
2018-12-03 18:10:08 +0300

    EDU-1756 Show project notification instead of check button popup for Hyperskill

--- educational-core/src/com/jetbrains/edu/learning/stepik/hyperskill/HyperskillCheckListener.kt
+++ educational-core/src/com/jetbrains/edu/learning/stepik/hyperskill/HyperskillCheckListener.kt
@@ -1,13 +1,13 @@
 package com.jetbrains.edu.learning.stepik.hyperskill
 
+import com.intellij.notification.Notification
+import com.intellij.notification.NotificationType
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.ui.MessageType
 import com.jetbrains.edu.learning.checker.CheckListener
 import com.jetbrains.edu.learning.checker.CheckResult
 import com.jetbrains.edu.learning.courseFormat.tasks.Task
 import com.jetbrains.edu.learning.projectView.ProgressUtil
 import com.jetbrains.edu.learning.stepik.hyperskill.courseFormat.HyperskillCourse
-import com.jetbrains.edu.learning.ui.taskDescription.TaskDescriptionView
 
 class HyperskillCheckListener : CheckListener {
   override fun afterCheck(project: Project, task: Task, result: CheckResult) {
@@ -15,8 +15,10 @@ class HyperskillCheckListener : CheckListener {
     if (course !is HyperskillCourse) return
     val (solved, total) = ProgressUtil.countProgress(course)
     if (solved == total) {
-      TaskDescriptionView.getInstance(project).showBalloon("Congratulations! You finished this project. " +
-                                       "Visit <a href=\"https://hyperskill.org/\">Hyperskill</a> to choose new project.", MessageType.INFO)
+      val notification = Notification("hyperskill", "Well done!", "Congratulations! You finished this project. " +
+                                                                  "Visit <a href=\"https://hyperskill.org/\">Hyperskill</a> to choose new project.",
+                                      NotificationType.INFORMATION)
+      notification.notify(project)
     }
   }
 }
