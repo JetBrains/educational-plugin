ea1c67000d026e6f3f50b9567cc0152cc87765aa
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2018-12-07 15:48:01 +0300

    no need for stepik migration

--- educational-core/src/com/jetbrains/edu/learning/serialization/converter/xml/To12VersionXmlConverter.kt
+++ educational-core/src/com/jetbrains/edu/learning/serialization/converter/xml/To12VersionXmlConverter.kt
@@ -3,15 +3,10 @@ package com.jetbrains.edu.learning.serialization.converter.xml
 import com.jetbrains.edu.learning.coursera.CourseraNames
 import com.jetbrains.edu.learning.serialization.SerializationUtils
 import com.jetbrains.edu.learning.serialization.SerializationUtils.Xml.REMOTE_COURSE
-import com.jetbrains.edu.learning.serialization.SerializationUtils.Xml.SECTION_IDS
 import org.jdom.Element
 
 class To12VersionXmlConverter : BaseXmlConverter() {
   override fun convertCourseElement(course: Element) {
-    val sectionIds = SerializationUtils.Xml.getChildList(course, SECTION_IDS, true)
-    if (sectionIds != null && sectionIds.isEmpty() && course.name == REMOTE_COURSE) {
-      course.name = "StepikCourse"
-    }
     if (course.name == SerializationUtils.COURSE_TITLED || course.name == REMOTE_COURSE) {
       course.name = "EduCourse"
     }
