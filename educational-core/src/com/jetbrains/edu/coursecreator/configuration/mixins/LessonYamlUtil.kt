03368afdbe049e050c46e6a8a890b1c93a7ced76
Valentina Kiryushkina <valentina.kiryushkina@jetbrains.com>
2018-12-07 18:01:34 +0300

    EDU-1788 Add types for lessons in yaml config files

--- educational-core/src/com/jetbrains/edu/coursecreator/configuration/mixins/LessonYamlUtil.kt
+++ educational-core/src/com/jetbrains/edu/coursecreator/configuration/mixins/LessonYamlUtil.kt
@@ -4,20 +4,28 @@ package com.jetbrains.edu.coursecreator.configuration.mixins
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.core.JsonGenerator
+import com.fasterxml.jackson.databind.BeanDescription
+import com.fasterxml.jackson.databind.SerializerProvider
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize
 import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder
 import com.fasterxml.jackson.databind.annotation.JsonSerialize
+import com.fasterxml.jackson.databind.ser.BeanSerializerFactory
+import com.fasterxml.jackson.databind.ser.std.StdSerializer
 import com.jetbrains.edu.coursecreator.configuration.InvalidYamlFormatException
+import com.jetbrains.edu.learning.courseFormat.FrameworkLesson
 import com.jetbrains.edu.learning.courseFormat.Lesson
 import com.jetbrains.edu.learning.courseFormat.tasks.Task
 
 private const val CONTENT = "content"
+private const val TYPE = "content"
 
 @Suppress("UNUSED_PARAMETER", "unused") // used for yaml serialization
 @JsonAutoDetect(getterVisibility = JsonAutoDetect.Visibility.NONE,
                 isGetterVisibility = JsonAutoDetect.Visibility.NONE,
                 fieldVisibility = JsonAutoDetect.Visibility.NONE)
 @JsonDeserialize(builder = LessonBuilder::class)
+@JsonSerialize(using = LessonSerializer::class)
 abstract class LessonYamlMixin {
   @JsonProperty(CONTENT)
   @JsonSerialize(contentConverter = StudyItemConverter::class)
@@ -37,5 +45,21 @@ private class LessonBuilder(@JsonProperty(CONTENT) val content: List<String?>) {
     }
     lesson.updateTaskList(items)
     return lesson
+  }
+}
+
+private class LessonSerializer : StdSerializer<Lesson>(Lesson::class.java){
+  override fun serialize(value: Lesson?, gen: JsonGenerator?, provider: SerializerProvider?) {
+    if (value == null || gen == null || provider == null) {
+      return
+    }
+
+    gen.writeStartObject()
+    gen.writeObjectField(TYPE, if (value is FrameworkLesson) "framework" else "plain")
+    val type = provider.constructType(Lesson::class.java)
+    val beanDescription = provider.config.introspect<BeanDescription>(type)
+    val beanSerializer = BeanSerializerFactory.instance.findBeanSerializer(provider, type, beanDescription)
+    beanSerializer.unwrappingSerializer(null).serialize(value, gen, provider)
+    gen.writeEndObject()
   }
 }
