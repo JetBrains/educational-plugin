655af409df4f1a94e645b577464df8ea699d6037
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2018-12-07 15:51:32 +0300

    EDU-1796: Adapt gradle task checker

--- educational-core/src/com/jetbrains/edu/learning/checker/gradle/GradleEduTaskChecker.kt
+++ educational-core/src/com/jetbrains/edu/learning/checker/gradle/GradleEduTaskChecker.kt
@@ -6,12 +6,16 @@ import com.jetbrains.edu.learning.checker.CheckResult
 import com.jetbrains.edu.learning.checker.CheckResult.Companion.FAILED_TO_CHECK
 import com.jetbrains.edu.learning.checker.TaskChecker
 import com.jetbrains.edu.learning.courseFormat.CheckStatus
+import com.jetbrains.edu.learning.courseFormat.ext.configurator
 import com.jetbrains.edu.learning.courseFormat.tasks.EduTask
 
 open class GradleEduTaskChecker(task: EduTask, project: Project) : TaskChecker<EduTask>(task, project) {
   override fun check(indicator: ProgressIndicator): CheckResult {
     val (taskName, params) = getGradleTask()
-    if (task.testsText.isEmpty()) {
+
+    val testDirs = task.course.configurator?.testDirs ?: return FAILED_TO_CHECK
+    val hasTestFiles = task.taskFiles.any { (path, _) -> testDirs.any { path.startsWith(it) } }
+    if (!hasTestFiles) {
       return CheckResult(CheckStatus.Solved, "Task marked as completed")
     }
 
