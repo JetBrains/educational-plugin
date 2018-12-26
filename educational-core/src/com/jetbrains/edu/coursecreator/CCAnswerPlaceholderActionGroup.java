aa852d34597c73f97a46468deced4d566afca5f2
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2018-12-26 13:51:41 +0300

    cleanup

--- educational-core/src/com/jetbrains/edu/coursecreator/CCAnswerPlaceholderActionGroup.java
+++ educational-core/src/com/jetbrains/edu/coursecreator/CCAnswerPlaceholderActionGroup.java
@@ -3,10 +3,12 @@ package com.jetbrains.edu.coursecreator;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.DefaultActionGroup;
 import com.intellij.openapi.project.DumbAware;
+import org.jetbrains.annotations.NotNull;
 
+@SuppressWarnings("ComponentNotRegistered") // educational-core.xml
 public class CCAnswerPlaceholderActionGroup extends DefaultActionGroup implements DumbAware {
   @Override
-  public void update(AnActionEvent e) {
+  public void update(@NotNull AnActionEvent e) {
     CCUtils.updateActionGroup(e);
   }
 }
