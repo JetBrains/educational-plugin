0ecf9aa2a1cfe80f6de029698838056bebf04010
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2018-12-26 12:24:13 +0300

    EDU-1837: convert CheckActionListener to kotlin. step 2

--- educational-core/testSrc/com/jetbrains/edu/learning/checker/CheckActionListener.kt
+++ educational-core/testSrc/com/jetbrains/edu/learning/checker/CheckActionListener.kt
@@ -1,70 +1,62 @@
-package com.jetbrains.edu.learning.checker;
+package com.jetbrains.edu.learning.checker
 
-import com.intellij.openapi.project.Project;
-import com.jetbrains.edu.learning.courseFormat.CheckStatus;
-import com.jetbrains.edu.learning.courseFormat.tasks.Task;
-import kotlin.Unit;
-import kotlin.jvm.functions.Function1;
-import kotlin.jvm.functions.Function2;
-import org.jetbrains.annotations.NotNull;
-import org.junit.Assert;
+import com.intellij.openapi.project.Project
+import com.jetbrains.edu.learning.courseFormat.CheckStatus
+import com.jetbrains.edu.learning.courseFormat.tasks.Task
+import org.junit.Assert
 
-public class CheckActionListener implements CheckListener {
-    private static final Function2<Task, CheckResult, Unit> SHOULD_FAIL = new Function2<Task, CheckResult, Unit>() {
-        @Override
-        public Unit invoke(@NotNull Task task, @NotNull CheckResult result) {
-            String taskName = getTaskName(task);
-            Assert.assertFalse("Check Task Action skipped for " + taskName, result.getStatus() == CheckStatus.Unchecked);
-            Assert.assertFalse("Check Task Action passed for " + taskName, result.getStatus() == CheckStatus.Solved);
-            System.out.println("Checking status for " + taskName + " fails as expected");
-            return Unit.INSTANCE;
-        }
-    };
+class CheckActionListener : CheckListener {
 
-    private static final Function2<Task, CheckResult, Unit> SHOULD_PASS = new Function2<Task, CheckResult, Unit>() {
-        @Override
-        public Unit invoke(@NotNull Task task, @NotNull CheckResult result) {
-            String taskName = getTaskName(task);
-            Assert.assertFalse("Check Task Action skipped for " + taskName, result.getStatus() == CheckStatus.Unchecked);
-            Assert.assertFalse("Check Task Action failed for " + taskName, result.getStatus() == CheckStatus.Failed);
-            System.out.println("Checking status for " + taskName + " passed");
-            return Unit.INSTANCE;
-        }
-    };
+  override fun afterCheck(project: Project, task: Task, result: CheckResult) {
+    checkStatus.invoke(task, result)
+    if (expectedMessageForTask != null) {
+      val expectedMessage = expectedMessageForTask!!.invoke(task)
+      if (expectedMessage != null) {
+        Assert.assertEquals("Checking output for " + getTaskName(task) + " fails", expectedMessage, result.message)
+      }
+      else {
+        throw IllegalStateException(String.format("Unexpected task `%s`", task.name))
+      }
 
-    @NotNull
-    private static String getTaskName(@NotNull Task task) {
-        return task.getLesson().getName() + "/" + task.getName();
+    }
+  }
+
+  companion object {
+    private val SHOULD_FAIL = fun(task: Task, result: CheckResult): Unit {
+      val taskName = getTaskName(task)
+      Assert.assertFalse("Check Task Action skipped for $taskName", result.status == CheckStatus.Unchecked)
+      Assert.assertFalse("Check Task Action passed for $taskName", result.status == CheckStatus.Solved)
+      println("Checking status for $taskName fails as expected")
+      return Unit
+    }
+
+    private val SHOULD_PASS: Function2<Task, CheckResult, Unit> = fun(task: Task, result: CheckResult): Unit {
+      val taskName = getTaskName(task)
+      Assert.assertFalse("Check Task Action skipped for $taskName", result.status == CheckStatus.Unchecked)
+      Assert.assertFalse("Check Task Action failed for $taskName", result.status == CheckStatus.Failed)
+      println("Checking status for $taskName passed")
+      return Unit
+    }
+
+    private fun getTaskName(task: Task): String {
+      return task.lesson.name + "/" + task.name
     }
 
     // Those fields can be modified if some special checks are needed (return true if should run standard checks)
-    private static Function2<Task, CheckResult, Unit> checkStatus = SHOULD_PASS;
-    private static Function1<Task, String> expectedMessageForTask = null;
+    private var checkStatus = SHOULD_PASS
+    private var expectedMessageForTask: Function1<Task, String>? = null
 
-    @Override
-    public void afterCheck(@NotNull Project project, @NotNull Task task, @NotNull CheckResult result) {
-        checkStatus.invoke(task, result);
-        if (expectedMessageForTask != null) {
-            String expectedMessage = expectedMessageForTask.invoke(task);
-            if (expectedMessage != null) {
-                Assert.assertEquals("Checking output for " + getTaskName(task) + " fails", expectedMessage, result.getMessage());
-            } else {
-                throw new IllegalStateException(String.format("Unexpected task `%s`", task.getName()));
-            }
-
-        }
+    fun reset() {
+      checkStatus = SHOULD_PASS
+      expectedMessageForTask = null
     }
 
-    public static void reset() {
-        checkStatus = SHOULD_PASS;
-        expectedMessageForTask = null;
+    fun shouldFail() {
+      checkStatus = SHOULD_FAIL
     }
 
-    public static void shouldFail() {
-        checkStatus = SHOULD_FAIL;
+    fun expectedMessage(f: Function1<Task, String>) {
+      expectedMessageForTask = f
     }
-
-    public static void expectedMessage(@NotNull Function1<Task, String> f) {
-        expectedMessageForTask = f;
-    }
+  }
 }
