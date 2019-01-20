bcc182a3f705a7cd484db9a8703df0f3fbb55f7e
Ekaterina Tuzova <ekaterina.tuzova@jetbrains.com>
2019-01-20 17:06:00 +0300

    stepik course comment

--- educational-core/src/com/jetbrains/edu/learning/stepik/course/StepikCourse.kt
+++ educational-core/src/com/jetbrains/edu/learning/stepik/course/StepikCourse.kt
@@ -3,6 +3,12 @@ package com.jetbrains.edu.learning.stepik.course
 import com.intellij.util.xmlb.XmlSerializer
 import com.jetbrains.edu.learning.courseFormat.EduCourse
 
+/**
+ * Specific stepik course created via `StartStepikCourseAction`.
+ * This course can't be open with "View as Educator".
+ * We do not push this kind of courses to the stepik.
+ * Stepik courses do not contain pycharm tasks.
+ */
 class StepikCourse : EduCourse()
 
 fun stepikCourseFromRemote(remoteCourse: EduCourse?): StepikCourse? {
