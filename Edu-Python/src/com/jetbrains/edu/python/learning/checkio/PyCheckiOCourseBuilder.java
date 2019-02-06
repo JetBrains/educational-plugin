22e1f029fbebfd4df15d248a435f5c6d0d21debb
Liana Bakradze <liana.bakradze@jetbrains.com>
2019-02-06 21:19:38 +0300

    use language version for Py CheckiO course

--- Edu-Python/src/com/jetbrains/edu/python/learning/checkio/PyCheckiOCourseBuilder.java
+++ Edu-Python/src/com/jetbrains/edu/python/learning/checkio/PyCheckiOCourseBuilder.java
@@ -1,6 +1,5 @@
 package com.jetbrains.edu.python.learning.checkio;
 
-import com.jetbrains.edu.learning.LanguageSettings;
 import com.jetbrains.edu.learning.courseFormat.Course;
 import com.jetbrains.edu.learning.newproject.CourseProjectGenerator;
 import com.jetbrains.edu.python.learning.PyCourseBuilder;
@@ -20,12 +19,6 @@ public class PyCheckiOCourseBuilder extends PyCourseBuilder {
   @Override
   public String getTestTemplateName() {
     return null;
-  }
-
-  @NotNull
-  @Override
-  public LanguageSettings<PyNewProjectSettings> getLanguageSettings() {
-    return new PyCheckiOLanguageSettings();
   }
 
   @Nullable
--- Edu-Python/src/com/jetbrains/edu/python/learning/checkio/PyCheckiOCourseProvider.java
+++ Edu-Python/src/com/jetbrains/edu/python/learning/checkio/PyCheckiOCourseProvider.java
@@ -5,6 +5,7 @@ import com.jetbrains.edu.learning.EduNames;
 import com.jetbrains.edu.learning.checkio.courseFormat.CheckiOCourse;
 import com.jetbrains.edu.learning.courseFormat.Course;
 import com.jetbrains.edu.python.learning.checkio.utils.PyCheckiONames;
+import com.jetbrains.edu.python.learning.newproject.PyLanguageSettings;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Collections;
@@ -14,6 +15,6 @@ public class PyCheckiOCourseProvider implements CoursesProvider {
   @NotNull
   @Override
   public List<Course> loadCourses() {
-    return Collections.singletonList(new CheckiOCourse(PyCheckiONames.PY_CHECKIO, EduNames.PYTHON));
+    return Collections.singletonList(new CheckiOCourse(PyCheckiONames.PY_CHECKIO, EduNames.PYTHON + " " + PyLanguageSettings.PYTHON_3));
   }
 }
