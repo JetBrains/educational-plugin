677f59d9b2d3d44b169da3ad9472b81247008a78
Ekaterina Tuzova <ekaterina.tuzova@jetbrains.com>
2019-02-12 14:39:32 +0300

    show hyperskill widget only in study projects

--- educational-core/src/com/jetbrains/edu/learning/stepik/hyperskill/HyperskillWidgetProvider.kt
+++ educational-core/src/com/jetbrains/edu/learning/stepik/hyperskill/HyperskillWidgetProvider.kt
@@ -4,8 +4,15 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.StatusBar
 import com.intellij.openapi.wm.StatusBarWidget
 import com.intellij.openapi.wm.StatusBarWidgetProvider
+import com.jetbrains.edu.learning.EduUtils
 
 class HyperskillWidgetProvider : StatusBarWidgetProvider {
-  override fun getWidget(project: Project): StatusBarWidget = HyperskillWidget()
+  override fun getWidget(project: Project): StatusBarWidget? {
+    if (EduUtils.isStudyProject(project)) {
+      return HyperskillWidget()
+    }
+    return null
+  }
+
   override fun getAnchor(): String  = StatusBar.Anchors.before(StatusBar.StandardWidgets.POSITION_PANEL)
 }
