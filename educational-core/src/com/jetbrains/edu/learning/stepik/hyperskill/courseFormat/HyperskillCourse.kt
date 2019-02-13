373b74fc12aae405254b5aca68a61a3a95af1609
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2019-02-13 19:52:24 +0300

    EDU-1938: hardcode language version for hyperskill courses
    It allow us to correctly show error when user tries to use wrong jdk.
    But it should be changed in future to support language versions for other languages

--- educational-core/src/com/jetbrains/edu/learning/stepik/hyperskill/courseFormat/HyperskillCourse.kt
+++ educational-core/src/com/jetbrains/edu/learning/stepik/hyperskill/courseFormat/HyperskillCourse.kt
@@ -22,5 +22,9 @@ class HyperskillCourse : Course {
     courseType = HYPERSKILL
   }
 
+  // temporary solution for hyperskill java courses
+  // TODO: store language version on server side
+  override fun getLanguageVersion(): String = "11"
+
   private fun descriptionNote(projectId: Int): String = "<br/><br/>Learn more at <a href=\"https://hyperskill.org\">https://hyperskill.org/projects/${projectId}</a>"
 }
