ae3f2ea224f5b7bb41a104ca20a00588b8127354
Ekaterina Tuzova <ekaterina.tuzova@jetbrains.com>
2019-02-25 10:44:06 +0300

    EDU-1989 Show notification when we fail to post submission to the hyperskill
    (cherry picked from commit c98453d2919a16250366ed49b12fa544b39f2acd)

--- educational-core/src/com/jetbrains/edu/learning/stepik/hyperskill/HyperskillUtils.kt
+++ educational-core/src/com/jetbrains/edu/learning/stepik/hyperskill/HyperskillUtils.kt
@@ -1,6 +1,8 @@
 package com.jetbrains.edu.learning.stepik.hyperskill
 
 import com.intellij.lang.Language
+import com.intellij.notification.Notification
+import com.intellij.notification.NotificationType
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Key
 import com.jetbrains.edu.learning.EduUtils
@@ -10,6 +12,7 @@ import com.jetbrains.edu.learning.navigation.NavigationUtils
 import com.jetbrains.edu.learning.stepik.hyperskill.courseFormat.HyperskillCourse
 
 val HYPERSKILL_STAGE: Key<Int> = Key.create("HYPERSKILL_STAGE")
+const val HYPERSKILL_GROUP_ID = "Hyperskill.post"
 
 fun openSelectedStage(course: Course, project: Project) {
   val stageId = course.getUserData(HYPERSKILL_STAGE) ?: return
@@ -28,4 +31,10 @@ fun openSelectedStage(course: Course, project: Project) {
 
 fun isHyperskillSupportAvailable(): Boolean {
   return Language.findLanguageByID("JAVA") != null && !EduUtils.isAndroidStudio()
+}
+
+fun showFailedToPostNotification() {
+  val notification = Notification(HYPERSKILL_GROUP_ID, "Failed to post submission to the Hyperskill",
+                                  "Please, try to check again", NotificationType.WARNING)
+  notification.notify(null)
 }
