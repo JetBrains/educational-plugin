dbaa525bd0151f4b2071c565ed016d3a7761f50c
Ekaterina Tuzova <ekaterina.tuzova@jetbrains.com>
2019-03-10 12:45:00 +0300

    remove Hyperskill from the experimental features

--- educational-core/src/com/jetbrains/edu/learning/EduExperimentalFeatures.kt
+++ educational-core/src/com/jetbrains/edu/learning/EduExperimentalFeatures.kt
@@ -4,5 +4,4 @@ object EduExperimentalFeatures {
   const val YAML_FORMAT = "edu.course.creator.yaml"
   const val ANDROID_COURSES = "com.jetbrains.edu.android"
   const val SPLIT_EDITOR = "edu.course.creator.split.editor"
-  const val HYPERSKILL = "edu.course.hyperskill"
 }
