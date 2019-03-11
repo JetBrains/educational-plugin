b3ce6dc241d1b6b683bef372262e3547cbfc5a0c
zinaida.smirnova <zinaida.smirnova@jetbrains.com>
2019-03-11 12:35:12 +0300

    EDU-1311 out coming message united into single string, "+" removed

--- educational-core/src/com/jetbrains/edu/learning/checker/gradle/runGradleUtils.kt
+++ educational-core/src/com/jetbrains/edu/learning/checker/gradle/runGradleUtils.kt
@@ -147,8 +147,7 @@ fun runGradleRunTask(
   mainClassForFile: (Project, VirtualFile) -> String?
 ): Result<String, CheckResult> {
   val mainClassName = findMainClass(project, task, mainClassForFile)
-                      ?: return Err(CheckResult(CheckStatus.Unchecked, "Unable to execute task ${task.name}, " +
-                                                                       "main method is missing"))
+                      ?: return Err(CheckResult(CheckStatus.Unchecked, "Unable to execute task ${task.name}, main method is missing"))
   val taskName = if (task.hasSeparateModule(project)) "${getGradleProjectName(task)}:run" else "run"
 
   val gradleOutput = GradleCommandLine.create(project, taskName, "${MAIN_CLASS_PROPERTY_PREFIX}$mainClassName")
