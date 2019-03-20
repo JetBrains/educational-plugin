341b0ceda5aded297e346345a7e56a8c646b5705
Valentina Kiryushkina <valentina.kiryushkina@jetbrains.com>
2019-03-20 15:18:03 +0300

    EDU-1980 Validate Edu and Stepik course properly
    * For `EduCourse` we have to check compatibility with current plugin version for them,
    see `courseCompatibility` to see what compatibility means
    
    * For `StepikCourse` we have to check if there are supported languages for this course
    and set courseType and language fields

--- educational-core/src/com/jetbrains/edu/learning/stepik/course/StartStepikCourseAction.kt
+++ educational-core/src/com/jetbrains/edu/learning/stepik/course/StartStepikCourseAction.kt
@@ -9,6 +9,8 @@ import com.jetbrains.edu.learning.EduSettings
 import com.jetbrains.edu.learning.EduUtils
 import com.jetbrains.edu.learning.JSON_FORMAT_VERSION
 import com.jetbrains.edu.learning.courseFormat.Course
+import com.jetbrains.edu.learning.courseFormat.CourseCompatibility.*
+import com.jetbrains.edu.learning.courseFormat.EduCourse
 import com.jetbrains.edu.learning.newproject.ui.JoinCourseDialog
 import com.jetbrains.edu.learning.stepik.StepikAuthorizer
 import com.jetbrains.edu.learning.stepik.StepikNames
@@ -41,30 +43,57 @@ class StartStepikCourseAction : DumbAwareAction("Start Stepik Course") {
       return null
     }
 
-    // course language is already set if we opened idea compatible course by link
-    if (course.isCompatible) {
+    if (course is StepikCourse) {
+      val language = getLanguageForStepikCourse(course)
+      language?.let {
+        course.type = "${StepikNames.PYCHARM_PREFIX}${JSON_FORMAT_VERSION} ${language.id}"
+        course.language = language.id
+        return course
+      }
+    }
+    else if (isCompatibleEduCourse(course)) {
       return course
     }
 
-    private fun showDialogAndGetCourseLink(): String? {
-      val dialog = ImportStepikCourseDialog()
-      if (!dialog.showAndGet()) {
-        return null
-      }
-      return dialog.courseLink()
-    }
+    return null
+  }
 
+  private fun getLanguageForStepikCourse(course: StepikCourse): Language? {
     val languages = getLanguagesUnderProgress(course)
 
     if (languages.isEmpty()) {
-      Messages.showErrorDialog("No supported languages available for the course", "Failed to Import Course")
+      Messages.showErrorDialog("No supported languages available for '${course.name}'", "Failed to Import Course")
       return null
     }
-    val language = chooseLanguageIfNeeded(languages, course) ?: return null
-    course.type = "${StepikNames.PYCHARM_PREFIX}${JSON_FORMAT_VERSION} ${language.id}"
-    course.language = language.id
-    return course
+
+    return chooseLanguageIfNeeded(languages, course)
   }
+
+  private fun showDialogAndGetCourseLink(): String? {
+    val dialog = ImportStepikCourseDialog()
+    if (!dialog.showAndGet()) {
+      return null
+    }
+    return dialog.courseLink()
+  }
+
+  private fun isCompatibleEduCourse(course: EduCourse): Boolean {
+    return when (course.compatibility) {
+      COMPATIBLE -> true
+
+      UNSUPPORTED -> {
+        showFailedImportCourseMessage("Looks like the programming language of '${course.name}' is not supported yet.")
+        false
+      }
+
+      INCOMPATIBLE_VERSION -> {
+        showFailedImportCourseMessage("'${course.name}' is supported in the latest plugin version only. Please, update the plugin.")
+        false
+      }
+    }
+  }
+
+  private fun showFailedImportCourseMessage(message: String) = Messages.showErrorDialog(message, "Failed to Import Course")
 
   private fun chooseLanguageIfNeeded(languages: List<Language>, course: StepikCourse): Language? {
     return if (languages.size == 1) {
