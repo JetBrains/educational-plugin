073a9d751807b0338340db93876af9cab1df3d89
Valentina Kiryushkina <valentina.kiryushkina@jetbrains.com>
2019-03-28 19:27:36 +0300

    Rename .java to .kt

--- educational-core/src/com/jetbrains/edu/coursecreator/CCProjectComponent.kt
+++ educational-core/src/com/jetbrains/edu/coursecreator/CCProjectComponent.kt
@@ -1,94 +1,82 @@
-package com.jetbrains.edu.coursecreator;
+package com.jetbrains.edu.coursecreator
 
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.components.ProjectComponent;
-import com.intellij.openapi.editor.EditorFactory;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vfs.VirtualFileManager;
-import com.jetbrains.edu.coursecreator.handlers.CCVirtualFileListener;
-import com.jetbrains.edu.coursecreator.yaml.YamlFormatSynchronizer;
-import com.jetbrains.edu.coursecreator.yaml.YamlLoader;
-import com.jetbrains.edu.learning.CourseSetListener;
-import com.jetbrains.edu.learning.StudyTaskManager;
-import com.jetbrains.edu.learning.courseFormat.Course;
-import com.jetbrains.edu.learning.statistics.EduUsagesCollector;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.components.ProjectComponent
+import com.intellij.openapi.editor.EditorFactory
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFileManager
+import com.jetbrains.edu.coursecreator.handlers.CCVirtualFileListener
+import com.jetbrains.edu.coursecreator.yaml.YamlFormatSettings.isEduYamlProject
+import com.jetbrains.edu.coursecreator.yaml.YamlFormatSynchronizer
+import com.jetbrains.edu.coursecreator.yaml.YamlLoader
+import com.jetbrains.edu.learning.CourseSetListener
+import com.jetbrains.edu.learning.StudyTaskManager
+import com.jetbrains.edu.learning.courseFormat.Course
+import com.jetbrains.edu.learning.statistics.EduUsagesCollector
 
-import static com.jetbrains.edu.coursecreator.yaml.YamlFormatSettings.isEduYamlProject;
-@SuppressWarnings("ComponentNotRegistered") // educational-core.xml
-public class CCProjectComponent implements ProjectComponent {
+@Suppress("ComponentNotRegistered") // educational-core.xml
+class CCProjectComponent(private val myProject: Project) : ProjectComponent {
+  private var myTaskFileLifeListener: CCVirtualFileListener? = null
 
-  private CCVirtualFileListener myTaskFileLifeListener;
-  private final Project myProject;
-
-  protected CCProjectComponent(Project project) {
-    myProject = project;
+  private fun startTaskDescriptionFilesSynchronization() {
+    StudyTaskManager.getInstance(myProject).course ?: return
+    EditorFactory.getInstance().eventMulticaster.addDocumentListener(SynchronizeTaskDescription(myProject), myProject)
   }
 
-  private void startTaskDescriptionFilesSynchronization() {
-    Course course = StudyTaskManager.getInstance(myProject).getCourse();
-    if (course == null) {
-      return;
-    }
-    EditorFactory.getInstance().getEventMulticaster().addDocumentListener(new SynchronizeTaskDescription(myProject), myProject);
+  override fun getComponentName(): String {
+    return "CCProjectComponent"
   }
 
-  @NotNull
-  public String getComponentName() {
-    return "CCProjectComponent";
-  }
-
-  public void projectOpened() {
+  override fun projectOpened() {
     // it is also false for newly created courses as config files isn't created yet.
     // it's ok as we don't need to load course from config
-    if (isEduYamlProject(myProject)) {
-      loadCourse();
+    if (myProject.isEduYamlProject()) {
+      loadCourse()
     }
 
-    if (StudyTaskManager.getInstance(myProject).getCourse() != null) {
-      initCCProject();
+    if (StudyTaskManager.getInstance(myProject).course != null) {
+      initCCProject()
     }
     else {
-      myProject.getMessageBus().connect().subscribe(StudyTaskManager.COURSE_SET, new CourseSetListener() {
-        @Override
-        public void courseSet(@NotNull Course course) {
+      myProject.messageBus.connect().subscribe(StudyTaskManager.COURSE_SET, object : CourseSetListener {
+        override fun courseSet(course: Course) {
           // in case course was reset from StudyTaskManager
-          if (isEduYamlProject(myProject)) {
-            loadCourse();
+          if (myProject.isEduYamlProject()) {
+            loadCourse()
           }
-          initCCProject();
+          initCCProject()
         }
-      });
+      })
     }
   }
 
-  private void initCCProject() {
+  private fun initCCProject() {
     if (CCUtils.isCourseCreator(myProject)) {
-      if (!ApplicationManager.getApplication().isUnitTestMode()) {
-        registerListener();
+      if (!ApplicationManager.getApplication().isUnitTestMode) {
+        registerListener()
       }
 
-      EduUsagesCollector.projectTypeOpened(CCUtils.COURSE_MODE);
-      startTaskDescriptionFilesSynchronization();
-      YamlFormatSynchronizer.startSynchronization(myProject);
+      EduUsagesCollector.projectTypeOpened(CCUtils.COURSE_MODE)
+      startTaskDescriptionFilesSynchronization()
+      YamlFormatSynchronizer.startSynchronization(myProject)
     }
   }
 
-  private void loadCourse() {
-    Course course = YamlLoader.loadCourseRecursively(myProject);
-    StudyTaskManager.getInstance(myProject).setCourse(course);
+  private fun loadCourse() {
+    val course = YamlLoader.loadCourseRecursively(myProject)
+    StudyTaskManager.getInstance(myProject).course = course
   }
 
-  public void registerListener() {
+  private fun registerListener() {
     if (myTaskFileLifeListener == null) {
-      myTaskFileLifeListener = new CCVirtualFileListener(myProject);
-      VirtualFileManager.getInstance().addVirtualFileListener(myTaskFileLifeListener);
+      myTaskFileLifeListener = CCVirtualFileListener(myProject)
+      VirtualFileManager.getInstance().addVirtualFileListener(myTaskFileLifeListener!!)
     }
   }
 
-  public void projectClosed() {
+  override fun projectClosed() {
     if (myTaskFileLifeListener != null) {
-      VirtualFileManager.getInstance().removeVirtualFileListener(myTaskFileLifeListener);
+      VirtualFileManager.getInstance().removeVirtualFileListener(myTaskFileLifeListener!!)
     }
   }
 }
