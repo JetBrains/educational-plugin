33534731322d08c68f648b4e3dfe66d2a93f1005
Ekaterina Tuzova <ekaterina.tuzova@jetbrains.com>
2019-04-09 16:47:43 +0300

    EDU-1949 Confusing note in course description for non-featured courses

--- educational-core/src/com/jetbrains/edu/learning/newproject/ui/CoursePanel.java
+++ educational-core/src/com/jetbrains/edu/learning/newproject/ui/CoursePanel.java
@@ -41,9 +41,9 @@ import java.util.List;
 public class CoursePanel extends JPanel {
   private static final String ourNotVerifiedNote = "\n\nNote: This course has not been verified by the JetBrains team. " +
                                                    "If you want to join it, keep in mind we are not responsible for the content provided. " +
-                                                   "If you are the owner of the course and you want it to be listed as " +
-                                                   "a featured course, please <a href=\"mailto:academy@jetbrains.com\">get in touch with us</a> " +
-                                                   "and we would be glad to verify this with you.";
+                                                   "If you are the owner of the course and you want it to be " +
+                                                   "featured, please <a href=\"mailto:academy@jetbrains.com\">get in touch with us</a> " +
+                                                   "and we would be glad to verify it with you.";
 
   private static final int HORIZONTAL_MARGIN = 10;
   private static final int LARGE_HORIZONTAL_MARGIN = 15;
