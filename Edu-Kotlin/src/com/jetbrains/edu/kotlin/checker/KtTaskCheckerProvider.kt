85ec30e257a94fe736731adce90ed494a3135e84
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2019-04-12 11:42:45 +0300

    EDU-1754: new gradle edu task checker for Kotlin courses

--- Edu-Kotlin/src/com/jetbrains/edu/kotlin/checker/KtTaskCheckerProvider.kt
+++ Edu-Kotlin/src/com/jetbrains/edu/kotlin/checker/KtTaskCheckerProvider.kt
@@ -1,27 +1,32 @@
 package com.jetbrains.edu.kotlin.checker
 
-import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.application.runReadAction
+import com.intellij.execution.RunnerAndConfigurationSettings
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiManager
 import com.intellij.psi.util.PsiTreeUtil
-import com.jetbrains.edu.jvm.gradle.checker.*
+import com.jetbrains.edu.jvm.gradle.checker.GradleTaskCheckerProvider
+import com.jetbrains.edu.jvm.gradle.checker.NewGradleEduTaskChecker
+import com.jetbrains.edu.jvm.gradle.checker.hasSeparateModule
+import com.jetbrains.edu.learning.checker.TaskChecker
 import com.jetbrains.edu.learning.courseFormat.ext.findTestDirs
 import com.jetbrains.edu.learning.courseFormat.tasks.EduTask
 import org.jetbrains.kotlin.idea.run.KotlinJUnitRunConfigurationProducer
 import org.jetbrains.kotlin.idea.run.KotlinRunConfigurationProducer
 import org.jetbrains.kotlin.psi.KtElement
+import org.jetbrains.plugins.gradle.service.execution.GradleRunConfiguration
 
 class KtTaskCheckerProvider : GradleTaskCheckerProvider() {
 
-  override fun getEduTaskChecker(task: EduTask, project: Project): GradleEduTaskChecker {
-    return object : GradleEduTaskChecker(task, project) {
-
-      override fun getGradleTask(): GradleTask {
-        if (task.hasSeparateModule(project)) {
-          return super.getGradleTask()
+  override fun getEduTaskChecker(task: EduTask, project: Project): TaskChecker<EduTask> {
+    return object : NewGradleEduTaskChecker(task, project) {
+      override fun createTestConfigurations(): List<RunnerAndConfigurationSettings> {
+        val testConfigurations = super.createTestConfigurations()
+        if (task.hasSeparateModule(project) || testConfigurations.size != 1) {
+          return testConfigurations
         }
+
+        val configuration = testConfigurations.single().configuration as? GradleRunConfiguration ?: return emptyList()
 
         val testDirs = task.findTestDirs(project)
         check(testDirs.isNotEmpty()) {
@@ -29,24 +34,14 @@ class KtTaskCheckerProvider : GradleTaskCheckerProvider() {
         }
 
         val testClasses: List<String> = testDirs.flatMap { testDir ->
-          var tests: List<String>? = null
-          ApplicationManager.getApplication().invokeAndWait {
-             tests = runReadAction {
-              testDir.children.mapNotNull {
-                val psiFile = PsiManager.getInstance(project).findFile(it) ?: return@mapNotNull null
-                KotlinJUnitRunConfigurationProducer.getTestClass(psiFile)?.qualifiedName
-              }
+            testDir.children.mapNotNull {
+              val psiFile = PsiManager.getInstance(project).findFile(it) ?: return@mapNotNull null
+              KotlinJUnitRunConfigurationProducer.getTestClass(psiFile)?.qualifiedName
             }
-          }
-          tests ?: emptyList()
         }
 
-        return if (testClasses.isEmpty()) {
-          LOG.warn("Can't find any test class. Check course project is compilable")
-          GradleTask(ASSEMBLE_TASK_NAME)
-        } else {
-          GradleTask(TEST_TASK_NAME, testClasses.flatMap { listOf(TESTS_ARG, it) })
-        }
+        configuration.settings.scriptParameters = testClasses.joinToString(" ") { "--tests $it" }
+        return testConfigurations
       }
     }
   }
