0d8aea0cc56f6b3bbcf4d8bc72e6e5f3809f77d9
Damir Shagaev <damir.shagaev@jetbrains.com>
2019-05-07 19:36:26 +0300

    EDU-2104: getConfiguration() has been fixed

--- Edu-Cpp/src/com/jetbrains/edu/cpp/checker/CppTheoryTaskChecker.kt
+++ Edu-Cpp/src/com/jetbrains/edu/cpp/checker/CppTheoryTaskChecker.kt
@@ -1,19 +1,18 @@
 package com.jetbrains.edu.cpp.checker
 
 import com.intellij.execution.ProgramRunnerUtil
-import com.intellij.execution.RunManagerEx
 import com.intellij.execution.RunnerAndConfigurationSettings
 import com.intellij.execution.actions.ConfigurationContext
-import com.intellij.execution.configurations.ConfigurationType
 import com.intellij.execution.executors.DefaultRunExecutor
 import com.intellij.ide.DataManager
 import com.intellij.openapi.application.runInEdt
 import com.intellij.openapi.application.runReadAction
 import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.project.Project
-import com.intellij.util.containers.ContainerUtil
-import com.jetbrains.cidr.cpp.cmake.model.CMakeTarget
+import com.intellij.psi.util.PsiTreeUtil
 import com.jetbrains.cidr.execution.CidrTargetRunConfigurationProducer
+import com.jetbrains.cidr.execution.CidrTargetRunLineMarkerProvider
+import com.jetbrains.cidr.lang.psi.OCFunctionDeclaration
 import com.jetbrains.edu.learning.EduUtils
 import com.jetbrains.edu.learning.checker.CheckResult
 import com.jetbrains.edu.learning.checker.TheoryTaskChecker
@@ -23,34 +22,23 @@ import com.jetbrains.edu.learning.courseFormat.tasks.TheoryTask
 class CppTheoryTaskChecker(task: TheoryTask, project: Project) : TheoryTaskChecker(task, project) {
 
   override fun check(indicator: ProgressIndicator): CheckResult {
-    val configuration = runReadAction { getConfiguration() } ?:
-                        return CheckResult(CheckStatus.Unchecked, "No run configurations for this file.\n\n" +
-                                                                  "Please run the source code and then click the `Run` button again.")
+    val configuration = runReadAction { getConfiguration() } ?: return CheckResult(CheckStatus.Unchecked, "No run configurations for this file.\n\n" +
+                                                                                        "Please run the source code and then click the `Run` button again.")
     runInEdt {
       ProgramRunnerUtil.executeConfiguration(configuration, DefaultRunExecutor.getRunExecutorInstance())
     }
     return CheckResult(CheckStatus.Solved, "")
   }
 
-  private fun getTarget(): CMakeTarget? {
+  private fun getConfiguration(): RunnerAndConfigurationSettings? {
     val editor = EduUtils.getSelectedEditor(project) ?: return null
     val dataContext = DataManager.getInstance().getDataContext(editor.component)
     val context = ConfigurationContext.getFromContext(dataContext)
     val location = context.location ?: return null
-    val targets = CidrTargetRunConfigurationProducer.getInstance(project)?.getExecutableTargetsForFile(location.psiElement.containingFile)
-    if (targets == null || targets.isEmpty()) return null
-    return targets.first() as CMakeTarget
-  }
-
-  private fun getConfiguration(): RunnerAndConfigurationSettings? {
-    val runManager = RunManagerEx.getInstanceEx(project)
-    val cmakeTarget = getTarget() ?: return null
-    val configs = ContainerUtil.flatten(
-        ContainerUtil.map<ConfigurationType, List<RunnerAndConfigurationSettings>>(
-          ConfigurationType.CONFIGURATION_TYPE_EP.extensionList) {
-          runManager.getConfigurationSettingsList(it)
-        }
-    )
-    return configs.find { it.configuration.name == cmakeTarget.name }
+    val functions = PsiTreeUtil.findChildrenOfType(location.psiElement.containingFile, OCFunctionDeclaration::class.java)
+    val mainFunction = functions.find { CidrTargetRunLineMarkerProvider.isInEntryPointBody(it) } ?: return null
+    val fromContext = CidrTargetRunConfigurationProducer.getInstance(project)
+      ?.findOrCreateConfigurationFromContext(ConfigurationContext(mainFunction))
+    return fromContext?.configurationSettings
   }
 }
