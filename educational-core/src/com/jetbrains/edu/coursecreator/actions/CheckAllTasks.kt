991efa6b528954473504fcf0cfa71cab4690815a
Liana Bakradze <liana.bakradze@jetbrains.com>
2019-05-08 18:16:28 +0300

    EDU-1436 UI to show info about failed tasks

--- educational-core/src/com/jetbrains/edu/coursecreator/actions/CheckAllTasks.kt
+++ educational-core/src/com/jetbrains/edu/coursecreator/actions/CheckAllTasks.kt
@@ -1,25 +1,30 @@
 package com.jetbrains.edu.coursecreator.actions
 
 import com.google.common.annotations.VisibleForTesting
+import com.intellij.notification.Notification
+import com.intellij.notification.NotificationListener
+import com.intellij.notification.NotificationType
+import com.intellij.notification.Notifications
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.application.runInEdt
 import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.progress.ProgressManager
-import com.intellij.openapi.progress.Task
-import com.intellij.openapi.ui.Messages
+import com.intellij.openapi.project.Project
 import com.jetbrains.edu.learning.StudyTaskManager
 import com.jetbrains.edu.learning.courseFormat.CheckStatus
 import com.jetbrains.edu.learning.courseFormat.ext.configurator
+import com.jetbrains.edu.learning.courseFormat.tasks.Task
+import com.jetbrains.edu.learning.navigation.NavigationUtils
+import javax.swing.event.HyperlinkEvent
 
 class CheckAllTasks : AnAction("Check All Tasks") {
   override fun actionPerformed(e: AnActionEvent) {
     val project = e.project ?: return
     val course = StudyTaskManager.getInstance(project).course ?: return
 
-    ProgressManager.getInstance().run(object : Task.Backgroundable(project, "Checking all tasks...", true) {
+    ProgressManager.getInstance().run(object : com.intellij.openapi.progress.Task.Backgroundable(project, "Checking all tasks...", true) {
       override fun run(indicator: ProgressIndicator) {
-        var hasFailedTasks = false
+        val failedTasks = mutableListOf<Task>()
         var curTask = 0
         var tasksNum = 0
         course.visitTasks { tasksNum++ }
@@ -32,19 +37,48 @@ class CheckAllTasks : AnAction("Check All Tasks") {
           val checker = course.configurator?.taskCheckerProvider?.getTaskChecker(it, project)!!
           indicator.text = "Checking task $curTask/$tasksNum"
           if (checker.check(indicator).status != CheckStatus.Solved) {
-            hasFailedTasks = true
+            failedTasks.add(it)
           }
           checker.clearState()
         }
         if (indicator.isCanceled) {
           return
         }
-        runInEdt {
-          Messages.showInfoMessage(if (hasFailedTasks) FAILED_MESSAGE else SUCCESS_MESSAGE, "Check Finished")
+        val notification = if (failedTasks.isEmpty()) {
+          Notification(GROUP_ID, "Check Finished", SUCCESS_MESSAGE, NotificationType.INFORMATION)
         }
+        else {
+          createFailedTasksNotification(failedTasks, tasksNum, project)
+        }
+        Notifications.Bus.notify(notification, project)
       }
     })
   }
+
+  private fun createFailedTasksNotification(failedTasks: MutableList<Task>, tasksNum: Int, project: Project): Notification {
+    val subtitle = "${failedTasks.size} of $tasksNum tasks failed"
+    val tasksList = failedTasks.withIndex().joinToString("<br>") { "<a href=\"${it.index}\">${it.value.fullName}</a>" }
+    val notification = Notification(GROUP_ID, null, "Check", subtitle, tasksList, NotificationType.WARNING,
+                                    object : NotificationListener.Adapter() {
+                                      override fun hyperlinkActivated(notification: Notification, e: HyperlinkEvent) {
+                                        notification.hideBalloon()
+                                        NavigationUtils.navigateToTask(project, failedTasks[Integer.valueOf(e.description)])
+                                      }
+                                    })
+
+    if (failedTasks.size > 1) {
+      notification.addAction(object : AnAction("Open First Failed Task") {
+        override fun actionPerformed(e: AnActionEvent) {
+          notification.hideBalloon()
+          NavigationUtils.navigateToTask(project, failedTasks.first())
+        }
+      })
+    }
+    return notification
+  }
+
+  private val Task.fullName: String
+    get() = listOfNotNull(lesson.section, lesson, this).joinToString("/") { it.presentableName }
 
   override fun update(e: AnActionEvent) {
     //TODO: implement
@@ -56,5 +90,7 @@ class CheckAllTasks : AnAction("Check All Tasks") {
 
     @VisibleForTesting
     const val FAILED_MESSAGE = "Failed"
+
+    private const val GROUP_ID = "Education: all tasks check"
   }
 }
