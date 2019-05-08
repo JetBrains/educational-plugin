b21929ade58bb2bb1eb77e82aa621cb634439d60
Liana Bakradze <liana.bakradze@jetbrains.com>
2019-05-08 18:53:32 +0300

    EDU-1436 modify tests to work with notifications

--- educational-core/testSrc/com/jetbrains/edu/coursecreator/actions/CheckAllTest.kt
+++ educational-core/testSrc/com/jetbrains/edu/coursecreator/actions/CheckAllTest.kt
@@ -1,12 +1,21 @@
 package com.jetbrains.edu.coursecreator.actions
 
+import com.intellij.notification.Notification
+import com.intellij.notification.Notifications
+import com.intellij.notification.NotificationsAdapter
+import com.intellij.util.messages.MessageBusConnection
 import com.jetbrains.edu.coursecreator.CCUtils
 import com.jetbrains.edu.learning.EduActionTestCase
-import com.jetbrains.edu.learning.EduTestDialog
 import com.jetbrains.edu.learning.courseFormat.CheckStatus
-import com.jetbrains.edu.learning.withTestDialog
 
 class CheckAllTest : EduActionTestCase() {
+  private lateinit var connection: MessageBusConnection
+
+  override fun setUp() {
+    super.setUp()
+    connection = project.messageBus.connect(testRootDisposable)
+  }
+
   fun `test all solved`() {
     courseWithFiles(courseMode = CCUtils.COURSE_MODE) {
       lesson {
@@ -16,22 +25,37 @@ class CheckAllTest : EduActionTestCase() {
       }
     }
 
-    withTestDialog(EduTestDialog()) {
-      testAction(dataContext(emptyArray()), CheckAllTasks())
-    }.checkWasShown(CheckAllTasks.SUCCESS_MESSAGE)
+    doTestWithNotification {
+      assertEquals(CheckAllTasks.SUCCESS_MESSAGE , it.content)
+    }
   }
 
   fun `test failed tasks`() {
+    val taskName = "Failed Task"
     courseWithFiles(courseMode = CCUtils.COURSE_MODE) {
       lesson {
-        eduTask {
+        eduTask(name = taskName) {
           checkResultFile(CheckStatus.Failed)
         }
       }
     }
 
-    withTestDialog(EduTestDialog()) {
-      testAction(dataContext(emptyArray()), CheckAllTasks())
-    }.checkWasShown(CheckAllTasks.FAILED_MESSAGE)
+    doTestWithNotification {
+      assertEquals("1 of 1 tasks failed", it.subtitle)
+      assertEquals("<a href=\"0\">lesson1/$taskName</a>", it.content)
+    }
+  }
+
+  private fun doTestWithNotification(checkNotification: (Notification) -> Unit) {
+    var notificationShown = false
+    connection.subscribe(Notifications.TOPIC, object: NotificationsAdapter() {
+      override fun notify(notification: Notification) {
+        notificationShown = true
+        checkNotification(notification)
+      }
+    })
+
+    testAction(dataContext(emptyArray()), CheckAllTasks())
+    assertTrue("Notification wasn't shown", notificationShown)
   }
 }
