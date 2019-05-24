3073c07db17fdc0ad0782f5dd4d64f92bad880be
Liana Bakradze <liana.bakradze@jetbrains.com>
2019-05-24 20:11:37 +0300

    EDU-2163 doc reference

--- educational-core/src/com/jetbrains/edu/learning/courseFormat/tasks/choice/ChoiceOptionStatus.kt
+++ educational-core/src/com/jetbrains/edu/learning/courseFormat/tasks/choice/ChoiceOptionStatus.kt
@@ -1,7 +1,7 @@
 package com.jetbrains.edu.learning.courseFormat.tasks.choice
 
 /**
- * Choice tasks created on Stepik can't be checked locally, so they have UNKNOWN status
+ * Choice tasks created on Stepik can't be checked locally, so they have [UNKNOWN] status
  */
 enum class ChoiceOptionStatus {
   CORRECT, INCORRECT, UNKNOWN
