874dbb64bcd2ef2d8bf2210dbd3766235c94e583
Damir Shagaev <damir.shagaev@jetbrains.com>
2019-05-27 16:24:18 +0300

    EDU-2040: Delete C++ module from Experimental features

--- Edu-Cpp/src/com/jetbrains/edu/cpp/CppConfigurator.kt
+++ Edu-Cpp/src/com/jetbrains/edu/cpp/CppConfigurator.kt
@@ -1,16 +1,13 @@
 package com.jetbrains.edu.cpp
 
-import com.intellij.openapi.application.Experiments
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.jetbrains.edu.cpp.checker.CppTaskCheckerProvider
 import com.jetbrains.edu.learning.EduCourseBuilder
-import com.jetbrains.edu.learning.EduExperimentalFeatures
 import com.jetbrains.edu.learning.EduNames
 import com.jetbrains.edu.learning.checker.TaskCheckerProvider
 import com.jetbrains.edu.learning.configuration.EduConfigurator
 import com.jetbrains.edu.learning.courseGeneration.GeneratorUtils.getInternalTemplateText
-import com.jetbrains.edu.learning.isUnitTestMode
 
 class CppConfigurator : EduConfigurator<CppProjectSettings> {
 
@@ -30,8 +27,6 @@ class CppConfigurator : EduConfigurator<CppProjectSettings> {
   override fun getMockTemplate(): String = getInternalTemplateText(MOCK_CPP)
 
   override fun isCourseCreatorEnabled(): Boolean = false
-
-  override fun isEnabled(): Boolean = Experiments.isFeatureEnabled(EduExperimentalFeatures.CPP_COURSES) || isUnitTestMode
 
   override fun excludeFromArchive(project: Project, file: VirtualFile): Boolean {
     return super.excludeFromArchive(project, file) ||  file.path.contains("cmake-build-debug")
