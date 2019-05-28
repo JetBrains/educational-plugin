a68aed5b0949e66a7fea2e2f93d85172e60789f0
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2019-05-28 15:13:56 +0300

    EDU-249: Convert `PyCCCommandLineState` to kotlin

--- Edu-Python/src/com/jetbrains/edu/python/coursecreator/run/PyCCCommandLineState.kt
+++ Edu-Python/src/com/jetbrains/edu/python/coursecreator/run/PyCCCommandLineState.kt
@@ -1,114 +1,95 @@
-package com.jetbrains.edu.python.coursecreator.run;
+package com.jetbrains.edu.python.coursecreator.run
 
-import com.intellij.execution.ExecutionException;
-import com.intellij.execution.ExecutionResult;
-import com.intellij.execution.Executor;
-import com.intellij.execution.configurations.GeneralCommandLine;
-import com.intellij.execution.configurations.ParamsGroup;
-import com.intellij.execution.process.ProcessAdapter;
-import com.intellij.execution.process.ProcessEvent;
-import com.intellij.execution.process.ProcessHandler;
-import com.intellij.execution.runners.ExecutionEnvironment;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.fileEditor.impl.text.TextEditorProvider;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.io.FileUtil;
-import com.intellij.openapi.vfs.LocalFileSystem;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.jetbrains.edu.learning.StudyTaskManager;
-import com.jetbrains.edu.learning.EduUtils;
-import com.jetbrains.edu.learning.checker.CheckUtils;
-import com.jetbrains.edu.learning.EduNames;
-import com.jetbrains.edu.learning.courseFormat.Course;
-import com.jetbrains.edu.learning.courseFormat.TaskFile;
-import com.jetbrains.edu.learning.courseFormat.tasks.Task;
-import com.jetbrains.python.run.CommandLinePatcher;
-import com.jetbrains.python.run.PythonCommandLineState;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.execution.ExecutionException
+import com.intellij.execution.ExecutionResult
+import com.intellij.execution.Executor
+import com.intellij.execution.configurations.GeneralCommandLine
+import com.intellij.execution.process.ProcessAdapter
+import com.intellij.execution.process.ProcessEvent
+import com.intellij.execution.process.ProcessHandler
+import com.intellij.execution.runners.ExecutionEnvironment
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.runWriteAction
+import com.intellij.openapi.fileEditor.impl.text.TextEditorProvider
+import com.intellij.openapi.util.io.FileUtil
+import com.intellij.openapi.vfs.LocalFileSystem
+import com.intellij.openapi.vfs.VirtualFile
+import com.jetbrains.edu.learning.EduNames
+import com.jetbrains.edu.learning.EduUtils
+import com.jetbrains.edu.learning.StudyTaskManager
+import com.jetbrains.edu.learning.checker.CheckUtils
+import com.jetbrains.edu.learning.courseFormat.tasks.Task
+import com.jetbrains.python.run.CommandLinePatcher
+import com.jetbrains.python.run.PythonCommandLineState
 
-import java.util.Map;
+class PyCCCommandLineState(
+  private val runConfiguration: PyCCRunTestConfiguration,
+  env: ExecutionEnvironment
+) : PythonCommandLineState(runConfiguration, env) {
 
-public class PyCCCommandLineState extends PythonCommandLineState {
-  private final PyCCRunTestConfiguration myRunConfiguration;
-  private final VirtualFile myTaskDir;
-  private final Task myTask;
+  private val taskDir: VirtualFile
+  private val task: Task
 
-  public PyCCCommandLineState(PyCCRunTestConfiguration runConfiguration,
-                              ExecutionEnvironment env) {
-    super(runConfiguration, env);
-    myRunConfiguration = runConfiguration;
-
-    VirtualFile testsFile = LocalFileSystem.getInstance().findFileByPath(myRunConfiguration.getPathToTest());
-    assert testsFile != null;
-    Project project = myRunConfiguration.getProject();
-    myTask = EduUtils.getTaskForFile(project, testsFile);
-    assert myTask != null;
-    myTaskDir = myTask.getTaskDir(project);
-    assert myTaskDir != null;
+  init {
+    val testsFile = LocalFileSystem.getInstance().findFileByPath(runConfiguration.pathToTest)!!
+    val project = runConfiguration.project
+    task = EduUtils.getTaskForFile(project, testsFile)!!
+    taskDir = task.getTaskDir(project)!!
   }
 
-  @Override
-  protected void buildCommandLineParameters(GeneralCommandLine commandLine) {
-    commandLine.setWorkDirectory(myTaskDir.getPath());
-    ParamsGroup group = commandLine.getParametersList().getParamsGroup(GROUP_SCRIPT);
-    assert group != null;
+  private val currentTaskFilePath: String?
+    get() {
+      var textFile: String? = null
+      for ((key, value) in task.taskFiles) {
+        val path = getTaskFilePath(key)
+        if (value.answerPlaceholders.isNotEmpty()) {
+          return path
+        }
+        val virtualFile = LocalFileSystem.getInstance().findFileByPath(path) ?: continue
+        if (TextEditorProvider.isTextFile(virtualFile)) {
+          textFile = path
+        }
+      }
+      return textFile
+    }
 
-    Project project = myRunConfiguration.getProject();
-    Course course = StudyTaskManager.getInstance(project).getCourse();
-    assert course != null;
+  override fun buildCommandLineParameters(commandLine: GeneralCommandLine) {
+    val project = runConfiguration.project
+    check(StudyTaskManager.getInstance(project).course != null)
 
-    group.addParameter(myRunConfiguration.getPathToTest());
-
-    String path = getCurrentTaskFilePath();
+    commandLine.setWorkDirectory(taskDir.path)
+    val group = commandLine.parametersList.getParamsGroup(GROUP_SCRIPT)!!
+    group.addParameter(runConfiguration.pathToTest)
+    val path = currentTaskFilePath
     if (path != null) {
-      group.addParameter(path);
+      group.addParameter(path)
     }
   }
 
-  @Nullable
-  private String getCurrentTaskFilePath() {
-    String textFile = null;
-    for (Map.Entry<String, TaskFile> entry : myTask.getTaskFiles().entrySet()) {
-      String path = getTaskFilePath(entry.getKey());
-      if (!entry.getValue().getAnswerPlaceholders().isEmpty()) {
-        return path;
-      }
-      VirtualFile virtualFile = LocalFileSystem.getInstance().findFileByPath(path);
-      if (virtualFile == null) {
-        continue;
-      }
-      if (TextEditorProvider.isTextFile(virtualFile)) {
-        textFile = path;
-      }
+  private fun getTaskFilePath(name: String): String {
+    val taskDirPath = FileUtil.toSystemDependentName(taskDir.path)
+    return if (taskDir.findChild(EduNames.SRC) != null) {
+      FileUtil.join(taskDirPath, EduNames.SRC, name)
     }
-    return textFile;
+    else {
+      FileUtil.join(taskDirPath, name)
+    }
   }
 
-
-  private String getTaskFilePath(String name) {
-    String taskDirPath = FileUtil.toSystemDependentName(myTaskDir.getPath());
-    return myTaskDir.findChild(EduNames.SRC) != null ?
-           FileUtil.join(taskDirPath, EduNames.SRC, name) :
-           FileUtil.join(taskDirPath, name);
+  @Throws(ExecutionException::class)
+  override fun execute(executor: Executor, processStarter: PythonProcessStarter, vararg patchers: CommandLinePatcher): ExecutionResult {
+    runWriteAction { CheckUtils.flushWindows(task, taskDir) }
+    return super.execute(executor, processStarter, *patchers)
   }
 
-  @Override
-  public ExecutionResult execute(Executor executor, PythonProcessStarter processStarter, CommandLinePatcher... patchers) throws ExecutionException {
-    ApplicationManager.getApplication().runWriteAction(() -> CheckUtils.flushWindows(myTask, myTaskDir));
-
-    return super.execute(executor, processStarter, patchers);
-  }
-
-  @Override
-  protected ProcessHandler doCreateProcess(GeneralCommandLine commandLine) throws ExecutionException {
-    ProcessHandler handler = super.doCreateProcess(commandLine);
-    handler.addProcessListener(new ProcessAdapter() {
-      @Override
-      public void processTerminated(@NotNull ProcessEvent event) {
-        ApplicationManager.getApplication().invokeLater(() -> EduUtils.deleteWindowDescriptions(myTask, myTaskDir));
+  @Throws(ExecutionException::class)
+  override fun doCreateProcess(commandLine: GeneralCommandLine): ProcessHandler {
+    val handler = super.doCreateProcess(commandLine)
+    handler.addProcessListener(object : ProcessAdapter() {
+      override fun processTerminated(event: ProcessEvent) {
+        ApplicationManager.getApplication().invokeLater { EduUtils.deleteWindowDescriptions(task, taskDir) }
       }
-    });
-    return handler;
+    })
+    return handler
   }
 }
