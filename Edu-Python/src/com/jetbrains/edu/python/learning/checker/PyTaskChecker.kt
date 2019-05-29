f528c46adcd7ae4705d727feac86769adf0a215f
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2019-05-29 10:43:41 +0300

    EDU-249: convert `PyTaskChecker` to kotlin

--- Edu-Python/src/com/jetbrains/edu/python/learning/checker/PyTaskChecker.kt
+++ Edu-Python/src/com/jetbrains/edu/python/learning/checker/PyTaskChecker.kt
@@ -1,140 +1,135 @@
-package com.jetbrains.edu.python.learning.checker;
+package com.jetbrains.edu.python.learning.checker
 
-import com.intellij.execution.ExecutionException;
-import com.intellij.execution.process.CapturingProcessHandler;
-import com.intellij.execution.process.ProcessOutput;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.command.CommandProcessor;
-import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.progress.ProgressIndicator;
-import com.intellij.openapi.progress.ProgressManager;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.jetbrains.edu.learning.EduState;
-import com.jetbrains.edu.learning.EduUtils;
-import com.jetbrains.edu.learning.checker.CheckResult;
-import com.jetbrains.edu.learning.checker.CheckUtils;
-import com.jetbrains.edu.learning.checker.TaskChecker;
-import com.jetbrains.edu.learning.checker.TestsOutputParser;
-import com.jetbrains.edu.learning.courseFormat.CheckStatus;
-import com.jetbrains.edu.learning.courseFormat.Course;
-import com.jetbrains.edu.learning.courseFormat.TaskFile;
-import com.jetbrains.edu.learning.courseFormat.tasks.EduTask;
-import com.jetbrains.edu.learning.courseFormat.tasks.Task;
-import com.jetbrains.edu.learning.editor.EduEditorKt;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.execution.ExecutionException
+import com.intellij.execution.process.CapturingProcessHandler
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.runUndoTransparentWriteAction
+import com.intellij.openapi.application.runWriteAction
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.progress.ProgressIndicator
+import com.intellij.openapi.progress.ProgressManager
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+import com.jetbrains.edu.learning.EduState
+import com.jetbrains.edu.learning.EduUtils
+import com.jetbrains.edu.learning.checker.CheckResult
+import com.jetbrains.edu.learning.checker.CheckUtils
+import com.jetbrains.edu.learning.checker.TaskChecker
+import com.jetbrains.edu.learning.checker.TestsOutputParser
+import com.jetbrains.edu.learning.courseFormat.CheckStatus
+import com.jetbrains.edu.learning.courseFormat.tasks.EduTask
+import com.jetbrains.edu.learning.courseFormat.tasks.Task
+import com.jetbrains.edu.learning.editor.ACTION_TEXT
+import com.jetbrains.edu.learning.editor.BROKEN_SOLUTION_ERROR_TEXT_END
+import com.jetbrains.edu.learning.editor.BROKEN_SOLUTION_ERROR_TEXT_START
+import java.util.concurrent.CountDownLatch
 
-import java.util.Map;
-import java.util.concurrent.CountDownLatch;
+open class PyTaskChecker(task: EduTask, project: Project) : TaskChecker<EduTask>(task, project) {
 
-public class PyTaskChecker extends TaskChecker<EduTask> {
-  private static final Logger LOG = Logger.getInstance(PyTaskChecker.class);
-
-  public PyTaskChecker(@NotNull EduTask task, @NotNull Project project) {
-    super(task, project);
-  }
-
-  @NotNull
-  @Override
-  public CheckResult check(@NotNull ProgressIndicator indicator) {
-    VirtualFile taskDir = task.getTaskDir(project);
+  override fun check(indicator: ProgressIndicator): CheckResult {
+    val taskDir = task.getTaskDir(project)
     if (taskDir == null) {
-      LOG.info("taskDir is null for task " + task.getName());
-      return new CheckResult(CheckStatus.Unchecked, "Task is broken");
+      LOG.info("taskDir is null for task " + task.name)
+      return CheckResult(CheckStatus.Unchecked, "Task is broken")
     }
 
     if (!task.isValid(project)) {
-      return new CheckResult(CheckStatus.Unchecked,
-                             EduEditorKt.BROKEN_SOLUTION_ERROR_TEXT_START + EduEditorKt.ACTION_TEXT + EduEditorKt.BROKEN_SOLUTION_ERROR_TEXT_END);
+      return CheckResult(CheckStatus.Unchecked,
+                         BROKEN_SOLUTION_ERROR_TEXT_START + ACTION_TEXT + BROKEN_SOLUTION_ERROR_TEXT_END)
     }
-    CountDownLatch latch = new CountDownLatch(1);
-    ApplicationManager.getApplication()
-      .invokeLater(() -> ApplicationManager.getApplication().runWriteAction(() -> {
-        CheckUtils.flushWindows(task, taskDir);
-        latch.countDown();
-      }));
-    final PyTestRunner testRunner = new PyTestRunner(taskDir);
+    val latch = CountDownLatch(1)
+    ApplicationManager.getApplication().invokeLater {
+      runWriteAction {
+        CheckUtils.flushWindows(task, taskDir)
+        latch.countDown()
+      }
+    }
+    val testRunner = PyTestRunner(taskDir)
     try {
-      final VirtualFile fileToCheck = getTaskVirtualFile(task, taskDir);
+      val fileToCheck = getTaskVirtualFile(task, taskDir)
       if (fileToCheck != null) {
         //otherwise answer placeholders might have been not flushed yet
-        latch.await();
-        Process testProcess = testRunner.createCheckProcess(project, fileToCheck.getPath());
-        return getCheckResult(testProcess, testRunner.getCommandLine().getCommandLineString());
+        latch.await()
+        val testProcess = testRunner.createCheckProcess(project, fileToCheck.path)
+        return getCheckResult(testProcess, testRunner.commandLine.commandLineString)
       }
     }
-    catch (ExecutionException | InterruptedException e) {
-      LOG.error(e);
+    catch (e: ExecutionException) {
+      LOG.error(e)
     }
-    return CheckResult.FAILED_TO_CHECK;
+    catch (e: InterruptedException) {
+      LOG.error(e)
+    }
+
+    return CheckResult.FAILED_TO_CHECK
   }
 
-  @Override
-  public void clearState() {
-    ApplicationManager.getApplication().invokeLater(() -> {
-      VirtualFile taskDir = task.getTaskDir(project);
+  override fun clearState() {
+    ApplicationManager.getApplication().invokeLater {
+      val taskDir = task.getTaskDir(project)
       if (taskDir != null) {
-        EduUtils.deleteWindowDescriptions(task, taskDir);
+        EduUtils.deleteWindowDescriptions(task, taskDir)
       }
-    });
+    }
   }
 
-  @Override
-  public void onTaskFailed(@NotNull String message, @Nullable String details) {
-    super.onTaskFailed(message, details);
-    ApplicationManager.getApplication().invokeLater(() -> {
-      VirtualFile taskDir = task.getTaskDir(project);
-      if (taskDir == null) return;
-      for (Map.Entry<String, TaskFile> entry : task.getTaskFiles().entrySet()) {
-        final TaskFile taskFile = entry.getValue();
-        if (taskFile.getAnswerPlaceholders().size() < 2) {
-          continue;
+  override fun onTaskFailed(message: String, details: String?) {
+    super.onTaskFailed(message, details)
+    ApplicationManager.getApplication().invokeLater {
+      val taskDir = task.getTaskDir(project)
+      if (taskDir == null) return@invokeLater
+      for ((_, taskFile) in task.taskFiles) {
+        if (taskFile.answerPlaceholders.size < 2) {
+          continue
         }
-        final Course course = task.getLesson().getCourse();
-        if (course.isStudy()) {
-          CommandProcessor.getInstance().runUndoTransparentAction(
-            () -> ApplicationManager.getApplication().runWriteAction(
-              () -> PySmartChecker.runSmartTestProcess(taskDir, new PyTestRunner(taskDir), taskFile, project)));
+        val course = task.lesson.course
+        if (course.isStudy) {
+          runUndoTransparentWriteAction {
+            PySmartChecker.runSmartTestProcess(taskDir, PyTestRunner(taskDir), taskFile, project)
+          }
         }
       }
-      CheckUtils.navigateToFailedPlaceholder(new EduState(EduUtils.getSelectedEduEditor(project)), task, taskDir, project);
-    });
+      CheckUtils.navigateToFailedPlaceholder(EduState(EduUtils.getSelectedEduEditor(project)), task, taskDir, project)
+    }
   }
 
-  @Nullable
-  private static VirtualFile getTaskVirtualFile(@NotNull final Task task,
-                                                @NotNull final VirtualFile taskDir) {
-    VirtualFile firstFile = null;
-    for (Map.Entry<String, TaskFile> entry : task.getTaskFiles().entrySet()) {
-      TaskFile taskFile = entry.getValue();
-      VirtualFile file = EduUtils.findTaskFileInDir(taskFile, taskDir);
-      if (file == null) {
-        LOG.warn(String.format("Can't find virtual file for `%s` task file in `%s` task", taskFile.getName(), task.getName()));
-        continue;
-      }
-      if (firstFile == null) {
-        firstFile = file;
-      }
+  companion object {
+    private val LOG = Logger.getInstance(PyTaskChecker::class.java)
 
-      // TODO: Come up with a smarter way how to find correct task file
-      boolean hasNewPlaceholder = taskFile.getAnswerPlaceholders()
-        .stream()
-        .anyMatch(p -> p.getPlaceholderDependency() == null);
-      if (hasNewPlaceholder) return file;
-    }
-    return firstFile;
-  }
+    private fun getTaskVirtualFile(task: Task, taskDir: VirtualFile): VirtualFile? {
+      var firstFile: VirtualFile? = null
+      for ((_, taskFile) in task.taskFiles) {
+        val file = EduUtils.findTaskFileInDir(taskFile, taskDir)
+        if (file == null) {
+          LOG.warn(String.format("Can't find virtual file for `%s` task file in `%s` task", taskFile.name, task.name))
+          continue
+        }
+        if (firstFile == null) {
+          firstFile = file
+        }
 
-  public static CheckResult getCheckResult(@NotNull Process testProcess, @NotNull String commandLine) {
-    final CapturingProcessHandler handler = new CapturingProcessHandler(testProcess, null, commandLine);
-    final ProcessOutput output = ProgressManager.getInstance().hasProgressIndicator() ? handler
-      .runProcessWithProgressIndicator(ProgressManager.getInstance().getProgressIndicator()) : handler.runProcess();
-    String stderr = output.getStderr();
-    if (!stderr.isEmpty() && output.getStdout().isEmpty()) {
-      LOG.info("#educational " + stderr);
-      return new CheckResult(CheckStatus.Failed, stderr, null, null, false);
+        // TODO: Come up with a smarter way how to find correct task file
+        val hasNewPlaceholder = taskFile.answerPlaceholders
+          .any { p -> p.placeholderDependency == null }
+        if (hasNewPlaceholder) return file
+      }
+      return firstFile
     }
-    return TestsOutputParser.getCheckResult(output.getStdoutLines(), false);
+
+    fun getCheckResult(testProcess: Process, commandLine: String): CheckResult {
+      val handler = CapturingProcessHandler(testProcess, null, commandLine)
+      val output = if (ProgressManager.getInstance().hasProgressIndicator()) {
+        handler.runProcessWithProgressIndicator(ProgressManager.getInstance().progressIndicator)
+      }
+      else {
+        handler.runProcess()
+      }
+      val stderr = output.stderr
+      if (stderr.isNotEmpty() && output.stdout.isEmpty()) {
+        LOG.info("#educational $stderr")
+        return CheckResult(CheckStatus.Failed, stderr, null, null, false)
+      }
+      return TestsOutputParser.getCheckResult(output.stdoutLines, false)
+    }
   }
 }
