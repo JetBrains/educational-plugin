df6235dcaa14fa8e9cee60e0d0ca8821121f4d6a
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2019-05-30 14:11:09 +0300

    EDU-249: fix task checking in hyperskill python courses

--- Edu-Python/src/com/jetbrains/edu/python/learning/stepik/hyperskill/PyHyperskillConfigurator.kt
+++ Edu-Python/src/com/jetbrains/edu/python/learning/stepik/hyperskill/PyHyperskillConfigurator.kt
@@ -3,10 +3,11 @@ package com.jetbrains.edu.python.learning.stepik.hyperskill
 import com.intellij.util.PlatformUtils
 import com.jetbrains.edu.learning.EduUtils
 import com.jetbrains.edu.learning.stepik.hyperskill.HyperskillConfigurator
+import com.jetbrains.edu.python.learning.PyConfigurator
 import com.jetbrains.python.newProject.PyNewProjectSettings
 
 open class PyHyperskillConfigurator : HyperskillConfigurator<PyNewProjectSettings> {
-
+  override fun getTestFileName(): String = PyConfigurator.TESTS_PY
   override fun getCourseBuilder() = PyHyperskillCourseBuilder()
   override fun getTaskCheckerProvider() = PyHyperskillTaskCheckerProvider()
   override fun isEnabled(): Boolean = !PlatformUtils.isPyCharm() && !PlatformUtils.isCLion() && !EduUtils.isAndroidStudio()
