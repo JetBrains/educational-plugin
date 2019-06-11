f2d970f9d2b1851bd1b8f7c83e5f1c85d9ae9a00
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2019-06-11 18:33:34 +0300

    EDU-2220: Convert `EduDocumentListener` to kotlin

--- educational-core/src/com/jetbrains/edu/learning/EduDocumentListener.kt
+++ educational-core/src/com/jetbrains/edu/learning/EduDocumentListener.kt
@@ -1,111 +1,102 @@
-package com.jetbrains.edu.learning;
+package com.jetbrains.edu.learning
 
-import com.intellij.openapi.editor.event.DocumentEvent;
-import com.intellij.openapi.editor.event.DocumentListener;
-import com.intellij.openapi.editor.impl.event.DocumentEventImpl;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.Pair;
-import com.jetbrains.edu.coursecreator.yaml.YamlFormatSynchronizer;
-import com.jetbrains.edu.learning.courseFormat.AnswerPlaceholder;
-import com.jetbrains.edu.learning.courseFormat.TaskFile;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.editor.event.DocumentEvent
+import com.intellij.openapi.editor.event.DocumentListener
+import com.intellij.openapi.editor.impl.event.DocumentEventImpl
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Pair
+import com.jetbrains.edu.coursecreator.yaml.YamlFormatSynchronizer
+import com.jetbrains.edu.learning.courseFormat.AnswerPlaceholder
+import com.jetbrains.edu.learning.courseFormat.TaskFile
 
 /**
  * Listens changes in study files and updates
  * coordinates of all the placeholders in current task file
  */
-public class EduDocumentListener implements DocumentListener {
-  protected final Project myProject;
-  protected final TaskFile myTaskFile;
-  private final boolean updateYaml;
+class EduDocumentListener(
+  protected val myProject: Project,
+  protected val myTaskFile: TaskFile,
+  private val updateYaml: Boolean
+) : DocumentListener {
 
-  public EduDocumentListener(Project project, TaskFile taskFile, boolean updateYaml) {
-    myProject = project;
-    myTaskFile = taskFile;
-    this.updateYaml = updateYaml;
+  override fun beforeDocumentChange(e: DocumentEvent) {
+    if (!myTaskFile.isTrackChanges) {
+      return
+    }
+    myTaskFile.isHighlightErrors = true
   }
 
-  @Override
-  public void beforeDocumentChange(@NotNull DocumentEvent e) {
-    if (!myTaskFile.isTrackChanges()) {
-      return;
+  override fun documentChanged(e: DocumentEvent) {
+    if (!myTaskFile.isTrackChanges) {
+      return
     }
-    myTaskFile.setHighlightErrors(true);
-  }
+    if (myTaskFile.answerPlaceholders.isEmpty()) return
 
-  @Override
-  public void documentChanged(@NotNull DocumentEvent e) {
-    if (!myTaskFile.isTrackChanges()) {
-      return;
-    }
-    if (myTaskFile.getAnswerPlaceholders().isEmpty()) return;
-
-    if (!(e instanceof DocumentEventImpl)) {
-      return;
+    if (e !is DocumentEventImpl) {
+      return
     }
 
-    int offset = e.getOffset();
-    int change = e.getNewLength() - e.getOldLength();
+    val offset = e.getOffset()
+    val change = e.getNewLength() - e.getOldLength()
 
-    final CharSequence fragment = e.getNewFragment();
-    CharSequence oldFragment = e.getOldFragment();
+    val fragment = e.getNewFragment()
+    val oldFragment = e.getOldFragment()
 
-    for (AnswerPlaceholder placeholder : myTaskFile.getAnswerPlaceholders()) {
-      int placeholderStart = placeholder.getOffset();
-      int placeholderEnd = placeholder.getEndOffset();
+    for (placeholder in myTaskFile.answerPlaceholders) {
+      var placeholderStart = placeholder.offset
+      var placeholderEnd = placeholder.endOffset
 
-      final Pair<Integer, Integer> changes = getChangeForOffsets(offset, change, placeholder);
-      int changeForStartOffset = changes.getFirst();
-      int changeForEndOffset = changes.getSecond();
+      val changes = getChangeForOffsets(offset, change, placeholder)
+      val changeForStartOffset = changes.getFirst()
+      val changeForEndOffset = changes.getSecond()
 
-      placeholderStart += changeForStartOffset;
-      placeholderEnd += changeForEndOffset;
+      placeholderStart += changeForStartOffset
+      placeholderEnd += changeForEndOffset
 
       if (placeholderStart - 1 == offset && fragment.toString().isEmpty() && oldFragment.toString().startsWith("\n")) {
-        placeholderStart -= 1;
+        placeholderStart -= 1
       }
 
       if (placeholderStart == offset && oldFragment.toString().isEmpty() && fragment.toString().startsWith("\n")) {
-        placeholderStart += 1;
+        placeholderStart += 1
       }
 
-      int length = placeholderEnd - placeholderStart;
-      assert length >= 0;
-      assert placeholderStart >= 0;
-      updatePlaceholder(placeholder, placeholderStart, length);
+      val length = placeholderEnd - placeholderStart
+      assert(length >= 0)
+      assert(placeholderStart >= 0)
+      updatePlaceholder(placeholder, placeholderStart, length)
     }
   }
 
-  private static Pair<Integer, Integer> getChangeForOffsets(int offset, int change, @NotNull final AnswerPlaceholder placeholder) {
-    int placeholderStart = placeholder.getOffset();
-    int placeholderEnd = placeholder.getEndOffset();
-    int start = 0;
-    int end = change;
+  private fun getChangeForOffsets(offset: Int, change: Int, placeholder: AnswerPlaceholder): Pair<Int, Int> {
+    val placeholderStart = placeholder.offset
+    val placeholderEnd = placeholder.endOffset
+    var start = 0
+    var end = change
     if (offset > placeholderEnd) {
-      return Pair.create(0, 0);
+      return Pair.create(0, 0)
     }
 
     if (offset < placeholderStart) {
-      start = change;
+      start = change
 
       if (change < 0 && offset - change > placeholderStart) {  // delete part of placeholder start
-        start = offset - placeholderStart;
+        start = offset - placeholderStart
       }
     }
 
     if (change < 0 && offset - change > placeholderEnd) {   // delete part of placeholder end
-      end = offset - placeholderEnd;
+      end = offset - placeholderEnd
     }
 
-    return Pair.create(start, end);
+    return Pair.create(start, end)
   }
 
-  protected void updatePlaceholder(@NotNull AnswerPlaceholder answerPlaceholder, int start, int length) {
-    answerPlaceholder.setOffset(start);
-    answerPlaceholder.setLength(length);
+  protected fun updatePlaceholder(answerPlaceholder: AnswerPlaceholder, start: Int, length: Int) {
+    answerPlaceholder.offset = start
+    answerPlaceholder.length = length
     if (updateYaml) {
-      YamlFormatSynchronizer.saveItem(myTaskFile.getTask());
+      YamlFormatSynchronizer.saveItem(myTaskFile.task)
     }
   }
 }
-
