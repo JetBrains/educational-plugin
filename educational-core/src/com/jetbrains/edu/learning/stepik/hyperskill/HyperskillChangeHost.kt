939db6cf0223567ea389cb69b2d3b84d7b177dff
Ekaterina Tuzova <ekaterina.tuzova@jetbrains.com>
2019-06-12 17:43:24 +0300

    EDU-2224 Customizable hyperskill url

---
+++ educational-core/src/com/jetbrains/edu/learning/stepik/hyperskill/HyperskillChangeHost.kt
@@ -0,0 +1,29 @@
+package com.jetbrains.edu.learning.stepik.hyperskill
+
+import com.intellij.ide.util.PropertiesComponent
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.RightAlignedToolbarAction
+import com.intellij.openapi.application.Experiments
+import com.intellij.openapi.project.DumbAwareAction
+import com.intellij.openapi.ui.Messages
+import com.jetbrains.edu.learning.EduExperimentalFeatures
+
+private const val ACTION_TEXT = "Change Hyperskill url"
+
+@Suppress("ComponentNotRegistered")
+class HyperskillChangeHost : DumbAwareAction(ACTION_TEXT), RightAlignedToolbarAction {
+  override fun actionPerformed(e: AnActionEvent) {
+    val initialValue = PropertiesComponent.getInstance().getValue(HYPERSKILL_URL_PROPERTY, HYPERSKILL_DEFAULT_URL)
+    val result = Messages.showInputDialog("Enter Hyperskill url", "Enter Hyperskill url", null, initialValue, null)
+    if (result != null) {
+      PropertiesComponent.getInstance().setValue(HYPERSKILL_URL_PROPERTY, result)
+    }
+  }
+
+  override fun update(e: AnActionEvent) {
+    e.presentation.isEnabledAndVisible = false
+    if (!Experiments.isFeatureEnabled(EduExperimentalFeatures.HYPERSKILL)) return
+    e.presentation.isEnabledAndVisible = true
+  }
+
+}
--- educational-core/src/com/jetbrains/edu/learning/stepik/hyperskill/HyperskillNames.kt
+++ educational-core/src/com/jetbrains/edu/learning/stepik/hyperskill/HyperskillNames.kt
@@ -1,20 +1,19 @@
 package com.jetbrains.edu.learning.stepik.hyperskill
 
+import com.intellij.ide.util.PropertiesComponent
 import com.jetbrains.edu.learning.EduNames
 import com.jetbrains.edu.learning.checker.CheckUtils
 import org.jetbrains.ide.BuiltInServerManager
 
 const val HYPERSKILL = "Hyperskill"
 const val HYPERSKILL_TYPE = HYPERSKILL
-const val HYPERSKILL_URL = "https://hyperskill.org/"
+const val HYPERSKILL_URL_PROPERTY = "Hyperskill URL"
+const val HYPERSKILL_DEFAULT_URL = "https://hyperskill.org/"
 const val HYPERSKILL_PROJECTS_URL = "https://hyperskill.org/projects"
 private val port = BuiltInServerManager.getInstance().port
 val REDIRECT_URI = "http://localhost:$port/api/edu/hyperskill/oauth"
 
 var CLIENT_ID = HyperskillOAuthBundle.valueOrDefault("clientId", "")
-val AUTHORISATION_CODE_URL = "https://hyperskill.org/oauth2/authorize/?" +
-                             "client_id=$CLIENT_ID&redirect_uri=$REDIRECT_URI&grant_type=code&scope=read+write&response_type=code"
-
 const val HYPERSKILL_PROJECT_NOT_SUPPORTED = "Selected project is not supported yet. " +
                              "Please, <a href=\"$HYPERSKILL_PROJECTS_URL\">select another project</a> "
 
@@ -22,3 +21,11 @@ val HYPERSKILL_LANGUAGES = mapOf("java" to "${EduNames.JAVA} 11", "kotlin" to Ed
 
 const val SUCCESS_MESSAGE = "<html>${CheckUtils.CONGRATULATIONS} " +
                             "Continue on <a href=\"https://hyperskill.org/learning-path\">Hyperskill</a>.</html>"
+
+val HYPERSKILL_URL: String
+  get() = PropertiesComponent.getInstance().getValue(HYPERSKILL_URL_PROPERTY, HYPERSKILL_DEFAULT_URL)
+
+val AUTHORISATION_CODE_URL: String
+  get() = "${HYPERSKILL_URL}oauth2/authorize/?" +
+                             "client_id=$CLIENT_ID&redirect_uri=$REDIRECT_URI&grant_type=code&scope=read+write&response_type=code"
+
