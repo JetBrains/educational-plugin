3b94cb0bf5cf1f69950a4938dc827156d66d2371
Valentina Kiryushkina <valentina.kiryushkina@jetbrains.com>
2019-06-12 16:04:12 +0300

    EDU-2151 Fix add item tests
    * Create course before yaml-test-ready flag is enabled
    * Register yaml file type before creating config files. Otherwise we cannot
    find document for config file virtual file
    *

--- educational-core/testSrc/com/jetbrains/edu/coursecreator/yaml/AddNewItemYamlTest.kt
+++ educational-core/testSrc/com/jetbrains/edu/coursecreator/yaml/AddNewItemYamlTest.kt
@@ -3,25 +3,52 @@ package com.jetbrains.edu.coursecreator.yaml
 import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.jetbrains.edu.coursecreator.CCUtils
 import com.jetbrains.edu.coursecreator.yaml.YamlFormatSynchronizer.configFileName
+import com.jetbrains.edu.learning.StudyTaskManager
 import com.jetbrains.edu.learning.courseFormat.ItemContainer
 import com.jetbrains.edu.learning.courseFormat.Lesson
 import com.jetbrains.edu.learning.courseFormat.StudyItem
 
 class AddNewItemYamlTest : YamlTestCase() {
 
-  fun `test new task file added`() {
+  override fun setUp() {
+    super.setUp()
+    // as we don't create config file in CCProjectComponent, we have to create them manually
+    createConfigFiles()
+  }
+
+  private fun createConfigFiles() {
+    YamlFormatSynchronizer.saveAll(project)
+    FileDocumentManager.getInstance().saveAllDocuments()
+  }
+
+  override fun createCourse() {
     val course = courseWithFiles(courseMode = CCUtils.COURSE_MODE) {
       lesson("lesson1") {
         eduTask("task1") {
           taskFile("test1.txt")
           taskFile("test2.txt")
         }
+        eduTask("task2")
+      }
+      lesson("lesson2") {
+        eduTask("task1")
+      }
+      section("section1") {
+        lesson("lesson1") {
+          eduTask("task1")
+        }
+      }
+      section("section2") {
+        lesson("lesson1") {
+          eduTask("task1")
+        }
       }
     }
     course.description = "test"
+  }
 
-    createConfigFiles()
-
+  fun `test new task file added`() {
+    val course = StudyTaskManager.getInstance(project).course!!
     val task = course.lessons.first().taskList.first()
     task.taskFiles.remove("test2.txt")
 
@@ -32,18 +59,7 @@ class AddNewItemYamlTest : YamlTestCase() {
   }
 
   fun `test unexpected task file isn't added`() {
-    val course = courseWithFiles(courseMode = CCUtils.COURSE_MODE) {
-      lesson("lesson1") {
-        eduTask("task1") {
-          taskFile("test1.txt")
-          taskFile("test2.txt")
-        }
-      }
-    }
-    course.description = "test"
-
-    createConfigFiles()
-
+    val course = StudyTaskManager.getInstance(project).course!!
     val task = course.lessons.first().taskList.first()
     task.taskFiles.remove("test2.txt")
 
@@ -57,148 +73,72 @@ class AddNewItemYamlTest : YamlTestCase() {
   }
 
   fun `test new task added`() {
-    val course = courseWithFiles(courseMode = CCUtils.COURSE_MODE) {
-      lesson("lesson1") {
-        eduTask("task1")
-        eduTask("task2")
-      }
-    }
-    course.description = "test"
-
+    val course = StudyTaskManager.getInstance(project).course!!
     doAddedTest(course.lessons.first())
   }
 
   fun `test new unexpected task isn't added`() {
-    val course = courseWithFiles(courseMode = CCUtils.COURSE_MODE) {
-      lesson("lesson1") {
-        eduTask("task1")
-        eduTask("task2")
-      }
-    }
-    course.description = "test"
-
+    val course = StudyTaskManager.getInstance(project).course!!
     doNotAddedTest(course.lessons.first())
   }
 
   fun `test new lesson added`() {
-    val course = courseWithFiles(courseMode = CCUtils.COURSE_MODE) {
-      lesson("lesson1") {
-        eduTask("task1")
-      }
-      lesson("lesson2") {
-        eduTask("task1")
-      }
-    }
-    course.description = "test"
-
+    val course = StudyTaskManager.getInstance(project).course!!
     doAddedTest(course)
   }
 
   fun `test new lesson content added`() {
-    val course = courseWithFiles(courseMode = CCUtils.COURSE_MODE) {
-      lesson("lesson1") {
-        eduTask("task1")
-      }
-      lesson("lesson2") {
-        eduTask("task1")
-      }
-    }
-    course.description = "test"
-
-    doAddedTest(course)
-    val lesson = course.items.last() as Lesson
+    val course = StudyTaskManager.getInstance(project).course!!
+    val section = course.sections.first()
+    doAddedTest(section)
+    val lesson = section.items.last() as Lesson
     assertEquals(1, lesson.taskList.size)
   }
 
   fun `test new unexpected lesson isn't added`() {
-    val course = courseWithFiles(courseMode = CCUtils.COURSE_MODE) {
-      lesson("lesson1") {
-        eduTask("task1")
-      }
-      lesson("lesson2") {
-        eduTask("task1")
-      }
-    }
-    course.description = "test"
-
+    val course = StudyTaskManager.getInstance(project).course!!
     doNotAddedTest(course)
   }
 
   fun `test new section added`() {
-    val course = courseWithFiles(courseMode = CCUtils.COURSE_MODE) {
-      section("section1") {
-        lesson("lesson1") {
-          eduTask("task1")
-        }
-      }
-      section("section2") {
-        lesson("lesson1") {
-          eduTask("task1")
-        }
-      }
-    }
-    course.description = "test"
-
+    val course = StudyTaskManager.getInstance(project).course!!
     doAddedTest(course)
   }
 
-  fun `test new section content added`() {
-    val course = courseWithFiles(courseMode = CCUtils.COURSE_MODE) {
-      section("section1") {
-        lesson("lesson1") {
-          eduTask("task1")
-        }
-      }
-      section("section2") {
-        lesson("lesson1") {
-          eduTask("task1")
-        }
-      }
-    }
-    course.description = "test"
+  fun `test missing section added`() {
+    val course = StudyTaskManager.getInstance(project).course!!
+    doToParentAddedTest(course)
+  }
 
+  fun `test new section content added`() {
+    val course = StudyTaskManager.getInstance(project).course!!
     doAddedTest(course)
   }
 
   fun `test new unexpected section isn't added`() {
-    val course = courseWithFiles(courseMode = CCUtils.COURSE_MODE) {
-      section("section1") {
-        lesson("lesson1") {
-          eduTask("task1")
-        }
-      }
-      section("section2") {
-        lesson("lesson1") {
-          eduTask("task1")
-        }
-      }
-    }
-    course.description = "test"
+    val course = StudyTaskManager.getInstance(project).course!!
 
     doNotAddedTest(course)
   }
 
   private fun ItemContainer.removeLastItem() {
     val itemsCopy = items.toMutableList()
-    itemsCopy.removeAt(1)
+    itemsCopy.removeAt(items.lastIndex)
     items = itemsCopy
   }
 
   private fun doAddedTest(itemContainer: ItemContainer) {
-    createConfigFiles()
-
+    val expectedSize = itemContainer.items.size
     itemContainer.removeLastItem()
 
     val configFile = itemContainer.getDir(project)!!.findChild(itemContainer.configFileName)!!
     YamlLoader.loadItem(project, configFile, null)
 
-
-    assertEquals(2, itemContainer.items.size)
+    assertEquals(expectedSize, itemContainer.items.size)
   }
 
   private fun doNotAddedTest(itemContainer: ItemContainer) {
-    createConfigFiles()
-
+    val expectedSize = itemContainer.items.size - 1
     itemContainer.removeLastItem()
 
     YamlFormatSynchronizer.saveItem(itemContainer)
@@ -206,7 +146,7 @@ class AddNewItemYamlTest : YamlTestCase() {
 
     loadParent(itemContainer)
 
-    assertEquals(1, itemContainer.items.size)
+    assertEquals(expectedSize, itemContainer.items.size)
   }
 
   private fun loadParent(parentItem: StudyItem) {
@@ -214,8 +154,4 @@ class AddNewItemYamlTest : YamlTestCase() {
     YamlLoader.loadItem(project, lessonConfigFile, null)
   }
 
-  private fun createConfigFiles() {
-    YamlFormatSynchronizer.saveAll(project)
-    FileDocumentManager.getInstance().saveAllDocuments()
-  }
 }
