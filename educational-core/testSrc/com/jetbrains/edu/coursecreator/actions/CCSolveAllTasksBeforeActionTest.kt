4fbf91154b13999fdb13f2bcb9ca39cd46bc9cb9
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2019-06-13 14:07:23 +0300

    EDU-2220: test "Solve All Tasks Before" action

---
+++ educational-core/testSrc/com/jetbrains/edu/coursecreator/actions/CCSolveAllTasksBeforeActionTest.kt
@@ -0,0 +1,129 @@
+package com.jetbrains.edu.coursecreator.actions
+
+import com.intellij.openapi.project.Project
+import com.intellij.testFramework.LightPlatformTestCase
+import com.jetbrains.edu.coursecreator.ui.SelectTaskUi
+import com.jetbrains.edu.coursecreator.ui.withMockSelectTaskUi
+import com.jetbrains.edu.learning.EduActionTestCase
+import com.jetbrains.edu.learning.courseFormat.EduCourse
+import com.jetbrains.edu.learning.courseFormat.tasks.Task
+import com.jetbrains.edu.learning.fileTree
+
+class CCSolveAllTasksBeforeActionTest : EduActionTestCase() {
+
+  fun `test solve all tasks before action`() {
+    courseWithFiles {
+      lesson("lesson1") {
+        eduTask("task1") {
+          taskFile("Foo.kt", "fn foo() = <p>TODO()</p>") {
+            placeholder(0, "123")
+          }
+          taskFile("Bar.kt", "fn bar(<p>params</p>) = <p>TODO()</p>") {
+            placeholder(0, "a: Int")
+            placeholder(1, "a + 10")
+          }
+        }
+        eduTask("task2") {
+          taskFile("Baz.kt", "fn baz() = <p>TODO()</p>") {
+            placeholder(0, "true")
+          }
+        }
+      }
+      lesson("lesson2") {
+        eduTask("task3") {
+          taskFile("Foo.kt", "fn foo() = <p>TODO()</p> + <p>TODO()</p>") {
+            placeholder(0, "456", dependency = "lesson1#task1#Foo.kt#1")
+            placeholder(1, "789")
+          }
+        }
+        eduTask("task4") {
+          taskFile("Qqq.kt", "fn qqq() = <p>TODO()</p>") {
+            placeholder(0, "1.23")
+          }
+        }
+      }
+    }
+
+    withMockSelectTaskUi(object : SelectTaskUi {
+      override fun selectTask(project: Project, course: EduCourse): Task? {
+        return course.findTask("lesson2", "task4")
+      }
+    }) {
+      myFixture.testAction(CCSolveAllTasksBeforeAction())
+    }
+
+    fileTree {
+      dir("lesson1") {
+        dir("task1") {
+          file("Foo.kt", "fn foo() = 123")
+          file("Bar.kt", "fn bar(a: Int) = a + 10")
+        }
+        dir("task2") {
+          file("Baz.kt", "fn baz() = true")
+        }
+      }
+      dir("lesson2") {
+        dir("task3") {
+          file("Foo.kt", "fn foo() = 123 + 789")
+        }
+        dir("task4") {
+          file("Qqq.kt", "fn qqq() = TODO()")
+        }
+      }
+    }.assertEquals(LightPlatformTestCase.getSourceRoot(), myFixture)
+  }
+
+  fun `test solve all tasks before action with framework lessons`() {
+    courseWithFiles {
+      frameworkLesson("lesson1") {
+        eduTask("task1") {
+          taskFile("Foo.kt", "fn foo() = <p>TODO()</p>") {
+            placeholder(0, "123")
+          }
+        }
+
+        eduTask("task2") {
+          taskFile("Foo.kt", "fn foo() = <p>TODO()</p>") {
+            placeholder(0, "123", dependency = "lesson1#task1#Foo.kt#1")
+          }
+          taskFile("Bar.kt", "fn bar(<p>params</p>) = <p>TODO()</p>") {
+            placeholder(0, "a: Int")
+            placeholder(1, "a + 10")
+          }
+        }
+
+        eduTask("task3") {
+          taskFile("Foo.kt", "fn foo() = <p>TODO()</p>") {
+            placeholder(0, "123", dependency = "lesson1#task2#Foo.kt#1")
+          }
+          taskFile("Bar.kt", "fn bar(<p>params</p>) = <p>TODO()</p>") {
+            placeholder(0, "b: Int", dependency = "lesson1#task2#Bar.kt#1")
+            placeholder(1, "b + 10", dependency = "lesson1#task2#Bar.kt#2")
+          }
+          taskFile("Baz.kt", "fn baz() = <p>TODO()</p> + <p>TODO()</p>") {
+            placeholder(0, "456", dependency = "lesson1#task2#Foo.kt#1")
+            placeholder(1, "789")
+          }
+        }
+      }
+    }
+
+    withMockSelectTaskUi(object : SelectTaskUi {
+      override fun selectTask(project: Project, course: EduCourse): Task? {
+        return course.findTask("lesson1", "task3")
+      }
+    }) {
+      myFixture.testAction(CCSolveAllTasksBeforeAction())
+    }
+
+    fileTree {
+      dir("lesson1") {
+        dir("task") {
+          file("Foo.kt", "fn foo() = 123")
+          file("Bar.kt", "fn bar(a: Int) = a + 10")
+          file("Baz.kt", "fn baz() = 123 + TODO()")
+        }
+      }
+    }.assertEquals(LightPlatformTestCase.getSourceRoot(), myFixture)
+  }
+}
