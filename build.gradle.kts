cad79f525340f42cc228e3112dfa08cda708ecab
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2019-06-13 15:41:19 +0300

    Update gradle intellij plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,7 +45,7 @@ val clionSandbox = "${project.buildDir.absolutePath}/clion-sandbox"
 plugins {
   idea
   kotlin("jvm") version "1.3.11"
-  id("org.jetbrains.intellij") version "0.4.5"
+  id("org.jetbrains.intellij") version "0.4.9"
   id("de.undercouch.download") version "3.4.3"
   id("net.saliman.properties") version "1.4.6"
 }
