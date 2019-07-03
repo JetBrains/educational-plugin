0f89f9501e60de24542edaaaa6e4a5a4c66199c6
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2019-07-03 17:54:40 +0300

    Move setting of allowed roots to test code

--- Edu-YAML/src/com/jetbrains/edu/yaml/EduYamlSchemaProviderFactory.kt
+++ Edu-YAML/src/com/jetbrains/edu/yaml/EduYamlSchemaProviderFactory.kt
@@ -1,41 +1,37 @@
 package com.jetbrains.edu.yaml
 
+import com.google.common.annotations.VisibleForTesting
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.vfs.VfsUtilCore
 import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.openapi.vfs.newvfs.impl.VfsRootAccess
 import com.jetbrains.edu.coursecreator.yaml.YamlFormatSettings
 import com.jetbrains.edu.learning.EduNames
-import com.jetbrains.edu.learning.isUnitTestMode
 import com.jetbrains.jsonSchema.extension.JsonSchemaFileProvider
 import com.jetbrains.jsonSchema.extension.JsonSchemaProviderFactory
 import com.jetbrains.jsonSchema.extension.SchemaType
 import com.jetbrains.jsonSchema.impl.JsonSchemaVersion
 
 class EduYamlSchemaProviderFactory : JsonSchemaProviderFactory {
-  override fun getProviders(project: Project): List<JsonSchemaFileProvider> {
-    return listOf(StudyItemConfigSchemaProvider(project, EduNames.COURSE),
-                  StudyItemConfigSchemaProvider(project, EduNames.SECTION),
-                  StudyItemConfigSchemaProvider(project, EduNames.LESSON),
-                  StudyItemConfigSchemaProvider(project, EduNames.TASK))
+  override fun getProviders(project: Project): List<StudyItemConfigSchemaProvider> {
+    return listOf(StudyItemConfigSchemaProvider(EduNames.COURSE),
+                  StudyItemConfigSchemaProvider(EduNames.SECTION),
+                  StudyItemConfigSchemaProvider(EduNames.LESSON),
+                  StudyItemConfigSchemaProvider(EduNames.TASK))
   }
 
-  private class StudyItemConfigSchemaProvider(private val project: Project, private val itemKind: String) : JsonSchemaFileProvider {
+  class StudyItemConfigSchemaProvider(private val itemKind: String) : JsonSchemaFileProvider {
     override fun getName(): String = "${itemKind.capitalize()} Configuration"
 
     override fun isAvailable(file: VirtualFile): Boolean = YamlFormatSettings.getLocalConfigFileName(itemKind) == file.name
 
     override fun getSchemaFile(): VirtualFile? {
-      val resourcePath = "/yaml/${itemKind}-schema.json"
-      if (isUnitTestMode) {
-        val path = VfsUtilCore.urlToPath(EduYamlSchemaProviderFactory::class.java.getResource(resourcePath).path)
-        VfsRootAccess.allowRootAccess(project, path)
-      }
-      return JsonSchemaProviderFactory.getResourceFile(EduYamlSchemaProviderFactory::class.java, resourcePath)
+      return JsonSchemaProviderFactory.getResourceFile(EduYamlSchemaProviderFactory::class.java, getSchemaResourcePath())
     }
 
     override fun getSchemaType(): SchemaType = SchemaType.embeddedSchema
 
     override fun getSchemaVersion(): JsonSchemaVersion = JsonSchemaVersion.SCHEMA_7
+
+    @VisibleForTesting
+    fun getSchemaResourcePath(): String = "/yaml/${itemKind}-schema.json"
   }
 }
--- Edu-YAML/testSrc/com/jetbrains/edu/yaml/YamlCodeInsightTest.kt
+++ Edu-YAML/testSrc/com/jetbrains/edu/yaml/YamlCodeInsightTest.kt
@@ -2,12 +2,25 @@ package com.jetbrains.edu.yaml
 
 import com.intellij.openapi.application.runWriteAction
 import com.intellij.openapi.fileEditor.FileDocumentManager
+import com.intellij.openapi.vfs.VfsUtilCore
+import com.intellij.openapi.vfs.newvfs.impl.VfsRootAccess
 import com.intellij.testFramework.EditorTestUtil
 import com.jetbrains.edu.coursecreator.yaml.YamlFormatSynchronizer.configFileName
 import com.jetbrains.edu.learning.EduTestCase
 import com.jetbrains.edu.learning.courseFormat.StudyItem
+import com.jetbrains.jsonSchema.extension.JsonSchemaProviderFactory
 
 abstract class YamlCodeInsightTest : EduTestCase() {
+
+  override fun setUp() {
+    super.setUp()
+    val factory = JsonSchemaProviderFactory.EP_NAME.findExtension(EduYamlSchemaProviderFactory::class.java)
+    factory?.getProviders(project)?.forEach { provider ->
+      val schemaResourcePath = provider.getSchemaResourcePath()
+      val path = VfsUtilCore.urlToPath(EduYamlSchemaProviderFactory::class.java.getResource(schemaResourcePath).path)
+      VfsRootAccess.allowRootAccess(testRootDisposable, path)
+    }
+  }
 
   /**
    * Opens this config in editor, supports placing caret, selection and
