1fd8f773f5ec9edfe019e4ebe1f265d5d4575920
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2019-07-03 18:00:02 +0300

    Provide correct path to scheme resource file

--- Edu-YAML/testSrc/com/jetbrains/edu/yaml/YamlCodeInsightTest.kt
+++ Edu-YAML/testSrc/com/jetbrains/edu/yaml/YamlCodeInsightTest.kt
@@ -2,13 +2,13 @@ package com.jetbrains.edu.yaml
 
 import com.intellij.openapi.application.runWriteAction
 import com.intellij.openapi.fileEditor.FileDocumentManager
-import com.intellij.openapi.vfs.VfsUtilCore
 import com.intellij.openapi.vfs.newvfs.impl.VfsRootAccess
 import com.intellij.testFramework.EditorTestUtil
 import com.jetbrains.edu.coursecreator.yaml.YamlFormatSynchronizer.configFileName
 import com.jetbrains.edu.learning.EduTestCase
 import com.jetbrains.edu.learning.courseFormat.StudyItem
 import com.jetbrains.jsonSchema.extension.JsonSchemaProviderFactory
+import java.nio.file.Paths
 
 abstract class YamlCodeInsightTest : EduTestCase() {
 
@@ -17,8 +17,9 @@ abstract class YamlCodeInsightTest : EduTestCase() {
     val factory = JsonSchemaProviderFactory.EP_NAME.findExtension(EduYamlSchemaProviderFactory::class.java)
     factory?.getProviders(project)?.forEach { provider ->
       val schemaResourcePath = provider.getSchemaResourcePath()
-      val path = VfsUtilCore.urlToPath(EduYamlSchemaProviderFactory::class.java.getResource(schemaResourcePath).path)
-      VfsRootAccess.allowRootAccess(testRootDisposable, path)
+      val uri = EduYamlSchemaProviderFactory::class.java.getResource(schemaResourcePath).toURI()
+      val absolutePath = Paths.get(uri).toAbsolutePath().toString()
+      VfsRootAccess.allowRootAccess(testRootDisposable, absolutePath)
     }
   }
 
