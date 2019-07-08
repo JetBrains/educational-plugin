aa128e2df14fa1c99f58ef670f4cd282e6bf91aa
dmitry.nefedov <nefedov270399@yandex.ru>
2019-07-08 16:24:30 +0300

    EDU-2033 Rewrite CppTheoryTaskChecker

--- Edu-Cpp/src/com/jetbrains/edu/cpp/checker/CppTheoryTaskChecker.kt
+++ Edu-Cpp/src/com/jetbrains/edu/cpp/checker/CppTheoryTaskChecker.kt
@@ -3,17 +3,12 @@ package com.jetbrains.edu.cpp.checker
 import com.intellij.execution.RunManager
 import com.intellij.execution.RunnerAndConfigurationSettings
 import com.intellij.openapi.project.Project
-import com.jetbrains.edu.cpp.CppCourseProjectGenerator
 import com.jetbrains.edu.learning.checker.TheoryTaskChecker
-import com.jetbrains.edu.learning.courseFormat.Lesson
-import com.jetbrains.edu.learning.courseFormat.Section
 import com.jetbrains.edu.learning.courseFormat.tasks.TheoryTask
 
 class CppTheoryTaskChecker(task: TheoryTask, project: Project) : TheoryTaskChecker(task, project) {
   override fun createTestConfiguration(): RunnerAndConfigurationSettings? {
-    val lesson: Lesson = task.lesson
-    val section: Section? = lesson.section
-    val taskProjectName = CppCourseProjectGenerator.getCMakeProjectUniqueName(section, lesson, task)
+    val taskProjectName = generateCMakeProjectUniqueName(task)
     return RunManager.getInstance(project).findConfigurationByName(taskProjectName)
   }
 }
