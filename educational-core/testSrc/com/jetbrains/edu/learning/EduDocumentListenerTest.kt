e20b484c4d65046ef74391ade7a19a097e235998
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2019-07-17 19:37:52 +0300

    EDU-2329: test placeholder state after find and replace action

--- educational-core/testSrc/com/jetbrains/edu/learning/EduDocumentListenerTest.kt
+++ educational-core/testSrc/com/jetbrains/edu/learning/EduDocumentListenerTest.kt
@@ -1,14 +1,24 @@
 package com.jetbrains.edu.learning
 
+import com.intellij.find.FindModel
+import com.intellij.find.impl.FindInProjectUtil
+import com.intellij.find.replaceInProject.ReplaceInProjectManager
+import com.intellij.openapi.application.runUndoTransparentWriteAction
 import com.intellij.openapi.application.runWriteAction
 import com.intellij.openapi.command.CommandProcessor
 import com.intellij.openapi.fileTypes.PlainTextLanguage
+import com.intellij.usageView.UsageInfo
+import com.intellij.usages.FindUsagesProcessPresentation
+import com.intellij.usages.UsageInfo2UsageAdapter
+import com.intellij.util.CommonProcessors
+import com.jetbrains.edu.learning.courseFormat.AnswerPlaceholder
 import com.jetbrains.edu.learning.courseFormat.EduCourse
+import com.jetbrains.edu.learning.courseFormat.ext.getDocument
 import java.io.IOException
 
 class EduDocumentListenerTest : EduTestCase() {
 
-  fun testTypeInPlaceholder() {
+  fun `test type in placeholder`() {
     val lessonIndex = 1
     val taskIndex = 1
     val taskFileName = "taskFile1.txt"
@@ -21,11 +31,10 @@ class EduDocumentListenerTest : EduTestCase() {
     val taskFile = task!!.getTaskFile(taskFileName)
     val answerPlaceholders = taskFile!!.answerPlaceholders
     assertEquals(1, answerPlaceholders.size)
-    assertEquals(12, answerPlaceholders[0].offset)
-    assertEquals(6, answerPlaceholders[0].length)
+    assertEquals("itests", answerPlaceholders[0].currentText)
   }
 
-  fun testTypeBeforePlaceholder() {
+  fun `test type before placeholder`() {
     val lessonIndex = 1
     val taskIndex = 1
     val taskFileName = "taskFile1.txt"
@@ -38,11 +47,10 @@ class EduDocumentListenerTest : EduTestCase() {
     val taskFile = task!!.getTaskFile(taskFileName)
     val answerPlaceholders = taskFile!!.answerPlaceholders
     assertEquals(1, answerPlaceholders.size)
-    assertEquals(16, answerPlaceholders[0].offset)
-    assertEquals(2, answerPlaceholders[0].length)
+    assertEquals("is", answerPlaceholders[0].currentText)
   }
 
-  fun testTypeBetweenPlaceholder() {
+  fun `test type between placeholder`() {
     val lessonIndex = 1
     val taskIndex = 2
     val taskFileName = "taskFile2.txt"
@@ -55,13 +63,11 @@ class EduDocumentListenerTest : EduTestCase() {
     val taskFile = task!!.getTaskFile(taskFileName)
     val answerPlaceholders = taskFile!!.answerPlaceholders
     assertEquals(2, answerPlaceholders.size)
-    assertEquals(12, answerPlaceholders[0].offset)
-    assertEquals(2, answerPlaceholders[0].length)
-    assertEquals(44, answerPlaceholders[1].offset)
-    assertEquals(10, answerPlaceholders[1].length)
+    assertEquals("is", answerPlaceholders[0].currentText)
+    assertEquals("is another", answerPlaceholders[1].currentText)
   }
 
-  fun testTypeInSecondPlaceholder() {
+  fun `test type in second placeholder`() {
     val lessonIndex = 1
     val taskIndex = 2
     val taskFileName = "taskFile2.txt"
@@ -74,13 +80,11 @@ class EduDocumentListenerTest : EduTestCase() {
     val taskFile = task!!.getTaskFile(taskFileName)
     val answerPlaceholders = taskFile!!.answerPlaceholders
     assertEquals(2, answerPlaceholders.size)
-    assertEquals(12, answerPlaceholders[0].offset)
-    assertEquals(2, answerPlaceholders[0].length)
-    assertEquals(40, answerPlaceholders[1].offset)
-    assertEquals(14, answerPlaceholders[1].length)
+    assertEquals("is", answerPlaceholders[0].currentText)
+    assertEquals("testis another", answerPlaceholders[1].currentText)
   }
 
-  fun testTypeInFirstPlaceholder() {
+  fun `test type in first placeholder`() {
     val lessonIndex = 1
     val taskIndex = 2
     val taskFileName = "taskFile2.txt"
@@ -93,13 +97,11 @@ class EduDocumentListenerTest : EduTestCase() {
     val taskFile = task!!.getTaskFile(taskFileName)
     val answerPlaceholders = taskFile!!.answerPlaceholders
     assertEquals(2, answerPlaceholders.size)
-    assertEquals(12, answerPlaceholders[0].offset)
-    assertEquals(6, answerPlaceholders[0].length)
-    assertEquals(44, answerPlaceholders[1].offset)
-    assertEquals(10, answerPlaceholders[1].length)
+    assertEquals("testis", answerPlaceholders[0].currentText)
+    assertEquals("is another", answerPlaceholders[1].currentText)
   }
 
-  fun testTypeAfterLastPlaceholder() {
+  fun `test type after last placeholder`() {
     val lessonIndex = 1
     val taskIndex = 2
     val taskFileName = "taskFile2.txt"
@@ -112,13 +114,12 @@ class EduDocumentListenerTest : EduTestCase() {
     val taskFile = task!!.getTaskFile(taskFileName)
     val answerPlaceholders = taskFile!!.answerPlaceholders
     assertEquals(2, answerPlaceholders.size)
-    assertEquals(12, answerPlaceholders[0].offset)
-    assertEquals(2, answerPlaceholders[0].length)
-    assertEquals(40, answerPlaceholders[1].offset)
-    assertEquals(10, answerPlaceholders[1].length)
+    assertEquals("is", answerPlaceholders[0].currentText)
+    assertEquals("is another", answerPlaceholders[1].currentText)
+
   }
 
-  fun testNewLineAtTheBeginningPlaceholder() {
+  fun `test new line at the beginning placeholder`() {
     val lessonIndex = 1
     val taskIndex = 1
     val taskFileName = "taskFile1.txt"
@@ -131,98 +132,92 @@ class EduDocumentListenerTest : EduTestCase() {
     val taskFile = task!!.getTaskFile(taskFileName)
     val answerPlaceholders = taskFile!!.answerPlaceholders
     assertEquals(1, answerPlaceholders.size)
-    assertEquals(12, answerPlaceholders[0].offset)
-    assertEquals(3, answerPlaceholders[0].length)
+    assertEquals("i\ns", answerPlaceholders[0].currentText)
   }
 
-  fun testDeleteBeforePlaceholder() {
+  fun `test delete before placeholder`() {
     val taskFileName = "taskFile2.txt"
     configureByTaskFile(1, 2, taskFileName)
 
-    CommandProcessor.getInstance().runUndoTransparentAction { runWriteAction {
-      myFixture.getDocument(myFixture.file).deleteString(5, 11) } }
+    runUndoTransparentWriteAction {
+      myFixture.getDocument(myFixture.file).deleteString(5, 11)
+    }
 
     val lesson = StudyTaskManager.getInstance(myFixture.project).course!!.getLesson("lesson1")
     val task = lesson!!.getTask("task2")
     val taskFile = task!!.getTaskFile(taskFileName)
     val answerPlaceholders = taskFile!!.answerPlaceholders
     assertEquals(2, answerPlaceholders.size)
-    assertEquals(6, answerPlaceholders[0].offset)
-    assertEquals(2, answerPlaceholders[0].length)
-    assertEquals(34, answerPlaceholders[1].offset)
-    assertEquals(10, answerPlaceholders[1].length)
+    assertEquals("is", answerPlaceholders[0].currentText)
+    assertEquals("is another", answerPlaceholders[1].currentText)
   }
 
-  fun testDeleteBetweenPlaceholder() {
+  fun `test delete between placeholder`() {
     val taskFileName = "taskFile2.txt"
     configureByTaskFile(1, 2, taskFileName)
 
-    CommandProcessor.getInstance().runUndoTransparentAction { runWriteAction {
-      myFixture.getDocument(myFixture.file).deleteString(33, 39) } }
+    runUndoTransparentWriteAction {
+      myFixture.getDocument(myFixture.file).deleteString(33, 39)
+    }
 
     val lesson = StudyTaskManager.getInstance(myFixture.project).course!!.getLesson("lesson1")
     val task = lesson!!.getTask("task2")
     val taskFile = task!!.getTaskFile(taskFileName)
     val answerPlaceholders = taskFile!!.answerPlaceholders
     assertEquals(2, answerPlaceholders.size)
-    assertEquals(12, answerPlaceholders[0].offset)
-    assertEquals(2, answerPlaceholders[0].length)
-    assertEquals(34, answerPlaceholders[1].offset)
-    assertEquals(10, answerPlaceholders[1].length)
+    assertEquals("is", answerPlaceholders[0].currentText)
+    assertEquals("is another", answerPlaceholders[1].currentText)
   }
 
-  fun testDeleteAfterPlaceholder() {
+  fun `test delete after placeholder`() {
     val taskFileName = "taskFile2.txt"
     configureByTaskFile(1, 2, taskFileName)
 
-    CommandProcessor.getInstance().runUndoTransparentAction { runWriteAction {
-      myFixture.getDocument(myFixture.file).deleteString(55, 58) } }
+    runUndoTransparentWriteAction {
+      myFixture.getDocument(myFixture.file).deleteString(55, 58)
+    }
 
     val lesson = StudyTaskManager.getInstance(myFixture.project).course!!.getLesson("lesson1")
     val task = lesson!!.getTask("task2")
     val taskFile = task!!.getTaskFile(taskFileName)
     val answerPlaceholders = taskFile!!.answerPlaceholders
     assertEquals(2, answerPlaceholders.size)
-    assertEquals(12, answerPlaceholders[0].offset)
-    assertEquals(2, answerPlaceholders[0].length)
-    assertEquals(40, answerPlaceholders[1].offset)
-    assertEquals(10, answerPlaceholders[1].length)
+    assertEquals("is", answerPlaceholders[0].currentText)
+    assertEquals("is another", answerPlaceholders[1].currentText)
   }
 
-  fun testDeleteFirstPlaceholderStart() {
+  fun `test delete first placeholder start`() {
     val taskFileName = "taskFile2.txt"
     configureByTaskFile(1, 2, taskFileName)
 
-    CommandProcessor.getInstance().runUndoTransparentAction { runWriteAction {
-      myFixture.getDocument(myFixture.file).deleteString(11, 13) } }
+    runUndoTransparentWriteAction {
+      myFixture.getDocument(myFixture.file).deleteString(11, 13)
+    }
 
     val lesson = StudyTaskManager.getInstance(myFixture.project).course!!.getLesson("lesson1")
     val task = lesson!!.getTask("task2")
     val taskFile = task!!.getTaskFile(taskFileName)
     val answerPlaceholders = taskFile!!.answerPlaceholders
     assertEquals(2, answerPlaceholders.size)
-    assertEquals(11, answerPlaceholders[0].offset)
-    assertEquals(1, answerPlaceholders[0].length)
-    assertEquals(38, answerPlaceholders[1].offset)
-    assertEquals(10, answerPlaceholders[1].length)
+    assertEquals("s", answerPlaceholders[0].currentText)
+    assertEquals("is another", answerPlaceholders[1].currentText)
   }
 
-  fun testDeleteFirstPlaceholderEnd() {
+  fun `test delete first placeholder end`() {
     val taskFileName = "taskFile2.txt"
     configureByTaskFile(1, 2, taskFileName)
 
-    CommandProcessor.getInstance().runUndoTransparentAction { runWriteAction {
-      myFixture.getDocument(myFixture.file).deleteString(13, 15) } }
+    runUndoTransparentWriteAction {
+      myFixture.getDocument(myFixture.file).deleteString(13, 15)
+    }
 
     val lesson = StudyTaskManager.getInstance(myFixture.project).course!!.getLesson("lesson1")
     val task = lesson!!.getTask("task2")
     val taskFile = task!!.getTaskFile(taskFileName)
     val answerPlaceholders = taskFile!!.answerPlaceholders
     assertEquals(2, answerPlaceholders.size)
-    assertEquals(12, answerPlaceholders[0].offset)
-    assertEquals(1, answerPlaceholders[0].length)
-    assertEquals(38, answerPlaceholders[1].offset)
-    assertEquals(10, answerPlaceholders[1].length)
+    assertEquals("i", answerPlaceholders[0].currentText)
+    assertEquals("is another", answerPlaceholders[1].currentText)
   }
 
   fun `test delete from start beyond end`() {
@@ -237,8 +232,7 @@ class EduDocumentListenerTest : EduTestCase() {
     val taskFile = task!!.getTaskFile(taskFileName)
     val answerPlaceholders = taskFile!!.answerPlaceholders
     assertEquals(1, answerPlaceholders.size)
-    assertEquals(12, answerPlaceholders[0].offset)
-    assertEquals(4, answerPlaceholders[0].length)
+    assertEquals("test", answerPlaceholders[0].currentText)
   }
 
   fun `test delete before start beyond end`() {
@@ -253,29 +247,27 @@ class EduDocumentListenerTest : EduTestCase() {
     val taskFile = task!!.getTaskFile(taskFileName)
     val answerPlaceholders = taskFile!!.answerPlaceholders
     assertEquals(1, answerPlaceholders.size)
-    assertEquals(10, answerPlaceholders[0].offset)
-    assertEquals(4, answerPlaceholders[0].length)
+    assertEquals("test", answerPlaceholders[0].currentText)
   }
 
-  fun testDeleteSecondPlaceholderStart() {
+  fun `test delete second placeholder start`() {
     val taskFileName = "taskFile2.txt"
     configureByTaskFile(1, 2, taskFileName)
 
-    CommandProcessor.getInstance().runUndoTransparentAction { runWriteAction {
-      myFixture.getDocument(myFixture.file).deleteString(39, 41) } }
+    runUndoTransparentWriteAction {
+      myFixture.getDocument(myFixture.file).deleteString(39, 41)
+    }
 
     val lesson = StudyTaskManager.getInstance(myFixture.project).course!!.getLesson("lesson1")
     val task = lesson!!.getTask("task2")
     val taskFile = task!!.getTaskFile(taskFileName)
     val answerPlaceholders = taskFile!!.answerPlaceholders
     assertEquals(2, answerPlaceholders.size)
-    assertEquals(12, answerPlaceholders[0].offset)
-    assertEquals(2, answerPlaceholders[0].length)
-    assertEquals(39, answerPlaceholders[1].offset)
-    assertEquals(9, answerPlaceholders[1].length)
+    assertEquals("is", answerPlaceholders[0].currentText)
+    assertEquals("s another", answerPlaceholders[1].currentText)
   }
 
-  fun testDeleteSecondPlaceholderEnd() {
+  fun `test delete second placeholder end`() {
     val taskFileName = "taskFile2.txt"
     configureByTaskFile(1, 2, taskFileName)
 
@@ -287,10 +279,47 @@ class EduDocumentListenerTest : EduTestCase() {
     val taskFile = task!!.getTaskFile(taskFileName)
     val answerPlaceholders = taskFile!!.answerPlaceholders
     assertEquals(2, answerPlaceholders.size)
-    assertEquals(12, answerPlaceholders[0].offset)
-    assertEquals(2, answerPlaceholders[0].length)
-    assertEquals(40, answerPlaceholders[1].offset)
-    assertEquals(9, answerPlaceholders[1].length)
+    assertEquals("is", answerPlaceholders[0].currentText)
+    assertEquals("is anothe", answerPlaceholders[1].currentText)
+
+  }
+
+  fun `test find and replace in whole project`() {
+    val findModel = FindModel().apply {
+      stringToFind = "There"
+      stringToReplace = ""
+      isWholeWordsOnly = true
+      isFromCursor = false
+      isGlobal = true
+      isMultipleFiles = true
+      isProjectScope = true
+      isRegularExpressions = false
+      isPromptOnReplace = false
+    }
+
+    val usages = mutableListOf<UsageInfo>()
+    val collector = CommonProcessors.CollectProcessor(usages)
+    FindInProjectUtil
+      .findUsages(findModel, project, collector, FindUsagesProcessPresentation(FindInProjectUtil.setupViewPresentation(true, findModel)))
+
+    CommandProcessor.getInstance().executeCommand(project, {
+      val replaceManager = ReplaceInProjectManager.getInstance(project)
+      for (usage in usages) {
+        replaceManager.replaceUsage(UsageInfo2UsageAdapter(usage), findModel, emptySet(), false)
+      }
+    }, "", null)
+
+    val placeholder1 = getCourse().findTask("lesson1", "task1").getTaskFile("taskFile1.txt")!!.answerPlaceholders[0]
+    assertEquals("is", placeholder1.currentText)
+    val placeholder2 = getCourse().findTask("lesson1", "task2").getTaskFile("taskFile2.txt")!!.answerPlaceholders[0]
+    assertEquals("is", placeholder2.currentText)
+    val placeholder3 = getCourse().findTask("lesson1", "task2").getTaskFile("taskFile2.txt")!!.answerPlaceholders[1]
+    assertEquals("is another", placeholder3.currentText)
+  }
+
+  private val AnswerPlaceholder.currentText: String get() {
+    val document = taskFile.getDocument(project)!!
+    return document.text.substring(offset, endOffset)
   }
 
   @Throws(IOException::class)
