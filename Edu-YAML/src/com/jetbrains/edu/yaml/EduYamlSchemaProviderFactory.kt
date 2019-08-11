6c9127f1e210f95b837cd2b650f108f9d09378bf
Valentina Kiryushkina <valentina.kiryushkina@jetbrains.com>
2019-08-11 15:21:49 +0300

    EDU-1997 Apply schema for cc project only

--- Edu-YAML/src/com/jetbrains/edu/yaml/EduYamlSchemaProviderFactory.kt
+++ Edu-YAML/src/com/jetbrains/edu/yaml/EduYamlSchemaProviderFactory.kt
@@ -3,8 +3,9 @@ package com.jetbrains.edu.yaml
 import com.google.common.annotations.VisibleForTesting
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
-import com.jetbrains.edu.coursecreator.yaml.YamlFormatSettings
+import com.jetbrains.edu.coursecreator.CCUtils
 import com.jetbrains.edu.learning.EduNames
+import com.jetbrains.edu.learning.yaml.YamlFormatSettings
 import com.jetbrains.jsonSchema.extension.JsonSchemaFileProvider
 import com.jetbrains.jsonSchema.extension.JsonSchemaProviderFactory
 import com.jetbrains.jsonSchema.extension.SchemaType
@@ -12,16 +13,17 @@ import com.jetbrains.jsonSchema.impl.JsonSchemaVersion
 
 class EduYamlSchemaProviderFactory : JsonSchemaProviderFactory {
   override fun getProviders(project: Project): List<StudyItemConfigSchemaProvider> {
-    return listOf(StudyItemConfigSchemaProvider(EduNames.COURSE),
-                  StudyItemConfigSchemaProvider(EduNames.SECTION),
-                  StudyItemConfigSchemaProvider(EduNames.LESSON),
-                  StudyItemConfigSchemaProvider(EduNames.TASK))
+    return listOf(StudyItemConfigSchemaProvider(project, EduNames.COURSE),
+                  StudyItemConfigSchemaProvider(project, EduNames.SECTION),
+                  StudyItemConfigSchemaProvider(project, EduNames.LESSON),
+                  StudyItemConfigSchemaProvider(project, EduNames.TASK))
   }
 
-  class StudyItemConfigSchemaProvider(private val itemKind: String) : JsonSchemaFileProvider {
+  class StudyItemConfigSchemaProvider(private val project: Project, private val itemKind: String) : JsonSchemaFileProvider {
     override fun getName(): String = "${itemKind.capitalize()} Configuration"
 
-    override fun isAvailable(file: VirtualFile): Boolean = YamlFormatSettings.getLocalConfigFileName(itemKind) == file.name
+    override fun isAvailable(file: VirtualFile): Boolean = CCUtils.isCourseCreator(project)
+                                                           && YamlFormatSettings.getLocalConfigFileName(itemKind) == file.name
 
     override fun getSchemaFile(): VirtualFile? {
       return JsonSchemaProviderFactory.getResourceFile(EduYamlSchemaProviderFactory::class.java, getSchemaResourcePath())
