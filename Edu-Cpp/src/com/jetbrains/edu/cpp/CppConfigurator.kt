3876a14811efd07df8d3277dc5b4031731bd3ae3
dmitry.nefedov <nefedov270399@yandex.ru>
2019-09-27 01:24:41 +0300

    EDU-2586 Add ignoring test folder

--- Edu-Cpp/src/com/jetbrains/edu/cpp/CppConfigurator.kt
+++ Edu-Cpp/src/com/jetbrains/edu/cpp/CppConfigurator.kt
@@ -7,6 +7,8 @@ import com.jetbrains.edu.learning.EduCourseBuilder
 import com.jetbrains.edu.learning.EduNames
 import com.jetbrains.edu.learning.checker.TaskCheckerProvider
 import com.jetbrains.edu.learning.configuration.EduConfiguratorWithSubmissions
+import com.jetbrains.edu.learning.course
+import com.jetbrains.edu.learning.courseGeneration.GeneratorUtils
 import com.jetbrains.edu.learning.courseGeneration.GeneratorUtils.getInternalTemplateText
 import icons.EducationalCoreIcons
 import javax.swing.Icon
@@ -32,8 +34,19 @@ class CppConfigurator : EduConfiguratorWithSubmissions<CppProjectSettings>() {
 
   override fun isCourseCreatorEnabled(): Boolean = true
 
-  override fun excludeFromArchive(project: Project, file: VirtualFile): Boolean =
-    super.excludeFromArchive(project, file) || file.path.contains("cmake-build-debug")
+  override fun excludeFromArchive(project: Project, file: VirtualFile): Boolean {
+    if (super.excludeFromArchive(project, file)) {
+      return true
+    }
+
+    val courseDir = project.course?.getDir(project) ?: return false
+    // we could use it how indicator because CLion generate build dirs with names `cmake-build-*`
+    // @see com.jetbrains.cidr.cpp.cmake.workspace.CMakeWorkspace.getProfileGenerationDirNames
+    val buildDirPrefix = GeneratorUtils.joinPaths(courseDir.path, "cmake-build-")
+    val googleTestDirPrefix = GeneratorUtils.joinPaths(courseDir.path, "googletest")
+
+    return file.path.startsWith(buildDirPrefix) || file.path.startsWith(googleTestDirPrefix)
+  }
 
   override fun getLogo(): Icon = EducationalCoreIcons.CppLogo
 
