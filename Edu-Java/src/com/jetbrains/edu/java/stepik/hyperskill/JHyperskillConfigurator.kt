cc9a36c9e08711b088281cc2754cf7d6dd48d409
Ekaterina Tuzova <ekaterina.tuzova@jetbrains.com>
2019-10-03 12:50:15 +0300

    EDU-2608 "No tests have run" in Android projects in Hyperskill

--- Edu-Java/src/com/jetbrains/edu/java/stepik/hyperskill/JHyperskillConfigurator.kt
+++ Edu-Java/src/com/jetbrains/edu/java/stepik/hyperskill/JHyperskillConfigurator.kt
@@ -1,11 +1,12 @@
 package com.jetbrains.edu.java.stepik.hyperskill
 
-import com.intellij.openapi.project.Project
 import com.intellij.lang.java.JavaLanguage
+import com.intellij.openapi.project.Project
 import com.jetbrains.edu.jvm.JdkProjectSettings
 import com.jetbrains.edu.jvm.gradle.GradleConfiguratorBase
 import com.jetbrains.edu.jvm.stepik.fileName
 import com.jetbrains.edu.jvm.stepik.hyperskill.HyperskillGradleTaskCheckerProvider
+import com.jetbrains.edu.learning.EduNames
 import com.jetbrains.edu.learning.courseFormat.tasks.Task
 import com.jetbrains.edu.learning.stepik.hyperskill.HyperskillConfigurator
 import javax.swing.JPanel
@@ -18,4 +19,5 @@ class JHyperskillConfigurator : GradleConfiguratorBase(), HyperskillConfigurator
     return super<HyperskillConfigurator>.additionalTaskTab(currentTask, project)
   }
   override fun getMockFileName(text: String) = fileName(JavaLanguage.INSTANCE, text)
+  override fun getTestDirs(): MutableList<String> = mutableListOf("${EduNames.TEST}/stageTest", EduNames.TEST)
 }
