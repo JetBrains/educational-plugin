9ca9248f250acbe8a357928b39d84512dab137ab
zinaida.smirnova <zinaida.smirnova@jetbrains.com>
2019-10-04 17:36:29 +0300

    EDU-2439 Stepik icon in status bar missing fixed
    (cherry picked from commit c5d520546589979cc4a95f59a4ee356678d2d122)

--- educational-core/src/com/jetbrains/edu/learning/stepik/StepikWidgetProvider.kt
+++ educational-core/src/com/jetbrains/edu/learning/stepik/StepikWidgetProvider.kt
@@ -1,8 +1,8 @@
 package com.jetbrains.edu.learning.stepik
 
 import com.intellij.openapi.project.Project
-import com.jetbrains.edu.learning.LoginWidgetProvider
 import com.jetbrains.edu.learning.EduNames
+import com.jetbrains.edu.learning.LoginWidgetProvider
 import com.jetbrains.edu.learning.courseFormat.Course
 import com.jetbrains.edu.learning.courseFormat.EduCourse
 
@@ -10,7 +10,8 @@ class StepikWidgetProvider : LoginWidgetProvider() {
 
   override fun createLoginWidget(project: Project) = StepikWidget(project)
 
-  override fun isWidgetAvailable(courseType: String, languageId: String?) = courseType == EduNames.PYCHARM
+  override fun isWidgetAvailable(courseType: String,
+                                 languageId: String?) = courseType == EduNames.PYCHARM || courseType == StepikNames.STEPIK_TYPE
 
   override fun isWidgetAvailable(course: Course) = course is EduCourse
 }
