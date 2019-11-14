481465bda456fd43f9169f4d0230d041aacbc4fb
Olga Telezhnaya <olga.telezhnaya@jetbrains.com>
2019-11-14 12:42:00 +0300

    EDU-2136 Work smarter with go sdk, add validation

--- Edu-Go/src/com/jetbrains/edu/go/GoLanguageSettings.kt
+++ Edu-Go/src/com/jetbrains/edu/go/GoLanguageSettings.kt
@@ -1,21 +1,34 @@
 package com.jetbrains.edu.go
 
-import com.goide.GoConstants.GO_ROOT
+import com.goide.GoConstants.SDK_TYPE_ID
 import com.goide.sdk.GoSdk
 import com.goide.sdk.combobox.GoSdkChooserCombo
 import com.intellij.openapi.ui.LabeledComponent
 import com.intellij.openapi.util.UserDataHolder
 import com.jetbrains.edu.learning.LanguageSettings
 import com.jetbrains.edu.learning.courseFormat.Course
+import com.jetbrains.edu.learning.newproject.ui.ValidationMessage
 import java.awt.BorderLayout
 import javax.swing.JComponent
 
 class GoLanguageSettings : LanguageSettings<GoProjectSettings>() {
   private val sdkChooser: GoSdkChooserCombo = GoSdkChooserCombo()
 
-  override fun getSettings(): GoProjectSettings = GoProjectSettings(GoSdk.NULL)
+  init {
+    sdkChooser.addChangedListener {
+      notifyListeners()
+    }
+  }
 
-  override fun getLanguageSettingsComponents(course: Course, context: UserDataHolder?): MutableList<LabeledComponent<JComponent>> {
-    return mutableListOf(LabeledComponent.create(sdkChooser as JComponent, GO_ROOT, BorderLayout.WEST))
+  override fun getSettings(): GoProjectSettings = GoProjectSettings(sdkChooser.sdk)
+
+  override fun getLanguageSettingsComponents(course: Course, context: UserDataHolder?): List<LabeledComponent<JComponent>> {
+    return listOf(LabeledComponent.create(sdkChooser as JComponent, SDK_TYPE_ID, BorderLayout.WEST))
+  }
+
+  override fun validate(course: Course?, courseLocation: String?): ValidationMessage? {
+    if (sdkChooser.sdk == GoSdk.NULL) return ValidationMessage("Please specify $SDK_TYPE_ID")
+    val message = sdkChooser.validator.validate(sdkChooser.sdk).errorMessage ?: return null
+    return ValidationMessage(message)
   }
 }
