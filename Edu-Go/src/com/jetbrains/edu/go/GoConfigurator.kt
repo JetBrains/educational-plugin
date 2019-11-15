079e127da1d1e71b00f61d4b547b47000a18ba9c
Olga Telezhnaya <olga.telezhnaya@jetbrains.com>
2019-11-15 14:45:43 +0300

    EDU-2136 Make Go support experimental feature

--- Edu-Go/src/com/jetbrains/edu/go/GoConfigurator.kt
+++ Edu-Go/src/com/jetbrains/edu/go/GoConfigurator.kt
@@ -3,9 +3,12 @@ package com.jetbrains.edu.go
 import com.goide.GoIcons
 import com.jetbrains.edu.go.checker.GoEduTaskChecker
 import com.jetbrains.edu.learning.EduCourseBuilder
+import com.jetbrains.edu.learning.EduExperimentalFeatures
 import com.jetbrains.edu.learning.EduNames.TEST
 import com.jetbrains.edu.learning.checker.TaskCheckerProvider
 import com.jetbrains.edu.learning.configuration.EduConfiguratorWithSubmissions
+import com.jetbrains.edu.learning.isFeatureEnabled
+import com.jetbrains.edu.learning.isUnitTestMode
 import javax.swing.Icon
 
 class GoConfigurator : EduConfiguratorWithSubmissions<GoProjectSettings>() {
@@ -22,6 +25,8 @@ class GoConfigurator : EduConfiguratorWithSubmissions<GoProjectSettings>() {
   override fun getTaskCheckerProvider(): TaskCheckerProvider = TaskCheckerProvider { task, project -> GoEduTaskChecker(project, task) }
 
   override fun getLogo(): Icon = GoIcons.ICON
+
+  override fun isEnabled(): Boolean = isFeatureEnabled(EduExperimentalFeatures.GO_COURSES) || isUnitTestMode
 
   companion object {
     const val TEST_GO = "task_test.go"
--- educational-core/src/com/jetbrains/edu/learning/EduExperimentalFeatures.kt
+++ educational-core/src/com/jetbrains/edu/learning/EduExperimentalFeatures.kt
@@ -4,4 +4,5 @@ object EduExperimentalFeatures {
   const val ANDROID_COURSES = "com.jetbrains.edu.android"
   const val SPLIT_EDITOR = "edu.course.creator.split.editor"
   const val HYPERSKILL = "edu.course.hyperskill"
+  const val GO_COURSES = "com.jetbrains.edu.go"
 }
