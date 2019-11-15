c40d85e2d37b705996120bbdd44bfbbbe38f6583
Olga Telezhnaya <olga.telezhnaya@jetbrains.com>
2019-11-15 10:22:59 +0300

    EDU-2136 New default Go course structure

--- Edu-Go/src/com/jetbrains/edu/go/GoConfigurator.kt
+++ Edu-Go/src/com/jetbrains/edu/go/GoConfigurator.kt
@@ -3,6 +3,7 @@ package com.jetbrains.edu.go
 import com.goide.GoIcons
 import com.jetbrains.edu.go.checker.GoEduTaskChecker
 import com.jetbrains.edu.learning.EduCourseBuilder
+import com.jetbrains.edu.learning.EduNames.TEST
 import com.jetbrains.edu.learning.checker.TaskCheckerProvider
 import com.jetbrains.edu.learning.configuration.EduConfiguratorWithSubmissions
 import javax.swing.Icon
@@ -12,13 +13,19 @@ class GoConfigurator : EduConfiguratorWithSubmissions<GoProjectSettings>() {
 
   override fun getCourseBuilder(): EduCourseBuilder<GoProjectSettings> = courseBuilder
 
-  override fun getTestFileName() = ""
+  override fun getTestFileName() = TEST_GO
 
-  override fun getMockFileName(text: String): String = "task.go"
+  override fun getMockFileName(text: String): String = TASK_GO
 
-  override fun getTestDirs() = listOf("test")
+  override fun getTestDirs() = listOf(TEST)
 
   override fun getTaskCheckerProvider(): TaskCheckerProvider = TaskCheckerProvider { task, project -> GoEduTaskChecker(project, task) }
 
   override fun getLogo(): Icon = GoIcons.ICON
+
+  companion object {
+    const val TEST_GO = "task_test.go"
+    const val TASK_GO = "task.go"
+    const val MAIN_GO = "main.go"
+  }
 }
--- Edu-Go/src/com/jetbrains/edu/go/GoCourseBuilder.kt
+++ Edu-Go/src/com/jetbrains/edu/go/GoCourseBuilder.kt
@@ -1,16 +1,41 @@
 package com.jetbrains.edu.go
 
+import com.intellij.openapi.project.Project
+import com.jetbrains.edu.coursecreator.actions.NewStudyItemInfo
+import com.jetbrains.edu.coursecreator.actions.TemplateFileInfo
+import com.jetbrains.edu.go.GoConfigurator.Companion.MAIN_GO
+import com.jetbrains.edu.go.GoConfigurator.Companion.TASK_GO
+import com.jetbrains.edu.go.GoConfigurator.Companion.TEST_GO
 import com.jetbrains.edu.learning.EduCourseBuilder
+import com.jetbrains.edu.learning.EduNames.TEST
 import com.jetbrains.edu.learning.LanguageSettings
 import com.jetbrains.edu.learning.courseFormat.Course
+import com.jetbrains.edu.learning.courseFormat.Lesson
+import com.jetbrains.edu.learning.courseFormat.tasks.Task
+import com.jetbrains.edu.learning.courseGeneration.GeneratorUtils.joinPaths
 import com.jetbrains.edu.learning.newproject.CourseProjectGenerator
 
 class GoCourseBuilder : EduCourseBuilder<GoProjectSettings> {
-  override val taskTemplateName: String = "task.go"
-  override val testTemplateName: String = "task_test.go"
+  override val taskTemplateName: String = TASK_GO
+  override val testTemplateName: String = TEST_GO
 
   override fun getCourseProjectGenerator(course: Course): CourseProjectGenerator<GoProjectSettings> =
     GoCourseProjectGenerator(this, course)
 
   override fun getLanguageSettings(): LanguageSettings<GoProjectSettings> = GoLanguageSettings()
+
+  override fun initNewTask(project: Project, lesson: Lesson, task: Task, info: NewStudyItemInfo) {
+    if (task.taskFiles.isNotEmpty()) return
+    val params = mapOf("LESSON_NAME" to lesson.name, "TASK_NAME" to task.name)
+    for (templateInfo in defaultTaskFiles()) {
+      val taskFile = templateInfo.toTaskFile(params) ?: continue
+      task.addTaskFile(taskFile)
+    }
+  }
+
+  private fun defaultTaskFiles(): List<TemplateFileInfo> = listOf(
+    TemplateFileInfo(TASK_GO, TASK_GO, true),
+    TemplateFileInfo(MAIN_GO, joinPaths("main", MAIN_GO), true),
+    TemplateFileInfo(TEST_GO, joinPaths(TEST, TEST_GO), false)
+  )
 }
