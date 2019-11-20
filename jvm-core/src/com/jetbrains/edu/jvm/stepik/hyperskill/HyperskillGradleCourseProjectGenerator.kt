4df8533e5a5b05e395bfabd7f5f37677c53c7418
Liana Bakradze <liana.bakradze@jetbrains.com>
2019-11-20 20:23:28 +0300

    EDU-2757 Hyperskill: solutions are loaded twice for new projects

--- jvm-core/src/com/jetbrains/edu/jvm/stepik/hyperskill/HyperskillGradleCourseProjectGenerator.kt
+++ jvm-core/src/com/jetbrains/edu/jvm/stepik/hyperskill/HyperskillGradleCourseProjectGenerator.kt
@@ -1,13 +1,10 @@
 package com.jetbrains.edu.jvm.stepik.hyperskill
 
-import com.intellij.openapi.project.Project
 import com.jetbrains.edu.coursecreator.CCUtils
 import com.jetbrains.edu.jvm.gradle.GradleCourseBuilderBase
 import com.jetbrains.edu.jvm.gradle.generation.GradleCourseProjectGenerator
 import com.jetbrains.edu.learning.courseFormat.Course
-import com.jetbrains.edu.learning.stepik.hyperskill.HyperskillSettings
 import com.jetbrains.edu.learning.stepik.hyperskill.api.HyperskillConnector
-import com.jetbrains.edu.learning.stepik.hyperskill.api.HyperskillSolutionLoader
 import com.jetbrains.edu.learning.stepik.hyperskill.courseFormat.HyperskillCourse
 
 open class HyperskillGradleCourseProjectGenerator(
@@ -18,11 +15,5 @@ open class HyperskillGradleCourseProjectGenerator(
   override fun beforeProjectGenerated(): Boolean {
     assert(myCourse is HyperskillCourse)
     return myCourse.courseMode == CCUtils.COURSE_MODE || HyperskillConnector.getInstance().fillHyperskillCourse(myCourse as HyperskillCourse)
-  }
-
-  override fun loadSolutions(project: Project, course: Course) {
-    if (course.isStudy && course is HyperskillCourse && HyperskillSettings.INSTANCE.account != null) {
-      HyperskillSolutionLoader.getInstance(project).loadSolutionsInBackground()
-    }
   }
 }
