8365dccfe562a76922af550c2ed34e7a5c40492b
dmitry.nefedov <nefedov270399@yandex.ru>
2019-12-02 18:59:55 +0300

    EDU-2758 / EDU-CR-749 Fix tests for ide 191 build version

--- Edu-Cpp/testSrc/com/jetbrains/edu/cpp/inspections/NoTaskProjectNameInspectionTest.kt
+++ Edu-Cpp/testSrc/com/jetbrains/edu/cpp/inspections/NoTaskProjectNameInspectionTest.kt
@@ -1,7 +1,8 @@
 package com.jetbrains.edu.cpp.inspections
 
-import com.intellij.openapi.application.runWriteAction
+import com.intellij.openapi.application.ApplicationInfo
 import com.intellij.openapi.fileEditor.FileDocumentManager
+import com.intellij.openapi.util.BuildNumber
 import com.intellij.testFramework.EditorTestUtil
 import com.jetbrains.cidr.lang.OCLanguage
 import com.jetbrains.cmake.CMakeListsFileType
@@ -10,77 +11,77 @@ import com.jetbrains.edu.coursecreator.actions.create.MockNewStudyItemUi
 import com.jetbrains.edu.coursecreator.ui.withMockCreateStudyItemUi
 import com.jetbrains.edu.cpp.messages.EduCppBundle
 import com.jetbrains.edu.learning.EduTestCase
-import com.jetbrains.edu.learning.courseFormat.ext.findSourceDir
-import com.jetbrains.edu.learning.courseFormat.tasks.Task
 
 class NoTaskProjectNameInspectionTest : EduTestCase() {
 
-  fun `test add deleted project name to CMakeList with set minimum required`() {
+  // BACKCOMPAT: 2019.1. We skip tests in build 191 because of file inspection doesn't work correctly in test mode.
+  override fun runTest() {
+    val build192: BuildNumber = BuildNumber.fromString("192")
+    if (ApplicationInfo.getInstance().build >= build192) {
+      super.runTest()
+    }
+  }
+
+  private fun `add deleted task project name to CMakeLists base`(beforeWithHighlighting: String, after: String) {
     val course = courseWithFiles(
       language = OCLanguage.getInstance(),
       courseMode = CCUtils.COURSE_MODE,
-      environment = "GoogleTest"
+      environment = "GoogleTest" // Environment doesn't meter
     ) {
       lesson("lesson") {
         eduTask("task") {
-          taskFile("src/${CMakeListsFileType.FILE_NAME}", """
-            |<warning descr="Project name isn't set. It could break project structure.">cmake_minimum_required(VERSION 3.15)
-            |# some text</warning>
-            """.trimMargin("|"))
+          taskFile(CMakeListsFileType.FILE_NAME, beforeWithHighlighting)
         }
       }
     }
 
-    val task = course.findTask("lesson", "task")
-    doTest(task, EduCppBundle.message("projectName.addDefault.fix.description"), """
-      |cmake_minimum_required(VERSION 3.15)
-      |project(global-lesson-task)
-      |# some text
-    """.trimMargin("|"))
+    doTest(EduCppBundle.message("projectName.addDefault.fix.description"), after)
   }
 
-  fun `test add deleted project name to CMakeList without minimum required`() {
-    val course = courseWithFiles(
-      language = OCLanguage.getInstance(),
-      courseMode = CCUtils.COURSE_MODE,
-      environment = "GoogleTest"
-    ) {
-      lesson("lesson") {
-        eduTask("task") {
-          taskFile("src/${CMakeListsFileType.FILE_NAME}",
-                   """<warning descr="Project name isn't set. It could break project structure."># some text</warning>""")
-        }
-      }
-    }
+  fun `test add deleted project name to CMakeList with set minimum required`() =
+    `add deleted task project name to CMakeLists base`(
+      """
+        |<warning descr="Project name isn't set. It could break project structure.">cmake_minimum_required(VERSION 3.15)
+        |# some text<caret></warning>
+      """.trimMargin("|"),
+      """
+        |cmake_minimum_required(VERSION 3.15)
+        |project(global-lesson-task)
+        |# some text
+      """.trimMargin("|")
+    )
 
-    val task = course.findTask("lesson", "task")
-    doTest(task, EduCppBundle.message("projectName.addDefault.fix.description"), """
-      |project(global-lesson-task)
-      |# some text
-    """.trimMargin("|"))
-  }
+  fun `test add deleted project name to CMakeList without minimum required`() =
+    `add deleted task project name to CMakeLists base`(
+      """<warning descr="Project name isn't set. It could break project structure."># some text<caret></warning>""",
+      """
+        |project(global-lesson-task)
+        |# some text
+      """.trimMargin("|")
+    )
 
   override fun setUp() {
     super.setUp()
     myFixture.enableInspections(listOf(NoTaskProjectNameInspection::class.java))
   }
 
-  private fun doTest(task: Task, quickFixName: String, expectedText: String) {
+  private fun doTest(quickFixName: String, expectedText: String) {
     withMockCreateStudyItemUi(MockNewStudyItemUi()) {
-      testHighlighting(task)
+      testHighlighting()
       val action = myFixture.findSingleIntention(quickFixName)
       myFixture.launchAction(action)
       myFixture.checkResult(expectedText)
     }
   }
 
-  private fun testHighlighting(task: Task) {
-    val configFile = runWriteAction {
-      val taskDir = task.getTaskDir(project)!!
-      task.findSourceDir(taskDir)!!.findOrCreateChildData(project, CMakeListsFileType.FILE_NAME)
-    }
-    val document = FileDocumentManager.getInstance().getDocument(configFile)!!
-    myFixture.openFileInEditor(configFile)
+  private fun testHighlighting() {
+    val cMakeListsFile = findFile("lesson/task/${CMakeListsFileType.FILE_NAME}")
+
+    val document = FileDocumentManager.getInstance().getDocument(cMakeListsFile)
+    kotlin.test.assertNotNull(document, "CMakeLists file document is null")
+
+    myFixture.openFileInEditor(cMakeListsFile)
+
     val caretsState = EditorTestUtil.extractCaretAndSelectionMarkers(document)
     EditorTestUtil.setCaretsAndSelection(myFixture.editor, caretsState)
 
