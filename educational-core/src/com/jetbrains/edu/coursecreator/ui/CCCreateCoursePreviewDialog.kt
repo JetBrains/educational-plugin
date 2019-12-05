6a5c48794a5e06ebe72ee054333351fbf99fc69d
Olga Telezhnaya <olga.telezhnaya@jetbrains.com>
2019-12-05 17:54:01 +0300

    EDU-2215 Rewrite error handling for create course preview

--- educational-core/src/com/jetbrains/edu/coursecreator/ui/CCCreateCoursePreviewDialog.kt
+++ educational-core/src/com/jetbrains/edu/coursecreator/ui/CCCreateCoursePreviewDialog.kt
@@ -5,7 +5,7 @@ import com.intellij.ide.util.PropertiesComponent
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.DialogWrapper
-import com.intellij.openapi.ui.ValidationInfo
+import com.intellij.openapi.ui.Messages.showErrorDialog
 import com.intellij.openapi.util.io.FileUtil
 import com.intellij.util.ui.JBUI
 import com.jetbrains.edu.coursecreator.CCUtils
@@ -42,7 +42,8 @@ class CCCreateCoursePreviewDialog(
   override fun doOKAction() {
     val folder = CCUtils.getGeneratedFilesFolder(myProject)
     if (folder == null) {
-      showErrorMessage()
+      LOG.error(TMP_DIR_ERROR)
+      showErrorDialog(myProject, "$TMP_DIR_ERROR Please check permissions and try again.", PREVIEW_CREATION_ERROR_TITLE)
       return
     }
     val courseName = myCourse.name
@@ -59,9 +60,11 @@ class CCCreateCoursePreviewDialog(
         try {
           val location = FileUtil.createTempDirectory(PREVIEW_FOLDER_PREFIX, null)
           val settings = myPanel.projectSettings
-          val previewProject = myConfigurator.courseBuilder.getCourseProjectGenerator(course)?.doCreateCourseProject(location.absolutePath, settings)
+          val previewProject = myConfigurator.courseBuilder.getCourseProjectGenerator(course)
+            ?.doCreateCourseProject(location.absolutePath, settings)
           if (previewProject == null) {
-            showErrorMessage()
+            LOG.error(PREVIEW_PROJECT_ERROR)
+            showErrorDialog(myProject, "$PREVIEW_PROJECT_ERROR Please try again.", PREVIEW_CREATION_ERROR_TITLE)
             return
           }
           PropertiesComponent.getInstance(previewProject).setValue(IS_COURSE_PREVIEW, true)
@@ -69,8 +72,8 @@ class CCCreateCoursePreviewDialog(
           EduCounterUsageCollector.createCoursePreview()
         }
         catch (e: IOException) {
-          LOG.error("Failed to create tmp dir for course preview", e)
-          showErrorMessage()
+          LOG.error(TMP_DIR_ERROR, e)
+          showErrorDialog(myProject, "$TMP_DIR_ERROR Please check permissions and try again.", PREVIEW_CREATION_ERROR_TITLE)
         }
         finally {
           RecentProjectsManager.getInstance().lastProjectCreationLocation = lastProjectCreationLocation
@@ -78,17 +81,19 @@ class CCCreateCoursePreviewDialog(
       }
     }
     else {
-      showErrorMessage()
+      LOG.error(errorMessage)
+      showErrorDialog(myProject, errorMessage, PREVIEW_CREATION_ERROR_TITLE)
     }
   }
-
-  private fun showErrorMessage() = updateErrorInfo(listOf(ValidationInfo("Can not create course preview")))
 
   companion object {
     private val LOG: Logger = Logger.getInstance(CCCreateCoursePreviewDialog::class.java)
 
     private const val WIDTH: Int = 370
     private const val HEIGHT: Int = 330
+    private const val PREVIEW_CREATION_ERROR_TITLE = "Failed to Create Course Preview"
+    private const val TMP_DIR_ERROR = "Failed to create temp directory for course preview."
+    private const val PREVIEW_PROJECT_ERROR = "Failed to create project for course preview."
 
     const val PREVIEW_FOLDER_PREFIX: String = "course_preview"
     const val IS_COURSE_PREVIEW: String = "Edu.IsCoursePreview"
