4fa8cc02ce0b8e083c3071b003ac182fdc782dae
Olga Telezhnaya <olga.telezhnaya@jetbrains.com>
2019-12-13 16:16:13 +0300

    EDU-2772 Enable vgoIntergation
    Much more accurate way to solve initial problem

--- Edu-Go/src/com/jetbrains/edu/go/GoCourseProjectGenerator.kt
+++ Edu-Go/src/com/jetbrains/edu/go/GoCourseProjectGenerator.kt
@@ -1,17 +1,12 @@
 package com.jetbrains.edu.go
 
 import com.goide.sdk.GoSdkService
-import com.intellij.openapi.application.invokeAndWaitIfNeeded
-import com.intellij.openapi.application.runWriteAction
+import com.goide.vgo.configuration.VgoProjectSettings
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.vfs.VfsUtil.createDirectoryIfMissing
-import com.intellij.openapi.vfs.VfsUtil.getUserHomeDir
 import com.intellij.openapi.vfs.VirtualFile
-import com.jetbrains.edu.learning.EduNames
 import com.jetbrains.edu.learning.courseFormat.Course
 import com.jetbrains.edu.learning.courseGeneration.GeneratorUtils
 import com.jetbrains.edu.learning.courseGeneration.GeneratorUtils.getInternalTemplateText
-import com.jetbrains.edu.learning.courseGeneration.GeneratorUtils.joinPaths
 import com.jetbrains.edu.learning.newproject.CourseProjectGenerator
 
 class GoCourseProjectGenerator(builder: GoCourseBuilder, course: Course) :
@@ -24,9 +19,6 @@ class GoCourseProjectGenerator(builder: GoCourseBuilder, course: Course) :
   override fun afterProjectGenerated(project: Project, projectSettings: GoProjectSettings) {
     super.afterProjectGenerated(project, projectSettings)
     GoSdkService.getInstance(project).setSdk(projectSettings.sdk)
-    val homePath = getUserHomeDir()?.path ?: return
-    invokeAndWaitIfNeeded {
-      runWriteAction { createDirectoryIfMissing(joinPaths(homePath, EduNames.GO)) }
-    }
+    VgoProjectSettings.getInstance(project).isIntegrationEnabled = true
   }
 }
