2dd155a893ae57c3a9a88ae203eb2242d7aeed91
Liana Bakradze <liana.bakradze@jetbrains.com>
2019-12-17 22:36:19 +0300

    EDU-2830 Deadlock on opening Stepic courses

--- educational-core/src/com/jetbrains/edu/learning/stepik/IsUpToDateCachedValue.kt
+++ educational-core/src/com/jetbrains/edu/learning/stepik/IsUpToDateCachedValue.kt
@@ -1,30 +1,41 @@
 package com.jetbrains.edu.learning.stepik
 
+import com.intellij.openapi.progress.ProgressManager
 import com.jetbrains.edu.learning.courseFormat.EduCourse
 import java.util.concurrent.TimeUnit
+import java.util.concurrent.locks.ReentrantLock
+import kotlin.concurrent.withLock
 
 class IsUpToDateCachedValue(private val course: EduCourse) {
   private val timeoutMs: Long = TimeUnit.HOURS.toMillis(5)
   private var lastCalcTime: Long = 0
   private var cache: Boolean = true
+  private val lock = ReentrantLock()
 
-  @Synchronized
   fun get(): Boolean {
-    if (hasUpToDateValue()) {
+    //BACKCOMPAT: since 2019.3 use ProgressIndicatorUtils.computeWithLockAndCheckingCanceled
+    try {
+      while (!lock.tryLock(50, TimeUnit.MILLISECONDS)) {
+        ProgressManager.checkCanceled()
+      }
+      if (hasUpToDateValue()) {
+        return cache
+      }
+      resetCachedValue(course.checkIsUpToDate())
       return cache
+    } finally {
+      lock.unlock()
     }
-    resetCachedValue(course.checkIsUpToDate())
-    return cache
   }
 
-  @Synchronized
   private fun hasUpToDateValue(): Boolean {
     return timeoutMs > System.currentTimeMillis() - lastCalcTime
   }
 
-  @Synchronized
   fun resetCachedValue(value: Boolean) {
-    lastCalcTime = System.currentTimeMillis()
-    cache = value
+    lock.withLock {
+      lastCalcTime = System.currentTimeMillis()
+      cache = value
+    }
   }
 }
