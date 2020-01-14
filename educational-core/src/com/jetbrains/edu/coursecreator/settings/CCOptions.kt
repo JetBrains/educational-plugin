0fa5deadf6174e5318d10d727bc34db7dd99e4c7
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2020-01-14 17:15:43 +0300

    EDU-2910: convert `CCOptions` to kotlin. part 2

--- educational-core/src/com/jetbrains/edu/coursecreator/settings/CCOptions.kt
+++ educational-core/src/com/jetbrains/edu/coursecreator/settings/CCOptions.kt
@@ -1,80 +1,71 @@
-package com.jetbrains.edu.coursecreator.settings;
+package com.jetbrains.edu.coursecreator.settings
 
-import com.intellij.openapi.ui.LabeledComponent;
-import com.intellij.openapi.wm.IdeFocusManager;
-import com.intellij.ui.components.JBCheckBox;
-import com.jetbrains.edu.coursecreator.actions.CCPluginToggleAction;
-import com.jetbrains.edu.learning.EduExperimentalFeatures;
-import com.jetbrains.edu.learning.settings.OptionsProvider;
-import org.jetbrains.annotations.Nls;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.openapi.ui.LabeledComponent
+import com.intellij.openapi.wm.IdeFocusManager
+import com.intellij.ui.components.JBCheckBox
+import com.jetbrains.edu.coursecreator.actions.CCPluginToggleAction.Companion.isCourseCreatorFeaturesEnabled
+import com.jetbrains.edu.learning.EduExperimentalFeatures
+import com.jetbrains.edu.learning.isFeatureEnabled
+import com.jetbrains.edu.learning.settings.OptionsProvider
+import org.jetbrains.annotations.Nls
+import java.awt.BorderLayout
+import javax.swing.JComponent
+import javax.swing.JPanel
+import javax.swing.JRadioButton
 
-import javax.swing.*;
-import java.awt.*;
+class CCOptions : OptionsProvider {
+  private val myHtmlRadioButton: JRadioButton? = null
+  private val myMarkdownRadioButton: JRadioButton? = null
+  private val myPanel: JPanel? = null
+  private val myCustomOptions: JPanel? = null
+  private val myShowSplitEditorCheckBox = JBCheckBox(null, CCSettings.getInstance().showSplitEditor())
 
-import static com.jetbrains.edu.learning.ExperimentsKt.isFeatureEnabled;
-
-public class CCOptions implements OptionsProvider {
-  private JRadioButton myHtmlRadioButton;
-  private JRadioButton myMarkdownRadioButton;
-  private JPanel myPanel;
-  private JPanel myCustomOptions;
-
-  private JBCheckBox myShowSplitEditorCheckBox = new JBCheckBox(null, CCSettings.getInstance().showSplitEditor());
-
-  @Nullable
-  @Override
-  public JComponent createComponent() {
-    if (!CCPluginToggleAction.isCourseCreatorFeaturesEnabled()) return null;
+  override fun createComponent(): JComponent? {
+    if (!isCourseCreatorFeaturesEnabled) return null
     if (CCSettings.getInstance().useHtmlAsDefaultTaskFormat()) {
-      myHtmlRadioButton.setSelected(true);
-      IdeFocusManager.getGlobalInstance().doWhenFocusSettlesDown(
-        () -> IdeFocusManager.getGlobalInstance().requestFocus(myHtmlRadioButton, true));
+      myHtmlRadioButton!!.isSelected = true
+      IdeFocusManager.getGlobalInstance().doWhenFocusSettlesDown {
+        IdeFocusManager.getGlobalInstance().requestFocus(myHtmlRadioButton, true)
+      }
     }
     else {
-      myMarkdownRadioButton.setSelected(true);
-      IdeFocusManager.getGlobalInstance().doWhenFocusSettlesDown(
-        () -> IdeFocusManager.getGlobalInstance().requestFocus(myMarkdownRadioButton, true));
+      myMarkdownRadioButton!!.isSelected = true
+      IdeFocusManager.getGlobalInstance().doWhenFocusSettlesDown {
+        IdeFocusManager.getGlobalInstance().requestFocus(myMarkdownRadioButton, true)
+      }
     }
     if (isFeatureEnabled(EduExperimentalFeatures.SPLIT_EDITOR)) {
-      LabeledComponent<JBCheckBox> showSplitEditorComponent =
-        LabeledComponent.create(myShowSplitEditorCheckBox, "Show previous task file in framework lessons", BorderLayout.WEST);
-      myCustomOptions.add(showSplitEditorComponent);
+      val showSplitEditorComponent = LabeledComponent.create(
+        myShowSplitEditorCheckBox, "Show previous task file in framework lessons", BorderLayout.WEST)
+      myCustomOptions!!.add(showSplitEditorComponent)
     }
-    return myPanel;
+    return myPanel
   }
 
-  @Override
-  public boolean isModified() {
-    CCSettings settings = CCSettings.getInstance();
-    return myHtmlRadioButton.isSelected() != settings.useHtmlAsDefaultTaskFormat() ||
-           myShowSplitEditorCheckBox.isSelected() != settings.showSplitEditor();
+  override fun isModified(): Boolean {
+    val settings = CCSettings.getInstance()
+    return myHtmlRadioButton!!.isSelected != settings.useHtmlAsDefaultTaskFormat() ||
+           myShowSplitEditorCheckBox.isSelected != settings.showSplitEditor()
   }
 
-  @Override
-  public void apply() {
-    if (isModified()) {
-      CCSettings.getInstance().setUseHtmlAsDefaultTaskFormat(myHtmlRadioButton.isSelected());
-      CCSettings.getInstance().setShowSplitEditor(myShowSplitEditorCheckBox.isSelected());
+  override fun apply() {
+    if (isModified) {
+      CCSettings.getInstance().setUseHtmlAsDefaultTaskFormat(myHtmlRadioButton!!.isSelected)
+      CCSettings.getInstance().setShowSplitEditor(myShowSplitEditorCheckBox.isSelected)
     }
   }
 
-  @Override
-  public void reset() {
-    CCSettings settings = CCSettings.getInstance();
-    myHtmlRadioButton.setSelected(settings.useHtmlAsDefaultTaskFormat());
-    myMarkdownRadioButton.setSelected(!settings.useHtmlAsDefaultTaskFormat());
-    myShowSplitEditorCheckBox.setSelected(settings.showSplitEditor());
+  override fun reset() {
+    val settings = CCSettings.getInstance()
+    myHtmlRadioButton!!.isSelected = settings.useHtmlAsDefaultTaskFormat()
+    myMarkdownRadioButton!!.isSelected = !settings.useHtmlAsDefaultTaskFormat()
+    myShowSplitEditorCheckBox.isSelected = settings.showSplitEditor()
   }
 
-  @Override
-  public void disposeUIResources() {
-
-  }
+  override fun disposeUIResources() {}
 
   @Nls
-  @Override
-  public String getDisplayName() {
-    return "Course Creator options";
+  override fun getDisplayName(): String {
+    return "Course Creator options"
   }
 }
