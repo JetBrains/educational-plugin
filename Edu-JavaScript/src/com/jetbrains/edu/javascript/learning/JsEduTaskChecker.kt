18f97f1783aa78b80cfab89cb3126c99db6852cc
Olga Telezhnaya <olga.telezhnaya@jetbrains.com>
2020-01-16 11:29:39 +0300

    EDU-2861 Fix error message for JS tasks
    Jest adds `Failed: "..."` around educator's error message,
    looks better when we remove it.

--- Edu-JavaScript/src/com/jetbrains/edu/javascript/learning/JsEduTaskChecker.kt
+++ Edu-JavaScript/src/com/jetbrains/edu/javascript/learning/JsEduTaskChecker.kt
@@ -20,4 +20,11 @@ class JsEduTaskChecker(task: EduTask, project: Project) : EduTaskCheckerBase(tas
     val index = StringUtil.indexOfIgnoreCase(node.errorMessage, "Expected:", 0)
     return if (index != -1) node.errorMessage.substring(0, index).trim() else node.errorMessage
   }
+
+  override fun getErrorMessage(node: SMTestProxy): String {
+    val failedMessageStart = "Failed: \""
+    return if (node.errorMessage.startsWith(failedMessageStart))
+      node.errorMessage.substringAfter(failedMessageStart).substringBeforeLast('"')
+    else node.errorMessage
+  }
 }
