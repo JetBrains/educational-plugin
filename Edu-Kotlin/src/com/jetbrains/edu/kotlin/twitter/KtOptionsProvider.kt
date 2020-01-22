10bba04b66cf26d18e9f22582f491f63dd00c227
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2020-01-22 15:09:25 +0300

    EDU-2941: convert KtOptionsProvider to kotlin. step 2

--- Edu-Kotlin/src/com/jetbrains/edu/kotlin/twitter/KtOptionsProvider.kt
+++ Edu-Kotlin/src/com/jetbrains/edu/kotlin/twitter/KtOptionsProvider.kt
@@ -1,68 +1,53 @@
-package com.jetbrains.edu.kotlin.twitter;
+package com.jetbrains.edu.kotlin.twitter
 
-import com.intellij.openapi.project.Project;
-import com.intellij.ui.components.JBCheckBox;
-import com.jetbrains.edu.learning.EduUtils;
-import com.jetbrains.edu.learning.StudyTaskManager;
-import com.jetbrains.edu.learning.settings.OptionsProvider;
-import com.jetbrains.edu.learning.twitter.TwitterPluginConfigurator;
-import org.jetbrains.annotations.Nls;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.openapi.project.Project
+import com.intellij.ui.components.JBCheckBox
+import com.jetbrains.edu.learning.EduUtils
+import com.jetbrains.edu.learning.StudyTaskManager
+import com.jetbrains.edu.learning.settings.OptionsProvider
+import org.jetbrains.annotations.Nls
+import java.awt.event.ActionEvent
+import javax.swing.JComponent
+import javax.swing.JPanel
 
-import javax.swing.*;
+class KtOptionsProvider internal constructor(private val myProject: Project) : OptionsProvider {
+  private val myTwitterSettings: KtTwitterSettings
+  private val myAskToTweetCheckBox: JBCheckBox? = null
+  private val myPanel: JPanel? = null
+  private var myIsModified = false
+  override fun apply() {
+    myTwitterSettings.setAskToTweet(myAskToTweetCheckBox!!.isSelected)
+  }
 
+  override fun reset() {
+    myTwitterSettings.setAskToTweet(true)
+  }
 
-public class KtOptionsProvider implements OptionsProvider {
-  private final Project myProject;
-  private KtTwitterSettings myTwitterSettings;
-  private JBCheckBox myAskToTweetCheckBox;
-  private JPanel myPanel;
-  private boolean myIsModified = false;
-
-  KtOptionsProvider(@NotNull final Project project) {
-    myProject = project;
-    myTwitterSettings = KtTwitterSettings.getInstance(myProject);
-    TwitterPluginConfigurator twitterConfigurator = EduUtils.getTwitterConfigurator(myProject);
-    if (twitterConfigurator != null) {
-      myAskToTweetCheckBox.setSelected(myTwitterSettings.askToTweet());
+  override fun disposeUIResources() {}
+  override fun createComponent(): JComponent? {
+    val hasCourse = StudyTaskManager.getInstance(myProject).course != null
+    val twitterConfigurator = EduUtils.getTwitterConfigurator(myProject)
+    return if (hasCourse && twitterConfigurator != null) {
+      myPanel
     }
-    myAskToTweetCheckBox.addActionListener(e -> myIsModified = true);
+    else null
   }
 
-  @Override
-  public void apply() {
-    myTwitterSettings.setAskToTweet(myAskToTweetCheckBox.isSelected());
-  }
-
-  @Override
-  public void reset() {
-    myTwitterSettings.setAskToTweet(true);
-  }
-
-  @Override
-  public void disposeUIResources() {
-  }
-
-  @Nullable
-  @Override
-  public JComponent createComponent() {
-    final boolean hasCourse = StudyTaskManager.getInstance(myProject).getCourse() != null;
-    TwitterPluginConfigurator twitterConfigurator = EduUtils.getTwitterConfigurator(myProject);
-    if (hasCourse && twitterConfigurator != null) {
-      return myPanel;
-    }
-    return null;
-  }
-
-  @Override
-  public boolean isModified() {
-    return myIsModified;
+  override fun isModified(): Boolean {
+    return myIsModified
   }
 
   @Nls
-  @Override
-  public String getDisplayName() {
-    return "Twitter Settings";
+  override fun getDisplayName(): String {
+    return "Twitter Settings"
+  }
+
+  init {
+    myTwitterSettings = KtTwitterSettings.getInstance(myProject)
+    val twitterConfigurator = EduUtils.getTwitterConfigurator(myProject)
+    if (twitterConfigurator != null) {
+      myAskToTweetCheckBox!!.isSelected = myTwitterSettings.askToTweet()
+    }
+    myAskToTweetCheckBox!!.addActionListener { e: ActionEvent? -> myIsModified = true }
   }
 }
