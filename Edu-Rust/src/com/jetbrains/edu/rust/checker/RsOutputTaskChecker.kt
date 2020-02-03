3cf3aa2555c3321ce02d1a976db6a4390ca6b953
dmitry.nefedov <nefedov270399@yandex.ru>
2020-02-03 12:24:49 +0300

    EDU-2952 output checker: Simplify Rust implementation

--- Edu-Rust/src/com/jetbrains/edu/rust/checker/RsOutputTaskChecker.kt
+++ Edu-Rust/src/com/jetbrains/edu/rust/checker/RsOutputTaskChecker.kt
@@ -1,18 +1,10 @@
 package com.jetbrains.edu.rust.checker
 
-import com.intellij.openapi.diagnostic.Logger
-import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.vfs.VfsUtil
-import com.intellij.openapi.vfs.VirtualFile
 import com.jetbrains.edu.learning.Err
-import com.jetbrains.edu.learning.Ok
 import com.jetbrains.edu.learning.checker.*
-import com.jetbrains.edu.learning.checker.CheckResult.Companion.FAILED_TO_CHECK
 import com.jetbrains.edu.learning.courseFormat.CheckStatus
-import com.jetbrains.edu.learning.courseFormat.ext.findTestDirs
 import com.jetbrains.edu.learning.courseFormat.tasks.OutputTask
-import java.io.IOException
 
 class RsOutputTaskChecker(
   project: Project,
@@ -20,70 +12,31 @@ class RsOutputTaskChecker(
   codeExecutor: CodeExecutor
 ) : OutputTaskChecker(task, project, codeExecutor) {
 
-  override fun check(indicator: ProgressIndicator): CheckResult {
-    var outputFile: VirtualFile? = null
-    for (testDir in task.findTestDirs(project)) {
-      outputFile = testDir.findChild(OUTPUT_PATTERN_NAME)
-      if (outputFile != null) break
-    }
-
-    if (outputFile == null) {
-      logWarning("Failed to find `output.txt` file")
-      return FAILED_TO_CHECK
-    }
-
-    val expectedOutput = try {
-      VfsUtil.loadText(outputFile)
-    }
-    catch (e: IOException) {
-      LOG.warn("Failed to read expected output", e)
-      return FAILED_TO_CHECK
-    }
-
-    val output = when (val result = codeExecutor.execute(project, task, indicator)) {
-      is Ok -> result.value
-      is Err -> {
-        return when {
-          result.error.contains(COMPILATION_ERROR_MESSAGE, true) -> CheckResult(CheckStatus.Failed, CheckUtils.COMPILATION_FAILED_MESSAGE, result.error)
-          else -> {
-            logWarning(result.error)
-            CheckResult(CheckStatus.Unchecked, result.error)
-          }
-        }
-      }
-    }
-    return checkOutput(output, expectedOutput)
+  override fun hasCompileError(result: Err<String>): Boolean {
+    return result.error.contains(COMPILATION_ERROR_MESSAGE, true)
   }
 
-  private fun logWarning(logMessage: String) {
-    LOG.warn("$logMessage (task `${task.lesson.name}/${task.name}`)")
-  }
-
-  companion object {
-    private val LOG: Logger = Logger.getInstance(RsOutputTaskChecker::class.java)
-
-    private fun checkOutput(actual: String, expected: String): CheckResult {
-      var programOutputStarted = false
-      val outputBuffer = StringBuilder()
-      for (line in actual.lineSequence()) {
-        if (programOutputStarted) {
-          outputBuffer.appendln(line)
-        }
-        else {
-          if (line.trimStart().startsWith("Running")) {
-            programOutputStarted = true
-          }
-        }
-      }
-
-      val output = CheckUtils.postProcessOutput(outputBuffer.toString())
-      return if (expected != output) {
-        val diff = CheckResultDiff(expected = expected, actual = output)
-        CheckResult(CheckStatus.Failed, "Expected output:\n<$expected>\nActual output:\n<$output>", diff = diff)
+  override fun checkOutput(actual: String, expected: String): CheckResult {
+    var programOutputStarted = false
+    val outputBuffer = StringBuilder()
+    for (line in actual.lineSequence()) {
+      if (programOutputStarted) {
+        outputBuffer.appendln(line)
       }
       else {
-        CheckResult(CheckStatus.Solved, CheckUtils.CONGRATULATIONS)
+        if (line.trimStart().startsWith("Running")) {
+          programOutputStarted = true
+        }
       }
+    }
+
+    val output = CheckUtils.postProcessOutput(outputBuffer.toString())
+    return if (expected != output) {
+      val diff = CheckResultDiff(expected = expected, actual = output)
+      CheckResult(CheckStatus.Failed, "Expected output:\n<$expected>\nActual output:\n<$output>", diff = diff)
+    }
+    else {
+      CheckResult(CheckStatus.Solved, CheckUtils.CONGRATULATIONS)
     }
   }
 }
--- educational-core/src/com/jetbrains/edu/learning/checker/OutputTaskChecker.kt
+++ educational-core/src/com/jetbrains/edu/learning/checker/OutputTaskChecker.kt
@@ -15,29 +15,52 @@ import com.jetbrains.edu.learning.courseFormat.tasks.OutputTask
 open class OutputTaskChecker(
   task: OutputTask,
   project: Project,
-  protected val codeExecutor: CodeExecutor
+  private val codeExecutor: CodeExecutor
 ) : TaskChecker<OutputTask>(task, project) {
 
   override fun check(indicator: ProgressIndicator): CheckResult {
     try {
       val outputString = when (val result = codeExecutor.execute(project, task, indicator)) {
         is Ok -> result.value
-        is Err -> return CheckResult(CheckStatus.Unchecked, result.error)
+        is Err -> return when {
+          hasCompileError(result) -> CheckResult(CheckStatus.Failed, CheckUtils.COMPILATION_FAILED_MESSAGE, result.error)
+          else -> {
+            logWarning(result.error)
+            CheckResult(CheckStatus.Unchecked, result.error)
+          }
+        }
       }
 
-      val outputPatternFile = getOutputFile() ?: return FAILED_TO_CHECK
-
+      val outputPatternFile = getOutputFile()
+      if (outputPatternFile == null) {
+        logWarning("Failed to find `output.txt` file")
+        return FAILED_TO_CHECK
+      }
       val expectedOutput = VfsUtil.loadText(outputPatternFile)
-      if (expectedOutput.trimEnd('\n') == outputString.trimEnd('\n')) {
-        return CheckResult(CheckStatus.Solved, CheckUtils.CONGRATULATIONS)
-      }
-      val diff = CheckResultDiff(expected = expectedOutput, actual = outputString)
-      return CheckResult(CheckStatus.Failed, "Expected output:\n$expectedOutput \nActual output:\n$outputString", diff = diff)
+      return checkOutput(outputString, expectedOutput)
     }
     catch (e: Exception) {
       LOG.error(e)
       return FAILED_TO_CHECK
     }
+  }
+
+  protected open fun hasCompileError(result: Err<String>): Boolean = false
+
+  protected open fun checkOutput(actual: String, expected: String): CheckResult = when {
+    compareResults(actual, expected) -> CheckResult(CheckStatus.Solved, CheckUtils.CONGRATULATIONS)
+    else -> {
+      val diff = CheckResultDiff(expected = expected, actual = actual)
+      CheckResult(CheckStatus.Failed, "Expected output:\n$expected \nActual output:\n$actual", diff = diff)
+    }
+  }
+
+  protected open fun compareResults(actual: String, expected: String): Boolean {
+    return CheckUtils.postProcessOutput(actual) == CheckUtils.postProcessOutput(expected)
+  }
+
+  private fun logWarning(logMessage: String) {
+    LOG.warn("$logMessage (output task `${task.lesson.name}/${task.name}`)")
   }
 
   private fun getOutputFile(): VirtualFile? {
