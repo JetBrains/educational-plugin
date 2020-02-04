667689ff117639a64b55cdbeacae45a71dc7d28b
Aleksey Rostovskiy <aleksey.rostovskiy@jetbrains.com>
2020-02-04 13:25:04 +0300

    [codeforces] EDU-2985 Support Java 11 selection

--- Edu-Java/src/com/jetbrains/edu/java/codeforces/JCodeforcesLanguageProvider.kt
+++ Edu-Java/src/com/jetbrains/edu/java/codeforces/JCodeforcesLanguageProvider.kt
@@ -1,15 +1,20 @@
 package com.jetbrains.edu.java.codeforces
 
+import com.intellij.openapi.projectRoots.JavaSdkVersion.JDK_11
+import com.intellij.openapi.projectRoots.JavaSdkVersion.JDK_1_8
 import com.jetbrains.edu.java.JConfigurator
 import com.jetbrains.edu.learning.EduNames
 import com.jetbrains.edu.learning.codeforces.CodeforcesLanguageProvider
 import com.jetbrains.edu.learning.configuration.EduConfigurator
 
 class JCodeforcesLanguageProvider : CodeforcesLanguageProvider {
-  override val codeforcesLanguageNamings: List<String> = listOf("Java 8")
+  override val codeforcesLanguageNamings: List<String> = listOf("Java 8", "Java 11")
   override val configurator: EduConfigurator<*> = JConfigurator()
   override val languageId: String = EduNames.JAVA
   override val templateFileName: String = "codeforces.Main.java"
 
-  override fun getLanguageVersion(codeforcesLanguage: String): String? = "8"
+  override fun getLanguageVersion(codeforcesLanguage: String): String? {
+    val version = codeforcesLanguage.split(" ").last()
+    return if (version in listOf(JDK_1_8, JDK_11).map { it.description }) version else null
+  }
 }
