c1b78b6605ccc836d86fff586a243fe0ebc73cb2
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2020-02-13 09:29:05 +0300

    EDU-2974: fix tests compilation in Edu-Java module

---
+++ Edu-Java/branches/192/testSrc/com/jetbrains/edu/java/courseView/JNodesTestsBase.kt
@@ -0,0 +1,5 @@
+package com.jetbrains.edu.java.courseView
+
+import com.jetbrains.edu.learning.courseView.CourseViewTestBase
+
+abstract class JNodesTestsBase : CourseViewTestBase()
--- Edu-Java/testSrc/com/jetbrains/edu/java/courseView/JNodesTests.kt
+++ Edu-Java/testSrc/com/jetbrains/edu/java/courseView/JNodesTests.kt
@@ -1,17 +1,9 @@
 package com.jetbrains.edu.java.courseView
 
-import com.intellij.ide.projectView.impl.ProjectViewState
 import com.intellij.lang.java.JavaLanguage
 import com.jetbrains.edu.jvm.JdkProjectSettings
-import com.jetbrains.edu.learning.courseView.CourseViewTestBase
 
-class JNodesTests : CourseViewTestBase() {
-
-  override fun setUp() {
-    super.setUp()
-    // Since 2020.1 this setting is disabled by default in tests
-    ProjectViewState.getInstance(project).hideEmptyMiddlePackages = true
-  }
+class JNodesTests : JNodesTestsBase() {
 
   fun `test nested packages`() {
     courseWithFiles(language = JavaLanguage.INSTANCE, settings = JdkProjectSettings.emptySettings()) {
