124f1b4379774e8583205bca139c8a488d090154
dmitry.nefedov <nefedov270399@yandex.ru>
2020-03-03 15:36:22 +0300

    EDU-3056 cpp constants: fix missed updating of using `TEST_FRAMEWORK_DIR` constant

--- Edu-Cpp/src/com/jetbrains/edu/cpp/CppProjectComponent.kt
+++ Edu-Cpp/src/com/jetbrains/edu/cpp/CppProjectComponent.kt
@@ -4,6 +4,7 @@ import com.intellij.openapi.components.ProjectComponent
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VfsUtil
 import com.jetbrains.cidr.cpp.cmake.workspace.CMakeWorkspaceListener
+import com.jetbrains.edu.cpp.constants.TestFrameworks
 import com.jetbrains.edu.learning.courseDir
 
 // BACKCOMPAT: 2019.2. Delete this class. In 2019.3 all files are refreshed correctly.
@@ -23,7 +24,7 @@ class CppProjectComponent(private val project: Project) : ProjectComponent {
 
         // The current method is called under write action, so we could safely call a synchronous refresh here.
         // See com.jetbrains.cidr.cpp.cmake.workspace.CMakeWorkspace#doReload method.
-        VfsUtil.refreshAndFindChild(project.courseDir, TEST_FRAMEWORK_DIR)?.refresh(true, true)
+        VfsUtil.refreshAndFindChild(project.courseDir, TestFrameworks.baseDir.value)?.refresh(true, true)
       }
     })
 
