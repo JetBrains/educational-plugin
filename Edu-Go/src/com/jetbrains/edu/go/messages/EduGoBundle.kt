f969951bb997f9f74c34731d2c6f2c7e4a2cad2a
dmitry.nefedov <dmitry.nefedov@jetbrains.com>
2020-03-17 15:06:36 +0300

    EDU-2983 i18n: Add bundle support for Go

---
+++ Edu-Go/src/com/jetbrains/edu/go/messages/EduGoBundle.kt
@@ -0,0 +1,16 @@
+package com.jetbrains.edu.go.messages
+
+import com.intellij.CommonBundle
+import com.jetbrains.edu.learning.messages.EduBundle
+import org.jetbrains.annotations.NonNls
+import org.jetbrains.annotations.PropertyKey
+
+object EduGoBundle : EduBundle() {
+  @JvmStatic
+  fun message(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any): String {
+    return CommonBundle.message(getBundle(BUNDLE), key, *params)
+  }
+
+  @NonNls
+  private const val BUNDLE = "messages.EduGoBundle"
+}
