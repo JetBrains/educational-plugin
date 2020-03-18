1acbe468dece74f2a1aaba93a961df3f30d0dbfb
Liana Bakradze <liana.bakradze@jetbrains.com>
2020-03-18 19:27:12 +0300

    EDU-3029 Hyperskill: count only project's stages in progress bar

--- educational-core/src/com/jetbrains/edu/learning/projectView/ProgressUtil.kt
+++ educational-core/src/com/jetbrains/edu/learning/projectView/ProgressUtil.kt
@@ -3,6 +3,7 @@ package com.jetbrains.edu.learning.projectView
 import com.jetbrains.edu.learning.courseFormat.CheckStatus
 import com.jetbrains.edu.learning.courseFormat.Course
 import com.jetbrains.edu.learning.courseFormat.Lesson
+import com.jetbrains.edu.learning.stepik.hyperskill.courseFormat.HyperskillCourse
 
 object ProgressUtil {
   /**
@@ -10,6 +11,12 @@ object ProgressUtil {
    */
   @JvmStatic
   fun countProgress(course: Course): Pair<Int, Int> {
+    if (course is HyperskillCourse) {
+      // we want empty progress in case project stages are not loaded
+      // and only code challenges are present
+      val projectLesson = course.getProjectLesson() ?: return 0 to 0
+      return countProgress(projectLesson)
+    }
     var taskNum = 0
     var taskSolved = 0
     course.visitLessons { lesson ->
