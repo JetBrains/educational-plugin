01e6869927bfdd835b4cfc6937ee23dd5a2ef263
dmitry.nefedov <dmitry.nefedov@jetbrains.com>
2020-03-27 13:55:26 +0300

    EDU-2983 / EDU-CR-923 i18n: Simplify bundle name in EduCoreBundle

--- educational-core/src/com/jetbrains/edu/learning/messages/EduCoreBundle.kt
+++ educational-core/src/com/jetbrains/edu/learning/messages/EduCoreBundle.kt
@@ -4,13 +4,13 @@ import org.jetbrains.annotations.NonNls
 import org.jetbrains.annotations.PropertyKey
 
 @NonNls
-const val EDU_CORE_BUNDLE_NAME = "messages.EduCoreBundle"
+const val BUNDLE = "messages.EduCoreBundle"
 
-object EduCoreBundle : EduBundle(EDU_CORE_BUNDLE_NAME) {
+object EduCoreBundle : EduBundle(BUNDLE) {
   const val FAILED_TO_CONVERT_TO_STUDENT_FILE = "Failed to convert answer file to student one because placeholder is broken."
 
   @JvmStatic
-  fun message(@PropertyKey(resourceBundle = EDU_CORE_BUNDLE_NAME) key: String, vararg params: Any): String {
+  fun message(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any): String {
     return getMessage(key, *params)
   }
 }
