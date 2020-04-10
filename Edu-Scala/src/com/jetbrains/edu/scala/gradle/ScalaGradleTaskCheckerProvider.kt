ae9d2caf7d288b4c49ed1b43b45a5fec819d8357
Aleksey Rostovskiy <aleksey.rostovskiy@jetbrains.com>
2020-04-10 17:57:31 +0300

    EDU-3171 Codeforces: Scala support issue

--- Edu-Scala/src/com/jetbrains/edu/scala/gradle/ScalaGradleTaskCheckerProvider.kt
+++ Edu-Scala/src/com/jetbrains/edu/scala/gradle/ScalaGradleTaskCheckerProvider.kt
@@ -2,11 +2,23 @@ package com.jetbrains.edu.scala.gradle
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiManager
+import com.intellij.psi.util.PsiTreeUtil
 import com.jetbrains.edu.jvm.gradle.checker.GradleTaskCheckerProvider
+import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScObject
+import org.jetbrains.plugins.scala.util.ScalaMainMethodUtil
 
 class ScalaGradleTaskCheckerProvider : GradleTaskCheckerProvider() {
 
   override fun mainClassForFile(project: Project, file: VirtualFile): String? {
+    val psiFile = PsiManager.getInstance(project).findFile(file) ?: return null
+    if (psiFile !is ScalaFile) return null
+
+    PsiTreeUtil.findChildrenOfType(psiFile, ScObject::class.java).forEach {
+      val mainMethod = ScalaMainMethodUtil.findMainMethod(it)
+      if (mainMethod.isDefined) return mainMethod.get().name
+    }
     return null
   }
 }
