daefba1a72dbeab8813f4c6ff97aded289b94cbe
Olga Telezhnaya <olga.telezhnaya@jetbrains.com>
2020-04-16 18:03:25 +0300

    EDU-3082 Add EduNames.TEST as alternative test folder for JBA

--- Edu-JavaScript/src/com/jetbrains/edu/javascript/learning/stepik/hyperskill/JsHyperskillConfigurator.kt
+++ Edu-JavaScript/src/com/jetbrains/edu/javascript/learning/stepik/hyperskill/JsHyperskillConfigurator.kt
@@ -2,9 +2,10 @@ package com.jetbrains.edu.javascript.learning.stepik.hyperskill
 
 import com.jetbrains.edu.javascript.learning.JsConfigurator
 import com.jetbrains.edu.javascript.learning.JsNewProjectSettings
+import com.jetbrains.edu.learning.EduNames
 import com.jetbrains.edu.learning.stepik.hyperskill.HyperskillConfigurator
 
 class JsHyperskillConfigurator : HyperskillConfigurator<JsNewProjectSettings>(JsConfigurator()) {
   override val testDirs: List<String>
-    get() = listOf(HYPERSKILL_TEST_DIR)
+    get() = listOf(HYPERSKILL_TEST_DIR, EduNames.TEST)
 }
