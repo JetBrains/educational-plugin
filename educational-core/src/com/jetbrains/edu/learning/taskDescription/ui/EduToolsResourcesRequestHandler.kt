52f0c2eb0c8c609f630765e0962e0098ededc326
Aleksey Rostovskiy <aleksey.rostovskiy@jetbrains.com>
2020-04-25 06:27:03 +0300

    EDU-3013 Rollback loading local resources for JavaFX

--- educational-core/src/com/jetbrains/edu/learning/taskDescription/ui/EduToolsResourcesRequestHandler.kt
+++ educational-core/src/com/jetbrains/edu/learning/taskDescription/ui/EduToolsResourcesRequestHandler.kt
@@ -1,6 +1,7 @@
 package com.jetbrains.edu.learning.taskDescription.ui
 
 import com.intellij.openapi.diagnostic.Logger
+import com.jetbrains.edu.learning.JavaUILibrary.Companion.isJCEF
 import io.netty.channel.Channel
 import io.netty.channel.ChannelFutureListener
 import io.netty.channel.ChannelHandlerContext
@@ -12,6 +13,7 @@ import org.jetbrains.io.FileResponses
 import org.jetbrains.io.addCommonHeaders
 import org.jetbrains.io.addKeepAliveIfNeeded
 import java.io.ByteArrayInputStream
+import java.net.URL
 import java.util.*
 
 class EduToolsResourcesRequestHandler : HttpRequestHandler() {
@@ -21,14 +23,13 @@ class EduToolsResourcesRequestHandler : HttpRequestHandler() {
       return false
     }
 
-    LOG.info("URI is $uri")
-    val file: String = uri.split(EDU_RESOURCES)[1]
-    val bytes: ByteArray = readResourceFile(file) ?: return false
-    return sendData(bytes, file, request, context.channel())
+    val url = getResource(uri.split(EDU_RESOURCES)[1]) ?: return false
+    val bytes = url.readBytes()
+    return sendData(bytes, url.file, request, context.channel())
   }
 
   private fun readResourceFile(file: String): ByteArray? {
-    val resource = object {}.javaClass.getResource(file) ?: return null
+    val resource = getResource(file) ?: return null
     return resource.readBytes()
   }
 
@@ -60,12 +61,23 @@ class EduToolsResourcesRequestHandler : HttpRequestHandler() {
   }
 
   companion object {
-    val LOG = Logger.getInstance(EduToolsResourcesRequestHandler::class.java)
+    val LOG: Logger = Logger.getInstance(EduToolsResourcesRequestHandler::class.java)
 
     const val EDU_RESOURCES: String = "eduResources"
 
-    fun eduResourceUrl(name: String): String {
-      val resource = object {}.javaClass.getResource(name)
+    /**
+     * JCEF doesn't load local resources
+     * Otherwise JavaFX does load only local resources, see [javafx.scene.web.WebEngine.userStyleSheetLocation]
+     */
+    fun eduResourceUrl(name: String): String = when {
+      isJCEF() -> eduResourceWebUrl(name)
+      else -> eduResourceFileUrl(name)
+    }
+
+    private fun getResource(name: String): URL? = object {}.javaClass.getResource(name)
+
+    private fun eduResourceWebUrl(name: String): String {
+      val resource = getResource(name)
       if (resource == null) {
         LOG.warn("Cannot find resource: $name")
         return ""
@@ -73,6 +85,17 @@ class EduToolsResourcesRequestHandler : HttpRequestHandler() {
 
       val port = BuiltInServerManager.getInstance().port
       return "http://localhost:$port/$EDU_RESOURCES/${name.trimStart('/')}"
+    }
+
+    private fun eduResourceFileUrl(name: String): String {
+      val resource = getResource(name)?.toExternalForm()
+      return if (resource != null) {
+        resource
+      }
+      else {
+        LOG.warn("Cannot find resource: $name")
+        ""
+      }
     }
   }
 }
