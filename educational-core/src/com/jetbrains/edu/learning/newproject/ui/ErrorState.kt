d6056a1e4afed50c055820a6c422f45a42ef08a4
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2020-04-30 11:44:18 +0300

    EDU-3235: improve `ErrorState`
    Now:
    - `ErrorState#forCourse` checks all possible errors for all course types
    - `ErrorState#forCourse` is refactored and looks much more readable
    - `ErrorState` uses `ErrorSeverity` enum instead of unclear int constants as severity level

--- educational-core/src/com/jetbrains/edu/learning/newproject/ui/ErrorState.kt
+++ educational-core/src/com/jetbrains/edu/learning/newproject/ui/ErrorState.kt
@@ -8,14 +8,13 @@ import com.jetbrains.edu.learning.EduSettings
 import com.jetbrains.edu.learning.EduUtils
 import com.jetbrains.edu.learning.checkio.CheckiOConnectorProvider
 import com.jetbrains.edu.learning.checkio.courseFormat.CheckiOCourse
-import com.jetbrains.edu.learning.codeforces.api.ContestInfo
 import com.jetbrains.edu.learning.courseFormat.Course
 import com.jetbrains.edu.learning.compatibility.CourseCompatibility
 import com.jetbrains.edu.learning.courseFormat.EduCourse
 import com.jetbrains.edu.learning.courseFormat.ext.configurator
-import com.jetbrains.edu.learning.coursera.CourseraCourse
 import com.jetbrains.edu.learning.messages.EduCoreBundle
 import com.jetbrains.edu.learning.newproject.JetBrainsAcademyCourse
+import com.jetbrains.edu.learning.newproject.ui.ErrorSeverity.*
 import com.jetbrains.edu.learning.newproject.ui.ValidationMessageType.ERROR
 import com.jetbrains.edu.learning.newproject.ui.ValidationMessageType.WARNING
 import com.jetbrains.edu.learning.plugins.PluginInfo
@@ -27,60 +26,84 @@ import com.jetbrains.edu.learning.ui.EduColors.warningTextForeground
 import java.awt.Color
 
 sealed class ErrorState(
-  private val severity: Int,
+  private val severity: ErrorSeverity,
   val message: ValidationMessage?,
   val foregroundColor: Color,
   val courseCanBeStarted: Boolean
 ) {
 
-  object NothingSelected : ErrorState(0, null, Color.BLACK, false)
-  object None : ErrorState(1, null, Color.BLACK, true)
-  object NotLoggedIn : ErrorState(2, ValidationMessage("", "Log in", " to Stepik to see more courses", type = WARNING), warningTextForeground, true)
-  abstract class LoginRequired(platformName: String) : ErrorState(3, ValidationMessage("", "Log in", " to $platformName to start this course"), errorTextForeground, false)
+  object NothingSelected : ErrorState(OK, null, Color.BLACK, false)
+  object None : ErrorState(OK, null, Color.BLACK, true)
+
+  object NotLoggedIn : ErrorState(LOGIN_RECOMMENDED, ValidationMessage("", "Log in", " to Stepik to see more courses", type = WARNING), warningTextForeground, true)
+  object JetBrainsAcademyLoginRecommended : ErrorState(LOGIN_RECOMMENDED, ValidationMessage("", linkText = "Log in", afterLink = " to existing ${EduNames.JBA} account to open a project"), warningTextForeground, true)
+
+  abstract class LoginRequired(platformName: String) : ErrorState(LOGIN_ERROR, ValidationMessage("", "Log in", " to $platformName to start this course"), errorTextForeground, false)
   object StepikLoginRequired : LoginRequired(StepikNames.STEPIK)
   class CheckiOLoginRequired(courseName: String) : LoginRequired(courseName) // Name of CheckiO course equals corresponding CheckiO platform name
   //TODO: remove it?
   object HyperskillLoginRequired : LoginRequired(EduNames.JBA)
-  object IncompatibleVersion : ErrorState(3, ValidationMessage("", "Update", " plugin to start this course"), errorTextForeground, false)
-  class UnsupportedCourse(message: String) : ErrorState(3, ValidationMessage(message), errorTextForeground, false)
-  data class RequirePlugins(val pluginIds: List<PluginInfo>) :
-    ErrorState(3, errorMessage(pluginIds), errorTextForeground, false)
-  object RestartNeeded : ErrorState(3, ValidationMessage("", "Restart", " to activate plugin updates"), errorTextForeground, false)
-  class LanguageSettingsError(message: ValidationMessage) : ErrorState(3, message, errorTextForeground, false)
-  object JavaFXRequired : ErrorState(4, ValidationMessage("No JavaFX found. Please ", "switch", " to JetBrains Runtime to start the course"), errorTextForeground, false)
   class CustomSevereError(beforeLink: String, link: String = "", afterLink: String = "", val action: Runnable? = null) :
-    ErrorState(3, ValidationMessage(beforeLink, link, afterLink), errorTextForeground, false)
-  object JetBrainsAcademyLoginRecommended : ErrorState(2, ValidationMessage("", linkText = "Log in", afterLink = " to existing ${EduNames.JBA} account to open a project"), warningTextForeground, true)
+    ErrorState(LOGIN_ERROR, ValidationMessage(beforeLink, link, afterLink), errorTextForeground, false)
+
+  class LanguageSettingsError(message: ValidationMessage) : ErrorState(LANGUAGE_SETTINGS_ERROR, message, errorTextForeground, false)
+
+  object JavaFXRequired : ErrorState(NO_JAVA_FX, ValidationMessage("No JavaFX found. Please ", "switch", " to JetBrains Runtime to start the course"), errorTextForeground, false)
+
+  object IncompatibleVersion : ErrorState(PLUGIN_UPDATE_REQUIRED, ValidationMessage("", "Update", " plugin to start this course"), errorTextForeground, false)
+  data class RequirePlugins(val pluginIds: List<PluginInfo>) : ErrorState(PLUGIN_UPDATE_REQUIRED, errorMessage(pluginIds), errorTextForeground, false)
+  object RestartNeeded : ErrorState(PLUGIN_UPDATE_REQUIRED, ValidationMessage("", "Restart", " to activate plugin updates"), errorTextForeground, false)
+
+  class UnsupportedCourse(message: String) : ErrorState(UNSUPPORTED_COURSE, ValidationMessage(message), errorTextForeground, false)
 
   fun merge(other: ErrorState): ErrorState = if (severity < other.severity) other else this
 
   companion object {
     @JvmStatic
     fun forCourse(course: Course?): ErrorState {
-      val courseCompatibility = course?.compatibility
-      return when {
-        course == null -> NothingSelected
-        course is JetBrainsAcademyCourse -> if (HyperskillSettings.INSTANCE.account == null) JetBrainsAcademyLoginRecommended else None
-        courseCompatibility is CourseCompatibility.PluginsRequired -> {
-          if (courseCompatibility.toInstallOrEnable.isEmpty()) RestartNeeded else RequirePlugins(courseCompatibility.toInstallOrEnable)
-        }
-        courseCompatibility == CourseCompatibility.IncompatibleVersion -> IncompatibleVersion
-        courseCompatibility == CourseCompatibility.Unsupported -> UnsupportedCourse(course.unsupportedCourseMessage)
-        course is CourseraCourse -> None
-        course is CheckiOCourse -> getCheckiOError(course)
-        course is HyperskillCourse -> if (HyperskillSettings.INSTANCE.account == null) HyperskillLoginRequired else None
-        course is ContestInfo -> None
-        !isLoggedInToStepik() -> if (isStepikLoginRequired(course)) StepikLoginRequired else NotLoggedIn
-        else -> None
-      }
+      if (course == null) return NothingSelected
+      return None
+        .merge(course.compatibilityError)
+        .merge(course.courseSpecificError)
     }
 
-    private fun getCheckiOError(course: Course): ErrorState {
-      if (!EduUtils.hasJavaFx()) {
-        return JavaFXRequired
+    private val Course.compatibilityError: ErrorState
+      get() {
+        return when (val compatibility = compatibility) {
+          CourseCompatibility.Unsupported -> UnsupportedCourse(unsupportedCourseMessage)
+          CourseCompatibility.IncompatibleVersion -> IncompatibleVersion
+          is CourseCompatibility.PluginsRequired -> {
+            if (compatibility.toInstallOrEnable.isEmpty()) RestartNeeded else RequirePlugins(compatibility.toInstallOrEnable)
+          }
+          else -> None
+        }
       }
-      return if (isCheckiOLoginRequired(course)) CheckiOLoginRequired(course.name) else None
-    }
+
+    private val Course.courseSpecificError: ErrorState
+      get() {
+        return when (this) {
+          is CheckiOCourse -> checkiOError
+          is JetBrainsAcademyCourse -> if (HyperskillSettings.INSTANCE.account == null) JetBrainsAcademyLoginRecommended else None
+          is HyperskillCourse -> if (HyperskillSettings.INSTANCE.account == null) HyperskillLoginRequired else None
+          is EduCourse -> {
+            if (!isLoggedInToStepik()) {
+              if (isStepikLoginRequired(this)) StepikLoginRequired else NotLoggedIn
+            }
+            else {
+              None
+            }
+          }
+          else -> None
+        }
+      }
+
+    private val CheckiOCourse.checkiOError: ErrorState
+      get() {
+        if (!EduUtils.hasJavaFx()) {
+          return JavaFXRequired
+        }
+        return if (isCheckiOLoginRequired(this)) CheckiOLoginRequired(name) else None
+      }
 
     private fun errorMessage(plugins: Collection<PluginInfo>, limit: Int = 3): ValidationMessage {
       require(limit > 1)
@@ -114,18 +137,33 @@ sealed class ErrorState(
 
     private fun isLoggedInToStepik(): Boolean = EduSettings.isLoggedIn()
 
-    private fun isStepikLoginRequired(selectedCourse: Course): Boolean =
-      selectedCourse is EduCourse && selectedCourse.isRemote && !selectedCourse.isCompatible
+    private fun isStepikLoginRequired(selectedCourse: EduCourse): Boolean =
+      selectedCourse.isRemote && !selectedCourse.isCompatible
 
-    private fun isCheckiOLoginRequired(selectedCourse: Course): Boolean {
-      if (selectedCourse is CheckiOCourse) {
-        val checkiOConnectorProvider = selectedCourse.configurator as CheckiOConnectorProvider
-        val checkiOAccount = checkiOConnectorProvider.oAuthConnector.account
-        return checkiOAccount == null
-      }
-      return false
+    private fun isCheckiOLoginRequired(selectedCourse: CheckiOCourse): Boolean {
+      val checkiOConnectorProvider = selectedCourse.configurator as CheckiOConnectorProvider
+      val checkiOAccount = checkiOConnectorProvider.oAuthConnector.account
+      return checkiOAccount == null
     }
   }
+}
+
+/**
+ * Order is meaningful. Errors with more priority are located lower
+ */
+private enum class ErrorSeverity {
+  OK,
+
+  LOGIN_RECOMMENDED,
+  LOGIN_ERROR,
+
+  LANGUAGE_SETTINGS_ERROR,
+
+  NO_JAVA_FX,
+
+  PLUGIN_UPDATE_REQUIRED,
+
+  UNSUPPORTED_COURSE
 }
 
 data class ValidationMessage @JvmOverloads constructor(
