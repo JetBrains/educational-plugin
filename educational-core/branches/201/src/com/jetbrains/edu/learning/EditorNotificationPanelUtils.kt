2eea95d9e46a488fd78d37ed213f86a438012809
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2020-05-12 12:01:26 +0300

    Update IDEs and plugins for 201 builds

--- educational-core/branches/201/src/com/jetbrains/edu/learning/EditorNotificationPanelUtils.kt
+++ educational-core/branches/201/src/com/jetbrains/edu/learning/EditorNotificationPanelUtils.kt
@@ -1,13 +1,7 @@
 package com.jetbrains.edu.learning
 
 import com.intellij.ui.EditorNotificationPanel
-import org.jetbrains.annotations.TestOnly
 
-class GeneratedRemoteInfoNotificationPanel : EditorNotificationPanel() {
-  val text: String? = myLabel.text
-}
+class GeneratedRemoteInfoNotificationPanel : EditorNotificationPanel()
 
-class UnsolvedDependenciesNotificationPanel : EditorNotificationPanel() {
-  @TestOnly
-  fun getText(): String = myLabel.text
-}
+class UnsolvedDependenciesNotificationPanel : EditorNotificationPanel()
