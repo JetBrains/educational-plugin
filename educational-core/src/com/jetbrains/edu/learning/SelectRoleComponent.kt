00e36bfd887131f6491d5a8ddc8e636dc12e8f8e
Liana Bakradze <liana.bakradze@jetbrains.com>
2020-05-12 19:16:38 +0300

    EDU-3192 show role selection dialog blocking
    otherwise "Learn" action is created with incorrect text

--- educational-core/src/com/jetbrains/edu/learning/SelectRoleComponent.kt
+++ educational-core/src/com/jetbrains/edu/learning/SelectRoleComponent.kt
@@ -2,13 +2,16 @@ package com.jetbrains.edu.learning
 
 import com.intellij.ide.AppLifecycleListener
 import com.intellij.ide.util.PropertiesComponent
+import com.intellij.openapi.actionSystem.ActionManager
+import com.intellij.openapi.application.AppUIExecutor
 import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.application.runInEdt
+import com.intellij.openapi.application.impl.coroutineDispatchingContext
 import com.intellij.openapi.components.BaseComponent
 import com.intellij.openapi.ui.DialogBuilder
 import com.intellij.util.PlatformUtils
 import com.jetbrains.edu.coursecreator.actions.CCPluginToggleAction
 import com.jetbrains.edu.learning.ui.SelectRolePanel
+import kotlinx.coroutines.runBlocking
 
 @Suppress("ComponentNotRegistered") // educational-core.xml
 class SelectRoleComponent : BaseComponent {
@@ -29,7 +32,12 @@ class SelectRoleComponent : BaseComponent {
     val connection = ApplicationManager.getApplication().messageBus.connect()
     connection.subscribe(AppLifecycleListener.TOPIC, object : AppLifecycleListener {
       override fun appFrameCreated(commandLineArgs: MutableList<String>) {
-        runInEdt {
+        // HACK: ActionManager is instantiated here
+        // otherwise it is instantiated during dialog showing (to render buttons on Mac OS magic touch bar)
+        // which causes assert because one shouldn't instantiate ActionManager in EDT
+        ActionManager.getInstance()
+
+        runBlocking(AppUIExecutor.onUiThread().coroutineDispatchingContext()) {
           showInitialConfigurationDialog()
         }
       }
