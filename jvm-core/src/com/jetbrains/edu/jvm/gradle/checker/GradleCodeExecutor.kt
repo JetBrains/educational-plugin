14277cce1363d7fa3eacd28682081ecf692e4430
Aleksey Rostovskiy <aleksey.rostovskiy@jetbrains.com>
2020-05-13 01:11:23 +0300

    EDU-3261 Run Local Tests button doesn't work

--- jvm-core/src/com/jetbrains/edu/jvm/gradle/checker/GradleCodeExecutor.kt
+++ jvm-core/src/com/jetbrains/edu/jvm/gradle/checker/GradleCodeExecutor.kt
@@ -6,9 +6,15 @@ import com.intellij.openapi.vfs.VirtualFile
 import com.jetbrains.edu.learning.Result
 import com.jetbrains.edu.learning.checker.CheckResult
 import com.jetbrains.edu.learning.checker.CodeExecutor
+import com.jetbrains.edu.learning.checker.DefaultCodeExecutor
+import com.jetbrains.edu.learning.codeforces.courseFormat.CodeforcesTask
 import com.jetbrains.edu.learning.courseFormat.tasks.Task
 
 class GradleCodeExecutor(private val mainClassForFile: (Project, VirtualFile) -> String?) : CodeExecutor {
   override fun execute(project: Project, task: Task, indicator: ProgressIndicator, input: String?): Result<String, CheckResult> =
-    runGradleRunTask(project, task, indicator, mainClassForFile)
+    when (task) {
+      // TODO https://youtrack.jetbrains.com/issue/EDU-3272
+      is CodeforcesTask -> DefaultCodeExecutor().execute(project, task, indicator, input)
+      else -> runGradleRunTask(project, task, indicator, mainClassForFile)
+    }
 }
