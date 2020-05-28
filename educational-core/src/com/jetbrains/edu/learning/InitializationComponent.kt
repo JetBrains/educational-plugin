e02c237f87f62bfff73ad0b0fe30b69a9050ab97
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2020-05-28 12:32:29 +0300

    EDU-3173: drop disabling incompatible old plugins
    Looks like this code is outdated because incompatible plugins are too old and nobody uses them

--- educational-core/src/com/jetbrains/edu/learning/InitializationComponent.kt
+++ educational-core/src/com/jetbrains/edu/learning/InitializationComponent.kt
@@ -1,8 +1,6 @@
 package com.jetbrains.edu.learning
 
 import com.intellij.ide.RecentProjectsManagerBase
-import com.intellij.ide.plugins.PluginManager
-import com.intellij.ide.plugins.PluginManagerCore
 import com.intellij.ide.util.PropertiesComponent
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.application.ApplicationManager
@@ -10,9 +8,7 @@ import com.intellij.openapi.application.runReadAction
 import com.intellij.openapi.components.BaseComponent
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.editor.EditorFactory
-import com.intellij.openapi.extensions.PluginId
 import com.intellij.openapi.util.Disposer
-import com.intellij.openapi.util.text.StringUtil
 import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.util.PathUtil
 import com.jetbrains.edu.learning.authUtils.CustomAuthorizationServer
@@ -35,7 +31,6 @@ import org.jetbrains.io.DelegatingHttpRequestHandlerBase
 import org.jetbrains.io.NettyUtil
 import java.io.File
 import java.net.InetAddress
-import java.util.*
 
 @Suppress("ComponentNotRegistered") // registered in educational-core.xml
 class InitializationComponent : BaseComponent {
@@ -65,27 +60,7 @@ class InitializationComponent : BaseComponent {
       PropertiesComponent.getInstance().setValue(RECENT_COURSES_FILLED, true)
     }
 
-    if (PropertiesComponent.getInstance().isValueSet(CONFLICTING_PLUGINS_DISABLED)) {
-      newCoursesNotifier.scheduleNotification()
-      return
-    }
-
-    // Remove conflicting plugins
-    var disabledPlugins = disablePlugins()
-    if (disabledPlugins.isNotEmpty()) {
-      disabledPlugins = disabledPlugins.map { name -> "'$name'" }
-      val multiplePluginsDisabled = disabledPlugins.size != 1
-
-      val names = if (multiplePluginsDisabled) StringUtil.join(disabledPlugins, ", ") else disabledPlugins[0]
-      val ending = if (multiplePluginsDisabled) "s" else ""
-      val verb = if (multiplePluginsDisabled) "were" else "was"
-
-      restartIDE("Conflicting plugin$ending $names $verb disabled")
-    }
-    else {
-      PropertiesComponent.getInstance().setValue(CONFLICTING_PLUGINS_DISABLED, "true")
-      newCoursesNotifier.scheduleNotification()
-    }
+    newCoursesNotifier.scheduleNotification()
   }
 
   private fun fillRecentCourses() {
@@ -150,33 +125,7 @@ class InitializationComponent : BaseComponent {
     Disposer.register(ApplicationManager.getApplication(), Disposable { channelRegistrar.close() })
   }
 
-  private fun disablePlugins(): List<String> {
-    val disabledPlugins = ArrayList<String>()
-    for (id in IDS) {
-      val plugin = PluginManager.getPlugin(PluginId.getId(id)) ?: continue
-      if (plugin.isEnabled) {
-        disabledPlugins.add(plugin.name)
-        PluginManagerCore.disablePlugin(id)
-      }
-    }
-    return disabledPlugins
-  }
-
   companion object {
-    @JvmField
-    val IDS = arrayOf(
-      "com.jetbrains.edu.intellij",
-      "com.jetbrains.edu.interactivelearning",
-      "com.jetbrains.python.edu.interactivelearning.python",
-      "com.jetbrains.edu.coursecreator",
-      "com.jetbrains.edu.coursecreator.python",
-      "com.jetbrains.edu.kotlin",
-      "com.jetbrains.edu.coursecreator.intellij",
-      "com.jetbrains.edu.java",
-      "com.jetbrains.python.edu.core",
-      "com.jetbrains.edu.core"
-    )
-    const val CONFLICTING_PLUGINS_DISABLED = "Educational.conflictingPluginsDisabled"
     const val RECENT_COURSES_FILLED = "Educational.recentCoursesFilled"
   }
 }
