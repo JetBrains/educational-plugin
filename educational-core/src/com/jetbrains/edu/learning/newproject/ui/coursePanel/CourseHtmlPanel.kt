aa0af31f4e565762ea20fce6a9400a645efbaff4
valentina.kiryushkina <valentina.kiryushkina@jetbrains.com>
2020-06-11 18:16:30 +0300

    EDU-2962 Force JEditorPanel to calculate its size

--- educational-core/src/com/jetbrains/edu/learning/newproject/ui/coursePanel/CourseHtmlPanel.kt
+++ educational-core/src/com/jetbrains/edu/learning/newproject/ui/coursePanel/CourseHtmlPanel.kt
@@ -4,6 +4,7 @@ import com.intellij.ide.plugins.newui.EmptyCaret
 import com.intellij.util.ui.HtmlPanel
 import com.intellij.util.ui.UIUtil
 import com.jetbrains.edu.learning.courseFormat.Course
+import com.jetbrains.edu.learning.messages.EduCoreBundle
 
 abstract class CourseHtmlPanel : HtmlPanel() {
   protected var course: Course? = null
@@ -13,6 +14,9 @@ abstract class CourseHtmlPanel : HtmlPanel() {
     isFocusable = false
     border = null
     caret = EmptyCaret.INSTANCE
+
+    // set some text to force JEditorPane calculate its height properly
+    text = EduCoreBundle.message("course.dialog.no.course.selected")
   }
 
   fun bind(course: Course) {
