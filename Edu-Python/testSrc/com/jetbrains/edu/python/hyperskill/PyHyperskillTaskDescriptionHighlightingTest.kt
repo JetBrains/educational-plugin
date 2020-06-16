add1570c65f2b6ef7a4c43f21ffb6e5507c55600
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2020-06-16 18:56:05 +0300

    Disable python task description highlighting tests in AS
    Hyperskill python support is not available in Android Studio so these tests make no sense in AS

--- Edu-Python/testSrc/com/jetbrains/edu/python/hyperskill/PyHyperskillTaskDescriptionHighlightingTest.kt
+++ Edu-Python/testSrc/com/jetbrains/edu/python/hyperskill/PyHyperskillTaskDescriptionHighlightingTest.kt
@@ -1,11 +1,20 @@
 package com.jetbrains.edu.python.hyperskill
 
 import com.intellij.lang.Language
+import com.jetbrains.edu.learning.EduUtils
 import com.jetbrains.edu.learning.stepik.hyperskill.HyperskillTaskDescriptionHighlightingTest
 import com.jetbrains.python.PythonLanguage
 import com.jetbrains.python.newProject.PyNewProjectSettings
 
 class PyHyperskillTaskDescriptionHighlightingTest : HyperskillTaskDescriptionHighlightingTest() {
+
+  override fun runTest() {
+    // Hyperskill python support is not available in Android Studio
+    if (!EduUtils.isAndroidStudio()) {
+      super.runTest()
+    }
+  }
+
   override val language: Language
     get() = PythonLanguage.INSTANCE
 
