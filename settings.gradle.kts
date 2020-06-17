cc977f6461e8bf2af7ffc5e7285fcd13128920bb
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2020-06-17 12:01:51 +0300

    EDU-3394: convert `settings.gradle` to `settings.gradle.kts`

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+rootProject.name = "educational-plugin"
+include(
+  "educational-core",
+  "jvm-core",
+  "Edu-YAML",
+  "Edu-Java",
+  "Edu-Kotlin",
+  "Edu-Scala",
+  "Edu-Python",
+  "Edu-Python:Idea", // python support for IDEA and Android Studio
+  "Edu-Python:PyCharm", // python support for PyCharm and CLion
+  "Edu-Android",
+  "Edu-JavaScript",
+  "Edu-Rust",
+  "Edu-Cpp",
+  "Edu-Go"
+)
