8da0855124c142c2e994d478d66fe2b1d37fdb8b
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2020-06-30 15:01:04 +0300

    Drop outdated code

--- Edu-Cpp/src/com/jetbrains/edu/cpp/compatibility/CppGTestCourseCompatibilityProvider.kt
+++ Edu-Cpp/src/com/jetbrains/edu/cpp/compatibility/CppGTestCourseCompatibilityProvider.kt
@@ -1,20 +1,13 @@
 package com.jetbrains.edu.cpp.compatibility
 
-import com.intellij.openapi.application.ApplicationInfo
-import com.intellij.openapi.util.BuildNumber
 import com.jetbrains.edu.learning.compatibility.CourseCompatibilityProvider
 import com.jetbrains.edu.learning.plugins.PluginInfo
 import icons.EducationalCoreIcons
 import javax.swing.Icon
 
 class CppGTestCourseCompatibilityProvider : CourseCompatibilityProvider {
-  override fun requiredPlugins(): List<PluginInfo>? =
-    if (ApplicationInfo.getInstance().build < BUILD_193) emptyList() else listOf(PluginInfo.GOOGLE_TEST)
+  override fun requiredPlugins(): List<PluginInfo>? = listOf(PluginInfo.GOOGLE_TEST)
 
   override val technologyName: String get() = "C/C++"
   override val logo: Icon get() = EducationalCoreIcons.CppLogo
-
-  companion object {
-    private val BUILD_193: BuildNumber = BuildNumber.fromString("193")!!
-  }
 }
