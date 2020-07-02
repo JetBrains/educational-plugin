f0c5455087a8729fde976399513546c802bdbec8
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2020-07-02 12:47:52 +0300

    EDU-3173: convert GradleProjectComponent into startup activity

--- jvm-core/src/com/jetbrains/edu/jvm/gradle/GradleStartupActivity.kt
+++ jvm-core/src/com/jetbrains/edu/jvm/gradle/GradleStartupActivity.kt
@@ -1,53 +1,53 @@
 package com.jetbrains.edu.jvm.gradle
 
-import com.intellij.openapi.components.ProjectComponent
 import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.project.DumbService
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ProjectRootManager
-import com.intellij.openapi.startup.StartupManager
+import com.intellij.openapi.startup.StartupActivity
 import com.intellij.openapi.util.io.FileUtil
 import com.intellij.openapi.vfs.VirtualFileManager
 import com.jetbrains.edu.jvm.gradle.generation.EduGradleUtils
-import com.jetbrains.edu.learning.EduUtils.isEduProject
+import com.jetbrains.edu.learning.EduUtils
 import com.jetbrains.edu.learning.StudyTaskManager
-import com.jetbrains.edu.learning.gradle.GradleConstants.GRADLE_WRAPPER_UNIX
+import com.jetbrains.edu.learning.gradle.GradleConstants
 import java.io.File
 
-@Suppress("ComponentNotRegistered") // jvm-core.xml
-class GradleProjectComponent(private val project: Project) : ProjectComponent {
+class GradleStartupActivity : StartupActivity.DumbAware {
 
-  override fun projectOpened() {
-    if (project.isDisposed || !isEduProject(project)) {
+  override fun runActivity(project: Project) {
+    if (project.isDisposed || !EduUtils.isEduProject(project)) {
       return
     }
     if (EduGradleUtils.isConfiguredWithGradle(project)) {
-      updateGradleSettings()
+      updateGradleSettings(project)
     }
 
-    StartupManager.getInstance(project).runWhenProjectIsInitialized {
-      val course = StudyTaskManager.getInstance(project).course
+    DumbService.getInstance(project).runWhenSmart {
+      val taskManager = StudyTaskManager.getInstance(project)
+      val course = taskManager.course
       if (course == null) {
         LOG.warn("Opened project is with null course")
-        return@runWhenProjectIsInitialized
+        return@runWhenSmart
       }
 
       if (EduGradleUtils.isConfiguredWithGradle(project)) {
-        setupGradleProject()
+        setupGradleProject(project)
       }
     }
   }
 
-  private fun updateGradleSettings() {
+  private fun updateGradleSettings(project: Project) {
     val projectBasePath = project.basePath ?: return
     val sdk = ProjectRootManager.getInstance(project).projectSdk
     EduGradleUtils.setGradleSettings(project, sdk, projectBasePath)
   }
 
-  private fun setupGradleProject() {
+  private fun setupGradleProject(project: Project) {
     val projectBasePath = project.basePath
     if (projectBasePath != null) {
       // Android Studio creates non executable `gradlew`
-      val gradlew = File(FileUtil.toSystemDependentName(projectBasePath), GRADLE_WRAPPER_UNIX)
+      val gradlew = File(FileUtil.toSystemDependentName(projectBasePath), GradleConstants.GRADLE_WRAPPER_UNIX)
       if (gradlew.exists()) {
         gradlew.setExecutable(true)
       }
@@ -58,6 +58,6 @@ class GradleProjectComponent(private val project: Project) : ProjectComponent {
   }
 
   companion object {
-    private val LOG = Logger.getInstance(GradleProjectComponent::class.java)
+    private val LOG = Logger.getInstance(GradleStartupActivity::class.java)
   }
 }
