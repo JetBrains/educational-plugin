08349af070213228eee851f282ec95ffbea0d0b9
Liana Bakradze <liana.bakradze@jetbrains.com>
2020-07-22 15:06:54 +0300

    EDU-3497 register test as test dir for Hyperskill legacy python

--- Edu-Python/src/com/jetbrains/edu/python/learning/stepik/hyperskill/PyHyperskillConfigurator.kt
+++ Edu-Python/src/com/jetbrains/edu/python/learning/stepik/hyperskill/PyHyperskillConfigurator.kt
@@ -1,12 +1,13 @@
 package com.jetbrains.edu.python.learning.stepik.hyperskill
 
+import com.jetbrains.edu.learning.EduNames
 import com.jetbrains.edu.learning.EduUtils.isAndroidStudio
 import com.jetbrains.edu.learning.stepik.hyperskill.HyperskillConfigurator
 import com.jetbrains.edu.python.learning.PyConfigurator
 import com.jetbrains.python.newProject.PyNewProjectSettings
 
 class PyHyperskillConfigurator : HyperskillConfigurator<PyNewProjectSettings>(PyConfigurator()) {
-  override val testDirs: List<String> = listOf(HYPERSKILL_TEST_DIR)
+  override val testDirs: List<String> = listOf(HYPERSKILL_TEST_DIR, EduNames.TEST)
   override val isEnabled: Boolean = !isAndroidStudio()
   override val isCourseCreatorEnabled: Boolean = true
 }
