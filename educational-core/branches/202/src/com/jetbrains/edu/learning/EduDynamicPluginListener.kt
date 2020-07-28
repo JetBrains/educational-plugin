d43e1354a25f61357b565f5da1572f72b0fe14c9
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2020-07-28 11:03:20 +0300

    EDU-3548: fix IllegalStateException during plugin installation
    Found out that `pluginDescriptor.pluginId` can be null here that led to `IllegalStateException` for some plugins

--- educational-core/branches/202/src/com/jetbrains/edu/learning/EduDynamicPluginListener.kt
+++ educational-core/branches/202/src/com/jetbrains/edu/learning/EduDynamicPluginListener.kt
@@ -3,11 +3,12 @@ package com.jetbrains.edu.learning
 import com.intellij.ide.plugins.CannotUnloadPluginException
 import com.intellij.ide.plugins.DynamicPluginListener
 import com.intellij.ide.plugins.IdeaPluginDescriptor
+import com.intellij.openapi.extensions.PluginId
 
 class EduDynamicPluginListener : DynamicPluginListener {
 
   override fun checkUnloadPlugin(pluginDescriptor: IdeaPluginDescriptor) {
-    if (pluginDescriptor.pluginId.idString == EduNames.PLUGIN_ID) {
+    if (pluginDescriptor.pluginId == PluginId.getId(EduNames.PLUGIN_ID)) {
       throw CannotUnloadPluginException("EduTools plugin unloading is not supported yet")
     }
   }
