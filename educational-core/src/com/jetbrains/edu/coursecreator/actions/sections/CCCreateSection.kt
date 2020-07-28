a3110d0f59de5d8b111990c6752b4bc59df754eb
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2020-07-28 18:45:49 +0300

    Convert `CCCreateSection` into kotlin. Step 2

--- educational-core/src/com/jetbrains/edu/coursecreator/actions/sections/CCCreateSection.kt
+++ educational-core/src/com/jetbrains/edu/coursecreator/actions/sections/CCCreateSection.kt
@@ -1,92 +1,55 @@
-package com.jetbrains.edu.coursecreator.actions.sections;
+package com.jetbrains.edu.coursecreator.actions.sections
 
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.util.Function;
-import com.jetbrains.edu.coursecreator.CCUtils;
-import com.jetbrains.edu.coursecreator.actions.CCCreateStudyItemActionBase;
-import com.jetbrains.edu.coursecreator.actions.NewStudyItemInfo;
-import com.jetbrains.edu.coursecreator.actions.StudyItemVariant;
-import com.jetbrains.edu.learning.OpenApiExtKt;
-import com.jetbrains.edu.learning.courseFormat.Course;
-import com.jetbrains.edu.learning.courseFormat.Section;
-import com.jetbrains.edu.learning.courseFormat.StudyItem;
-import icons.EducationalCoreIcons;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.util.Function
+import com.jetbrains.edu.coursecreator.CCUtils.createSectionDir
+import com.jetbrains.edu.coursecreator.StudyItemType
+import com.jetbrains.edu.coursecreator.actions.CCCreateStudyItemActionBase
+import com.jetbrains.edu.coursecreator.actions.NewStudyItemInfo
+import com.jetbrains.edu.coursecreator.actions.StudyItemVariant
+import com.jetbrains.edu.coursecreator.presentableTitleName
+import com.jetbrains.edu.learning.courseDir
+import com.jetbrains.edu.learning.courseFormat.Course
+import com.jetbrains.edu.learning.courseFormat.Section
+import com.jetbrains.edu.learning.courseFormat.StudyItem
+import icons.EducationalCoreIcons
 
-import java.util.Collections;
-import java.util.List;
-
-import static com.jetbrains.edu.coursecreator.StudyItemType.SECTION_TYPE;
-import static com.jetbrains.edu.coursecreator.StudyItemTypeKt.getPresentableTitleName;
-
-public class CCCreateSection extends CCCreateStudyItemActionBase<Section> {
-
-  public CCCreateSection() {
-    super(SECTION_TYPE, EducationalCoreIcons.Section);
+class CCCreateSection_ : CCCreateStudyItemActionBase<Section>(StudyItemType.SECTION_TYPE, EducationalCoreIcons.Section) {
+  override fun addItem(course: Course, item: Section) {
+    course.addSection(item)
   }
 
-  @Override
-  protected void addItem(@NotNull Course course, @NotNull Section section) {
-    course.addSection(section);
+  override fun getStudyOrderable(item: StudyItem, course: Course): Function<VirtualFile, out StudyItem> {
+    return Function<VirtualFile, StudyItem> { file: VirtualFile -> course.getItem(file.name) }
   }
 
-  @NotNull
-  @Override
-  protected Function<VirtualFile, ? extends StudyItem> getStudyOrderable(@NotNull final StudyItem item,
-                                                                         @NotNull Course course) {
-    return file -> course.getItem(file.getName());
+  override fun createItemDir(project: Project, course: Course, item: Section, parentDirectory: VirtualFile): VirtualFile? {
+    return createSectionDir(project, item.name)
   }
 
-  @Override
-  @Nullable
-  protected VirtualFile createItemDir(@NotNull final Project project, @NotNull final Course course, @NotNull final Section section,
-                                      @NotNull final VirtualFile parentDirectory) {
-    return CCUtils.createSectionDir(project, section.getName());
+  override fun getSiblingsSize(course: Course, parentItem: StudyItem?): Int = course.items.size
+
+  override fun getParentItem(project: Project, course: Course, directory: VirtualFile): StudyItem? = course
+
+  override fun getThresholdItem(project: Project, course: Course, sourceDirectory: VirtualFile): StudyItem? {
+    return course.getItem(sourceDirectory.name)
   }
 
-  @Override
-  protected int getSiblingsSize(@NotNull Course course, @Nullable StudyItem parentItem) {
-    return course.getItems().size();
+  override fun isAddedAsLast(project: Project, course: Course, sourceDirectory: VirtualFile): Boolean {
+    return sourceDirectory == project.courseDir
   }
 
-  @Nullable
-  @Override
-  protected StudyItem getParentItem(@NotNull Project project, @NotNull Course course, @NotNull VirtualFile directory) {
-    return course;
+  override fun sortSiblings(course: Course, parentItem: StudyItem?) {
+    course.sortItems()
   }
 
-  @Nullable
-  @Override
-  protected StudyItem getThresholdItem(@NotNull Project project, @NotNull final Course course, @NotNull final VirtualFile sourceDirectory) {
-    return course.getItem(sourceDirectory.getName());
+  override fun initItem(project: Project, course: Course, parentItem: StudyItem?, item: Section, info: NewStudyItemInfo) {
+    item.course = course
   }
 
-  @Override
-  protected boolean isAddedAsLast(@NotNull Project project, @NotNull Course course, @NotNull VirtualFile sourceDirectory) {
-    return sourceDirectory.equals(OpenApiExtKt.getCourseDir(project));
-  }
-
-  @Override
-  protected void sortSiblings(@NotNull Course course, @Nullable StudyItem parentItem) {
-    course.sortItems();
-  }
-
-  @Override
-  protected void initItem(@NotNull Project project,
-                          @NotNull Course course,
-                          @Nullable StudyItem parentItem,
-                          @NotNull Section item,
-                          @NotNull NewStudyItemInfo info) {
-    item.setCourse(course);
-  }
-
-  @NotNull
-  @Override
-  protected List<StudyItemVariant> getStudyItemVariants() {
-    return Collections.singletonList(
-      new StudyItemVariant(getPresentableTitleName(SECTION_TYPE), "", EducationalCoreIcons.Section, Section::new)
-    );
-  }
+  override val studyItemVariants: List<StudyItemVariant>
+    get() = listOf(
+      StudyItemVariant(StudyItemType.SECTION_TYPE.presentableTitleName, "", EducationalCoreIcons.Section) { Section() }
+    )
 }
