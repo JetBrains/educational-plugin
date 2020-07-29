96f548175e79a8084ad74203ee626b6a3466edab
Liana Bakradze <liana.bakradze@jetbrains.com>
2020-07-29 16:57:32 +0300

    EDU-3168 added error for missing js interpreter

--- Edu-JavaScript/src/com/jetbrains/edu/javascript/learning/JsEnvironmentChecker.kt
+++ Edu-JavaScript/src/com/jetbrains/edu/javascript/learning/JsEnvironmentChecker.kt
@@ -1,10 +1,14 @@
 package com.jetbrains.edu.javascript.learning
 
+import com.intellij.javascript.nodejs.interpreter.NodeJsInterpreterManager
 import com.intellij.openapi.project.Project
 import com.jetbrains.edu.learning.checker.EnvironmentChecker
+import com.jetbrains.edu.learning.messages.EduCoreErrorBundle
 
 class JsEnvironmentChecker : EnvironmentChecker() {
   override fun checkEnvironment(project: Project): String? {
-    return null // TODO implement validation
+    return if (NodeJsInterpreterManager.getInstance(project).interpreter == null)
+      EduCoreErrorBundle.message("no.interpreter", "Node.js")
+    else null
   }
 }
