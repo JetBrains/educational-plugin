97cdeaa4d6589edabc0ce7c8a1ef598253a0b1a1
Liana Bakradze <liana.bakradze@jetbrains.com>
2020-07-29 18:00:41 +0300

    EDU-3168 rust env validation: toolchain and cargo project

--- Edu-Rust/src/com/jetbrains/edu/rust/checker/RsTaskCheckerProvider.kt
+++ Edu-Rust/src/com/jetbrains/edu/rust/checker/RsTaskCheckerProvider.kt
@@ -2,12 +2,31 @@ package com.jetbrains.edu.rust.checker
 
 import com.intellij.openapi.project.Project
 import com.jetbrains.edu.learning.checker.CodeExecutor
+import com.jetbrains.edu.learning.checker.EnvironmentChecker
 import com.jetbrains.edu.learning.checker.TaskChecker
 import com.jetbrains.edu.learning.checker.TaskCheckerProvider
 import com.jetbrains.edu.learning.courseFormat.tasks.EduTask
+import com.jetbrains.edu.rust.messages.EduRustBundle
+import org.rust.cargo.project.model.cargoProjects
+import org.rust.cargo.project.settings.rustSettings
 
 class RsTaskCheckerProvider : TaskCheckerProvider {
-  // TODO implement envChecker validation
+  override val envChecker: EnvironmentChecker
+    get() = object: EnvironmentChecker() {
+      override fun checkEnvironment(project: Project): String? {
+        val toolchain = project.rustSettings.toolchain
+        if (toolchain == null || !toolchain.looksLikeValidToolchain()) {
+          return EduRustBundle.message("error.no.toolchain.location")
+        }
+
+        if (!project.cargoProjects.hasAtLeastOneValidProject) {
+          return EduRustBundle.message("error.no.cargo.project")
+        }
+
+        return null
+      }
+    }
+
   override fun getEduTaskChecker(task: EduTask, project: Project): TaskChecker<EduTask> = RsEduTaskChecker(project, envChecker, task)
 
   override fun getCodeExecutor(): CodeExecutor = RsCodeExecutor()
