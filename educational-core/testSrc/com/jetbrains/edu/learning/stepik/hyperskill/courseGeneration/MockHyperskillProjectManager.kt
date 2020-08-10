a7509dd7d6a8bf6b55ba52e51e1d11513dc9735b
Liana Bakradze <liana.bakradze@jetbrains.com>
2020-08-10 13:50:38 +0300

    EDU-3589 assert project consistently

--- educational-core/testSrc/com/jetbrains/edu/learning/stepik/hyperskill/courseGeneration/MockHyperskillProjectManager.kt
+++ educational-core/testSrc/com/jetbrains/edu/learning/stepik/hyperskill/courseGeneration/MockHyperskillProjectManager.kt
@@ -11,12 +11,17 @@ class MockHyperskillProjectManager : HyperskillProjectManager() {
   var project: Project? = null
 
   override fun newProject(course: HyperskillCourse) {
-    project ?: error("Set up project explicitly in tests")
+    assertProject()
     course.createCourseFiles(project!!, ModuleManager.getInstance(project!!).modules[0])
   }
 
   override fun focusOpenProject(coursePredicate: (Course) -> Boolean): Pair<Project, Course>? {
+    assertProject()
     val course = project!!.course ?: return null
     return if (coursePredicate(course)) project!! to course else null
+  }
+
+  private fun assertProject() {
+    project ?: error("Set up project explicitly in tests")
   }
 }
