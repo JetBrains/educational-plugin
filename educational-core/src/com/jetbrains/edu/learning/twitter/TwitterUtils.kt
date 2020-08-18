1f467cb700b3283d1d85fad50efeb92fa3019ff0
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2020-08-18 14:03:21 +0300

    EDU-3600: wrap tweet posting in background task
    It moved network requests from UI thread and prevents freezes

--- educational-core/src/com/jetbrains/edu/learning/twitter/TwitterUtils.kt
+++ educational-core/src/com/jetbrains/edu/learning/twitter/TwitterUtils.kt
@@ -3,8 +3,12 @@ package com.jetbrains.edu.learning.twitter
 import com.intellij.ide.BrowserUtil
 import com.intellij.ide.plugins.PluginManagerCore
 import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.invokeAndWaitIfNeeded
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.extensions.PluginId
+import com.intellij.openapi.progress.ProgressIndicator
+import com.intellij.openapi.progress.ProgressManager
+import com.intellij.openapi.progress.Task.Backgroundable
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.InputValidatorEx
 import com.intellij.openapi.ui.Messages
@@ -12,6 +16,7 @@ import com.intellij.openapi.util.io.FileUtil
 import com.intellij.openapi.util.text.StringUtil
 import com.intellij.util.io.exists
 import com.jetbrains.edu.learning.EduNames
+import com.jetbrains.edu.learning.checkIsBackgroundThread
 import com.jetbrains.edu.learning.courseFormat.tasks.Task
 import com.jetbrains.edu.learning.messages.EduCoreBundle
 import com.jetbrains.edu.learning.messages.EduCoreErrorBundle
@@ -49,29 +54,35 @@ object TwitterUtils {
       val dialog = createTwitterDialogUI(project) { configurator.getTweetDialogPanel(task, it) }
       if (dialog.showAndGet()) {
         val settings = TwitterSettings.getInstance()
-        try {
-          val isAuthorized = settings.accessToken.isNotEmpty()
-          val twitter = twitter
-          val info = TweetInfo(dialog.message, configurator.getImagePath(task))
+        val isAuthorized = settings.accessToken.isNotEmpty()
+        val twitter = twitter
+        val info = TweetInfo(dialog.message, configurator.getImagePath(task))
 
-          if (!isAuthorized) {
-            authorize(project, twitter)
+        ProgressManager.getInstance().run(object : Backgroundable(project, EduCoreBundle.message("twitter.loading.posting"), true) {
+          override fun run(indicator: ProgressIndicator) {
+            if (!isAuthorized) {
+              if (!authorize(project, twitter)) return
+            }
+            else {
+              twitter.oAuthAccessToken = AccessToken(settings.accessToken, settings.tokenSecret)
+            }
+
+            ProgressManager.checkCanceled()
+            updateStatus(twitter, info)
+            EduCounterUsageCollector.twitterAchievementPosted(task.course)
           }
-          else {
-            twitter.oAuthAccessToken = AccessToken(settings.accessToken, settings.tokenSecret)
+
+          override fun onThrowable(error: Throwable) {
+            LOG.warn(error)
+            val message = if (error is TwitterException && error.statusCode == HttpStatus.SC_UNAUTHORIZED) {
+              EduCoreErrorBundle.message("failed.to.authorize")
+            }
+            else {
+              EduCoreErrorBundle.message("failed.to.update.status")
+            }
+            Messages.showErrorDialog(project, message, EduCoreErrorBundle.message("twitter.failed.to.tweet"))
           }
-          updateStatus(twitter, info)
-          EduCounterUsageCollector.twitterAchievementPosted(task.course)
-        }
-        catch (e: Exception) {
-          LOG.warn(e)
-          val message = if (e is TwitterException && e.statusCode == HttpStatus.SC_UNAUTHORIZED) {
-            EduCoreErrorBundle.message("failed.to.authorize")
-          } else {
-            EduCoreErrorBundle.message("failed.to.update.status")
-          }
-          Messages.showErrorDialog(project, message, EduCoreErrorBundle.message("twitter.failed.to.tweet"))
-        }
+        })
       }
     }
   }
@@ -81,6 +92,7 @@ object TwitterUtils {
    */
   @Throws(IOException::class, TwitterException::class)
   private fun updateStatus(twitter: Twitter, info: TweetInfo) {
+    checkIsBackgroundThread()
     val update = StatusUpdate(info.message)
     val mediaPath = info.mediaPath
     if (mediaPath != null) {
@@ -90,15 +102,24 @@ object TwitterUtils {
     BrowserUtil.browse("https://twitter.com/")
   }
 
+  /**
+   * Returns true if a user finished authorization successfully, false otherwise
+   */
   @Throws(TwitterException::class)
-  private fun authorize(project: Project, twitter: Twitter) {
+  private fun authorize(project: Project, twitter: Twitter): Boolean {
+    checkIsBackgroundThread()
     val requestToken = twitter.oAuthRequestToken
     BrowserUtil.browse(requestToken.authorizationURL)
-    val pin = createAndShowPinDialog(project) ?: return
+    val pin = invokeAndWaitIfNeeded { createAndShowPinDialog(project) } ?: return false
+    ProgressManager.checkCanceled()
     val token = twitter.getOAuthAccessToken(requestToken, pin)
-    val settings = TwitterSettings.getInstance()
-    settings.accessToken = token.token
-    settings.tokenSecret = token.tokenSecret
+    ProgressManager.checkCanceled()
+    invokeAndWaitIfNeeded {
+      val settings = TwitterSettings.getInstance()
+      settings.accessToken = token.token
+      settings.tokenSecret = token.tokenSecret
+    }
+    return true
   }
 
   private fun createAndShowPinDialog(project: Project): String? {
