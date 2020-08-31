f3af64c477ee6cb00aee8f9df3e645c95f23f926
Olga Telezhnaya <olga.telezhnaya@jetbrains.com>
2020-08-31 10:40:41 +0300

    EDU-3634 CF: add test that proper file is selected for submit

--- educational-core/testSrc/com/jetbrains/edu/learning/codeforces/CodeforcesCopyAndSubmitTest.kt
+++ educational-core/testSrc/com/jetbrains/edu/learning/codeforces/CodeforcesCopyAndSubmitTest.kt
@@ -21,4 +21,25 @@ class CodeforcesCopyAndSubmitTest : CodeforcesTestCase() {
     val actualContentsString = actualContents.getTransferData(actualContents.transferDataFlavors.first())
     assertEquals(contents, actualContentsString)
   }
+
+  fun `test codeforces Task file is selected for submitting`() {
+    val testFileName = "testData/1/input.txt"
+    val taskFileName = "Task.txt"
+    val contents = "123\n456"
+    courseWithFiles(courseProducer = ::CodeforcesCourse) {
+      lesson {
+        codeforcesTask {
+          taskFile("Foo.txt", visible = false)
+          taskFile(testFileName)
+          taskFile(taskFileName, contents)
+        }
+      }
+    }
+    configureByTaskFile(1, 1, testFileName)
+
+    myFixture.testAction(CodeforcesCopyAndSubmitAction())
+    val actualContents = CopyPasteManager.getInstance().contents!!
+    val actualContentsString = actualContents.getTransferData(actualContents.transferDataFlavors.first())
+    assertEquals(contents, actualContentsString)
+  }
 }
