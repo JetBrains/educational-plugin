02aa0604c10a640fac6c186da739c2ede55aa271
Valentina Kiryushkina <valentina.kiryushkina@jetbrains.com>
2020-09-14 19:17:21 +0300

    remove unused class

--- educational-core/src/com/jetbrains/edu/learning/newproject/ui/courseSettings/NoLineTitledSeparator.kt
+++
@@ -1,9 +0,0 @@
-package com.jetbrains.edu.learning.newproject.ui.courseSettings
-
-import com.intellij.ui.components.JBLabel
-import com.intellij.util.ui.JBUI
-import com.intellij.util.ui.UIUtil
-import org.jetbrains.annotations.Nls
-import java.awt.BorderLayout
-import javax.swing.JPanel
-
