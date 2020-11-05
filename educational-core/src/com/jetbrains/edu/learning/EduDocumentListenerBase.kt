5aa1376b3b34a11605e02f3d1344e4ed1673a6d7
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2020-11-05 18:58:16 +0300

    EDU-3696: update placeholder properly while project importing
    Previously, we used `ProjectFileIndex` to detect if file belongs to project.
    But while project importing content roots are not set up yet so we could miss some events.
    Now, we just check that changed file is located under project directory

--- educational-core/src/com/jetbrains/edu/learning/EduDocumentListenerBase.kt
+++ educational-core/src/com/jetbrains/edu/learning/EduDocumentListenerBase.kt
@@ -4,15 +4,16 @@ import com.intellij.openapi.editor.event.DocumentEvent
 import com.intellij.openapi.editor.event.DocumentListener
 import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.roots.ProjectFileIndex
+import com.intellij.openapi.project.guessProjectDir
+import com.intellij.openapi.vfs.VfsUtil
 
 abstract class EduDocumentListenerBase(protected val project: Project) : DocumentListener {
 
   protected val fileDocumentManager: FileDocumentManager = FileDocumentManager.getInstance()
-  private val projectFileIndex: ProjectFileIndex = ProjectFileIndex.getInstance(project)
 
   protected fun DocumentEvent.isInProjectContent(): Boolean {
     val file = fileDocumentManager.getFile(document) ?: return false
-    return projectFileIndex.isInContent(file)
+    val projectDir = project.guessProjectDir() ?: return false
+    return VfsUtil.isAncestor(projectDir, file, true)
   }
 }
