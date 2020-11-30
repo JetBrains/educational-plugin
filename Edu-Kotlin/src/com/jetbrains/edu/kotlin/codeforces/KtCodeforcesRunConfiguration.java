ad700ba11a915ea09b243ce1300b5fd9eb24c9fb
Olga Telezhnaya <olga.telezhnaya@jetbrains.com>
2020-11-30 14:46:35 +0300

    EDU-2989 properly fill Kotlin main class name for run configuration

--- Edu-Kotlin/src/com/jetbrains/edu/kotlin/codeforces/KtCodeforcesRunConfiguration.java
+++ Edu-Kotlin/src/com/jetbrains/edu/kotlin/codeforces/KtCodeforcesRunConfiguration.java
@@ -3,9 +3,13 @@ package com.jetbrains.edu.kotlin.codeforces;
 import com.intellij.execution.configurations.ConfigurationFactory;
 import com.intellij.execution.configurations.JavaRunConfigurationModule;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiManager;
 import com.jetbrains.edu.learning.codeforces.run.CodeforcesRunConfiguration;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.kotlin.idea.run.KotlinRunConfiguration;
+import org.jetbrains.kotlin.idea.run.KotlinRunConfigurationProducer;
+import org.jetbrains.kotlin.psi.KtDeclarationContainer;
 
 import static com.intellij.openapi.module.ModuleUtilCore.findModuleForFile;
 import static com.jetbrains.edu.learning.codeforces.run.CodeforcesRunConfigurationType.CONFIGURATION_ID;
@@ -18,6 +22,15 @@ public class KtCodeforcesRunConfiguration extends KotlinRunConfiguration impleme
   @Override
   public void setExecutableFile(@NotNull VirtualFile file) {
     setModule(findModuleForFile(file, getProject()));
-    setRunClass("MainKt"); // don't like this, still search for possibility to get rid of it
+    PsiElement element = PsiManager.getInstance(getProject()).findFile(file);
+    if (element == null) {
+      throw new IllegalStateException("Unable to find psiElement for " + file.getPath());
+    }
+    KtDeclarationContainer container = KotlinRunConfigurationProducer.Companion.getEntryPointContainer(element);
+    if (container == null) {
+      throw new IllegalStateException("Unable to set executable file for " + file.getPath());
+    }
+    String name = KotlinRunConfigurationProducer.Companion.getStartClassFqName(container);
+    setRunClass(name);
   }
 }
