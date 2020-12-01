5152f72fd71e8e1254e63700d856d61290d462d8
Ekaterina Smal <ekaterina.smal@jetbrains.com>
2020-12-01 16:26:02 +0300

    show marketplace course tab only when MARKETPLACE feature is enabled

--- educational-core/src/com/jetbrains/edu/learning/marketplace/MarketplacePlatformProviderFactory.kt
+++ educational-core/src/com/jetbrains/edu/learning/marketplace/MarketplacePlatformProviderFactory.kt
@@ -1,6 +1,8 @@
 package com.jetbrains.edu.learning.marketplace
 
 import com.intellij.icons.AllIcons
+import com.jetbrains.edu.learning.EduExperimentalFeatures
+import com.jetbrains.edu.learning.isFeatureEnabled
 import com.jetbrains.edu.learning.marketplace.api.MarketplaceConnector
 import com.jetbrains.edu.learning.newproject.ui.CoursesPanel
 import com.jetbrains.edu.learning.newproject.ui.CoursesPlatformProvider
@@ -11,7 +13,8 @@ import kotlinx.coroutines.CoroutineScope
 import javax.swing.Icon
 
 class MarketplacePlatformProviderFactory : CoursesPlatformProviderFactory {
-  override fun getProviders(): List<CoursesPlatformProvider> = listOf(MarketplacePlatformProvider())
+  override fun getProviders(): List<CoursesPlatformProvider> =
+    if (isFeatureEnabled(EduExperimentalFeatures.MARKETPLACE)) listOf (MarketplacePlatformProvider()) else emptyList()
 }
 
 private class MarketplacePlatformProvider : CoursesPlatformProvider() {
