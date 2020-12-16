647b3ae261c345cc48b6ec846988c6996dc55ffb
Aleksey Rostovskiy <aleksey.rostovskiy@jetbrains.com>
2020-12-16 14:19:50 +0300

    process Edu-Python

--- Edu-Python/branches/201/src/com/jetbrains/edu/python/learning/compatibilityUtils.kt
+++
@@ -1,8 +0,0 @@
-package com.jetbrains.edu.python.learning
-
-import com.intellij.openapi.project.Project
-import com.intellij.openapi.projectRoots.Sdk
-import com.jetbrains.python.sdk.PythonSdkUpdater
-import java.awt.Component
-
-fun updateOrShowError(sdk: Sdk, project: Project) = PythonSdkUpdater.updateOrShowError(sdk, null, project, null)
