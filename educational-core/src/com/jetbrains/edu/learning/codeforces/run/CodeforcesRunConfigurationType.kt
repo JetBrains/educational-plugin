8ca0a4d5386a2585662199bb3f56271a361476b5
Olga Telezhnaya <olga.telezhnaya@jetbrains.com>
2021-01-14 17:31:29 +0300

    EDU-2989 refactor CodeforcesRunConfigurationType
    ConfigurationTypeBase gives us no advantages and it asks not to overload
    getConfigurationFactories, so we should use addFactory method instead.
    It's easier for us just to implement interface

--- educational-core/src/com/jetbrains/edu/learning/codeforces/run/CodeforcesRunConfigurationType.kt
+++ educational-core/src/com/jetbrains/edu/learning/codeforces/run/CodeforcesRunConfigurationType.kt
@@ -1,21 +1,25 @@
 package com.jetbrains.edu.learning.codeforces.run
 
 import com.intellij.execution.configurations.ConfigurationFactory
-import com.intellij.execution.configurations.ConfigurationTypeBase
+import com.intellij.execution.configurations.ConfigurationType
 import com.intellij.execution.configurations.runConfigurationType
 import icons.EducationalCoreIcons
 import org.jetbrains.annotations.NonNls
+import javax.swing.Icon
 
 /**
  * id has to be one of these values
  * @see com.intellij.execution.InputRedirectAware.TYPES_WITH_REDIRECT_AWARE_UI
  * in other cases, Java configuration will not work
  */
-class CodeforcesRunConfigurationType : ConfigurationTypeBase("Application", CONFIGURATION_ID, CONFIGURATION_ID,
-                                                             EducationalCoreIcons.Codeforces) {
-  override fun getDisplayName(): String {
-    return CONFIGURATION_ID
-  }
+class CodeforcesRunConfigurationType : ConfigurationType {
+  override fun getId(): String = "Application"
+
+  override fun getIcon(): Icon = EducationalCoreIcons.Codeforces
+
+  override fun getConfigurationTypeDescription(): String = CONFIGURATION_ID
+
+  override fun getDisplayName(): String = CONFIGURATION_ID
 
   override fun getConfigurationFactories(): Array<ConfigurationFactory> {
     return arrayOf(CodeforcesRunConfigurationFactory(this))
