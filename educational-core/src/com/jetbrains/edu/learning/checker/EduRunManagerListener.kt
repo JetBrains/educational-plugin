50c1e0ad2b1dc101262db0a806fdf3a6b99d9866
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2021-02-11 11:36:40 +0300

    EDU-4082: set up module configuration properly in Gradle-based projects
    Previously, we used module of run configuration file itself. But in case of Gradle-based projects and Application run configurations it doesn't work because source files have own module (for example, `lesson_name-task_name` for run configuration file and `lesson_name-task_name.main` for source code in task). As a result, run configuration failed during launch.
    
    Now, `EduRunManagerListener` uses module of task source directory as module of custom run configuration

--- educational-core/src/com/jetbrains/edu/learning/checker/EduRunManagerListener.kt
+++ educational-core/src/com/jetbrains/edu/learning/checker/EduRunManagerListener.kt
@@ -3,11 +3,14 @@ package com.jetbrains.edu.learning.checker
 import com.intellij.execution.RunManagerListener
 import com.intellij.execution.RunnerAndConfigurationSettings
 import com.intellij.execution.configurations.ModuleBasedConfiguration
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.module.ModuleUtilCore
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.LocalFileSystem
-import com.jetbrains.edu.learning.EduUtils
-import com.jetbrains.edu.learning.isTaskRunConfigurationFile
+import com.intellij.openapi.vfs.VirtualFile
+import com.jetbrains.edu.learning.*
+import com.jetbrains.edu.learning.courseFormat.ext.findSourceDir
 
 /**
  * Sets up correct module for custom task run configuration if needed.
@@ -26,7 +29,23 @@ class EduRunManagerListener(private val project: Project) : RunManagerListener {
 
     val file = LocalFileSystem.getInstance().findFileByPath(path) ?: return
     if (file.isTaskRunConfigurationFile(project)) {
-      configuration.setModule(ModuleUtilCore.findModuleForFile(file, project))
+      // It's essential to search module for src dir because in some cases (Gradle-based projects)
+      // modules of task directory and source directory may differ
+      when (val result = findSrcDir(file)) {
+        is Ok -> configuration.setModule(ModuleUtilCore.findModuleForFile(result.value, project))
+        is Err -> LOG.error(result.error)
+      }
     }
+  }
+
+  private fun findSrcDir(file: VirtualFile): Result<VirtualFile, String> {
+    val taskDir = file.getTaskDir(project) ?: return Err("Failed to find task dir for $file")
+    val task = taskDir.getTask(project) ?: return Err("Failed to find task for $taskDir")
+    val srcDir = task.findSourceDir(taskDir) ?: return Err("Failed to find source dir for ${task.name} task")
+    return Ok(srcDir)
+  }
+
+  companion object {
+    private val LOG: Logger = logger<EduRunManagerListener>()
   }
 }
