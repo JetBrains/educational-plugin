18609423bfa17975bfa301452f4d910c9620dcd8
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2021-02-18 10:38:58 +0300

    EDU-4018: disable Rust support for old releases
    Otherwise, we will get some runtime errors

--- Edu-Rust/src/com/jetbrains/edu/rust/RsConfigurator.kt
+++ Edu-Rust/src/com/jetbrains/edu/rust/RsConfigurator.kt
@@ -1,6 +1,8 @@
 package com.jetbrains.edu.rust
 
+import com.intellij.openapi.application.ApplicationInfo
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.BuildNumber
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.util.text.VersionComparatorUtil
 import com.jetbrains.edu.learning.EduCourseBuilder
@@ -42,12 +44,20 @@ class RsConfigurator : EduConfigurator<RsProjectSettings> {
     get() {
       val rustPluginVersion = pluginVersion("org.rust.lang") ?: return false
       val tomlPluginVersion = pluginVersion("org.toml.lang") ?: return false
-      // TOML plugin has incompatibility in API that we use before 0.2.140
-      // so disable Rust support for all versions below 0.2.140
-      return VersionComparatorUtil.compare(rustPluginVersion, "0.3.140") >= 0 &&
-             VersionComparatorUtil.compare(tomlPluginVersion, "0.2.140") >= 0
+      // TOML plugin has incompatibility in API that we use before 0.2.140.
+      // Also, starting with 203 platform, we uses API available only since 141 releases of Rust plugin.
+      // so disable Rust support for all versions below 140 release for 202 platform and below 141 release for 203 branch
+      val releaseNumber = if (ApplicationInfo.getInstance().build < BUILD_203) "140" else "141"
+
+      return VersionComparatorUtil.compare(rustPluginVersion, "0.3.$releaseNumber") >= 0 &&
+             VersionComparatorUtil.compare(tomlPluginVersion, "0.2.$releaseNumber") >= 0
     }
 
   override val defaultPlaceholderText: String
     get() = "/* TODO */"
+
+  companion object {
+    // BACKCOMPAT: 2020.2
+    private val BUILD_203: BuildNumber = BuildNumber.fromString("203")!!
+  }
 }
