71c5c3282e37bdb80f31cbbb03cdd331a90b01b5
Valentina Kiryushkina <valentina.kiryushkina@jetbrains.com>
2021-02-18 18:05:16 +0300

    removed redundant @JvmStatic

--- educational-core/src/com/jetbrains/edu/learning/codeforces/CodeforcesContestConnector.kt
+++ educational-core/src/com/jetbrains/edu/learning/codeforces/CodeforcesContestConnector.kt
@@ -4,16 +4,13 @@ import com.jetbrains.edu.learning.codeforces.CodeforcesNames.CODEFORCES_URL
 import org.jsoup.nodes.Document
 
 object CodeforcesContestConnector {
-  @JvmStatic
   fun getContestIdFromLink(link: String): Int {
     val match = Regex("codeforces.com/contest/(\\d*)").find(link) ?: return -1
     return match.destructured.component1().also { if (it == "") return -1 }.toInt()
   }
 
-  @JvmStatic
   fun getContestURLFromID(id: Int): String = "$CODEFORCES_URL/contest/$id"
 
-  @JvmStatic
   fun getContestId(text: String): Int =
     try {
       Integer.parseInt(text)
@@ -22,7 +19,6 @@ object CodeforcesContestConnector {
       getContestIdFromLink(text)
     }
 
-  @JvmStatic
   fun getLanguages(contest: Document): List<String>? {
     val supportedLanguages = CodeforcesLanguageProvider.getSupportedLanguages()
     return contest.selectFirst("#programTypeForInvoker")
