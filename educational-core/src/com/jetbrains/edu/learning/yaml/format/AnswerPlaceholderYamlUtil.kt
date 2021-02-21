103ff469a6d230939a7f6e11843bf209e306dd50
Aleksey Rostovskiy <aleksey.rostovskiy@jetbrains.com>
2021-02-22 01:42:47 +0300

    remove EmptyTextSerializer

--- educational-core/src/com/jetbrains/edu/learning/yaml/format/AnswerPlaceholderYamlUtil.kt
+++ educational-core/src/com/jetbrains/edu/learning/yaml/format/AnswerPlaceholderYamlUtil.kt
@@ -3,14 +3,8 @@ package com.jetbrains.edu.learning.yaml.format
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.annotation.JsonPropertyOrder
-import com.fasterxml.jackson.core.JsonGenerator
-import com.fasterxml.jackson.databind.JsonSerializer
-import com.fasterxml.jackson.databind.SerializerProvider
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize
 import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder
-import com.fasterxml.jackson.databind.annotation.JsonSerialize
-import com.fasterxml.jackson.dataformat.yaml.YAMLGenerator
-import com.intellij.openapi.util.text.StringUtil
 import com.jetbrains.edu.learning.courseFormat.AnswerPlaceholder
 import com.jetbrains.edu.learning.courseFormat.AnswerPlaceholderDependency
 import com.jetbrains.edu.learning.yaml.errorHandling.formatError
@@ -34,7 +28,6 @@ abstract class AnswerPlaceholderYamlMixin {
 
   @JsonProperty(PLACEHOLDER_TEXT)
   @JsonInclude(JsonInclude.Include.ALWAYS)
-  @JsonSerialize(using = EmptyTextSerializer::class)
   private fun getPlaceholderText(): String {
     throw NotImplementedInMixin()
   }
@@ -42,22 +35,6 @@ abstract class AnswerPlaceholderYamlMixin {
   @JsonProperty(DEPENDENCY)
   private var myPlaceholderDependency: AnswerPlaceholderDependency? = null
 }
-
-// TODO: This could be removed updating jackson-dataformats to 2.10. see https://github.com/FasterXML/jackson-dataformats-text/issues/50
-private class EmptyTextSerializer : JsonSerializer<String>() {
-  override fun serialize(placeholderText: String, generator: JsonGenerator, provider: SerializerProvider) {
-    if (StringUtil.isEmpty(placeholderText)) {
-      val yamlGenerator = generator as YAMLGenerator
-      yamlGenerator.disable(YAMLGenerator.Feature.MINIMIZE_QUOTES)
-      yamlGenerator.writeString("")
-      yamlGenerator.enable(YAMLGenerator.Feature.MINIMIZE_QUOTES)
-    }
-    else {
-      generator.writeString(placeholderText)
-    }
-  }
-}
-
 
 @JsonPOJOBuilder(withPrefix = "")
 open class AnswerPlaceholderBuilder(@JsonProperty(OFFSET) val offset: Int,
