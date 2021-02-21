45ab2862f8ecbb8493e709514f6da774049d0999
Aleksey Rostovskiy <aleksey.rostovskiy@jetbrains.com>
2021-02-21 21:45:35 +0300

    fix PyFakeSdkType to compile with 211 branch
    after 65821d3d95d03cd812735c5fa452b1f7a5ba4c6b commit in intellij repo

--- Edu-Python/src/com/jetbrains/edu/python/learning/newproject/PyFakeSdkType.kt
+++ Edu-Python/src/com/jetbrains/edu/python/learning/newproject/PyFakeSdkType.kt
@@ -8,8 +8,8 @@ import javax.swing.Icon
 
 object PyFakeSdkType : SdkType("") {
   override fun getPresentableName(): String = ""
-  override fun isValidSdkHome(path: String?): Boolean = false
-  override fun suggestSdkName(currentSdkName: String?, sdkHome: String?): String = ""
+  override fun isValidSdkHome(path: String): Boolean = false
+  override fun suggestSdkName(currentSdkName: String?, sdkHome: String): String = ""
   override fun suggestHomePath(): String? = null
   override fun createAdditionalDataConfigurable(sdkModel: SdkModel,
                                                 sdkModificator: SdkModificator): AdditionalDataConfigurable? = null
