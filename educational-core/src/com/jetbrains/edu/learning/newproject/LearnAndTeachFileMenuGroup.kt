32927e011a8761f92d548b62a3394f6c604eace6
Aleksey Rostovskiy <aleksey.rostovskiy@jetbrains.com>
2021-03-04 15:38:10 +0300

    drop LearnAndTeachAction

--- educational-core/src/com/jetbrains/edu/learning/newproject/LearnAndTeachFileMenuGroup.kt
+++ educational-core/src/com/jetbrains/edu/learning/newproject/LearnAndTeachFileMenuGroup.kt
@@ -3,11 +3,15 @@ package com.jetbrains.edu.learning.newproject
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.DefaultActionGroup
 import com.intellij.openapi.project.DumbAware
+import com.jetbrains.edu.coursecreator.actions.CCPluginToggleAction
 import icons.EducationalCoreIcons
 
 class LearnAndTeachFileMenuGroup: DefaultActionGroup(), DumbAware {
   override fun update(e: AnActionEvent) {
     e.presentation.icon = EducationalCoreIcons.CourseAction
-    LearnAndTeachAction.updatePresentationText(e.presentation)
+    e.presentation.text = "Learn"
+    if (CCPluginToggleAction.isCourseCreatorFeaturesEnabled) {
+      e.presentation.text += " and Teach"
+    }
   }
 }
