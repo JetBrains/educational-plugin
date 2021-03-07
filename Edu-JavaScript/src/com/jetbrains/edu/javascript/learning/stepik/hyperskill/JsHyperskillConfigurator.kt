e62c7e9bb598cf2bca70a34abb69d77231c4def4
Aleksey Rostovskiy <aleksey.rostovskiy@jetbrains.com>
2021-03-07 18:42:12 +0300

    cleanup JsHyperskillConfigurator: remove unnecessary anonymous class

--- Edu-JavaScript/src/com/jetbrains/edu/javascript/learning/stepik/hyperskill/JsHyperskillConfigurator.kt
+++ Edu-JavaScript/src/com/jetbrains/edu/javascript/learning/stepik/hyperskill/JsHyperskillConfigurator.kt
@@ -1,13 +1,14 @@
 package com.jetbrains.edu.javascript.learning.stepik.hyperskill
 
 import com.jetbrains.edu.javascript.learning.JsConfigurator
+import com.jetbrains.edu.javascript.learning.JsConfigurator.Companion.MAIN_JS
 import com.jetbrains.edu.javascript.learning.JsNewProjectSettings
 import com.jetbrains.edu.learning.EduNames
 import com.jetbrains.edu.learning.stepik.hyperskill.HyperskillConfigurator
 
-class JsHyperskillConfigurator : HyperskillConfigurator<JsNewProjectSettings>(object : JsConfigurator() {
-  override fun getMockFileName(text: String): String = MAIN_JS
-}) {
+class JsHyperskillConfigurator : HyperskillConfigurator<JsNewProjectSettings>(JsConfigurator()) {
   override val testDirs: List<String>
     get() = listOf(HYPERSKILL_TEST_DIR, EduNames.TEST)
+
+  override fun getMockFileName(text: String): String = MAIN_JS
 }
