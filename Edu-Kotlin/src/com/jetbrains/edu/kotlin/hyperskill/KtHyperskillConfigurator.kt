234e7febbc3562a6161de686422a5554a0dcacca
Aleksey Rostovskiy <aleksey.rostovskiy@jetbrains.com>
2021-03-07 18:40:54 +0300

    cleanup KtHyperskillConfigurator: remove unnecessary anonymous class

--- Edu-Kotlin/src/com/jetbrains/edu/kotlin/hyperskill/KtHyperskillConfigurator.kt
+++ Edu-Kotlin/src/com/jetbrains/edu/kotlin/hyperskill/KtHyperskillConfigurator.kt
@@ -3,15 +3,16 @@ package com.jetbrains.edu.kotlin.hyperskill
 import com.google.common.annotations.VisibleForTesting
 import com.jetbrains.edu.jvm.JdkProjectSettings
 import com.jetbrains.edu.kotlin.KtConfigurator
+import com.jetbrains.edu.kotlin.KtConfigurator.Companion.MAIN_KT
 import com.jetbrains.edu.kotlin.KtCourseBuilder
 import com.jetbrains.edu.learning.EduCourseBuilder
 import com.jetbrains.edu.learning.stepik.hyperskill.HyperskillConfigurator
 
-class KtHyperskillConfigurator : HyperskillConfigurator<JdkProjectSettings>(object : KtConfigurator() {
-  override fun getMockFileName(text: String): String = MAIN_KT
-}) {
+class KtHyperskillConfigurator : HyperskillConfigurator<JdkProjectSettings>(KtConfigurator()) {
   override val courseBuilder: EduCourseBuilder<JdkProjectSettings>
     get() = KtHyperskillCourseBuilder()
+
+  override fun getMockFileName(text: String): String = MAIN_KT
 
   private class KtHyperskillCourseBuilder : KtCourseBuilder() {
     override val buildGradleTemplateName: String = KOTLIN_HYPERSKILL_BUILD_GRADLE_TEMPLATE_NAME
