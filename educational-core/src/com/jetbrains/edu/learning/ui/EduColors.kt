f1fa5dce18a80e2a3a6bf4e4b931ccfcdf757255
Valentina Kiryushkina <valentina.kiryushkina@jetbrains.com>
2021-03-30 17:50:54 +0300

    rename color keys according ot guide

--- educational-core/src/com/jetbrains/edu/learning/ui/EduColors.kt
+++ educational-core/src/com/jetbrains/edu/learning/ui/EduColors.kt
@@ -7,7 +7,7 @@ import java.awt.Color
 object EduColors {
   val errorTextForeground: Color = JBColor(0xac0013, 0xef5f65)
   val warningTextForeground: Color = JBColor(0xa49152, 0xbbb529)
-  val correctLabelForeground: Color = JBColor.namedColor("Submissions.label.correctForeground", 0x368746, 0x499C54)
+  val correctLabelForeground: Color = JBColor.namedColor("Submissions.CorrectLabel.foreground", 0x368746, 0x499C54)
   val wrongLabelForeground: Color = UIUtil.getErrorForeground()
-  val hyperlinkColor: Color = JBColor.namedColor("Hyperlink.foreground", 0x6894C6, 0x5C84C9)
+  val hyperlinkColor: Color = JBColor.namedColor("Edu.Hyperlink.foreground", 0x6894C6, 0x5C84C9)
 }
