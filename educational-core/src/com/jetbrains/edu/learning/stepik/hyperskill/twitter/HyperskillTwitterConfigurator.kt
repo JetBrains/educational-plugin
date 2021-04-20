JetBrains/educational-plugin

commit d0eb5ff1549c354e5305947c197b2669dd6586b1

process educational-core

educational-core/src/com/jetbrains/edu/learning/stepik/hyperskill/twitter/HyperskillTwitterConfigurator.kt
@@ -1,6 +1,5 @@
 package com.jetbrains.edu.learning.stepik.hyperskill.twitter
 
-import com.intellij.openapi.application.ApplicationInfo
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.BuildNumber
 import com.jetbrains.edu.learning.course
@@ -17,7 +16,7 @@ import kotlin.random.Random
 class HyperskillTwitterConfigurator : TwitterPluginConfigurator {
 
   override fun askToTweet(project: Project, solvedTask: Task, statusBeforeCheck: CheckStatus): Boolean {
-    if (ApplicationInfo.getInstance().build < BUILD_202 && !isUnitTestMode) return false
+    if (!isUnitTestMode) return false
     val course = project.course as? HyperskillCourse ?: return false
     if (!course.isStudy) return false
     if (statusBeforeCheck == CheckStatus.Solved) return false
@@ -44,9 +43,6 @@ class HyperskillTwitterConfigurator : TwitterPluginConfigurator {
   }
 
   companion object {
-    // BACKCOMPAT: 2020.1
-    private val BUILD_202 = BuildNumber.fromString("202")!!
-
     private const val NUMBER_OF_IMAGES = 3
   }
 }

educational-core/branches/201/resources/META-INF/platform-educational-core.xml
@@ -1,2 +1,0 @@
-<idea-plugin>
-</idea-plugin>

educational-core/branches/201/src/com/jetbrains/edu/learning/actions/LeaveCommentActionUtils.kt
@@ -1,4 +1,0 @@
-package com.jetbrains.edu.learning.actions
-
-@Suppress("unused")
-fun LeaveCommentAction.addSynonym(@Suppress("UNUSED_PARAMETER") text: String) {}

educational-core/branches/201/src/com/jetbrains/edu/learning/checkio/checker/JCEFCheckiOMissionCheckUtils.kt
@@ -1,11 +1,0 @@
-package com.jetbrains.edu.learning.checkio.checker
-
-import com.intellij.openapi.project.Project
-import com.jetbrains.edu.learning.checkio.connectors.CheckiOOAuthConnector
-import com.jetbrains.edu.learning.courseFormat.tasks.Task
-
-fun getJCEFCheckiOMissionCheck(project: Project,
-                               task: Task,
-                               oAuthConnector: CheckiOOAuthConnector,
-                               interpreterName: String,
-                               testFormTargetUrl: String): CheckiOMissionCheck? = null

educational-core/branches/201/src/com/jetbrains/edu/learning/framework/impl/FrameworkRecordsTableBase.kt
@@ -1,8 +1,0 @@
-package com.jetbrains.edu.learning.framework.impl
-
-import com.intellij.util.io.PagePool
-import com.intellij.util.io.storage.AbstractRecordsTable
-import java.nio.file.Path
-
-@Suppress("UnstableApiUsage")
-abstract class FrameworkRecordsTableBase(storageFilePath: Path, pool: PagePool) : AbstractRecordsTable(storageFilePath.toFile(), pool)

educational-core/branches/201/src/com/jetbrains/edu/learning/framework/impl/FrameworkStorageBase.kt
@@ -1,24 +1,0 @@
-package com.jetbrains.edu.learning.framework.impl
-
-import com.intellij.openapi.util.ThrowableComputable
-import com.intellij.util.io.PagePool
-import com.intellij.util.io.storage.AbstractRecordsTable
-import com.intellij.util.io.storage.AbstractStorage
-import java.io.File
-import java.nio.file.Path
-
-@Suppress("UnstableApiUsage", "DEPRECATION")
-abstract class FrameworkStorageBase(storagePath: Path) : AbstractStorage(storagePath.toString()) {
-
-  override fun createRecordsTable(pool: PagePool, recordsFile: File): AbstractRecordsTable {
-    return FrameworkRecordsTable(recordsFile.toPath(), pool)
-  }
-
-  protected fun <T, E : Throwable> withWriteLock(runnable: ThrowableComputable<T, E>): T {
-    return synchronized(myLock) { runnable.compute() }
-  }
-
-  protected fun <T, E : Throwable> withReadLock(runnable: ThrowableComputable<T, E>): T {
-    return synchronized(myLock) { runnable.compute() }
-  }
-}

educational-core/branches/201/src/com/jetbrains/edu/learning/newproject/openProjectUtils.kt
@@ -1,21 +1,0 @@
-@file:JvmName("OpenProjectUtils")
-
-package com.jetbrains.edu.learning.newproject
-
-import com.intellij.ide.impl.OpenProjectTask
-import com.intellij.openapi.project.Project
-import com.intellij.platform.PlatformProjectOpenProcessor
-import com.intellij.projectImport.ProjectOpenedCallback
-import java.nio.file.Path
-
-fun openNewProject(location: Path, callback: ProjectOpenedCallback): Project? {
-  val task = OpenProjectTask(
-    forceOpenInNewFrame = true,
-    projectToClose = null,
-    isNewProject = true,
-    callback = callback
-  )
-
-  @Suppress("UnstableApiUsage")
-  return PlatformProjectOpenProcessor.doOpenProject(location, task)
-}

educational-core/branches/201/src/com/jetbrains/edu/learning/newproject/ui/learnAndTeachActionUtils.kt
@@ -1,3 +1,0 @@
-package com.jetbrains.edu.learning.newproject.ui
-
-fun isLearnAndTeachVisible() = true

commit 3c74306deaf6aac9db23775447369644ec74e2fd

EDU-4273: fix tweeting suggestions on JBA course finish
Regression was introduced in d0eb5ff1549c354e5305947c197b2669dd6586b1

educational-core/src/com/jetbrains/edu/learning/stepik/hyperskill/twitter/HyperskillTwitterConfigurator.kt
@@ -1,11 +1,9 @@
 package com.jetbrains.edu.learning.stepik.hyperskill.twitter
 
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.util.BuildNumber
 import com.jetbrains.edu.learning.course
 import com.jetbrains.edu.learning.courseFormat.CheckStatus
 import com.jetbrains.edu.learning.courseFormat.tasks.Task
-import com.jetbrains.edu.learning.isUnitTestMode
 import com.jetbrains.edu.learning.messages.EduCoreBundle
 import com.jetbrains.edu.learning.stepik.hyperskill.courseFormat.HyperskillCourse
 import com.jetbrains.edu.learning.twitter.TwitterPluginConfigurator
@@ -16,7 +14,6 @@ import kotlin.random.Random
 class HyperskillTwitterConfigurator : TwitterPluginConfigurator {
 
   override fun askToTweet(project: Project, solvedTask: Task, statusBeforeCheck: CheckStatus): Boolean {
-    if (!isUnitTestMode) return false
     val course = project.course as? HyperskillCourse ?: return false
     if (!course.isStudy) return false
     if (statusBeforeCheck == CheckStatus.Solved) return false