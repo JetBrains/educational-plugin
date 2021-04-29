f85da8e80908b96f4fa417833da774f35a72f685
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2021-04-29 18:26:30 +0300

    EDU-4296: use new statistics API for `EduStateUsagesCollector`

--- educational-core/src/com/jetbrains/edu/learning/statistics/EduStateUsagesCollector.kt
+++ educational-core/src/com/jetbrains/edu/learning/statistics/EduStateUsagesCollector.kt
@@ -1,35 +1,50 @@
 package com.jetbrains.edu.learning.statistics
 
 import com.intellij.internal.statistic.beans.MetricEvent
-import com.intellij.internal.statistic.beans.newMetric
+import com.intellij.internal.statistic.eventLog.EventLogGroup
+import com.intellij.internal.statistic.eventLog.events.EventField
+import com.intellij.internal.statistic.eventLog.events.EventFields
 import com.intellij.internal.statistic.service.fus.collectors.ApplicationUsagesCollector
 import com.jetbrains.edu.coursecreator.actions.CCPluginToggleAction
 import com.jetbrains.edu.learning.EduSettings
+import com.jetbrains.edu.learning.JavaUILibrary
+import java.util.*
+import kotlin.collections.HashSet
 
 /**
  * IMPORTANT: if you modify anything in this class, updated whitelist rules should be
  * provided to analytics platform team.
- * See `docs/statisticsRules.md` for more information
  */
+@Suppress("UnstableApiUsage")
 class EduStateUsagesCollector : ApplicationUsagesCollector() {
 
   private enum class EduRole {
     STUDENT, EDUCATOR
   }
 
-  override fun getGroupId() = "educational.state"
+  override fun getGroup(): EventLogGroup = GROUP
 
-  override fun getMetrics(): MutableSet<MetricEvent> {
+  override fun getMetrics(): Set<MetricEvent> {
     val metrics = HashSet<MetricEvent>()
 
     val taskPanel = EduSettings.getInstance().javaUiLibrary
-    metrics.add(newMetric("task.panel", taskPanel))
+    metrics += TASK_PANEL_EVENT.metric(taskPanel)
 
     val role = if (CCPluginToggleAction.isCourseCreatorFeaturesEnabled) EduRole.EDUCATOR else EduRole.STUDENT
-    metrics.add(newMetric("role", role))
+    metrics += ROLE_EVENT.metric(role)
 
     return metrics
   }
 
-  override fun getVersion() = 2
+  companion object {
+    private val GROUP = EventLogGroup("educational.state", 2)
+
+    private val TASK_PANEL_EVENT = GROUP.registerEvent("task.panel", enumField<JavaUILibrary>())
+    private val ROLE_EVENT = GROUP.registerEvent("role", enumField<EduRole>())
+
+    private inline fun <reified T : Enum<*>> enumField() : EventField<T> {
+      // field name and transform function are written in this way to be compatible with previous version
+      return EventFields.Enum("value") { it.name.toLowerCase(Locale.ENGLISH) }
+    }
+  }
 }
