230da057aefe8080e529a69851d73262b91703ed
Valentina Kiryushkina <valentina.kiryushkina@jetbrains.com>
2021-05-17 19:48:29 +0200

    EDU-4321 Remove the second link from coursera tab description

--- educational-core/src/com/jetbrains/edu/learning/coursera/newProjectUI/CourseraCoursesPanel.kt
+++ educational-core/src/com/jetbrains/edu/learning/coursera/newProjectUI/CourseraCoursesPanel.kt
@@ -1,12 +1,10 @@
 package com.jetbrains.edu.learning.coursera.newProjectUI
 
-import com.jetbrains.edu.learning.codeforces.CodeforcesNames
 import com.jetbrains.edu.learning.coursera.CourseraNames.COURSERA
 import com.jetbrains.edu.learning.coursera.CourseraNames.COURSERA_HELP
 import com.jetbrains.edu.learning.coursera.CourseraPlatformProvider
 import com.jetbrains.edu.learning.messages.EduCoreBundle
 import com.jetbrains.edu.learning.newproject.ui.CoursesPanel
-import com.jetbrains.edu.learning.newproject.ui.LinkInfo
 import com.jetbrains.edu.learning.newproject.ui.TabInfo
 import kotlinx.coroutines.CoroutineScope
 
@@ -14,7 +12,7 @@ class CourseraCoursesPanel(platformProvider: CourseraPlatformProvider, scope: Co
 
   override fun tabInfo(): TabInfo {
     val linkText = """<a href="$COURSERA_HELP">$COURSERA</a>"""
-    val infoText = EduCoreBundle.message("coursera.courses.explanation", linkText)
+    val infoText = EduCoreBundle.message("coursera.courses.explanation", linkText, COURSERA)
     return TabInfo(infoText)
   }
 }
