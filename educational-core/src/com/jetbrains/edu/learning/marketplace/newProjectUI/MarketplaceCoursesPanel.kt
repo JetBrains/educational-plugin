db586adf19330c5114b9dea3c81c747f23e19a1f
Valentina Kiryushkina <valentina.kiryushkina@jetbrains.com>
2021-05-31 15:36:10 +0200

    EDU-4336 Remove duplicated add course link

--- educational-core/src/com/jetbrains/edu/learning/marketplace/newProjectUI/MarketplaceCoursesPanel.kt
+++ educational-core/src/com/jetbrains/edu/learning/marketplace/newProjectUI/MarketplaceCoursesPanel.kt
@@ -1,6 +1,5 @@
 package com.jetbrains.edu.learning.marketplace.newProjectUI
 
-import com.jetbrains.edu.coursecreator.actions.CCNewCourseAction
 import com.jetbrains.edu.learning.courseFormat.Course
 import com.jetbrains.edu.learning.marketplace.MARKETPLACE
 import com.jetbrains.edu.learning.marketplace.MARKETPLACE_COURSES_HELP
@@ -12,10 +11,6 @@ class MarketplaceCoursesPanel(
   coursesPlatformProvider: CoursesPlatformProvider,
   scope: CoroutineScope
 ) : CoursesPanel(coursesPlatformProvider, scope) {
-
-  override fun toolbarAction(): ToolbarActionWrapper {
-    return ToolbarActionWrapper(EduCoreBundle.lazyMessage("course.dialog.create.course"), CCNewCourseAction())
-  }
 
   override fun tabInfo(): TabInfo {
     val linkText = """<a href="$MARKETPLACE_COURSES_HELP">$MARKETPLACE</a>"""
