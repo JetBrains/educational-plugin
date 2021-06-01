6e6f266ad2a7ee4609cf4647379dce23d896e57b
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2021-06-01 16:35:12 +0300

    Make `Enable Course Creator Features` action available during indexing

--- educational-core/src/com/jetbrains/edu/coursecreator/actions/CCPluginToggleAction.kt
+++ educational-core/src/com/jetbrains/edu/coursecreator/actions/CCPluginToggleAction.kt
@@ -2,12 +2,12 @@ package com.jetbrains.edu.coursecreator.actions
 
 import com.intellij.ide.util.PropertiesComponent
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.actionSystem.ToggleAction
+import com.intellij.openapi.project.DumbAwareToggleAction
 import com.jetbrains.edu.learning.isUnitTestMode
 import com.jetbrains.edu.learning.messages.EduCoreBundle
 import org.jetbrains.annotations.NonNls
 
-class CCPluginToggleAction : ToggleAction(EduCoreBundle.lazyMessage("action.toggle.cc.features.text")) {
+class CCPluginToggleAction : DumbAwareToggleAction(EduCoreBundle.lazyMessage("action.toggle.cc.features.text")) {
   override fun isSelected(e: AnActionEvent): Boolean = isCourseCreatorFeaturesEnabled
 
   override fun setSelected(e: AnActionEvent, state: Boolean) {
