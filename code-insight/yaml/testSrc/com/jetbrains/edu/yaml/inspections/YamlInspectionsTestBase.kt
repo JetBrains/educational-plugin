8c792dbbf1ee7bf2a676c0c3e8e21642e36f3995
Ekaterina Smal <ekaterina.smal@jetbrains.com>
2021-06-08 13:20:37 +0300

    fixed inspection test

--- code-insight/yaml/testSrc/com/jetbrains/edu/yaml/inspections/YamlInspectionsTestBase.kt
+++ code-insight/yaml/testSrc/com/jetbrains/edu/yaml/inspections/YamlInspectionsTestBase.kt
@@ -1,7 +1,9 @@
 package com.jetbrains.edu.yaml.inspections
 
 import com.intellij.codeInspection.LocalInspectionTool
+import com.jetbrains.edu.learning.courseDir
 import com.jetbrains.edu.learning.courseFormat.StudyItem
+import com.jetbrains.edu.learning.yaml.configFileName
 import com.jetbrains.edu.yaml.YamlCodeInsightTest
 import kotlin.reflect.KClass
 
@@ -16,6 +18,8 @@ abstract class YamlInspectionsTestBase(private val inspectionClass: KClass<out L
     testHighlighting(item, configTextBefore)
     val action = myFixture.findSingleIntention(fixName)
     myFixture.launchAction(action)
+    val configFile = item.getDir(project.courseDir)!!.findChild(item.configFileName)!!
+    myFixture.openFileInEditor(configFile)
     myFixture.checkResult(configTextAfter)
   }
 
