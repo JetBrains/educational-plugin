58fb09c1807fc7bdd913f084679e91b7e233f65c
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2021-06-17 16:52:55 +0300

    EDU-4107: use more flexible API in `JCheckErrorsTest`

--- Edu-Java/testSrc/com/jetbrains/edu/java/slow/checker/JCheckErrorsTest.kt
+++ Edu-Java/testSrc/com/jetbrains/edu/java/slow/checker/JCheckErrorsTest.kt
@@ -9,12 +9,17 @@ import com.intellij.openapi.roots.ProjectRootManager
 import com.intellij.util.ui.UIUtil
 import com.jetbrains.edu.jvm.slow.checker.JdkCheckerTestBase
 import com.jetbrains.edu.learning.checker.CheckActionListener
+import com.jetbrains.edu.learning.checker.CheckResultDiff
+import com.jetbrains.edu.learning.checker.CheckResultDiffMatcher.Companion.diff
 import com.jetbrains.edu.learning.checker.CheckUtils
 import com.jetbrains.edu.learning.course
+import com.jetbrains.edu.learning.courseFormat.CheckStatus
 import com.jetbrains.edu.learning.courseFormat.Course
 import com.jetbrains.edu.learning.messages.EduCoreBundle
+import com.jetbrains.edu.learning.nullValue
 import com.jetbrains.edu.learning.xmlEscaped
 import org.hamcrest.CoreMatchers.containsString
+import org.hamcrest.CoreMatchers.equalTo
 import org.junit.Assert.assertThat
 
 class JCheckErrorsTest : JdkCheckerTestBase() {
@@ -86,15 +91,20 @@ class JCheckErrorsTest : JdkCheckerTestBase() {
   }
 
   fun `test errors`() {
-    CheckActionListener.shouldFail()
-    CheckActionListener.expectedMessage { task ->
-      when (task.name) {
-        "javaCompilationError" -> CheckUtils.COMPILATION_FAILED_MESSAGE
-        "testFail" -> "Task.foo() should return 42"
-        "comparisonTestFail" -> EduCoreBundle.message("check.incorrect")
-        "escapeMessageInFailedTest" -> "<br>".xmlEscaped
-        else -> error("Unexpected task name: ${task.name}")
+    CheckActionListener.setCheckResultVerifier { task, checkResult ->
+      assertEquals("${task.name} should be failed", CheckStatus.Failed, checkResult.status)
+      val (messageMatcher, diffMatcher) = when (task.name) {
+        "javaCompilationError" -> equalTo(CheckUtils.COMPILATION_FAILED_MESSAGE) to nullValue()
+        "testFail" -> equalTo("Task.foo() should return 42") to nullValue()
+        "comparisonTestFail" ->
+          equalTo(EduCoreBundle.message("check.incorrect")) to
+            diff(CheckResultDiff(expected = "42", actual = "0", title = "Comparison Failure (test)"))
+        "escapeMessageInFailedTest" ->
+          equalTo("<br>".xmlEscaped) to nullValue()
+        else -> error("Unexpected task `${task.name}`")
       }
+      assertThat("Checker message for ${task.name} doesn't match", checkResult.message, messageMatcher)
+      assertThat("Checker diff for ${task.name} doesn't match", checkResult.diff, diffMatcher)
     }
     doTest()
   }
