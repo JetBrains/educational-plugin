63f4daf2359ee896daa6641a320fa84bcd9ed871
Valentina Kiryushkina <valentina.kiryushkina@jetbrains.com>
2021-06-21 13:38:01 +0200

    EDU-4394 Add description to Codeforces contest

--- educational-core/src/com/jetbrains/edu/learning/codeforces/ContestInformation.kt
+++ educational-core/src/com/jetbrains/edu/learning/codeforces/ContestInformation.kt
@@ -2,8 +2,11 @@ package com.jetbrains.edu.learning.codeforces
 
 import com.jetbrains.edu.learning.codeforces.courseFormat.CodeforcesCourse
 import com.jetbrains.edu.learning.courseFormat.Tag
+import com.jetbrains.edu.learning.messages.EduCoreBundle
 import java.time.Duration
 import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 class ContestInformation(
   id: Int,
@@ -18,6 +21,11 @@ class ContestInformation(
     this.id = id
     this.name = name
     this.endDateTime = endDateTime
+
+    val codeforcesLink = "<a href='${CodeforcesContestConnector.getContestURLFromID(id)}'>${CodeforcesNames.CODEFORCES_TITLE}</a>"
+    val eduToolsLink = "<a href='${CodeforcesNames.CODEFORCES_EDU_TOOLS_HELP}'>EduTools</a>"
+    val date = endDateTime.toLocalDate().format(DateTimeFormatter.ofPattern("dd MMM YYYY", Locale.ENGLISH))
+    this.description = EduCoreBundle.message("codeforces.past.course.description", date, codeforcesLink, eduToolsLink)
   }
 
   override fun getTags(): List<Tag> {
