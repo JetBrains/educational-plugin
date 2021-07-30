d3a107147d15c6316e3fd1784ce99355d1ead926
Naiden <sviatoslav.naiden@jetbrains.com>
2021-07-30 12:17:00 +0200

    EDU-2923: fix existing test directories check and create testData dir if missing

--- educational-core/src/com/jetbrains/edu/learning/codeforces/actions/CodeforcesCreateTestAction.kt
+++ educational-core/src/com/jetbrains/edu/learning/codeforces/actions/CodeforcesCreateTestAction.kt
@@ -4,10 +4,12 @@ import com.intellij.ide.ui.newItemPopup.NewItemPopupUtil
 import com.intellij.ide.ui.newItemPopup.NewItemSimplePopupPanel
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.application.runWriteAction
 import com.intellij.openapi.fileEditor.ex.FileEditorManagerEx
 import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.InputValidatorEx
+import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.openapi.vfs.VirtualFile
 import com.jetbrains.edu.coursecreator.CCStudyItemPathInputValidator
 import com.jetbrains.edu.learning.EduUtils
@@ -27,10 +29,11 @@ class CodeforcesCreateTestAction : DumbAwareAction(EduCoreBundle.lazyMessage("co
     val project = e.project ?: return
     if (project.isDisposed) return
 
-    val currentTask = CommonDataKeys.VIRTUAL_FILE.getData(e.dataContext)?.getContainingTask(project) as? CodeforcesTask ?: return
-    val taskDir = currentTask.getDir(project.courseDir) ?: return
-    val defaultTestName = getDefaultTestName(currentTask, project)
-    val validator = CCStudyItemPathInputValidator(project, currentTask.course, currentTask.studyItemType, taskDir)
+    val selectedTask = CommonDataKeys.VIRTUAL_FILE.getData(e.dataContext)?.getContainingTask(project) as? CodeforcesTask ?: return
+    val taskDir = selectedTask.getDir(project.courseDir) ?: return
+    val defaultTestName = getDefaultTestName(selectedTask, project)
+    val testDataDir = runWriteAction { VfsUtil.createDirectoryIfMissing(taskDir, CodeforcesNames.TEST_DATA_FOLDER) }
+    val validator = CCStudyItemPathInputValidator(project, selectedTask.course, selectedTask.studyItemType, testDataDir)
     val contentPanel = NewItemSimplePopupPanel()
 
     val nameField = contentPanel.textField
@@ -41,10 +44,10 @@ class CodeforcesCreateTestAction : DumbAwareAction(EduCoreBundle.lazyMessage("co
       val testName = nameField.text
       if (validator.checkInput(testName)) {
         popup.closeOk(it)
-        val testDir = GeneratorUtils.joinPaths(CodeforcesNames.TEST_DATA_FOLDER, testName)
-        val inputFile = GeneratorUtils.createChildFile(project, taskDir, GeneratorUtils.joinPaths(testDir, currentTask.inputFileName), "")
-                        ?: return@setApplyAction
-        val outputFile = GeneratorUtils.createChildFile(project, taskDir, GeneratorUtils.joinPaths(testDir, currentTask.outputFileName), "")
+        val inputFile = GeneratorUtils.createChildFile(project, testDataDir, GeneratorUtils.joinPaths(testName, selectedTask.inputFileName),
+                                                       "") ?: return@setApplyAction
+        val outputFile = GeneratorUtils.createChildFile(project, testDataDir,
+                                                        GeneratorUtils.joinPaths(testName, selectedTask.outputFileName), "")
                          ?: return@setApplyAction
         openInSplitEditors(project, outputFile, inputFile)
       }
