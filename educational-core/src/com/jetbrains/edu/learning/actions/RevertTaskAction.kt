66a0174af4acf0b800030cf7b9f108361dbd2bb8
Aleksey Rostovskiy <aleksey.rostovskiy@jetbrains.com>
2021-09-03 04:02:46 +0300

    EDU-4567 Convert RevertTaskAction to Kotlin

--- educational-core/src/com/jetbrains/edu/learning/actions/RevertTaskAction.kt
+++ educational-core/src/com/jetbrains/edu/learning/actions/RevertTaskAction.kt
@@ -1,94 +1,72 @@
-package com.jetbrains.edu.learning.actions;
+package com.jetbrains.edu.learning.actions
 
-import com.intellij.ide.projectView.ProjectView;
-import com.intellij.notification.Notification;
-import com.intellij.notification.NotificationType;
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.Presentation;
-import com.intellij.openapi.actionSystem.RightAlignedToolbarAction;
-import com.intellij.openapi.project.DumbAwareAction;
-import com.intellij.openapi.project.Project;
-import com.intellij.ui.EditorNotifications;
-import com.intellij.util.ui.EmptyIcon;
-import com.jetbrains.edu.EducationalCoreIcons;
-import com.jetbrains.edu.learning.EduUtils;
-import com.jetbrains.edu.learning.StudyTaskManager;
-import com.jetbrains.edu.learning.courseFormat.Course;
-import com.jetbrains.edu.learning.courseFormat.tasks.Task;
-import com.jetbrains.edu.learning.messages.EduCoreBundle;
-import com.jetbrains.edu.learning.placeholderDependencies.PlaceholderDependencyManager;
-import com.jetbrains.edu.learning.projectView.ProgressUtil;
-import com.jetbrains.edu.learning.statistics.EduCounterUsageCollector;
-import com.jetbrains.edu.learning.taskDescription.ui.TaskDescriptionView;
-import com.jetbrains.edu.learning.yaml.YamlFormatSynchronizer;
-import org.jetbrains.annotations.NonNls;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.ide.projectView.ProjectView
+import com.intellij.notification.Notification
+import com.intellij.notification.NotificationType
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.RightAlignedToolbarAction
+import com.intellij.openapi.project.DumbAwareAction
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.MessageDialogBuilder
+import com.intellij.ui.EditorNotifications
+import com.intellij.util.ui.EmptyIcon
+import com.jetbrains.edu.EducationalCoreIcons
+import com.jetbrains.edu.learning.EduUtils
+import com.jetbrains.edu.learning.courseFormat.ext.revertTaskFiles
+import com.jetbrains.edu.learning.courseFormat.ext.revertTaskParameters
+import com.jetbrains.edu.learning.messages.EduCoreBundle
+import com.jetbrains.edu.learning.messages.EduCoreBundle.lazyMessage
+import com.jetbrains.edu.learning.placeholderDependencies.PlaceholderDependencyManager.updateDependentPlaceholders
+import com.jetbrains.edu.learning.projectView.ProgressUtil.updateCourseProgress
+import com.jetbrains.edu.learning.statistics.EduCounterUsageCollector.Companion.revertTask
+import com.jetbrains.edu.learning.taskDescription.ui.TaskDescriptionView
+import com.jetbrains.edu.learning.yaml.YamlFormatSynchronizer
+import org.jetbrains.annotations.NonNls
+import org.jetbrains.annotations.VisibleForTesting
 
-import static com.intellij.openapi.ui.Messages.*;
-import static com.jetbrains.edu.learning.courseFormat.ext.TaskExt.revertTaskFiles;
-import static com.jetbrains.edu.learning.courseFormat.ext.TaskExt.revertTaskParameters;
-
-
-public class RevertTaskAction extends DumbAwareAction implements RightAlignedToolbarAction {
-  @NonNls
-  public static final String ACTION_ID = "Educational.RefreshTask";
-
-  public RevertTaskAction() {
-    super(EduCoreBundle.lazyMessage("action.reset.request"),
-          EduCoreBundle.lazyMessage("action.reset.to.initial.state"),
-          EducationalCoreIcons.ResetTask);
+class RevertTaskAction : DumbAwareAction(lazyMessage("action.reset.request"),
+                                         lazyMessage("action.reset.to.initial.state"),
+                                         EducationalCoreIcons.ResetTask), RightAlignedToolbarAction {
+  override fun actionPerformed(e: AnActionEvent) {
+    val project = e.project ?: return
+    val result = MessageDialogBuilder.yesNo(EduCoreBundle.message("action.reset.request"),
+                                            EduCoreBundle.message("action.reset.progress.dropped")).ask(project)
+    if (!result) return
+    revert(project)
+    revertTask()
   }
 
-  public static void revert(@NotNull final Project project) {
-    final Task task = EduUtils.getCurrentTask(project);
-    if (task == null) return;
+  override fun update(e: AnActionEvent) {
+    EduUtils.updateAction(e)
+    val project = e.project ?: return
+    val task = EduUtils.getCurrentTask(project) ?: return
 
-    revertTaskFiles(task, project);
-    revertTaskParameters(task, project);
-    YamlFormatSynchronizer.saveItem(task);
-
-    PlaceholderDependencyManager.updateDependentPlaceholders(project, task);
-    EditorNotifications.getInstance(project).updateAllNotifications();
-    Notification notification = new Notification("EduTools", EmptyIcon.ICON_16, "", "",
-                                                 EduCoreBundle.message("action.reset.result"), NotificationType.INFORMATION, null);
-    notification.notify(project);
-    ProjectView.getInstance(project).refresh();
-    TaskDescriptionView.getInstance(project).updateTaskSpecificPanel();
-    TaskDescriptionView.getInstance(project).readyToCheck();
-    ProgressUtil.updateCourseProgress(project);
+    if (!task.course.isStudy) {
+      val presentation = e.presentation
+      presentation.isEnabledAndVisible = false
+    }
   }
 
-  public void actionPerformed(@NotNull AnActionEvent event) {
-    final Project project = event.getProject();
-    if (project == null) return;
+  companion object {
+    const val ACTION_ID: @NonNls String = "Educational.RefreshTask"
 
-    int result = showOkCancelDialog(project, EduCoreBundle.message("action.reset.progress.dropped"),
-                                    EduCoreBundle.message("action.reset.request"), getOkButton(), getCancelButton(), getQuestionIcon());
-    if (result != OK) return;
-    revert(project);
-    EduCounterUsageCollector.revertTask();
-  }
+    @VisibleForTesting
+    fun revert(project: Project) {
+      val task = EduUtils.getCurrentTask(project) ?: return
+      task.apply {
+        revertTaskFiles(project)
+        revertTaskParameters(project)
+        YamlFormatSynchronizer.saveItem(this)
+      }
 
-  @Override
-  public void update(@NotNull AnActionEvent event) {
-    EduUtils.updateAction(event);
-    final Project project = event.getProject();
-    if (project == null) {
-      return;
-    }
-    Course course = StudyTaskManager.getInstance(project).getCourse();
-    if (course == null) {
-      return;
-    }
-
-    Presentation presentation = event.getPresentation();
-
-    Task task = EduUtils.getCurrentTask(project);
-    if (task == null) {
-      return;
-    }
-    if (!course.isStudy()) {
-      presentation.setEnabledAndVisible(false);
+      updateDependentPlaceholders(project, task)
+      EditorNotifications.getInstance(project).updateAllNotifications()
+      Notification("EduTools", EmptyIcon.ICON_16, "", "", EduCoreBundle.message("action.reset.result"),
+                   NotificationType.INFORMATION, null).notify(project)
+      ProjectView.getInstance(project).refresh()
+      TaskDescriptionView.getInstance(project).updateTaskSpecificPanel()
+      TaskDescriptionView.getInstance(project).readyToCheck()
+      updateCourseProgress(project)
     }
   }
 }
