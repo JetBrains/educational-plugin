3be61eaee85e55c51115ec7e13f87bc85e890374
Sviatoslav Naiden <sviatoslav.naiden@jetbrains.com>
2021-10-04 13:32:44 +0200

    EDU-4619: update Codeforces kotlin version name

--- Edu-Kotlin/src/com/jetbrains/edu/kotlin/codeforces/KtCodeforcesLanguageProvider.kt
+++ Edu-Kotlin/src/com/jetbrains/edu/kotlin/codeforces/KtCodeforcesLanguageProvider.kt
@@ -9,7 +9,7 @@ import com.jetbrains.edu.learning.configuration.EduConfigurator
 import javax.swing.Icon
 
 class KtCodeforcesLanguageProvider : CodeforcesLanguageProvider {
-  override val codeforcesLanguageNamings: List<String> = listOf("Kotlin")
+  override val codeforcesLanguageNamings: List<String> = listOf("Kotlin 1.4")
   override val configurator: EduConfigurator<JdkProjectSettings> = KtConfigurator()
   override val languageId: String = EduNames.KOTLIN
   override val templateFileName: String = "codeforces.Kotlin Main.kt"
