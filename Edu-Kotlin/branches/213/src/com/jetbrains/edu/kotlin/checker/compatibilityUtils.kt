65fb81dafe1f6aa0f579faad5a6d1b03b5f867dd
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2021-10-14 23:16:05 +0300

    EDU-4650: fix exception during class loading
    Direct usage of `JunitKotlinTestFrameworkProvider` class leads to exception during class loading because it's located in `kotlin.junit` module (introduced in 2021.3) and `Edu-Kotlin` module doesn't declare dependency on it.
    So no we use `KotlinTestFrameworkProvider` extension point to iterate all possible implementations including `JunitKotlinTestFrameworkProvider`

--- Edu-Kotlin/branches/213/src/com/jetbrains/edu/kotlin/checker/compatibilityUtils.kt
+++ Edu-Kotlin/branches/213/src/com/jetbrains/edu/kotlin/checker/compatibilityUtils.kt
@@ -2,9 +2,12 @@ package com.jetbrains.edu.kotlin.checker
 
 import com.intellij.psi.PsiClass
 import com.intellij.psi.PsiElement
-import org.jetbrains.kotlin.idea.junit.JunitKotlinTestFrameworkProvider
+import org.jetbrains.kotlin.idea.extensions.KotlinTestFrameworkProvider
 
 // BACKCOMPAT: 2021.2. Inline it
 fun getTestClass(element: PsiElement): PsiClass? {
-  return JunitKotlinTestFrameworkProvider.getJavaTestEntity(element, checkMethod = false)?.testClass
+  for (extension in KotlinTestFrameworkProvider.EP_NAME.extensionList) {
+    return extension.getJavaTestEntity(element, checkMethod = false)?.testClass ?: continue
+  }
+  return null
 }
