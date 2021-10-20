a2142236405423197c9d68b2a1c251fcb7bb72ac
Aleksey Rostovskiy <aleksey.rostovskiy@jetbrains.com>
2021-10-20 19:05:07 +0300

    EDU-4503 Add experimental feature

--- Edu-Go/testSrc/com/jetbrains/edu/go/hyperskill/GoHyperskillNewTaskStructureTest.kt
+++ Edu-Go/testSrc/com/jetbrains/edu/go/hyperskill/GoHyperskillNewTaskStructureTest.kt
@@ -3,17 +3,26 @@ package com.jetbrains.edu.go.hyperskill
 import com.goide.GoLanguage
 import com.goide.sdk.GoSdk
 import com.intellij.lang.Language
+import com.intellij.util.ThrowableRunnable
 import com.jetbrains.edu.coursecreator.actions.create.CCNewTaskStructureTestBase
 import com.jetbrains.edu.go.GoConfigurator.Companion.GO_MOD
 import com.jetbrains.edu.go.GoConfigurator.Companion.MAIN_GO
 import com.jetbrains.edu.go.GoProjectSettings
+import com.jetbrains.edu.learning.EduExperimentalFeatures.HYPERSKILL_GO_SUPPORT
 import com.jetbrains.edu.learning.courseFormat.Course
 import com.jetbrains.edu.learning.stepik.hyperskill.courseFormat.HyperskillCourse
+import com.jetbrains.edu.learning.withFeature
 
 class GoHyperskillNewTaskStructureTest : CCNewTaskStructureTestBase() {
   override val language: Language get() = GoLanguage.INSTANCE
   override val settings: Any get() = GoProjectSettings(GoSdk.NULL)
   override val courseProducer: () -> Course = ::HyperskillCourse
+
+  override fun runTestRunnable(context: ThrowableRunnable<Throwable>) {
+    withFeature(HYPERSKILL_GO_SUPPORT, true) {
+      super.runTestRunnable(context)
+    }
+  }
 
 //  fun `test create edu task`() = checkEduTaskCreation(
 //    fullTaskStructure = {
--- Edu-Go/testSrc/com/jetbrains/edu/go/hyperskill/GoHyperskillTaskDescriptionHighlightingTest.kt
+++ Edu-Go/testSrc/com/jetbrains/edu/go/hyperskill/GoHyperskillTaskDescriptionHighlightingTest.kt
@@ -3,8 +3,11 @@ package com.jetbrains.edu.go.hyperskill
 import com.goide.GoLanguage
 import com.goide.sdk.GoSdk
 import com.intellij.lang.Language
+import com.intellij.util.ThrowableRunnable
 import com.jetbrains.edu.go.GoProjectSettings
+import com.jetbrains.edu.learning.EduExperimentalFeatures.HYPERSKILL_GO_SUPPORT
 import com.jetbrains.edu.learning.stepik.hyperskill.HyperskillTaskDescriptionHighlightingTest
+import com.jetbrains.edu.learning.withFeature
 
 class GoHyperskillTaskDescriptionHighlightingTest : HyperskillTaskDescriptionHighlightingTest() {
   override val language: Language
@@ -18,4 +21,11 @@ class GoHyperskillTaskDescriptionHighlightingTest : HyperskillTaskDescriptionHig
 
   override val codeSampleWithHighlighting: String
     get() = """<span style="...">fun main</span><span style="...">() {}</span>"""
+
+  override fun runTestRunnable(context: ThrowableRunnable<Throwable>) {
+    withFeature(HYPERSKILL_GO_SUPPORT, true) {
+      super.runTestRunnable(context)
+    }
+  }
+
 }
