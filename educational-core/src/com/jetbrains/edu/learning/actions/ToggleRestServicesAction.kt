7e18b3c52027f0666b905a102e6281100adeb933
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2021-10-21 17:43:02 +0300

    Make `ToggleRestServicesAction` action dumb aware

--- educational-core/src/com/jetbrains/edu/learning/actions/ToggleRestServicesAction.kt
+++ educational-core/src/com/jetbrains/edu/learning/actions/ToggleRestServicesAction.kt
@@ -1,11 +1,11 @@
 package com.jetbrains.edu.learning.actions
 
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.actionSystem.ToggleAction
+import com.intellij.openapi.project.DumbAwareToggleAction
 import com.jetbrains.edu.learning.authUtils.OAuthRestService
 import com.jetbrains.edu.learning.messages.EduCoreBundle
 
-class ToggleRestServicesAction : ToggleAction(EduCoreBundle.lazyMessage("action.toggle.rest.services.title")) {
+class ToggleRestServicesAction : DumbAwareToggleAction(EduCoreBundle.lazyMessage("action.toggle.rest.services.title")) {
 
   override fun isSelected(e: AnActionEvent): Boolean = OAuthRestService.isRestServicesEnabled()
 
