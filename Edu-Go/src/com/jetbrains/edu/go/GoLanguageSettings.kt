9e3e140093a331c28955168f110d29d305c9b5e2
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2021-11-16 18:23:12 +0300

    EDU-4684: dispose `GoSdkChooserCombo` properly

--- Edu-Go/src/com/jetbrains/edu/go/GoLanguageSettings.kt
+++ Edu-Go/src/com/jetbrains/edu/go/GoLanguageSettings.kt
@@ -5,6 +5,7 @@ import com.goide.sdk.GoSdk
 import com.goide.sdk.combobox.GoSdkChooserCombo
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.ui.LabeledComponent
+import com.intellij.openapi.util.Disposer
 import com.intellij.openapi.util.UserDataHolder
 import com.intellij.openapi.util.io.FileUtil.isAncestor
 import com.jetbrains.edu.go.messages.EduGoBundle
@@ -15,24 +16,29 @@ import java.awt.BorderLayout
 import javax.swing.JComponent
 
 class GoLanguageSettings : LanguageSettings<GoProjectSettings>() {
-  private val sdkChooser: GoSdkChooserCombo = GoSdkChooserCombo()
 
-  init {
-    sdkChooser.addChangedListener {
-      notifyListeners()
-    }
-  }
+  private var selectedSdk: GoSdk? = null
 
-  override fun getSettings(): GoProjectSettings = GoProjectSettings(sdkChooser.sdk)
+  override fun getSettings(): GoProjectSettings = GoProjectSettings(selectedSdk ?: GoSdk.NULL)
 
   override fun getLanguageSettingsComponents(course: Course, disposable: Disposable, context: UserDataHolder?): List<LabeledComponent<JComponent>> {
+    val sdkChooser = GoSdkChooserCombo()
+    Disposer.register(disposable, sdkChooser)
+
+    selectedSdk = sdkChooser.sdk
+    sdkChooser.addChangedListener {
+      selectedSdk = sdkChooser.sdk
+      notifyListeners()
+    }
+
     return listOf(LabeledComponent.create(sdkChooser as JComponent, SDK_TYPE_ID, BorderLayout.WEST))
   }
 
   override fun validate(course: Course?, courseLocation: String?): ValidationMessage? {
-    if (sdkChooser.sdk == GoSdk.NULL) return ValidationMessage(EduGoBundle.message("error.no.sdk", SDK_TYPE_ID))
-    if (!sdkChooser.sdk.isValid) return ValidationMessage(EduGoBundle.message("error.invalid.sdk", SDK_TYPE_ID))
-    if (courseLocation != null && isAncestor(courseLocation, sdkChooser.sdk.homePath, false))
+    val sdk = selectedSdk ?: return null
+    if (sdk == GoSdk.NULL) return ValidationMessage(EduGoBundle.message("error.no.sdk", SDK_TYPE_ID))
+    if (!sdk.isValid) return ValidationMessage(EduGoBundle.message("error.invalid.sdk", SDK_TYPE_ID))
+    if (courseLocation != null && isAncestor(courseLocation, sdk.homePath, false))
       return ValidationMessage(EduGoBundle.message("error.invalid.sdk.location", SDK_TYPE_ID))
     return null
   }
