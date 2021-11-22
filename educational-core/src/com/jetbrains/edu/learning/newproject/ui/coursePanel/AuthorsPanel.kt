45919598df2b6351a4aa981f1788be0d0920d646
Valentina Kiryushkina <valentina.kiryushkina@jetbrains.com>
2021-11-22 17:09:54 +0100

    EDU-4729 Wrap authors string if it's too long

--- educational-core/src/com/jetbrains/edu/learning/newproject/ui/coursePanel/AuthorsPanel.kt
+++ educational-core/src/com/jetbrains/edu/learning/newproject/ui/coursePanel/AuthorsPanel.kt
@@ -1,39 +1,32 @@
 package com.jetbrains.edu.learning.newproject.ui.coursePanel
 
-import com.intellij.ide.plugins.newui.HorizontalLayout
-import com.intellij.ui.JBColor
-import com.intellij.ui.components.JBLabel
-import com.intellij.ui.components.panels.NonOpaquePanel
+import com.intellij.ui.components.JBScrollPane
 import com.intellij.util.ui.JBUI
 import com.jetbrains.edu.learning.courseFormat.Course
-import java.awt.BorderLayout
-import java.awt.Color
+import com.jetbrains.edu.learning.newproject.ui.GrayTextHtmlPanel
+import javax.swing.ScrollPaneConstants
 
 
 private const val INFO_PANEL_TOP_OFFSET = 7
-private const val HORIZONTAL_OFFSET = 10
 
-private val GRAY_TEXT_FOREGROUND: Color = JBColor.namedColor("Plugins.tagForeground", JBColor(0x787878, 0x999999))
-
-class AuthorsPanel : NonOpaquePanel(), CourseSelectionListener {
-  private var authorsLabel: JBLabel = JBLabel()
+class AuthorsPanel : JBScrollPane(ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER,
+                                  ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER), CourseSelectionListener {
+  private var authorsLabel: GrayTextHtmlPanel = GrayTextHtmlPanel("text").apply {
+    border = JBUI.Borders.empty(INFO_PANEL_TOP_OFFSET, HORIZONTAL_MARGIN, 0, 0)
+  }
 
   private val Course.allAuthors: String
     get() = course.authorFullNames.joinToString()
 
   init {
-    border = JBUI.Borders.empty(INFO_PANEL_TOP_OFFSET, HORIZONTAL_MARGIN, 0, 0)
-    layout = HorizontalLayout(HORIZONTAL_OFFSET)
-
-    authorsLabel.foreground = GRAY_TEXT_FOREGROUND
-    add(authorsLabel, BorderLayout.PAGE_START)
+    setViewportView(authorsLabel)
   }
 
   override fun onCourseSelectionChanged(courseInfo: CourseInfo, courseDisplaySettings: CourseDisplaySettings) {
     val course = courseInfo.course
     isVisible = courseDisplaySettings.showInstructorField && course.allAuthors.isNotEmpty()
     if (authorsLabel.isVisible) {
-      authorsLabel.text = "by ${course.allAuthors}"
+      authorsLabel.setBody("by ${course.allAuthors}")
     }
   }
 }
