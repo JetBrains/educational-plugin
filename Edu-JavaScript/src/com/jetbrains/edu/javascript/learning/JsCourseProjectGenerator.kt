7a8813837782153526e265286f366c10dc151bc5
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2021-12-09 13:39:24 +0300

    Convert `JsCourseProjectGenerator` to Kotlin. Step 2

--- Edu-JavaScript/src/com/jetbrains/edu/javascript/learning/JsCourseProjectGenerator.kt
+++ Edu-JavaScript/src/com/jetbrains/edu/javascript/learning/JsCourseProjectGenerator.kt
@@ -1,65 +1,58 @@
-package com.jetbrains.edu.javascript.learning;
+package com.jetbrains.edu.javascript.learning
 
-import com.intellij.javascript.nodejs.interpreter.NodeJsInterpreter;
-import com.intellij.javascript.nodejs.interpreter.NodeJsInterpreterManager;
-import com.intellij.javascript.nodejs.settings.NodeSettingsConfigurable;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.application.ModalityState;
-import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.module.ModuleManager;
-import com.intellij.openapi.options.ShowSettingsUtil;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.jetbrains.edu.learning.OpenApiExtKt;
-import com.jetbrains.edu.learning.courseFormat.Course;
-import com.jetbrains.edu.learning.courseGeneration.GeneratorUtils;
-import com.jetbrains.edu.learning.newproject.CourseProjectGenerator;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.javascript.nodejs.interpreter.NodeJsInterpreterManager
+import com.intellij.javascript.nodejs.settings.NodeSettingsConfigurable
+import com.intellij.lang.javascript.ui.NodeModuleNamesUtil
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.ModalityState
+import com.intellij.openapi.diagnostic.logger
+import com.intellij.openapi.module.ModuleManager
+import com.intellij.openapi.options.ShowSettingsUtil
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+import com.jetbrains.edu.learning.courseFormat.Course
+import com.jetbrains.edu.learning.courseGeneration.GeneratorUtils.createChildFile
+import com.jetbrains.edu.learning.courseGeneration.GeneratorUtils.getInternalTemplateText
+import com.jetbrains.edu.learning.isUnitTestMode
+import com.jetbrains.edu.learning.newproject.CourseProjectGenerator
+import java.io.IOException
 
-import java.io.IOException;
-
-import static com.intellij.lang.javascript.ui.NodeModuleNamesUtil.PACKAGE_JSON;
-import static com.jetbrains.edu.learning.courseGeneration.GeneratorUtils.getInternalTemplateText;
-
-public class JsCourseProjectGenerator extends CourseProjectGenerator<JsNewProjectSettings> {
-  public static final Logger LOG = Logger.getInstance(JsCourseProjectGenerator.class);
-
-  public JsCourseProjectGenerator(@NotNull JsCourseBuilder builder, @NotNull Course course) {
-    super(builder, course);
-  }
-
-  @Override
-  public void afterProjectGenerated(@NotNull Project project, @NotNull JsNewProjectSettings projectSettings) {
-    super.afterProjectGenerated(project, projectSettings);
-    NodeJsInterpreter interpreter = projectSettings.getSelectedInterpreter();
-    NodeJsInterpreterManager.getInstance(project).setInterpreterRef(interpreter.toRef());
-    ModalityState modalityState = ModalityState.current();
-    interpreter.provideCachedVersionOrFetch(version -> ApplicationManager.getApplication().invokeLater(() -> {
+class JsCourseProjectGenerator(builder: JsCourseBuilder, course: Course) : CourseProjectGenerator<JsNewProjectSettings>(builder, course) {
+  override fun afterProjectGenerated(project: Project, projectSettings: JsNewProjectSettings) {
+    super.afterProjectGenerated(project, projectSettings)
+    val interpreter = projectSettings.selectedInterpreter
+    NodeJsInterpreterManager.getInstance(project).setInterpreterRef(interpreter.toRef())
+    val modalityState = ModalityState.current()
+    interpreter.provideCachedVersionOrFetch { version ->
+      ApplicationManager.getApplication().invokeLater({
         if (version != null) {
-          EduNodeJsCoreLibraryConfigurator.configureAndAssociateWithProject(project, interpreter, version);
+          configureAndAssociateWithProject(project, interpreter, version)
         }
         else {
-          LOG.warn("Couldn't retrieve Node interpreter version");
-          //noinspection UnstableApiUsage
-          VirtualFile requester = ModuleManager.getInstance(project).getModules()[0].getModuleFile();
-          ShowSettingsUtil.getInstance().editConfigurable(project, new NodeSettingsConfigurable(project, requester, true));
+          LOG.warn("Couldn't retrieve Node interpreter version")
+          @Suppress("UnstableApiUsage")
+          val requester = ModuleManager.getInstance(project).modules[0].moduleFile
+          // BACKCOMPAT: 2021.2
+          @Suppress("DEPRECATION")
+          ShowSettingsUtil.getInstance().editConfigurable(project, NodeSettingsConfigurable(project, requester, true))
         }
-      }, modalityState, project.getDisposed()));
+      }, modalityState, project.disposed)
     }
+  }
 
-
-  @Override
-  public void createAdditionalFiles(@NotNull Project project,
-                                    @NotNull VirtualFile baseDir,
-                                    boolean isNewCourse) throws IOException {
-    VirtualFile packageJsonFile = baseDir.findChild(PACKAGE_JSON);
-    if (packageJsonFile == null && !myCourse.isStudy()) {
-      final String templateText = getInternalTemplateText(PACKAGE_JSON);
-      packageJsonFile = GeneratorUtils.createChildFile(project, baseDir, PACKAGE_JSON, templateText);
+  @Throws(IOException::class)
+  override fun createAdditionalFiles(project: Project, baseDir: VirtualFile, isNewCourse: Boolean) {
+    var packageJsonFile = baseDir.findChild(NodeModuleNamesUtil.PACKAGE_JSON)
+    if (packageJsonFile == null && !myCourse.isStudy) {
+      val templateText = getInternalTemplateText(NodeModuleNamesUtil.PACKAGE_JSON)
+      packageJsonFile = createChildFile(project, baseDir, NodeModuleNamesUtil.PACKAGE_JSON, templateText)
     }
-
-    if (packageJsonFile != null && !OpenApiExtKt.isUnitTestMode()) {
-      JsUtils.installNodeDependencies(project, packageJsonFile);
+    if (packageJsonFile != null && !isUnitTestMode) {
+      installNodeDependencies(project, packageJsonFile)
     }
+  }
+
+  companion object {
+    private val LOG = logger<JsCourseProjectGenerator>()
   }
 }
