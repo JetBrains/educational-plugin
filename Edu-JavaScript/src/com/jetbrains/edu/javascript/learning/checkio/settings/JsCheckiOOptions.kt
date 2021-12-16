605a474cf59376cfc9216163a4bb0459bb885d42
Aleksey Rostovskiy <aleksey.rostovskiy@jetbrains.com>
2021-12-16 07:36:58 +0300

    EDU-4774 Improve CheckiOOptions

--- Edu-JavaScript/src/com/jetbrains/edu/javascript/learning/checkio/settings/JsCheckiOOptions.kt
+++ Edu-JavaScript/src/com/jetbrains/edu/javascript/learning/checkio/settings/JsCheckiOOptions.kt
@@ -1,13 +1,41 @@
 package com.jetbrains.edu.javascript.learning.checkio.settings
 
+import com.intellij.openapi.application.ApplicationManager
+import com.jetbrains.edu.javascript.learning.checkio.JsCheckiOSettings
 import com.jetbrains.edu.javascript.learning.checkio.connectors.JsCheckiOOAuthConnector
 import com.jetbrains.edu.javascript.learning.checkio.utils.profileUrl
 import com.jetbrains.edu.learning.checkio.account.CheckiOAccount
-import com.jetbrains.edu.learning.checkio.options.CheckiOOptions
 import com.jetbrains.edu.learning.checkio.utils.CheckiONames
+import com.jetbrains.edu.learning.settings.LoginOptions
+import com.jetbrains.edu.learning.stepik.hyperskill.api.HyperskillConnector
 import org.jetbrains.annotations.Nls
+import javax.swing.event.HyperlinkEvent
 
-class JsCheckiOOptions : CheckiOOptions(JsCheckiOOAuthConnector) {
+class JsCheckiOOptions : LoginOptions<CheckiOAccount>() {
+  override fun getCurrentAccount(): CheckiOAccount? = JsCheckiOSettings.getInstance().account
+
+  override fun setCurrentAccount(account: CheckiOAccount?) {
+    JsCheckiOSettings.getInstance().account = account
+    val messageBus = ApplicationManager.getApplication().messageBus
+    if (account != null) {
+      messageBus.syncPublisher(HyperskillConnector.AUTHORIZATION_TOPIC).userLoggedIn()
+    }
+    else {
+      messageBus.syncPublisher(HyperskillConnector.AUTHORIZATION_TOPIC).userLoggedOut()
+    }
+  }
+
+  override fun createAuthorizeListener(): LoginListener {
+    return object : LoginListener() {
+      override fun authorize(e: HyperlinkEvent?) {
+        JsCheckiOOAuthConnector.doAuthorize(Runnable {
+          lastSavedAccount = getCurrentAccount()
+          updateLoginLabels()
+        })
+      }
+    }
+  }
+
   @Nls
   override fun getDisplayName(): String = CheckiONames.JS_CHECKIO
 
--- Edu-Python/src/com/jetbrains/edu/python/learning/checkio/settings/PyCheckiOOptions.kt
+++ Edu-Python/src/com/jetbrains/edu/python/learning/checkio/settings/PyCheckiOOptions.kt
@@ -1,13 +1,41 @@
 package com.jetbrains.edu.python.learning.checkio.settings
 
+import com.intellij.openapi.application.ApplicationManager
 import com.jetbrains.edu.learning.checkio.account.CheckiOAccount
-import com.jetbrains.edu.learning.checkio.options.CheckiOOptions
 import com.jetbrains.edu.learning.checkio.utils.CheckiONames
+import com.jetbrains.edu.learning.settings.LoginOptions
+import com.jetbrains.edu.learning.stepik.hyperskill.api.HyperskillConnector
+import com.jetbrains.edu.python.learning.checkio.PyCheckiOSettings
 import com.jetbrains.edu.python.learning.checkio.connectors.PyCheckiOOAuthConnector
 import com.jetbrains.edu.python.learning.checkio.utils.profileUrl
 import org.jetbrains.annotations.Nls
+import javax.swing.event.HyperlinkEvent
 
-class PyCheckiOOptions : CheckiOOptions(PyCheckiOOAuthConnector) {
+class PyCheckiOOptions : LoginOptions<CheckiOAccount>() {
+  override fun getCurrentAccount(): CheckiOAccount? = PyCheckiOSettings.INSTANCE.account
+
+  override fun setCurrentAccount(account: CheckiOAccount?) {
+    PyCheckiOSettings.INSTANCE.account = account
+    val messageBus = ApplicationManager.getApplication().messageBus
+    if (account != null) {
+      messageBus.syncPublisher(HyperskillConnector.AUTHORIZATION_TOPIC).userLoggedIn()
+    }
+    else {
+      messageBus.syncPublisher(HyperskillConnector.AUTHORIZATION_TOPIC).userLoggedOut()
+    }
+  }
+
+  override fun createAuthorizeListener(): LoginListener {
+    return object : LoginListener() {
+      override fun authorize(e: HyperlinkEvent?) {
+        PyCheckiOOAuthConnector.doAuthorize(Runnable {
+          lastSavedAccount = getCurrentAccount()
+          updateLoginLabels()
+        })
+      }
+    }
+  }
+
   @Nls
   override fun getDisplayName(): String = CheckiONames.PY_CHECKIO
 
