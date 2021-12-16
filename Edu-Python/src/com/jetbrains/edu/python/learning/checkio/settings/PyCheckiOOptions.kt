fdffea97aeeb2778863ddb1ed0273ce94caa8b0b
Aleksey Rostovskiy <aleksey.rostovskiy@jetbrains.com>
2021-12-16 07:32:20 +0300

    EDU-4774 Convert PyCheckiOOptions to Kotlin

--- Edu-Python/src/com/jetbrains/edu/python/learning/checkio/settings/PyCheckiOOptions.kt
+++ Edu-Python/src/com/jetbrains/edu/python/learning/checkio/settings/PyCheckiOOptions.kt
@@ -1,27 +1,15 @@
-package com.jetbrains.edu.python.learning.checkio.settings;
+package com.jetbrains.edu.python.learning.checkio.settings
 
-import com.jetbrains.edu.learning.checkio.account.CheckiOAccount;
-import com.jetbrains.edu.learning.checkio.options.CheckiOOptions;
-import com.jetbrains.edu.learning.checkio.utils.CheckiONames;
-import com.jetbrains.edu.python.learning.checkio.connectors.PyCheckiOOAuthConnector;
-import com.jetbrains.edu.python.learning.checkio.utils.PyCheckiOUtils;
-import org.jetbrains.annotations.Nls;
-import org.jetbrains.annotations.NotNull;
+import com.jetbrains.edu.learning.checkio.account.CheckiOAccount
+import com.jetbrains.edu.learning.checkio.options.CheckiOOptions
+import com.jetbrains.edu.learning.checkio.utils.CheckiONames
+import com.jetbrains.edu.python.learning.checkio.connectors.PyCheckiOOAuthConnector
+import com.jetbrains.edu.python.learning.checkio.utils.profileUrl
+import org.jetbrains.annotations.Nls
 
-public class PyCheckiOOptions extends CheckiOOptions {
-  protected PyCheckiOOptions() {
-    super(PyCheckiOOAuthConnector.INSTANCE);
-  }
-
+class PyCheckiOOptions : CheckiOOptions(PyCheckiOOAuthConnector) {
   @Nls
-  @Override
-  public String getDisplayName() {
-    return CheckiONames.PY_CHECKIO;
-  }
+  override fun getDisplayName(): String = CheckiONames.PY_CHECKIO
 
-  @NotNull
-  @Override
-  protected String profileUrl(@NotNull CheckiOAccount account) {
-    return PyCheckiOUtils.getProfileUrl(account);
-  }
+  override fun profileUrl(account: CheckiOAccount): String = account.profileUrl
 }
