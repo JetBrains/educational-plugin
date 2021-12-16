1fe212f4a7a32ed8cc7c950b4e7fd5a4e1f109f9
Aleksey Rostovskiy <aleksey.rostovskiy@jetbrains.com>
2021-12-16 07:31:42 +0300

    EDU-4774 Convert JsCheckiOOptions to Kotlin

--- Edu-JavaScript/src/com/jetbrains/edu/javascript/learning/checkio/settings/JsCheckiOOptions.kt
+++ Edu-JavaScript/src/com/jetbrains/edu/javascript/learning/checkio/settings/JsCheckiOOptions.kt
@@ -1,27 +1,15 @@
-package com.jetbrains.edu.javascript.learning.checkio.settings;
+package com.jetbrains.edu.javascript.learning.checkio.settings
 
-import com.jetbrains.edu.javascript.learning.checkio.connectors.JsCheckiOOAuthConnector;
-import com.jetbrains.edu.javascript.learning.checkio.utils.JsCheckiOUtils;
-import com.jetbrains.edu.learning.checkio.account.CheckiOAccount;
-import com.jetbrains.edu.learning.checkio.options.CheckiOOptions;
-import com.jetbrains.edu.learning.checkio.utils.CheckiONames;
-import org.jetbrains.annotations.Nls;
-import org.jetbrains.annotations.NotNull;
+import com.jetbrains.edu.javascript.learning.checkio.connectors.JsCheckiOOAuthConnector
+import com.jetbrains.edu.javascript.learning.checkio.utils.profileUrl
+import com.jetbrains.edu.learning.checkio.account.CheckiOAccount
+import com.jetbrains.edu.learning.checkio.options.CheckiOOptions
+import com.jetbrains.edu.learning.checkio.utils.CheckiONames
+import org.jetbrains.annotations.Nls
 
-public class JsCheckiOOptions extends CheckiOOptions {
-  protected JsCheckiOOptions() {
-    super(JsCheckiOOAuthConnector.INSTANCE);
-  }
-
+class JsCheckiOOptions : CheckiOOptions(JsCheckiOOAuthConnector) {
   @Nls
-  @Override
-  public String getDisplayName() {
-    return CheckiONames.JS_CHECKIO;
-  }
+  override fun getDisplayName(): String = CheckiONames.JS_CHECKIO
 
-  @NotNull
-  @Override
-  protected String profileUrl(@NotNull CheckiOAccount account) {
-    return JsCheckiOUtils.getProfileUrl(account);
-  }
+  override fun profileUrl(account: CheckiOAccount): String = account.profileUrl
 }
