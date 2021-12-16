208586450090a7d841c162483ac7635dfeb6b980
Aleksey Rostovskiy <aleksey.rostovskiy@jetbrains.com>
2021-12-16 19:16:37 +0300

    cleanup: extract anonymous TaskCheckerProvider objects into separate classes

--- Edu-JavaScript/src/com/jetbrains/edu/javascript/learning/checkio/JsCheckiOConfigurator.kt
+++ Edu-JavaScript/src/com/jetbrains/edu/javascript/learning/checkio/JsCheckiOConfigurator.kt
@@ -1,43 +1,31 @@
 package com.jetbrains.edu.javascript.learning.checkio
 
 import com.intellij.lang.javascript.JavaScriptFileType
-import com.intellij.openapi.project.Project
 import com.jetbrains.edu.EducationalCoreIcons
 import com.jetbrains.edu.javascript.learning.JsConfigurator
-import com.jetbrains.edu.javascript.learning.JsEnvironmentChecker
+import com.jetbrains.edu.javascript.learning.checkio.checker.JsCheckiOTaskCheckerProvider
 import com.jetbrains.edu.javascript.learning.checkio.connectors.JsCheckiOApiConnector
 import com.jetbrains.edu.javascript.learning.checkio.connectors.JsCheckiOOAuthConnector
-import com.jetbrains.edu.javascript.learning.checkio.utils.JsCheckiONames.*
-import com.jetbrains.edu.learning.checker.EnvironmentChecker
-import com.jetbrains.edu.learning.checker.TaskChecker
+import com.jetbrains.edu.javascript.learning.checkio.utils.JsCheckiONames.JS_CHECKIO_API_HOST
 import com.jetbrains.edu.learning.checker.TaskCheckerProvider
 import com.jetbrains.edu.learning.checkio.CheckiOConnectorProvider
 import com.jetbrains.edu.learning.checkio.CheckiOCourseContentGenerator
-import com.jetbrains.edu.learning.checkio.checker.CheckiOTaskChecker
 import com.jetbrains.edu.learning.checkio.connectors.CheckiOOAuthConnector
 import com.jetbrains.edu.learning.checkio.courseFormat.CheckiOCourse
 import com.jetbrains.edu.learning.checkio.utils.CheckiOCourseGenerationUtils.getCourseFromServerUnderProgress
 import com.jetbrains.edu.learning.courseFormat.Course
-import com.jetbrains.edu.learning.courseFormat.tasks.EduTask
 import javax.swing.Icon
 
 class JsCheckiOConfigurator : JsConfigurator(), CheckiOConnectorProvider {
-  override val taskCheckerProvider: TaskCheckerProvider = object : TaskCheckerProvider {
-    override val envChecker: EnvironmentChecker
-      get() = JsEnvironmentChecker()
-
-    override fun getEduTaskChecker(task: EduTask, project: Project): TaskChecker<EduTask> =
-      CheckiOTaskChecker(task, envChecker, project, JsCheckiOOAuthConnector,
-                         JS_CHECKIO_INTERPRETER, JS_CHECKIO_TEST_FORM_TARGET_URL)
-  }
-
-  override fun getOAuthConnector(): CheckiOOAuthConnector = JsCheckiOOAuthConnector
+  override val taskCheckerProvider: TaskCheckerProvider
+    get() = JsCheckiOTaskCheckerProvider()
 
   override val logo: Icon
     get() = EducationalCoreIcons.JSCheckiO
 
-  override val isCourseCreatorEnabled: Boolean
-    get() = false
+  override val isCourseCreatorEnabled: Boolean = false
+
+  override fun getOAuthConnector(): CheckiOOAuthConnector = JsCheckiOOAuthConnector
 
   override fun beforeCourseStarted(course: Course) {
     val contentGenerator = CheckiOCourseContentGenerator(JavaScriptFileType.INSTANCE, JsCheckiOApiConnector)
--- Edu-Python/src/com/jetbrains/edu/python/learning/checkio/PyCheckiOConfigurator.kt
+++ Edu-Python/src/com/jetbrains/edu/python/learning/checkio/PyCheckiOConfigurator.kt
@@ -1,25 +1,20 @@
 package com.jetbrains.edu.python.learning.checkio
 
-import com.intellij.openapi.project.Project
 import com.jetbrains.edu.EducationalCoreIcons
 import com.jetbrains.edu.learning.EduCourseBuilder
 import com.jetbrains.edu.learning.EduUtils
-import com.jetbrains.edu.learning.checker.EnvironmentChecker
-import com.jetbrains.edu.learning.checker.TaskChecker
 import com.jetbrains.edu.learning.checker.TaskCheckerProvider
 import com.jetbrains.edu.learning.checkio.CheckiOConnectorProvider
 import com.jetbrains.edu.learning.checkio.CheckiOCourseContentGenerator
-import com.jetbrains.edu.learning.checkio.checker.CheckiOTaskChecker
 import com.jetbrains.edu.learning.checkio.connectors.CheckiOOAuthConnector
 import com.jetbrains.edu.learning.checkio.courseFormat.CheckiOCourse
 import com.jetbrains.edu.learning.checkio.utils.CheckiOCourseGenerationUtils.getCourseFromServerUnderProgress
 import com.jetbrains.edu.learning.courseFormat.Course
-import com.jetbrains.edu.learning.courseFormat.tasks.EduTask
 import com.jetbrains.edu.python.learning.PyConfigurator
-import com.jetbrains.edu.python.learning.checker.PyEnvironmentChecker
+import com.jetbrains.edu.python.learning.checkio.checker.PyCheckiOTaskCheckerProvider
 import com.jetbrains.edu.python.learning.checkio.connectors.PyCheckiOApiConnector
 import com.jetbrains.edu.python.learning.checkio.connectors.PyCheckiOOAuthConnector
-import com.jetbrains.edu.python.learning.checkio.utils.PyCheckiONames.*
+import com.jetbrains.edu.python.learning.checkio.utils.PyCheckiONames.PY_CHECKIO_API_HOST
 import com.jetbrains.python.PythonFileType
 import com.jetbrains.python.newProject.PyNewProjectSettings
 import javax.swing.Icon
@@ -28,29 +23,21 @@ class PyCheckiOConfigurator : PyConfigurator(), CheckiOConnectorProvider {
   override val courseBuilder: EduCourseBuilder<PyNewProjectSettings>
     get() = PyCheckiOCourseBuilder()
 
-  override val taskCheckerProvider: TaskCheckerProvider = object : TaskCheckerProvider {
-    override val envChecker: EnvironmentChecker
-      get() = PyEnvironmentChecker()
-
-    override fun getEduTaskChecker(task: EduTask, project: Project): TaskChecker<EduTask> {
-      return CheckiOTaskChecker(task, envChecker, project, PyCheckiOOAuthConnector, PY_CHECKIO_INTERPRETER,
-                                PY_CHECKIO_TEST_FORM_TARGET_URL)
-    }
-  }
-
-  override fun getOAuthConnector(): CheckiOOAuthConnector = PyCheckiOOAuthConnector
+  override val taskCheckerProvider: TaskCheckerProvider
+    get() = PyCheckiOTaskCheckerProvider()
 
   override val logo: Icon
     get() = EducationalCoreIcons.CheckiO
 
-  override val isCourseCreatorEnabled: Boolean
-    get() = false
+  override val isCourseCreatorEnabled: Boolean = false
+
+  override val isEnabled: Boolean
+    get() = !EduUtils.isAndroidStudio()
 
   override fun beforeCourseStarted(course: Course) {
     val contentGenerator = CheckiOCourseContentGenerator(PythonFileType.INSTANCE, PyCheckiOApiConnector)
     getCourseFromServerUnderProgress(contentGenerator, (course as CheckiOCourse), PyCheckiOSettings.INSTANCE.account, PY_CHECKIO_API_HOST)
   }
 
-  override val isEnabled: Boolean
-    get() = !EduUtils.isAndroidStudio()
+  override fun getOAuthConnector(): CheckiOOAuthConnector = PyCheckiOOAuthConnector
 }
