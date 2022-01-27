3b17ba260ddb71968991a69584173f558b73f22d
ElenaDolgova <elena.dolgova@jetbrains.com>
2022-01-27 14:06:16 +0000

    EDU-4854 exclude vendor and composer.phar from archive
    Merge-request: EDU-MR-1584

--- Edu-Php/src/com/jetbrains/edu/php/PhpConfigurator.kt
+++ Edu-Php/src/com/jetbrains/edu/php/PhpConfigurator.kt
@@ -1,11 +1,14 @@
 package com.jetbrains.edu.php
 
 import com.intellij.openapi.extensions.PluginId
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
 import com.jetbrains.edu.EducationalCoreIcons
 import com.jetbrains.edu.learning.*
 import com.jetbrains.edu.learning.checker.TaskCheckerProvider
 import com.jetbrains.edu.learning.configuration.EduConfigurator
 import com.jetbrains.edu.learning.courseFormat.tasks.Task
+import com.jetbrains.php.composer.ComposerUtils
 import javax.swing.Icon
 
 class PhpConfigurator : EduConfigurator<PhpProjectSettings> {
@@ -37,6 +40,11 @@ class PhpConfigurator : EduConfigurator<PhpProjectSettings> {
   override fun getMockFileName(text: String): String = TASK_PHP
 
   override fun isTestFile(task: Task, path: String): Boolean = super.isTestFile(task, path) || path == testFileName
+
+  override fun excludeFromArchive(project: Project, file: VirtualFile): Boolean =
+    super.excludeFromArchive(project, file) ||
+    file.path.contains(ComposerUtils.VENDOR_DIR_DEFAULT_NAME) ||
+    file.path.contains(ComposerUtils.COMPOSER_PHAR_NAME)
 
   companion object {
     const val MAIN_PHP = "main.php"
