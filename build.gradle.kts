9679370e7494a3b8eca531e638580a36509dc998
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2022-01-27 16:31:24 +0300

    Update `org.jetbrains.intellij` plugin to 1.4.0
    It provides a proper classloader to run 2022.1 IDEs

--- build.gradle.kts
+++ build.gradle.kts
@@ -98,7 +98,7 @@ val isTeamCity: Boolean get() = System.getenv("TEAMCITY_VERSION") != null
 plugins {
   idea
   kotlin("jvm") version "1.6.10"
-  id("org.jetbrains.intellij") version "1.2.0"
+  id("org.jetbrains.intellij") version "1.4.0"
   id("de.undercouch.download") version "4.0.4"
   id("net.saliman.properties") version "1.5.1"
   id("org.gradle.test-retry") version "1.3.1"
