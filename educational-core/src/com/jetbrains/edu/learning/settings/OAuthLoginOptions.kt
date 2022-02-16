3622f9710c7089d5b282b5e48f6daa1b6fd4d7b8
Aleksey Rostovskiy <aleksey.rostovskiy@jetbrains.com>
2022-02-17 01:15:46 +0300

    revise doAuthorize at OAuthLoginOptions

--- educational-core/src/com/jetbrains/edu/learning/settings/OAuthLoginOptions.kt
+++ educational-core/src/com/jetbrains/edu/learning/settings/OAuthLoginOptions.kt
@@ -1,13 +1,25 @@
 package com.jetbrains.edu.learning.settings
 
+import com.intellij.openapi.application.ModalityState
+import com.intellij.openapi.application.runInEdt
+import com.intellij.openapi.util.Disposer
 import com.intellij.ui.HyperlinkAdapter
+import com.jetbrains.edu.learning.EduLogInListener
 import com.jetbrains.edu.learning.api.EduOAuthConnector
 import com.jetbrains.edu.learning.authUtils.OAuthAccount
 import com.jetbrains.edu.learning.statistics.EduCounterUsageCollector.AuthorizationPlace
+import javax.swing.JComponent
 import javax.swing.event.HyperlinkEvent
 
-abstract class OAuthLoginOptions <T : OAuthAccount<out Any>> : LoginOptions<T>() {
+abstract class OAuthLoginOptions<T : OAuthAccount<out Any>> : LoginOptions<T>() {
   protected abstract val connector: EduOAuthConnector<T, *>
+  private val disposable = Disposer.newDisposable()
+
+  override fun createComponent(): JComponent? {
+    val component = super.createComponent() ?: return null
+    subscribeLogInListener()
+    return component
+  }
 
   override fun getCurrentAccount(): T? = connector.account
 
@@ -15,25 +27,31 @@ abstract class OAuthLoginOptions <T : OAuthAccount<out Any>> : LoginOptions<T>()
     connector.account = account
   }
 
-  override fun createAuthorizeListener(): HyperlinkAdapter =
-    object : HyperlinkAdapter() {
-      override fun hyperlinkActivated(e: HyperlinkEvent?) {
-        connector.doAuthorize(
-          Runnable {
-            lastSavedAccount = getCurrentAccount()
-            updateLoginLabels()
-          },
-          authorizationPlace = AuthorizationPlace.SETTINGS
-        )
-      }
-    }
+  override fun disposeUIResources() = Disposer.dispose(disposable)
 
-  override fun createLogOutListener(): HyperlinkAdapter =
-    object : HyperlinkAdapter() {
-      override fun hyperlinkActivated(event: HyperlinkEvent) {
-        lastSavedAccount = null
-        connector.doLogout(authorizationPlace = AuthorizationPlace.SETTINGS)
-        updateLoginLabels()
+  override fun createAuthorizeListener(): HyperlinkAdapter = object : HyperlinkAdapter() {
+    override fun hyperlinkActivated(event: HyperlinkEvent) = connector.doAuthorize(authorizationPlace = AuthorizationPlace.SETTINGS)
+  }
+
+  override fun createLogOutListener(): HyperlinkAdapter = object : HyperlinkAdapter() {
+    override fun hyperlinkActivated(event: HyperlinkEvent) = connector.doLogout(authorizationPlace = AuthorizationPlace.SETTINGS)
+  }
+
+  private fun subscribeLogInListener() {
+    connector.subscribe(object : EduLogInListener {
+      override fun userLoggedIn() {
+        runInEdt(ModalityState.any()) {
+          lastSavedAccount = getCurrentAccount()
+          updateLoginLabels()
+        }
       }
-    }
+
+      override fun userLoggedOut() {
+        runInEdt(ModalityState.any()) {
+          lastSavedAccount = null
+          updateLoginLabels()
+        }
+      }
+    }, disposable)
+  }
 }
