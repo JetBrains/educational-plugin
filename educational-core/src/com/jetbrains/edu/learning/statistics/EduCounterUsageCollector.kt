1c6381fa1d42a0152a00d62152644a4c59910f2e
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2022-02-22 11:10:15 +0300

    Increase version of `EduCounterUsageCollector`
    We have to do it because of changes in collector scheme in 5dcda996d126a94ed2879a5ce71e8febc391c3a0

--- educational-core/src/com/jetbrains/edu/learning/statistics/EduCounterUsageCollector.kt
+++ educational-core/src/com/jetbrains/edu/learning/statistics/EduCounterUsageCollector.kt
@@ -122,7 +122,7 @@ class EduCounterUsageCollector : CounterUsagesCollector() {
     private const val LANGUAGE = "language"
     private const val EDU_TAB = "tab"
 
-    private val GROUP = EventLogGroup("educational.counters", 6)
+    private val GROUP = EventLogGroup("educational.counters", 7)
 
     private val COURSE_MODE_FIELD = EventFields.String(MODE,
                                                        listOf(EduNames.STUDY, CCUtils.COURSE_MODE))
