8a673f8a3a489327deced9a889f2d1de99eda715
Valentina Kiryushkina <valentina.kiryushkina@jetbrains.com>
2022-04-20 15:10:27 +0200

    extract MouseAdapter into separate class

--- educational-core/src/com/jetbrains/edu/learning/ui/ClickableLabel.kt
+++ educational-core/src/com/jetbrains/edu/learning/ui/ClickableLabel.kt
@@ -1,6 +1,7 @@
 package com.jetbrains.edu.learning.ui
 
 import com.intellij.openapi.util.NlsContexts
+import com.intellij.openapi.util.NlsSafe
 import com.intellij.ui.components.JBLabel
 import com.intellij.util.ui.JBUI
 import java.awt.Component
@@ -18,18 +19,23 @@ class ClickableLabel(
     border = JBUI.Borders.emptyLeft(5)
     cursor = Cursor.getPredefinedCursor(Cursor.HAND_CURSOR)
 
-    addMouseListener(object : MouseAdapter() {
-      override fun mouseClicked(e: MouseEvent?) {
-        onClick.invoke()
-      }
+    addMouseListener(UnderlinedTextMouseAdapter())
+  }
 
-      override fun mouseEntered(e: MouseEvent?) {
-        setText("<html><u>$initialText</u></html>")
-      }
+  private inner class UnderlinedTextMouseAdapter : MouseAdapter() {
+    override fun mouseClicked(e: MouseEvent?) {
+      onClick.invoke()
+    }
 
-      override fun mouseExited(e: MouseEvent?) {
-        setText(initialText)
-      }
-    })
+    override fun mouseEntered(e: MouseEvent?) {
+      text = getLabelText()
+    }
+
+    @NlsSafe
+    private fun getLabelText() = "<html><u>$initialText</u></html>"
+
+    override fun mouseExited(e: MouseEvent?) {
+      text = initialText
+    }
   }
 }
