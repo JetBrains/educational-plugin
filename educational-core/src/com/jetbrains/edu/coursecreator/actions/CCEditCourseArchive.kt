449178d31a1300f10622165a07e85766656b75b3
Ekaterina Smal <ekaterina.smal@jetbrains.com>
2022-06-20 16:04:05 +0300

    EDU-5115 added unpack course in educator mode action

---
+++ educational-core/src/com/jetbrains/edu/coursecreator/actions/CCEditCourseArchive.kt
@@ -0,0 +1,38 @@
+package com.jetbrains.edu.coursecreator.actions
+
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.fileChooser.FileChooser
+import com.intellij.openapi.fileChooser.FileChooserDescriptor
+import com.intellij.openapi.project.DumbAwareAction
+import com.intellij.openapi.ui.Messages
+import com.jetbrains.edu.coursecreator.ui.CCNewCourseDialog
+import com.jetbrains.edu.learning.EduUtils
+import com.jetbrains.edu.learning.courseFormat.CourseMode
+import com.jetbrains.edu.learning.courseFormat.EduCourse
+import com.jetbrains.edu.learning.messages.EduCoreBundle
+
+@Suppress("ComponentNotRegistered")  // educational-core.xml
+class CCEditCourseArchive : DumbAwareAction(
+  EduCoreBundle.lazyMessage("action.unpack.and.edit.course.archive.text"),
+  EduCoreBundle.lazyMessage("action.unpack.and.edit.course.archive.description"),
+  null
+) {
+  override fun actionPerformed(e: AnActionEvent) {
+    val project = e.getData(CommonDataKeys.PROJECT)
+    val descriptor = FileChooserDescriptor(true, true, true, true,
+                                           true, false)
+    val virtualFile = FileChooser.chooseFile(descriptor, project, null) ?: return
+    val course = EduUtils.getLocalCourse(virtualFile.path)
+    if (course == null) {
+      Messages.showErrorDialog(
+        EduCoreBundle.message("dialog.message.course.incompatible"),
+        EduCoreBundle.message("dialog.title.failed.to.unpack.course")
+      )
+      return
+    }
+
+    val producer = { EduCourse().apply { courseMode = CourseMode.EDUCATOR } }
+    CCNewCourseDialog(EduCoreBundle.message("dialog.title.unpack.course"), EduCoreBundle.message("button.unpack"), course, producer).show()
+  }
+}
