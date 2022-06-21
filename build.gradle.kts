87c69dd02b8cc4c614d21343ba066aee585014c8
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2022-06-21 12:24:26 +0300

    EDU-5104: use snapshot version of gradle-intellij-plugin to properly generate UI from .from files
    More precisely, we need [this](https://github.com/JetBrains/gradle-intellij-plugin/commit/662f705ba6ba28b635c83504aa5f6d8aa182d7a4) fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -93,7 +93,8 @@ val isTeamCity: Boolean get() = System.getenv("TEAMCITY_VERSION") != null
 plugins {
   idea
   kotlin("jvm") version "1.6.10"
-  id("org.jetbrains.intellij") version "1.6.0"
+  // Snapshot version is used to avoid https://youtrack.jetbrains.com/issue/EDU-5104
+  id("org.jetbrains.intellij") version "1.7.0-SNAPSHOT"
   id("de.undercouch.download") version "5.1.0"
   id("net.saliman.properties") version "1.5.1"
   id("org.gradle.test-retry") version "1.3.1"
