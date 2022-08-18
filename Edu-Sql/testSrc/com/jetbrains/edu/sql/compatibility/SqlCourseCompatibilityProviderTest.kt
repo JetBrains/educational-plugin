94c6f3047bd27c7b828a325c1d8cbf1ab76e6865
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2022-08-18 16:40:38 +0200

    EDU-5170: remove code of old `Edu-Sql` module

--- Edu-Sql/testSrc/com/jetbrains/edu/sql/compatibility/SqlCourseCompatibilityProviderTest.kt
+++
@@ -1,6 +0,0 @@
-package com.jetbrains.edu.sql.compatibility
-
-import com.jetbrains.edu.learning.compatibility.CourseCompatibilityProviderTestBase
-import com.jetbrains.edu.learning.compatibility.SqlCourseCompatibilityProvider
-
-class SqlCourseCompatibilityProviderTest : CourseCompatibilityProviderTestBase(SqlCourseCompatibilityProvider::class)
