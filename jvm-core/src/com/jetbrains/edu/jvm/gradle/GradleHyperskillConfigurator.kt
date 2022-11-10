fcfec28cc2162ba32f8507c089bfd1abbb8ceb8e
Zinaida.Smirnova <Zinaida.Smirnova@jetbrains.com>
2022-11-10 09:53:41 +0000

    EDU-5210 use runReadActionInSmartMode

--- jvm-core/src/com/jetbrains/edu/jvm/gradle/GradleHyperskillConfigurator.kt
+++ jvm-core/src/com/jetbrains/edu/jvm/gradle/GradleHyperskillConfigurator.kt
@@ -1,6 +1,5 @@
 package com.jetbrains.edu.jvm.gradle
 
-import com.intellij.openapi.application.runReadAction
 import com.intellij.openapi.project.Project
 import com.jetbrains.edu.jvm.MainFileProvider
 import com.jetbrains.edu.learning.configuration.EduConfigurator
@@ -8,15 +7,16 @@ import com.jetbrains.edu.learning.courseFormat.TaskFile
 import com.jetbrains.edu.learning.courseFormat.ext.getVirtualFile
 import com.jetbrains.edu.learning.courseFormat.ext.languageById
 import com.jetbrains.edu.learning.courseFormat.tasks.Task
+import com.jetbrains.edu.learning.runReadActionInSmartMode
 import com.jetbrains.edu.learning.stepik.hyperskill.HyperskillConfigurator
 
 abstract class GradleHyperskillConfigurator<T : Any>(baseConfigurator: EduConfigurator<T>) : HyperskillConfigurator<T>(baseConfigurator) {
   override fun getCodeTaskFile(project: Project, task: Task): TaskFile? {
     val language = task.course.languageById ?: return super.getCodeTaskFile(project, task)
-    return runReadAction {
+    return runReadActionInSmartMode(project) {
       for (file in task.taskFiles.values) {
         val virtualFile = file.getVirtualFile(project) ?: continue
-        if (MainFileProvider.getMainClassName(project, virtualFile, language) != null) return@runReadAction file
+        if (MainFileProvider.getMainClassName(project, virtualFile, language) != null) return@runReadActionInSmartMode file
       }
       null
     } ?: super.getCodeTaskFile(project, task)
