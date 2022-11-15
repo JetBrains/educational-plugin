be60044270ee78c90deb43fe40eb0587de12c463
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2022-11-15 17:36:35 +0100

    Explicitly enable go support in go courses
    It should fix checker tests for Go courses

--- Edu-Go/src/com/jetbrains/edu/go/GoCourseProjectGenerator.kt
+++ Edu-Go/src/com/jetbrains/edu/go/GoCourseProjectGenerator.kt
@@ -1,7 +1,9 @@
 package com.jetbrains.edu.go
 
+import com.goide.project.GoModuleSettings
 import com.goide.sdk.GoSdkService
 import com.goide.vgo.configuration.VgoProjectSettings
+import com.intellij.openapi.module.ModuleManager
 import com.intellij.openapi.project.Project
 import com.jetbrains.edu.learning.courseFormat.Course
 import com.jetbrains.edu.learning.newproject.CourseProjectGenerator
@@ -11,6 +13,16 @@ class GoCourseProjectGenerator(builder: GoCourseBuilder, course: Course) :
   override fun afterProjectGenerated(project: Project, projectSettings: GoProjectSettings) {
     super.afterProjectGenerated(project, projectSettings)
     GoSdkService.getInstance(project).setSdk(projectSettings.sdk)
+    val module = ModuleManager.getInstance(project).modules.singleOrNull()
+    if (module != null) {
+      // Enable go support in created course project.
+      // It will be enabled anyway by `GoPluginInitialConfigurator`.
+      // But because `GoPluginInitialConfigurator` is registered `backgroundPostStartupActivity`,
+      // the support will be enabled with some delay.
+      // As a result, our checker tests don't work as expected because they are run before `GoPluginInitialConfigurator#runActivity`.
+      // So let's enable it by default - we're absolutely sure that it's Go project
+      GoModuleSettings.getInstance(module).isGoSupportEnabled = true
+    }
     VgoProjectSettings.getInstance(project).isIntegrationEnabled = true
   }
 }
