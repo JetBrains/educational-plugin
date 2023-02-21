c8b2efa4cd2ce782fd8255f0498f3e21b658b03f
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2023-02-21 11:13:30 +0100

    Update `intellij-gradle-plugin` to 1.13.3

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -133,3 +133,10 @@ buildCache {
     removeUnusedEntriesAfterDays = 30
   }
 }
+
+pluginManagement {
+  repositories {
+    maven("https://oss.sonatype.org/content/repositories/snapshots/")
+    gradlePluginPortal()
+  }
+}
