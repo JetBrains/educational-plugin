a2acdd9a228bb0c5c30b4e28c218aac00f9d4575
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2023-02-23 15:13:31 +0100

    EDU-5285: use group id in message property for course creator action group

--- educational-core/src/com/jetbrains/edu/coursecreator/actions/CCProjectViewActionGroup.kt
+++ educational-core/src/com/jetbrains/edu/coursecreator/actions/CCProjectViewActionGroup.kt
@@ -5,11 +5,9 @@ import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.DefaultActionGroup
 import com.intellij.openapi.project.DumbAware
 import com.jetbrains.edu.coursecreator.CCUtils.updateActionGroup
-import com.jetbrains.edu.learning.messages.EduCoreBundle
 
 @Suppress("ComponentNotRegistered") // educational-core.xml
-class CCProjectViewActionGroup : DefaultActionGroup(EduCoreBundle.lazyMessage("group.educator.projectView.text"),
-                                                    EduCoreBundle.lazyMessage("group.educator.projectView.description"), null), DumbAware {
+class CCProjectViewActionGroup : DefaultActionGroup(), DumbAware {
   override fun update(e: AnActionEvent) {
     updateActionGroup(e)
   }
