e26af8a9c0965ba1f1bd7d7c07f355f1f5c11080
Ekaterina Smal <ekaterina.smal@jetbrains.com>
2023-03-30 16:14:27 +0300

    migrate JsCheckiONames to kotlin

--- Edu-JavaScript/src/com/jetbrains/edu/javascript/learning/checkio/utils/JsCheckiONames.kt
+++ Edu-JavaScript/src/com/jetbrains/edu/javascript/learning/checkio/utils/JsCheckiONames.kt
@@ -1,14 +1,9 @@
-package com.jetbrains.edu.javascript.learning.checkio.utils;
+package com.jetbrains.edu.javascript.learning.checkio.utils
 
-import static com.jetbrains.edu.learning.checkio.utils.CheckiONames.CHECKIO_HOST;
-import static com.jetbrains.edu.learning.checkio.utils.CheckiONames.CHECKIO_TEST_FORM_TARGET_PATH;
+import com.jetbrains.edu.learning.checkio.utils.CheckiONames
 
-public final class JsCheckiONames {
-  private JsCheckiONames() {}
-
-  public static final String JS_CHECKIO_URL = "https://js." + CHECKIO_HOST;
-
-  public static final String JS_CHECKIO_INTERPRETER = "js-node";
-
-  public static final String JS_CHECKIO_TEST_FORM_TARGET_URL = JS_CHECKIO_URL + CHECKIO_TEST_FORM_TARGET_PATH + "/";
+object JsCheckiONames {
+  const val JS_CHECKIO_URL = "https://js." + CheckiONames.CHECKIO_HOST
+  const val JS_CHECKIO_INTERPRETER = "js-node"
+  const val JS_CHECKIO_TEST_FORM_TARGET_URL = JS_CHECKIO_URL + CheckiONames.CHECKIO_TEST_FORM_TARGET_PATH + "/"
 }
