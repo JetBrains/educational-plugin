be6e9d398da03f77973f3d92d025253b844d5a01
Ekaterina Smal <ekaterina.smal@jetbrains.com>
2023-03-30 16:12:49 +0300

    migrate JsCheckiOCheckListener to kotlin

--- Edu-JavaScript/src/com/jetbrains/edu/javascript/learning/checkio/checker/JsCheckiOCheckListener.kt
+++ Edu-JavaScript/src/com/jetbrains/edu/javascript/learning/checkio/checker/JsCheckiOCheckListener.kt
@@ -1,28 +1,22 @@
-package com.jetbrains.edu.javascript.learning.checkio.checker;
+package com.jetbrains.edu.javascript.learning.checkio.checker
 
-import com.intellij.lang.javascript.JavaScriptFileType;
-import com.intellij.lang.javascript.JavascriptLanguage;
-import com.jetbrains.edu.javascript.learning.checkio.connectors.JsCheckiOApiConnector;
-import com.jetbrains.edu.javascript.learning.checkio.connectors.JsCheckiOOAuthConnector;
-import com.jetbrains.edu.learning.checkio.CheckiOCourseContentGenerator;
-import com.jetbrains.edu.learning.checkio.checker.CheckiOCheckListener;
-import com.jetbrains.edu.learning.checkio.courseFormat.CheckiOCourse;
-import com.jetbrains.edu.learning.courseFormat.ext.CourseExt;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.lang.javascript.JavaScriptFileType
+import com.intellij.lang.javascript.JavascriptLanguage
+import com.jetbrains.edu.javascript.learning.checkio.connectors.JsCheckiOApiConnector
+import com.jetbrains.edu.javascript.learning.checkio.connectors.JsCheckiOOAuthConnector
+import com.jetbrains.edu.learning.checkio.CheckiOCourseContentGenerator
+import com.jetbrains.edu.learning.checkio.checker.CheckiOCheckListener
+import com.jetbrains.edu.learning.checkio.courseFormat.CheckiOCourse
+import com.jetbrains.edu.learning.courseFormat.ext.languageById
 
-public class JsCheckiOCheckListener extends CheckiOCheckListener {
-  public JsCheckiOCheckListener() {
-    super(
-      new CheckiOCourseContentGenerator(
-        JavaScriptFileType.INSTANCE,
-        JsCheckiOApiConnector.INSTANCE
-      ),
-      JsCheckiOOAuthConnector.INSTANCE
-    );
-  }
-
-  @Override
-  protected boolean isEnabledForCourse(@NotNull CheckiOCourse course) {
-    return CourseExt.getLanguageById(course) == JavascriptLanguage.INSTANCE;
+class JsCheckiOCheckListener : CheckiOCheckListener(
+  CheckiOCourseContentGenerator(
+    JavaScriptFileType.INSTANCE,
+    JsCheckiOApiConnector
+  ),
+  JsCheckiOOAuthConnector
+) {
+  override fun isEnabledForCourse(course: CheckiOCourse): Boolean {
+    return course.languageById === JavascriptLanguage.INSTANCE
   }
 }
