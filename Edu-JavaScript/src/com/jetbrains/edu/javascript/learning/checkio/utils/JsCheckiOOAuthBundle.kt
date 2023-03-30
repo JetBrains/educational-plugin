136a593de8e5d91a41347ce7b6f14b355ee987d6
Ekaterina Smal <ekaterina.smal@jetbrains.com>
2023-03-30 16:15:52 +0300

    migrate JsCheckiOOAuthBundle to kotlin

--- Edu-JavaScript/src/com/jetbrains/edu/javascript/learning/checkio/utils/JsCheckiOOAuthBundle.kt
+++ Edu-JavaScript/src/com/jetbrains/edu/javascript/learning/checkio/utils/JsCheckiOOAuthBundle.kt
@@ -1,23 +1,14 @@
-package com.jetbrains.edu.javascript.learning.checkio.utils;
+package com.jetbrains.edu.javascript.learning.checkio.utils
 
-import com.jetbrains.edu.learning.messages.EduPropertiesBundle;
-import org.jetbrains.annotations.NonNls;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.PropertyKey;
+import com.jetbrains.edu.learning.messages.EduPropertiesBundle
+import org.jetbrains.annotations.NonNls
+import org.jetbrains.annotations.PropertyKey
 
-public class JsCheckiOOAuthBundle extends EduPropertiesBundle {
-  @NonNls
-  private static final String BUNDLE_NAME = "checkio.js-checkio-oauth";
+@NonNls
+private const val BUNDLE_NAME = "checkio.js-checkio-oauth"
 
-  private JsCheckiOOAuthBundle() {
-    super(BUNDLE_NAME);
-  }
-
-  public static String value(@NotNull @PropertyKey(resourceBundle = BUNDLE_NAME) String key) {
-    return InstanceHolder.INSTANCE.valueOrEmpty(key);
-  }
-
-  private static class InstanceHolder {
-    private static final JsCheckiOOAuthBundle INSTANCE = new JsCheckiOOAuthBundle();
+object JsCheckiOOAuthBundle : EduPropertiesBundle(BUNDLE_NAME) {
+  fun value(key: @PropertyKey(resourceBundle = BUNDLE_NAME) String): String {
+    return valueOrEmpty(key)
   }
 }
