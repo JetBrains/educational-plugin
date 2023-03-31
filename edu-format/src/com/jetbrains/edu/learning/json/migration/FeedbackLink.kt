eef4cdd64f02f78d5eea1dcfc58840ca2687a56d
Ekaterina Smal <ekaterina.smal@jetbrains.com>
2023-03-31 12:42:12 +0300

    convert FeedbackLink to kotlin

--- edu-format/src/com/jetbrains/edu/learning/json/migration/FeedbackLink.kt
+++ edu-format/src/com/jetbrains/edu/learning/json/migration/FeedbackLink.kt
@@ -1,38 +1,13 @@
-package com.jetbrains.edu.learning.json.migration;
+package com.jetbrains.edu.learning.json.migration
 
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+class FeedbackLink {
+  var type: LinkType = LinkType.STEPIK
+  var link: String? = null
 
-public class FeedbackLink {
-  @NotNull
-  private LinkType myType;
-
-  @Nullable
-  private String myLink;
-
-  public FeedbackLink() {
-    myType = LinkType.STEPIK;
-  }
-
-  @NotNull
-  public LinkType getType() {
-    return myType;
-  }
-
-  public void setType(@NotNull LinkType type) {
-    myType = type;
-  }
-
-  @Nullable
-  public String getLink() {
-    return myLink;
-  }
-
-  public void setLink(@Nullable String link) {
-    myLink = link;
-  }
-
-  public enum LinkType {
-    MARKETPLACE, STEPIK, CUSTOM, NONE
+  enum class LinkType {
+    MARKETPLACE,
+    STEPIK,
+    CUSTOM,
+    NONE
   }
 }
