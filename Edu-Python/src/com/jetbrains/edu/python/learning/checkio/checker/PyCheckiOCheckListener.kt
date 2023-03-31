45658138ae988854b15b7d7640133b50f29572ed
Ekaterina Smal <ekaterina.smal@jetbrains.com>
2023-03-31 17:20:45 +0300

    convert PyCheckiOCheckListener to kotlin

--- Edu-Python/src/com/jetbrains/edu/python/learning/checkio/checker/PyCheckiOCheckListener.kt
+++ Edu-Python/src/com/jetbrains/edu/python/learning/checkio/checker/PyCheckiOCheckListener.kt
@@ -1,28 +1,22 @@
-package com.jetbrains.edu.python.learning.checkio.checker;
+package com.jetbrains.edu.python.learning.checkio.checker
 
-import com.jetbrains.edu.learning.checkio.CheckiOCourseContentGenerator;
-import com.jetbrains.edu.learning.checkio.checker.CheckiOCheckListener;
-import com.jetbrains.edu.learning.checkio.courseFormat.CheckiOCourse;
-import com.jetbrains.edu.learning.courseFormat.ext.CourseExt;
-import com.jetbrains.edu.python.learning.checkio.connectors.PyCheckiOApiConnector;
-import com.jetbrains.edu.python.learning.checkio.connectors.PyCheckiOOAuthConnector;
-import com.jetbrains.python.PythonFileType;
-import com.jetbrains.python.PythonLanguage;
-import org.jetbrains.annotations.NotNull;
+import com.jetbrains.edu.learning.checkio.CheckiOCourseContentGenerator
+import com.jetbrains.edu.learning.checkio.checker.CheckiOCheckListener
+import com.jetbrains.edu.learning.checkio.courseFormat.CheckiOCourse
+import com.jetbrains.edu.learning.courseFormat.ext.languageById
+import com.jetbrains.edu.python.learning.checkio.connectors.PyCheckiOApiConnector
+import com.jetbrains.edu.python.learning.checkio.connectors.PyCheckiOOAuthConnector
+import com.jetbrains.python.PythonFileType
+import com.jetbrains.python.PythonLanguage
 
-public class PyCheckiOCheckListener extends CheckiOCheckListener {
-  public PyCheckiOCheckListener() {
-    super(
-      new CheckiOCourseContentGenerator(
-        PythonFileType.INSTANCE,
-        PyCheckiOApiConnector.INSTANCE
-      ),
-      PyCheckiOOAuthConnector.INSTANCE
-    );
-  }
-
-  @Override
-  protected boolean isEnabledForCourse(@NotNull CheckiOCourse course) {
-    return PythonLanguage.INSTANCE == CourseExt.getLanguageById(course);
+class PyCheckiOCheckListener : CheckiOCheckListener(
+  CheckiOCourseContentGenerator(
+    PythonFileType.INSTANCE,
+    PyCheckiOApiConnector
+  ),
+  PyCheckiOOAuthConnector
+) {
+  override fun isEnabledForCourse(course: CheckiOCourse): Boolean {
+    return PythonLanguage.INSTANCE === course.languageById
   }
 }
