982669e5947a16f2a4c68edc0691bd4e80bef5a2
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2023-04-11 17:34:27 +0200

    EDU-5814: migrate `Solve All Tasks Before` dialog to Kotlin UI DSL v2

--- educational-core/src/com/jetbrains/edu/coursecreator/ui/SelectTaskUi.kt
+++ educational-core/src/com/jetbrains/edu/coursecreator/ui/SelectTaskUi.kt
@@ -1,9 +1,10 @@
 package com.jetbrains.edu.coursecreator.ui
 
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.ui.ComboBox
 import com.intellij.openapi.ui.DialogWrapper
-import com.intellij.ui.layout.*
+import com.intellij.ui.dsl.builder.bindItemNullable
+import com.intellij.ui.dsl.builder.panel
+import com.intellij.ui.dsl.gridLayout.HorizontalAlign
 import com.intellij.util.ui.JBUI
 import com.jetbrains.edu.learning.courseFormat.EduCourse
 import com.jetbrains.edu.learning.courseFormat.ext.allTasks
@@ -46,40 +47,54 @@ private class SelectTaskDialogUI : SelectTaskUi {
   override fun selectTask(project: Project, course: EduCourse): Task? = SelectTaskDialog(project, course).showAndGetSelectedTask()
 }
 
-private class SelectTaskDialog(project: Project, course: EduCourse) : DialogWrapper(project) {
+private class SelectTaskDialog(project: Project, private val course: EduCourse) : DialogWrapper(project) {
 
-  private val tasks: ComboBox<Task> = ComboBox(course.allTasks.toTypedArray())
+  private var selectedTask: Task? = null
 
   init {
     title = EduCoreBundle.message("course.creator.select.task.dialog.title")
-    tasks.renderer = object : DefaultListCellRenderer() {
-      override fun getListCellRendererComponent(
-        list: JList<*>,
-        value: Any?,
-        index: Int,
-        isSelected: Boolean,
-        cellHasFocus: Boolean
-      ): Component {
-        val component = super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus)
-        if (component is JLabel && value is Task) {
-          component.text = value.fullName
-        }
-        return component
-      }
-    }
     super.init()
   }
 
   override fun createCenterPanel(): JComponent {
     val panel = panel {
-      row { tasks(CCFlags.growX) }
+      row {
+        val tasks = course.allTasks
+        selectedTask = tasks.firstOrNull()
+        // BACKCOMPAT: 2022.2.
+        // Use `align(AlignX.FILL)` instead of `horizontalAlign(HorizontalAlign.FILL)`
+        //
+        // BACKCOMPAT: 2022.3.
+        // Use `bindItem` instead of `bindItemNullable`
+        @Suppress("UnstableApiUsage", "DEPRECATION")
+        comboBox(tasks, TaskRenderer())
+          .horizontalAlign(HorizontalAlign.FILL)
+          .focused()
+          .bindItemNullable(::selectedTask)
+      }
     }
     panel.minimumSize = JBUI.size(400, 80)
     return panel
   }
 
   fun showAndGetSelectedTask(): Task? {
-    return if (showAndGet()) tasks.selectedItem as Task else null
+    return if (showAndGet()) selectedTask else null
+  }
+
+  private class TaskRenderer : DefaultListCellRenderer() {
+    override fun getListCellRendererComponent(
+      list: JList<*>,
+      value: Any?,
+      index: Int,
+      isSelected: Boolean,
+      cellHasFocus: Boolean
+    ): Component {
+      val component = super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus)
+      if (component is JLabel && value is Task) {
+        component.text = value.fullName
+      }
+      return component
+    }
   }
 }
 
