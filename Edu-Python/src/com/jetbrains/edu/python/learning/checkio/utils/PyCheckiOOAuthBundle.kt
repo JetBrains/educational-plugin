1290adc55c09a9e6bb2e3a4ece58a8bf45aa9d80
Ekaterina Smal <ekaterina.smal@jetbrains.com>
2023-05-05 11:30:02 +0300

    PyCheckiOOAuthBundle to kotlin

--- Edu-Python/src/com/jetbrains/edu/python/learning/checkio/utils/PyCheckiOOAuthBundle.kt
+++ Edu-Python/src/com/jetbrains/edu/python/learning/checkio/utils/PyCheckiOOAuthBundle.kt
@@ -1,23 +1,14 @@
-package com.jetbrains.edu.python.learning.checkio.utils;
+package com.jetbrains.edu.python.learning.checkio.utils
 
-import com.jetbrains.edu.learning.messages.EduPropertiesBundle;
-import org.jetbrains.annotations.NonNls;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.PropertyKey;
+import com.jetbrains.edu.learning.messages.EduPropertiesBundle
+import org.jetbrains.annotations.NonNls
+import org.jetbrains.annotations.PropertyKey
 
-public class PyCheckiOOAuthBundle extends EduPropertiesBundle {
-  @NonNls
-  private static final String BUNDLE_NAME = "checkio.py-checkio-oauth";
+@NonNls
+private const val BUNDLE = "checkio.py-checkio-oauth"
 
-  private PyCheckiOOAuthBundle() {
-    super(BUNDLE_NAME);
-  }
-
-  public static String value(@NotNull @PropertyKey(resourceBundle = BUNDLE_NAME) String key) {
-    return InstanceHolder.INSTANCE.valueOrEmpty(key);
-  }
-
-  private static class InstanceHolder {
-    private static final PyCheckiOOAuthBundle INSTANCE = new PyCheckiOOAuthBundle();
+object PyCheckiOOAuthBundle : EduPropertiesBundle(BUNDLE) {
+  fun value(@PropertyKey(resourceBundle = BUNDLE) key: String): String {
+    return valueOrEmpty(key)
   }
 }
