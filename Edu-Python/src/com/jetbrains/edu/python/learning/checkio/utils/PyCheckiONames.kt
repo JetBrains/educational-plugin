38f3cebc1a6fc3c7a0ad4defc3ef865ed85ee1cb
Ekaterina Smal <ekaterina.smal@jetbrains.com>
2023-05-05 11:28:40 +0300

    PyCheckiONames to kotlin

--- Edu-Python/src/com/jetbrains/edu/python/learning/checkio/utils/PyCheckiONames.kt
+++ Edu-Python/src/com/jetbrains/edu/python/learning/checkio/utils/PyCheckiONames.kt
@@ -1,14 +1,9 @@
-package com.jetbrains.edu.python.learning.checkio.utils;
+package com.jetbrains.edu.python.learning.checkio.utils
 
-import static com.jetbrains.edu.learning.checkio.utils.CheckiONames.CHECKIO_HOST;
-import static com.jetbrains.edu.learning.checkio.utils.CheckiONames.CHECKIO_TEST_FORM_TARGET_PATH;
+import com.jetbrains.edu.learning.checkio.utils.CheckiONames
 
-public final class PyCheckiONames {
-  private PyCheckiONames() {}
-
-  public static final String PY_CHECKIO_URL = "https://py." + CHECKIO_HOST;
-
-  public static final String PY_CHECKIO_INTERPRETER = "python-3";
-
-  public static final String PY_CHECKIO_TEST_FORM_TARGET_URL = PY_CHECKIO_URL + CHECKIO_TEST_FORM_TARGET_PATH + "/";
+object PyCheckiONames {
+  const val PY_CHECKIO_URL = "https://py.${CheckiONames.CHECKIO_HOST}"
+  const val PY_CHECKIO_INTERPRETER = "python-3"
+  const val PY_CHECKIO_TEST_FORM_TARGET_URL = "$PY_CHECKIO_URL${CheckiONames.CHECKIO_TEST_FORM_TARGET_PATH}/"
 }
