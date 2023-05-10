cf709975ac85d53753efe8cb9e5a29b079801cbb
Ekaterina Smal <ekaterina.smal@jetbrains.com>
2023-05-10 13:42:29 +0300

    PyRunTestsConfigurationProducer to kotlin

--- Edu-Python/src/com/jetbrains/edu/python/learning/run/PyRunTestsConfigurationProducer.kt
+++ Edu-Python/src/com/jetbrains/edu/python/learning/run/PyRunTestsConfigurationProducer.kt
@@ -1,98 +1,65 @@
-package com.jetbrains.edu.python.learning.run;
+package com.jetbrains.edu.python.learning.run
 
-import com.intellij.execution.Location;
-import com.intellij.execution.actions.ConfigurationContext;
-import com.intellij.execution.actions.LazyRunConfigurationProducer;
-import com.intellij.execution.configurations.ConfigurationFactory;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.Ref;
-import com.intellij.openapi.util.io.FileUtil;
-import com.intellij.openapi.vfs.LocalFileSystem;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.PsiElement;
-import com.jetbrains.edu.learning.EduNames;
-import com.jetbrains.edu.learning.OpenApiExtKt;
-import com.jetbrains.edu.learning.VirtualFileExt;
-import com.jetbrains.edu.learning.courseFormat.ext.StudyItemExtKt;
-import com.jetbrains.edu.learning.courseFormat.tasks.Task;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.execution.actions.ConfigurationContext
+import com.intellij.execution.actions.LazyRunConfigurationProducer
+import com.intellij.execution.configurations.ConfigurationFactory
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Ref
+import com.intellij.openapi.util.io.FileUtil
+import com.intellij.openapi.vfs.LocalFileSystem
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiElement
+import com.jetbrains.edu.learning.EduNames
+import com.jetbrains.edu.learning.courseDir
+import com.jetbrains.edu.learning.courseFormat.ext.getDir
+import com.jetbrains.edu.learning.getContainingTask
+import com.jetbrains.edu.python.learning.PyConfigurator
 
-import static com.jetbrains.edu.python.learning.PyConfigurator.TESTS_PY;
-
-public class PyRunTestsConfigurationProducer extends LazyRunConfigurationProducer<PyCCRunTestConfiguration> {
-
-  @Override
-  protected boolean setupConfigurationFromContext(@NotNull PyCCRunTestConfiguration configuration,
-                                                  ConfigurationContext context,
-                                                  @NotNull Ref<PsiElement> sourceElement) {
-    Project project = context.getProject();
-    String testsPath = getTestPath(context);
-    if (testsPath == null) {
-      return false;
-    }
-    VirtualFile testsFile = LocalFileSystem.getInstance().findFileByPath(testsPath);
-    if (testsFile == null) {
-      return false;
-    }
-
-    String generatedName = generateName(testsFile, project);
-    if (generatedName == null) {
-      return false;
-    }
-
-    configuration.setPathToTest(testsPath);
-    configuration.setName(generatedName);
-    configuration.setScriptName(testsFile.getPath());
-    return true;
+class PyRunTestsConfigurationProducer : LazyRunConfigurationProducer<PyCCRunTestConfiguration>() {
+  override fun setupConfigurationFromContext(
+    configuration: PyCCRunTestConfiguration,
+    context: ConfigurationContext,
+    sourceElement: Ref<PsiElement>
+  ): Boolean {
+    val testsPath = getTestPath(context) ?: return false
+    val testsFile = LocalFileSystem.getInstance().findFileByPath(testsPath) ?: return false
+    val generatedName = generateName(testsFile, context.project) ?: return false
+    configuration.pathToTest = testsPath
+    configuration.name = generatedName
+    configuration.scriptName = testsFile.path
+    return true
   }
 
-  @NotNull
-  @Override
-  public ConfigurationFactory getConfigurationFactory() {
-    return PyRunTestsConfigurationType.getInstance().getConfigurationFactories()[0];
+  override fun getConfigurationFactory(): ConfigurationFactory {
+    return PyRunTestsConfigurationType.getInstance().configurationFactories[0]
   }
 
-  @Nullable
-  private static String generateName(@NotNull VirtualFile testsFile, @NotNull Project project) {
-    Task task = VirtualFileExt.getContainingTask(testsFile, project);
-    if (task == null) {
-      return null;
-    }
-    return task.getLesson().getName() + "/" + task.getName();
+  override fun isConfigurationFromContext(configuration: PyCCRunTestConfiguration, context: ConfigurationContext): Boolean {
+    val path = getTestPath(context)
+    return path != null && path == configuration.pathToTest
   }
 
-  @Nullable
-  private static String getTestPath(@NotNull ConfigurationContext context) {
-    Location<?> location = context.getLocation();
-    if (location == null) {
-      return null;
+  companion object {
+    private fun generateName(testsFile: VirtualFile, project: Project): String? {
+      val task = testsFile.getContainingTask(project) ?: return null
+      return FileUtil.join(task.lesson.name, task.name)
     }
-    VirtualFile file = location.getVirtualFile();
-    if (file == null) {
-      return null;
-    }
-    Project project = location.getProject();
-    Task task = VirtualFileExt.getContainingTask(file, project);
-    if (task == null) {
-      return null;
-    }
-    VirtualFile taskDir = StudyItemExtKt.getDir(task, OpenApiExtKt.getCourseDir(project));
-    if (taskDir == null) {
-      return null;
-    }
-    String testsFileName = TESTS_PY;
-    String taskDirPath = FileUtil.toSystemDependentName(taskDir.getPath());
-    String testsPath = taskDir.findChild(EduNames.SRC) != null ?
-                       FileUtil.join(taskDirPath, EduNames.SRC, testsFileName) :
-                       FileUtil.join(taskDirPath, testsFileName);
-    String filePath = FileUtil.toSystemDependentName(file.getPath());
-    return filePath.equals(testsPath) ? testsPath : null;
-  }
 
-  @Override
-  public boolean isConfigurationFromContext(@NotNull PyCCRunTestConfiguration configuration, @NotNull ConfigurationContext context) {
-    String path = getTestPath(context);
-    return path != null && path.equals(configuration.getPathToTest());
+    private fun getTestPath(context: ConfigurationContext): String? {
+      val location = context.location ?: return null
+      val file = location.virtualFile ?: return null
+      val project = location.project
+      val task = file.getContainingTask(project) ?: return null
+      val taskDir = task.getDir(project.courseDir) ?: return null
+      val taskDirPath = FileUtil.toSystemDependentName(taskDir.path)
+      val testsPath = if (taskDir.findChild(EduNames.SRC) != null) {
+        FileUtil.join(taskDirPath, EduNames.SRC, PyConfigurator.TESTS_PY)
+      }
+      else {
+        FileUtil.join(taskDirPath, PyConfigurator.TESTS_PY)
+      }
+      val filePath = FileUtil.toSystemDependentName(file.path)
+      return if (filePath == testsPath) testsPath else null
+    }
   }
 }
