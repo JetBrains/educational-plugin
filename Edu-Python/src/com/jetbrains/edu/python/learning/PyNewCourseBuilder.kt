c5f55f7c7cb6697e7d582c5d57fcd4c744b37249
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2023-05-10 15:33:41 +0200

    EDU-5905: provide default settings for python courses

--- Edu-Python/src/com/jetbrains/edu/python/learning/PyNewCourseBuilder.kt
+++ Edu-Python/src/com/jetbrains/edu/python/learning/PyNewCourseBuilder.kt
@@ -1,10 +1,9 @@
 package com.jetbrains.edu.python.learning
 
+import com.intellij.openapi.projectRoots.impl.ProjectJdkImpl
 import com.jetbrains.edu.coursecreator.actions.TemplateFileInfo
 import com.jetbrains.edu.coursecreator.actions.studyItem.NewStudyItemInfo
-import com.jetbrains.edu.learning.CourseInfoHolder
-import com.jetbrains.edu.learning.EduCourseBuilder
-import com.jetbrains.edu.learning.LanguageSettings
+import com.jetbrains.edu.learning.*
 import com.jetbrains.edu.learning.courseFormat.Course
 import com.jetbrains.edu.learning.newproject.CourseProjectGenerator
 import com.jetbrains.edu.python.learning.PyConfigurator.Companion.MAIN_PY
@@ -12,9 +11,11 @@ import com.jetbrains.edu.python.learning.PyConfigurator.Companion.TASK_PY
 import com.jetbrains.edu.python.learning.PyNewConfigurator.Companion.TEST_FILE_NAME
 import com.jetbrains.edu.python.learning.PyNewConfigurator.Companion.TEST_FOLDER
 import com.jetbrains.edu.python.learning.newproject.PyCourseProjectGenerator
+import com.jetbrains.edu.python.learning.newproject.PyFakeSdkType
 import com.jetbrains.edu.python.learning.newproject.PyLanguageSettings
 import com.jetbrains.edu.python.learning.newproject.PyProjectSettings
 import com.jetbrains.python.PyNames
+import com.jetbrains.python.sdk.flavors.PythonSdkFlavor
 
 class PyNewCourseBuilder : EduCourseBuilder<PyProjectSettings> {
   override val taskTemplateName: String = TASK_PY
@@ -22,6 +23,16 @@ class PyNewCourseBuilder : EduCourseBuilder<PyProjectSettings> {
   override val testTemplateName: String = TEST_FILE_NAME
 
   override fun getLanguageSettings(): LanguageSettings<PyProjectSettings> = PyLanguageSettings()
+
+  override fun getDefaultSettings(): Result<PyProjectSettings, String> {
+    val sdkPath = System.getProperty(INTERPRETER_PROPERTY)
+      ?: return Err("Failed to find Python interpreter because `$INTERPRETER_PROPERTY` system property is not provided")
+
+    val versionString = PythonSdkFlavor.getApplicableFlavors(false)[0].getVersionString(sdkPath)
+
+    val sdk = ProjectJdkImpl(versionString, PyFakeSdkType, sdkPath, versionString)
+    return Ok(PyProjectSettings(sdk))
+  }
 
   override fun getSupportedLanguageVersions(): List<String> = getSupprotedVersions()
 
@@ -45,5 +56,9 @@ class PyNewCourseBuilder : EduCourseBuilder<PyProjectSettings> {
       templates += TemplateFileInfo(PyNames.INIT_DOT_PY, "$TEST_FOLDER/${PyNames.INIT_DOT_PY}", false)
     }
     return templates
+  }
+
+  companion object {
+    private const val INTERPRETER_PROPERTY = "project.python.interpreter"
   }
 }
