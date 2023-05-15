1fc512ffe2ef7b3701013f985b27add2b2f7775a
Ekaterina Smal <ekaterina.smal@jetbrains.com>
2023-05-15 14:13:51 +0300

    PyEduInspectionExtension to kotlin

--- Edu-Python/src/com/jetbrains/edu/python/learning/highlighting/PyEduInspectionExtension.kt
+++ Edu-Python/src/com/jetbrains/edu/python/learning/highlighting/PyEduInspectionExtension.kt
@@ -1,37 +1,26 @@
-package com.jetbrains.edu.python.learning.highlighting;
+package com.jetbrains.edu.python.learning.highlighting
 
-import com.intellij.openapi.project.Project;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiReference;
-import com.intellij.psi.util.PsiTreeUtil;
-import com.jetbrains.edu.learning.StudyTaskManager;
-import com.jetbrains.edu.learning.VirtualFileExt;
-import com.jetbrains.edu.learning.courseFormat.EduFileErrorHighlightLevel;
-import com.jetbrains.edu.learning.courseFormat.TaskFile;
-import com.jetbrains.python.inspections.PyInspectionExtension;
-import com.jetbrains.python.psi.PyElement;
-import com.jetbrains.python.psi.PyImportStatementBase;
-import com.jetbrains.python.psi.types.TypeEvalContext;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.psi.PsiReference
+import com.intellij.psi.util.PsiTreeUtil
+import com.jetbrains.edu.learning.StudyTaskManager
+import com.jetbrains.edu.learning.courseFormat.EduFileErrorHighlightLevel
+import com.jetbrains.edu.learning.getTaskFile
+import com.jetbrains.python.inspections.PyInspectionExtension
+import com.jetbrains.python.psi.PyElement
+import com.jetbrains.python.psi.PyImportStatementBase
+import com.jetbrains.python.psi.types.TypeEvalContext
 
-public class PyEduInspectionExtension extends PyInspectionExtension {
-
-  @Override
-  public boolean ignoreUnresolvedReference(@NotNull PyElement element, @NotNull PsiReference reference, @NotNull TypeEvalContext context) {
-    final PsiFile file = element.getContainingFile();
-    final Project project = file.getProject();
-
-    if (StudyTaskManager.getInstance(project).getCourse() == null) {
-      return false;
+class PyEduInspectionExtension : PyInspectionExtension() {
+  override fun ignoreUnresolvedReference(element: PyElement, reference: PsiReference, context: TypeEvalContext): Boolean {
+    val file = element.containingFile
+    val project = file.project
+    if (StudyTaskManager.getInstance(project).course == null) {
+      return false
     }
-    TaskFile taskFile = VirtualFileExt.getTaskFile(file.getVirtualFile(), project);
-    if (taskFile == null || taskFile.getErrorHighlightLevel() != EduFileErrorHighlightLevel.TEMPORARY_SUPPRESSION) {
-      return false;
+    val taskFile = file.virtualFile.getTaskFile(project)
+    if (taskFile == null || taskFile.errorHighlightLevel != EduFileErrorHighlightLevel.TEMPORARY_SUPPRESSION) {
+      return false
     }
-    if (PsiTreeUtil.getParentOfType(element, PyImportStatementBase.class) != null) {
-      return false;
-    }
-    return true;
+    return PsiTreeUtil.getParentOfType(element, PyImportStatementBase::class.java) == null
   }
-
 }
