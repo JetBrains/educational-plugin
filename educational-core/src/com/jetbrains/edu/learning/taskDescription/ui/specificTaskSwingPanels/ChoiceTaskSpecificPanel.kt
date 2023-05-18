bc9a9320282c79091ea1ebc946200558ae33e42e
Gleb Oborin <Gleb.Oborin@jetbrains.com>
2023-05-18 16:13:53 +0200

    EDU-5907: Rewrite choice task swing panel to UI DSL

--- educational-core/src/com/jetbrains/edu/learning/taskDescription/ui/specificTaskSwingPanels/ChoiceTaskSpecificPanel.kt
+++ educational-core/src/com/jetbrains/edu/learning/taskDescription/ui/specificTaskSwingPanels/ChoiceTaskSpecificPanel.kt
@@ -1,64 +1,62 @@
 package com.jetbrains.edu.learning.taskDescription.ui.specificTaskSwingPanels
 
-import com.intellij.openapi.ui.VerticalFlowLayout
-import com.intellij.ui.components.panels.NonOpaquePanel
 import com.intellij.ui.components.panels.Wrapper
+import com.intellij.ui.dsl.builder.*
+import com.intellij.ui.dsl.gridLayout.Gaps
 import com.intellij.util.ui.JBUI
 import com.jetbrains.edu.learning.courseFormat.CheckStatus
 import com.jetbrains.edu.learning.courseFormat.tasks.choice.ChoiceTask
+import com.jetbrains.edu.learning.taskDescription.ui.addBorder
 import java.awt.event.ItemEvent
 import java.awt.event.ItemListener
 import javax.swing.*
 
 class ChoiceTaskSpecificPanel(task: ChoiceTask) : Wrapper() {
   init {
-    val panel = NonOpaquePanel(VerticalFlowLayout())
-      .apply { border = JBUI.Borders.empty(15, 0, 10, 10) }
-      .addBox(task, task.isMultipleChoice)
+    val panel = panel {
+      buttonsGroup {
+        row {
+          text(task.presentableQuizHeader).apply { isOpaque = false }
+        }
+        for (index in task.choiceOptions.indices) {
+          optionRow(task, index)
+            .layout(RowLayout.PARENT_GRID)
+        }
+      }
+    }.apply {
+      addBorder(JBUI.Borders.empty(15, 0, 10, 10))
+      isOpaque = false
+    }
     setContent(panel)
   }
 
-  private fun NonOpaquePanel.addBox(task: ChoiceTask, isMultipleChoice: Boolean): NonOpaquePanel {
-    if (isMultipleChoice) {
-      addSpecificBox(task) { JCheckBox(it) }
-    }
-    else {
-      addSpecificBox(task, ButtonGroup()) { JRadioButton(it) }
-    }
-    return this
-  }
-
-  private fun <Button : JToggleButton> NonOpaquePanel.addSpecificBox(
+  private fun Panel.optionRow(
     task: ChoiceTask,
-    group: ButtonGroup? = null,
-    createBox: (str: String) -> Button
-  ) {
-    createTopicForSpecificPanel(task.presentableQuizHeader)
-    val isEnabled = task.status != CheckStatus.Failed
-    for ((index, option) in task.choiceOptions.withIndex()) {
-      val box = createBox(option.text).createButton(index, task, enabled = isEnabled, opaque = false, group)
-      add(box)
+    index: Int
+  ): Row = row {
+    val choiceOption = task.choiceOptions[index]
+    val areOptionsEnabled = task.status != CheckStatus.Failed
+    val isMultipleChoice = task.isMultipleChoice
+    chooseButton(isMultipleChoice)
+      .widthGroup("buttonsGroup")
+      .enabled(areOptionsEnabled)
+      .customize(Gaps(right = 8))
+      .component
+      .apply {
+        isSelected = task.selectedVariants.contains(index)
+        addItemListener(createListener(task, index))
+        isOpaque = false
+      }
+    text(choiceOption.text)
+      .resizableColumn()
+  }
+
+  private fun Row.chooseButton(isMultipleChoice: Boolean): Cell<JToggleButton> {
+    return if (isMultipleChoice) {
+      checkBox("")
+    } else {
+      radioButton("")
     }
-  }
-
-  private fun NonOpaquePanel.createTopicForSpecificPanel(message: String) {
-    val text = JLabel(message, SwingConstants.LEFT).apply { isOpaque = false }
-    add(text)
-  }
-
-  private fun <Button : JToggleButton> Button.createButton(
-    index: Int,
-    task: ChoiceTask,
-    enabled: Boolean,
-    opaque: Boolean,
-    group: ButtonGroup? = null
-  ): Button {
-    isOpaque = opaque
-    isSelected = task.selectedVariants.contains(index)
-    addItemListener(createListener(task, index))
-    isEnabled = enabled
-    group?.add(this)
-    return this
   }
 
   private fun createListener(task: ChoiceTask, index: Int): ItemListener {
