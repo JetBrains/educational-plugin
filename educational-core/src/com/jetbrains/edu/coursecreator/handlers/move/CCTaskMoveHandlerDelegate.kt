218abf89c345ad02761ec0a4c87b466b12ec0eb3
Ekaterina Smal <ekaterina.smal@jetbrains.com>
2023-05-30 12:19:04 +0300

    convert CCTaskMoveHandlerDelegate to kotlin

--- educational-core/src/com/jetbrains/edu/coursecreator/handlers/move/CCTaskMoveHandlerDelegate.kt
+++ educational-core/src/com/jetbrains/edu/coursecreator/handlers/move/CCTaskMoveHandlerDelegate.kt
@@ -1,144 +1,110 @@
-package com.jetbrains.edu.coursecreator.handlers.move;
+package com.jetbrains.edu.coursecreator.handlers.move
 
-import com.intellij.ide.projectView.ProjectView;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.Messages;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.PsiDirectory;
-import com.intellij.psi.PsiElement;
-import com.intellij.refactoring.move.MoveCallback;
-import com.jetbrains.edu.coursecreator.CCUtils;
-import com.jetbrains.edu.learning.StudyTaskManager;
-import com.jetbrains.edu.learning.VirtualFileExt;
-import com.jetbrains.edu.learning.courseFormat.Course;
-import com.jetbrains.edu.learning.courseFormat.Lesson;
-import com.jetbrains.edu.learning.courseFormat.tasks.Task;
-import com.jetbrains.edu.learning.messages.EduCoreBundle;
-import com.jetbrains.edu.learning.yaml.YamlFormatSynchronizer;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.ide.projectView.ProjectView
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.Messages
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiDirectory
+import com.intellij.psi.PsiElement
+import com.intellij.refactoring.move.MoveCallback
+import com.jetbrains.edu.coursecreator.CCUtils.updateHigherElements
+import com.jetbrains.edu.coursecreator.StudyItemType
+import com.jetbrains.edu.learning.*
+import com.jetbrains.edu.learning.courseFormat.Lesson
+import com.jetbrains.edu.learning.courseFormat.tasks.Task
+import com.jetbrains.edu.learning.messages.EduCoreBundle.message
+import com.jetbrains.edu.learning.yaml.YamlFormatSynchronizer.saveItem
+import java.io.IOException
 
-import java.io.IOException;
-import java.util.List;
-
-import static com.jetbrains.edu.coursecreator.StudyItemType.TASK_TYPE;
-
-public class CCTaskMoveHandlerDelegate extends CCStudyItemMoveHandlerDelegate {
-
-  private static final Logger LOG = Logger.getInstance(CCTaskMoveHandlerDelegate.class);
-
-  public CCTaskMoveHandlerDelegate() {
-    super(TASK_TYPE);
+class CCTaskMoveHandlerDelegate : CCStudyItemMoveHandlerDelegate(StudyItemType.TASK_TYPE) {
+  override fun isAvailable(directory: PsiDirectory): Boolean {
+    return directory.virtualFile.isTaskDirectory(directory.project)
   }
 
-  @Override
-  protected boolean isAvailable(@NotNull PsiDirectory directory) {
-    return VirtualFileExt.isTaskDirectory(directory.getVirtualFile(), directory.getProject());
-  }
-
-  @Override
-  public void doMove(final Project project,
-                     PsiElement[] elements,
-                     @Nullable PsiElement targetContainer,
-                     @Nullable MoveCallback callback) {
-    if (!(targetContainer instanceof PsiDirectory)) {
-      return;
+  override fun doMove(
+    project: Project,
+    elements: Array<PsiElement>,
+    targetContainer: PsiElement?,
+    callback: MoveCallback?
+  ) {
+    if (targetContainer !is PsiDirectory) {
+      return
     }
-
-    final VirtualFile targetVFile = ((PsiDirectory)targetContainer).getVirtualFile();
-
-    if (!VirtualFileExt.isTaskDirectory(targetVFile, project) && !VirtualFileExt.isLessonDirectory(targetVFile, project)) {
-      Messages.showInfoMessage(EduCoreBundle.message("dialog.message.incorrect.movement.task"),
-                               EduCoreBundle.message("dialog.title.incorrect.target.for.move"));
-      return;
+    val targetVFile = targetContainer.virtualFile
+    if (!targetVFile.isTaskDirectory(project) && !targetVFile.isLessonDirectory(project)) {
+      Messages.showInfoMessage(
+        message("dialog.message.incorrect.movement.task"),
+        message("dialog.title.incorrect.target.for.move")
+      )
+      return
     }
-    final Course course = StudyTaskManager.getInstance(project).getCourse();
-    final PsiDirectory sourceDirectory = (PsiDirectory)elements[0];
-
-    if (course == null) {
-      return;
-    }
-    final Task taskToMove = VirtualFileExt.getTask(sourceDirectory.getVirtualFile(), project);
-    if (taskToMove == null) {
-      return;
-    }
-
-    Lesson sourceLesson = taskToMove.getLesson();
-    if (VirtualFileExt.isLessonDirectory(targetVFile, project)) {
+    StudyTaskManager.getInstance(project).course ?: return
+    val sourceDirectory = elements[0] as PsiDirectory
+    val taskToMove = sourceDirectory.virtualFile.getTask(project) ?: return
+    val sourceLesson = taskToMove.lesson
+    if (targetVFile.isLessonDirectory(project)) {
       //if user moves task to any lesson, this task is inserted as the last task in this lesson
-      Lesson targetLesson = VirtualFileExt.getLesson(targetVFile, project);
-      if (targetLesson == null) {
-        return;
+      val targetLesson = targetVFile.getLesson(project) ?: return
+      if (targetVFile.findChild(taskToMove.name) != null) {
+        Messages.showInfoMessage(
+          message("dialog.message.task.name.conflict.in.lesson"),
+          message("dialog.title.incorrect.target.for.move")
+        )
+        return
       }
-      if (targetVFile.findChild(taskToMove.getName()) != null) {
-        Messages.showInfoMessage(EduCoreBundle.message("dialog.message.task.name.conflict.in.lesson"),
-                                 EduCoreBundle.message("dialog.title.incorrect.target.for.move"));
-        return;
-      }
-      List<Task> taskList = targetLesson.getTaskList();
-      moveTask(sourceDirectory, taskToMove, taskList.isEmpty() ? null : taskList.get(taskList.size() - 1),
-               1, targetVFile, targetLesson);
-      YamlFormatSynchronizer.saveItem(sourceLesson);
-      YamlFormatSynchronizer.saveItem(targetLesson);
+      val taskList = targetLesson.taskList
+      val targetTask = if (taskList.isEmpty()) null else taskList[taskList.size - 1]
+      moveTask(sourceDirectory, taskToMove, targetTask, 1, targetVFile, targetLesson)
+      saveItem(sourceLesson)
+      saveItem(targetLesson)
     }
     else {
-      VirtualFile lessonDir = targetVFile.getParent();
-      if (lessonDir == null) {
-        return;
-      }
-      Task targetTask = VirtualFileExt.getTask(targetVFile, project);
-      if (targetTask == null) {
-        return;
-      }
-      final Integer delta = getDelta(project, targetTask);
-      if (delta == null) {
-        return;
-      }
-      moveTask(sourceDirectory, taskToMove, targetTask, delta, lessonDir, targetTask.getLesson());
-      YamlFormatSynchronizer.saveItem(sourceLesson);
-      YamlFormatSynchronizer.saveItem(targetTask.getLesson());
+      val lessonDir = targetVFile.parent ?: return
+      val targetTask = targetVFile.getTask(project) ?: return
+      val delta = getDelta(project, targetTask) ?: return
+      moveTask(sourceDirectory, taskToMove, targetTask, delta, lessonDir, targetTask.lesson)
+      saveItem(sourceLesson)
+      saveItem(targetTask.lesson)
     }
-    ProjectView.getInstance(project).refresh();
+    ProjectView.getInstance(project).refresh()
   }
 
-  private void moveTask(@NotNull final PsiDirectory sourceDirectory,
-                        @NotNull final Task taskToMove,
-                        @Nullable Task targetTask,
-                        int indexDelta,
-                        @NotNull final VirtualFile targetDirectory,
-                        @NotNull Lesson targetLesson) {
-    final VirtualFile sourceLessonDir = sourceDirectory.getVirtualFile().getParent();
-    if (sourceLessonDir == null) {
-      return;
-    }
-    CCUtils.updateHigherElements(sourceLessonDir.getChildren(), file -> taskToMove.getLesson().getTask(file.getName()),
-                                 taskToMove.getIndex(),-1);
-
-    final int newItemIndex = targetTask != null ? targetTask.getIndex() + indexDelta : 1;
-    taskToMove.setIndex(-1);
-    taskToMove.getLesson().removeTask(taskToMove);
-    final Lesson finalTargetLesson = targetLesson;
-    CCUtils.updateHigherElements(targetDirectory.getChildren(), file -> finalTargetLesson.getTask(file.getName()), newItemIndex - 1, 1);
-
-    taskToMove.setIndex(newItemIndex);
-    taskToMove.setParent(targetLesson);
-    targetLesson.addTask(taskToMove);
-    targetLesson.sortItems();
-    ApplicationManager.getApplication().runWriteAction(new Runnable() {
-      @Override
-      public void run() {
+  private fun moveTask(
+    sourceDirectory: PsiDirectory,
+    taskToMove: Task,
+    targetTask: Task?,
+    indexDelta: Int,
+    targetDirectory: VirtualFile,
+    targetLesson: Lesson
+  ) {
+    val sourceLessonDir = sourceDirectory.virtualFile.parent ?: return
+    updateHigherElements(sourceLessonDir.children, { file: VirtualFile -> taskToMove.lesson.getTask(file.name) }, taskToMove.index, -1)
+    val newItemIndex = if (targetTask != null) targetTask.index + indexDelta else 1
+    taskToMove.index = -1
+    taskToMove.lesson.removeTask(taskToMove)
+    updateHigherElements(targetDirectory.children, { file: VirtualFile -> targetLesson.getTask(file.name) }, newItemIndex - 1, 1)
+    taskToMove.index = newItemIndex
+    taskToMove.parent = targetLesson
+    targetLesson.addTask(taskToMove)
+    targetLesson.sortItems()
+    ApplicationManager.getApplication().runWriteAction(object : Runnable {
+      override fun run() {
         try {
           //moving file to the same directory leads to exception
-          if (!targetDirectory.equals(sourceLessonDir)) {
-            sourceDirectory.getVirtualFile().move(this, targetDirectory);
+          if (targetDirectory != sourceLessonDir) {
+            sourceDirectory.virtualFile.move(this, targetDirectory)
           }
         }
-        catch (IOException e) {
-          LOG.error(e);
+        catch (e: IOException) {
+          LOG.error(e)
         }
       }
-    });
+    })
+  }
+
+  companion object {
+    private val LOG = Logger.getInstance(CCTaskMoveHandlerDelegate::class.java)
   }
 }
