d06d87e7f4af21e279998ccd3a0ab8c1b044f123
Ekaterina Smal <ekaterina.smal@jetbrains.com>
2023-05-30 10:54:48 +0300

    convert CCHighlightErrorFilter to kotlin

--- educational-core/src/com/jetbrains/edu/coursecreator/CCHighlightErrorFilter.kt
+++ educational-core/src/com/jetbrains/edu/coursecreator/CCHighlightErrorFilter.kt
@@ -1,25 +1,12 @@
-package com.jetbrains.edu.coursecreator;
+package com.jetbrains.edu.coursecreator
 
-import com.intellij.codeInsight.highlighting.HighlightErrorFilter;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.PsiErrorElement;
-import com.intellij.psi.PsiFile;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.codeInsight.highlighting.HighlightErrorFilter
+import com.intellij.psi.PsiErrorElement
 
-public class CCHighlightErrorFilter extends HighlightErrorFilter {
-  @Override
-  public boolean shouldHighlightErrorElement(@NotNull PsiErrorElement element) {
-    PsiFile file = element.getContainingFile();
-    if (file == null) {
-      return true;
-    }
-    VirtualFile virtualFile = file.getVirtualFile();
-    if (virtualFile == null) {
-      return true;
-    }
-    if (virtualFile.getPath().contains(CCUtils.GENERATED_FILES_FOLDER)) {
-      return false;
-    }
-    return true;
+class CCHighlightErrorFilter : HighlightErrorFilter() {
+  override fun shouldHighlightErrorElement(element: PsiErrorElement): Boolean {
+    val file = element.containingFile ?: return true
+    val virtualFile = file.virtualFile ?: return true
+    return !virtualFile.path.contains(CCUtils.GENERATED_FILES_FOLDER)
   }
 }
