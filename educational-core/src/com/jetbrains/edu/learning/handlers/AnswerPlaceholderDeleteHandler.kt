2803923dcf9e9e9cd09c67829a60bcc5a7bcbfad
Ekaterina Smal <ekaterina.smal@jetbrains.com>
2023-06-06 18:28:28 +0300

    convert AnswerPlaceholderDeleteHandler to kotlin

--- educational-core/src/com/jetbrains/edu/learning/handlers/AnswerPlaceholderDeleteHandler.kt
+++ educational-core/src/com/jetbrains/edu/learning/handlers/AnswerPlaceholderDeleteHandler.kt
@@ -1,23 +1,14 @@
-package com.jetbrains.edu.learning.handlers;
+package com.jetbrains.edu.learning.handlers
 
-import com.intellij.codeInsight.hint.HintManager;
-import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.editor.ReadOnlyFragmentModificationException;
-import com.intellij.openapi.editor.actionSystem.ReadonlyFragmentModificationHandler;
-import com.jetbrains.edu.learning.messages.EduCoreBundle;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.codeInsight.hint.HintManager
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.editor.ReadOnlyFragmentModificationException
+import com.intellij.openapi.editor.actionSystem.ReadonlyFragmentModificationHandler
+import com.jetbrains.edu.learning.messages.EduCoreBundle.message
 
-public class AnswerPlaceholderDeleteHandler implements ReadonlyFragmentModificationHandler {
-
-  private final Editor myEditor;
-
-  public AnswerPlaceholderDeleteHandler(@NotNull final Editor editor) {
-    myEditor = editor;
-  }
-
-  @Override
-  public void handle(ReadOnlyFragmentModificationException e) {
-    if (myEditor.isDisposed()) return;
-    HintManager.getInstance().showErrorHint(myEditor, EduCoreBundle.message("notification.text.error.hint.placeholder.delete"));
+class AnswerPlaceholderDeleteHandler(private val editor: Editor) : ReadonlyFragmentModificationHandler {
+  override fun handle(e: ReadOnlyFragmentModificationException) {
+    if (editor.isDisposed) return
+    HintManager.getInstance().showErrorHint(editor, message("notification.text.error.hint.placeholder.delete"))
   }
 }
