cbb9678953ee770f6e7a42c8998e3a8c8389c268
Ekaterina Smal <ekaterina.smal@jetbrains.com>
2023-06-06 18:18:46 +0300

    convert EduHighlightErrorFilter to kotlin

--- educational-core/src/com/jetbrains/edu/learning/editor/EduHighlightErrorFilter.kt
+++ educational-core/src/com/jetbrains/edu/learning/editor/EduHighlightErrorFilter.kt
@@ -1,26 +1,15 @@
-package com.jetbrains.edu.learning.editor;
+package com.jetbrains.edu.learning.editor
 
-import com.intellij.codeInsight.highlighting.HighlightErrorFilter;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.PsiErrorElement;
-import com.intellij.psi.PsiFile;
-import com.jetbrains.edu.learning.VirtualFileExt;
-import com.jetbrains.edu.learning.courseFormat.EduFileErrorHighlightLevel;
-import com.jetbrains.edu.learning.courseFormat.TaskFile;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.codeInsight.highlighting.HighlightErrorFilter
+import com.intellij.psi.PsiErrorElement
+import com.jetbrains.edu.learning.courseFormat.EduFileErrorHighlightLevel
+import com.jetbrains.edu.learning.getTaskFile
 
-public class EduHighlightErrorFilter extends HighlightErrorFilter {
-  @Override
-  public boolean shouldHighlightErrorElement(@NotNull PsiErrorElement element) {
-    PsiFile file = element.getContainingFile();
-    if (file == null) {
-      return true;
-    }
-    VirtualFile virtualFile = file.getVirtualFile();
-    if (virtualFile == null) {
-      return true;
-    }
-    TaskFile taskFile = VirtualFileExt.getTaskFile(virtualFile, element.getProject());
-    return taskFile == null || taskFile.getErrorHighlightLevel() != EduFileErrorHighlightLevel.TEMPORARY_SUPPRESSION;
+class EduHighlightErrorFilter : HighlightErrorFilter() {
+  override fun shouldHighlightErrorElement(element: PsiErrorElement): Boolean {
+    val file = element.containingFile ?: return true
+    val virtualFile = file.virtualFile ?: return true
+    val taskFile = virtualFile.getTaskFile(element.project)
+    return taskFile == null || taskFile.errorHighlightLevel !== EduFileErrorHighlightLevel.TEMPORARY_SUPPRESSION
   }
 }
