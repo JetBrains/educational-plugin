11be6e34cf0561c849c93e10f3295e9949b9ee89
Ekaterina Smal <ekaterina.smal@jetbrains.com>
2023-06-06 17:46:10 +0300

    convert EducationalExtensionPoint to kotlin

--- educational-core/src/com/jetbrains/edu/learning/configuration/EducationalExtensionPoint.kt
+++ educational-core/src/com/jetbrains/edu/learning/configuration/EducationalExtensionPoint.kt
@@ -1,35 +1,32 @@
-package com.jetbrains.edu.learning.configuration;
+package com.jetbrains.edu.learning.configuration
 
-import com.intellij.openapi.extensions.ExtensionPointName;
-import com.intellij.openapi.extensions.RequiredElement;
-import com.intellij.serviceContainer.BaseKeyedLazyInstance;
-import com.intellij.util.xmlb.annotations.Attribute;
-import com.jetbrains.edu.learning.courseFormat.EduFormatNames;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.openapi.extensions.ExtensionPointName
+import com.intellij.openapi.extensions.RequiredElement
+import com.intellij.serviceContainer.BaseKeyedLazyInstance
+import com.intellij.util.xmlb.annotations.Attribute
+import com.jetbrains.edu.learning.courseFormat.EduFormatNames
 
-public class EducationalExtensionPoint<T> extends BaseKeyedLazyInstance<T> {
-  public static final ExtensionPointName<EducationalExtensionPoint<EduConfigurator<?>>> EP_NAME =
-    ExtensionPointName.create("Educational.configurator");
-
+class EducationalExtensionPoint<T> : BaseKeyedLazyInstance<T>() {
   @Attribute("implementationClass")
   @RequiredElement
-  public String implementationClass;
+  var implementationClass: String? = null
 
   @Attribute("language")
   @RequiredElement
-  public String language = "";
+  var language = ""
 
   @Attribute("courseType")
-  public String courseType = EduFormatNames.PYCHARM;
+  var courseType = EduFormatNames.PYCHARM
 
   @Attribute("environment")
-  public String environment = "";
+  var environment = ""
 
   @Attribute("displayName")
-  public String displayName = null;
+  var displayName: String? = null
 
-  @Override
-  protected @Nullable String getImplementationClassName() {
-    return implementationClass;
+  override fun getImplementationClassName(): String? = implementationClass
+
+  companion object {
+    val EP_NAME: ExtensionPointName<EducationalExtensionPoint<EduConfigurator<*>>> = ExtensionPointName.create("Educational.configurator")
   }
 }
