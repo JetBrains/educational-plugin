b1ddec847a639d15908d710484c45075525cab2f
Ekaterina Smal <ekaterina.smal@jetbrains.com>
2023-06-06 17:39:35 +0300

    convert NextPlaceholderAction to kotlin

--- educational-core/src/com/jetbrains/edu/learning/actions/NextPlaceholderAction.kt
+++ educational-core/src/com/jetbrains/edu/learning/actions/NextPlaceholderAction.kt
@@ -1,34 +1,30 @@
-package com.jetbrains.edu.learning.actions;
+package com.jetbrains.edu.learning.actions
 
-import com.jetbrains.edu.learning.courseFormat.AnswerPlaceholder;
-import com.jetbrains.edu.learning.courseFormat.TaskFile;
-import org.jetbrains.annotations.NonNls;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
-import java.util.List;
+import com.intellij.openapi.actionSystem.ActionUpdateThread
+import com.jetbrains.edu.learning.courseFormat.AnswerPlaceholder
+import com.jetbrains.edu.learning.courseFormat.TaskFile
+import org.jetbrains.annotations.NonNls
 
 /**
  * move caret to next answer placeholder
  */
-public class NextPlaceholderAction extends PlaceholderNavigationAction {
-  @NonNls
-  public static final String ACTION_ID = "Educational.NextPlaceholder";
-
-  @Nullable
-  @Override
-  protected AnswerPlaceholder getTargetPlaceholder(@NotNull final TaskFile taskFile, int offset) {
-    final AnswerPlaceholder selectedAnswerPlaceholder = taskFile.getAnswerPlaceholder(offset);
-    final List<AnswerPlaceholder> placeholders = taskFile.getAnswerPlaceholders();
-    int startIndex = selectedAnswerPlaceholder != null ? selectedAnswerPlaceholder.getIndex() + 1 : 0;
-    if (!indexIsValid(startIndex, placeholders)) return null;
-
-    for (AnswerPlaceholder placeholder : placeholders.subList(startIndex, placeholders.size())) {
-      if (placeholder.getOffset() > offset && placeholder.isVisible()) {
-        return placeholder;
+class NextPlaceholderAction : PlaceholderNavigationAction() {
+  override fun getTargetPlaceholder(taskFile: TaskFile, offset: Int): AnswerPlaceholder? {
+    val selectedAnswerPlaceholder = taskFile.getAnswerPlaceholder(offset)
+    val placeholders = taskFile.answerPlaceholders
+    val startIndex = if (selectedAnswerPlaceholder != null) selectedAnswerPlaceholder.index + 1 else 0
+    if (!indexIsValid(startIndex, placeholders)) return null
+    for (placeholder in placeholders.subList(startIndex, placeholders.size)) {
+      if (placeholder.offset > offset && placeholder.isVisible) {
+        return placeholder
       }
     }
+    return null
+  }
 
-    return null;
+  override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.BGT
+
+  companion object {
+    const val ACTION_ID: @NonNls String = "Educational.NextPlaceholder"
   }
 }
