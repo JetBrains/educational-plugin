bdf0a86b8f5875a02b220a556c16a16d2c19fb72
Ekaterina Smal <ekaterina.smal@jetbrains.com>
2023-06-08 12:41:16 +0300

    converted ToFourthVersionJsonStepOptionsConverter to kotlin

--- educational-core/src/com/jetbrains/edu/learning/serialization/converter/json/ToFourthVersionJsonStepOptionsConverter.kt
+++ educational-core/src/com/jetbrains/edu/learning/serialization/converter/json/ToFourthVersionJsonStepOptionsConverter.kt
@@ -1,22 +1,17 @@
-package com.jetbrains.edu.learning.serialization.converter.json;
+package com.jetbrains.edu.learning.serialization.converter.json
 
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.jetbrains.edu.learning.EduNames;
-import com.jetbrains.edu.learning.stepik.StepikNames;
-import org.jetbrains.annotations.NotNull;
+import com.fasterxml.jackson.databind.node.ObjectNode
+import com.jetbrains.edu.learning.EduNames
+import com.jetbrains.edu.learning.serialization.SerializationUtils
+import com.jetbrains.edu.learning.stepik.StepikNames
 
-import static com.jetbrains.edu.learning.serialization.SerializationUtils.Json.TITLE;
-
-public class ToFourthVersionJsonStepOptionsConverter implements JsonStepOptionsConverter {
-
-  @NotNull
-  @Override
-  public ObjectNode convert(@NotNull ObjectNode stepOptionsJson) {
-    if (stepOptionsJson.has(TITLE) &&
-        StepikNames.PYCHARM_ADDITIONAL.equals(stepOptionsJson.get(TITLE).asText())) {
-      stepOptionsJson.remove(TITLE);
-      stepOptionsJson.put(TITLE, EduNames.ADDITIONAL_MATERIALS);
+class ToFourthVersionJsonStepOptionsConverter : JsonStepOptionsConverter {
+  override fun convert(stepOptionsJson: ObjectNode): ObjectNode {
+    if (stepOptionsJson.has(SerializationUtils.Json.TITLE)
+        && StepikNames.PYCHARM_ADDITIONAL == stepOptionsJson[SerializationUtils.Json.TITLE].asText()) {
+      stepOptionsJson.remove(SerializationUtils.Json.TITLE)
+      stepOptionsJson.put(SerializationUtils.Json.TITLE, EduNames.ADDITIONAL_MATERIALS)
     }
-    return stepOptionsJson;
+    return stepOptionsJson
   }
 }
