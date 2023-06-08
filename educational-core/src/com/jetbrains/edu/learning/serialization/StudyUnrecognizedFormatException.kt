9f9219a082c22540577b74ac6489d2d52f7860fd
Ekaterina Smal <ekaterina.smal@jetbrains.com>
2023-06-08 12:12:51 +0300

    converted StudyUnrecognizedFormatException to kotlin

--- educational-core/src/com/jetbrains/edu/learning/serialization/StudyUnrecognizedFormatException.kt
+++ educational-core/src/com/jetbrains/edu/learning/serialization/StudyUnrecognizedFormatException.kt
@@ -1,12 +1,3 @@
-package com.jetbrains.edu.learning.serialization;
+package com.jetbrains.edu.learning.serialization
 
-public class StudyUnrecognizedFormatException extends Exception {
-
-  public StudyUnrecognizedFormatException() {
-    super();
-  }
-
-  public StudyUnrecognizedFormatException(String message) {
-    super(message);
-  }
-}
+class StudyUnrecognizedFormatException : Exception()
