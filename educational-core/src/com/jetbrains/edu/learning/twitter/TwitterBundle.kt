bda304067862bbbde9bad092123817e0e22e7a28
Ekaterina Smal <ekaterina.smal@jetbrains.com>
2023-06-08 12:15:05 +0300

    converted TwitterBundle to kotlin

--- educational-core/src/com/jetbrains/edu/learning/twitter/TwitterBundle.kt
+++ educational-core/src/com/jetbrains/edu/learning/twitter/TwitterBundle.kt
@@ -1,23 +1,14 @@
-package com.jetbrains.edu.learning.twitter;
+package com.jetbrains.edu.learning.twitter
 
-import com.jetbrains.edu.learning.messages.EduPropertiesBundle;
-import org.jetbrains.annotations.NonNls;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.PropertyKey;
+import com.jetbrains.edu.learning.messages.EduPropertiesBundle
+import org.jetbrains.annotations.NonNls
+import org.jetbrains.annotations.PropertyKey
 
-public class TwitterBundle extends EduPropertiesBundle {
-  @NonNls
-  private static final String BUNDLE = "twitter.oauth_twitter";
+@NonNls
+private const val BUNDLE = "twitter.oauth_twitter"
 
-  private TwitterBundle() {
-    super(BUNDLE);
-  }
-
-  public static String value(@NotNull @PropertyKey(resourceBundle = BUNDLE) String key) {
-    return InstanceHolder.INSTANCE.valueOrEmpty(key);
-  }
-
-  private static class InstanceHolder {
-    private static final TwitterBundle INSTANCE = new TwitterBundle();
+object TwitterBundle : EduPropertiesBundle(BUNDLE) {
+  fun value(@PropertyKey(resourceBundle = BUNDLE) key: String): String {
+    return valueOrEmpty(key)
   }
 }
