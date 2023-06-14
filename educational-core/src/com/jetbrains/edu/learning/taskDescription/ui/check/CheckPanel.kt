4cde571575781e338c31fc5e3dbbefc7926827de
Zinaida.Smirnova <Zinaida.Smirnova@jetbrains.com>
2023-06-14 11:53:47 +0400

    EDU-6001 cleanup

--- educational-core/src/com/jetbrains/edu/learning/taskDescription/ui/check/CheckPanel.kt
+++ educational-core/src/com/jetbrains/edu/learning/taskDescription/ui/check/CheckPanel.kt
@@ -78,10 +78,10 @@ class CheckPanel(val project: Project, parentDisposable: Disposable) : JPanel(Bo
     //these options affect paddings
     toolbar.layoutPolicy = ActionToolbar.NOWRAP_LAYOUT_POLICY
     toolbar.adjustTheSameSize(true)
-    toolbar.setTargetComponent(this)
+    toolbar.targetComponent = this
 
     val component = toolbar.component
-    component.border = JBUI.Borders.empty(5, 0, 0, 0)
+    component.border = JBUI.Borders.emptyTop(5)
     return component
   }
 
