1612a42ccff5fe303f995850f848dfe76a2648cb
Ekaterina Smal <ekaterina.smal@jetbrains.com>
2023-06-15 13:37:19 +0300

    converted StepikOptions to kotlin

--- educational-core/src/com/jetbrains/edu/learning/stepik/StepikOptions.kt
+++ educational-core/src/com/jetbrains/edu/learning/stepik/StepikOptions.kt
@@ -1,48 +1,18 @@
-/*
- * Copyright 2000-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package com.jetbrains.edu.learning.stepik;
+package com.jetbrains.edu.learning.stepik
 
-import com.jetbrains.edu.learning.api.EduOAuthCodeFlowConnector;
-import com.jetbrains.edu.learning.settings.OAuthLoginOptions;
-import com.jetbrains.edu.learning.stepik.api.StepikConnector;
-import org.jetbrains.annotations.NotNull;
+import com.jetbrains.edu.learning.EduExperimentalFeatures.CC_HYPERSKILL
+import com.jetbrains.edu.learning.api.EduOAuthCodeFlowConnector
+import com.jetbrains.edu.learning.isFeatureEnabled
+import com.jetbrains.edu.learning.settings.OAuthLoginOptions
+import com.jetbrains.edu.learning.stepik.api.StepikConnector
 
-import static com.jetbrains.edu.learning.EduExperimentalFeatures.CC_HYPERSKILL;
-import static com.jetbrains.edu.learning.ExperimentsKt.isFeatureEnabled;
+class StepikOptions : OAuthLoginOptions<StepikUser>() {
+  override val connector: EduOAuthCodeFlowConnector<StepikUser, *>
+    get() = StepikConnector.getInstance()
 
-public class StepikOptions extends OAuthLoginOptions<StepikUser> {
-  @Override
-  protected @NotNull EduOAuthCodeFlowConnector<StepikUser, ?> getConnector() {
-    return StepikConnector.getInstance();
-  }
+  override fun getDisplayName(): String = StepikNames.STEPIK
 
-  @NotNull
-  @Override
-  public String getDisplayName() {
-    return StepikNames.STEPIK;
-  }
+  override fun profileUrl(account: StepikUser): String = account.profileUrl
 
-  @NotNull
-  @Override
-  protected String profileUrl(@NotNull StepikUser account) {
-    return StepikUtils.getProfileUrl(account);
-  }
-
-  @Override
-  public boolean isAvailable() {
-    return isFeatureEnabled(CC_HYPERSKILL);
-  }
+  override fun isAvailable(): Boolean = isFeatureEnabled(CC_HYPERSKILL)
 }
