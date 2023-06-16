33ef05bc61823120046e04a11350ed1f289c17cb
Aleksey Rostovskiy <aleksey.rostovskiy@jetbrains.com>
2023-06-16 13:59:14 +0200

    fixup! move shellcheck setup to projectGenerator

--- Edu-Shell/src/com/jetbrains/edu/shell/ShellCourseProjectGenerator.kt
+++ Edu-Shell/src/com/jetbrains/edu/shell/ShellCourseProjectGenerator.kt
@@ -1,5 +1,9 @@
 package com.jetbrains.edu.shell
 
+import com.intellij.openapi.application.runReadAction
+import com.intellij.openapi.project.Project
+import com.intellij.sh.shellcheck.ShShellcheckUtil
+import com.intellij.ui.EditorNotifications
 import com.jetbrains.edu.learning.EduCourseBuilder
 import com.jetbrains.edu.learning.courseFormat.Course
 import com.jetbrains.edu.learning.newproject.CourseProjectGenerator
@@ -8,4 +12,17 @@ import com.jetbrains.edu.learning.newproject.EmptyProjectSettings
 class ShellCourseProjectGenerator(
   builder: EduCourseBuilder<EmptyProjectSettings>,
   course: Course
-) : CourseProjectGenerator<EmptyProjectSettings>(builder, course)
+) : CourseProjectGenerator<EmptyProjectSettings>(builder, course) {
+  override fun afterProjectGenerated(project: Project, projectSettings: EmptyProjectSettings) {
+    super.afterProjectGenerated(project, projectSettings)
+
+    val onSuccess = Runnable {
+      runReadAction {
+        if (project.isDisposed) return@runReadAction
+        EditorNotifications.getInstance(project).updateAllNotifications()
+      }
+    }
+    val onFailure = Runnable { }
+    ShShellcheckUtil.download(project, onSuccess, onFailure)
+  }
+}
