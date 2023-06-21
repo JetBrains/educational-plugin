baa9d08e88c597f51205737ea089cfc97849b28d
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2023-06-21 11:23:21 +0200

    EDU-5701: remove all `style` tags when Swing rendering is used
    Swing's `JTextPane` doesn't process `style` tags. Moreover, it shows the content of this tag as plain text.
    So new `CssHtmlTransformer` just removes all `styles` tags from initial html not to show css content for users

--- educational-core/src/com/jetbrains/edu/learning/taskDescription/htmlTransformers/TaskDescriptionTransformer.kt
+++ educational-core/src/com/jetbrains/edu/learning/taskDescription/htmlTransformers/TaskDescriptionTransformer.kt
@@ -3,6 +3,7 @@ package com.jetbrains.edu.learning.taskDescription.htmlTransformers
 import com.jetbrains.edu.learning.taskDescription.htmlTransformers.steps.*
 
 private val TaskDescriptionHtmlTransformer = HtmlTransformer.pipeline(
+  CssHtmlTransformer,
   MediaThemesTransformer,
   ExternalLinkIconsTransformer,
   CodeHighlighter,
---
+++ educational-core/src/com/jetbrains/edu/learning/taskDescription/htmlTransformers/steps/CssHtmlTransformer.kt
@@ -0,0 +1,26 @@
+package com.jetbrains.edu.learning.taskDescription.htmlTransformers.steps
+
+import com.jetbrains.edu.learning.JavaUILibrary
+import com.jetbrains.edu.learning.taskDescription.htmlTransformers.HtmlTransformer
+import com.jetbrains.edu.learning.taskDescription.htmlTransformers.HtmlTransformerContext
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import org.jsoup.select.NodeFilter.FilterResult.CONTINUE
+import org.jsoup.select.NodeFilter.FilterResult.REMOVE
+import javax.swing.JTextPane
+
+/**
+ * Remote all `style` tags from task description if Swing is used as rendering engine
+ * because `style` tags are not processed by [JTextPane]
+ */
+object CssHtmlTransformer : HtmlTransformer {
+  override fun transform(html: Document, context: HtmlTransformerContext): Document {
+    if (context.uiMode != JavaUILibrary.SWING) return html
+
+    html.filter { node, _ ->
+      if (node is Element && node.tagName() == "style") REMOVE else CONTINUE
+    }
+
+    return html
+  }
+}
