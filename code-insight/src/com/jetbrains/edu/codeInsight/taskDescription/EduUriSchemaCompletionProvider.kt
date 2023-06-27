70a4c1cba1a8afac87bd835136366a4088d6d671
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2023-06-27 16:21:48 +0200

    EDU-6024: complete `settings://` and `tool_window://` schemas in task description files

--- code-insight/src/com/jetbrains/edu/codeInsight/taskDescription/EduUriSchemaCompletionProvider.kt
+++ code-insight/src/com/jetbrains/edu/codeInsight/taskDescription/EduUriSchemaCompletionProvider.kt
@@ -11,10 +11,21 @@ import com.jetbrains.edu.learning.taskDescription.ui.TaskDescriptionLinkProtocol
 class EduUriSchemaCompletionProvider : CompletionProvider<CompletionParameters>() {
 
   override fun addCompletions(parameters: CompletionParameters, context: ProcessingContext, result: CompletionResultSet) {
-    result.addElement(LookupElementBuilder.create(TaskDescriptionLinkProtocol.PSI_ELEMENT.protocol)
-                        .withTypeText(EduCodeInsightBundle.message("task.description.completion.psi.schema.description")))
-
-    result.addElement(LookupElementBuilder.create(TaskDescriptionLinkProtocol.COURSE.protocol)
-                        .withTypeText(EduCodeInsightBundle.message("task.description.completion.course.schema.description")))
+    result.addElement(
+      LookupElementBuilder.create(TaskDescriptionLinkProtocol.PSI_ELEMENT.protocol)
+        .withTypeText(EduCodeInsightBundle.message("task.description.completion.psi.schema.description"))
+    )
+    result.addElement(
+      LookupElementBuilder.create(TaskDescriptionLinkProtocol.COURSE.protocol)
+        .withTypeText(EduCodeInsightBundle.message("task.description.completion.course.schema.description"))
+    )
+    result.addElement(
+      LookupElementBuilder.create(TaskDescriptionLinkProtocol.SETTINGS.protocol)
+        .withTypeText(EduCodeInsightBundle.message("task.description.completion.settings.schema.description"))
+    )
+    result.addElement(
+      LookupElementBuilder.create(TaskDescriptionLinkProtocol.TOOL_WINDOW.protocol)
+        .withTypeText(EduCodeInsightBundle.message("task.description.completion.tool.window.schema.description"))
+    )
   }
 }
--- code-insight/testSrc/com/jetbrains/edu/codeInsight/taskDescription/EduUriSchemaCompletionProviderTestBase.kt
+++ code-insight/testSrc/com/jetbrains/edu/codeInsight/taskDescription/EduUriSchemaCompletionProviderTestBase.kt
@@ -3,4 +3,6 @@ package com.jetbrains.edu.codeInsight.taskDescription
 abstract class EduUriSchemaCompletionProviderTestBase : EduTaskDescriptionCompletionTestBase() {
   fun `test course schema`() = doTest("cour<caret>", "course://<caret>")
   fun `test psi_element schema`() = doTest("psi<caret>", "psi_element://<caret>")
+  fun `test settings schema`() = doTest("sett<caret>", "settings://<caret>")
+  fun `test tool window schema`() = doTest("tool<caret>", "tool_window://<caret>")
 }
