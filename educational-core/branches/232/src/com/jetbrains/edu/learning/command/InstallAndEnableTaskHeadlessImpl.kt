641b305276a1f4b7c9eaf646acf22b953916e3d2
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2023-06-28 18:09:08 +0200

    EDU-6019: add `installCoursePlugins` command

---
+++ educational-core/branches/232/src/com/jetbrains/edu/learning/command/InstallAndEnableTaskHeadlessImpl.kt
@@ -0,0 +1,5 @@
+package com.jetbrains.edu.learning.command
+
+// BACKCOMPAT: 2022.3. Inline it
+@Suppress("UnstableApiUsage")
+typealias InstallAndEnableTaskHeadlessImpl = com.intellij.openapi.updateSettings.impl.pluginsAdvertisement.InstallAndEnableTaskHeadlessImpl
---
+++ educational-core/src/com/jetbrains/edu/learning/command/EduCoursePluginInstallerAppStarter.kt
@@ -0,0 +1,44 @@
+package com.jetbrains.edu.learning.command
+
+import com.intellij.ide.plugins.PluginManager
+import com.intellij.openapi.extensions.PluginId
+import com.intellij.openapi.progress.ProgressManager
+import com.jetbrains.edu.learning.courseFormat.Course
+import com.jetbrains.edu.learning.courseFormat.ext.compatibilityProvider
+import kotlin.system.exitProcess
+
+/**
+ * Adds `installCoursePlugins` command for IDE to install all necessary plugins for given course
+ *
+ * Expected usages:
+ * - `installCoursePlugins %/path/to/project/dir% --marketplace %marketplace-course-link%`
+ * - `installCoursePlugins %/path/to/project/dir% --archive %/path/to/course/archive%`
+ */
+class EduCoursePluginInstallerAppStarter : EduAppStarterBase() {
+  @Suppress("OVERRIDE_DEPRECATION")
+  override val commandName: String
+    get() = "installCoursePlugins"
+
+  override fun doMain(course: Course, projectPath: String) {
+    val provider = course.compatibilityProvider
+    if (provider == null) {
+      logErrorAndExit(course.incompatibleCourseMessage())
+    }
+
+    val pluginManager = PluginManager.getInstance()
+    val allRequiredPlugins = provider.requiredPlugins().orEmpty() + course.pluginDependencies
+    val pluginIds = allRequiredPlugins
+      .map { PluginId.getId(it.stringId) }
+      .filterTo(hashSetOf()) { pluginManager.findEnabledPlugin(it) == null }
+
+    LOG.info("Installing: $pluginIds")
+
+    @Suppress("UnstableApiUsage")
+    ProgressManager.getInstance().run(object : InstallAndEnableTaskHeadlessImpl(pluginIds, {}) {
+      override fun onThrowable(error: Throwable) {
+        LOG.error("Failed to install plugins:", error)
+        exitProcess(1)
+      }
+    })
+  }
+}
