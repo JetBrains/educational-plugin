49d9a6a400bafc2acfe538584bf978158b4f7d14
Ekaterina Smal <ekaterina.smal@jetbrains.com>
2023-06-28 13:22:33 +0300

    added tests

---
+++ code-insight/yaml/testSrc/com/jetbrains/edu/yaml/inspections/UnsupportedLanguageVersionInspectionTest.kt
@@ -0,0 +1,45 @@
+package com.jetbrains.edu.yaml.inspections
+
+import com.jetbrains.edu.learning.courseFormat.CourseMode
+
+class UnsupportedLanguageVersionInspectionTest : YamlInspectionsTestBase(UnsupportedLanguageVersionInspection::class) {
+
+  fun `test unsupported version`() {
+    val version = "8"
+    val course = courseWithFiles(courseMode = CourseMode.EDUCATOR) {
+      lesson("lesson1") {
+        eduTask("task1") {}
+      }
+    }.apply { languageVersion = version }
+
+    testHighlighting(
+      course, """
+      |title: Test Course
+      |summary: sum
+      |programming_language: Plain text
+      |programming_language_version: <error descr="Unsupported language 'Plain text', version: '8'">$version</error>
+      |content:
+      |- lesson1
+    """.trimMargin("|")
+    )
+  }
+  fun `test supported version`() {
+    val version = "1.42"
+    val course = courseWithFiles(courseMode = CourseMode.EDUCATOR) {
+      lesson("lesson1") {
+        eduTask("task1") {}
+      }
+    }.apply { languageVersion = version }
+
+    testHighlighting(
+      course, """
+      |title: Test Course
+      |summary: sum
+      |programming_language: Plain text
+      |programming_language_version: $version
+      |content:
+      |- lesson1
+    """.trimMargin("|")
+    )
+  }
+}
