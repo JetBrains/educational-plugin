516d0300902ee1a1f8ae7d131df9fb133713587a
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2023-06-29 10:16:23 +0200

    Update gradle intellij plugin to 1.14.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -123,7 +123,7 @@ val isTeamCity: Boolean get() = System.getenv("TEAMCITY_VERSION") != null
 plugins {
   idea
   kotlin("jvm") version "1.8.0"
-  id("org.jetbrains.intellij") version "1.14.1"
+  id("org.jetbrains.intellij") version "1.14.2"
   id("de.undercouch.download") version "5.3.0"
   id("net.saliman.properties") version "1.5.2"
   id("org.gradle.test-retry") version "1.5.1"
