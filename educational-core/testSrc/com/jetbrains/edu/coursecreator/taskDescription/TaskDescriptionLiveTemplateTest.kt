52a0238bce07c71e239ea06fdc3fac627b8e2c34
Sviatoslav Naiden <sviatoslav.naiden@jetbrains.com>
2023-07-11 10:48:38 +0200

    switch to 232 by default

--- educational-core/testSrc/com/jetbrains/edu/coursecreator/taskDescription/TaskDescriptionLiveTemplateTest.kt
+++ educational-core/testSrc/com/jetbrains/edu/coursecreator/taskDescription/TaskDescriptionLiveTemplateTest.kt
@@ -1,8 +1,11 @@
 package com.jetbrains.edu.coursecreator.taskDescription
 
 import com.intellij.openapi.actionSystem.IdeActions
+import com.intellij.openapi.application.ApplicationInfo
 import com.intellij.openapi.application.runWriteAction
+import com.intellij.openapi.util.BuildNumber
 import com.intellij.openapi.vfs.VfsUtil
+import com.intellij.util.ThrowableRunnable
 import com.jetbrains.edu.learning.EduTestCase
 import com.jetbrains.edu.learning.courseFormat.CourseMode
 import com.jetbrains.edu.learning.courseFormat.DescriptionFormat
@@ -10,6 +13,12 @@ import org.intellij.lang.annotations.Language
 
 class TaskDescriptionLiveTemplateTest : EduTestCase() {
 
+  override fun runTestRunnable(testRunnable: ThrowableRunnable<Throwable>) {
+    // https://youtrack.jetbrains.com/issue/EDU-6069/Fix-TaskDescriptionLiveTemplateTest-for-232
+    if (ApplicationInfo.getInstance().build < BUILD_232) {
+      super.runTestRunnable(testRunnable)
+    }
+  }
   fun `test hint live template in task description file in cc mode`() {
     createCourse(CourseMode.EDUCATOR)
 
@@ -74,5 +83,9 @@ class TaskDescriptionLiveTemplateTest : EduTestCase() {
     myFixture.configureFromExistingVirtualFile(file)
     myFixture.performEditorAction(IdeActions.ACTION_EXPAND_LIVE_TEMPLATE_BY_TAB)
     myFixture.checkResult(after.trimIndent())
+  }
+
+  companion object {
+    private val BUILD_232: BuildNumber = BuildNumber.fromString("232")!!
   }
 }
