4cd2596145cdfb6c655e78711f6bb4dff7d656e3
Aleksey Rostovskiy <aleksei.rostovskii@jetbrains.com>
2023-07-24 18:51:23 +0000

    EA-888284 IAE: AdvancedSettingsImpl.getOption
    The "Terminal emulation output console" was moved from the global Advanced Settings to Run Configuration settings, see CPP-34037. Now terminal emulation is false by default.
    
    Merge-request: EDU-MR-2416
    Merged-by: Aleksey Rostovskiy <aleksei.rostovskii@jetbrains.com>
    (cherry picked from commit 7335529aa57ccf6ce6fb3588ed4f7eb2beecb2ec)

--- Edu-Cpp/branches/232/src/com/jetbrains/edu/cpp/checker/compatibilityUtils.kt
+++ Edu-Cpp/branches/232/src/com/jetbrains/edu/cpp/checker/compatibilityUtils.kt
@@ -1,17 +1,6 @@
 package com.jetbrains.edu.cpp.checker
 
-import com.intellij.openapi.options.advanced.AdvancedSettings
-
-
-private const val EMULATE_TERMINAL_SETTING_ID = "cidr.execution.emulate.terminal.in.output.console"
 
 fun <T> withoutTerminalEmulation(action: () -> T): T {
-  val emulateTerminal = AdvancedSettings.getBoolean(EMULATE_TERMINAL_SETTING_ID)
-  try {
-    AdvancedSettings.setBoolean(EMULATE_TERMINAL_SETTING_ID, false)
-    return action()
-  }
-  finally {
-    AdvancedSettings.setBoolean(EMULATE_TERMINAL_SETTING_ID, emulateTerminal)
-  }
+  return action()
 }
