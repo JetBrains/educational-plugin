7f2f3eeac73076b32ce695a7ff3a69979385f181
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2023-08-09 19:02:28 +0200

    EDU-4868: refactor `HyperskillCheckDataTaskTest`

--- educational-core/testSrc/com/jetbrains/edu/learning/stepik/hyperskill/checker/HyperskillCheckDataTaskTest.kt
+++ educational-core/testSrc/com/jetbrains/edu/learning/stepik/hyperskill/checker/HyperskillCheckDataTaskTest.kt
@@ -1,82 +1,54 @@
 package com.jetbrains.edu.learning.stepik.hyperskill.checker
 
 import com.jetbrains.edu.learning.MockResponseFactory
-import com.jetbrains.edu.learning.checker.CheckActionListener
-import com.jetbrains.edu.learning.checker.CheckersTestBase
 import com.jetbrains.edu.learning.checker.DefaultCodeExecutor.Companion.NO_OUTPUT
-import com.jetbrains.edu.learning.checker.EduCheckerFixture
-import com.jetbrains.edu.learning.checker.PlaintTextCheckerFixture
 import com.jetbrains.edu.learning.configuration.PlainTextConfigurator.Companion.CHECK_RESULT_FILE
-import com.jetbrains.edu.learning.course
-import com.jetbrains.edu.learning.courseFormat.Course
+import com.jetbrains.edu.learning.courseFormat.CheckStatus
 import com.jetbrains.edu.learning.courseFormat.ext.allTasks
-import com.jetbrains.edu.learning.courseFormat.tasks.Task
 import com.jetbrains.edu.learning.courseFormat.tasks.data.DataTask.Companion.DATASET_FOLDER_NAME
 import com.jetbrains.edu.learning.courseFormat.tasks.data.DataTask.Companion.DATA_FOLDER_NAME
 import com.jetbrains.edu.learning.courseFormat.tasks.data.DataTask.Companion.INPUT_FILE_NAME
 import com.jetbrains.edu.learning.courseFormat.tasks.data.DataTaskAttempt.Companion.toDataTaskAttempt
-import com.jetbrains.edu.learning.newproject.EmptyProjectSettings
-import com.jetbrains.edu.learning.stepik.api.Attempt
-import com.jetbrains.edu.learning.stepik.hyperskill.api.HyperskillConnector
-import com.jetbrains.edu.learning.stepik.hyperskill.api.MockHyperskillConnector
-import com.jetbrains.edu.learning.stepik.hyperskill.courseFormat.HyperskillCourse
 import com.jetbrains.edu.learning.pathWithoutPrams
-import com.jetbrains.edu.learning.stepik.hyperskill.logInFakeHyperskillUser
-import com.jetbrains.edu.learning.stepik.hyperskill.logOutFakeHyperskillUser
+import com.jetbrains.edu.learning.stepik.api.Attempt
+import com.jetbrains.edu.learning.stepik.hyperskill.courseFormat.HyperskillCourse
 import org.intellij.lang.annotations.Language
 import java.util.*
 
-class HyperskillCheckDataTaskTest : CheckersTestBase<EmptyProjectSettings>() {
-  private val mockConnector: MockHyperskillConnector get() = HyperskillConnector.getInstance() as MockHyperskillConnector
+class HyperskillCheckDataTaskTest : HyperskillCheckActionTestBase() {
 
-  override fun checkTask(task: Task): List<AssertionError> {
-    val assertions = super.checkTask(task)
-    assertEmpty(assertions)
-    return assertions
-  }
-
-  override fun createCheckerFixture(): EduCheckerFixture<EmptyProjectSettings> = PlaintTextCheckerFixture()
-
-  override fun createCourse(): Course = course(courseProducer = ::HyperskillCourse) {
-    section("Topics") {
-      lesson("Topic name") {
-        dataTask(
-          DATA_TASK_1,
-          stepId = 1,
-          attempt = Attempt(ATTEMPT_ID_OF_SUCCEED_SUBMISSION, Date(0), 300).toDataTaskAttempt()
-        ) {
-          taskFile(CHECK_RESULT_FILE, "some text")
-          dir(DATA_FOLDER_NAME) {
-            dir(DATASET_FOLDER_NAME) {
-              taskFile(INPUT_FILE_NAME, "some text")
+  override fun createCourse() {
+    courseWithFiles(courseProducer = ::HyperskillCourse) {
+      section("Topics") {
+        lesson("Topic name") {
+          dataTask(
+            DATA_TASK_1,
+            stepId = 1,
+            attempt = Attempt(ATTEMPT_ID_OF_SUCCEED_SUBMISSION, Date(0), 300).toDataTaskAttempt()
+          ) {
+            taskFile(CHECK_RESULT_FILE, "some text")
+            dir(DATA_FOLDER_NAME) {
+              dir(DATASET_FOLDER_NAME) {
+                taskFile(INPUT_FILE_NAME, "some text")
+              }
             }
           }
-        }
-        dataTask(
-          DATA_TASK_2,
-          stepId = 2,
-          attempt = Attempt(ATTEMPT_ID_OF_FAILED_SUBMISSION, Date(0), 300).toDataTaskAttempt()
-        ) {
-          // mimics no output result
-          taskFile(CHECK_RESULT_FILE, NO_OUTPUT)
-          dir(DATA_FOLDER_NAME) {
-            dir(DATASET_FOLDER_NAME) {
-              taskFile(INPUT_FILE_NAME, "some text")
+          dataTask(
+            DATA_TASK_2,
+            stepId = 2,
+            attempt = Attempt(ATTEMPT_ID_OF_FAILED_SUBMISSION, Date(0), 300).toDataTaskAttempt()
+          ) {
+            // mimics no output result
+            taskFile(CHECK_RESULT_FILE, NO_OUTPUT)
+            dir(DATA_FOLDER_NAME) {
+              dir(DATASET_FOLDER_NAME) {
+                taskFile(INPUT_FILE_NAME, "some text")
+              }
             }
           }
         }
       }
     }
-  } as HyperskillCourse
-
-  override fun setUp() {
-    super.setUp()
-    logInFakeHyperskillUser()
-  }
-
-  override fun tearDown() {
-    logOutFakeHyperskillUser()
-    super.tearDown()
   }
 
   fun `test solved data task`() {
@@ -90,8 +62,7 @@ class HyperskillCheckDataTaskTest : CheckersTestBase<EmptyProjectSettings>() {
       )
     }
 
-    CheckActionListener.reset()
-    checkTask(myCourse.allTasks[0])
+    checkCheckAction(getCourse().allTasks[0], CheckStatus.Solved)
   }
 
   fun `test failed data task`() {
@@ -105,13 +76,11 @@ class HyperskillCheckDataTaskTest : CheckersTestBase<EmptyProjectSettings>() {
       )
     }
 
-    CheckActionListener.shouldFail()
-    checkTask(myCourse.allTasks[0])
+    checkCheckAction(getCourse().allTasks[0], CheckStatus.Failed)
   }
 
   fun `test skipped data task`() {
-    CheckActionListener.shouldSkip()
-    checkTask(myCourse.allTasks[1])
+    checkCheckAction(getCourse().allTasks[1], CheckStatus.Unchecked)
   }
 
   // TODO TIME/TIMER RELATED tests (EDU-4845)
