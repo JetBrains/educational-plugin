e5056dbbf3b59d442e1fb25e11d4525bb6f0a4df
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2023-08-09 22:02:13 +0200

    EDU-4868: refactor `HyperskillCheckSortingBasedTaskTest`

--- educational-core/testSrc/com/jetbrains/edu/learning/stepik/hyperskill/checker/HyperskillCheckSortingBasedTaskTest.kt
+++ educational-core/testSrc/com/jetbrains/edu/learning/stepik/hyperskill/checker/HyperskillCheckSortingBasedTaskTest.kt
@@ -1,82 +1,59 @@
 package com.jetbrains.edu.learning.stepik.hyperskill.checker
 
 import com.jetbrains.edu.learning.MockResponseFactory
-import com.jetbrains.edu.learning.actions.CheckAction
-import com.jetbrains.edu.learning.checker.CheckersTestBase
-import com.jetbrains.edu.learning.checker.EduCheckerFixture
-import com.jetbrains.edu.learning.checker.CheckActionListener
-import com.jetbrains.edu.learning.checker.PlaintTextCheckerFixture
-import com.jetbrains.edu.learning.course
-import com.jetbrains.edu.learning.courseFormat.Course
+import com.jetbrains.edu.learning.courseFormat.CheckStatus
 import com.jetbrains.edu.learning.courseFormat.ext.allTasks
-import com.jetbrains.edu.learning.navigation.NavigationUtils
-import com.jetbrains.edu.learning.newproject.EmptyProjectSettings
 import com.jetbrains.edu.learning.pathWithoutPrams
 import com.jetbrains.edu.learning.stepik.StepikTestUtils.format
-import com.jetbrains.edu.learning.stepik.hyperskill.api.HyperskillConnector
-import com.jetbrains.edu.learning.stepik.hyperskill.api.MockHyperskillConnector
 import com.jetbrains.edu.learning.stepik.hyperskill.courseFormat.HyperskillCourse
-import com.jetbrains.edu.learning.stepik.hyperskill.logInFakeHyperskillUser
-import com.jetbrains.edu.learning.stepik.hyperskill.logOutFakeHyperskillUser
-import com.jetbrains.edu.learning.testAction
-import com.jetbrains.edu.learning.ui.getUICheckLabel
 import org.apache.http.HttpStatus
 import org.intellij.lang.annotations.Language
 import java.util.*
 
-class HyperskillCheckSortingBasedTaskTest : CheckersTestBase<EmptyProjectSettings>() {
-  private val defaultResponseCode: Int = HttpStatus.SC_OK
+class HyperskillCheckSortingBasedTaskTest : HyperskillCheckActionTestBase() {
 
-  private val mockConnector: MockHyperskillConnector get() = HyperskillConnector.getInstance() as MockHyperskillConnector
-
-  override fun createCheckerFixture(): EduCheckerFixture<EmptyProjectSettings> = PlaintTextCheckerFixture()
-
-  override fun setUp() {
-    super.setUp()
-    logInFakeHyperskillUser()
-  }
-
-  override fun tearDown() {
-    logOutFakeHyperskillUser()
-    super.tearDown()
-  }
-
-  override fun createCourse(): Course = course(courseProducer = ::HyperskillCourse) {
-    section("Topics") {
-      lesson("1_lesson_correct") {
-        sortingTask(stepId = 1,
-          name = "sorting_task_correct",
-          options = listOf("1", "2", "0"),
-          ordering = intArrayOf(1, 2, 0)
-        ) {
-          taskFile("Task.txt", "")
-        }
-        sortingTask(stepId = 2,
-          name = "sorting_task_incorrect",
-          options = listOf("1", "2", "0"),
-          ordering = intArrayOf(1, 0, 2)
-        ) {
-          taskFile("Task.txt", "")
-        }
-        matchingTask(stepId = 3,
-          name = "matching_task_correct",
-          captions = listOf("A", "B", "C"),
-          options = listOf("1", "2", "0"),
-          ordering = intArrayOf(1, 2, 0)
-        ) {
-          taskFile("Task.txt", "")
-        }
-        matchingTask(stepId = 4,
-          name = "matching_task_incorrect",
-          captions = listOf("A", "B", "C"),
-          options = listOf("1", "2", "0"),
-          ordering = intArrayOf(1, 0, 2)
-        ) {
-          taskFile("Task.txt", "")
+  override fun createCourse() {
+    courseWithFiles(courseProducer = ::HyperskillCourse) {
+      section("Topics") {
+        lesson("1_lesson_correct") {
+          sortingTask(
+            stepId = 1,
+            name = "sorting_task_correct",
+            options = listOf("1", "2", "0"),
+            ordering = intArrayOf(1, 2, 0)
+          ) {
+            taskFile("Task.txt", "")
+          }
+          sortingTask(
+            stepId = 2,
+            name = "sorting_task_incorrect",
+            options = listOf("1", "2", "0"),
+            ordering = intArrayOf(1, 0, 2)
+          ) {
+            taskFile("Task.txt", "")
+          }
+          matchingTask(
+            stepId = 3,
+            name = "matching_task_correct",
+            captions = listOf("A", "B", "C"),
+            options = listOf("1", "2", "0"),
+            ordering = intArrayOf(1, 2, 0)
+          ) {
+            taskFile("Task.txt", "")
+          }
+          matchingTask(
+            stepId = 4,
+            name = "matching_task_incorrect",
+            captions = listOf("A", "B", "C"),
+            options = listOf("1", "2", "0"),
+            ordering = intArrayOf(1, 0, 2)
+          ) {
+            taskFile("Task.txt", "")
+          }
         }
       }
     }
-  } as HyperskillCourse
+  }
 
   private fun configureResponses(succeed: Boolean) {
     mockConnector.withResponseHandler(testRootDisposable) { request, _ ->
@@ -92,23 +69,20 @@ class HyperskillCheckSortingBasedTaskTest : CheckersTestBase<EmptyProjectSetting
           }
           else -> error("Wrong path: ${path}")
         },
-        responseCode = defaultResponseCode
+        responseCode = HttpStatus.SC_OK
       )
     }
   }
 
   private fun doTest(succeed: Boolean, taskNum: Int) {
-    if (succeed) {
-      CheckActionListener.reset()
-      CheckActionListener.expectedMessage { "<html>Succeed solution</html>" }
-    } else {
-      CheckActionListener.shouldFail()
-      CheckActionListener.expectedMessage { "Wrong solution" }
-    }
-    val task = myCourse.allTasks[taskNum]
+    val task = getCourse().allTasks[taskNum]
     configureResponses(succeed)
-    NavigationUtils.navigateToTask(project, task)
-    testAction(CheckAction(task.getUICheckLabel()))
+    if (succeed) {
+      checkCheckAction(task, CheckStatus.Solved, "<html>Succeed solution</html>")
+    }
+    else {
+      checkCheckAction(task, CheckStatus.Failed, "Wrong solution")
+    }
   }
 
   fun `test sorting task correct`() = doTest(true, 0)
