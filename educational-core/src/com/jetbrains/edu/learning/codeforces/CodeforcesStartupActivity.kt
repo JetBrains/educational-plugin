b63aff264a6054dc13b6ef5f31b49f5d217a29b8
Mark Prudnikov <mark.prudnikov@jetbrains.com>
2023-09-25 14:06:28 +0000

    EDU-6126 Codeforces | Not logged in User can see unending spinner in Submissions tab

--- educational-core/src/com/jetbrains/edu/learning/codeforces/CodeforcesStartupActivity.kt
+++ educational-core/src/com/jetbrains/edu/learning/codeforces/CodeforcesStartupActivity.kt
@@ -5,37 +5,31 @@ import com.intellij.openapi.startup.StartupActivity
 import com.jetbrains.edu.learning.EduLogInListener
 import com.jetbrains.edu.learning.EduUtilsKt.isStudentProject
 import com.jetbrains.edu.learning.codeforces.CodeforcesUtils.updateCheckStatus
-import com.jetbrains.edu.learning.courseFormat.codeforces.CodeforcesCourse
 import com.jetbrains.edu.learning.codeforces.update.CodeforcesCourseUpdateChecker
 import com.jetbrains.edu.learning.course
+import com.jetbrains.edu.learning.courseFormat.codeforces.CodeforcesCourse
 import com.jetbrains.edu.learning.isUnitTestMode
 import com.jetbrains.edu.learning.submissions.SubmissionsManager
 import com.jetbrains.edu.learning.taskToolWindow.ui.TaskToolWindowView
 import com.jetbrains.edu.learning.taskToolWindow.ui.tab.TabType
 
 class CodeforcesStartupActivity : StartupActivity {
-  override fun runActivity(project: Project) {
-    if (project.isDisposed || !project.isStudentProject() || isUnitTestMode) return
 
-    if (project.course !is CodeforcesCourse) return
+  override fun runActivity(project: Project) {
+    if (project.isDisposed || !project.isStudentProject() || isUnitTestMode || project.course !is CodeforcesCourse) return
 
     val submissionsManager = SubmissionsManager.getInstance(project)
 
-    submissionsManager.prepareSubmissionsContentWhenLoggedIn {
-      updateCheckStatus(project)
+    if (submissionsManager.isLoggedIn()) {
+      submissionsManager.prepareSubmissionsContentWhenLoggedIn { updateCheckStatus(project) }
     }
 
     project.messageBus.connect().subscribe(CodeforcesSettings.AUTHENTICATION_TOPIC, object : EduLogInListener {
-      override fun userLoggedIn() {
-        submissionsManager.prepareSubmissionsContentWhenLoggedIn {
-          updateCheckStatus(project)
-        }
-      }
+      override fun userLoggedIn() = submissionsManager.prepareSubmissionsContentWhenLoggedIn { updateCheckStatus(project) }
 
-      override fun userLoggedOut() {
-        TaskToolWindowView.getInstance(project).updateTab(TabType.SUBMISSIONS_TAB)
-      }
+      override fun userLoggedOut() = TaskToolWindowView.getInstance(project).updateTab(TabType.SUBMISSIONS_TAB)
     })
+
     CodeforcesCourseUpdateChecker.getInstance(project).check()
   }
 }
