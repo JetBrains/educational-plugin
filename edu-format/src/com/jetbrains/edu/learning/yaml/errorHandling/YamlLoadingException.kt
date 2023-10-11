3a19e6fefe872859c7dad56b7bf912dc1cf478a1
Ekaterina Smal <ekaterina.smal@jetbrains.com>
2023-10-11 17:16:00 +0300

    move YamlLoadingException to format module

---
+++ edu-format/src/com/jetbrains/edu/learning/yaml/errorHandling/YamlLoadingException.kt
@@ -0,0 +1,15 @@
+package com.jetbrains.edu.learning.yaml.errorHandling
+
+import com.jetbrains.edu.learning.courseFormat.EduFormatNames
+import com.jetbrains.edu.learning.courseFormat.message
+
+class YamlLoadingException(override val message: String) : IllegalStateException(message)
+
+fun loadingError(message: String): Nothing = throw YamlLoadingException(message)
+
+fun noDirForItemMessage(name: String, itemTypeName: String = EduFormatNames.ITEM): String =
+  message("yaml.editor.invalid.format.no.dir", name, itemTypeName)
+
+fun unknownConfigMessage(configName: String): String = message("yaml.editor.invalid.format.unknown.config", configName)
+
+fun unexpectedItemTypeMessage(itemType: String): String = message("yaml.editor.invalid.format.unexpected.type", itemType)
