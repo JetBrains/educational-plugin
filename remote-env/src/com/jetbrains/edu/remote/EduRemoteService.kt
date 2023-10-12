4a6d814354118d5d00b8d887740acd9cd22beb36
Aleksey Rostovskiy <aleksei.rostovskii@jetbrains.com>
2023-10-12 18:05:30 +0200

    EDU-6231 "Failed to login to Marketplace" due to Licensing plugin requirement

--- remote-env/src/com/jetbrains/edu/remote/EduRemoteService.kt
+++ remote-env/src/com/jetbrains/edu/remote/EduRemoteService.kt
@@ -8,6 +8,7 @@ import com.jetbrains.codeWithMe.model.projectViewModel
 import com.jetbrains.edu.learning.EduUtilsKt.isEduProject
 import com.jetbrains.edu.learning.course
 import com.jetbrains.edu.learning.navigation.NavigationUtils
+import com.jetbrains.edu.learning.newproject.CourseProjectGenerator.Companion.EDU_PROJECT_CREATED
 import com.jetbrains.edu.learning.projectView.CourseViewPane
 import com.jetbrains.edu.learning.taskToolWindow.ui.TaskToolWindowView
 import com.jetbrains.edu.remote.termination.EduRemoteDisconnectWatcherService
@@ -22,6 +23,11 @@ class EduRemoteService(private val session: RemoteProjectSession) : LifetimedSer
     serviceLifetime.launchOnUi {
       val model = session.protocol.projectViewModel
       if (project.isEduProject()) {
+        // This flag is set to true because the remote development project is not actually created from scratch when connected,
+        // but rather the first course project opening for a user. Therefore, the corresponding existing code in the plugin is not invoked,
+        // and we need to explicitly do it here.
+        project.putUserData(EDU_PROJECT_CREATED, true)
+
         val course = project.course ?: return@launchOnUi
         // This hack is needed because at the time this service is loaded our Course view is not present in the model
         ApplicationManager.getApplication().executeOnPooledThread {
