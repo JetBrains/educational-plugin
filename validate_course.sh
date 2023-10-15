8b4ab7f163c1d063de6b10233b3b8bcdc2b69a25
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2023-10-15 21:42:01 +0200

    Install necessary plugins

--- validate_course.sh
+++ validate_course.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 #/idea/bin/idea.sh validateCourse /project --marketplace $1 -Djava.awt.headless=true -Didea.is.internal=true
+#/idea/bin/idea.sh installCoursePlugins /project --marketplace $1 -Djava.awt.headless=true -Didea.is.internal=true
+/idea/bin/idea.sh installCoursePlugins /project --marketplace 16630 -Djava.awt.headless=true -Didea.is.internal=true
 /idea/bin/idea.sh validateCourse /project --marketplace 16630 -Djava.awt.headless=true -Didea.is.internal=true -Dproject.python.interpreter=/usr/bin/python3
