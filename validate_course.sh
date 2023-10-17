2640d95b36011f550249b8bc36d8b3536b5b29de
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2023-10-17 09:11:39 +0200

    Specify JDK location

--- validate_course.sh
+++ validate_course.sh
@@ -3,4 +3,4 @@
 #/idea/bin/idea.sh validateCourse /project --marketplace $1 -Djava.awt.headless=true -Didea.is.internal=true
 #/idea/bin/idea.sh installCoursePlugins /project --marketplace $1 -Djava.awt.headless=true -Didea.is.internal=true
 /idea/bin/idea.sh installCoursePlugins /project --marketplace 16628 -Djava.awt.headless=true -Didea.is.internal=true
-/idea/bin/idea.sh validateCourse /project --marketplace 16628 -Djava.awt.headless=true -Didea.is.internal=true -Dproject.python.interpreter=/usr/bin/python3
+/idea/bin/idea.sh validateCourse /project --marketplace 16628 -Djava.awt.headless=true -Didea.is.internal=true -Dproject.jdk=/idea/jbr
