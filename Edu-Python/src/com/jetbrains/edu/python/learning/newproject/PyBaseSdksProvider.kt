3a7aa24f26fc22992232f6506580f6c818c48322
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2023-10-19 20:45:58 +0200

    EDU-6371: fix course selection view rendering
    Previously, rendering of python interpreter combobox caused an exception in EDT which broke UI rendering.
    
    The new implementation computes all necessary things in background
    
    Should also fix EDU-5215
    
    (cherry picked from commit 838364d5091c580e878c23f9d6194ac1e3dfb6e8)

--- Edu-Python/src/com/jetbrains/edu/python/learning/newproject/PyBaseSdksProvider.kt
+++ Edu-Python/src/com/jetbrains/edu/python/learning/newproject/PyBaseSdksProvider.kt
@@ -1,7 +1,8 @@
 package com.jetbrains.edu.python.learning.newproject
 
-import com.intellij.openapi.application.invokeAndWaitIfNeeded
 import com.intellij.openapi.util.UserDataHolder
+import com.intellij.util.concurrency.annotations.RequiresBackgroundThread
+import com.intellij.util.concurrency.annotations.RequiresReadLockAbsence
 import com.jetbrains.python.psi.LanguageLevel
 import com.jetbrains.python.sdk.flavors.PythonSdkFlavor
 import kotlin.io.path.pathString
@@ -15,21 +16,21 @@ object PyBaseSdksProvider {
   private var sdkDescriptors: Map<String, PyBaseSdkDescriptor> = emptyMap()
 
   @Suppress("UnstableApiUsage")
+  @RequiresBackgroundThread
+  @RequiresReadLockAbsence
   fun getBaseSdks(context: UserDataHolder? = null): Collection<PyBaseSdkDescriptor> {
     val oldDescriptors = sdkDescriptors
     val newDescriptors = mutableMapOf<String, PyBaseSdkDescriptor>()
 
     val flavor = PythonSdkFlavor.getApplicableFlavors(false).first()
-    val sdkPaths = invokeAndWaitIfNeeded {
-      flavor.suggestLocalHomePaths(null, context).map { it.pathString }
-    }
+    val sdkPaths = flavor.suggestLocalHomePaths(null, context).map { it.pathString }
     for (sdkPath in sdkPaths) {
       val sdkDescriptor = oldDescriptors[sdkPath]
       if (sdkDescriptor != null) {
         newDescriptors[sdkPath] = sdkDescriptor
         continue
       }
-      val versionString = invokeAndWaitIfNeeded { flavor.getVersionString(sdkPath) } ?: continue
+      val versionString = flavor.getVersionString(sdkPath) ?: continue
       val languageLevel = flavor.getLanguageLevelFromVersionString(versionString)
       newDescriptors[sdkPath] = PyBaseSdkDescriptor(sdkPath, versionString, languageLevel)
     }
