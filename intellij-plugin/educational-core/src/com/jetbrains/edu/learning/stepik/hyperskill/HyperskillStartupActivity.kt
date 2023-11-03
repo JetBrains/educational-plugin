70437658e4742f79f88671dabc44c2e229894ec2
Valentina Kiryushkina <valentina.kiryushkina@jetbrains.com>
2023-11-03 15:08:15 +0100

    EDU-6430 Use project only after project is opened

--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/stepik/hyperskill/HyperskillStartupActivity.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/stepik/hyperskill/HyperskillStartupActivity.kt
@@ -2,23 +2,23 @@ package com.jetbrains.edu.learning.stepik.hyperskill
 
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.startup.StartupActivity
+import com.intellij.openapi.startup.ProjectActivity
 import com.jetbrains.edu.learning.EduLogInListener
 import com.jetbrains.edu.learning.EduUtilsKt.isStudentProject
 import com.jetbrains.edu.learning.StudyTaskManager
+import com.jetbrains.edu.learning.courseFormat.hyperskill.HyperskillCourse
 import com.jetbrains.edu.learning.isUnitTestMode
 import com.jetbrains.edu.learning.stepik.hyperskill.api.HyperskillConnector
 import com.jetbrains.edu.learning.stepik.hyperskill.api.HyperskillSolutionLoader
-import com.jetbrains.edu.learning.courseFormat.hyperskill.HyperskillCourse
 import com.jetbrains.edu.learning.stepik.hyperskill.update.HyperskillCourseUpdateChecker
 import com.jetbrains.edu.learning.submissions.SubmissionsManager
 import com.jetbrains.edu.learning.taskToolWindow.ui.TaskToolWindowView
 import com.jetbrains.edu.learning.taskToolWindow.ui.tab.TabType.SUBMISSIONS_TAB
 import com.jetbrains.edu.learning.yaml.YamlFormatSynchronizer
 
-class HyperskillStartupActivity : StartupActivity {
+class HyperskillStartupActivity : ProjectActivity {
 
-  override fun runActivity(project: Project) {
+  override suspend fun execute(project: Project) {
     if (project.isDisposed || !project.isStudentProject() || isUnitTestMode) return
 
     val course = StudyTaskManager.getInstance(project).course as? HyperskillCourse ?: return
