eb9962db58d9554605041a5750651b46f6ad0444
Ilia Posov <ilia.posov@jetbrains.com>
2023-11-06 11:20:21 +0100

    EDU-2468: Rework the Progress Task to create the archive
    The progress task to create the archive is now only the task to run tests before creating the archive. It makes it possible to simplify switches between a BG and the EDT thread.

--- educational-core/src/com/jetbrains/edu/coursecreator/actions/CCCreateCourseArchiveAction.kt
+++ educational-core/src/com/jetbrains/edu/coursecreator/actions/CCCreateCourseArchiveAction.kt
@@ -1,18 +1,28 @@
 package com.jetbrains.edu.coursecreator.actions
 
+import com.intellij.ide.actions.RevealFileAction
+import com.intellij.ide.util.PropertiesComponent
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.progress.ProgressManager
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.Messages
+import com.jetbrains.edu.coursecreator.CCNotificationUtils
 import com.jetbrains.edu.coursecreator.CCUtils.askToWrapTopLevelLessons
 import com.jetbrains.edu.coursecreator.CCUtils.isCourseCreator
+import com.jetbrains.edu.coursecreator.ui.CCCreateCourseArchiveDialog
 import com.jetbrains.edu.learning.StudyTaskManager
+import com.jetbrains.edu.learning.courseFormat.Course
 import com.jetbrains.edu.learning.courseFormat.EduCourse
+import com.jetbrains.edu.learning.courseFormat.Vendor
 import com.jetbrains.edu.learning.courseFormat.ext.hasSections
 import com.jetbrains.edu.learning.courseFormat.ext.hasTopLevelLessons
 import com.jetbrains.edu.learning.messages.EduCoreBundle
+import com.jetbrains.edu.learning.statistics.EduCounterUsageCollector
 import org.jetbrains.annotations.NonNls
+import java.io.File
 
 class CCCreateCourseArchiveAction : AnAction(EduCoreBundle.lazyMessage("action.create.course.archive.text")) {
 
@@ -30,9 +40,59 @@ class CCCreateCourseArchiveAction : AnAction(EduCoreBundle.lazyMessage("action.c
       return
     }
 
-    ProgressManager.getInstance().run(CreateCourseArchiveProgressTask(project, course))
+    val (locationPath, authorName, checkAllTasksFlag) = showCourseArchiveDialog(project, course) ?: return
+    if (checkAllTasksFlag) {
+      ProgressManager.getInstance().run(CheckAllTasksBeforeCreateCourseArchiveProgressTask(project, course) {
+        createSourceArchive(project, course, locationPath, authorName)
+      })
+    }
+    else {
+      createSourceArchive(project, course, locationPath, authorName)
+    }
   }
+
   override fun getActionUpdateThread() = ActionUpdateThread.BGT
+
+  private fun createSourceArchive(project: Project, course: Course, locationPath: String, authorName: String) {
+    course.vendor = Vendor(authorName)
+    PropertiesComponent.getInstance(project).setValue(AUTHOR_NAME, authorName)
+
+    val errorMessage = CourseArchiveCreator(project, locationPath).createArchive()
+    if (errorMessage == null) {
+      CCNotificationUtils.showNotification(
+        project,
+        EduCoreBundle.message("action.create.course.archive.success.message"),
+        ShowFileAction(locationPath)
+      )
+      PropertiesComponent.getInstance(project).setValue(LAST_ARCHIVE_LOCATION, locationPath)
+      EduCounterUsageCollector.createCourseArchive()
+    } else {
+      Messages.showErrorDialog(project, errorMessage, EduCoreBundle.message("error.failed.to.create.course.archive"))
+    }
+  }
+
+  private fun showCourseArchiveDialog(project: Project, course: Course): Triple<String, String, Boolean>? {
+    val dialog = CCCreateCourseArchiveDialog(project, course.name)
+    if (!dialog.showAndGet()) {
+      return null
+    }
+    return Triple(dialog.locationPath, dialog.authorName, dialog.checkAllTasksFlag)
+  }
+
+  class ShowFileAction(val path: String) : AnAction(
+    EduCoreBundle.message("action.create.course.archive.open.file", RevealFileAction.getFileManagerName())
+  ) {
+    override fun actionPerformed(e: AnActionEvent) {
+      RevealFileAction.openFile(File(path))
+    }
+
+    override fun getActionUpdateThread() = ActionUpdateThread.BGT
+
+    override fun update(e: AnActionEvent) {
+      val presentation = e.presentation
+      presentation.isVisible = RevealFileAction.isSupported()
+    }
+  }
 
   companion object {
     @NonNls
---
+++ educational-core/src/com/jetbrains/edu/coursecreator/actions/CheckAllTasksBeforeCreateCourseArchiveProgressTask.kt
@@ -0,0 +1,78 @@
+package com.jetbrains.edu.coursecreator.actions
+
+import com.intellij.ide.IdeBundle
+import com.intellij.notification.Notifications
+import com.intellij.openapi.progress.ProgressIndicator
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.MessageConstants
+import com.intellij.openapi.ui.Messages.showYesNoCancelDialog
+import com.jetbrains.edu.coursecreator.actions.checkAllTasks.checkAllTasksInItemContainer
+import com.jetbrains.edu.coursecreator.actions.checkAllTasks.createFailedTasksNotification
+import com.jetbrains.edu.learning.courseFormat.Course
+import com.jetbrains.edu.learning.courseFormat.tasks.Task
+import com.jetbrains.edu.learning.getInEdt
+import com.jetbrains.edu.learning.messages.EduCoreBundle
+import com.intellij.openapi.progress.Task.Backgroundable
+
+/**
+ * This task checks all the tasks in the course, and if at least one task fails, it asks user
+ * whether they want to create the archive anyway.
+ *
+ * [doIfNeedToCreateTheArchive] is a function that runs in EDT after the task is competed and if
+ * the archive needs to be created, either because there are no failing tasks, or because the user
+ * wants to create the archive anyway.
+ */
+class CheckAllTasksBeforeCreateCourseArchiveProgressTask(
+  project: Project,
+  private val course: Course,
+  private val doIfNeedToCreateTheArchive: () -> Unit
+): Backgroundable(
+  project,
+  EduCoreBundle.message("action.create.course.archive.progress.bar"),
+  true
+) {
+
+  private var failedTasks: List<Task>? = null
+
+  /**
+   * Checks all tasks and shows notification if there are any failed
+   *
+   * @return true if we should create archive next step, false otherwise
+   */
+  override fun run(indicator: ProgressIndicator) {
+    indicator.isIndeterminate = false
+    val prevText = indicator.text
+    indicator.text = EduCoreBundle.message("progress.title.checking.all.tasks")
+    failedTasks = checkAllTasksInItemContainer(project, course, course, indicator)
+    indicator.text = prevText
+  }
+
+  override fun onSuccess() {
+    val failedTasks = failedTasks ?: return
+
+    val needToCreateTheArchive = failedTasks.isEmpty() || when (showFailedTasksDialog(failedTasks.size)) {
+      MessageConstants.CANCEL -> false
+      MessageConstants.YES -> {
+        val notification = createFailedTasksNotification(failedTasks, failedTasks.size, project)
+        Notifications.Bus.notify(notification, project)
+        false
+      }
+      else -> true
+    }
+
+    if (needToCreateTheArchive) {
+      doIfNeedToCreateTheArchive()
+    }
+  }
+
+  private fun showFailedTasksDialog(failedTestsNumber: Int): Int = getInEdt {
+    showYesNoCancelDialog(
+      EduCoreBundle.message("course.creator.create.archive.failed.task.checking.message", failedTestsNumber),
+      EduCoreBundle.message("error.failed.to.create.course.archive"),
+      EduCoreBundle.message("course.creator.create.archive.failed.task.checking.show.check.result"),
+      EduCoreBundle.message("course.creator.create.archive.failed.task.checking.create.anyway"),
+      IdeBundle.message("button.cancel"),
+      null
+    )
+  }
+}
