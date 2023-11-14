f7e20a570dc74b2a1f28f19d0d4d1ee3524f507a
Mark Prudnikov <mark.prudnikov@jetbrains.com>
2023-11-14 03:03:00 +0100

    EDU-5879 follow-up: avoid changing project-level properties in tests
    Since `MarketplaceSolutionSharingTest` is a light-tests class, its test-cases share the same instance of the project between them, so making test-specific changes in the shared value is a bad practice. Now the described behaviour is avoided.

--- educational-core/testSrc/com/jetbrains/edu/learning/marketplace/MarketplaceSolutionSharingTest.kt
+++ educational-core/testSrc/com/jetbrains/edu/learning/marketplace/MarketplaceSolutionSharingTest.kt
@@ -1,31 +1,43 @@
 package com.jetbrains.edu.learning.marketplace
 
+import com.intellij.openapi.util.Disposer
 import com.intellij.openapi.util.registry.Registry
 import com.jetbrains.edu.learning.course
 import com.jetbrains.edu.learning.marketplace.actions.ShareMySolutionsAction
 import com.jetbrains.edu.learning.stepik.SubmissionsTestBase
 import com.jetbrains.edu.learning.testAction
+import com.jetbrains.edu.learning.withRegistryKeyOff
 
 class MarketplaceSolutionSharingTest : SubmissionsTestBase() {
 
+  override fun setUp() {
+    super.setUp()
+    val registryValue = Registry.get(ShareMySolutionsAction.REGISTRY_KEY)
+    val oldValue = registryValue.asBoolean()
+    registryValue.setValue(true)
+    Disposer.register(testRootDisposable) {
+      registryValue.setValue(oldValue)
+    }
+  }
+
   fun `test ShareMySolutionsAction is not visible under registry`() {
-    testAction(ShareMySolutionsAction.ACTION_ID, shouldBeVisible = false)
+    withRegistryKeyOff(ShareMySolutionsAction.REGISTRY_KEY) {
+      testAction(ShareMySolutionsAction.ACTION_ID, shouldBeEnabled = false, shouldBeVisible = false)
+    }
   }
 
   fun `test ShareMySolutionsAction is not visible for non-marketplace course`() {
-    disableRegistry()
-    testAction(ShareMySolutionsAction.ACTION_ID, shouldBeVisible = false)
+    setMarketplaceCourse(false)
+    testAction(ShareMySolutionsAction.ACTION_ID, shouldBeEnabled = false, shouldBeVisible = false)
   }
 
   fun `test ShareMySolutionsAction is visible for marketplace course`() {
-    disableRegistry()
-    setMarketplaceCourse()
+    setMarketplaceCourse(true)
     testAction(ShareMySolutionsAction.ACTION_ID, shouldBeEnabled = false, shouldBeVisible = true)
   }
 
-  private fun disableRegistry() = Registry.get(ShareMySolutionsAction.REGISTRY_KEY).setValue(true)
-
-  private fun setMarketplaceCourse() {
-    project.course?.isMarketplace = true
+  private fun setMarketplaceCourse(state: Boolean) {
+    val course = project.course ?: error("Course is null")
+    course.isMarketplace = state
   }
 }
