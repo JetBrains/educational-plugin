13fbded1e6b2fb0e7e2554fc512d3d7e25917cf4
Sviatoslav Naiden <sviatoslav.naiden@jetbrains.com>
2023-11-21 12:22:55 +0100

    EDU-6424: add build listener to ScalaSbtEduTaskChecker to monitor compilation errors

--- intellij-plugin/Edu-Scala/src/com/jetbrains/edu/scala/sbt/ScalaSbtEduTaskChecker.kt
+++ intellij-plugin/Edu-Scala/src/com/jetbrains/edu/scala/sbt/ScalaSbtEduTaskChecker.kt
@@ -1,13 +1,21 @@
 package com.jetbrains.edu.scala.sbt
 
+import com.intellij.build.BuildProgressListener
+import com.intellij.build.BuildViewManager
+import com.intellij.build.events.BuildEvent
 import com.intellij.execution.RunManager
 import com.intellij.execution.RunnerAndConfigurationSettings
 import com.intellij.execution.testframework.sm.runner.SMTestProxy
 import com.intellij.openapi.module.ModuleUtilCore
+import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiDirectory
+import com.jetbrains.edu.learning.checker.CheckUtils
 import com.jetbrains.edu.learning.checker.EduTaskCheckerBase
 import com.jetbrains.edu.learning.checker.EnvironmentChecker
+import com.jetbrains.edu.learning.courseFormat.CheckResult
+import com.jetbrains.edu.learning.courseFormat.CheckResult.Companion.noTestsRun
+import com.jetbrains.edu.learning.courseFormat.CheckStatus
 import com.jetbrains.edu.learning.courseFormat.ext.getAllTestDirectories
 import com.jetbrains.edu.learning.courseFormat.tasks.EduTask
 import com.jetbrains.edu.learning.xmlEscaped
@@ -40,6 +48,23 @@ class ScalaSbtEduTaskChecker(
     configuration.testKind = packageTestData.kind
     configuration.module = ModuleUtilCore.findModuleForPsiElement(testDirectory)
     return settings
+  }
+
+  override fun check(indicator: ProgressIndicator): CheckResult {
+    val buildViewManager = project.getService(BuildViewManager::class.java)
+    val buildEvents = mutableListOf<BuildEvent>()
+    val listener = BuildProgressListener { _, event -> buildEvents.add(event) }
+
+    @Suppress("UnstableApiUsage")
+    buildViewManager.addListener(listener, buildViewManager)
+
+    val checkResult = super.check(indicator)
+
+    if (checkResult == noTestsRun && buildEvents.find { it.message.contains("Errors occurred") } != null) {
+      val description = buildEvents.find { it.description != null }?.description
+      return CheckResult(CheckStatus.Unchecked, CheckUtils.COMPILATION_FAILED_MESSAGE, description)
+    }
+    return checkResult
   }
 
   override fun getErrorMessage(node: SMTestProxy): String = super.getErrorMessage(node).xmlEscaped
