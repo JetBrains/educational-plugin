c105f057f8221c44029e45eee60e6d4de744eade
Gleb Oborin <Gleb.Oborin@jetbrains.com>
2023-12-18 20:32:42 +0100

    EDU-6545: Try to get the file type from the corresponding virtual file

--- intellij-plugin/educational-core/src/com/jetbrains/edu/coursecreator/framework/diff/diffUtils.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/coursecreator/framework/diff/diffUtils.kt
@@ -1,12 +1,12 @@
 package com.jetbrains.edu.coursecreator.framework.diff
 
 import com.intellij.openapi.fileEditor.FileDocumentManager
-import com.intellij.openapi.fileTypes.FileTypeManager
+import com.intellij.openapi.fileTypes.FileType
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Disposer
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.testFramework.LightVirtualFile
-import com.intellij.util.PathUtil
+import com.jetbrains.edu.learning.courseFormat.ext.getVirtualFile
 import com.jetbrains.edu.learning.courseFormat.tasks.Task
 import com.jetbrains.edu.learning.framework.impl.FLTaskState
 import com.jetbrains.edu.learning.isUnitTestMode
@@ -24,9 +24,10 @@ fun applyChangesWithMergeDialog(
 ): FLTaskState? {
   val mergeProvider = FLMergeProvider(project, targetTask, leftState, baseState, rightState, initialBaseState)
   val mergeDialogCustomizer = FLMergeDialogCustomizer(project, currentTask.name, targetTask.name)
-  val tmpConflictFiles = conflictFiles.map {
-    val fileType = FileTypeManager.getInstance().getFileTypeByExtension(PathUtil.getFileExtension(it) ?: "")
-    LightVirtualFile(it, fileType, baseState[it] ?: "")
+  val tmpConflictFiles = conflictFiles.map { name ->
+    val fileType = findConflictFileType(project, name, currentTask, targetTask) ?: error("Couldn't find file corresponding for $name")
+    val fileContent = baseState[name] ?: error("Conflict file content was not added to baseState during conflict resolution")
+    LightVirtualFile(name, fileType, fileContent)
   }
   val isOk = showMultipleFileMergeDialog(project, tmpConflictFiles, mergeProvider, mergeDialogCustomizer, currentTask.name, targetTask.name)
 
@@ -94,4 +95,15 @@ fun resolveConflicts(
       Disposer.dispose(this)
     }
   }
+}
+
+private fun findConflictFileType(project: Project, fileName: String, currentTask: Task, targetTask: Task): FileType? {
+  // conflict file must exist as virtual file on disk either in the current task or target task
+  // otherwise there cannot be a conflict
+  return findFileType(project, fileName, currentTask) ?: findFileType(project, fileName, targetTask)
+}
+
+private fun findFileType(project: Project, fileName: String, task: Task): FileType? {
+  val taskFile = task.taskFiles[fileName] ?: return null
+  return taskFile.getVirtualFile(project)?.fileType
 }
