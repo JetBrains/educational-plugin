32ff7cbbb6ee5c0de3f2b9f4392e03d6d37f9cda
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2024-01-04 22:25:39 +0100

    EDU-6567: extract test error message from stacktrace for Gradle-based course
    Temporary workaround for cases when error message is missing for tests in Gradle-based courses. For example, 2023.2 and 2023.3 major versions don't provide error messages for tests in Gradle project if it's not comparison error

--- intellij-plugin/jvm-core/src/com/jetbrains/edu/jvm/gradle/checker/NewGradleEduTaskChecker.kt
+++ intellij-plugin/jvm-core/src/com/jetbrains/edu/jvm/gradle/checker/NewGradleEduTaskChecker.kt
@@ -8,6 +8,7 @@ import com.jetbrains.edu.learning.checker.EduTaskCheckerBase
 import com.jetbrains.edu.learning.checker.EnvironmentChecker
 import com.jetbrains.edu.learning.courseFormat.CheckResult
 import com.jetbrains.edu.learning.courseFormat.tasks.EduTask
+import com.jetbrains.edu.learning.messages.EduCoreBundle
 import com.jetbrains.edu.learning.xmlEscaped
 
 open class NewGradleEduTaskChecker(task: EduTask, envChecker: EnvironmentChecker, project: Project) :
@@ -24,7 +25,39 @@ open class NewGradleEduTaskChecker(task: EduTask, envChecker: EnvironmentChecker
     }.orEmpty()
   }
 
-  override fun getErrorMessage(node: SMTestProxy): String = super.getErrorMessage(node).xmlEscaped
+  override fun getErrorMessage(node: SMTestProxy): String {
+    // @formatter:off
+    val message = node.errorMessage ?:
+                  // Gradle integration doesn't pass error messages in some cases in 2023.2 and 2023.3
+                  // See https://youtrack.jetbrains.com/issue/EDU-6567
+                  node.stacktrace?.extractMessageFromStackTrace() ?:
+                  EduCoreBundle.message("error.execution.failed")
+    // @formatter:on
+    return message.xmlEscaped
+  }
 
   override fun getComparisonErrorMessage(node: SMTestProxy): String = extractComparisonErrorMessage(node).xmlEscaped
+
+  companion object {
+
+    // Each stacktrace line after error message starts with some space symbols and `at`
+    private val STACK_TRACE_LINE_START: Regex = """^\s*at""".toRegex()
+
+    private fun String.extractMessageFromStackTrace(): String? {
+      val builder = StringBuilder()
+
+      // The first line of stacktrace starts with a fully qualified name of exception class with `: ` after it
+      val firstLine = lineSequence().firstOrNull()?.substringAfter(": ") ?: return null
+      builder.append(firstLine)
+
+      for (line in lineSequence().drop(1)) {
+        if (STACK_TRACE_LINE_START.find(line) != null) {
+          break
+        }
+        builder.appendLine()
+        builder.append(line)
+      }
+      return builder.toString()
+    }
+  }
 }
