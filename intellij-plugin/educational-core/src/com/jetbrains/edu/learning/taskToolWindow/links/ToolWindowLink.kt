572ad11176325aaa3012ee1e756afe06150fb680
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2024-01-08 01:50:40 +0100

    EDU-6503: properly validate tool window links in headless mode
    Links validation is supposed to work correctly in headless mode (on CI, for example). But IDEs don't load tool windows in headless mode by default. As a result, you can't just use `ToolWindowManager#getToolWindow` to ensure if a tool window link is valid or not
    
    These changes add manual checks of the corresponding tool window extensions

--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/taskToolWindow/links/ToolWindowLink.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/taskToolWindow/links/ToolWindowLink.kt
@@ -1,23 +1,33 @@
 package com.jetbrains.edu.learning.taskToolWindow.links
 
+import com.intellij.facet.ui.FacetDependentToolWindow
 import com.intellij.openapi.application.runInEdt
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.wm.ToolWindow
+import com.intellij.openapi.wm.ToolWindowEP
 import com.intellij.openapi.wm.ToolWindowManager
+import com.intellij.openapi.wm.ext.LibraryDependentToolWindow
 
 @Suppress("PARAMETER_NAME_CHANGED_ON_OVERRIDE")
-class ToolWindowLink(link: String) : TaskDescriptionLink<ToolWindow, ToolWindow?>(link) {
-  override fun resolve(project: Project): ToolWindow? {
-    return ToolWindowManager.getInstance(project).getToolWindow(linkPath)
-  }
+class ToolWindowLink(link: String) : TaskDescriptionLink<String, String>(link) {
+  override fun resolve(project: Project): String = linkPath
 
-  override fun open(project: Project, toolWindow: ToolWindow) {
+  override fun open(project: Project, toolWindowId: String) {
+    val toolWindow = ToolWindowManager.getInstance(project).getToolWindow(linkPath) ?: return
     runInEdt {
       toolWindow.show()
     }
   }
 
-  override suspend fun validate(project: Project, toolWindow: ToolWindow?): String? {
-    return if (toolWindow == null) "Failed to find a tool window by `$linkPath` id" else null
+  override suspend fun validate(project: Project, toolWindowId: String): String? {
+    val hasToolWindow = TOOL_WINDOW_EPS.flatMap { it.extensionList }.any { it.id == toolWindowId }
+    return if (hasToolWindow) null else "Failed to find a tool window by `$toolWindowId` id"
+  }
+
+  companion object {
+    private val TOOL_WINDOW_EPS = listOf(
+      ToolWindowEP.EP_NAME,
+      LibraryDependentToolWindow.EXTENSION_POINT_NAME,
+      FacetDependentToolWindow.EXTENSION_POINT_NAME
+    )
   }
 }
