605bd7a7f5096f20e6513886033b0e50151c8627
Aleksey Rostovskiy <aleksey.rostovskiy@jetbrains.com>
2024-01-09 18:23:53 +0100

    EDU-6576 Exception: ClientId might be missing.Notification is trying to be shown under local ClientId in RD mode

--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/SolutionLoaderBase.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/SolutionLoaderBase.kt
@@ -156,7 +156,7 @@ abstract class SolutionLoaderBase(protected val project: Project) : Disposable {
     val needToShowNotification = needToShowUpdateNotification(futures.values)
     runInEdt {
       if (project.isDisposed) return@runInEdt
-      if (needToShowNotification) {
+      if (needToShowNotification && !RemoteEnvHelper.isRemoteDevServer() ) {
         // Suppression is needed here because DialogTitleCapitalization is demanded by the superclass constructor,
         // but the plugin naming with the capital letters used in the notification title
         @Suppress("DialogTitleCapitalization")
