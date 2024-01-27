3f7dbe5b8ff251a56140a8e23d67226298144e4d
Ilia Posov <ilia.posov@jetbrains.com>
2024-01-27 10:37:07 +0100

    EDU-6476: Add comment on the HugeBinaryFileException

--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/exceptions/HugeBinaryFileException.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/exceptions/HugeBinaryFileException.kt
@@ -4,6 +4,10 @@ import com.intellij.openapi.util.io.FileUtilRt
 import com.intellij.openapi.util.text.StringUtil
 import com.jetbrains.edu.learning.messages.EduCoreBundle
 
+/**
+ * [HugeBinaryFileException] is similar to [com.intellij.openapi.util.io.FileTooBigException],
+ * but stores additional information and generates a user visible message.
+ */
 class HugeBinaryFileException(val path: String, val size: Long, val limit: Long, insideFrameworkLesson: Boolean = false) : IllegalStateException(
   buildString {
     appendLine(EduCoreBundle.message("error.educator.file.size", path))
