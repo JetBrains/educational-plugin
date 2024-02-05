4b346d3ab8d9729fdb14402459b52abfe717c639
Aleksey Rostovskiy <aleksei.rostovskii@jetbrains.com>
2024-02-05 17:16:23 +0100

    EDU-6447 Refactor RsCourseCompatibilityProvider class
    WRONG BACKCOMPAT COMMENT!
    
    The algorithm to provide the requiredPlugins was simplified, removing the unnecessary condition.

--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/compatibility/RsCourseCompatibilityProvider.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/compatibility/RsCourseCompatibilityProvider.kt
@@ -1,7 +1,5 @@
 package com.jetbrains.edu.learning.compatibility
 
-import com.intellij.openapi.application.ApplicationInfo
-import com.intellij.openapi.util.BuildNumber
 import com.intellij.util.PlatformUtils.isCLion
 import com.intellij.util.PlatformUtils.isIdeaUltimate
 import com.jetbrains.edu.EducationalCoreIcons
@@ -9,21 +7,14 @@ import com.jetbrains.edu.learning.courseFormat.PluginInfo
 import com.jetbrains.edu.learning.courseFormat.PluginInfos
 import javax.swing.Icon
 
-// BACKCOMPAT: 2023.2
-private val BUILD_232 = BuildNumber.fromString("232")!!
-
 private val RUST_PLUGINS = listOf(
   PluginInfos.RUST,
   PluginInfos.TOML
 )
 
 class RsCourseCompatibilityProvider : CourseCompatibilityProvider {
-
   @Suppress("UnstableApiUsage", "DEPRECATION")
-  override fun requiredPlugins(): List<PluginInfo>? {
-    if (ApplicationInfo.getInstance().build < BUILD_232) return RUST_PLUGINS
-    return if (isIdeaUltimate() || isCLion() || isRustRover()) RUST_PLUGINS else null
-  }
+  override fun requiredPlugins(): List<PluginInfo>? = if (isIdeaUltimate() || isCLion() || isRustRover()) RUST_PLUGINS else null
 
   override val technologyName: String get() = "Rust"
   override val logo: Icon get() = EducationalCoreIcons.RustLogo
