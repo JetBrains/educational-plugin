18b4d7075c0796233fe84787f648c97d07529817
Aleksey Rostovskiy <aleksei.rostovskii@jetbrains.com>
2024-02-05 16:25:01 +0100

    EDU-6447 Deprecated class `EduWelcomeTabPanel` was removed

--- intellij-plugin/educational-core/branches/232/src/com/jetbrains/edu/learning/ui/themeUtils.kt
+++ intellij-plugin/educational-core/branches/232/src/com/jetbrains/edu/learning/ui/themeUtils.kt
@@ -2,7 +2,6 @@ package com.jetbrains.edu.learning.ui
 
 import com.intellij.ide.ui.LafManager
 import com.intellij.ide.ui.laf.UIThemeBasedLookAndFeelInfo
-import com.intellij.openapi.actionSystem.impl.ActionButton
 
 fun getCurrentThemeName(): String? {
   val lookAndFeel = LafManager.getInstance().currentLookAndFeel as? UIThemeBasedLookAndFeelInfo
@@ -13,9 +12,4 @@ fun getCurrentThemeName(): String? {
 fun getCurrentThemeId(): String? {
   val lookAndFeel = LafManager.getInstance().currentLookAndFeel as? UIThemeBasedLookAndFeelInfo
   return lookAndFeel?.theme?.id
-}
-
-// BACKCOMPAT: 2023.1. Remove it
-@Suppress("UnusedReceiverParameter")
-fun ActionButton.setLook() {
 }
--- intellij-plugin/educational-core/branches/233/src/com/jetbrains/edu/learning/ui/themeUtils.kt
+++ intellij-plugin/educational-core/branches/233/src/com/jetbrains/edu/learning/ui/themeUtils.kt
@@ -1,7 +1,6 @@
 package com.jetbrains.edu.learning.ui
 
 import com.intellij.ide.ui.LafManager
-import com.intellij.openapi.actionSystem.impl.ActionButton
 
 // BACKCOMPAT: 2023.2. Inline it
 fun getCurrentThemeName(): String? {
@@ -13,9 +12,4 @@ fun getCurrentThemeName(): String? {
 fun getCurrentThemeId(): String? {
   @Suppress("UnstableApiUsage")
   return LafManager.getInstance().currentUIThemeLookAndFeel?.id
-}
-
-// BACKCOMPAT: 2023.1. Remove it
-@Suppress("UnusedReceiverParameter")
-fun ActionButton.setLook() {
 }
