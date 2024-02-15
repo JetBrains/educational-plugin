097f86ce1c3ee8397127c0440c04a8d895de41b4
Gleb Oborin <Gleb.Oborin@jetbrains.com>
2024-02-15 17:50:04 +0100

    EDU-6439: Fix line length

--- intellij-plugin/educational-core/src/com/jetbrains/edu/coursecreator/actions/CCFrameworkLessonActionGroup.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/coursecreator/actions/CCFrameworkLessonActionGroup.kt
@@ -5,7 +5,7 @@ import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.DefaultActionGroup
 import com.intellij.openapi.project.DumbAware
 import com.jetbrains.edu.coursecreator.CCUtils
-import com.jetbrains.edu.learning.EduExperimentalFeatures
+import com.jetbrains.edu.learning.EduExperimentalFeatures.CC_FL_SYNC_CHANGES
 import com.jetbrains.edu.learning.isFeatureEnabled
 
 @Suppress("ComponentNotRegistered") // educational-core.xml
@@ -16,6 +16,6 @@ class CCFrameworkLessonActionGroup : DefaultActionGroup(), DumbAware {
   override fun update(e: AnActionEvent) {
     val presentation = e.presentation
     val project = e.project
-    presentation.isEnabledAndVisible = project != null && CCUtils.isCourseCreator(project) && isFeatureEnabled(EduExperimentalFeatures.CC_FL_SYNC_CHANGES)
+    presentation.isEnabledAndVisible = project != null && CCUtils.isCourseCreator(project) && isFeatureEnabled(CC_FL_SYNC_CHANGES)
   }
 }
