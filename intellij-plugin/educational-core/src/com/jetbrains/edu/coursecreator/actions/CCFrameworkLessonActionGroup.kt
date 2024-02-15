b65e3c33e42ec266c5f84a240069e8476cb11651
Gleb Oborin <Gleb.Oborin@jetbrains.com>
2024-02-15 17:43:43 +0100

    EDU-6439: Fix action thread & update

--- intellij-plugin/educational-core/src/com/jetbrains/edu/coursecreator/actions/CCFrameworkLessonActionGroup.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/coursecreator/actions/CCFrameworkLessonActionGroup.kt
@@ -4,14 +4,18 @@ import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.DefaultActionGroup
 import com.intellij.openapi.project.DumbAware
-import com.jetbrains.edu.coursecreator.CCUtils.updateActionGroup
+import com.jetbrains.edu.coursecreator.CCUtils
+import com.jetbrains.edu.learning.EduExperimentalFeatures
+import com.jetbrains.edu.learning.isFeatureEnabled
 
 @Suppress("ComponentNotRegistered") // educational-core.xml
 class CCFrameworkLessonActionGroup : DefaultActionGroup(), DumbAware {
 
-  override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.EDT
+  override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.BGT
 
   override fun update(e: AnActionEvent) {
-    updateActionGroup(e)
+    val presentation = e.presentation
+    val project = e.project
+    presentation.isEnabledAndVisible = project != null && CCUtils.isCourseCreator(project) && isFeatureEnabled(EduExperimentalFeatures.CC_FL_APPLY_CHANGES)
   }
 }
