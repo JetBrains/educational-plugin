87c489785a131fae6ad51696c66c51b8f70fb1b5
Mark Prudnikov <mark.prudnikov@jetbrains.com>
2024-02-22 15:24:52 +0100

    Refactor `segmentedButtonUtils.kt`: Introduce `updateCommunityButton`
    Simplifies `SubmissionsTab` content update by merging `enableCommunityButton` and `disableCommunityButton` logic.

--- intellij-plugin/educational-core/branches/241/src/com/jetbrains/edu/learning/submissions/segmentedButtonUtils.kt
+++ intellij-plugin/educational-core/branches/241/src/com/jetbrains/edu/learning/submissions/segmentedButtonUtils.kt
@@ -14,26 +14,17 @@ fun SegmentedButton.ItemPresentation.segmentedButtonRenderer(item: JButton) {
 
 // BACKCOMPAT: 2023.2. Move to [com.jetbrains.edu.learning.submissions.SubmissionsTab.kt]
 @Suppress("UnstableApiUsage")
-fun SegmentedButton<JButton>.enableCommunityButton() {
+fun SegmentedButton<JButton>.updateCommunityButton(isEnabled: Boolean, isAgreementTooltip: Boolean) {
   // todo: fix to `communityButton.text` after move
   val communityButton = items.findLast { it.text == EduCoreBundle.message("submissions.button.community") } ?: return
-  communityButton.isEnabled = true
-  communityButton.toolTipText = EduCoreBundle.message("submissions.button.community.tooltip.text.enabled")
-  update(communityButton)
-}
-
-// BACKCOMPAT: 2023.2. Move to [com.jetbrains.edu.learning.submissions.SubmissionsTab.kt]
-@Suppress("UnstableApiUsage")
-fun SegmentedButton<JButton>.disableCommunityButton(isAgreementTooltip: Boolean = false) {
-  // todo: fix to `communityButton.text` after move
-  val communityButton = items.findLast { it.text == EduCoreBundle.message("submissions.button.community") } ?: return
-  communityButton.isEnabled = false
-  communityButton.toolTipText = if (isAgreementTooltip) {
-    EduCoreBundle.message("submissions.tab.solution.sharing.agreement")
+  communityButton.isEnabled = isEnabled
+  communityButton.toolTipText = when {
+    isAgreementTooltip -> EduCoreBundle.message("submissions.tab.solution.sharing.agreement")
+    isEnabled -> EduCoreBundle.message("submissions.button.community.tooltip.text.enabled")
+    else -> EduCoreBundle.message("submissions.button.community.tooltip.text.disabled")
   }
-  else {
-    EduCoreBundle.message("submissions.button.community.tooltip.text.disabled")
+  if (!isEnabled) {
+    selectedItem = items.first()
   }
-  selectedItem = items.first()
   update(communityButton)
 }
