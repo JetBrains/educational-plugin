d010cecef5a815b43921175bc1ab4ba8f39371d9
antipov <andrey.antipov@jetbrains.com>
2024-03-14 16:11:57 +0100

    EDU-6709: store presentable StudyItem name in course-info.yaml

--- edu-format/src/com/jetbrains/edu/learning/yaml/format/StudyItemConverter.kt
+++ edu-format/src/com/jetbrains/edu/learning/yaml/format/StudyItemConverter.kt
@@ -5,5 +5,5 @@ import com.jetbrains.edu.learning.courseFormat.StudyItem
 
 
 class StudyItemConverter : StdConverter<StudyItem, String>() {
-  override fun convert(item: StudyItem): String = item.name
+  override fun convert(item: StudyItem): String = item.presentableName
 }
