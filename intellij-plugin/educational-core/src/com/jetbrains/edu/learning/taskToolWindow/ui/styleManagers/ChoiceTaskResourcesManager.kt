JetBrains/educational-plugin

commit 1ef7dee9e7060d285a6e031a3615f23e8723fd47

Use builtin template mechanism to substitute placeholders in task description page template
It allows us to simplify code as well as reduce usage of external libraries

intellij-plugin/educational-core/src/com/jetbrains/edu/learning/taskToolWindow/ui/styleManagers/StyleResourcesManager.kt
@@ -134,7 +134,7 @@ object StyleResourcesManager {
       JETBRAINS_ACADEMY_CSS_LIGHT
     }
 
-  // update style/template.html.ft in case of changing key names
+  // update fileTemplates/internal/taskDescriptionPage.html.ft in case of changing key names
   fun getResources(content: String) = mapOf(
     resourcePair("base_css", BROWSER_CSS),
     "typography_color_style" to StyleManager().typographyAndColorStylesheet(),

intellij-plugin/educational-core/branches/232/src/com/jetbrains/edu/learning/taskToolWindow/ui/utils.kt
@@ -1,12 +1,6 @@
 package com.jetbrains.edu.learning.taskToolWindow.ui
 
 import com.intellij.openapi.actionSystem.ActionToolbar
-import org.apache.commons.lang.text.StrSubstitutor
-
-// BACKCOMPAT: 2023.2. Inline it.
-fun replaceWithTemplateText(resources: Map<String, String>, templateText: String): String {
-  return StrSubstitutor(resources).replace(templateText)
-}
 
 // BACKCOMPAT: 2023.3. Inline it.
 fun ActionToolbar.setupLayoutStrategy() {

intellij-plugin/educational-core/branches/233/src/com/jetbrains/edu/learning/taskToolWindow/ui/utils.kt
@@ -1,12 +1,6 @@
 package com.jetbrains.edu.learning.taskToolWindow.ui
 
 import com.intellij.openapi.actionSystem.ActionToolbar
-import org.apache.commons.text.StringSubstitutor
-
-// BACKCOMPAT: 2023.2. Inline it.
-fun replaceWithTemplateText(resources: Map<String, String>, templateText: String): String {
-  return StringSubstitutor(resources).replace(templateText)
-}
 
 // BACKCOMPAT: 2023.3. Inline it.
 fun ActionToolbar.setupLayoutStrategy() {

intellij-plugin/educational-core/branches/241/src/com/jetbrains/edu/learning/taskToolWindow/ui/utils.kt
@@ -2,12 +2,6 @@ package com.jetbrains.edu.learning.taskToolWindow.ui
 
 import com.intellij.openapi.actionSystem.ActionToolbar
 import com.intellij.openapi.actionSystem.toolbarLayout.ToolbarLayoutStrategy
-import org.apache.commons.text.StringSubstitutor
-
-// BACKCOMPAT: 2023.2. Inline it.
-fun replaceWithTemplateText(resources: Map<String, String>, templateText: String): String {
-  return StringSubstitutor(resources).replace(templateText)
-}
 
 // BACKCOMPAT: 2023.3. Inline it.
 fun ActionToolbar.setupLayoutStrategy() {

intellij-plugin/educational-core/resources/fileTemplates/internal/taskDescriptionPage.html.ft
@@ -1,6 +1,3 @@
-<!-- <!DOCTYPE html>-->
-<!-- we use key from substrings "${key}" in a StrSubstitutor as keys and in case of renaming keys one has to update map keys in
- com.jetbrains.edu.learning.ui.taskToolWindow.TaskDescriptionHtml-->
 <html>
 <head>
   <script type="text/javascript" src="${mathJax}"></script>

intellij-plugin/educational-core/src/com/jetbrains/edu/learning/taskToolWindow/ui/TaskUtils.kt
@@ -7,6 +7,7 @@ import com.jetbrains.edu.learning.actions.EduActionUtils.getCurrentTask
 import com.jetbrains.edu.learning.courseDir
 import com.jetbrains.edu.learning.courseFormat.ext.getDir
 import com.jetbrains.edu.learning.courseFormat.tasks.Task
+import com.jetbrains.edu.learning.courseGeneration.GeneratorUtils
 import com.jetbrains.edu.learning.messages.EduCoreBundle
 import com.jetbrains.edu.learning.taskToolWindow.IMG_TAG
 import com.jetbrains.edu.learning.taskToolWindow.SCRIPT_TAG
@@ -22,22 +23,9 @@ fun htmlWithResources(
   content: String,
   task: Task? = project.getCurrentTask()
 ): String {
-  val templateText = loadText("/style/template.html.ft") ?: "Cannot load task text"
   val resources = StyleManager.resources(content)
-  val textWithResources = replaceWithTemplateText(resources, templateText)
+  val textWithResources = GeneratorUtils.getInternalTemplateText("taskDescriptionPage.html", resources)
   return absolutizePaths(project, textWithResources, task)
-}
-
-fun loadText(filePath: String): String? {
-  val stream = try {
-    object {}.javaClass.getResourceAsStream(filePath)
-  } catch (e: NullPointerException) {
-    return null
-  }
-
-  return stream.use {
-    it.bufferedReader().readText()
-  }
 }
 
 fun wrapHintTagsInsideHTML(document: Document, wrapHint: (e: Element, number: String, title: String) -> String): Document {

commit 350dc134545d2a85bd75d65d1ef178ca598f0008

EDU-6750 Choice option text is moved to the next line from radio button
Is broken after 1ef7dee9. Before it worked kind of flimsy: we first constructed a long string with substitution variables that can apprear from 1) file template itself 2) after substitution of `content` variable. After switching to `GeneratorUtils.getInternalTemplateText` from StrSubstitutor it logically stopped working.

intellij-plugin/educational-core/src/com/jetbrains/edu/learning/taskToolWindow/ui/styleManagers/ChoiceTaskResourcesManager.kt
@@ -4,20 +4,18 @@ import com.google.gson.Gson
 import com.jetbrains.edu.learning.EduNames
 import com.jetbrains.edu.learning.JavaUILibrary.Companion.isJCEF
 import com.jetbrains.edu.learning.courseFormat.CheckStatus
+import com.jetbrains.edu.learning.courseFormat.hyperskill.HyperskillCourse
 import com.jetbrains.edu.learning.courseFormat.tasks.choice.ChoiceTask
 import com.jetbrains.edu.learning.courseGeneration.GeneratorUtils
 import com.jetbrains.edu.learning.messages.EduCoreBundle
 import com.jetbrains.edu.learning.stepik.StepikNames
 import com.jetbrains.edu.learning.stepik.getStepikLink
-import com.jetbrains.edu.learning.courseFormat.hyperskill.HyperskillCourse
 import com.jetbrains.edu.learning.stepik.hyperskill.stepLink
-import com.jetbrains.edu.learning.taskToolWindow.ui.*
+import com.jetbrains.edu.learning.taskToolWindow.ui.getRadioButtonCSS
 import kotlinx.css.*
-import kotlinx.css.properties.*
+import kotlinx.css.properties.lh
 
 class ChoiceTaskResourcesManager : TaskResourcesManager<ChoiceTask> {
-  override val resources: Map<String, String>
-    get() = mapOf("choice_options_style" to choiceOptionsStylesheet())
 
   // because task object is inserted after html is loaded
   private fun getTaskResources(task: ChoiceTask): Map<String, Any> = mapOf(
@@ -25,7 +23,8 @@ class ChoiceTaskResourcesManager : TaskResourcesManager<ChoiceTask> {
     "selected_variants" to task.selectedVariants,
     "choice_options" to Gson().toJson(task.choiceOptions.map { it.text }),
     "is_multiple_choice" to task.isMultipleChoice,
-    "is_disabled" to (task.status == CheckStatus.Failed && task.isChangedOnFailed)
+    "is_disabled" to (task.status == CheckStatus.Failed && task.isChangedOnFailed),
+    "choice_options_style" to choiceOptionsStylesheet()
   )
 
   override fun getText(task: ChoiceTask): String {

intellij-plugin/educational-core/src/com/jetbrains/edu/learning/taskToolWindow/ui/styleManagers/StyleResourcesManager.kt
@@ -4,8 +4,6 @@ import com.intellij.openapi.diagnostic.Logger
 import com.intellij.ui.JBColor
 import com.jetbrains.edu.learning.JavaUILibrary.Companion.isJCEF
 import com.jetbrains.edu.learning.taskToolWindow.ui.EduToolsResourcesRequestHandler
-import com.jetbrains.edu.learning.taskToolWindow.ui.styleManagers.sortingBasedTask.MatchingTaskResourcesManager
-import com.jetbrains.edu.learning.taskToolWindow.ui.styleManagers.sortingBasedTask.SortingTaskResourcesManager
 import com.jetbrains.edu.learning.ui.getCurrentThemeId
 import java.net.URL
 
@@ -150,10 +148,6 @@ object StyleResourcesManager {
     resourcePair("jetbrains_academy_style_base", JETBRAINS_ACADEMY_CSS_BASE)
   )
     .plus(panelSpecificHintResources)
-    .plus(ChoiceTaskResourcesManager().resources)
-    .plus(MatchingTaskResourcesManager().resources)
-    .plus(SortingTaskResourcesManager().resources)
-    .plus(TableTaskResourcesManager().resources)
 
   private fun resourcePair(name: String, path: String) = name to resourceUrl(path)
 

intellij-plugin/educational-core/src/com/jetbrains/edu/learning/taskToolWindow/ui/styleManagers/TableTaskResourcesManager.kt
@@ -17,9 +17,6 @@ import kotlinx.css.properties.*
 class TableTaskResourcesManager: TaskResourcesManager<TableTask> {
   private val templateName = "tableTask.html"
 
-  override val resources: Map<String, String>
-    get() = mapOf("table_task_style" to stylesheet)
-
   override fun getText(task: TableTask): String {
     return if (task.rows.isNotEmpty() && task.columns.isNotEmpty()) {
       GeneratorUtils.getInternalTemplateText(templateName, getTextResources(task))
@@ -35,6 +32,7 @@ class TableTaskResourcesManager: TaskResourcesManager<TableTask> {
       "columns" to Gson().toJson(task.columns),
       "selected" to task.selected.map { it.toList()} .toList(),
       "is_checkbox" to task.isMultipleChoice,
+      "table_task_style" to stylesheet
     )
   }
 

intellij-plugin/educational-core/src/com/jetbrains/edu/learning/taskToolWindow/ui/styleManagers/TaskResourcesManager.kt
@@ -3,7 +3,6 @@ package com.jetbrains.edu.learning.taskToolWindow.ui.styleManagers
 import com.jetbrains.edu.learning.courseFormat.tasks.Task
 
 interface TaskResourcesManager<T : Task> {
-  val resources: Map<String, String>
 
   fun getText(task: T): String
 }

intellij-plugin/educational-core/src/com/jetbrains/edu/learning/taskToolWindow/ui/styleManagers/sortingBasedTask/MatchingTaskResourcesManager.kt
@@ -9,8 +9,7 @@ import kotlinx.css.*
 class MatchingTaskResourcesManager : SortingBasedTaskResourcesManager<MatchingTask>() {
   override fun getCaptions(task: MatchingTask): String = Gson().toJson(task.captions)
 
-  override val resources: Map<String, String>
-    get() = mapOf(wrapIntoStyleName(MatchingTask.MATCHING_TASK_TYPE) to stylesheet)
+  override fun taskSpecificStyles(): Map<String, String> = mapOf(wrapIntoStyleName(MatchingTask.MATCHING_TASK_TYPE) to stylesheet)
 
   override val stylesheet: String
     get() {

intellij-plugin/educational-core/src/com/jetbrains/edu/learning/taskToolWindow/ui/styleManagers/sortingBasedTask/SortingBasedTaskResourcesManager.kt
@@ -28,6 +28,8 @@ abstract class SortingBasedTaskResourcesManager<T : SortingBasedTask> : TaskReso
 
   protected abstract fun getCaptions(task: T): String
 
+  protected abstract fun taskSpecificStyles(): Map<String, String>
+
   private fun getTextResources(task: T): Map<String, String> {
     val moveUpUrl = StyleResourcesManager.resourceUrl(getIconPath(isDown = false))
     val moveDownUrl = StyleResourcesManager.resourceUrl(getIconPath(isDown = true))
@@ -41,6 +43,7 @@ abstract class SortingBasedTaskResourcesManager<T : SortingBasedTask> : TaskReso
       "tutorial" to getTutorialHTML(moveUpUrl, moveDownUrl),
       "captions" to getCaptions(task),
     )
+      .plus(taskSpecificStyles())
   }
 
   protected fun wrapIntoStyleName(s: String) = "${s}_style"

intellij-plugin/educational-core/src/com/jetbrains/edu/learning/taskToolWindow/ui/styleManagers/sortingBasedTask/SortingTaskResourcesManager.kt
@@ -7,8 +7,7 @@ import kotlinx.css.*
 class SortingTaskResourcesManager : SortingBasedTaskResourcesManager<SortingTask>() {
   override fun getCaptions(task: SortingTask): String = Gson().toJson(emptyList<String>())
 
-  override val resources: Map<String, String>
-    get() = mapOf(wrapIntoStyleName(SortingTask.SORTING_TASK_TYPE) to stylesheet)
+  override fun taskSpecificStyles(): Map<String, String> = mapOf(wrapIntoStyleName(SortingTask.SORTING_TASK_TYPE) to stylesheet)
 
   override val stylesheet: String
     get() {