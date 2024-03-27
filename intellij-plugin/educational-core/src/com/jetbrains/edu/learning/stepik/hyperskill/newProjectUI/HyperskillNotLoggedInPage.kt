a9abb7f64a7df77de09f19793ac41b37f7c2c507
Valentina Kiryushkina <valentina.kiryushkina@jetbrains.com>
2024-03-27 18:22:07 +0100

    wip
    what's wrong with colors??

--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/stepik/hyperskill/newProjectUI/HyperskillNotLoggedInPage.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/stepik/hyperskill/newProjectUI/HyperskillNotLoggedInPage.kt
@@ -1,7 +1,8 @@
 package com.jetbrains.edu.learning.stepik.hyperskill.newProjectUI
 
+import com.intellij.openapi.ui.DialogPanel
 import com.intellij.openapi.util.IconLoader
-import com.intellij.ui.Gray
+import com.intellij.ui.JBColor
 import com.intellij.ui.components.panels.Wrapper
 import com.intellij.ui.dsl.builder.AlignX
 import com.intellij.ui.dsl.builder.panel
@@ -9,35 +10,60 @@ import com.intellij.util.IconUtil
 import com.intellij.util.ui.JBDimension
 import com.intellij.util.ui.JBFont
 import com.intellij.util.ui.JBUI
+import com.jetbrains.edu.learning.messages.EduCoreBundle
 import java.awt.Color
 import java.awt.Font
 import javax.swing.JLabel
 
 class HyperskillNotLoggedInPanel : Wrapper() {
-  val academyOnHyperskillBanner = IconLoader.getIcon(
-    "selectCourseDialog/banner.svg",
-    HyperskillNotLoggedInPanel::class.java.classLoader
-  )
-  val jbaAcademyHyperskillLogoIcon = IconLoader.getIcon(
+  private val jbaAcademyHyperskillLogoIcon = IconLoader.getIcon(
     "selectCourseDialog/academyHyperskill.svg",
     HyperskillNotLoggedInPanel::class.java.classLoader
   )
-  val background = Gray.x13
+  val topPanelBackground = JBColor.namedColor("Grey2")
 
   init {
-    val leftColumn = "left"
-    val rightColumn = "right"
+    val leftPanel = createLeftPanel()
+    val banner = createBannerPanel()
 
-    val height = 250
+    val contentPanel = panel {
+      row {
+        cell(leftPanel).align(com.intellij.ui.dsl.builder.AlignY.TOP)
+        cell(banner).align(AlignX.FILL)
+      }
+    }.apply {
+      background = topPanelBackground
+      minimumSize = JBDimension(560, 250)
+      maximumSize = JBDimension(this.maximumSize.width, 250)
+    }
 
-    val topPanel = panel {
+    setContent(Wrapper(contentPanel))
+  }
+
+  private fun createBannerPanel(): DialogPanel {
+    val academyOnHyperskillBanner = IconUtil.scale(IconLoader.getIcon(
+      "selectCourseDialog/hyperskill-freemium-cover-light@2x.png",
+      HyperskillNotLoggedInPanel::class.java.classLoader
+    ), this, 0.5f)
+
+    return panel {
+      row {
+        cell(JLabel(academyOnHyperskillBanner))
+      }
+    }.apply {
+      preferredSize = JBDimension(310, 250)
+    }
+  }
+
+  private fun createLeftPanel(): DialogPanel {
+    return panel {
       row {
         val bannerHeight = 40
         val scale = JBUI.scale(bannerHeight).toFloat() / jbaAcademyHyperskillLogoIcon.iconHeight
         cell(object : JLabel(IconUtil.scale(jbaAcademyHyperskillLogoIcon, null, scale)) {
           override fun setBackground(bg: Color?) {
             // Deny changing background by the tool window framework
-            super.setBackground(background)
+            super.setBackground(topPanelBackground)
           }
         })
           .align(AlignX.LEFT)
@@ -49,56 +75,16 @@ class HyperskillNotLoggedInPanel : Wrapper() {
 
       }
       row {
-        text("JetBrains Academy on Hyperskill").applyToComponent {
+        text(EduCoreBundle.message("course.dialog.hyperskill.jba.on.hyperskill.label.text")).applyToComponent {
           font = JBFont.regular().biggerOn(7f).deriveFont(Font.PLAIN)
         }
       }
       row {
-        text(
-          "Together with Hyperskill, JetBrains Academy provides a project-based approach to learning computer science available right in " +
-          "JetBrains IDEs", maxLineLength = 55)
+        text(EduCoreBundle.message("course.dialog.hyperskill.jba.on.hyperskill.description"), maxLineLength = 55)
       }
     }.apply {
-      background = background
-      isOpaque = true
-      minimumSize = JBDimension(400, height)
-    }
-
-    val banner = panel {
-      row {
-
-        val width = 180
-
-        val scale = JBUI.scale(height).toFloat() / academyOnHyperskillBanner.iconHeight
-        cell(object : JLabel(IconUtil.scale(academyOnHyperskillBanner, null, scale)) {
-          override fun setBackground(bg: Color?) {
-            // Deny changing background by the tool window framework
-            super.setBackground(background)
-          }
-        })
-          .align(AlignX.LEFT)
-          .applyToComponent {
-            minimumSize = JBDimension(82, height)
-            preferredSize = JBDimension(width, height)
-            isOpaque = true
-          }
-      }
-    }.apply {
-      background = background
+      background = topPanelBackground
       isOpaque = true
     }
-
-    val contentPanel = panel {
-      row {
-        cell(topPanel)
-        cell(banner).align(AlignX.FILL)
-      }
-    }.apply {
-      background = Gray.x13
-      minimumSize = JBDimension(560, 250)
-      maximumSize = JBDimension(this.maximumSize.width, 250)
-    }
-
-    setContent(Wrapper(contentPanel))
   }
 }
