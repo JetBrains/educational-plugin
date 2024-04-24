938981e3d56e32f9c182a84ce2459f2eb9e0f914
anchouls <potryasaeva.anna@mail.ru>
2024-04-24 19:34:07 +0200

    Processing dsl description and draft

---
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/actions/DraftIntentionAction.kt
@@ -0,0 +1,37 @@
+package com.jetbrains.edu.learning.actions
+
+import com.intellij.codeInsight.intention.IntentionAction
+import com.intellij.icons.AllIcons
+import com.intellij.openapi.editor.CaretModel
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Iconable
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
+import com.jetbrains.edu.learning.messages.EduCoreBundle
+import javax.swing.Icon
+
+class DraftIntentionAction : IntentionAction, Iconable {
+  override fun startInWriteAction(): Boolean = false
+
+  override fun getFamilyName(): String = EduCoreBundle.message("action.draft.intention.family.name")
+
+  override fun getText(): String = EduCoreBundle.message("action.draft.intention.text")
+
+  override fun isAvailable(project: Project, editor: Editor?, file: PsiFile?): Boolean {
+    // TODO: verify that there are no errors
+    val caretModel: CaretModel = editor?.caretModel ?: return false
+    val element: PsiElement = file?.findElementAt(caretModel.offset) ?: return false
+    return element.text == DRAFT
+  }
+
+  override fun invoke(project: Project, editor: Editor?, file: PsiFile?) {
+    // TODO: replace description with draft
+  }
+
+  override fun getIcon(flags: Int): Icon = AllIcons.Actions.IntentionBulb
+
+  companion object {
+    private const val DRAFT = "draft"
+  }
+}
