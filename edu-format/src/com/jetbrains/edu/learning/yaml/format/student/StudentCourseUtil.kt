e65560d2c9d65e4295e922a4b0af6b9cc9494fca
Ilia Posov <ilia.posov@jetbrains.com>
2024-04-24 20:14:41 +0200

    EDU-6752: Write additional files to course-info.yaml
    Add the `additional_files` field to `course-info.yaml`.

--- edu-format/src/com/jetbrains/edu/learning/yaml/format/student/StudentCourseUtil.kt
+++ edu-format/src/com/jetbrains/edu/learning/yaml/format/student/StudentCourseUtil.kt
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize
 import com.fasterxml.jackson.databind.util.StdConverter
 import com.jetbrains.edu.learning.courseFormat.CourseMode
 import com.jetbrains.edu.learning.yaml.format.CourseYamlMixin
+import com.jetbrains.edu.learning.yaml.format.YamlMixinNames.ADDITIONAL_FILES
 import com.jetbrains.edu.learning.yaml.format.YamlMixinNames.CONTENT
 import com.jetbrains.edu.learning.yaml.format.YamlMixinNames.ENVIRONMENT
 import com.jetbrains.edu.learning.yaml.format.YamlMixinNames.LANGUAGE
@@ -27,6 +28,7 @@ import com.jetbrains.edu.learning.yaml.format.YamlMixinNames.TYPE
   PROGRAMMING_LANGUAGE_VERSION,
   ENVIRONMENT,
   CONTENT,
+  ADDITIONAL_FILES,
   MODE,
   TAGS
 )
