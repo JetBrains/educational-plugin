684f8200786eb5915afd5fff69075f17d847d25e
anchouls <potryasaeva.anna@mail.ru>
2024-04-24 19:34:07 +0200

    Processing dsl description and draft
    Added Edu-Jarvis module and fixed issues

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -168,6 +168,7 @@ buildCache {
 pluginManagement {
   repositories {
     mavenCentral()
+    mavenLocal()
     gradlePluginPortal()
     maven("https://oss.sonatype.org/content/repositories/snapshots/")
     if (settings.providers.gradleProperty("fleetIntegration").get().toBoolean()) {
