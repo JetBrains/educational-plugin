fe97c0c1a2fbbebad459a89f5f91b720ed2a8943
anchouls <potryasaeva.anna@mail.ru>
2024-05-15 14:01:41 +0200

    Added Edu-Jarvis module and fixed issues

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -165,7 +165,6 @@ buildCache {
 pluginManagement {
   repositories {
     mavenCentral()
-    mavenLocal()
     gradlePluginPortal()
     maven("https://oss.sonatype.org/content/repositories/snapshots/")
     if (settings.providers.gradleProperty("fleetIntegration").get().toBoolean()) {
