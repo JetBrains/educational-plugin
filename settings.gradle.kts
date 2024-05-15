b6c086f096948326ccc19b84c038603b4fcedb85
anchouls <potryasaeva.anna@mail.ru>
2024-05-15 14:01:41 +0200

    Added Edu-Jarvis module and fixed issues

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -34,6 +34,7 @@ include(
   "intellij-plugin:sql:sql-jvm",
   "intellij-plugin:github",
   "intellij-plugin:localization",
+  "intellij-plugin:Edu-Jarvis"
 )
 
 // BACKCOMPAT: 2024.1
@@ -167,7 +168,6 @@ buildCache {
 pluginManagement {
   repositories {
     mavenCentral()
-    mavenLocal()
     gradlePluginPortal()
     maven("https://oss.sonatype.org/content/repositories/snapshots/")
     if (settings.providers.gradleProperty("fleetIntegration").get().toBoolean()) {
