99f062b2f72a8111c0ae227b6d62a1256638436b
Mark Prudnikov <mark.prudnikov@jetbrains.com>
2024-05-16 20:11:57 +0000

    EDU-5592: Update `LoginWidgetFactory` once Course is set

--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/LoginWidgetFactory.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/LoginWidgetFactory.kt
@@ -1,10 +1,12 @@
 package com.jetbrains.edu.learning
 
+import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Disposer
 import com.intellij.openapi.wm.StatusBar
 import com.intellij.openapi.wm.StatusBarWidget
 import com.intellij.openapi.wm.StatusBarWidgetFactory
+import com.intellij.openapi.wm.impl.status.widget.StatusBarWidgetsManager
 import com.jetbrains.edu.coursecreator.CCUtils.isLocalCourse
 import com.jetbrains.edu.learning.EduUtilsKt.isEduProject
 import com.jetbrains.edu.learning.courseFormat.Course
@@ -19,11 +21,19 @@ abstract class LoginWidgetFactory : StatusBarWidgetFactory {
 
   override fun isAvailable(project: Project): Boolean {
     if (!project.isEduProject()) return false
-    val course = StudyTaskManager.getInstance(project).course
-    return if (course != null && !course.isPreview && !project.isLocalCourse) {
-      isWidgetAvailable(course)
+    val course = project.service<StudyTaskManager>().course
+
+    if (course == null) {
+      project.messageBus.connect().subscribe(StudyTaskManager.COURSE_SET, object : CourseSetListener {
+        override fun courseSet(course: Course) {
+          val statusBarWidgetsManager = project.service<StatusBarWidgetsManager>()
+          statusBarWidgetsManager.updateWidget(this@LoginWidgetFactory)
+        }
+      })
+      return false
     }
-    else false
+
+    return !course.isPreview && !project.isLocalCourse && isWidgetAvailable(course)
   }
 
   override fun disposeWidget(widget: StatusBarWidget) = Disposer.dispose(widget)
