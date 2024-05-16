d5883ce563f34e638c97b72e66b446acbd66cca9
Gleb Oborin <gleb.oborin@jetbrains.com>
2024-05-16 06:43:10 +0200

    EDU-6651: Use CardLayout to add LessonSelection panel to Dialog wrapper

--- intellij-plugin/educational-core/src/com/jetbrains/edu/coursecreator/ui/CCNewCourseDialog.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/coursecreator/ui/CCNewCourseDialog.kt
@@ -3,10 +3,18 @@ package com.jetbrains.edu.coursecreator.ui
 import com.intellij.openapi.ui.DialogWrapper
 import com.intellij.openapi.util.NlsContexts.Button
 import com.intellij.openapi.util.NlsContexts.DialogTitle
+import com.intellij.ui.JBCardLayout
 import com.jetbrains.edu.learning.courseFormat.Course
 import com.jetbrains.edu.learning.courseFormat.EduCourse
+import com.jetbrains.edu.learning.courseFormat.FrameworkLesson
+import com.jetbrains.edu.learning.courseFormat.Lesson
 import com.jetbrains.edu.learning.courseFormat.ext.configurator
+import com.jetbrains.edu.learning.messages.EduCoreBundle
+import java.awt.event.ActionEvent
+import javax.swing.AbstractAction
+import javax.swing.Action
 import javax.swing.JComponent
+import javax.swing.JPanel
 
 class CCNewCourseDialog @Suppress("UnstableApiUsage") constructor(
   @DialogTitle title: String,
@@ -15,16 +23,31 @@ class CCNewCourseDialog @Suppress("UnstableApiUsage") constructor(
   courseProducer: () -> Course = ::EduCourse,
   private val onOKAction: () -> Unit = {}
 ) : DialogWrapper(true) {
+  private val newCoursePanel: CCNewCoursePanel = CCNewCoursePanel(disposable, course, courseProducer)
+  private val lessonTypeSelectionPanel: LessonTypeSelectionPanel = LessonTypeSelectionPanel(disposable)
 
-  private val panel: CCNewCoursePanel = CCNewCoursePanel(disposable, course, courseProducer)
+  private val panel = CCNewCourseCardPanel()
+
+  private val nextAction = object : AbstractAction(EduCoreBundle.message("cc.new.course.button.next")) {
+    override fun actionPerformed(e: ActionEvent?) {
+      prepareLessonSelectionPanel()
+    }
+  }
+
+  private val backAction = object : AbstractAction(EduCoreBundle.message("cc.new.course.button.back")) {
+    override fun actionPerformed(e: ActionEvent?) {
+      prepareCreateCoursePanel()
+    }
+  }
 
   init {
     setTitle(title)
     setOKButtonText(okButtonText)
     init()
-    panel.setValidationListener(object : CCNewCoursePanel.ValidationListener {
+    prepareCreateCoursePanel()
+    newCoursePanel.setValidationListener(object : CCNewCoursePanel.ValidationListener {
       override fun onInputDataValidated(isInputDataComplete: Boolean) {
-        isOKActionEnabled = isInputDataComplete
+        getButton(nextAction)?.isEnabled = isInputDataComplete
       }
     })
   }
@@ -32,18 +55,71 @@ class CCNewCourseDialog @Suppress("UnstableApiUsage") constructor(
   override fun createCenterPanel(): JComponent = panel
 
   override fun doOKAction() {
-    panel.validateLocation()
-    if (!isOKActionEnabled) {
-      return
-    }
+    val validationResult = lessonTypeSelectionPanel.validateAll()
+    if (validationResult.isNotEmpty()) return
+
+    val isFrameworkLessonSelected = lessonTypeSelectionPanel.isFrameworkLessonSelected ?: return
     close(OK_EXIT_CODE)
     onOKAction()
-    val course = panel.course
-    val projectSettings = panel.projectSettings
-    val location = panel.locationString
+    val course = newCoursePanel.course
+    val projectSettings = newCoursePanel.projectSettings
+    val location = newCoursePanel.locationString
+    val lessonProducer = if (isFrameworkLessonSelected) ::FrameworkLesson else ::Lesson
     course.configurator
       ?.courseBuilder
       ?.getCourseProjectGenerator(course)
-      ?.doCreateCourseProject(location, projectSettings)
+      ?.doCreateCourseProject(location, projectSettings, lessonProducer)
+  }
+
+  override fun createActions(): Array<Action> = arrayOf(backAction, nextAction, okAction)
+
+  override fun createLeftSideActions(): Array<Action> {
+    return arrayOf(cancelAction)
+  }
+
+  private fun prepareCreateCoursePanel() {
+    getButton(backAction)?.isVisible = false
+    getButton(okAction)?.isVisible = false
+    getButton(nextAction)?.isVisible = true
+    rootPane.defaultButton = getButton(nextAction)
+    panel.showCoursePanel()
+  }
+
+  private fun prepareLessonSelectionPanel() {
+    val validationInfos = newCoursePanel.validateAll()
+    if (validationInfos.isNotEmpty()) return
+    newCoursePanel.validateLocation()
+    if (getButton(nextAction)?.isEnabled != true) {
+      return
+    }
+
+    lessonTypeSelectionPanel.course = newCoursePanel.course
+
+    getButton(backAction)?.isVisible = true
+    getButton(okAction)?.isVisible = true
+    getButton(nextAction)?.isVisible = false
+    rootPane.defaultButton = getButton(okAction)
+    panel.showLessonSelectionPanel()
+  }
+
+  inner class CCNewCourseCardPanel : JPanel() {
+    private val courseCardName = "NEW_COURSE"
+    private val lessonSelectionCardName = "LESSON_SELECTION"
+
+    private val cardLayout = JBCardLayout()
+
+    init {
+      layout = cardLayout
+      add(courseCardName, newCoursePanel)
+      add(lessonSelectionCardName, lessonTypeSelectionPanel)
+    }
+
+    fun showCoursePanel() {
+      cardLayout.show(this, courseCardName)
+    }
+
+    fun showLessonSelectionPanel() {
+      cardLayout.show(this, lessonSelectionCardName)
+    }
   }
 }
