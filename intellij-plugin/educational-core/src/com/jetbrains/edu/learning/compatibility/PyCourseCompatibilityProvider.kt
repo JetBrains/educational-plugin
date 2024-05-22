a3194af89e805ce35533b0f78398eddae3e21da8
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2024-05-22 20:57:14 +0200

    EDU-6861: require Toml plugin for python courses since 241

--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/compatibility/PyCourseCompatibilityProvider.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/compatibility/PyCourseCompatibilityProvider.kt
@@ -1,23 +1,35 @@
 package com.jetbrains.edu.learning.compatibility
 
+import com.intellij.openapi.application.ApplicationInfo
+import com.intellij.openapi.util.BuildNumber
 import com.intellij.util.PlatformUtils.*
 import com.jetbrains.edu.EducationalCoreIcons
 import com.jetbrains.edu.learning.EduUtilsKt
 import com.jetbrains.edu.learning.courseFormat.PluginInfo
-import com.jetbrains.edu.learning.courseFormat.PluginInfos
+import com.jetbrains.edu.learning.courseFormat.PluginInfos.PYTHON_COMMUNITY
+import com.jetbrains.edu.learning.courseFormat.PluginInfos.PYTHON_PRO
+import com.jetbrains.edu.learning.courseFormat.PluginInfos.TOML
 import javax.swing.Icon
+
+// BACKCOMPAT: 2023.3
+private val BUILD_241 = BuildNumber.fromString("241")!!
 
 class PyCourseCompatibilityProvider : CourseCompatibilityProvider {
 
   override fun requiredPlugins(): List<PluginInfo>? {
+    val requiredPlugins = mutableListOf<PluginInfo>()
     @Suppress("DEPRECATION", "UnstableApiUsage")
-    return when {
-      isPyCharmPro() -> listOf(PluginInfos.PYTHON_PRO)
-      isPyCharm() || isDataSpell() -> listOf(PluginInfos.PYTHON_COMMUNITY)
-      isIdeaUltimate() -> listOf(PluginInfos.PYTHON_PRO)
-      isCLion() || isIntelliJ() || EduUtilsKt.isAndroidStudio() -> listOf(PluginInfos.PYTHON_COMMUNITY)
-      else -> null
+    requiredPlugins += when {
+      isPyCharmPro() -> PYTHON_PRO
+      isPyCharm() || isDataSpell() -> PYTHON_COMMUNITY
+      isIdeaUltimate() -> PYTHON_PRO
+      isCLion() || isIntelliJ() || EduUtilsKt.isAndroidStudio() -> PYTHON_COMMUNITY
+      else -> return null
     }
+    if (ApplicationInfo.getInstance().build >= BUILD_241) {
+      requiredPlugins += TOML
+    }
+    return requiredPlugins
   }
 
   override val technologyName: String get() = "Python"
