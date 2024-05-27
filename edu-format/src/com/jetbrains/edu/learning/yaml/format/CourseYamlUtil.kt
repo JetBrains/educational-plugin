9c5cb628f2ae4ec883e1a7a84d44051e227d9e5e
Ilia Posov <ilia.posov@jetbrains.com>
2024-05-27 17:15:48 +0200

    fix yaml version serialization

--- edu-format/src/com/jetbrains/edu/learning/yaml/format/CourseYamlUtil.kt
+++ edu-format/src/com/jetbrains/edu/learning/yaml/format/CourseYamlUtil.kt
@@ -3,9 +3,18 @@
 package com.jetbrains.edu.learning.yaml.format
 
 import com.fasterxml.jackson.annotation.*
+import com.fasterxml.jackson.core.JsonGenerator
+import com.fasterxml.jackson.databind.JavaType
+import com.fasterxml.jackson.databind.SerializerProvider
+import com.fasterxml.jackson.databind.annotation.JsonAppend
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize
 import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder
 import com.fasterxml.jackson.databind.annotation.JsonSerialize
+import com.fasterxml.jackson.databind.cfg.MapperConfig
+import com.fasterxml.jackson.databind.introspect.AnnotatedClass
+import com.fasterxml.jackson.databind.introspect.BeanPropertyDefinition
+import com.fasterxml.jackson.databind.ser.VirtualBeanPropertyWriter
+import com.fasterxml.jackson.databind.util.Annotations
 import com.fasterxml.jackson.databind.util.StdConverter
 import com.jetbrains.edu.learning.courseFormat.*
 import com.jetbrains.edu.learning.courseFormat.EduFormatNames.DEFAULT_ENVIRONMENT
@@ -49,6 +58,11 @@ import java.util.*
  * Update [com.jetbrains.edu.learning.yaml.format.CourseChangeApplier] and [CourseBuilder] if new fields added to mixin
  */
 @Suppress("unused") // used for yaml serialization
+@JsonAppend(
+  props = [
+    JsonAppend.Prop(YamlVersionWriter::class, name=YAML_VERSION)
+  ]
+)
 @JsonPropertyOrder(
   YAML_VERSION,
   TYPE,
@@ -76,8 +90,6 @@ import java.util.*
   JsonSubTypes.Type(EduCourse::class, name = "marketplace")
 )
 abstract class CourseYamlMixin {
-  @JsonProperty(YAML_VERSION)
-  val yamlVersion: Int = CURRENT_YAML_VERSION
 
   val itemType: String
     @JsonSerialize(converter = CourseTypeSerializationConverter::class)
@@ -147,6 +159,28 @@ abstract class CourseYamlMixin {
 
   @JsonIgnore
   private var programmingLanguage: String? = null
+}
+
+private class YamlVersionWriter : VirtualBeanPropertyWriter {
+  override fun value(bean: Any?, gen: JsonGenerator?, prov: SerializerProvider?): Any = CURRENT_YAML_VERSION
+
+  @Suppress("unused")
+  constructor()
+
+  constructor(
+    propDef: BeanPropertyDefinition?,
+    contextAnnotations: Annotations?,
+    declaredType: JavaType?
+  ) : super(propDef, contextAnnotations, declaredType)
+
+  override fun withConfig(
+    config: MapperConfig<*>?,
+    declaringClass: AnnotatedClass?,
+    propDef: BeanPropertyDefinition?,
+    type: JavaType?
+  ): VirtualBeanPropertyWriter {
+    return YamlVersionWriter(propDef, declaringClass?.annotations, type)
+  }
 }
 
 private class ProgrammingLanguageConverter : StdConverter<String, String>() {
