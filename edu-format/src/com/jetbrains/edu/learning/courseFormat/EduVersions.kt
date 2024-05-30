65596ad073fe71840978f8356b904819842937d0
Ilia Posov <ilia.posov@jetbrains.com>
2024-05-30 13:02:30 +0200

    EDU-6878: Generate new version of course archive under a feature flag

--- edu-format/src/com/jetbrains/edu/learning/courseFormat/EduVersions.kt
+++ edu-format/src/com/jetbrains/edu/learning/courseFormat/EduVersions.kt
@@ -4,6 +4,8 @@ package com.jetbrains.edu.learning.courseFormat
 
 // If you change version of any format, add point about it in `documentation/Versions.md`
 const val JSON_FORMAT_VERSION: Int = 18
+// We need the two versions here because the 19's version is generated with the feature flag COURSE_FORMAT_WITH_FILES_OUTSIDE_JSON
+const val JSON_FORMAT_VERSION_WITH_FILES_OUTSIDE: Int = 19
 
 /**
  * Since that version we deprecated [com.jetbrains.edu.learning.json.mixins.JsonMixinNames.PROGRAMMING_LANGUAGE] and started to use
