0498d00062f901f5ccf012e6420f1d8936b84169
Ilia Posov <ilia.posov@jetbrains.com>
2024-05-31 16:52:30 +0200

    EDU-6396: Fix for tests that generate course.json

--- intellij-plugin/educational-core/testSrc/com/jetbrains/edu/coursecreator/actions/CourseArchiveTestBase.kt
+++ intellij-plugin/educational-core/testSrc/com/jetbrains/edu/coursecreator/actions/CourseArchiveTestBase.kt
@@ -7,15 +7,31 @@ import com.intellij.openapi.util.io.FileUtil
 import com.intellij.openapi.util.text.StringUtilRt
 import com.jetbrains.edu.coursecreator.CCUtils
 import com.jetbrains.edu.learning.EduActionTestCase
+import com.jetbrains.edu.learning.EduExperimentalFeatures.COURSE_FORMAT_WITH_FILES_OUTSIDE_JSON
 import com.jetbrains.edu.learning.StudyTaskManager
+import com.jetbrains.edu.learning.courseFormat.copy
 import com.jetbrains.edu.learning.json.encrypt.TEST_AES_KEY
+import com.jetbrains.edu.learning.withFeature
 import java.io.File
 
 abstract class CourseArchiveTestBase : EduActionTestCase() {
   protected fun doTest() {
-    val generatedJsonFile = generateJson()
     val expectedCourseJson = loadExpectedJson()
-    assertEquals(expectedCourseJson, generatedJsonFile)
+
+    withFeature(COURSE_FORMAT_WITH_FILES_OUTSIDE_JSON, false) {
+      val generatedJsonFile = generateJson()
+      assertEquals(expectedCourseJson, generatedJsonFile)
+    }
+
+    withFeature(COURSE_FORMAT_WITH_FILES_OUTSIDE_JSON, true) {
+      // TODO: when COURSE_FORMAT_WITH_FILES_OUTSIDE_JSON feature is removed remove "text" fields from test jsons
+      val expectedCourseJsonVersion19 = expectedCourseJson
+        .replace(""""version" : \d{1,2}""".toRegex(), """"version" : 19""")
+        .replace("""^\s*"text" : ".{2,}",\n""".toRegex(RegexOption.MULTILINE), "")
+
+      val generatedJsonFile = generateJson()
+      assertEquals(expectedCourseJsonVersion19, generatedJsonFile)
+    }
   }
 
   private fun loadExpectedJson(): String {
@@ -25,10 +41,16 @@ abstract class CourseArchiveTestBase : EduActionTestCase() {
 
   protected fun generateJson(): String {
     val course = StudyTaskManager.getInstance(project).course ?: error("No course found")
+
+    // TODO: when COURSE_FORMAT_WITH_FILES_OUTSIDE_JSON is removed, do not copy the course
+
+    val copiedCourse = course.copy()
+    copiedCourse.authors = course.authors
+
     val creator = getArchiveCreator()
-    creator.prepareCourse(course)
-    val mapper = creator.getMapper(course)
-    val json = mapper.writer(printer).writeValueAsString(course)
+    creator.prepareCourse(copiedCourse)
+    val mapper = creator.getMapper(copiedCourse)
+    val json = mapper.writer(printer).writeValueAsString(copiedCourse)
     return StringUtilRt.convertLineSeparators(json).replace("\\n\\n".toRegex(), "\n")
   }
 
