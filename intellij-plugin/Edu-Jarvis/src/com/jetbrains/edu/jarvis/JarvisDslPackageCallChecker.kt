432714399a784cb25559315cb41383a67e9274a4
anchouls <potryasaeva.anna@mail.ru>
2024-05-31 10:48:06 +0200

    Added jarvis dsl package checker

---
+++ intellij-plugin/Edu-Jarvis/src/com/jetbrains/edu/jarvis/JarvisDslPackageCallChecker.kt
@@ -0,0 +1,20 @@
+package com.jetbrains.edu.jarvis
+
+import com.intellij.lang.Language
+import com.intellij.lang.LanguageExtension
+import com.intellij.psi.PsiElement
+import com.intellij.util.concurrency.ThreadingAssertions
+
+interface JarvisDslPackageCallChecker {
+  fun isCallFromJarvisDslPackage(element: PsiElement): Boolean
+
+  companion object {
+    private val EP_NAME = LanguageExtension<JarvisDslPackageCallChecker>("Educational.jarvisDslPackageCallChecker")
+
+    fun isCallFromJarvisDslPackage(element: PsiElement, language: Language): Boolean {
+      ThreadingAssertions.assertReadAccess()
+      return EP_NAME.forLanguage(language)?.isCallFromJarvisDslPackage(element)
+      ?: error("Not supported to provide a draft expression for the ${language.displayName} language")
+    }
+  }
+}
