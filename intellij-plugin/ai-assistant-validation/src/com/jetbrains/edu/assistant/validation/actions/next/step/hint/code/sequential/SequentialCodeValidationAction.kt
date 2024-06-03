f078a8d3aa1b2c9920d1c37472c0085e8996df45
anchouls <potryasaeva.anna@mail.ru>
2024-06-03 17:22:39 +0200

    Added auto validation for compilation error hints

--- intellij-plugin/ai-assistant-validation/src/com/jetbrains/edu/assistant/validation/actions/next/step/hint/code/sequential/SequentialCodeValidationAction.kt
+++ intellij-plugin/ai-assistant-validation/src/com/jetbrains/edu/assistant/validation/actions/next/step/hint/code/sequential/SequentialCodeValidationAction.kt
@@ -1,37 +1,28 @@
 package com.jetbrains.edu.assistant.validation.actions.next.step.hint.code.sequential
 
-<<<<<<< HEAD
-=======
 import com.intellij.openapi.actionSystem.ex.ActionUtil
 import com.intellij.openapi.actionSystem.impl.SimpleDataContext
 import com.intellij.openapi.application.EDT
->>>>>>> 21c17561e (Added validation for compilation error hints)
 import com.intellij.openapi.progress.runBlockingCancellable
 import com.intellij.openapi.project.DumbService
 import com.intellij.openapi.project.Project
 import com.intellij.platform.ide.progress.withBackgroundProgress
 import com.jetbrains.edu.assistant.validation.actions.next.step.hint.BaseAssistantInfoStorage
 import com.jetbrains.edu.assistant.validation.actions.next.step.hint.code.CodeValidationAction
-<<<<<<< HEAD
 import com.jetbrains.edu.assistant.validation.util.runCheckAction
-=======
 import com.jetbrains.edu.learning.actions.CheckAction
 import com.jetbrains.edu.learning.checker.CheckListener
->>>>>>> 21c17561e (Added validation for compilation error hints)
+import com.jetbrains.edu.assistant.validation.util.runCheckAction
 import com.jetbrains.edu.learning.courseFormat.CheckStatus
 import com.jetbrains.edu.learning.courseFormat.Lesson
 import com.jetbrains.edu.learning.courseFormat.tasks.EduTask
 import com.jetbrains.edu.learning.courseFormat.tasks.Task
-<<<<<<< HEAD
 import com.jetbrains.edu.learning.eduAssistant.core.AssistantResponse
 import kotlinx.coroutines.delay
-=======
 import com.jetbrains.edu.learning.eduAssistant.check.EduAssistantValidationCheckListener
 import com.jetbrains.edu.learning.eduAssistant.core.AssistantResponse
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.withContext
->>>>>>> 21c17561e (Added validation for compilation error hints)
 
 abstract class SequentialCodeValidationAction<T> : CodeValidationAction<T>() {
 
@@ -83,11 +74,7 @@ abstract class SequentialCodeValidationAction<T> : CodeValidationAction<T>() {
 
     // To avoid saving old task states
     task.resetStatus()
-<<<<<<< HEAD
     runCheckAction(baseAssistantInfoStorage.project)
-=======
-    runCheckAction(baseAssistantInfoStorage)
->>>>>>> 21c17561e (Added validation for compilation error hints)
 
     val records = mutableListOf<T>()
     var currentUserCode = userCode
@@ -109,11 +96,7 @@ abstract class SequentialCodeValidationAction<T> : CodeValidationAction<T>() {
 
         lesson.replaceContent(task, currentUserCode, baseAssistantInfoStorage.eduState, baseAssistantInfoStorage.project)
 
-<<<<<<< HEAD
         runCheckAction(baseAssistantInfoStorage.project)
-=======
-        runCheckAction(baseAssistantInfoStorage)
->>>>>>> 21c17561e (Added validation for compilation error hints)
 
         records.add(getRecord(task, hintIndex, baseAssistantInfoStorage, response, userCode, currentUserCode))
 
@@ -129,23 +112,6 @@ abstract class SequentialCodeValidationAction<T> : CodeValidationAction<T>() {
     lesson.replaceContent(task, currentUserCode, baseAssistantInfoStorage.eduState, baseAssistantInfoStorage.project)
     return records
   }
-
-<<<<<<< HEAD
-=======
-  private suspend fun runCheckAction(baseAssistantInfoStorage: BaseAssistantInfoStorage) {
-    val checkListener = CheckListener.EP_NAME.findExtension(EduAssistantValidationCheckListener::class.java)
-                        ?: error("Check listener not found")
-    checkListener.clear()
-
-    withContext(Dispatchers.EDT) {
-      val dataContext = SimpleDataContext.getProjectContext(baseAssistantInfoStorage.project)
-      ActionUtil.invokeAction(CheckAction(), dataContext, "", null, null)
-    }
-
-    checkListener.wait()
-  }
-
->>>>>>> 21c17561e (Added validation for compilation error hints)
   companion object {
     private const val GETTING_HINT_MESSAGE = "Getting Hint"
     private const val MAX_HINTS = 10
