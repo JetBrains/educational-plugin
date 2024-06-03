0e933197259a0df7f555db025b7a4e9ca18ba52f
anchouls <potryasaeva.anna@mail.ru>
2024-06-03 11:43:42 +0200

    Added the description for intention

--- intellij-plugin/Edu-Kotlin/src/com/jetbrains/edu/kotlin/jarvis/psi/KtDescriptionExpressionParser.kt
+++ intellij-plugin/Edu-Kotlin/src/com/jetbrains/edu/kotlin/jarvis/psi/KtDescriptionExpressionParser.kt
@@ -3,8 +3,8 @@ package com.jetbrains.edu.kotlin.jarvis.psi
 import com.intellij.psi.PsiElement
 import com.intellij.psi.util.PsiTreeUtil
 import com.jetbrains.edu.jarvis.DescriptionExpressionParser
+import com.jetbrains.edu.jarvis.JarvisDslPackageCallChecker
 import com.jetbrains.edu.kotlin.jarvis.DescriptionRunLineMarkerContributor.Companion.DESCRIPTION
-import com.jetbrains.edu.kotlin.jarvis.psi.KtJarvisDslPackageCallChecker.Companion.isCallFromJarvisDslPackage
 import com.jetbrains.edu.learning.courseFormat.jarvis.DescriptionExpression
 import org.jetbrains.kotlin.psi.KtCallExpression
 
@@ -23,6 +23,6 @@ class KtDescriptionExpressionParser : DescriptionExpressionParser {
   private fun existsNestedDescriptionExpressions(descriptionExpression: KtCallExpression) =
     PsiTreeUtil.findChildrenOfType(descriptionExpression, KtCallExpression::class.java).any {
       it.calleeExpression?.text == DESCRIPTION &&
-      isCallFromJarvisDslPackage(it)
+      JarvisDslPackageCallChecker.isCallFromJarvisDslPackage(it, it.language)
     }
 }
