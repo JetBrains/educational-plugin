f00f510db6674e5c39c309829191704c95627dce
anchouls <potryasaeva.anna@mail.ru>
2024-06-05 14:52:37 +0200

    Added ability to completion in a prompt inside a description block
    Added comments

---
+++ intellij-plugin/Edu-Kotlin/src/com/jetbrains/edu/kotlin/jarvis/completion/JarvisCompletionConfidence.kt
@@ -0,0 +1,29 @@
+package com.jetbrains.edu.kotlin.jarvis.completion
+
+import com.intellij.codeInsight.completion.CompletionConfidence
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
+import com.intellij.util.ThreeState
+import com.jetbrains.edu.jarvis.JarvisDslPackageCallChecker
+import com.jetbrains.edu.kotlin.jarvis.DescriptionRunLineMarkerContributor.Companion.DESCRIPTION
+import org.jetbrains.kotlin.psi.KtLiteralStringTemplateEntry
+
+
+/**
+ * Decides whether the autocomplete functionality should be skipped or not within a given context for the Jarvis application.
+ * If the current element is part of a string literal in a `description` method call, the autocomplete functionality should not be skipped.
+ *
+ * @see CompletionConfidence
+ */
+class JarvisCompletionConfidence : CompletionConfidence() {
+  override fun shouldSkipAutopopup(contextElement: PsiElement, psiFile: PsiFile, offset: Int): ThreeState {
+    val functionElement = contextElement.parent.parent.parent.parent.parent
+    if (contextElement.parent is KtLiteralStringTemplateEntry &&
+        JarvisDslPackageCallChecker.isCallFromJarvisDslPackage(functionElement, functionElement.language) &&
+        functionElement.text.startsWith(DESCRIPTION)) {
+      return ThreeState.NO
+    }
+
+    return ThreeState.UNSURE
+  }
+}
