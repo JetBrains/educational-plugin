6841dfb22ccb2068e3e3f25faf4142f67a0435e6
anchouls <potryasaeva.anna@mail.ru>
2024-06-05 18:36:11 +0200

    Added the ability to apply code inside the draft block

--- intellij-plugin/Edu-Kotlin/src/com/jetbrains/edu/kotlin/jarvis/psi/KtDescriptionExpressionParser.kt
+++ intellij-plugin/Edu-Kotlin/src/com/jetbrains/edu/kotlin/jarvis/psi/KtDescriptionExpressionParser.kt
@@ -4,25 +4,25 @@ import com.intellij.psi.PsiElement
 import com.intellij.psi.util.PsiTreeUtil
 import com.jetbrains.edu.jarvis.DescriptionExpressionParser
 import com.jetbrains.edu.jarvis.JarvisDslPackageCallChecker
-import com.jetbrains.edu.kotlin.jarvis.DescriptionRunLineMarkerContributor.Companion.DESCRIPTION
+import com.jetbrains.edu.kotlin.jarvis.utils.DESCRIPTION
 import com.jetbrains.edu.learning.courseFormat.jarvis.DescriptionExpression
 import org.jetbrains.kotlin.psi.KtCallExpression
 
 class KtDescriptionExpressionParser : DescriptionExpressionParser {
   override fun parseDescriptionExpression(descriptionExpression: PsiElement): DescriptionExpression? {
-    if (descriptionExpression is KtCallExpression) {
-      if (existsNestedDescriptionExpressions(descriptionExpression)) return null
-      return DescriptionExpression(
-        descriptionExpression.valueArguments.first().text,
-        descriptionExpression.lambdaArguments.first().text
-      )
-    }
-    return null
+    if (!isDescriptionBlock(descriptionExpression) ||
+        descriptionExpression !is KtCallExpression ||
+        existsNestedDescriptionExpressions(descriptionExpression))
+      return null
+    return DescriptionExpression(
+      descriptionExpression.valueArguments.firstOrNull()?.text ?: "",
+      descriptionExpression.lambdaArguments.firstOrNull()?.getLambdaExpression()?.bodyExpression?.text ?: ""
+    )
   }
 
   private fun existsNestedDescriptionExpressions(descriptionExpression: KtCallExpression) =
-    PsiTreeUtil.findChildrenOfType(descriptionExpression, KtCallExpression::class.java).any {
-      it.calleeExpression?.text == DESCRIPTION &&
-      JarvisDslPackageCallChecker.isCallFromJarvisDslPackage(it, it.language)
-    }
+    PsiTreeUtil.findChildrenOfType(descriptionExpression, KtCallExpression::class.java).any { isDescriptionBlock(it) }
+
+  private fun isDescriptionBlock(element: PsiElement) = element.text.startsWith(DESCRIPTION) &&
+                      JarvisDslPackageCallChecker.isCallFromJarvisDslPackage(element, element.language)
 }
