1823e977f1b71160c782c096496747a4a337a273
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2024-06-11 00:52:15 +0200

    Refactor `JVersionValidationTest`
    - move `languageLevel` iteration from test code to test data
    - don't use deprecated `MockSdk`
    - use `assertIs` from `kotlin.test` to utilize kotlin smart casts
    - create language settings via `EduCourseBuilder.getLanguageSettings` instead of manual creation to check actual implementation

--- intellij-plugin/Edu-Java/testSrc/com/jetbrains/edu/java/environmentSettings/JVersionValidationTest.kt
+++ intellij-plugin/Edu-Java/testSrc/com/jetbrains/edu/java/environmentSettings/JVersionValidationTest.kt
@@ -1,59 +1,53 @@
 package com.jetbrains.edu.java.environmentSettings
 
+import com.intellij.lang.java.JavaLanguage
+import com.intellij.openapi.projectRoots.JavaSdk
 import com.intellij.openapi.projectRoots.JavaSdkVersion.*
-import com.intellij.openapi.projectRoots.impl.JavaSdkImpl
-import com.intellij.openapi.projectRoots.impl.MockSdk
+import com.intellij.openapi.projectRoots.impl.ProjectJdkImpl
 import com.intellij.pom.java.LanguageLevel
 import com.intellij.testFramework.LightPlatformTestCase
-import com.intellij.util.containers.MultiMap
-import com.jetbrains.edu.java.JLanguageSettings
+import com.jetbrains.edu.jvm.JdkLanguageSettings
 import com.jetbrains.edu.jvm.setLanguageLevel
 import com.jetbrains.edu.learning.course
+import com.jetbrains.edu.learning.courseFormat.ext.configurator
 import com.jetbrains.edu.learning.newproject.ui.errors.SettingsValidationResult
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
 import org.junit.runners.Parameterized.Parameters
+import kotlin.test.assertIs
 
 @RunWith(Parameterized::class)
 class JVersionValidationTest(
   private val languageVersion: String?,
+  private val languageLevel: String?,
   private val selectedSdkVersion: String?,
   private val expectedValidationMessage: String?
 ) : LightPlatformTestCase() {
 
   @Test
   fun `jdk validation messages`() {
-    val languageLevels = listOf(null, LanguageLevel.JDK_19.name, /*unsupported*/ "57.121")
-
-    //iterate through all language levels, but the validation result must not depend on it
-    for (languageLevel in languageLevels) {
-      val validationMessage = validateSdk(languageLevel)
-      assertEquals(expectedValidationMessage, validationMessage)
-    }
-  }
-
-  private fun validateSdk(languageLevel: String?): String? {
-    val course = course {}
-    course.languageId = "Java"
+    val course = course(language = JavaLanguage.INSTANCE) {}
     course.languageVersion = languageVersion
     course.setLanguageLevel(languageLevel)
-
-    val sdk = selectedSdkVersion?.let { MockSdk(it, "", it, MultiMap(), JavaSdkImpl()) }
-    val languageSettings = JLanguageSettings()
+    val sdk = selectedSdkVersion?.let { ProjectJdkImpl(it, JavaSdk.getInstance(), "", it) }
+    val languageSettings = course.configurator?.courseBuilder?.getLanguageSettings()
+    assertIs<JdkLanguageSettings>(languageSettings)
     languageSettings.selectJdk(sdk)
 
-    val validation = languageSettings.validate(course, "")
+    val validationResult = languageSettings.validate(course, "")
+    assertIs<SettingsValidationResult.Ready>(validationResult)
 
-    assertTrue(validation is SettingsValidationResult.Ready)
-    val ready = validation as SettingsValidationResult.Ready
-    return ready.validationMessage?.message
+    val validationMessage = validationResult.validationMessage?.message
+    assertEquals(expectedValidationMessage, validationMessage)
   }
 
   companion object {
     @JvmStatic
-    @Parameters(name = "language version: {0}, selected sdk version: {1}")
+    @Parameters(name = "language version: {0}, language level: {1}, selected sdk version: {2}")
     fun data(): Collection<Array<Any?>> {
+      val languageLevels = listOf(null, LanguageLevel.JDK_19.name, /*unsupported*/ "57.121")
+
       return listOf(
         arrayOf(null, null, "JDK is not selected. In the settings section, choose or download some JDK with a version at least 8"),
         arrayOf(null, JDK_11.description, null),
@@ -80,7 +74,12 @@ class JVersionValidationTest(
         arrayOf("6.67", JDK_17.description, "Unsupported Java version: 6.67"),
         arrayOf("6.67", JDK_19.description, "Unsupported Java version: 6.67"),
         arrayOf("6.67", "2.39", "Unsupported Java version: 6.67")
-      )
+      ).flatMap { (languageVersion, sdkVersion, message) ->
+        // iterate through all language levels, but the validation result must not depend on it for Java
+        languageLevels.map { languageLevel ->
+          arrayOf(languageVersion, languageLevel, sdkVersion, message)
+        }
+      }
     }
   }
 }
