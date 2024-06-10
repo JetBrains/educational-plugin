255b11c439e2042894135ec15be21b44e46fac6b
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2024-06-11 00:45:59 +0200

    EDU-6789: migrate `JVersionValidationTest` to JUnit 4

--- intellij-plugin/Edu-Java/testSrc/com/jetbrains/edu/java/environmentSettings/JVersionValidationTest.kt
+++ intellij-plugin/Edu-Java/testSrc/com/jetbrains/edu/java/environmentSettings/JVersionValidationTest.kt
@@ -1,6 +1,6 @@
 package com.jetbrains.edu.java.environmentSettings
 
-import com.intellij.openapi.projectRoots.JavaSdkVersion
+import com.intellij.openapi.projectRoots.JavaSdkVersion.*
 import com.intellij.openapi.projectRoots.impl.JavaSdkImpl
 import com.intellij.openapi.projectRoots.impl.MockSdk
 import com.intellij.pom.java.LanguageLevel
@@ -10,85 +10,77 @@ import com.jetbrains.edu.java.JLanguageSettings
 import com.jetbrains.edu.jvm.setLanguageLevel
 import com.jetbrains.edu.learning.course
 import com.jetbrains.edu.learning.newproject.ui.errors.SettingsValidationResult
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+import org.junit.runners.Parameterized.Parameters
 
-class JVersionValidationTest : LightPlatformTestCase() {
+@RunWith(Parameterized::class)
+class JVersionValidationTest(
+  private val languageVersion: String?,
+  private val selectedSdkVersion: String?,
+  private val expectedValidationMessage: String?
+) : LightPlatformTestCase() {
 
-  //ashjdvashd
-  //ashbdhsadb
-  fun `test jdk validation messages`() {
-    val course = course {}
-
-    val courseLanguageVersions = listOf(null, JavaSdkVersion.JDK_11, JavaSdkVersion.JDK_17, JavaSdkVersion.JDK_19)
-      .map { it?.description }
-      .plus("6.67") // unsupported
-
+  @Test
+  fun `jdk validation messages`() {
     val languageLevels = listOf(null, LanguageLevel.JDK_19.name, /*unsupported*/ "57.121")
 
-    val selectedSdkVersions = listOf(null, JavaSdkVersion.JDK_11, JavaSdkVersion.JDK_17, JavaSdkVersion.JDK_19)
-      .map { it?.description }
-      .plus("2.39") // unsupported
-
-    fun doTest(courseLanguageVersion: String?, languageLevel: String?, selectedSdkVersion: String?): String? {
-      course.setLanguageLevel(languageLevel)
-      course.languageId = "Java"
-      course.languageVersion = courseLanguageVersion
-
-      val sdk = selectedSdkVersion?.let { MockSdk(it, "", it, MultiMap(), JavaSdkImpl()) }
-
-      val languageSettings = JLanguageSettings()
-      languageSettings.selectJdk(sdk)
-
-      val validation = languageSettings.validate(course, "")
-
-      assertTrue(validation is SettingsValidationResult.Ready)
-      val ready = validation as SettingsValidationResult.Ready
-      return ready.validationMessage?.message
+    //iterate through all language levels, but the validation result must not depend on it
+    for (languageLevel in languageLevels) {
+      val validationMessage = validateSdk(languageLevel)
+      assertEquals(expectedValidationMessage, validationMessage)
     }
+  }
 
-    val csv = StringBuffer("course language|selected jdk|validation message\n")
-    for (courseLanguageVersion in courseLanguageVersions)
-      for (selectedSdkVersion in selectedSdkVersions) {
-        //iterate through all language levels, but the validation result must not depend on it
-        val firstTestResult = doTest(courseLanguageVersion, languageLevels[0], selectedSdkVersion)
+  private fun validateSdk(languageLevel: String?): String? {
+    val course = course {}
+    course.languageId = "Java"
+    course.languageVersion = languageVersion
+    course.setLanguageLevel(languageLevel)
 
-        for (languageLevel in languageLevels.drop(1)) {
-          val anotherTestResult = doTest(courseLanguageVersion, languageLevel, selectedSdkVersion)
-          assertEquals(firstTestResult, anotherTestResult)
-        }
+    val sdk = selectedSdkVersion?.let { MockSdk(it, "", it, MultiMap(), JavaSdkImpl()) }
+    val languageSettings = JLanguageSettings()
+    languageSettings.selectJdk(sdk)
 
-        csv.append("$courseLanguageVersion|$selectedSdkVersion|$firstTestResult\n")
-      }
+    val validation = languageSettings.validate(course, "")
 
-    val expectedTestResult = """
-      course language|selected jdk|validation message
-      null|null|JDK is not selected. In the settings section, choose or download some JDK with a version at least 8
-      null|11|null
-      null|17|null
-      null|19|null
-      null|2.39|Failed to determine Java version from string: 2.39. In the settings section, choose or download another JDK
-      11|null|JDK is not selected. In the settings section, choose or download some JDK with a version at least 11
-      11|11|null
-      11|17|null
-      11|19|null
-      11|2.39|Failed to determine Java version from string: 2.39. In the settings section, choose or download another JDK
-      17|null|JDK is not selected. In the settings section, choose or download some JDK with a version at least 17
-      17|11|Your Java version is 11, while it should be at least 17. In the settings section, choose or download a newer version of JDK
-      17|17|null
-      17|19|null
-      17|2.39|Failed to determine Java version from string: 2.39. In the settings section, choose or download another JDK
-      19|null|JDK is not selected. In the settings section, choose or download some JDK with a version at least 19
-      19|11|Your Java version is 11, while it should be at least 19. In the settings section, choose or download a newer version of JDK
-      19|17|Your Java version is 17, while it should be at least 19. In the settings section, choose or download a newer version of JDK
-      19|19|null
-      19|2.39|Failed to determine Java version from string: 2.39. In the settings section, choose or download another JDK
-      6.67|null|Unsupported Java version: 6.67
-      6.67|11|Unsupported Java version: 6.67
-      6.67|17|Unsupported Java version: 6.67
-      6.67|19|Unsupported Java version: 6.67
-      6.67|2.39|Unsupported Java version: 6.67
-      
-    """.trimIndent()
+    assertTrue(validation is SettingsValidationResult.Ready)
+    val ready = validation as SettingsValidationResult.Ready
+    return ready.validationMessage?.message
+  }
 
-    assertEquals(expectedTestResult, csv.toString())
+  companion object {
+    @JvmStatic
+    @Parameters(name = "language version: {0}, selected sdk version: {1}")
+    fun data(): Collection<Array<Any?>> {
+      return listOf(
+        arrayOf(null, null, "JDK is not selected. In the settings section, choose or download some JDK with a version at least 8"),
+        arrayOf(null, JDK_11.description, null),
+        arrayOf(null, JDK_17.description, null),
+        arrayOf(null, JDK_19.description, null),
+        arrayOf(null, "2.39", "Failed to determine Java version from string: 2.39. In the settings section, choose or download another JDK"),
+        arrayOf(JDK_11.description, null, "JDK is not selected. In the settings section, choose or download some JDK with a version at least 11"),
+        arrayOf(JDK_11.description, JDK_11.description, null),
+        arrayOf(JDK_11.description, JDK_17.description, null),
+        arrayOf(JDK_11.description, JDK_19.description, null),
+        arrayOf(JDK_11.description, "2.39", "Failed to determine Java version from string: 2.39. In the settings section, choose or download another JDK"),
+        arrayOf(JDK_17.description, null, "JDK is not selected. In the settings section, choose or download some JDK with a version at least 17"),
+        arrayOf(JDK_17.description, JDK_11.description, "Your Java version is 11, while it should be at least 17. In the settings section, choose or download a newer version of JDK"),
+        arrayOf(JDK_17.description, JDK_17.description, null),
+        arrayOf(JDK_17.description, JDK_19.description, null),
+        arrayOf(JDK_17.description, "2.39", "Failed to determine Java version from string: 2.39. In the settings section, choose or download another JDK"),
+        arrayOf(JDK_19.description, null, "JDK is not selected. In the settings section, choose or download some JDK with a version at least 19"),
+        arrayOf(JDK_19.description, JDK_11.description, "Your Java version is 11, while it should be at least 19. In the settings section, choose or download a newer version of JDK"),
+        arrayOf(JDK_19.description, JDK_17.description, "Your Java version is 17, while it should be at least 19. In the settings section, choose or download a newer version of JDK"),
+        arrayOf(JDK_19.description, JDK_19.description, null),
+        arrayOf(JDK_19.description, "2.39", "Failed to determine Java version from string: 2.39. In the settings section, choose or download another JDK"),
+        arrayOf("6.67", null, "Unsupported Java version: 6.67"),
+        arrayOf("6.67", JDK_11.description, "Unsupported Java version: 6.67"),
+        arrayOf("6.67", JDK_17.description, "Unsupported Java version: 6.67"),
+        arrayOf("6.67", JDK_19.description, "Unsupported Java version: 6.67"),
+        arrayOf("6.67", "2.39", "Unsupported Java version: 6.67")
+      )
+    }
   }
 }
