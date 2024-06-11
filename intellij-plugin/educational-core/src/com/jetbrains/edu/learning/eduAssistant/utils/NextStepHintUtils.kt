24399480e6954b385dbb28233518cc9d4ba3a75e
anchouls <potryasaeva.anna@mail.ru>
2024-06-11 12:57:14 +0100

    Fixed comments

--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/eduAssistant/utils/NextStepHintUtils.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/eduAssistant/utils/NextStepHintUtils.kt
@@ -9,4 +9,4 @@ import com.jetbrains.edu.learning.courseFormat.tasks.Task
 // Only for the Kotlin Onboarding Introduction: https://plugins.jetbrains.com/plugin/21067-kotlin-onboarding-introduction and for Edu tasks
 fun isNextStepHintApplicable(task: Task) = task.course.id == 21067 && task is EduTask
 
-fun isGetHintButtonShown(task: Task) = isNextStepHintApplicable(task) && task.course.courseMode == CourseMode.STUDENT && task.status != CheckStatus.Solved // TODO: when should we show this button?
+fun isGetHintButtonShown(task: Task) = isNextStepHintApplicable(task) && task.course.courseMode == CourseMode.STUDENT && task.status == CheckStatus.Failed // TODO: when should we show this button?
