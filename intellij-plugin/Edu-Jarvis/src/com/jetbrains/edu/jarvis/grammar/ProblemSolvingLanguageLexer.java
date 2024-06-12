2b67acb2525052bc47bbcc86d4ecbe763352fa9a
anchouls <potryasaeva.anna@mail.ru>
2024-06-12 11:36:15 +0200

    Removed unused imports

--- intellij-plugin/Edu-Jarvis/src/com/jetbrains/edu/jarvis/grammar/ProblemSolvingLanguageLexer.java
+++ intellij-plugin/Edu-Jarvis/src/com/jetbrains/edu/jarvis/grammar/ProblemSolvingLanguageLexer.java
@@ -3,12 +3,9 @@ package com.jetbrains.edu.jarvis.grammar;
 
 import org.antlr.v4.runtime.Lexer;
 import org.antlr.v4.runtime.CharStream;
-import org.antlr.v4.runtime.Token;
-import org.antlr.v4.runtime.TokenStream;
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.atn.*;
 import org.antlr.v4.runtime.dfa.DFA;
-import org.antlr.v4.runtime.misc.*;
 
 @SuppressWarnings({"all", "warnings", "unchecked", "unused", "cast", "CheckReturnValue", "this-escape"})
 public class ProblemSolvingLanguageLexer extends Lexer {
--- intellij-plugin/Edu-Jarvis/src/com/jetbrains/edu/jarvis/grammar/ProblemSolvingLanguageParser.java
+++ intellij-plugin/Edu-Jarvis/src/com/jetbrains/edu/jarvis/grammar/ProblemSolvingLanguageParser.java
@@ -4,11 +4,8 @@ package com.jetbrains.edu.jarvis.grammar;
 import org.antlr.v4.runtime.atn.*;
 import org.antlr.v4.runtime.dfa.DFA;
 import org.antlr.v4.runtime.*;
-import org.antlr.v4.runtime.misc.*;
 import org.antlr.v4.runtime.tree.*;
 import java.util.List;
-import java.util.Iterator;
-import java.util.ArrayList;
 
 @SuppressWarnings({"all", "warnings", "unchecked", "unused", "cast", "CheckReturnValue"})
 public class ProblemSolvingLanguageParser extends Parser {
