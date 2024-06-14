e0596bbee5f2a301b1de8752c20bb8a4698312bb
Stanislav.Sandler <sandler.stanislav@jetbrains.com>
2024-06-14 16:31:31 +0200

    Enabled error highlighting for the description block

--- intellij-plugin/Edu-Kotlin/src/com/jetbrains/edu/kotlin/jarvis/psi/KtDescriptionExpressionParser.kt
+++ intellij-plugin/Edu-Kotlin/src/com/jetbrains/edu/kotlin/jarvis/psi/KtDescriptionExpressionParser.kt
@@ -3,14 +3,13 @@ package com.jetbrains.edu.kotlin.jarvis.psi
 import com.intellij.psi.PsiElement
 import com.intellij.psi.util.PsiTreeUtil
 import com.jetbrains.edu.jarvis.DescriptionExpressionParser
-import com.jetbrains.edu.jarvis.JarvisDslPackageCallChecker
-import com.jetbrains.edu.kotlin.jarvis.utils.DESCRIPTION
+import com.jetbrains.edu.kotlin.jarvis.utils.isDescriptionBlock
 import com.jetbrains.edu.learning.courseFormat.jarvis.DescriptionExpression
 import org.jetbrains.kotlin.psi.KtCallExpression
 
 class KtDescriptionExpressionParser : DescriptionExpressionParser {
   override fun parseDescriptionExpression(descriptionExpression: PsiElement): DescriptionExpression? {
-    if (!isDescriptionBlock(descriptionExpression) ||
+    if (!descriptionExpression.isDescriptionBlock() ||
         descriptionExpression !is KtCallExpression ||
         existsNestedDescriptionExpressions(descriptionExpression))
       return null
@@ -21,8 +20,5 @@ class KtDescriptionExpressionParser : DescriptionExpressionParser {
   }
 
   private fun existsNestedDescriptionExpressions(descriptionExpression: KtCallExpression) =
-    PsiTreeUtil.findChildrenOfType(descriptionExpression, KtCallExpression::class.java).any { isDescriptionBlock(it) }
-
-  private fun isDescriptionBlock(element: PsiElement) = element.text.startsWith(DESCRIPTION) &&
-                      JarvisDslPackageCallChecker.isCallFromJarvisDslPackage(element, element.language)
+    PsiTreeUtil.findChildrenOfType(descriptionExpression, KtCallExpression::class.java).any { it.isDescriptionBlock() }
 }
