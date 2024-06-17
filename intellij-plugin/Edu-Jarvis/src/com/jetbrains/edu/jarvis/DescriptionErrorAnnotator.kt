f47b4d20a9e6816702f11eb0ead84defaf6ed7c3
Stanislav.Sandler <sandler.stanislav@jetbrains.com>
2024-06-17 14:03:00 +0200

    Extracted some methods from Kotlin implementation to the base class.

--- intellij-plugin/Edu-Jarvis/src/com/jetbrains/edu/jarvis/DescriptionErrorAnnotator.kt
+++ intellij-plugin/Edu-Jarvis/src/com/jetbrains/edu/jarvis/DescriptionErrorAnnotator.kt
@@ -1,6 +1,13 @@
 package com.jetbrains.edu.jarvis
 
+import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.lang.annotation.Annotator
+import com.intellij.lang.annotation.HighlightSeverity
+import com.intellij.openapi.util.TextRange
+import com.intellij.psi.PsiElement
+import com.intellij.refactoring.suggested.startOffset
+import com.jetbrains.edu.jarvis.errors.AnnotatorError
+import com.jetbrains.edu.jarvis.messages.EduJarvisBundle
 
 /**
  * Highlights parts containing errors inside the `description` DSL element.
@@ -8,7 +15,35 @@ import com.intellij.lang.annotation.Annotator
 
 interface DescriptionErrorAnnotator : Annotator {
 
-  fun getIncorrectParts(descriptionText: String): Sequence<DescriptionAnnotatorResult>
+  fun applyAnnotation(
+    descriptionContent: PsiElement,
+    holder: AnnotationHolder
+  ) =
+    getIncorrectParts(descriptionContent.text).forEach {
+      val errorRange = TextRange(
+        descriptionContent.startOffset + it.range.first,
+        descriptionContent.startOffset + it.range.last + 1
+      )
+      holder.newAnnotation(HighlightSeverity.ERROR, EduJarvisBundle.message(it.error.message))
+        .range(errorRange)
+        .create()
+    }
+
+  fun getIncorrectParts(descriptionText: String) =
+    codeBlockRegex
+      .findAll(descriptionText)
+      .mapNotNull { it.groups[1] }
+      .map {
+        DescriptionAnnotatorResult(
+          it.range,
+          it.value.getError()
+        )
+      }
+      .filter { it.error != AnnotatorError.NONE }
+
+  fun String.getError(): AnnotatorError
+
+  fun PsiElement.isRelevant(): Boolean
 
   companion object {
     val codeBlockRegex = "`([^`]+)`".toRegex()
--- intellij-plugin/Edu-Kotlin/src/com/jetbrains/edu/kotlin/jarvis/psi/KtDescriptionErrorAnnotator.kt
+++ intellij-plugin/Edu-Kotlin/src/com/jetbrains/edu/kotlin/jarvis/psi/KtDescriptionErrorAnnotator.kt
@@ -1,52 +1,25 @@
 package com.jetbrains.edu.kotlin.jarvis.psi
 
 import com.intellij.lang.annotation.AnnotationHolder
-import com.intellij.lang.annotation.HighlightSeverity
-import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
 import com.jetbrains.edu.jarvis.DescriptionErrorAnnotator
-import com.jetbrains.edu.jarvis.DescriptionErrorAnnotator.Companion.codeBlockRegex
-import com.jetbrains.edu.jarvis.DescriptionAnnotatorResult
 import com.jetbrains.edu.jarvis.errors.AnnotatorError
-import com.jetbrains.edu.jarvis.messages.EduJarvisBundle
 import com.jetbrains.edu.kotlin.jarvis.utils.isDescriptionBlock
 import org.jetbrains.kotlin.psi.KtValueArgumentList
 import org.jetbrains.kotlin.psi.psiUtil.getChildOfType
-import org.jetbrains.kotlin.psi.psiUtil.startOffset
 
 class KtDescriptionErrorAnnotator : DescriptionErrorAnnotator {
 
   override fun annotate(element: PsiElement, holder: AnnotationHolder) {
-    if (!element.containsCode()) return
+    if (!element.isRelevant()) return
 
     val descriptionContent = element.getChildOfType<KtValueArgumentList>() ?: return
-
-    getIncorrectParts(descriptionContent.text).forEach {
-      val errorRange = TextRange(
-        descriptionContent.startOffset + it.range.first,
-        descriptionContent.startOffset + it.range.last + 1
-      )
-      holder.newAnnotation(HighlightSeverity.ERROR, EduJarvisBundle.message(it.error.message))
-        .range(errorRange)
-        .create()
-    }
+    applyAnnotation(descriptionContent, holder)
   }
 
-  override fun getIncorrectParts(descriptionText: String) =
-    codeBlockRegex
-      .findAll(descriptionText)
-      .mapNotNull { it.groups[1] }
-      .map {
-        DescriptionAnnotatorResult(
-          it.range,
-          it.value.getError()
-        )
-      }
-      .filter { it.error != AnnotatorError.NONE }
+  override fun PsiElement.isRelevant() = isDescriptionBlock()
 
-  private fun PsiElement.containsCode() = isDescriptionBlock()
-
-  private fun String.getError(): AnnotatorError {
+  override fun String.getError(): AnnotatorError {
     // TODO: Actually detect errors
     return if (this.lowercase().contains("bad")) AnnotatorError.PLACEHOLDER_ERROR
     else AnnotatorError.NONE
