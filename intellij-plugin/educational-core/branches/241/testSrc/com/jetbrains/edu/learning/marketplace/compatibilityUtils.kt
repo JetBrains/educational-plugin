efebef258b1948e5fdb1f33cdb97a0fe4f1c7243
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2024-06-18 12:19:42 +0200

    EDU-6866: fix incompatibilities in tests

---
+++ intellij-plugin/educational-core/branches/241/testSrc/com/jetbrains/edu/learning/marketplace/compatibilityUtils.kt
@@ -0,0 +1,5 @@
+package com.jetbrains.edu.learning.marketplace
+
+import com.intellij.ui.JBAccountInfoService.JBAData
+
+fun mockJBAData(): JBAData = JBAData("test_id", "test user name", "test@email.com")
