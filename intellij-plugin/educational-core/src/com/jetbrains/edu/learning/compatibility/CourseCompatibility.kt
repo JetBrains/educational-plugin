602652c5958bda538e2f7c95f21b2c16b0c169bf
Aleksey Rostovskiy <aleksei.rostovskii@jetbrains.com>
2024-06-20 04:19:37 +0200

    Update CourseCompatibility class: mark objects as data objects

--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/compatibility/CourseCompatibility.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/compatibility/CourseCompatibility.kt
@@ -3,8 +3,8 @@ package com.jetbrains.edu.learning.compatibility
 import com.jetbrains.edu.learning.courseFormat.PluginInfo
 
 sealed class CourseCompatibility {
-  object Compatible : CourseCompatibility()
-  object IncompatibleVersion : CourseCompatibility()
-  object Unsupported : CourseCompatibility()
+  data object Compatible : CourseCompatibility()
+  data object IncompatibleVersion : CourseCompatibility()
   class PluginsRequired(val toInstallOrEnable: List<PluginInfo>) : CourseCompatibility()
+  data object Unsupported : CourseCompatibility()
 }
