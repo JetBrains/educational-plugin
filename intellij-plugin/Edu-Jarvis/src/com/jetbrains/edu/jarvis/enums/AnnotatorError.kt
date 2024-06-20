56befe0faa9688d0037bb36d321fd07f2d4c777f
Stanislav.Sandler <sandler.stanislav@jetbrains.com>
2024-06-20 15:22:38 +0200

    Added variable error detection.

--- intellij-plugin/Edu-Jarvis/src/com/jetbrains/edu/jarvis/enums/AnnotatorError.kt
+++ intellij-plugin/Edu-Jarvis/src/com/jetbrains/edu/jarvis/enums/AnnotatorError.kt
@@ -4,4 +4,5 @@ enum class AnnotatorError(val message: String) {
   NONE(""),
   UNKNOWN_FUNCTION("description.annotator.unknown.function.error"),
   WRONG_NUMBER_OF_ARGUMENTS("description.annotator.wrong.number.of.args.error"),
+  UNKNOWN_VARIABLE("description.annotator.unknown.variable.error"),
 }
