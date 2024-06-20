bdcff8ae98c027b019d412ea87010bf2ba0572e2
Stanislav.Sandler <sandler.stanislav@jetbrains.com>
2024-06-20 15:22:38 +0200

    Added variable error detection.

--- intellij-plugin/Edu-Kotlin/src/com/jetbrains/edu/kotlin/jarvis/psi/KtDescriptionErrorAnnotator.kt
+++ intellij-plugin/Edu-Kotlin/src/com/jetbrains/edu/kotlin/jarvis/psi/KtDescriptionErrorAnnotator.kt
@@ -2,19 +2,18 @@ package com.jetbrains.edu.kotlin.jarvis.psi
 
 import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.psi.PsiElement
+import com.intellij.psi.util.PsiTreeUtil
 import com.jetbrains.edu.jarvis.DescriptionAnnotatorResult
 import com.jetbrains.edu.jarvis.DescriptionErrorAnnotator
 import com.jetbrains.edu.jarvis.enums.AnnotatorRule
 import com.jetbrains.edu.jarvis.enums.AnnotatorError
 import com.jetbrains.edu.jarvis.errors.AnnotatorParametrizedError
-import com.jetbrains.edu.jarvis.models.FunctionCall
+import com.jetbrains.edu.jarvis.models.NamedFunction
+import com.jetbrains.edu.jarvis.models.NamedVariable
 import com.jetbrains.edu.kotlin.KtErrorProcessor
 import com.jetbrains.edu.kotlin.jarvis.utils.isDescriptionBlock
-import org.jetbrains.kotlin.psi.KtNamedFunction
-import org.jetbrains.kotlin.psi.KtParameterList
-import org.jetbrains.kotlin.psi.KtValueArgumentList
+import org.jetbrains.kotlin.psi.*
 import org.jetbrains.kotlin.psi.psiUtil.getChildOfType
-import org.jetbrains.kotlin.psi.psiUtil.getChildrenOfType
 
 
 class KtDescriptionErrorAnnotator : DescriptionErrorAnnotator {
@@ -28,43 +27,74 @@ class KtDescriptionErrorAnnotator : DescriptionErrorAnnotator {
   override fun PsiElement.isRelevant() = isDescriptionBlock()
 
   override fun getIncorrectParts(context: PsiElement): Collection<DescriptionAnnotatorResult> {
-    val visibleFunctions = context.containingFile
-      .getChildrenOfType<KtNamedFunction>()
-      .mapNotNull { it.toFunctionCallOrNull() }
-    return AnnotatorRule.values()
-      .flatMap { rule ->
+    val visibleFunctions =
+      PsiTreeUtil.collectElementsOfType(context.containingFile, KtNamedFunction::class.java).mapNotNull { it.toNamedFunctionOrNull() }
+    val visibleVariables = (PsiTreeUtil.collectElementsOfType(
+      context.containingFile,
+      KtProperty::class.java
+    ) + PsiTreeUtil.collectElementsOfType(context.containingFile, KtParameter::class.java)).mapNotNull { it.toNamedVariableOrNull() }
+      .toMutableSet()
+    return AnnotatorRule.values().asSequence().flatMap { rule ->
         getIncorrectPartsByRegex(context, rule.regex).map {
-          DescriptionAnnotatorResult(
-            it.range,
-            it.value.getError(rule, visibleFunctions)
-          )
+          it to rule
         }
-      }
-      .toSet()
-      .filter { it.parametrizedError.errorType != AnnotatorError.NONE }
+      }.sortedBy { (match, _) ->
+        match.range.first
+      }.distinctBy { (match, _) ->
+        match.range.first
+      }.map { (match, rule) ->
+        DescriptionAnnotatorResult(
+          match.range, match.value.getError(rule, visibleFunctions, visibleVariables)
+        )
+      }.filter { it.parametrizedError.errorType != AnnotatorError.NONE }.toList()
 
   }
 
   private fun getIncorrectPartsByRegex(context: PsiElement, regex: Regex): Sequence<MatchGroup> {
-    return regex
-      .findAll(context.text)
-      .mapNotNull { it.groups[1] }
+    return regex.findAll(context.text).mapNotNull { it.groups[1] }
   }
 
-  private fun KtNamedFunction.toFunctionCallOrNull(): FunctionCall? {
+  private fun PsiElement.toNamedFunctionOrNull(): NamedFunction? {
+    if (this !is KtNamedFunction) return null
     val numberOfParameters = getChildOfType<KtParameterList>()?.parameters?.size ?: 0
-    return FunctionCall(name ?: return null, numberOfParameters)
+    return NamedFunction(name ?: return null, numberOfParameters)
   }
 
-  private fun String.getError(rule: AnnotatorRule, visibleFunctions: Collection<FunctionCall>): AnnotatorParametrizedError {
-    val processor = KtErrorProcessor(this, visibleFunctions)
-    return when(rule) {
+  private fun PsiElement.toNamedVariableOrNull(): NamedVariable? {
+    return when (this) {
+      is KtProperty -> NamedVariable(name ?: return null)
+      is KtParameter -> NamedVariable(name ?: return null)
+      else -> null
+    }
+  }
+
+
+  private fun String.isANamedFunction() = DescriptionErrorAnnotator.namedFunctionRegex.matches(this)
+
+  private fun String.isANamedVariable() = DescriptionErrorAnnotator.namedVariableRegex.matches(this)
+
+  private fun String.getError(
+    rule: AnnotatorRule, visibleFunctions: Collection<NamedFunction>, visibleVariables: MutableSet<NamedVariable>
+  ): AnnotatorParametrizedError {
+    val processor = KtErrorProcessor(this, visibleFunctions, visibleVariables)
+    return when (rule) {
+      AnnotatorRule.VARIABLE_DECLARATION -> {
+        visibleVariables.add(NamedVariable(this))
+        AnnotatorParametrizedError.NO_ERROR
+      }
+
+      AnnotatorRule.NO_PARENTHESES_FUNCTION -> {
+        processor.processNamedFunction()
+      }
+
       AnnotatorRule.ISOLATED_CODE -> {
-        processor.processIsolatedCode()
+        when {
+          isANamedFunction() -> processor.processNamedFunction()
+          isANamedVariable() -> processor.processNamedVariable()
+          else -> AnnotatorParametrizedError.NO_ERROR
+        }
       }
-      AnnotatorRule.NO_PARENTHESES_FUNCTION_CALL -> {
-        processor.processNoParenthesesFunctionCall()
-      }
+
       else -> AnnotatorParametrizedError.NO_ERROR
     }
   }
