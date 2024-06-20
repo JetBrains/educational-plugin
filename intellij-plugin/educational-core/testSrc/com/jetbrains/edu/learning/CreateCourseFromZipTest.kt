510388e97ae7e7478a24fd3ddc9bf07edff66f1e
Ilia Posov <ilia.posov@jetbrains.com>
2024-06-20 14:09:46 +0200

    EDU-6396: test reading course archive in the new format

--- intellij-plugin/educational-core/testSrc/com/jetbrains/edu/learning/CreateCourseFromZipTest.kt
+++ intellij-plugin/educational-core/testSrc/com/jetbrains/edu/learning/CreateCourseFromZipTest.kt
@@ -1,8 +1,12 @@
 package com.jetbrains.edu.learning
 
+import com.jetbrains.edu.learning.courseFormat.BinaryContents
 import com.jetbrains.edu.learning.courseFormat.Course
 import com.jetbrains.edu.learning.courseFormat.EduCourse
+import com.jetbrains.edu.learning.courseFormat.TextualContents
 import org.junit.Test
+import kotlin.test.assertContains
+import kotlin.test.assertIs
 
 class CreateCourseFromZipTest : EduTestCase() {
 
@@ -55,6 +59,40 @@ class CreateCourseFromZipTest : EduTestCase() {
     assertEquals(jsonVersion, (course as EduCourse).formatVersion)
 
     return course
+  }
+
+  @Test
+  fun `test reading archive in the format with files outside json`() {
+    val zipPath = "$testDataPath/Kotlin_Course_to_test_archive_reading.zip"
+    val course = EduUtilsKt.getLocalCourse(zipPath) ?: error("Failed to load course from $zipPath")
+    course.init(false)
+
+    assertEquals("Kotlin Course to test archive reading", course.name)
+
+    course.visitTasks { task ->
+      for (taskFile in task.taskFiles.values) {
+        // all task files are textual; have file name inside; have lesson1.task1 name inside
+        val contents = taskFile.contents as? TextualContents ?: error("all task files are textual in the example")
+        assertContains(contents.text, taskFile.name.substringAfterLast("/"))
+        assertContains(contents.text, "${task.lesson.name}.${task.name}")
+      }
+    }
+
+    // must have additional file additional_file.txt with the same contents
+    // build.gradle
+    // nonempty image img.png
+    assertEquals(3, course.additionalFiles.size)
+
+    for (additionalFile in course.additionalFiles) {
+      val contents = additionalFile.contents
+      when (additionalFile.name) {
+        "additional_file.txt" -> assertEquals("additional_file.txt", (contents as TextualContents).text)
+        "img.png" -> {
+          assertIs<BinaryContents>(contents)
+          assertTrue(contents.bytes.isNotEmpty())
+        }
+      }
+    }
   }
 
   override fun getTestDataPath(): String {
