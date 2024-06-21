943ee00b82ce139fad70b75e45c00e4797d10438
Stanislav.Sandler <sandler.stanislav@jetbrains.com>
2024-06-21 10:21:46 +0200

    Added tests. Refactored the code.

--- intellij-plugin/Edu-Jarvis/src/com/jetbrains/edu/jarvis/ErrorProcessor.kt
+++ intellij-plugin/Edu-Jarvis/src/com/jetbrains/edu/jarvis/ErrorProcessor.kt
@@ -3,7 +3,7 @@ package com.jetbrains.edu.jarvis
 import com.jetbrains.edu.jarvis.errors.AnnotatorParametrizedError
 
 /**
- * Returns an [[AnnotatorParametrizedError]] associated with relevant context.
+ * Returns an [AnnotatorParametrizedError] associated with the relevant context.
  */
 interface ErrorProcessor {
   /**
