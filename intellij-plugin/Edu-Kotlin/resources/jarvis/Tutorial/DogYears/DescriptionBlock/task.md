0c1f5e678098833831c8cccc5badd13ca93cc861
anchouls <potryasaeva.anna@mail.ru>
2024-06-21 11:36:03 +0200

    Added tutorial

---
+++ intellij-plugin/Edu-Kotlin/resources/jarvis/Tutorial/DogYears/DescriptionBlock/task.md
@@ -0,0 +1,7 @@
+Now let's declare a description block inside the `calculateDogAgeInDogYears` function, in which the solution of the problem will be described:
+
+```kotlin
+description("""
+
+""".trimIndent())
+```
---
+++ intellij-plugin/Edu-Kotlin/resources/jarvis/Tutorial/DogYears/Function/Main.kt
@@ -0,0 +1,9 @@
+package jetbrains.course.tutorial.dog.years
+
+fun calculateDogAgeInDogYears() {
+  TODO("Not implemented yet")
+}
+
+fun verifyHumanYearsInput(humanYears: Int): Boolean {
+  TODO("Not implemented yet")
+}
