3cd1c0ff021606f49b0d27ddd870d0b7d9cb3cbe
Stanislav.Sandler <sandler.stanislav@jetbrains.com>
2024-06-21 10:21:46 +0200

    Added tests. Refactored the code.

--- intellij-plugin/Edu-Jarvis/src/com/jetbrains/edu/jarvis/errors/AnnotatorParametrizedError.kt
+++ intellij-plugin/Edu-Jarvis/src/com/jetbrains/edu/jarvis/errors/AnnotatorParametrizedError.kt
@@ -2,6 +2,11 @@ package com.jetbrains.edu.jarvis.errors
 
 import com.jetbrains.edu.jarvis.enums.AnnotatorError
 
+/**
+ * A parametrized version of [AnnotatorError],
+ * designed to hold information about a specific instance of [AnnotatorError], along with associated parameters.
+ * These parameters are further used to display the error message.
+ */
 class AnnotatorParametrizedError(val errorType: AnnotatorError, val params: Array<Any>) {
   companion object {
     val NO_ERROR = AnnotatorParametrizedError(AnnotatorError.NONE, emptyArray())
