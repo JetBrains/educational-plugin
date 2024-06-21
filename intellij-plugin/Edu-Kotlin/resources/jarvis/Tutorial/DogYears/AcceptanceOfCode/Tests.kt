a9dcf1df55a7983a4592d3aa05b51ac7a1b3892f
anchouls <potryasaeva.anna@mail.ru>
2024-06-21 11:36:03 +0200

    Added tutorial

---
+++ intellij-plugin/Edu-Kotlin/resources/jarvis/Tutorial/DogYears/AcceptanceOfCode/Tests.kt
@@ -0,0 +1,36 @@
+import org.jetbrains.academy.test.system.kotlin.test.BaseIjTestClass
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import java.io.File
+
+
+class Test : BaseIjTestClass() {
+    companion object {
+
+        private lateinit var sourceText: String
+
+        @JvmStatic
+        @BeforeAll
+        fun initialize() {
+            val taskDirectoryPath = System.getProperty("user.dir")
+            val sourceCodeFile =
+                File("$taskDirectoryPath/src/jetbrains/course/tutorial/dog/years/Main.kt")
+            sourceText = sourceCodeFile.readText()
+        }
+    }
+
+    @Test
+    fun testCodeAccept() {
+        setUp()
+        myFixture.configureByText("Task.kt", sourceText)
+
+        Assertions.assertNull(getMethodCallArguments("description")) {
+            "Please, accept the suggested code by clicking on the bulb."
+        }
+
+        Assertions.assertNull(getMethodCallArguments("draft")) {
+            "Please, accept the suggested code by clicking on the bulb."
+        }
+    }
+}
---
+++ intellij-plugin/Edu-Kotlin/resources/jarvis/Tutorial/DogYears/DescriptionBlock/Main.kt
@@ -0,0 +1,13 @@
+package jetbrains.course.tutorial.dog.years
+
+import org.jetbrains.academy.jarvis.dsl.description
+
+fun calculateDogAgeInDogYears() {
+  description("""
+        
+    """.trimIndent())
+}
+
+fun verifyHumanYearsInput(humanYears: Int): Boolean {
+  TODO("Not implemented yet")
+}
