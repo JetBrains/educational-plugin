a0df0dac9c0d4469020621ba00ac003d5cb9958f
Stanislav.Sandler <sandler.stanislav@jetbrains.com>
2024-06-21 10:21:46 +0200

    Added tests. Refactored the code.

--- intellij-plugin/Edu-Jarvis/src/com/jetbrains/edu/jarvis/enums/AnnotatorRule.kt
+++ intellij-plugin/Edu-Jarvis/src/com/jetbrains/edu/jarvis/enums/AnnotatorRule.kt
@@ -3,14 +3,16 @@ package com.jetbrains.edu.jarvis.enums
 import com.jetbrains.edu.jarvis.DescriptionErrorAnnotator.Companion.callSynonyms
 import com.jetbrains.edu.jarvis.DescriptionErrorAnnotator.Companion.declareSynonyms
 import com.jetbrains.edu.jarvis.DescriptionErrorAnnotator.Companion.functionSynonyms
+import com.jetbrains.edu.jarvis.DescriptionErrorAnnotator.Companion.variableSynonyms
 
 enum class AnnotatorRule(val regex: Regex) {
   VARIABLE_DECLARATION(
-    "(?i)(?:${declareSynonyms.joinToString("|")})(?:\\s+the)?(?-i)\\s+`([a-zA-Z_][a-zA-Z0-9_]*)`".toRegex()
+    ("(?i)(?:${declareSynonyms().joinToString("|")})" +
+     "(?:\\s+the)?(?:\\s+(?:${variableSynonyms().joinToString("|")}))?(?-i)\\s+`([a-zA-Z_][a-zA-Z0-9_]*)`").toRegex()
   ),
   NO_PARENTHESES_FUNCTION(
-    ("(?i)(?:${callSynonyms.joinToString("|")})" +
-     "(?:\\s+the)?(?:\\s+(?:${functionSynonyms.joinToString("|")}))?(?-i)\\s+`([A-Za-z][A-Za-z0-9]+)`").toRegex()
+    ("(?i)(?:${callSynonyms().joinToString("|")})" +
+     "(?:\\s+the)?(?:\\s+(?:${functionSynonyms().joinToString("|")}))?(?-i)\\s+`([a-zA-Z_][a-zA-Z0-9_]*)`").toRegex()
   ),
   ISOLATED_CODE("`([^`]+)`".toRegex()),
 }
