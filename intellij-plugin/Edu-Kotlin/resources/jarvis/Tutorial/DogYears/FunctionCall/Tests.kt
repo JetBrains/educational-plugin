53a0fb852398ecc73563d5cbc7dc439a255a24df
anchouls <potryasaeva.anna@mail.ru>
2024-06-21 11:36:03 +0200

    Added tutorial

---
+++ intellij-plugin/Edu-Kotlin/resources/jarvis/Tutorial/DogYears/FunctionCall/Tests.kt
@@ -0,0 +1,40 @@
+import org.jetbrains.academy.test.system.kotlin.test.BaseIjTestClass
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import java.io.File
+
+
+class Test : BaseIjTestClass() {
+    companion object {
+
+        private lateinit var sourceText: String
+
+        @JvmStatic
+        @BeforeAll
+        fun initialize() {
+            val taskDirectoryPath = System.getProperty("user.dir")
+            val sourceCodeFile =
+                File("$taskDirectoryPath/src/jetbrains/course/tutorial/dog/years/Main.kt")
+            sourceText = sourceCodeFile.readText()
+        }
+    }
+
+    @Test
+    fun testFunctionCall() {
+        setUp()
+        myFixture.configureByText("Task.kt", sourceText)
+
+        val argument = getMethodCallArguments("description")?.firstOrNull()
+        Assertions.assertNotNull(argument) {
+            "Please, create a description block."
+        }
+        val values = Regex("`[^`]*`").findAll(argument!!).map { it.value }
+        Assertions.assertNotNull(values.find { it == "`verifyHumanYearsInput`" }) {
+            "Please, call the verifyHumanYearsInput function and wrap it in backticks (``)."
+        }
+        Assertions.assertEquals(2, values.filter { it == "`humanYears`" }.count()) {
+            "Please, call the verifyHumanYearsInput function with humanYears argument and wrap it in backticks (``)."
+        }
+    }
+}
