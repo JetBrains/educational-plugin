2189d644614996ad59483b724b2f48949b645c0f
anchouls <potryasaeva.anna@mail.ru>
2024-06-21 15:34:02 +0200

    Connect ml lib

--- intellij-plugin/build.gradle.kts
+++ intellij-plugin/build.gradle.kts
@@ -208,6 +208,9 @@ allprojects {
     implementationWithoutKotlin(rootProject.libs.retrofit)
     implementationWithoutKotlin(rootProject.libs.converter.jackson)
     implementationWithoutKotlin(rootProject.libs.kotlin.css.jvm)
+    implementationWithoutKotlin(rootProject.libs.educational.ml.library.core)
+    implementationWithoutKotlin(rootProject.libs.educational.ml.library.jarvis)
+    implementation(rootProject.libs.freemarker)
 
     testImplementation(rootProject.libs.junit)
     testImplementation(rootProject.libs.openTest4J)
@@ -1058,6 +1061,7 @@ fun <T : ModuleDependency> T.excludeKotlinDeps() {
   exclude(module = "kotlin-stdlib")
   exclude(module = "kotlin-stdlib-common")
   exclude(module = "kotlin-stdlib-jdk8")
+  exclude(module = "kotlinx-coroutines-core-jvm")
 }
 
 fun loadProperties(path: String): Properties {
