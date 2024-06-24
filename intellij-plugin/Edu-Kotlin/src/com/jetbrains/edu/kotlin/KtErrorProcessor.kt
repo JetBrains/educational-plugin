b4ec1d2c360ad4e7d75e0a74978ff5bc7eba6499
Stanislav.Sandler <sandler.stanislav@jetbrains.com>
2024-06-24 16:17:24 +0200

    Refactored tests. Pushed some methods to the respected interfaces.

--- intellij-plugin/Edu-Kotlin/src/com/jetbrains/edu/kotlin/KtErrorProcessor.kt
+++ intellij-plugin/Edu-Kotlin/src/com/jetbrains/edu/kotlin/KtErrorProcessor.kt
@@ -1,8 +1,6 @@
 package com.jetbrains.edu.kotlin
 
 import com.jetbrains.edu.jarvis.ErrorProcessor
-import com.jetbrains.edu.jarvis.enums.AnnotatorError
-import com.jetbrains.edu.jarvis.errors.AnnotatorParametrizedError
 import com.jetbrains.edu.jarvis.models.NamedFunction
 import com.jetbrains.edu.jarvis.models.NamedVariable
 import com.jetbrains.edu.kotlin.jarvis.utils.ARGUMENT_SEPARATOR
@@ -11,41 +9,11 @@ import com.jetbrains.edu.kotlin.jarvis.utils.EMPTY_STRING
 import com.jetbrains.edu.kotlin.jarvis.utils.OPEN_PARENTHESIS
 
 class KtErrorProcessor(
-  private val context: String,
-  private val visibleFunctions: Collection<NamedFunction>,
-  private val visibleVariables: Collection<NamedVariable>
+  override val visibleFunctions: MutableSet<NamedFunction>,
+  override val visibleVariables: MutableSet<NamedVariable>
 ) : ErrorProcessor {
 
-  override fun processNamedFunction(): AnnotatorParametrizedError {
-    val namedFunction = context.toNamedFunction()
-    return when {
-      visibleFunctions.none { it.name == namedFunction.name } ->
-        AnnotatorParametrizedError(
-          AnnotatorError.UNKNOWN_FUNCTION,
-          arrayOf(namedFunction.name)
-        )
-      namedFunction !in visibleFunctions ->
-        AnnotatorParametrizedError(
-          AnnotatorError.WRONG_NUMBER_OF_ARGUMENTS,
-          arrayOf(namedFunction.name, namedFunction.numberOfArguments)
-        )
-
-      else -> AnnotatorParametrizedError.NO_ERROR
-    }
-  }
-
-  override fun processNamedVariable(): AnnotatorParametrizedError {
-    val namedVariable = context.toNamedVariable()
-    return if (namedVariable !in visibleVariables) {
-      AnnotatorParametrizedError(
-        AnnotatorError.UNKNOWN_VARIABLE,
-        arrayOf(namedVariable.name)
-      )
-    }
-    else AnnotatorParametrizedError.NO_ERROR
-  }
-
-  private fun String.toNamedFunction(): NamedFunction {
+  override fun String.toNamedFunction(): NamedFunction {
     val functionName = this.substringBefore(OPEN_PARENTHESIS)
     val parameters = if (OPEN_PARENTHESIS in this) {
       this.substringAfter(OPEN_PARENTHESIS).substringBefore(CLOSE_PARENTHESIS)
@@ -61,5 +29,5 @@ class KtErrorProcessor(
   }
   else 0
 
-  private fun String.toNamedVariable(): NamedVariable = NamedVariable(this)
+  override fun String.toNamedVariable(): NamedVariable = NamedVariable(this)
 }
