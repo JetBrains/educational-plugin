6339ed5658763288767f172d15cadd896f1bbd59
Aleksey Rostovskiy <aleksei.rostovskii@jetbrains.com>
2024-06-25 03:12:50 +0200

    Refactor code to use selectedTaskFile instead of eduState
    This refactoring updates multiple files where the `eduState` property was being used to access the current `TaskFile`.
    The necessary changes have been made to directly use the `selectedTaskFile` property from the `Project` object instead.
    This reduces the dependency on `eduState` and streamlines the code, simplifying the workflow and improving readability.

--- intellij-plugin/ai-assistant-validation/src/com/jetbrains/edu/assistant/validation/actions/next/step/hint/BaseAssistantInfoStorage.kt
+++ intellij-plugin/ai-assistant-validation/src/com/jetbrains/edu/assistant/validation/actions/next/step/hint/BaseAssistantInfoStorage.kt
@@ -4,7 +4,6 @@ import com.jetbrains.edu.learning.courseFormat.ext.languageById
 import com.jetbrains.edu.learning.courseFormat.ext.project
 import com.jetbrains.edu.learning.courseFormat.tasks.EduTask
 import com.jetbrains.edu.learning.eduAssistant.processors.TaskProcessorImpl
-import com.jetbrains.edu.learning.eduState
 import com.jetbrains.educational.ml.hints.assistant.AiHintsAssistant
 
 data class BaseAssistantInfoStorage(private val task: EduTask) {
@@ -12,6 +11,4 @@ data class BaseAssistantInfoStorage(private val task: EduTask) {
   val assistant get() = AiHintsAssistant.getAssistant(taskProcessor)
   val language = task.course.languageById ?: error("Language could not be determined")
   val project get() = task.project ?: error("Cannot get project")
-
-  val eduState get() = project.eduState ?: error("Cannot get eduState for project ${project.name}")
 }
