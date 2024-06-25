d58d6b0c17ba73485504153d9d7edad8a85003fb
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2024-06-25 14:41:42 +0200

    Minor refactoring of `TestHighlightLevelFeature`
    It should simplify code reading as well as test assert output

--- intellij-plugin/educational-core/testSrc/com/jetbrains/edu/learning/yamlFeatures/TestHighlightLevelFeature.kt
+++ intellij-plugin/educational-core/testSrc/com/jetbrains/edu/learning/yamlFeatures/TestHighlightLevelFeature.kt
@@ -2,7 +2,7 @@ package com.jetbrains.edu.learning.yamlFeatures
 
 import com.intellij.codeInsight.daemon.impl.analysis.FileHighlightingSetting
 import com.intellij.codeInsight.daemon.impl.analysis.HighlightingSettingsPerFile
-import com.intellij.psi.PsiManager
+import com.intellij.testFramework.utils.vfs.getPsiFile
 import com.intellij.util.ui.UIUtil
 import com.jetbrains.edu.learning.course
 import com.jetbrains.edu.learning.courseFormat.EduFileErrorHighlightLevel
@@ -47,19 +47,8 @@ class TestHighlightLevelFeature : CourseGenerationTestBase<EmptyProjectSettings>
     // wait for the project to be initialized
     UIUtil.dispatchAllInvocationEvents()
 
-    val highlightManager = HighlightingSettingsPerFile.getInstance(project)
-    val psiManager = PsiManager.getInstance(project)
-
-    fun assertHighlightLevel(path: String, highlightLevel: EduFileErrorHighlightLevel) {
-      val virtualFile = findFile(path)
-      val actualHighlightLevel = highlightManager.getHighlightingSettingForRoot(
-        psiManager.findFile(virtualFile) ?: error("failed to find psi file $path")
-      )
-      assertEquals(highlightLevel == EduFileErrorHighlightLevel.NONE, actualHighlightLevel == FileHighlightingSetting.SKIP_HIGHLIGHTING)
-    }
-
-    assertHighlightLevel("lesson1/task1-no-highlight/Main1.java", EduFileErrorHighlightLevel.NONE)
-    assertHighlightLevel("lesson1/task2/Main2.java", EduFileErrorHighlightLevel.ALL_PROBLEMS)
+    assertHighlightLevel("lesson1/task1-no-highlight/Main1.java", FileHighlightingSetting.SKIP_HIGHLIGHTING)
+    assertHighlightLevel("lesson1/task2/Main2.java", FileHighlightingSetting.FORCE_HIGHLIGHTING)
 
     // framework lesson
     val frameworkLesson = course.getLesson("lesson2-framework")!!
@@ -67,10 +56,16 @@ class TestHighlightLevelFeature : CourseGenerationTestBase<EmptyProjectSettings>
     val task2 = frameworkLesson.taskList[1]
     val task3 = frameworkLesson.taskList[2]
 
-    assertHighlightLevel("lesson2-framework/task/Main1.java", EduFileErrorHighlightLevel.ALL_PROBLEMS)
+    assertHighlightLevel("lesson2-framework/task/Main1.java", FileHighlightingSetting.FORCE_HIGHLIGHTING)
     NavigationUtils.navigateToTask(project, task2, task1)
-    assertHighlightLevel("lesson2-framework/task/Main1.java", EduFileErrorHighlightLevel.NONE)
+    assertHighlightLevel("lesson2-framework/task/Main1.java", FileHighlightingSetting.SKIP_HIGHLIGHTING)
     NavigationUtils.navigateToTask(project, task3, task2)
-    assertHighlightLevel("lesson2-framework/task/Main1.java", EduFileErrorHighlightLevel.ALL_PROBLEMS)
+    assertHighlightLevel("lesson2-framework/task/Main1.java", FileHighlightingSetting.FORCE_HIGHLIGHTING)
+  }
+
+  private fun assertHighlightLevel(path: String, expectedHighlightLevel: FileHighlightingSetting) {
+    val file = findFile(path).getPsiFile(project)
+    val actualHighlightLevel = HighlightingSettingsPerFile.getInstance(project).getHighlightingSettingForRoot(file)
+    assertEquals("Unexpected highlighting level for $path", expectedHighlightLevel, actualHighlightLevel)
   }
 }
