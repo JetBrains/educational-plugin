82d30c827e2c65c7199e20d5cb2ccf2ce6d7ff4f
Stanislav.Sandler <sandler.stanislav@jetbrains.com>
2024-06-26 15:23:40 +0200

    Added descriptions. Renamed some classes for clarity.

--- intellij-plugin/Edu-Jarvis/src/com/jetbrains/edu/jarvis/highlighting/AnnotatorRule.kt
+++ intellij-plugin/Edu-Jarvis/src/com/jetbrains/edu/jarvis/highlighting/AnnotatorRule.kt
@@ -6,6 +6,10 @@ import com.jetbrains.edu.jarvis.highlighting.GrammarRegex.isolatedCode
 import com.jetbrains.edu.jarvis.highlighting.GrammarRegex.setVariable
 import com.jetbrains.edu.jarvis.highlighting.GrammarRegex.storeVariable
 
+/**
+ * Represents a rule based on which the [AnnotatorError] can be found.
+ * These rules are dictated with the regular expressions.
+ */
 enum class AnnotatorRule(val regex: Regex) {
   STORE_VARIABLE(storeVariable),
   CREATE_VARIABLE(createVariable),
