5b79d1c02fc57aa58348bec7cd7e67fe084b9cdc
Stanislav.Sandler <sandler.stanislav@jetbrains.com>
2024-06-26 15:23:40 +0200

    Added descriptions. Renamed some classes for clarity.

--- intellij-plugin/Edu-Jarvis/testSrc/com/jetbrains/edu/jarvis/regex/CreateVariableRegexTest.kt
+++ intellij-plugin/Edu-Jarvis/testSrc/com/jetbrains/edu/jarvis/regex/CreateVariableRegexTest.kt
@@ -1,43 +1,45 @@
 package com.jetbrains.edu.jarvis.regex
 
+import com.jetbrains.edu.jarvis.TestGenerator.generateCreateVariable
 import com.jetbrains.edu.jarvis.highlighting.AnnotatorRule
 import com.jetbrains.edu.jarvis.regex.RegexTest.Companion.MAX_IDENTIFIER_NAME_LENGTH
 import com.jetbrains.edu.jarvis.regex.RegexTest.Companion.MIN_IDENTIFIER_NAME_LENGTH
 import com.jetbrains.edu.jarvis.regex.RegexTest.Companion.NUMBER_OF_RUNS
-import com.jetbrains.edu.jarvis.regex.RegexTest.Companion.generateVariableDeclaration
 import com.jetbrains.edu.learning.EduTestCase
 
 class CreateVariableRegexTest : RegexTest, EduTestCase() {
 
   override val regex = AnnotatorRule.CREATE_VARIABLE.regex
 
-  override fun shouldMatch(): List<String> = emptyList()
-
   override fun shouldMatchGroup() =
+    // Generated smoke tests
     List(NUMBER_OF_RUNS) {
-      generateVariableDeclaration(
+      generateCreateVariable(
         (MIN_IDENTIFIER_NAME_LENGTH..MAX_IDENTIFIER_NAME_LENGTH).random()
       )
     } + listOf(
-      TestAnswer("Create `foo`", "foo"),
-      TestAnswer("declare `bar`", "bar"),
-      TestAnswer("CreaTe the Variable `test1`", "test1"),
-      TestAnswer("Create an empty string `bar`", "bar")
+      // Test all CREATE synonyms:
+      TestAnswer("Create `foo`", listOf("foo")),
+      TestAnswer("declare `bar`", listOf("bar")),
+      TestAnswer("set up `buz`", listOf("buz")),
+      // Test more complex sentences, different articles:
+      TestAnswer("CreaTe the Variable `test1`", listOf("test1")),
+      TestAnswer("set up  an empty string `bar`", listOf("bar")),
+      TestAnswer("Create a random   string called `testMe` ", listOf("testMe"))
     )
 
   override fun shouldNotMatch() =
   listOf(
-    "create `foo()`",
-    "run the `test`",
-    "store the `test",
-    "store the `2test2`",
-    "call `test(a,b,c,)`",
-    "print `foo`",
-    "declare bar",
-    "invoke the foo",
+    "create `foo()`", // cannot create a function
+    "run the `test`", // invalid CALL synonym
+    "store the `2test2`", // invalid identifier name
+    "create `test(a,b,c,)`", // cannot create a function
+    "create `foo", // backticks aren't closed
+    "declare bar", // no backticks
+    "set up the something `foo`", // invalid grammar
   )
 
-  fun testValidNamedFunctions() = testShouldMatchGroup()
-  fun testInvalidNamedFunctions() = testShouldNotMatch()
+  fun `test valid create variable sentences`() = runTestShouldMatchGroup()
+  fun `test invalid create variable sentences`() = runTestShouldNotMatch()
 
 }
