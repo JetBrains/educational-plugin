da7246193c260e2ee91560aeb532f40f228004ce
Stanislav.Sandler <sandler.stanislav@jetbrains.com>
2024-06-28 16:37:33 +0200

    Improved test explanations.

--- intellij-plugin/Edu-Jarvis/testSrc/com/jetbrains/edu/jarvis/regex/SaveVariableRegexTest.kt
+++ intellij-plugin/Edu-Jarvis/testSrc/com/jetbrains/edu/jarvis/regex/SaveVariableRegexTest.kt
@@ -19,27 +19,27 @@ class SaveVariableRegexTest : RegexTest, EduTestCase() {
       TestAnswer("save true to `foo`", listOf("foo")), // test true value
       TestAnswer("save false to `foo`", listOf("foo")), // test false value
 
-      TestAnswer("save the result to `buzz`", listOf("buzz")), // test the `the` article (1)
-      TestAnswer("save a `pear` to `buzz`", listOf("buzz")), // test the `a` article (1)
-      TestAnswer("save an `apple` to `buzz`", listOf("buzz")), // test the `an` article (1)
-      TestAnswer("save 3 to the `buzz`", listOf("buzz")), // test the `the` article (2)
-      TestAnswer("save 3 to a `buzz`", listOf("buzz")), // test the `a` article (2)
-      TestAnswer("save 3 to an `apple`", listOf("apple")), // test the `an` article (2)
+      TestAnswer("save the result to `buzz`", listOf("buzz")), // test the `the` article, before the value
+      TestAnswer("save a `pear` to `buzz`", listOf("buzz")), // test the `a` article, before the value
+      TestAnswer("save an `apple` to `buzz`", listOf("buzz")), // test the `an` article, before the value
+      TestAnswer("save 3 to the `buzz`", listOf("buzz")), // test the `the` article, before the variable name
+      TestAnswer("save 3 to a `buzz`", listOf("buzz")), // test the `a` article, before the variable name
+      TestAnswer("save 3 to an `apple`", listOf("apple")), // test the `an` article, before the variable name
 
       TestAnswer("save the value 3 to `buzz`", listOf("buzz")), // test the optional `value` word
       TestAnswer("save 3 to the variable `buzz`", listOf("buzz")), // test the optional `variable` word
 
-      TestAnswer("save `foo` incremented by 3 to `buzz`", listOf("buzz")), // test arbitrary text (1)
-      TestAnswer("save the result multiplied by `multiplier` to `foo`", listOf("foo")), // test arbitrary text (2)
+      TestAnswer("save `foo` incremented by 3 to `buzz`", listOf("buzz")), // test arbitrary text
+      TestAnswer("save the result multiplied by `multiplier` to `foo`", listOf("foo")), // test arbitrary text
 
-      TestAnswer("sAvE tHe ResUlT tO `foo`", listOf("foo")), // case-insensitive (1)
-      TestAnswer("SAVE THE VALUe 3 To `buzz`", listOf("buzz")), // case-insensitive (2)
-      TestAnswer("SaVE 3 tO `foo`", listOf("foo")), // test case-insensitive (3)
-      TestAnswer("saVe tHE REsUlT DIVideD bY `divisor` TO `foo`", listOf("foo")), // test case-insensitive (4)
+      TestAnswer("sAvE tHe ResUlT tO `foo`", listOf("foo")), // test case-insensitive
+      TestAnswer("SAVE THE VALUe 3 To `buzz`", listOf("buzz")), // test case-insensitive
+      TestAnswer("SaVE 3 tO `foo`", listOf("foo")), // test case-insensitive
+      TestAnswer("saVe tHE REsUlT DIVideD bY `divisor` TO `foo`", listOf("foo")), // test case-insensitive
 
-      TestAnswer("save     `foo`   multiplied    by  3    to    `buzz`", listOf("buzz")), // test spacing (1)
-      TestAnswer("Save     3      tO    `foo`", listOf("foo")), // test spacing (2)
-      TestAnswer("save     an  `apple`   to  `buzz`", listOf("buzz")), // test spacing (3)
+      TestAnswer("save     `foo`   multiplied    by  3    to    `buzz`", listOf("buzz")), // test spacing
+      TestAnswer("Save     3      tO    `foo`", listOf("foo")), // test spacing
+      TestAnswer("save     an  `apple`   to  `buzz`", listOf("buzz")), // test spacing
     )
 
   override fun shouldNotMatch() =
