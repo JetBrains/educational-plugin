34d9334ddd8bcdf8150f0cdbb44d94f64a3ff4d4
Stanislav.Sandler <sandler.stanislav@jetbrains.com>
2024-06-28 16:37:33 +0200

    Improved test explanations.

--- intellij-plugin/Edu-Jarvis/testSrc/com/jetbrains/edu/jarvis/regex/SetVariableRegexTest.kt
+++ intellij-plugin/Edu-Jarvis/testSrc/com/jetbrains/edu/jarvis/regex/SetVariableRegexTest.kt
@@ -22,14 +22,14 @@ class SetVariableRegexTest : RegexTest, EduTestCase() {
       TestAnswer("initialize the variable `buzz`", listOf("buzz")), // test the optional `variable` word
       TestAnswer("set the variable called `buzz`", listOf("buzz")), // test the optional `called` word
 
-      TestAnswer("SET `foo`", listOf("foo")), // case-insensitive (1)
-      TestAnswer("Set The `buzz`", listOf("buzz")), // case-insensitive (2)
-      TestAnswer("iNiTIAliZe tHE varIaBLe `buzz`", listOf("buzz")), // test case-insensitive (3)
-      TestAnswer("SEt THE vaRiABLE cALlEd `buzz`", listOf("buzz")), // test case-insensitive (4)
+      TestAnswer("SET `foo`", listOf("foo")), // test case-insensitive
+      TestAnswer("Set The `buzz`", listOf("buzz")), // test case-insensitive
+      TestAnswer("iNiTIAliZe tHE varIaBLe `buzz`", listOf("buzz")), // test case-insensitive
+      TestAnswer("SEt THE vaRiABLE cALlEd `buzz`", listOf("buzz")), // test case-insensitive
 
-      TestAnswer("give   `bar`", listOf("bar")), // test spacing (1)
-      TestAnswer("set  a   `buzz`", listOf("buzz")), // test spacing (2)
-      TestAnswer("set   the   variable    called `buzz`", listOf("buzz")), // test spacing (3)
+      TestAnswer("give   `bar`", listOf("bar")), // test spacing
+      TestAnswer("set  a   `buzz`", listOf("buzz")), // test spacing
+      TestAnswer("set   the   variable    called `buzz`", listOf("buzz")), // test spacing
       )
 
   override fun shouldNotMatch() =
