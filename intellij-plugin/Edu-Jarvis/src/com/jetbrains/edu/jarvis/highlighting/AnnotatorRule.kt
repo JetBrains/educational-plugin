9668605d27c3fd2d7f8561f5c18f2f835bfc00b8
Stanislav.Sandler <sandler.stanislav@jetbrains.com>
2024-06-28 15:05:14 +0200

    Added a new regular expression. Fixed "and" injection in `countAnd`.

--- intellij-plugin/Edu-Jarvis/src/com/jetbrains/edu/jarvis/highlighting/AnnotatorRule.kt
+++ intellij-plugin/Edu-Jarvis/src/com/jetbrains/edu/jarvis/highlighting/AnnotatorRule.kt
@@ -3,6 +3,7 @@ package com.jetbrains.edu.jarvis.highlighting
 import com.jetbrains.edu.jarvis.highlighting.GrammarRegex.callFunction
 import com.jetbrains.edu.jarvis.highlighting.GrammarRegex.createVariable
 import com.jetbrains.edu.jarvis.highlighting.GrammarRegex.isolatedCode
+import com.jetbrains.edu.jarvis.highlighting.GrammarRegex.saveVariable
 import com.jetbrains.edu.jarvis.highlighting.GrammarRegex.setVariable
 import com.jetbrains.edu.jarvis.highlighting.GrammarRegex.storeVariable
 
@@ -14,6 +15,7 @@ enum class AnnotatorRule(val regex: Regex) {
   STORE_VARIABLE(storeVariable),
   CREATE_VARIABLE(createVariable),
   SET_VARIABLE(setVariable),
+  SAVE_VARIABLE(saveVariable),
   CALL_FUNCTION(callFunction),
   ISOLATED_CODE(isolatedCode),
 }
