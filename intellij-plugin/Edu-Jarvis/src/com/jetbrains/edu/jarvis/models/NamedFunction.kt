70e0217aefffbb67ceecee32f998bc4cc53ea548
Stanislav.Sandler <sandler.stanislav@jetbrains.com>
2024-06-28 15:05:14 +0200

    Added a new regular expression. Fixed "and" injection in `countAnd`.

--- intellij-plugin/Edu-Jarvis/src/com/jetbrains/edu/jarvis/models/NamedFunction.kt
+++ intellij-plugin/Edu-Jarvis/src/com/jetbrains/edu/jarvis/models/NamedFunction.kt
@@ -17,17 +17,24 @@ data class NamedFunction(override val name: String, val numberOfArguments: Int) 
     /**
      * Calculates the number of arguments from the `arguments` string.
      * Examples:
-     * - `arguments = "1, 2 and 3"` -> one separator, presence of the word 'and' -> returns three
-     * - `arguments = "1, 2, 3, 4"` -> three separators, no presence of the word 'and' -> returns four
+     * - `arguments = "1, 2 and 3"` -> one separator, one occurrence of the word 'and' -> returns three
+     * - `arguments = "1, 2, 3, 4"` -> three separators, no occurrences of the word 'and' -> returns four
      * - `arguments = ""` -> a blank string -> return zero
      */
     fun getNumberOfArguments(arguments: String) = if (arguments.isNotBlank()) {
-      arguments.count { it == ARGUMENT_SEPARATOR } + arguments.containsAndInt() + 1
+      arguments.count { it == ARGUMENT_SEPARATOR } + arguments.countAnd() + 1
     }
     else 0
 
-    private fun String.containsAndInt() = if (AND in this.lowercase()) 1 else 0
+    private fun String.countAnd() = (
+      AND_REGEX.findAll(this).count()
+      - AND_STRING_LITERAL.findAll(this).count()
+      - AND_IDENTIFIER.findAll(this).count()
+      )
 
+    private val AND_REGEX = "(?i)$AND".toRegex()
+    private val AND_STRING_LITERAL = "(?i)\"[^\"]*$AND[^\"]*\"".toRegex()
+    private val AND_IDENTIFIER = "(?i)`[^`]*$AND[^`]*`".toRegex()
   }
 
 }
