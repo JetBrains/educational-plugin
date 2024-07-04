a9274d70018887b2b7bb40ee093b3545d0e7d26d
Sviatoslav Naiden <sviatoslav.naiden@jetbrains.com>
2024-07-04 11:11:01 +0200

    EDU-6069: Fix TaskDescriptionLiveTemplateTest

--- intellij-plugin/educational-core/testSrc/com/jetbrains/edu/coursecreator/taskDescription/TaskDescriptionLiveTemplateTest.kt
+++ intellij-plugin/educational-core/testSrc/com/jetbrains/edu/coursecreator/taskDescription/TaskDescriptionLiveTemplateTest.kt
@@ -3,7 +3,6 @@ package com.jetbrains.edu.coursecreator.taskDescription
 import com.intellij.openapi.actionSystem.IdeActions
 import com.intellij.openapi.application.runWriteAction
 import com.intellij.openapi.vfs.VfsUtil
-import com.intellij.util.ThrowableRunnable
 import com.jetbrains.edu.learning.EduTestCase
 import com.jetbrains.edu.learning.courseFormat.CourseMode
 import com.jetbrains.edu.learning.courseFormat.DescriptionFormat
@@ -11,10 +10,6 @@ import org.intellij.lang.annotations.Language
 import org.junit.Test
 
 class TaskDescriptionLiveTemplateTest : EduTestCase() {
-
-  override fun runTestRunnable(testRunnable: ThrowableRunnable<Throwable>) {
-    // https://youtrack.jetbrains.com/issue/EDU-6069/Fix-TaskDescriptionLiveTemplateTest-for-232
-  }
 
   @Test
   fun `test hint live template in task description file in cc mode`() {
@@ -86,6 +81,7 @@ class TaskDescriptionLiveTemplateTest : EduTestCase() {
     }
     myFixture.configureFromExistingVirtualFile(file)
     myFixture.performEditorAction(IdeActions.ACTION_EXPAND_LIVE_TEMPLATE_BY_TAB)
+    myFixture.availableIntentions
     myFixture.checkResult(after.trimIndent())
   }
 }
