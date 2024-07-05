d69c83301c623deffe80d6acb51ebc561e072746
Ilia Posov <ilia.posov@jetbrains.com>
2024-07-05 12:34:09 +0200

    EDU-6979: Add new LTI endpoint

---
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/marketplace/LTIRestService.kt
@@ -0,0 +1,90 @@
+package com.jetbrains.edu.learning.marketplace
+
+import com.intellij.openapi.diagnostic.logger
+import com.intellij.util.io.origin
+import com.jetbrains.edu.learning.Err
+import com.jetbrains.edu.learning.Ok
+import com.jetbrains.edu.learning.authUtils.OAuthRestService
+import com.jetbrains.edu.learning.authUtils.hasOpenDialogs
+import com.jetbrains.edu.learning.authUtils.sendPluginInfoResponse
+import com.jetbrains.edu.learning.courseGeneration.ProjectOpener
+import com.jetbrains.edu.learning.marketplace.api.MarketplaceConnector
+import com.jetbrains.edu.learning.marketplace.courseGeneration.MarketplaceOpenCourseRequest
+import com.jetbrains.edu.learning.marketplace.courseGeneration.MarketplaceOpenInIdeRequestHandler
+import com.jetbrains.edu.learning.messages.EduCoreBundle
+import com.jetbrains.edu.learning.newproject.ui.showNotificationFromCourseValidation
+import io.netty.channel.ChannelHandlerContext
+import io.netty.handler.codec.http.FullHttpRequest
+import io.netty.handler.codec.http.HttpRequest
+import io.netty.handler.codec.http.HttpResponseStatus
+import io.netty.handler.codec.http.QueryStringDecoder
+import java.util.regex.Pattern
+
+class LTIRestService : OAuthRestService(LTI) {
+
+  override fun execute(urlDecoder: QueryStringDecoder, request: FullHttpRequest, context: ChannelHandlerContext): String? {
+    val uri = urlDecoder.uri()
+
+    if (hasOpenDialogs(MARKETPLACE)) {
+      sendOk(request, context)
+      return null
+    }
+
+    val courseId = getIntParameter(COURSE_ID, urlDecoder)
+    val launchId = getStringParameter(LAUNCH_ID, urlDecoder)
+    val studyItemId = getStringParameter(STUDY_ITEM_ID, urlDecoder)
+    val lmsDescription = getStringParameter(LMS_DESCRIPTION, urlDecoder)
+    logger<LTIRestService>().info("LTI request with course=$courseId launchId=$launchId studyItemId=$studyItemId: $lmsDescription")
+    if (courseId != -1) {
+      openInIDE(MarketplaceOpenCourseRequest(courseId), request, context)
+    }
+
+    sendStatus(HttpResponseStatus.BAD_REQUEST, false, context.channel())
+    return "Unknown command: $uri"
+  }
+
+  override fun getServiceName(): String = "edu/lti"
+
+  private fun openInIDE(openCourseRequest: MarketplaceOpenCourseRequest,
+                        request: FullHttpRequest,
+                        context: ChannelHandlerContext): String? {
+    LOG.info("Opening $MARKETPLACE course: $openCourseRequest")
+    return when (val result = ProjectOpener.getInstance().open(MarketplaceOpenInIdeRequestHandler, openCourseRequest)) {
+      is Ok -> {
+        sendOk(request, context)
+        LOG.info("$MARKETPLACE course opened: $openCourseRequest")
+        null
+      }
+      is Err -> {
+        val validationResult = result.error
+        val message = validationResult.message
+        LOG.warn(message)
+        showNotificationFromCourseValidation(
+          validationResult,
+          EduCoreBundle.message("notification.title.failed.to.open.in.ide", openCourseRequest)
+        )
+        sendStatus(HttpResponseStatus.NOT_FOUND, false, context.channel())
+        message
+      }
+    }
+  }
+
+  override fun isOriginAllowed(request: HttpRequest): OriginCheckResult {
+    val originAllowed = super.isOriginAllowed(request)
+    if (originAllowed == OriginCheckResult.FORBID) {
+      val origin = request.origin ?: return OriginCheckResult.FORBID
+      return if (TRUSTED_ORIGINS.contains(origin) || JETBRAINS_ORIGIN_PATTERN.matcher(origin).matches()) OriginCheckResult.ALLOW
+      else OriginCheckResult.ASK_CONFIRMATION
+    }
+    return originAllowed
+  }
+
+  companion object {
+    private const val COURSE_ID = "marketplace_course_id"
+    private const val LAUNCH_ID = "launch_id"
+    private const val STUDY_ITEM_ID = "study_item_id"
+    private const val LMS_DESCRIPTION = "lms_description"
+    private val JETBRAINS_ORIGIN_PATTERN = Pattern.compile("https://([a-z0-9-]+\\.)*jetbrains.com$")
+    private val TRUSTED_ORIGINS = setOf(PLUGINS_REPOSITORY_URL, PLUGINS_EDU_DEMO, PLUGINS_MASTER_DEMO)
+  }
+}
