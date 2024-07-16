25fe2a9124105b3600a9db811b1e490f9929bf00
Stanislav.Sandler <stanislav.sandler@jetbrains.com>
2024-07-16 20:21:00 +0200

    Implemented grammar highlighting.

--- edu-format/src/com/jetbrains/edu/learning/courseFormat/jarvis/DescriptionExpression.kt
+++ edu-format/src/com/jetbrains/edu/learning/courseFormat/jarvis/DescriptionExpression.kt
@@ -1,6 +1,8 @@
 package com.jetbrains.edu.learning.courseFormat.jarvis
 
 data class DescriptionExpression(
+  val promptOffset: Int,
   val prompt: String,
+  val codeBlockOffset: Int,
   val codeBlock: String
 )
