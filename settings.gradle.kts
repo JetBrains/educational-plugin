de999ba4b48a2a39ce328aa04e1b7ec07107ec26
anchouls <anna.potriasaeva@jetbrains.com>
2024-07-16 16:21:33 +0200

    Added check box about jarvis mode

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -123,7 +123,6 @@ fun configureJarvisProperties() {
 
   jarvisProperties.extractAndStore(
     "intellij-plugin/educational-core/resources/jarvisTemplateVariables/jarvis.properties",
-    "isJarvis",
     "jarvisDslVersion"
   )
 }
