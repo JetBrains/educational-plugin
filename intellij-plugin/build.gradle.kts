80b9f0687b22eb3a76af0a064fe8c8e9b0830061
anchouls <anna.potriasaeva@jetbrains.com>
2024-07-16 14:40:13 +0200

    Fixed jarvis properties and extract version

--- intellij-plugin/build.gradle.kts
+++ intellij-plugin/build.gradle.kts
@@ -363,10 +363,6 @@ tasks {
 
     // Uncomment to enable FUS testing mode
     // jvmArgs("-Dfus.internal.test.mode=true")
-
-    if (hasProp("isJarvis")) {
-      jvmArgs("-Dis.jarvis.mode=${prop("isJarvis")}")
-    }
   }
   buildPlugin {
     dependsOn(":edu-format:jar")
