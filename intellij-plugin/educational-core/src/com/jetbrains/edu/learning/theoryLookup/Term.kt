51943ff3a74d73497a88bdc3e8d42c1575911c5f
anchouls <anna.potriasaeva@jetbrains.com>
2024-07-18 18:41:41 +0200

    ML4SE-A-137 Implemented display of term definitions
    Merge-request: EDU-MR-3378

--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/theoryLookup/Term.kt
+++
@@ -1,6 +0,0 @@
-package com.jetbrains.edu.learning.theoryLookup
-
-/**
- * Represents a Term that contains the original term from the task description text and its lemmatised (base form) version.
- */
-data class Term(val original: String, val lemmatisedVersion: String)
