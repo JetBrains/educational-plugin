00c9ef82aeb7f5471975a04d9a2868de46bac28e
Aleksey Rostovskiy <aleksei.rostovskii@jetbrains.com>
2024-07-22 06:08:29 +0200

    Refactor parsing of JS event data
    The class JsEventData has been moved into another package. Additionally, the parsing method has been refactored to use Jackson instead of Gson, offering more flexibility for data conversion. A unit test has been added to ensure that the deserialization of JS event data is correctly handled.

---
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/taskToolWindow/ui/JsEventData.kt
@@ -0,0 +1,39 @@
+package com.jetbrains.edu.learning.taskToolWindow.ui
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+import com.fasterxml.jackson.databind.util.StdConverter
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.intellij.openapi.diagnostic.Logger
+
+private const val TERM = "term"
+private const val X = "x"
+private const val Y = "y"
+
+class JsEventData private constructor(
+  @JsonProperty(TERM)
+  val term: String,
+
+  @JsonProperty(X)
+  @JsonDeserialize(contentConverter = DoubleToIntConverter::class)
+  val x: Int,
+
+  @JsonProperty(Y)
+  @JsonDeserialize(contentConverter = DoubleToIntConverter::class)
+  val y: Int
+) {
+  companion object {
+    private val LOG: Logger = Logger.getInstance(JsEventData::class.java)
+
+    fun fromJson(json: String): JsEventData? =
+      runCatching {
+        jacksonObjectMapper().readValue(json, JsEventData::class.java)
+      }.onFailure {
+        LOG.error("Failed to parse js event data $json", it)
+      }.getOrNull()
+
+    private class DoubleToIntConverter : StdConverter<Double, Int>() {
+      override fun convert(value: Double): Int = value.toInt()
+    }
+  }
+}
