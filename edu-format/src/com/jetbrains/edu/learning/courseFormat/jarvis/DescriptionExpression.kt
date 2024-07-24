3244ea76d270aaf0ad314409ac115d5ab842418c
Stanislav.Sandler <stanislav.sandler@jetbrains.com>
2024-07-24 15:12:04 +0200

    Extracted Notification creation to an extension function.

--- edu-format/src/com/jetbrains/edu/learning/courseFormat/jarvis/DescriptionExpression.kt
+++ edu-format/src/com/jetbrains/edu/learning/courseFormat/jarvis/DescriptionExpression.kt
@@ -3,6 +3,5 @@ package com.jetbrains.edu.learning.courseFormat.jarvis
 data class DescriptionExpression(
   val promptOffset: Int,
   val prompt: String,
-  val codeBlockOffset: Int,
-  val codeBlock: String
+  val code: String
 )
