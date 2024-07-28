31b6d14ab42b1b849370da522a2b3e75f3091292
Aleksey Rostovskiy <aleksei.rostovskii@jetbrains.com>
2024-07-28 05:17:20 +0200

    Refactor TaskTextToolWindow and its implementing classes
    This commit refactors the methods in the TaskTextToolWindow interface and its implementing classes, JCEFToolWindow and SwingToolWindow.
    Specifically, the 'setText' method is replaced by 'updateTaskInfoPanel' method, which better reflects its actual function.
    Modifications are also made to properly handle disposal of 'termsQueryManager' and 'taskSpecificQueryManager' in JCEFToolWindow.
    Moreover, the method 'setTaskText' in TaskToolWindow now calls 'updateTaskInfoPanel' and 'updateTaskSpecificPanel', providing clearer workflow.

--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/taskToolWindow/ui/tab/DescriptionTab.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/taskToolWindow/ui/tab/DescriptionTab.kt
@@ -72,8 +72,7 @@ class DescriptionTab(project: Project) : TaskToolWindowTab(project, TabType.DESC
   }
 
   override fun update(task: Task) {
-    taskTextToolWindow.setTaskText(project, task)
-    taskTextToolWindow.updateTaskSpecificPanel(task)
+    taskTextToolWindow.setTaskText(task)
   }
 
   fun updateCheckDetails(task: Task, checkResult: CheckResult) = checkPanel.updateCheckDetails(task, checkResult)
