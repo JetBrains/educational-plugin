2b2633cd36f53e1c1626bee3fa29e5eb190f9779
Aleksey Rostovskiy <aleksei.rostovskii@jetbrains.com>
2024-07-31 22:50:07 +0200

    Refactor terms extraction coroutine context
    Moved the coroutine context to Dispatchers.IO directly within the launch statement to simplify the coroutine hierarchy

--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/theoryLookup/TermsProcessorActivity.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/theoryLookup/TermsProcessorActivity.kt
@@ -1,6 +1,5 @@
 package com.jetbrains.edu.learning.theoryLookup
 
-import com.intellij.openapi.progress.runBlockingCancellable
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
 import com.jetbrains.edu.learning.StudyTaskManager
@@ -10,7 +9,6 @@ import com.jetbrains.edu.learning.isUnitTestMode
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.supervisorScope
-import kotlinx.coroutines.withContext
 
 /**
  * Represents an activity that finds terms in a course.
@@ -21,13 +19,9 @@ class TermsProcessorActivity : ProjectActivity {
     val course = StudyTaskManager.getInstance(project).course as? EduCourse ?: return
 
     val termsManager = TermsManager.getInstance(project)
-    runBlockingCancellable {
-      withContext(Dispatchers.IO) {
-        supervisorScope {
-          course.allTasks.forEach { task ->
-            launch { termsManager.extractTerms(task) }
-          }
-        }
+    supervisorScope {
+      course.allTasks.forEach { task ->
+        launch(Dispatchers.IO) { termsManager.extractTerms(task) }
       }
     }
   }
