5f2ceceffed85b5c8feb112f62d308eda92beb51
Stanislav.Sandler <stanislav.sandler@jetbrains.com>
2024-08-01 18:00:11 +0200

    Refactored and fixed errors.
    Fixed an error with incorrect anchor elements for `PsiWriter`. Removed redundant `DraftBody.kt.ft`. Removed the erroneous `DumbAware` interface implementation in `DescriptionRunLineMarkerContributor`.

--- intellij-plugin/Edu-Kotlin/src/com/jetbrains/edu/kotlin/jarvis/DescriptionRunLineMarkerContributor.kt
+++ intellij-plugin/Edu-Kotlin/src/com/jetbrains/edu/kotlin/jarvis/DescriptionRunLineMarkerContributor.kt
@@ -2,7 +2,6 @@ package com.jetbrains.edu.kotlin.jarvis
 
 import com.intellij.execution.lineMarker.RunLineMarkerContributor
 import com.intellij.icons.AllIcons
-import com.intellij.openapi.project.DumbAware
 import com.intellij.psi.PsiElement
 import com.intellij.psi.impl.source.tree.LeafPsiElement
 import com.jetbrains.edu.jarvis.JarvisDslPackageCallChecker
@@ -11,7 +10,7 @@ import com.jetbrains.edu.kotlin.jarvis.utils.DESCRIPTION
 import com.jetbrains.edu.kotlin.messages.EduKotlinBundle
 import org.jetbrains.kotlin.psi.KtCallExpression
 
-class DescriptionRunLineMarkerContributor : RunLineMarkerContributor(), DumbAware {
+class DescriptionRunLineMarkerContributor : RunLineMarkerContributor() {
   override fun getInfo(element: PsiElement): Info? {
     if (element is LeafPsiElement &&
         element.parent.parent is KtCallExpression &&
