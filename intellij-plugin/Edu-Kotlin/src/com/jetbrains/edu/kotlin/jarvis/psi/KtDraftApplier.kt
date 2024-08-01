3a1acb4a54c624732dd42452a3c5e9e6e494cfe1
Stanislav.Sandler <stanislav.sandler@jetbrains.com>
2024-08-01 18:00:11 +0200

    Refactored and fixed errors.
    Fixed an error with incorrect anchor elements for `PsiWriter`. Removed redundant `DraftBody.kt.ft`. Removed the erroneous `DumbAware` interface implementation in `DescriptionRunLineMarkerContributor`.

--- intellij-plugin/Edu-Kotlin/src/com/jetbrains/edu/kotlin/jarvis/psi/KtDraftApplier.kt
+++ intellij-plugin/Edu-Kotlin/src/com/jetbrains/edu/kotlin/jarvis/psi/KtDraftApplier.kt
@@ -2,10 +2,8 @@ package com.jetbrains.edu.kotlin.jarvis.psi
 
 import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.project.Project
-import com.intellij.psi.PsiComment
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
-import com.intellij.psi.util.childrenOfType
 import com.jetbrains.edu.jarvis.DraftApplier
 import com.jetbrains.edu.kotlin.jarvis.utils.DESCRIPTION
 import com.jetbrains.edu.kotlin.jarvis.utils.DRAFT
@@ -18,17 +16,11 @@ class KtDraftApplier : DraftApplier {
     val draftBlock = findBlock(element, { it.parent }, DRAFT) as? KtCallExpression ?: error("The draft block is not found")
     val lambdaArgument = draftBlock.lambdaArguments.firstOrNull() ?: return
     val lambdaBody = lambdaArgument.getLambdaExpression()?.bodyExpression ?: return
-    // TODO: parse these comments
-    val commentsToRemove = lambdaBody.childrenOfType<PsiComment>().take(NUM_COMMENTS_TO_REMOVE)
     val descriptionBlock = findBlock(draftBlock, { it.prevSibling }, DESCRIPTION) ?: error("The description block is not found")
     WriteCommandAction.runWriteCommandAction(project, null, null, {
-      commentsToRemove.forEach { it.delete() }
       descriptionBlock.replace(lambdaBody)
       draftBlock.delete()
     }, psiFile)
   }
 
-  companion object {
-    private const val NUM_COMMENTS_TO_REMOVE = 3
-  }
 }
