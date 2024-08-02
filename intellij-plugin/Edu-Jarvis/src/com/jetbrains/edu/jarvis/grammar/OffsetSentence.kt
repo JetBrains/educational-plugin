d4bbac3beccb107d4b0a7c796cfc68ca60f174a4
Stanislav.Sandler <stanislav.sandler@jetbrains.com>
2024-08-02 20:20:37 +0200

    Added description to code highlighting
    Implemented `HighlighterManager` and `HiglightingListenerManager` classes. Added a method for code offset in `DraftExpressionWriter` interface. Refactored the `OffsetSentence` class so that the higlighting is done in the `HiglightManager` class.

--- intellij-plugin/Edu-Jarvis/src/com/jetbrains/edu/jarvis/grammar/OffsetSentence.kt
+++ intellij-plugin/Edu-Jarvis/src/com/jetbrains/edu/jarvis/grammar/OffsetSentence.kt
@@ -1,18 +1,13 @@
 package com.jetbrains.edu.jarvis.grammar
 
-import com.intellij.openapi.editor.markup.HighlighterLayer
-import com.intellij.openapi.editor.markup.HighlighterTargetArea
-import com.intellij.openapi.editor.markup.MarkupModel
-import com.intellij.openapi.editor.markup.TextAttributes
-
 /**
  * Represents a sentence with an offset.
  * @param sentence The string containing the sentence itself.
  * @param fileOffset The offset from the beginning of the file to the beginning of the sentence.
  */
 class OffsetSentence(val sentence: String, fileOffset: Int) {
-  private val startOffset: Int
-  private val endOffset: Int
+  val startOffset: Int
+  val endOffset: Int
   init {
     val trimmedLength = sentence.trimStart().length
     val trimmedOffset = sentence.length - trimmedLength
@@ -20,13 +15,4 @@ class OffsetSentence(val sentence: String, fileOffset: Int) {
     startOffset = fileOffset + trimmedOffset
     endOffset = fileOffset + trimmedOffset + sentence.trim().length
   }
-
-  /**
-   * Highlights the sentence with a given markupModel and attributes.
-   */
-  fun highlight(markupModel: MarkupModel?, attributes: TextAttributes) = markupModel?.addRangeHighlighter(
-    startOffset,
-    endOffset,
-    HighlighterLayer.ERROR, attributes, HighlighterTargetArea.EXACT_RANGE
-  )
 }
