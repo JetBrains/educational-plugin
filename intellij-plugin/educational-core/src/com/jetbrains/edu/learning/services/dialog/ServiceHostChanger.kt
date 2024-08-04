b46b9485bdaceef592ae06bd2f8b18b469aa1ddd
Aleksey Rostovskiy <aleksei.rostovskii@jetbrains.com>
2024-08-04 10:06:38 +0200

    Refactor ServiceHostChanger interface comment and delete an empty file
    Updated the comment to directly address ongoing refactor considerations and provided a link to the corresponding YouTrack issue for reference.

--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/services/dialog/ServiceHostChanger.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/services/dialog/ServiceHostChanger.kt
@@ -1,8 +1,9 @@
 package com.jetbrains.edu.learning.services.dialog
 
 /**
- * This interface should be refactored, see https://youtrack.jetbrains.com/issue/EDU-7090/Refactor-ServiceHostChanger
- * Consider not using it
+ * This interface should be refactored, consider not using it.
+ *
+ * See [EDU-7090: Refactor ServiceHostChanger](https://youtrack.jetbrains.com/issue/EDU-7090/Refactor-ServiceHostChanger)
  */
 interface ServiceHostChanger {
   fun getResultUrl(): String?
