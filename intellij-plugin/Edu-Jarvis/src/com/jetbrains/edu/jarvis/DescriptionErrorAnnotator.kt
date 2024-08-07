3fd8b81e115c7f616db17884f4db9da51bb6f630
Stanislav.Sandler <stanislav.sandler@jetbrains.com>
2024-08-07 15:37:53 +0200

    Moved classes to more relevant packages.
    Added more packages in `highlighting` to better indicate the purposes of the contained classes. The new packages are: undefinedidentifier, descriptiontocode, and grammar.

--- intellij-plugin/Edu-Jarvis/src/com/jetbrains/edu/jarvis/DescriptionErrorAnnotator.kt
+++ intellij-plugin/Edu-Jarvis/src/com/jetbrains/edu/jarvis/DescriptionErrorAnnotator.kt
@@ -6,7 +6,7 @@ import com.intellij.lang.annotation.HighlightSeverity
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
 import com.intellij.refactoring.suggested.startOffset
-import com.jetbrains.edu.jarvis.highlighting.*
+import com.jetbrains.edu.jarvis.highlighting.undefinedidentifier.*
 import com.jetbrains.edu.jarvis.messages.EduJarvisBundle
 import com.jetbrains.edu.jarvis.models.NamedFunction
 import com.jetbrains.edu.jarvis.models.NamedVariable
