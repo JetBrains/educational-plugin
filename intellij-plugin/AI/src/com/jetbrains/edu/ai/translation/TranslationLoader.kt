ac595b3751d194397035163015902cf0c18e865d
Aleksey Rostovskiy <aleksei.rostovskii@jetbrains.com>
2024-08-08 02:38:14 +0200

    EDU-7094 Implement `TranslationLoader` service
    The service provides functionality for loading and saving course translations to disk

---
+++ intellij-plugin/AI/src/com/jetbrains/edu/ai/translation/TranslationLoader.kt
@@ -0,0 +1,92 @@
+package com.jetbrains.edu.ai.translation
+
+import com.intellij.openapi.application.writeAction
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.components.service
+import com.intellij.openapi.diagnostic.thisLogger
+import com.intellij.openapi.project.Project
+import com.intellij.platform.ide.progress.runWithModalProgressBlocking
+import com.intellij.util.concurrency.annotations.RequiresBlockingContext
+import com.intellij.util.concurrency.annotations.RequiresEdt
+import com.jetbrains.edu.ai.messages.EduAIBundle
+import com.jetbrains.edu.ai.translation.connector.TranslationServiceConnector
+import com.jetbrains.edu.learning.Result
+import com.jetbrains.edu.learning.courseFormat.DescriptionFormat.Companion.TASK_DESCRIPTION_PREFIX
+import com.jetbrains.edu.learning.courseFormat.EduCourse
+import com.jetbrains.edu.learning.courseFormat.ext.allTasks
+import com.jetbrains.edu.learning.courseFormat.ext.getTaskDirectory
+import com.jetbrains.edu.learning.courseFormat.tasks.Task
+import com.jetbrains.edu.learning.courseGeneration.GeneratorUtils
+import com.jetbrains.edu.learning.onError
+import com.jetbrains.educational.translation.enum.Language
+import com.jetbrains.educational.translation.format.CourseTranslation
+import com.jetbrains.educational.translation.format.DescriptionText
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.IOException
+
+@Service(Service.Level.PROJECT)
+class TranslationLoader(private val project: Project, private val scope: CoroutineScope) {
+  @RequiresBlockingContext
+  @RequiresEdt
+  fun loadAndSaveWithModalProgress(course: EduCourse, language: Language) {
+    runWithModalProgressBlocking(project, EduAIBundle.message("ai.service.getting.course.translation")) {
+      loadAndSave(course, language)
+    }
+  }
+
+  @Suppress("MemberVisibilityCanBePrivate")
+  fun loadAndSave(course: EduCourse, language: Language) {
+    scope.launch {
+      loadAndSaveAsync(course, language)
+    }
+  }
+
+  @Suppress("MemberVisibilityCanBePrivate")
+  suspend fun loadAndSaveAsync(course: EduCourse, language: Language) {
+    withContext(Dispatchers.IO) {
+      val translation = downloadTranslation(course, language).onError { error ->
+        LOG.error("Failed to download translation for ${course.name} to $language: $error")
+        return@withContext
+      }
+      course.saveTranslation(translation)
+    }
+  }
+
+  private suspend fun downloadTranslation(course: EduCourse, language: Language): Result<CourseTranslation, String> {
+    val marketplaceId = course.marketplaceId
+    val updateVersion = course.updateVersion
+    return TranslationServiceConnector.getInstance().getTranslatedCourse(marketplaceId, updateVersion, language)
+  }
+
+  private suspend fun EduCourse.saveTranslation(courseTranslation: CourseTranslation) {
+    val taskDescriptions = courseTranslation.taskDescriptions
+    writeAction {
+      for (task in allTasks) {
+        val translation = taskDescriptions[task.taskEduId] ?: continue
+        task.saveTranslation(translation)
+      }
+    }
+  }
+
+  @RequiresBlockingContext
+  private fun Task.saveTranslation(text: DescriptionText) {
+    val taskDirectory = getTaskDirectory(project) ?: return
+    val name = "${TASK_DESCRIPTION_PREFIX}_${text.language.code}.${descriptionFormat.extension}"
+
+    try {
+      GeneratorUtils.createTextChildFile(project, taskDirectory, name, text.text)
+    }
+    catch (exception: IOException) {
+      LOG.error("Failed to write text to $taskDirectory: $exception")
+    }
+  }
+
+  companion object {
+    private val LOG = thisLogger()
+
+    fun getInstance(project: Project): TranslationLoader = project.service()
+  }
+}
--- intellij-plugin/AI/src/com/jetbrains/edu/ai/translation/utils.kt
+++ intellij-plugin/AI/src/com/jetbrains/edu/ai/translation/utils.kt
@@ -1,7 +1,9 @@
 package com.jetbrains.edu.ai.translation
 
 import com.jetbrains.edu.learning.courseFormat.EduCourse
+import com.jetbrains.edu.learning.courseFormat.tasks.Task
 import com.jetbrains.educational.core.format.domain.MarketplaceId
+import com.jetbrains.educational.core.format.domain.TaskEduId
 import com.jetbrains.educational.core.format.domain.UpdateVersion
 import com.jetbrains.educational.translation.enum.Language
 
@@ -13,3 +15,6 @@ val EduCourse.marketplaceId: MarketplaceId
 
 val EduCourse.updateVersion: UpdateVersion
   get() = UpdateVersion(course.marketplaceCourseVersion)
+
+val Task.taskEduId: TaskEduId
+  get() = TaskEduId(id)
