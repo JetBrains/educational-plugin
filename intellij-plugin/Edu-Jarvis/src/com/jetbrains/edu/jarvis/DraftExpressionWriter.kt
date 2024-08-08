f999b3536d6fd75a91c1dd2a95cc3578e6c732a3
anchouls <anna.potriasaeva@jetbrains.com>
2024-08-08 11:41:55 +0200

    Improved the code offset calculation
    Instead of a method to the `DraftExpressionWriter` interface, the code offset is now returned by the `addDraftExpression` method.

--- intellij-plugin/Edu-Jarvis/src/com/jetbrains/edu/jarvis/DraftExpressionWriter.kt
+++ intellij-plugin/Edu-Jarvis/src/com/jetbrains/edu/jarvis/DraftExpressionWriter.kt
@@ -10,25 +10,21 @@ import com.intellij.util.concurrency.ThreadingAssertions
  * Adds a `draft` DSL block with the generated code to the code after the given [PsiElement].
  */
 interface DraftExpressionWriter {
-  fun addDraftExpression(project: Project, element: PsiElement, generatedCode: String)
-
-  fun getCodeLineOffset(): Int
+  fun addDraftExpression(project: Project, element: PsiElement, generatedCode: String): Int
 
   companion object {
     private val EP_NAME = LanguageExtension<DraftExpressionWriter>("Educational.draftExpressionWriter")
 
-    fun addDraftExpression(project: Project, element: PsiElement, generatedCode: String, language: Language) {
+    /**
+     * Adds a draft expression with the generated code to the code after the given [PsiElement].
+     * @return the offset of the code inside the draft block
+     * @throws IllegalStateException if the language does not support providing a draft expression
+     */
+    fun addDraftExpression(project: Project, element: PsiElement, generatedCode: String, language: Language): Int {
       ThreadingAssertions.assertReadAccess()
-      EP_NAME.forLanguage(language)?.addDraftExpression(project, element, generatedCode)
+      return EP_NAME.forLanguage(language)?.addDraftExpression(project, element, generatedCode)
              ?: error("Not supported to provide a draft expression for the ${language.displayName} language")
     }
-
-    /**
-     * Returns the number of lines before the start of the generated code in the draft expression.
-     */
-    fun getCodeLineOffset(language: Language): Int
-      = EP_NAME.forLanguage(language)?.getCodeLineOffset()
-      ?: error("Not supported to provide a draft expression for the ${language.displayName} language")
 
   }
 }
