93a45ed2a36a88f356258c7dda43b95a83e79656
anchouls <anna.potriasaeva@jetbrains.com>
2024-08-09 15:59:01 +0200

    Fixed showing errors - previously did not reach catch block due to asynchrony

--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/theoryLookup/TermsProcessorActivity.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/theoryLookup/TermsProcessorActivity.kt
@@ -1,11 +1,15 @@
 package com.jetbrains.edu.learning.theoryLookup
 
+import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
 import com.jetbrains.edu.learning.StudyTaskManager
 import com.jetbrains.edu.learning.courseFormat.EduCourse
 import com.jetbrains.edu.learning.courseFormat.ext.allTasks
 import com.jetbrains.edu.learning.isUnitTestMode
+import com.jetbrains.edu.learning.messages.EduCoreBundle
+import com.jetbrains.edu.learning.notification.EduNotificationManager
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.supervisorScope
@@ -18,10 +22,20 @@ class TermsProcessorActivity : ProjectActivity {
     if (project.isDisposed || isUnitTestMode) return
     val course = StudyTaskManager.getInstance(project).course as? EduCourse ?: return
 
+    val exceptionHandler = CoroutineExceptionHandler { _, exception ->
+      EduNotificationManager.showErrorNotification(
+        project,
+        EduCoreBundle.message("theory.lookup.notification.failed.to.extract.terms.title"),
+        exception.message ?:
+        EduCoreBundle.message("theory.lookup.notification.failed.to.extract.terms.text")
+      )
+      thisLogger().error("Terms extracting failed", exception)
+    }
+
     val termsManager = TermsManager.getInstance(project)
     supervisorScope {
       course.allTasks.forEach { task ->
-        launch(Dispatchers.IO) { termsManager.extractTerms(task) }
+        launch(Dispatchers.IO + exceptionHandler) { termsManager.extractTerms(task) }
       }
     }
   }
