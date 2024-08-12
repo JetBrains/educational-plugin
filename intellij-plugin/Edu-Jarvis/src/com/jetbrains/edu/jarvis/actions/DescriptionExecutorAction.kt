65b10c7c8e654d47411a2329ceed375032a0d29d
Stanislav.Sandler <stanislav.sandler@jetbrains.com>
2024-08-13 00:48:17 +0200

    Add CodeGenerator.kt and streamline code execution
    Introduced CodeGenerator class to handle DSL-based code generation. Refactored DescriptionExecutorAction to utilize CodeGenerator and improved error handling. Moved CodeGenerationState to a separate file for better modularity.

--- intellij-plugin/Edu-Jarvis/src/com/jetbrains/edu/jarvis/actions/DescriptionExecutorAction.kt
+++ intellij-plugin/Edu-Jarvis/src/com/jetbrains/edu/jarvis/actions/DescriptionExecutorAction.kt
@@ -1,18 +1,18 @@
 package com.jetbrains.edu.jarvis.actions
 
+import com.jetbrains.edu.jarvis.codegeneration.CodeGenerationState
 import com.intellij.notification.NotificationType.ERROR
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.invokeLater
-import com.intellij.openapi.components.Service
-import com.intellij.openapi.components.service
 import com.intellij.openapi.progress.runBackgroundableTask
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
 import com.jetbrains.edu.jarvis.DescriptionExpressionParser
 import com.jetbrains.edu.jarvis.DraftExpressionWriter
+import com.jetbrains.edu.jarvis.codegeneration.CodeGenerator
 import com.jetbrains.edu.jarvis.grammar.GrammarParser
 import com.jetbrains.edu.jarvis.grammar.OffsetSentence
 import com.jetbrains.edu.jarvis.highlighting.HighlighterManager
@@ -24,9 +24,6 @@ import com.jetbrains.edu.learning.actions.EduActionUtils
 import com.jetbrains.edu.learning.courseFormat.jarvis.DescriptionExpression
 import com.jetbrains.edu.learning.notification.EduNotificationManager
 import com.jetbrains.educational.ml.core.exception.AiAssistantException
-import com.jetbrains.educational.ml.cognifire.responses.DescriptionToCodeResponse
-import com.jetbrains.educational.ml.cognifire.responses.GeneratedCodeLine
-import java.util.concurrent.atomic.AtomicBoolean
 
 /**
  * An action class responsible for handling the running of `description` DSL (Domain-Specific Language) elements.
@@ -35,7 +32,6 @@ import java.util.concurrent.atomic.AtomicBoolean
  * @param element The PSI element associated with the `description` DSL that this action is supposed to execute.
  */
 class DescriptionExecutorAction(private val element: PsiElement) : AnAction() {
-
   override fun actionPerformed(e: AnActionEvent) {
     val project = e.project ?: error("Project was not found")
 
@@ -43,7 +39,6 @@ class DescriptionExecutorAction(private val element: PsiElement) : AnAction() {
     // TODO: Update highlighters on PsiElement update
     HighlighterManager.getInstance(project).clearAll()
     ListenerManager.getInstance(project).clearAll()
-
 
     val descriptionExpression = DescriptionExpressionParser.parseDescriptionExpression(element, element.language)
     if (descriptionExpression == null) {
@@ -53,13 +48,11 @@ class DescriptionExecutorAction(private val element: PsiElement) : AnAction() {
       )
       return
     }
-
-    generateCode(project, descriptionExpression)
+    executeAction(project, descriptionExpression)
   }
 
   override fun getActionUpdateThread() = ActionUpdateThread.BGT
-
-  private fun generateCode(project: Project, descriptionExpression: DescriptionExpression) = runBackgroundableTask(
+  private fun executeAction(project: Project, descriptionExpression: DescriptionExpression) = runBackgroundableTask(
     EduJarvisBundle.message("action.progress.bar.message"),
     project
   ) { indicator ->
@@ -79,12 +72,15 @@ class DescriptionExecutorAction(private val element: PsiElement) : AnAction() {
       project.notifyError(content = EduJarvisBundle.message("action.not.run.due.to.ai.assistant.exception"))
       return@runBackgroundableTask
     }
+    catch (e: Throwable) {
+      project.notifyError(content = EduJarvisBundle.message("action.not.run.due.to.unknown.exception"))
+      return@runBackgroundableTask
+    }
 
     GrammarHighlighter.highlightAll(project, unparsableSentences)
 
-    CodeGenerationState.getInstance(project).unlock()
-
     if (unparsableSentences.isNotEmpty()) {
+      CodeGenerationState.getInstance(project).unlock()
       project.notifyError(
         EduJarvisBundle.message("action.not.run.due.to.incorrect.grammar.title"),
         EduJarvisBundle.message("action.not.run.due.to.incorrect.grammar.text")
@@ -92,89 +88,27 @@ class DescriptionExecutorAction(private val element: PsiElement) : AnAction() {
       return@runBackgroundableTask
     }
 
-    // TODO: get the generated code with errors
-    val descriptionToCodeTranslation = getDescriptionToCodeTranslation(descriptionExpression)
 
-    val descriptionToDraftLines = descriptionToCodeTranslation
-      .groupBy { it.descriptionLineNumber }
-      .mapValues { descriptionGroup ->
-        descriptionGroup.value.map { it.codeLineNumber }
-      }
-
-    val draftToDescriptionLines = descriptionToCodeTranslation
-      .groupBy { it.codeLineNumber }
-      .mapValues { codeGroup ->
-        codeGroup.value.map { it.descriptionLineNumber }
-      }
+    CodeGenerationState.getInstance(project).unlock()
+    val codeGenerator = CodeGenerator(descriptionExpression)
 
     invokeLater {
-      val generatedCode = descriptionToCodeTranslation.joinToString(System.lineSeparator()) { it.generatedCodeLine }
+      val generatedCode = codeGenerator.generatedCode
       // TODO: reformat and improve the generated code
       val draftBodyOffset = DraftExpressionWriter.addDraftExpression(project, element, generatedCode, element.language)
 
       DescriptionToDraftHighlighter(project).setUp(
         descriptionExpression.promptOffset,
         draftBodyOffset,
-        descriptionToDraftLines,
-        draftToDescriptionLines,
+        codeGenerator.descriptionToDraftLines,
+        codeGenerator.draftToDescriptionLines
       )
     }
   }
 
-  // TODO: Generate code using ML library
-  private fun getDescriptionToCodeTranslation(descriptionExpression: DescriptionExpression): DescriptionToCodeResponse {
-    return when (descriptionExpression.prompt.filter { !it.isWhitespace() }) {
-      """Read user input as an integer and save the result to `userInput`. 
-      If `userInput` is divisible by 3,
-      Print "Divisible by 3".
-      Else,
-      Print "Not divisible by 3".
-      """.filter { !it.isWhitespace() } -> {
-        listOf(
-          GeneratedCodeLine(0, 0, "val userInput = readln().toInt()"),
-          GeneratedCodeLine(1, 1, "if(userInput % 3 == 0) {"),
-          GeneratedCodeLine(2, 2, "  println(\"Divisible by 3\")"),
-          GeneratedCodeLine(3, 3, "} else {"),
-          GeneratedCodeLine(4, 4, "  println(\"Not divisible by 3\")"),
-          GeneratedCodeLine(3, 5, "}")
-        )
-      }
-
-      """Read user input as an integer and save the result to `userInput`. 
-      If `userInput` is divisible by 3,
-      Print "Divisible by 3".
-      """.filter { !it.isWhitespace() }  -> {
-        listOf(
-          GeneratedCodeLine(0, 0, "val userInput = readln().toInt()"),
-          GeneratedCodeLine(1, 1, "if(userInput % 3 == 0) {"),
-          GeneratedCodeLine(2, 2, "  println(\"Divisible by 3\")"),
-          GeneratedCodeLine(1, 3, "}"),
-        )
-      }
-
-      else -> emptyList()
-    }
-  }
-
-  private fun Project.notifyError(title: String = "", content: String = "") =
+  private fun Project.notifyError(title: String = "", content: String) =
     EduNotificationManager.create(
       ERROR, content, title
     ).notify(this)
 
-  @Service(Service.Level.PROJECT)
-  private class CodeGenerationState {
-    private val isBusy = AtomicBoolean(false)
-
-    fun lock(): Boolean {
-      return isBusy.compareAndSet(false, true)
-    }
-
-    fun unlock() {
-      isBusy.set(false)
-    }
-
-    companion object {
-      fun getInstance(project: Project): CodeGenerationState = project.service()
-    }
-  }
 }
