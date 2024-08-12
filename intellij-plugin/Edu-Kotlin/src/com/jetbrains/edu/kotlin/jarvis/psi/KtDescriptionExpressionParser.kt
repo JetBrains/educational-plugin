526ca01b3f348889e7659c9df2f8dd3db1005a10
Stanislav.Sandler <stanislav.sandler@jetbrains.com>
2024-08-13 00:47:52 +0200

    Create DescriptionExpression function signatures.
    Added a new field `functionSignature` to `DescriptionExpression` to store the signature of the containing function. Updated `KtDescriptionExpressionParser` to parse and include function signatures in `DescriptionExpression` for better context representation.

--- intellij-plugin/Edu-Kotlin/src/com/jetbrains/edu/kotlin/jarvis/psi/KtDescriptionExpressionParser.kt
+++ intellij-plugin/Edu-Kotlin/src/com/jetbrains/edu/kotlin/jarvis/psi/KtDescriptionExpressionParser.kt
@@ -3,10 +3,12 @@ package com.jetbrains.edu.kotlin.jarvis.psi
 import ai.grazie.utils.dropPostfix
 import com.intellij.psi.PsiElement
 import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.psi.util.findParentOfType
 import com.jetbrains.edu.jarvis.DescriptionExpressionParser
 import com.jetbrains.edu.kotlin.jarvis.utils.isDescriptionBlock
 import com.jetbrains.edu.learning.courseFormat.jarvis.DescriptionExpression
 import org.jetbrains.kotlin.psi.KtCallExpression
+import org.jetbrains.kotlin.psi.KtNamedFunction
 
 class KtDescriptionExpressionParser : DescriptionExpressionParser {
   override fun parseDescriptionExpression(descriptionExpression: PsiElement): DescriptionExpression? {
@@ -15,6 +17,8 @@ class KtDescriptionExpressionParser : DescriptionExpressionParser {
         existsNestedDescriptionExpressions(descriptionExpression)) {
       return null
     }
+    val containingFunction = descriptionExpression.findParentOfType<KtNamedFunction>() ?: return null
+
     val descriptionPromptPsi = descriptionExpression.valueArguments.firstOrNull()
     val descriptionCodeBlockPsi = descriptionExpression.lambdaArguments.firstOrNull()?.getLambdaExpression()
 
@@ -24,10 +28,20 @@ class KtDescriptionExpressionParser : DescriptionExpressionParser {
     val trimmedOffset = descriptionPromptText.length - trimmedDescriptionPromptText.length
 
     return DescriptionExpression(
+      containingFunction.getSignature(),
       (descriptionPromptPsi?.textOffset ?: 0) + trimmedOffset,
       trimmedDescriptionPromptText.dropPostfix(TRIM_INDENT_POSTFIX).dropPostfix(QUOTE_POSTFIX),
       descriptionCodeBlockPsi?.bodyExpression?.text ?: ""
     )
+  }
+
+  private fun KtNamedFunction.getSignature(): String {
+    val parameterListString = valueParameterList?.parameters?.joinToString(ARGUMENT_SEPARATOR) { param ->
+        "${param.name}: ${param.typeReference?.text ?: ""}"
+    }
+    val returnType = typeReference?.text
+    return "fun $name ($parameterListString)" +
+           if(returnType != null) ": $returnType" else ""
   }
 
   private fun existsNestedDescriptionExpressions(descriptionExpression: KtCallExpression) =
@@ -37,5 +51,6 @@ class KtDescriptionExpressionParser : DescriptionExpressionParser {
     private const val QUOTE_CHAR = '"'
     private const val TRIM_INDENT_POSTFIX = ".trimIndent()"
     private const val QUOTE_POSTFIX = "\"\"\""
+    private const val ARGUMENT_SEPARATOR = ", "
   }
 }
