9075b71736353bed29344632fbdfd5d4f8845276
Stanislav.Sandler <stanislav.sandler@jetbrains.com>
2024-08-13 00:47:52 +0200

    Create DescriptionExpression function signatures.
    Added a new field `functionSignature` to `DescriptionExpression` to store the signature of the containing function. Updated `KtDescriptionExpressionParser` to parse and include function signatures in `DescriptionExpression` for better context representation.

--- edu-format/src/com/jetbrains/edu/learning/courseFormat/jarvis/DescriptionExpression.kt
+++ edu-format/src/com/jetbrains/edu/learning/courseFormat/jarvis/DescriptionExpression.kt
@@ -1,6 +1,7 @@
 package com.jetbrains.edu.learning.courseFormat.jarvis
 
 data class DescriptionExpression(
+  val functionSignature: String,
   val promptOffset: Int,
   val prompt: String,
   val code: String
