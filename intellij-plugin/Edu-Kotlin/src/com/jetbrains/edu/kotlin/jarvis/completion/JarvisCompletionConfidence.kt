0e402ce6ba3c2edef2e6a99a4c0a61121a8ff9c9
Stanislav.Sandler <stanislav.sandler@jetbrains.com>
2024-08-15 16:55:52 +0200

    Refactor and optimize element search logic
    Refactored element search. Implemented draft applying for new DSL.

--- intellij-plugin/Edu-Kotlin/src/com/jetbrains/edu/kotlin/jarvis/completion/JarvisCompletionConfidence.kt
+++ intellij-plugin/Edu-Kotlin/src/com/jetbrains/edu/kotlin/jarvis/completion/JarvisCompletionConfidence.kt
@@ -16,7 +16,7 @@ import org.jetbrains.kotlin.psi.KtLiteralStringTemplateEntry
  */
 class JarvisCompletionConfidence : CompletionConfidence() {
   override fun shouldSkipAutopopup(contextElement: PsiElement, psiFile: PsiFile, offset: Int): ThreeState {
-    val descriptionBlock = ElementSearch.findDescriptionElement(contextElement)
+    val descriptionBlock = ElementSearch.findDescriptionElement(contextElement) { it.nextSibling }
     if (descriptionBlock != null && contextElement.parent is KtLiteralStringTemplateEntry) {
       return ThreeState.NO
     }
