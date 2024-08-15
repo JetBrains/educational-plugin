81b31b59dc7fb6684695d0763faa5adcb508436a
Stanislav.Sandler <stanislav.sandler@jetbrains.com>
2024-08-15 16:55:52 +0200

    Refactor and optimize element search logic
    Refactored element search. Implemented draft applying for new DSL.

--- intellij-plugin/Edu-Kotlin/src/com/jetbrains/edu/kotlin/jarvis/psi/ElementSearch.kt
+++ intellij-plugin/Edu-Kotlin/src/com/jetbrains/edu/kotlin/jarvis/psi/ElementSearch.kt
@@ -3,6 +3,7 @@ package com.jetbrains.edu.kotlin.jarvis.psi
 import com.intellij.psi.PsiElement
 import com.jetbrains.edu.jarvis.JarvisDslPackageCallChecker
 import com.jetbrains.edu.kotlin.jarvis.utils.DESCRIPTION
+import com.jetbrains.edu.kotlin.jarvis.utils.DRAFT
 import com.jetbrains.edu.kotlin.jarvis.utils.RETURN_DRAFT
 import com.jetbrains.edu.kotlin.jarvis.utils.findBlock
 import org.jetbrains.kotlin.psi.KtCallExpression
@@ -10,16 +11,22 @@ import org.jetbrains.kotlin.psi.KtReturnExpression
 import org.jetbrains.kotlin.psi.psiUtil.getChildOfType
 
 object ElementSearch {
-  fun findReturnDraftElement(element: PsiElement): KtReturnExpression?
-    = findBlock(element, { it.nextSibling }, { it?.isReturnDraftElement() ?: true }) as? KtReturnExpression
+  fun findReturnDraftElement(element: PsiElement, step: (PsiElement) -> PsiElement?): KtReturnExpression?
+    = findBlock(element, step) { it.isReturnDraftElement() } as? KtReturnExpression
 
-  fun findDescriptionElement(element: PsiElement): PsiElement?
-    = findBlock(element, { it.nextSibling }, { it?.isDescriptionElement() ?: true })
+  fun findDraftElement(element: PsiElement, step: (PsiElement) -> PsiElement?): PsiElement?
+    = findBlock(element, step) { it.isDraftElement() }
+
+  fun findDescriptionElement(element: PsiElement, step: (PsiElement) -> PsiElement?): PsiElement?
+    = findBlock(element, step) { it.isDescriptionElement() }
+
+  private fun PsiElement.isDraftElement() =
+    text.startsWith(DRAFT) && JarvisDslPackageCallChecker.isCallFromJarvisDslPackage(this, language)
 
   private fun PsiElement.isDescriptionElement() =
     text.startsWith(DESCRIPTION) && JarvisDslPackageCallChecker.isCallFromJarvisDslPackage(this, language)
 
-  private fun PsiElement.isReturnDraftElement() =
+  private fun PsiElement?.isReturnDraftElement() =
     this is KtReturnExpression
     && text.startsWith(RETURN_DRAFT)
     && getDraftBlock()?.let { JarvisDslPackageCallChecker.isCallFromJarvisDslPackage(it, it.language) } == true
