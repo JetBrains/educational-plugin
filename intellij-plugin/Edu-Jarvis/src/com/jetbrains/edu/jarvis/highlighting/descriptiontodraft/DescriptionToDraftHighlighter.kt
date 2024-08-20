a1157f43579ee2e28109665eea84c6568eb9532d
Stanislav.Sandler <stanislav.sandler@jetbrains.com>
2024-08-20 17:17:49 +0200

    Refactor and improve highlighter management.
    Separate highlighters into specific types for better clarity and manageability. Added methods to clear specific types of highlighters and updated all relevant classes to reflect these changes.

--- intellij-plugin/Edu-Jarvis/src/com/jetbrains/edu/jarvis/highlighting/descriptiontodraft/DescriptionToDraftHighlighter.kt
+++ intellij-plugin/Edu-Jarvis/src/com/jetbrains/edu/jarvis/highlighting/descriptiontodraft/DescriptionToDraftHighlighter.kt
@@ -2,27 +2,21 @@ package com.jetbrains.edu.jarvis.highlighting.descriptiontodraft
 
 import com.intellij.openapi.editor.event.EditorMouseEvent
 import com.intellij.openapi.editor.event.EditorMouseMotionListener
-import com.intellij.openapi.editor.markup.RangeHighlighter
 import com.intellij.openapi.editor.markup.TextAttributes
 import com.intellij.openapi.project.Project
 import com.intellij.ui.JBColor
 import com.jetbrains.edu.jarvis.highlighting.HighlighterManager
 import com.jetbrains.edu.jarvis.highlighting.ListenerManager
 
-
 /**
  * Class DescriptionToDraftHighlighter is responsible for highlighting the description and draft lines
  * in an editor based on the mouse movement.
  *
  * @property project The current project.
- * @property descriptionHighlighters The list of description highlighters.
- * @property draftHighlighters The list of draft highlighters.
  * @property highlighterManager The instance of HighlighterManager.
  */
 class DescriptionToDraftHighlighter(private val project: Project) {
 
-  private val descriptionHighlighters = mutableListOf<RangeHighlighter>()
-  private val draftHighlighters = mutableListOf<RangeHighlighter>()
   private val highlighterManager = HighlighterManager.getInstance(project)
 
   fun setUp(
@@ -31,29 +25,30 @@ class DescriptionToDraftHighlighter(private val project: Project) {
     draftToDescriptionLines: Map<Int, List<Int>>,
     draftBodyOffset: Int,
   ) = object : EditorMouseMotionListener {
-    override fun mouseMoved(e: EditorMouseEvent) {
+    override fun mouseMoved(e: EditorMouseEvent)
+    {
       val editor = e.editor
       val selectedLineWithOffset = editor.xyToLogicalPosition(e.mouseEvent.point).line
 
       val descriptionLineOffset = editor.document.getLineNumber(descriptionOffset)
       val draftLineOffset = editor.document.getLineNumber(draftBodyOffset)
 
+      highlighterManager.clearDescriptionToDraftHighlighters()
 
       showHighlighters(
         selectedLineWithOffset - descriptionLineOffset,
-        descriptionLineOffset,
         draftLineOffset,
+        descriptionLineOffset,
         descriptionToDraftLines,
-        draftToDescriptionLines,
-        true
+        draftToDescriptionLines
       )
+
       showHighlighters(
         selectedLineWithOffset - draftLineOffset,
-        draftLineOffset,
         descriptionLineOffset,
+        draftLineOffset,
         draftToDescriptionLines,
-        descriptionToDraftLines,
-        false
+        descriptionToDraftLines
       )
     }
   }.also {
@@ -66,36 +61,18 @@ class DescriptionToDraftHighlighter(private val project: Project) {
     toLineOffset: Int,
     fromMap: Map<Int, List<Int>>,
     toMap: Map<Int, List<Int>>,
-    descriptionToDraft: Boolean
   ) {
-    fromMap[fromLine]?.map { it + toLineOffset }
-      ?.let { fromLinesWithOffset ->
-        val lines = fromMap[fromLine]?.let { toMap[it.singleOrNull()] }
-                    ?: listOf(fromLine)
-        val toLinesWithOffset = lines.map { it + fromLineOffset }
-        if (descriptionToDraft) setHighlighters(fromLinesWithOffset, toLinesWithOffset)
-        else setHighlighters(toLinesWithOffset, fromLinesWithOffset)
-      }
+    val fromLines = fromMap[fromLine] ?: return
+    val fromLinesWithOffset = fromLines.map { it + fromLineOffset }
+    val toLines = toMap[fromLines.singleOrNull()] ?: listOf(fromLine)
+    val toLinesWithOffset = toLines.map { it + toLineOffset }
+
+    addHighlighters(fromLinesWithOffset + toLinesWithOffset)
   }
 
-
-  private fun setHighlighters(descriptionLines: List<Int>, draftLines: List<Int>) {
-    descriptionHighlighters.clearAndDispose()
-    draftHighlighters.clearAndDispose()
-    descriptionHighlighters.addHighlighters(descriptionLines)
-    draftHighlighters.addHighlighters(draftLines)
-  }
-
-  private fun MutableList<RangeHighlighter>.clearAndDispose() {
-    forEach { it.dispose() }
-    clear()
-  }
-
-  private fun MutableList<RangeHighlighter>.addHighlighters(lines: List<Int>) = try {
+  private fun addHighlighters(lines: List<Int>) = try {
     lines.forEach { line ->
-      highlighterManager.addLineHighlighter(line, attributes)?.let { highlighter ->
-        add(highlighter)
-      }
+      highlighterManager.addDescriptionToDraftHighlighter(line, attributes)
     }
   }
   catch (_: IndexOutOfBoundsException) {
