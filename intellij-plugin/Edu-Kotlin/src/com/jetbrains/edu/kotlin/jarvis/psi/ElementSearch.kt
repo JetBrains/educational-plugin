a0350c51d0b190530be44f94a841679022aa2896
Stanislav.Sandler <stanislav.sandler@jetbrains.com>
2024-08-20 11:43:59 +0200

    Remove return type from draft handling logic
    Eliminated the return type from draft block handling across the codebase. This involved removing associated constants, refactoring functions, and updating templates, ensuring a streamlined approach to draft expression processing.

--- intellij-plugin/Edu-Kotlin/src/com/jetbrains/edu/kotlin/jarvis/psi/ElementSearch.kt
+++ intellij-plugin/Edu-Kotlin/src/com/jetbrains/edu/kotlin/jarvis/psi/ElementSearch.kt
@@ -4,32 +4,20 @@ import com.intellij.psi.PsiElement
 import com.jetbrains.edu.jarvis.JarvisDslPackageCallChecker
 import com.jetbrains.edu.kotlin.jarvis.utils.DESCRIPTION
 import com.jetbrains.edu.kotlin.jarvis.utils.DRAFT
-import com.jetbrains.edu.kotlin.jarvis.utils.RETURN_DRAFT
 import com.jetbrains.edu.kotlin.jarvis.utils.findBlock
 import org.jetbrains.kotlin.psi.KtCallExpression
-import org.jetbrains.kotlin.psi.KtReturnExpression
-import org.jetbrains.kotlin.psi.psiUtil.getChildOfType
 
 object ElementSearch {
-  fun findReturnDraftElement(element: PsiElement, step: (PsiElement) -> PsiElement?): KtReturnExpression?
-    = findBlock(element, step) { it.isReturnDraftElement() } as? KtReturnExpression
 
-  fun findDraftElement(element: PsiElement, step: (PsiElement) -> PsiElement?): PsiElement?
-    = findBlock(element, step) { it.isDraftElement() }
+  fun findDraftElement(element: PsiElement, step: (PsiElement) -> PsiElement?): KtCallExpression? =
+    findBlock(element, step) { it.isDraftElement() } as? KtCallExpression
 
-  fun findDescriptionElement(element: PsiElement, step: (PsiElement) -> PsiElement?): PsiElement?
-    = findBlock(element, step) { it.isDescriptionElement() }
+  fun findDescriptionElement(element: PsiElement, step: (PsiElement) -> PsiElement?): PsiElement? =
+    findBlock(element, step) { it.isDescriptionElement() }
 
   private fun PsiElement.isDraftElement() =
     text.startsWith(DRAFT) && JarvisDslPackageCallChecker.isCallFromJarvisDslPackage(this, language)
 
   private fun PsiElement.isDescriptionElement() =
     text.startsWith(DESCRIPTION) && JarvisDslPackageCallChecker.isCallFromJarvisDslPackage(this, language)
-
-  private fun PsiElement?.isReturnDraftElement() =
-    this is KtReturnExpression
-    && text.startsWith(RETURN_DRAFT)
-    && getDraftBlock()?.let { JarvisDslPackageCallChecker.isCallFromJarvisDslPackage(it, it.language) } == true
-
-  fun PsiElement.getDraftBlock() = children.firstOrNull()?.getChildOfType<KtCallExpression>()
 }
