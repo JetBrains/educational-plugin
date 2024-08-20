701f60727108848c52db7b2114399dfc14502a71
Stanislav.Sandler <stanislav.sandler@jetbrains.com>
2024-08-20 16:59:17 +0200

    Refactor DraftExpression returns and encapsulate offsets
    Refactored functions to return a DraftExpression object instead of just an integer offset to better encapsulate related data including code, start offset, and end offset. Also added the DraftExpression data class to model these attributes effectively.

--- intellij-plugin/Edu-Jarvis/src/com/jetbrains/edu/jarvis/DraftExpressionWriter.kt
+++ intellij-plugin/Edu-Jarvis/src/com/jetbrains/edu/jarvis/DraftExpressionWriter.kt
@@ -5,12 +5,13 @@ import com.intellij.lang.LanguageExtension
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
 import com.intellij.util.concurrency.ThreadingAssertions
+import com.jetbrains.edu.jarvis.models.DraftExpression
 
 /**
  * Adds a `draft` DSL block with the generated code to the code after the given [PsiElement].
  */
 interface DraftExpressionWriter {
-  fun addDraftExpression(project: Project, element: PsiElement, generatedCode: String): Int
+  fun addDraftExpression(project: Project, element: PsiElement, generatedCode: String): DraftExpression
 
   companion object {
     private val EP_NAME = LanguageExtension<DraftExpressionWriter>("Educational.draftExpressionWriter")
@@ -20,7 +21,7 @@ interface DraftExpressionWriter {
      * @return the offset of the code inside the draft block
      * @throws IllegalStateException if the language doesn't support providing a draft expression
      */
-    fun addDraftExpression(project: Project, element: PsiElement, generatedCode: String, language: Language): Int {
+    fun addDraftExpression(project: Project, element: PsiElement, generatedCode: String, language: Language): DraftExpression {
       ThreadingAssertions.assertReadAccess()
       return EP_NAME.forLanguage(language)?.addDraftExpression(project, element, generatedCode)
              ?: error("Not supported to provide a draft expression for the ${language.displayName} language")
