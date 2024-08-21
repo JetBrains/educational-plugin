f4a44dbbe0259fb42da90629f755d81c7f6a0fb3
Stanislav.Sandler <stanislav.sandler@jetbrains.com>
2024-08-21 13:15:08 +0200

    Refactor description-to-draft highlighting logic
    Reorganize parameters in DescriptionToDraftHighlighter for clarity and consistency. Updated related invocation points to align with the new parameter structure and improved readability of underlying methods.
    
    Added if statements to improve readability.
    
    In the `mouseMove` method of the `EditorMouseMotion` listener, I added conditionals to explicitly mark what part is being highlighted.

--- intellij-plugin/Edu-Jarvis/src/com/jetbrains/edu/jarvis/highlighting/descriptiontodraft/DescriptionToDraftHighlighter.kt
+++ intellij-plugin/Edu-Jarvis/src/com/jetbrains/edu/jarvis/highlighting/descriptiontodraft/DescriptionToDraftHighlighter.kt
@@ -19,11 +19,23 @@ class DescriptionToDraftHighlighter(private val project: Project) {
 
   private val highlighterManager = HighlighterManager.getInstance(project)
 
+  /**
+   * Sets up the EditorMouseMotionListener to handle mouse movement events in the editor.
+   * The method creates an anonymous class implementation of the EditorMouseMotionListener interface
+   *  and registers it with the ListenerManager. The listener handles the logic for highlighting lines of code based on the mouse position.
+   *
+   * @param descriptionOffset The offset in the document where the description section starts.
+   * @param draftOffset The offset in the document where the draft section starts.
+   * @param descriptionToDraftLines A map that contains the line numbers in the description section as the keys
+   * and a list of corresponding line numbers in the draft section as the values.
+   * @param draftToDescriptionLines A map that contains the line numbers in the draft section as the keys
+   * and a list of corresponding line numbers in the description section as the values.
+   */
   fun setUp(
     descriptionOffset: Int,
+    draftOffset: Int,
     descriptionToDraftLines: Map<Int, List<Int>>,
     draftToDescriptionLines: Map<Int, List<Int>>,
-    draftBodyOffset: Int,
   ) = object : EditorMouseMotionListener {
     override fun mouseMoved(e: EditorMouseEvent)
     {
@@ -31,22 +43,21 @@ class DescriptionToDraftHighlighter(private val project: Project) {
       val selectedLineWithOffset = editor.xyToLogicalPosition(e.mouseEvent.point).line
 
       val descriptionLineOffset = editor.document.getLineNumber(descriptionOffset)
-      val draftLineOffset = editor.document.getLineNumber(draftBodyOffset)
+      val draftLineOffset = editor.document.getLineNumber(draftOffset)
 
       highlighterManager.clearDescriptionToDraftHighlighters()
 
-      showHighlighters(
+      if(selectedLineWithOffset - descriptionLineOffset in descriptionToDraftLines.keys) showHighlighters(
         selectedLineWithOffset - descriptionLineOffset,
-        draftLineOffset,
         descriptionLineOffset,
+        draftLineOffset,
         descriptionToDraftLines,
         draftToDescriptionLines
       )
-
-      showHighlighters(
+      else if (selectedLineWithOffset - draftLineOffset in draftToDescriptionLines.keys) showHighlighters(
         selectedLineWithOffset - draftLineOffset,
-        descriptionLineOffset,
         draftLineOffset,
+        descriptionLineOffset,
         draftToDescriptionLines,
         descriptionToDraftLines
       )
@@ -56,18 +67,18 @@ class DescriptionToDraftHighlighter(private val project: Project) {
   }
 
   private fun showHighlighters(
-    fromLine: Int,
-    fromLineOffset: Int,
-    toLineOffset: Int,
-    fromMap: Map<Int, List<Int>>,
-    toMap: Map<Int, List<Int>>,
+    originLine: Int,
+    originLineOffset: Int,
+    destinationLineOffset: Int,
+    originToDestination: Map<Int, List<Int>>,
+    destinationToOrigin: Map<Int, List<Int>>,
   ) {
-    val fromLines = fromMap[fromLine] ?: return
-    val fromLinesWithOffset = fromLines.map { it + fromLineOffset }
-    val toLines = toMap[fromLines.singleOrNull()] ?: listOf(fromLine)
-    val toLinesWithOffset = toLines.map { it + toLineOffset }
+    val destinationLines = originToDestination[originLine] ?: return
+    val destinationLinesWithOffset = destinationLines.map { it + destinationLineOffset }
+    val originLines = destinationToOrigin[destinationLines.singleOrNull()] ?: listOf(originLine)
+    val originLinesWithOffset = originLines.map { it + originLineOffset }
 
-    addHighlighters(fromLinesWithOffset + toLinesWithOffset)
+    addHighlighters(destinationLinesWithOffset + originLinesWithOffset)
   }
 
   private fun addHighlighters(lines: List<Int>) = try {
