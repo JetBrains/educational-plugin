1e14e2c98b0c8261ed1512d4fd2898dac4f993e4
anchouls <anna.potriasaeva@jetbrains.com>
2024-08-21 10:33:04 +0200

    Moved the definition of the CoroutineContext into TermsManager

--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/theoryLookup/TermsManager.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/theoryLookup/TermsManager.kt
@@ -3,20 +3,40 @@ package com.jetbrains.edu.learning.theoryLookup
 import com.intellij.openapi.application.runReadAction
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.service
+import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
 import com.intellij.util.messages.Topic
 import com.jetbrains.edu.learning.courseFormat.ext.getDescriptionFile
 import com.jetbrains.edu.learning.courseFormat.tasks.Task
 import com.jetbrains.edu.learning.createTopic
 import com.jetbrains.edu.learning.getTextFromTaskTextFile
+import com.jetbrains.edu.learning.messages.EduCoreBundle
+import com.jetbrains.edu.learning.notification.EduNotificationManager
 import com.jetbrains.educational.ml.theory.lookup.core.TermsProvider
+import kotlinx.coroutines.*
 import java.util.concurrent.ConcurrentHashMap
+import kotlin.coroutines.CoroutineContext
 
 /**
  * Searches for terms in a task using the ml library and stores a map that stores a list of terms for each task.
  */
 @Service(Service.Level.PROJECT)
-class TermsManager(private val project: Project) {
+class TermsManager(private val project: Project) : CoroutineScope {
+
+  private val job = SupervisorJob()
+
+  private val exceptionHandler = CoroutineExceptionHandler { _, exception ->
+    EduNotificationManager.showErrorNotification(
+      project,
+      EduCoreBundle.message("theory.lookup.notification.failed.to.extract.terms.title"),
+      exception.message ?:
+      EduCoreBundle.message("theory.lookup.notification.failed.to.extract.terms.text")
+    )
+    thisLogger().error("Terms extracting failed", exception)
+  }
+
+  override val coroutineContext: CoroutineContext
+    get() = Dispatchers.IO + job + exceptionHandler
 
   /**
    * Represents a map of terms (value and definition) for each task that may be of potential interest to the user.
