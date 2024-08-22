83c8d830946761b9d0a4ef6c3c5228a2076d2737
anchouls <anna.potriasaeva@jetbrains.com>
2024-08-22 16:45:16 +0200

    Implemented disabling the check button until the student runs everything for generation

--- intellij-plugin/Edu-Kotlin/src/com/jetbrains/edu/kotlin/jarvis/DescriptionRunLineMarkerContributor.kt
+++ intellij-plugin/Edu-Kotlin/src/com/jetbrains/edu/kotlin/jarvis/DescriptionRunLineMarkerContributor.kt
@@ -8,18 +8,27 @@ import com.jetbrains.edu.jarvis.JarvisDslPackageCallChecker
 import com.jetbrains.edu.jarvis.actions.DescriptionExecutorAction
 import com.jetbrains.edu.kotlin.jarvis.utils.DESCRIPTION
 import com.jetbrains.edu.kotlin.messages.EduKotlinBundle
+import com.jetbrains.edu.learning.actions.EduActionUtils.getCurrentTask
+import com.jetbrains.edu.learning.taskToolWindow.ui.TaskToolWindowView
 import org.jetbrains.kotlin.psi.KtCallExpression
 
 class DescriptionRunLineMarkerContributor : RunLineMarkerContributor() {
   override fun getInfo(element: PsiElement): Info? {
+    val project = element.project
+    val task = project.getCurrentTask() ?: return null
+    val targetElement = element.parent.parent
     if (element is LeafPsiElement &&
-        element.parent.parent is KtCallExpression &&
+        targetElement is KtCallExpression &&
         element.text == DESCRIPTION &&
         JarvisDslPackageCallChecker.isCallFromJarvisDslPackage(element.parent.parent, element.language)
       ) {
+      val uniqueId = "${targetElement.containingFile.name}:${targetElement.textOffset}"
+      val action = DescriptionExecutorAction(targetElement, uniqueId)
+      task.promptActions.addAction(uniqueId)
+      TaskToolWindowView.getInstance(project).updateCheckPanel(task)
       return Info(
         AllIcons.Actions.Execute,
-        arrayOf(DescriptionExecutorAction(element.parent.parent)),
+        arrayOf(action),
       ) { _ ->
         EduKotlinBundle.message("action.run.generate.code.text")
       }
