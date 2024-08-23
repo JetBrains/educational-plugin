ffd66ced9243997950a01c0a70386f79e5c97aa3
anchouls <anna.potriasaeva@jetbrains.com>
2024-08-23 16:54:02 +0200

    Overrided the equals in PromptAction class and renamed function in GeneratedCodeParser class

--- intellij-plugin/Edu-Jarvis/src/com/jetbrains/edu/jarvis/GeneratedCodeParser.kt
+++ intellij-plugin/Edu-Jarvis/src/com/jetbrains/edu/jarvis/GeneratedCodeParser.kt
@@ -9,14 +9,14 @@ import com.intellij.util.concurrency.ThreadingAssertions
  * Parses the generated code and returns a list of errors (error description inside TODO blocks)
  */
 interface GeneratedCodeParser {
-  fun parseGeneratedCode(project: Project, generatedCode: String): List<String>
+  fun hasErrors(project: Project, generatedCode: String): Boolean
 
   companion object {
     private val EP_NAME = LanguageExtension<GeneratedCodeParser>("Educational.generatedCodeParser")
 
-    fun parseGeneratedCode(project: Project, generatedCode: String, language: Language): List<String> {
+    fun hasErrors(project: Project, generatedCode: String, language: Language): Boolean {
       ThreadingAssertions.assertReadAccess()
-      return EP_NAME.forLanguage(language).parseGeneratedCode(project, generatedCode)
+      return EP_NAME.forLanguage(language).hasErrors(project, generatedCode)
     }
   }
 }
