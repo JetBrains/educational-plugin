d76e6ec3bc4e83c2277406acae72322252b697d1
Gleb Oborin <gleb.oborin@jetbrains.com>
2024-08-27 03:27:49 +0200

    EDU-7160: Prohibit the downloading of hyperskill courses with JDK > 19
    (cherry picked from commit f2a04abcbe0e79104cba0661271ee929e0d11e32)

--- intellij-plugin/jvm-core/src/com/jetbrains/edu/jvm/JdkEnvironmentSettings.kt
+++ intellij-plugin/jvm-core/src/com/jetbrains/edu/jvm/JdkEnvironmentSettings.kt
@@ -1,14 +1,22 @@
 package com.jetbrains.edu.jvm
 
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.projectRoots.JavaSdkVersion
 import com.intellij.openapi.roots.LanguageLevelProjectExtension
 import com.jetbrains.edu.learning.courseFormat.Course
+import com.jetbrains.edu.learning.courseFormat.hyperskill.HyperskillCourse
 import org.jetbrains.annotations.VisibleForTesting
 
 private const val JVM_LANGUAGE_LEVEL = "jvm_language_level"
 
 val Course.minJvmSdkVersion: ParsedJavaVersion
   get() = ParsedJavaVersion.fromStringLanguageLevel(environmentSettings[JVM_LANGUAGE_LEVEL])
+
+val Course.maxJvmSdkVersion: ParsedJavaVersion?
+  get() = when(this) {
+    is HyperskillCourse -> JavaVersionParseSuccess(JavaSdkVersion.JDK_19)
+    else -> null
+  }
 
 fun jvmEnvironmentSettings(project: Project): Map<String, String> = mapOf(
   JVM_LANGUAGE_LEVEL to LanguageLevelProjectExtension.getInstance(project).languageLevel.toString()
