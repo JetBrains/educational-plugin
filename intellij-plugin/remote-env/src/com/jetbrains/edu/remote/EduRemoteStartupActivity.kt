6e753f2f07a5e783b64f9aa2414ca4eb16dde5e6
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2024-09-02 17:59:13 +0200

    EDU-7210: disable IDE and plugin advertising for JetBrains Academy course launched in remote environment

--- intellij-plugin/remote-env/src/com/jetbrains/edu/remote/EduRemoteStartupActivity.kt
+++ intellij-plugin/remote-env/src/com/jetbrains/edu/remote/EduRemoteStartupActivity.kt
@@ -3,11 +3,16 @@ package com.jetbrains.edu.remote
 import com.intellij.openapi.project.NOTIFICATIONS_SILENT_MODE
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.StartupActivity
+import com.intellij.openapi.updateSettings.impl.pluginsAdvertisement.disableTryUltimate
+import com.jetbrains.edu.learning.EduUtilsKt.isEduProject
 import com.jetbrains.edu.learning.RemoteEnvHelper.Companion.isRemoteDevServer
 
 class EduRemoteStartupActivity : StartupActivity.DumbAware {
   override fun runActivity(project: Project) {
-    if (!isRemoteDevServer()) return
+    if (!project.isEduProject() || !isRemoteDevServer()) return
     NOTIFICATIONS_SILENT_MODE.set(project, true)
+    // Disables editor notifications which promote IDE/plugins.
+    // They don't make sense in remote environment since users don't control environment in this case
+    disableTryUltimate(project)
   }
 }
