02fa7de08bab74ba67db6fdfe0a303c3f6a282e6
Gleb Oborin <gleb.oborin@jetbrains.com>
2024-09-03 16:39:32 +0200

    EDU-7216: Use `isHeadless` instead of `isUnitTest` in Gradle refresher

--- intellij-plugin/jvm-core/src/com/jetbrains/edu/jvm/gradle/IdeaGradleCourseRefresher.kt
+++ intellij-plugin/jvm-core/src/com/jetbrains/edu/jvm/gradle/IdeaGradleCourseRefresher.kt
@@ -9,7 +9,7 @@ import com.intellij.openapi.externalSystem.util.ExternalSystemUtil
 import com.intellij.openapi.project.Project
 import com.intellij.util.PlatformUtils
 import com.jetbrains.edu.learning.RefreshCause
-import com.jetbrains.edu.learning.isUnitTestMode
+import com.jetbrains.edu.learning.isHeadlessEnvironment
 import com.jetbrains.edu.learning.projectView.CourseViewPane
 import org.jetbrains.plugins.gradle.util.GradleConstants
 
@@ -26,7 +26,7 @@ class IdeaGradleCourseRefresher : GradleCourseRefresher {
     // Build toolwindow will be opened if `ExternalSystemDataKeys.NEWLY_IMPORTED_PROJECT` is true while sync
     project.putUserData(ExternalSystemDataKeys.NEWLY_IMPORTED_PROJECT, null)
     ExternalSystemUtil.refreshProject(projectBasePath, builder.build())
-    if (!isUnitTestMode) {
+    if (!isHeadlessEnvironment) {
       ExternalSystemUtil.invokeLater(project, ModalityState.nonModal()) {
         ProjectView.getInstance(project).changeViewCB(CourseViewPane.ID, null)
       }
