45f9d15e4e9d4de29445a3f4a9474ba8c8acfc34
anchouls <anna.potriasaeva@jetbrains.com>
2024-09-05 09:43:34 +0000

    Replacing the Stanford library with LanguageTool

--- intellij-plugin/educational-core/testSrc/com/jetbrains/edu/theoryLookup/LemmatizerTest.kt
+++ intellij-plugin/educational-core/testSrc/com/jetbrains/edu/theoryLookup/LemmatizerTest.kt
@@ -32,7 +32,7 @@ class LemmatizerTest(private val term: String, private val lemmatizedTerm: Strin
   @Test
   fun testLemmatization() {
     val text = "Learning Kotlin as a new developer can feel like building a castle while drinking coffee. " +
-               "But don't worry, even the easiest application you build will equip you with its features. " +
+               "But do not worry, even the easiest application you build will equip you with its features. " +
                "Beginner's course will include real-world projects. You are writing Kotlin before you know it. " +
                "In the process, you will have contacted instructors and engaged in the developer's forum. " +
                "Not only will you be writing Kotlin code, but you'll find that often, Kotlin is writing your code for you."
