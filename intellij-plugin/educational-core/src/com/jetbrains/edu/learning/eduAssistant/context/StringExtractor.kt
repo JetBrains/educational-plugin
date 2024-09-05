e0b481dfb24d51a926969e98b7b5611bf214a08d
Mark Prudnikov <mark.prudnikov@jetbrains.com>
2024-09-05 14:47:11 +0200

    EDU-7052: [not finished] Refactor `AuthorSolutionContext`, Introduce `FunctionsToStrings`

--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/eduAssistant/context/StringExtractor.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/eduAssistant/context/StringExtractor.kt
@@ -2,20 +2,19 @@ package com.jetbrains.edu.learning.eduAssistant.context
 
 import com.intellij.lang.Language
 import com.intellij.lang.LanguageExtension
-import com.intellij.openapi.application.ApplicationManager
 import com.intellij.psi.PsiFile
-import com.jetbrains.edu.learning.courseFormat.eduAssistant.FunctionSignature
+import com.intellij.util.concurrency.ThreadingAssertions.assertReadAccess
 
 interface StringExtractor {
-  // If the function does not contain any strings we store an empty list
-  fun getFunctionsToStringsMap(psiFile: PsiFile): Map<FunctionSignature, List<String>>
+
+  fun getFunctionsToStringsMap(psiFile: PsiFile): FunctionsToStrings
 
   companion object {
     private val EP_NAME = LanguageExtension<StringExtractor>("Educational.stringExtractor")
 
-    fun getFunctionsToStringsMap(psiFile: PsiFile, language: Language): Map<FunctionSignature, List<String>> {
-      ApplicationManager.getApplication().assertReadAccessAllowed()
-      return EP_NAME.forLanguage(language)?.getFunctionsToStringsMap(psiFile) ?: emptyMap()
+    fun getFunctionsToStringsMap(psiFile: PsiFile, language: Language): FunctionsToStrings {
+      assertReadAccess()
+      return EP_NAME.forLanguage(language)?.getFunctionsToStringsMap(psiFile) ?: FunctionsToStrings(emptyMap())
     }
   }
 }
