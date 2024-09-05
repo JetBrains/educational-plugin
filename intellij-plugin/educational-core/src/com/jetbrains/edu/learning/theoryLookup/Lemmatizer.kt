b9ec7423777c750926b0b70dfc70cf712f17d375
anchouls <anna.potriasaeva@jetbrains.com>
2024-09-05 09:43:34 +0000

    Replacing the Stanford library with LanguageTool

--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/theoryLookup/Lemmatizer.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/theoryLookup/Lemmatizer.kt
@@ -2,10 +2,9 @@ package com.jetbrains.edu.learning.theoryLookup
 
 import com.intellij.openapi.diagnostic.Logger
 import com.jetbrains.educational.ml.theory.lookup.term.Term
-import edu.stanford.nlp.ling.CoreAnnotations
-import edu.stanford.nlp.ling.CoreLabel
-import edu.stanford.nlp.pipeline.*
-import java.util.*
+import org.languagetool.AnalyzedTokenReadings
+import org.languagetool.JLanguageTool
+import org.languagetool.language.AmericanEnglish
 
 /**
  * Generates a map from the original terms in a provided string to their respective lemmas.
@@ -21,23 +20,23 @@ value class Lemmatizer(private val termToDefinitionAndLemmaMap: MutableMap<Strin
 
   constructor(text: String, terms: List<Term>) : this(mutableMapOf()) {
     try {
-      val pipeline = createPipeline()
-      val lemmatizedTerms = terms.map { term -> Pair(term.value.textToTokens(pipeline).map { it.lemmatizeToken() }, term.definition) }
+      val languageTool = JLanguageTool(AmericanEnglish())
+      val lemmatizedTerms = terms.map { term -> Pair(term.value.textToTokens(languageTool).map { it.lemmatizeToken() }, term.definition) }
 
-      val tokens = text.textToTokens(pipeline)
-      val originalTokens = tokens.map { it.originalText() }
-      val lemmatizedText = tokens.joinToString(SPACE) { it.lemmatizeToken() }
+      val tokens = text.textToTokens(languageTool)
+      val originalTokens = tokens.map { it.token }
+      val lemmatizedText = tokens.joinToString(SEPARATOR) { it.lemmatizeToken() }
 
       lemmatizedTerms.forEach { (term, definition) ->
         // terms may consist of several tokens
-        val joinedTerm = term.joinToString(SPACE)
+        val joinedTerm = term.joinToString(SEPARATOR)
         // find all occurrences of the term in the text
         lemmatizedText.indicesOf(joinedTerm) { index ->
           // find index of the first token in the text
-          val indexOfTerm = if (index == 0) 0 else lemmatizedText.substring(0, index).trim().split(SPACE).count()
+          val indexOfTerm = if (index == 0) 0 else lemmatizedText.substring(0, index).tokenCount(languageTool)
           if (indexOfTerm + term.size < originalTokens.size) {
             // by index of the first token and number of tokens in the term find the original version of the term in the text
-            val originalTerm = originalTokens.subList(indexOfTerm, indexOfTerm + term.size).joinToString(SPACE)
+            val originalTerm = originalTokens.subList(indexOfTerm, indexOfTerm + term.size).joinToString(SEPARATOR)
             // the original term and its lemmatized version are put into a map
             termToDefinitionAndLemmaMap[originalTerm] = Pair(definition, joinedTerm)
           }
@@ -52,14 +51,11 @@ value class Lemmatizer(private val termToDefinitionAndLemmaMap: MutableMap<Strin
 
   fun getLemmatizedTerms(): Map<String, String> = termToDefinitionAndLemmaMap.mapValues { it.value.second }
 
-  private fun CoreLabel.lemmatizeToken() =
-    get(CoreAnnotations.LemmaAnnotation::class.java).lowercase()
+  private fun AnalyzedTokenReadings.lemmatizeToken() = readings.last().lemma?.lowercase() ?: token
 
-  private fun String.textToTokens(pipeline: StanfordCoreNLP): List<CoreLabel> {
-    val document = Annotation(this)
-    pipeline.annotate(document)
-    return document.get(CoreAnnotations.TokensAnnotation::class.java)
-  }
+  private fun String.textToTokens(languageTool: JLanguageTool) =
+    languageTool.analyzeText(this).map { sentence ->
+      sentence.tokens.filterNot { it.isSentenceStart }.toList() }.flatten()
 
   private fun String.indicesOf(substring: String, onEachIndex: (Int) -> Unit) {
     var index = indexOf(substring, ignoreCase = true)
@@ -69,15 +65,10 @@ value class Lemmatizer(private val termToDefinitionAndLemmaMap: MutableMap<Strin
     }
   }
 
-  private fun createPipeline() =
-    StanfordCoreNLP(object : Properties() {
-      init {
-        setProperty("annotators", "tokenize, pos, lemma")
-      }
-    })
+  private fun String.tokenCount(languageTool: JLanguageTool) = textToTokens(languageTool).map { it.token }.count()
 
   companion object {
-    private const val SPACE = " "
+    private const val SEPARATOR = ""
     private val LOG: Logger = Logger.getInstance(Lemmatizer::class.java)
   }
 }
