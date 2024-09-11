58cab292b33885527146ddc4b556dc685b66ff25
anchouls <anna.potriasaeva@jetbrains.com>
2024-09-11 10:19:02 +0000

    Renaming Jarvis to Cognifire

--- intellij-plugin/Edu-Jarvis/src/com/jetbrains/edu/jarvis/tutorial/ContentType.kt
+++
@@ -1,6 +0,0 @@
-package com.jetbrains.edu.jarvis.tutorial
-
-enum class ContentType(val isVisible: Boolean) {
-  MAIN(true),
-  TEST(false)
-}
---
+++ intellij-plugin/Edu-Kotlin/resources/cognifire.Tutorial/DogYears/PromptBlock/Main.kt
@@ -0,0 +1,13 @@
+package jetbrains.course.tutorial.dog.years
+
+import org.jetbrains.academy.cognifire.dsl.*
+
+fun calculateDogAgeInDogYears() {
+  prompt("""
+        
+    """.trimIndent())
+}
+
+fun verifyHumanYearsInput(humanYears: Int): Boolean {
+  TODO("Not implemented yet")
+}
