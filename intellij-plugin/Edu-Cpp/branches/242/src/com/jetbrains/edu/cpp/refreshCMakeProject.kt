31caa5e2738cb78677e3b45c3027443b2d17edef
Gleb Oborin <gleb.oborin@jetbrains.com>
2024-09-12 17:00:58 +0200

    EDU-7162: Get rid of `runBlockingMaybeCancellable`

--- intellij-plugin/Edu-Cpp/branches/242/src/com/jetbrains/edu/cpp/refreshCMakeProject.kt
+++ intellij-plugin/Edu-Cpp/branches/242/src/com/jetbrains/edu/cpp/refreshCMakeProject.kt
@@ -1,17 +1,17 @@
 package com.jetbrains.edu.cpp
 
-import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.progress.runBlockingMaybeCancellable
+import com.intellij.openapi.progress.runBlockingCancellable
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VfsUtil
 import com.jetbrains.cidr.cpp.cmake.workspace.CMakeWorkspace
 import com.jetbrains.edu.learning.courseDir
+import com.jetbrains.edu.learning.runInBackground
 
 // BACKCOMPAT: 2024.1. Inline it.
 fun refreshCMakeProject(project: Project) {
-  ApplicationManager.getApplication().executeOnPooledThread {
+  runInBackground(project, "Refreshing CMake project") {
     //TODO(launch coroutine properly)
-    runBlockingMaybeCancellable {
+    runBlockingCancellable {
       // if it is a new project it will be initialized, else it will be reloaded only.
       CMakeWorkspace.getInstance(project).linkCMakeProject(VfsUtil.virtualToIoFile(project.courseDir))
     }
