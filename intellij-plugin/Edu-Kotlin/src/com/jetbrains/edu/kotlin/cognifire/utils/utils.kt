b6f02e06869820b274e9f59fcd1fcb8f8730094f
anchouls <anna.potriasaeva@jetbrains.com>
2024-09-20 14:58:40 +0200

    Added validation

--- intellij-plugin/Edu-Kotlin/src/com/jetbrains/edu/kotlin/cognifire/utils/utils.kt
+++ intellij-plugin/Edu-Kotlin/src/com/jetbrains/edu/kotlin/cognifire/utils/utils.kt
@@ -1,6 +1,9 @@
 package com.jetbrains.edu.kotlin.cognifire.utils
 
 import com.intellij.psi.PsiElement
+import com.jetbrains.edu.cognifire.models.FunctionArgument
+import com.jetbrains.edu.cognifire.models.FunctionSignature
+import org.jetbrains.kotlin.psi.KtNamedFunction
 
 fun findBlock(
   element: PsiElement,
@@ -12,4 +15,21 @@ fun findBlock(
     possibleBlock = step(possibleBlock)
   }
   return possibleBlock
+}
+
+fun getFunctionSignature(containingFunction: KtNamedFunction): FunctionSignature {
+  val containingFunctionParameters =
+    containingFunction.valueParameterList?.parameters
+      ?.map {
+        FunctionArgument(
+          it?.name ?: "",
+          it?.typeReference?.text ?: ""
+        )
+      } ?: emptyList()
+
+  return FunctionSignature(
+    containingFunction.name ?: "",
+    containingFunctionParameters,
+    containingFunction.typeReference?.text ?: "Unit"
+  )
 }
