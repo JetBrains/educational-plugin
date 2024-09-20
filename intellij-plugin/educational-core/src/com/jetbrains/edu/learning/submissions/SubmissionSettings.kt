a61c7da6025db425f308eebe57d17d8d5f094553
zbsmirnova <zinaida.smirnova@jetbrains.com>
2024-09-20 11:44:21 +0000

    EDU-7460 apply submissions of previous update versions for remdev
    (cherry picked from commit 6fdd09084b366e935f3aab42144370ff9fdf9405)

--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/submissions/SubmissionSettings.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/submissions/SubmissionSettings.kt
@@ -13,8 +13,19 @@ class SubmissionSettings {
   var stateOnClose: Boolean = false
     get() = field || System.getProperty(STATE_ON_CLOSE_PROPERTY).toBoolean()
 
+  /**
+   * Shows if apply submissions should be performed regardless of the course update version
+   * Is added specially for courses launched via Remote Development solution to keep user code in editor on course updates
+   * To be fixed properly by EDU-7466
+   */
+  @Volatile
+  var applySubmissionsForce: Boolean = false
+    get() = field || System.getProperty(APPLY_SUBMISSIONS_FORCE).toBoolean()
+
   companion object {
     private const val STATE_ON_CLOSE_PROPERTY: String = "edu.course.state.on.close"
+
+    private const val APPLY_SUBMISSIONS_FORCE: String = "edu.course.apply.submissions.force"
 
     fun getInstance(project: Project): SubmissionSettings = project.service()
   }
