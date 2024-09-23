ad5228976123dbc747346011ef93795248af2541
zbsmirnova <zinaida.smirnova@jetbrains.com>
2024-09-23 09:41:29 +0000

    EDU-7462 show feedback notification only if task solved
    (cherry picked from commit 240620798a7429b8f5d7c26ffc5639ffdc567cf2)

--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/statistics/PostFeedbackCheckListener.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/statistics/PostFeedbackCheckListener.kt
@@ -3,23 +3,24 @@ package com.jetbrains.edu.learning.statistics
 import com.intellij.openapi.project.Project
 import com.jetbrains.edu.learning.checker.CheckListener
 import com.jetbrains.edu.learning.courseFormat.CheckResult
+import com.jetbrains.edu.learning.courseFormat.CheckStatus
 import com.jetbrains.edu.learning.courseFormat.tasks.Task
 import com.jetbrains.edu.learning.projectView.ProgressUtil
 
 class PostFeedbackCheckListener : CheckListener {
   override fun afterCheck(project: Project, task: Task, result: CheckResult) {
-    if (isQuestionnaireAdvertisingNotificationShown()) return
+    if (task.status != CheckStatus.Solved || isLeaveFeedbackPrompted()) return
     val lesson = task.lesson
     val course = lesson.course
 
     val progress = ProgressUtil.countProgress(course)
     if (course.isMarketplace && progressPassed(progress)) {
-      showQuestionnaireAdvertisingNotification(project, course)
+      showStudentPostFeedbackNotification(course.name, project)
     }
   }
 
   private fun progressPassed(progress: ProgressUtil.CourseProgress): Boolean {
-    return progress.tasksSolved.toFloat() / progress.tasksTotalNum.toFloat() > SOLVED_TASKS_PERCENTAGE || progress.tasksSolved > MIN_SOLVED_TASKS_NUMBER
+    return progress.tasksSolved.toFloat() / progress.tasksTotalNum.toFloat() > SOLVED_TASKS_PERCENTAGE && progress.tasksSolved > MIN_SOLVED_TASKS_NUMBER
   }
 
   companion object {
