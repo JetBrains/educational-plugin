1c2f17d9eb7f5a2a49b62c6a6ede946b17a002e0
anchouls <anna.potriasaeva@jetbrains.com>
2024-09-25 11:02:18 +0000

    Added rule that one prompt per function

--- intellij-plugin/Edu-Cognifire/src/com/jetbrains/edu/cognifire/utils/CognifireUtils.kt
+++ intellij-plugin/Edu-Cognifire/src/com/jetbrains/edu/cognifire/utils/CognifireUtils.kt
@@ -1,6 +1,14 @@
 package com.jetbrains.edu.cognifire.utils
 
+import com.intellij.psi.PsiElement
+import com.jetbrains.edu.cognifire.CognifireDslPackageCallChecker
 import com.jetbrains.edu.learning.courseFormat.Course
 import com.jetbrains.edu.learning.courseFormat.EduFormatNames.KOTLIN
 
 fun isCognifireApplicable(course: Course) = course.languageId == KOTLIN
+
+fun PsiElement.isPromptBlock() = text.startsWith(PROMPT) &&
+                                          CognifireDslPackageCallChecker.isCallFromCognifireDslPackage(this, this.language)
+
+const val PROMPT = "prompt"
+const val CODE = "code"
