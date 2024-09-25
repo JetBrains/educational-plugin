5b181edca196ac8668beca3da99f56d85cceea71
anchouls <anna.potriasaeva@jetbrains.com>
2024-09-25 12:17:44 +0000

    Logger

--- intellij-plugin/Edu-Cognifire/src/com/jetbrains/edu/cognifire/models/CognifireExpression.kt
+++ intellij-plugin/Edu-Cognifire/src/com/jetbrains/edu/cognifire/models/CognifireExpression.kt
@@ -3,5 +3,7 @@ package com.jetbrains.edu.cognifire.models
 interface CognifireExpression {
   var dynamicOffset: Int
   val contentOffset: Int
+  val startOffset: Int
+  val endOffset: Int
   fun shiftOffset(delta: Int)
 }
