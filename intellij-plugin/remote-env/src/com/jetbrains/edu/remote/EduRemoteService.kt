156bbef961553dffd7e5bdf5d76bbacc571fcdcd
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2024-10-03 14:38:43 +0200

    Refactor `EduRemoteService` initialization
    Now we use proper coroutine API

--- intellij-plugin/remote-env/src/com/jetbrains/edu/remote/EduRemoteService.kt
+++ intellij-plugin/remote-env/src/com/jetbrains/edu/remote/EduRemoteService.kt
@@ -1,13 +1,10 @@
 package com.jetbrains.edu.remote
 
-import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.application.invokeAndWaitIfNeeded
+import com.intellij.openapi.application.EDT
 import com.intellij.openapi.client.ClientProjectSession
 import com.intellij.openapi.components.service
-import com.intellij.openapi.rd.util.launchOnUi
 import com.jetbrains.codeWithMe.model.projectViewModel
 import com.jetbrains.edu.learning.EduUtilsKt.isEduProject
-import com.jetbrains.edu.learning.course
 import com.jetbrains.edu.learning.newproject.CourseProjectGenerator.Companion.EDU_PROJECT_CREATED
 import com.jetbrains.edu.learning.projectView.CourseViewPane
 import com.jetbrains.edu.learning.submissions.SubmissionSettings
@@ -15,15 +12,23 @@ import com.jetbrains.edu.learning.taskToolWindow.ui.TaskToolWindowView
 import com.jetbrains.edu.remote.termination.EduRemoteDisconnectWatcherService
 import com.jetbrains.edu.remote.termination.EduRemoteInactivityWatcherService
 import com.jetbrains.rd.platform.util.idea.LifetimedService
+import com.jetbrains.rd.util.threading.coroutines.lifetimedCoroutineScope
 import com.jetbrains.rdserver.core.protocolModel
+import kotlinx.coroutines.*
 
 @Suppress("UnstableApiUsage")
-class EduRemoteService(private val session: ClientProjectSession): LifetimedService() {
+class EduRemoteService(private val session: ClientProjectSession, private val scope: CoroutineScope): LifetimedService() {
   init {
+    init()
+  }
+
+  private fun init() {
     val project = session.project
-    serviceLifetime.launchOnUi {
-      val model = session.protocolModel.projectViewModel
-      if (project.isEduProject()) {
+    if (!project.isEduProject()) return
+
+    scope.launch(Dispatchers.EDT) {
+      lifetimedCoroutineScope(serviceLifetime) {
+        val model = session.protocolModel.projectViewModel
         // This flag is set to true because the remote development project is not actually created from scratch when connected,
         // but rather the first course project opening for a user. Therefore, the corresponding existing code in the plugin is not invoked,
         // and we need to explicitly do it here.
@@ -31,16 +36,15 @@ class EduRemoteService(private val session: ClientProjectSession): LifetimedServ
         SubmissionSettings.getInstance(project).stateOnClose = true
         SubmissionSettings.getInstance(project).applySubmissionsForce = true
 
-        val course = project.course ?: return@launchOnUi
         // This hack is needed because at the time this service is loaded our Course view is not present in the model
-        ApplicationManager.getApplication().executeOnPooledThread {
+        withContext(Dispatchers.Default) {
           while (true) {
-            Thread.sleep(1000)
-            invokeAndWaitIfNeeded {
+            delay(1000)
+            withContext(Dispatchers.EDT) {
               model.activate.fire(CourseViewPane.ID)
             }
             if (model.currentPane.value == CourseViewPane.ID) {
-              invokeAndWaitIfNeeded {
+              withContext(Dispatchers.EDT) {
                 TaskToolWindowView.getInstance(project).updateTaskDescription()
                 service<EduRemoteDisconnectWatcherService>().start()
                 service<EduRemoteInactivityWatcherService>().start()
