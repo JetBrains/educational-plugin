3bbf34150b807762de6dc7d4301bd2596d2bd297
Aleksey Rostovskiy <aleksey.rostovskiy@jetbrains.com>
2024-10-08 01:57:59 +0200

    Remove use of the Dispatchers.EDT coroutine context in StudyItemUpdater#deleteFilesOnDisc

--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/update/StudyItemUpdater.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/update/StudyItemUpdater.kt
@@ -1,14 +1,11 @@
 package com.jetbrains.edu.learning.update
 
-import com.intellij.openapi.application.EDT
 import com.intellij.openapi.application.writeAction
 import com.intellij.openapi.project.Project
 import com.jetbrains.edu.learning.courseDir
 import com.jetbrains.edu.learning.courseFormat.StudyItem
 import com.jetbrains.edu.learning.courseFormat.ext.getDir
 import com.jetbrains.edu.learning.update.elements.StudyItemUpdate
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 import org.jetbrains.annotations.TestOnly
 
 // TODO EDU-5830 maybe synchronization is needed
@@ -27,10 +24,8 @@ abstract class StudyItemUpdater<T : StudyItem, U : StudyItemUpdate<T>>(protected
     @Suppress("UnstableApiUsage")
     suspend fun <T : StudyItem> T.deleteFilesOnDisc(project: Project) {
       val virtualFile = getDir(project.courseDir) ?: return
-      withContext(Dispatchers.EDT) {
-        writeAction {
-          virtualFile.delete(this::class.java)
-        }
+      writeAction {
+        virtualFile.delete(this::class.java)
       }
     }
   }
