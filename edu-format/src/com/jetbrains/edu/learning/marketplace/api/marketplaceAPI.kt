2f2aec3035e865e9fb4f909baba14d326a70927c
Mark Prudnikov <mark.prudnikov@jetbrains.com>
2024-10-08 16:43:06 +0200

    EDU-7516 Support New Endpoints for Courses Uploading

--- edu-format/src/com/jetbrains/edu/learning/marketplace/api/marketplaceAPI.kt
+++ edu-format/src/com/jetbrains/edu/learning/marketplace/api/marketplaceAPI.kt
@@ -1,8 +1,7 @@
 package com.jetbrains.edu.learning.marketplace.api
 
-import com.fasterxml.jackson.annotation.JsonIgnore
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties
-import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.*
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.jetbrains.edu.learning.authUtils.OAuthAccount
 import com.jetbrains.edu.learning.courseFormat.*
 import com.jetbrains.edu.learning.courseFormat.EduFormatNames.DEFAULT_ENVIRONMENT
@@ -175,6 +174,43 @@ class CourseBean {
 
   @JsonProperty(NAME)
   var name: String = ""
+}
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.DEDUCTION)
+@JsonSubTypes(
+  JsonSubTypes.Type(value = SuccessCourseUploadResponse::class),
+  JsonSubTypes.Type(value = SuccessCourseUpdateUploadResponse::class),
+  JsonSubTypes.Type(value = FailedCourseUploadResponse::class)
+)
+sealed class UploadResponse
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class SuccessCourseUploadResponse(
+  val warnings: List<String>,
+  val plugin: CourseBean
+): UploadResponse()
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class SuccessCourseUpdateUploadResponse(
+  val warnings: List<String>,
+  val update: UpdateInfo
+): UploadResponse()
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class FailedCourseUploadResponse(
+  val warnings: List<String>,
+  val errors: List<String>
+): UploadResponse() {
+  companion object {
+    @JvmStatic
+    fun parse(objectMapper: ObjectMapper, content: String): FailedCourseUploadResponse {
+      return try {
+        objectMapper.readValue(content, FailedCourseUploadResponse::class.java)
+      } catch (e: Exception) {
+        FailedCourseUploadResponse(emptyList(), listOf("Unknown error occurred: $content"))
+      }
+    }
+  }
 }
 
 abstract class MarketplaceSubmissionBase : Submission() {
--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/marketplace/MarketplaceNames.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/marketplace/MarketplaceNames.kt
@@ -10,8 +10,6 @@ const val HUB_API_PATH = "/api/rest"
 const val PLUGINS_REPOSITORY_URL = "https://plugins.jetbrains.com"
 const val PLUGINS_EDU_DEMO = "https://edu-courses.dev.marketplace.intellij.net"
 const val PLUGINS_MASTER_DEMO = "https://master.demo.marketplace.intellij.net"
-const val MARKETPLACE_PROFILE_PATH = "$PLUGINS_REPOSITORY_URL/author/me"
-const val MARKETPLACE_CREATE_VENDOR_PATH = "$PLUGINS_REPOSITORY_URL/vendor/edu/new"
 const val JET_BRAINS_ACCOUNT_PROFILE_PATH = "https://account.jetbrains.com/profile-details"
 const val LICENSE_URL = "https://creativecommons.org/licenses/by-sa/4.0/"
 const val MARKETPLACE_PLUGIN_URL = "$PLUGINS_REPOSITORY_URL/plugin"
@@ -21,11 +19,3 @@ const val REVIEWS = "/reviews"
 const val SUBMISSIONS_SERVICE_STAGING_URL = "https://educational-service-dev.labs.jb.gg/"
 const val SUBMISSIONS_SERVICE_PRODUCTION_URL = "https://educational-service.labs.jb.gg/"
 const val SUBMISSIONS_SERVICE_HOST_PROPERTY = "submission.service.host"
-
-fun getOrganizationVendorPath(vendor: String?) =
-  if (vendor != null) {
-    "https://plugins.jetbrains.com/organizations/${vendor}/edit"
-  }
-  else {
-    "https://plugins.jetbrains.com/author/me/organizations"
-  }
