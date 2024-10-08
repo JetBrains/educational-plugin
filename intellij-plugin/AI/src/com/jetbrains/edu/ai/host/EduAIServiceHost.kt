55055147f841f83681e7fd4d8715aae8c669b615
Mark Prudnikov <mark.prudnikov@jetbrains.com>
2024-10-08 19:39:55 +0200

    Fix `@Nls` Annotation in `EduAIServiceHost`

--- intellij-plugin/AI/src/com/jetbrains/edu/ai/host/EduAIServiceHost.kt
+++ intellij-plugin/AI/src/com/jetbrains/edu/ai/host/EduAIServiceHost.kt
@@ -8,7 +8,7 @@ import com.jetbrains.edu.ai.messages.EduAIBundle
 import org.jetbrains.annotations.Nls
 import java.util.function.Supplier
 
-enum class EduAIServiceHost(private val visibleName: Supplier<@Nls String>, val url: String) {
+enum class EduAIServiceHost(@Nls private val visibleName: Supplier<String>, val url: String) {
   PRODUCTION(EduAIBundle.lazyMessage("ai.service.production.server"), EDU_AI_SERVICE_PRODUCTION_URL),
   STAGING(EduAIBundle.lazyMessage("ai.service.staging.server"), EDU_AI_SERVICE_STAGING_URL),
   OTHER(EduAIBundle.lazyMessage("ai.service.other"), "http://localhost:8080");
