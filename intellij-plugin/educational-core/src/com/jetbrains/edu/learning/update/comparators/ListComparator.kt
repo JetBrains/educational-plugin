0100d39d37bef1e1a9f420ddb6c2059c58c6a55c
Aleksey Rostovskiy <aleksey.rostovskiy@jetbrains.com>
2024-10-10 22:53:45 +0200

    fixup! use o1.zip(o2)

--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/update/comparators/ListComparator.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/update/comparators/ListComparator.kt
@@ -3,8 +3,8 @@ package com.jetbrains.edu.learning.update.comparators
 class ListComparator<T>(private val elementComparator: Comparator<T>) : Comparator<List<T>> {
   override fun compare(o1: List<T>, o2: List<T>): Int {
     if (o1.size != o2.size) return o1.size.compareTo(o2.size)
-    o1.indices.forEach { i ->
-      val comparison = elementComparator.compare(o1[i], o2[i])
+    o1.zip(o2).forEach { (e1, e2) ->
+      val comparison = elementComparator.compare(e1, e2)
       if (comparison != 0) return comparison
     }
     return 0
