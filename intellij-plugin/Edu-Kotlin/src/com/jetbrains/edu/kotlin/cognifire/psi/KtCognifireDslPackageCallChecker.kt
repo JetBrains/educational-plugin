2f38e4727c5bc59051387adc021789e5d96d467f
Aleksey Rostovskiy <aleksey.rostovskiy@jetbrains.com>
2024-10-11 00:43:48 +0200

    Refactor isCallFromCognifireDslPackage method
    Refactor the isCallFromCognifireDslPackage method to utilize KotlinCacheService and make the function K2 compatible.
    This change improves error handling and includes a temporary workaround for K2 mode until EDU-7553 is fixed.

--- intellij-plugin/Edu-Kotlin/src/com/jetbrains/edu/kotlin/cognifire/psi/KtCognifireDslPackageCallChecker.kt
+++ intellij-plugin/Edu-Kotlin/src/com/jetbrains/edu/kotlin/cognifire/psi/KtCognifireDslPackageCallChecker.kt
@@ -1,20 +1,31 @@
 package com.jetbrains.edu.kotlin.cognifire.psi
 
+import com.intellij.openapi.components.serviceOrNull
 import com.intellij.psi.PsiElement
 import com.jetbrains.edu.cognifire.CognifireDslPackageCallChecker
-import org.jetbrains.kotlin.idea.caches.resolve.analyze
+import org.jetbrains.kotlin.caches.resolve.KotlinCacheService
+import org.jetbrains.kotlin.idea.base.plugin.KotlinPluginModeProvider
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.psi.KtCallExpression
 import org.jetbrains.kotlin.resolve.calls.util.getResolvedCall
 import org.jetbrains.kotlin.resolve.descriptorUtil.fqNameSafe
 
 class KtCognifireDslPackageCallChecker : CognifireDslPackageCallChecker {
-
-  override fun isCallFromCognifireDslPackage(element: PsiElement) =
-    element is KtCallExpression &&
-    element.calleeExpression.getResolvedCall(element.analyze())?.resultingDescriptor?.containingDeclaration?.fqNameSafe == FqName(
-      COGNIFIRE_DSL_PACKAGE
-    )
+  override fun isCallFromCognifireDslPackage(element: PsiElement): Boolean {
+    val project = element.project
+    val ktElement = element as? KtCallExpression ?: return false
+    val service = project.serviceOrNull<KotlinCacheService>()
+    if (service == null) {
+      if (KotlinPluginModeProvider.isK2Mode()) {
+        // TODO workaround to support K2, remove after EDU-7553 is fixed
+        return true
+      }
+      error("KotlinCacheService is not found")
+    }
+    val bindingContext = service.getResolutionFacade(ktElement).analyze(ktElement)
+    val resolvedCall = ktElement.calleeExpression.getResolvedCall(bindingContext) ?: return false
+    return resolvedCall.resultingDescriptor.containingDeclaration.fqNameSafe == FqName(COGNIFIRE_DSL_PACKAGE)
+  }
 
   companion object {
     private const val COGNIFIRE_DSL_PACKAGE = "org.jetbrains.academy.cognifire.dsl"
