57e96d07833acf84e62534afc3ffcb7f6ea01fb9
anchouls <anna.potriasaeva@jetbrains.com>
2024-10-10 11:38:59 +0200

    Added updating prompt to code mapping

--- intellij-plugin/Edu-Cognifire/src/com/jetbrains/edu/cognifire/inspection/InspectionProcessor.kt
+++ intellij-plugin/Edu-Cognifire/src/com/jetbrains/edu/cognifire/inspection/InspectionProcessor.kt
@@ -7,22 +7,27 @@ import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiFileFactory
 import com.intellij.util.concurrency.ThreadingAssertions
+import com.jetbrains.educational.ml.cognifire.responses.PromptToCodeResponse
 
 interface InspectionProcessor {
-  fun applyInspections(project: Project, psiFile: PsiFile)
+  fun applyInspections(promptToCodeTranslation: PromptToCodeResponse, project: Project, psiFile: PsiFile): PromptToCodeResponse
 
   companion object {
     private val EP_NAME = LanguageExtension<InspectionProcessor>("Educational.inspectionProcessor")
 
-    private fun applyInspections(project: Project, psiFile: PsiFile, language: Language) {
-      ThreadingAssertions.assertReadAccess()
-      EP_NAME.forLanguage(language)?.applyInspections(project, psiFile)
+    private fun applyInspections(promptToCodeTranslation: PromptToCodeResponse, project: Project, psiFile: PsiFile, language: Language): PromptToCodeResponse? {
+      return EP_NAME.forLanguage(language)?.applyInspections(promptToCodeTranslation, project, psiFile)
     }
 
-    fun applyInspections(code: String, project: Project, language: Language): String {
+    fun applyInspections(promptToCodeTranslation: PromptToCodeResponse, functionSignature: String, project: Project, language: Language): PromptToCodeResponse? {
+      var code = promptToCodeTranslation.distinctBy { it.codeLineNumber }.joinToString(System.lineSeparator()) { it.generatedCodeLine }
+      code = """
+        $functionSignature {
+            $code
+        }
+      """.trimIndent()
       val psiFile = runReadAction { PsiFileFactory.getInstance(project).createFileFromText("Main.kt", language, code) }
-      runReadAction { applyInspections(project, psiFile, language) }
-      return runReadAction<String> { psiFile.text }
+      return applyInspections(promptToCodeTranslation, project, psiFile, language)
     }
   }
 }
---
+++ intellij-plugin/Edu-Kotlin/src/com/jetbrains/edu/kotlin/cognifire/inspection/processing/IntroduceWhenSubjectInspectionProcessing.kt
@@ -0,0 +1,34 @@
+package com.jetbrains.edu.kotlin.cognifire.inspection.processing
+
+import com.intellij.openapi.command.WriteCommandAction
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
+import com.jetbrains.educational.ml.cognifire.responses.PromptToCodeResponse
+import org.jetbrains.kotlin.idea.inspections.branchedTransformations.IntroduceWhenSubjectInspection
+import org.jetbrains.kotlin.psi.KtWhenExpression
+import org.jetbrains.kotlin.scripting.definitions.runReadAction
+
+class IntroduceWhenSubjectInspectionProcessing(private val project: Project, private val element: PsiElement) :
+  InspectionProcessing {
+  private val inspection = IntroduceWhenSubjectInspection()
+
+  override fun isApplicable(): Boolean = runReadAction {
+    if (!element.isValid) return@runReadAction false
+    if (element !is KtWhenExpression) return@runReadAction false
+    inspection.isApplicable(element)
+  }
+
+  override fun apply() {
+    if (element !is KtWhenExpression) return
+    WriteCommandAction.runWriteCommandAction(project, null, null, {
+      inspection.applyTo(element, project)
+    })
+  }
+
+  override fun applyInspection(promptToCode: PromptToCodeResponse, psiFile: PsiFile): PromptToCodeResponse {
+    if (!isApplicable()) return promptToCode
+    apply()
+    return getNewPromptToCodeWithoutChangingLines(promptToCode, psiFile)
+  }
+}
