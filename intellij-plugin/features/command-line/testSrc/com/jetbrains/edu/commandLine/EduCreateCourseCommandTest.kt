5b762711463c72c099641fe82871398afd50d430
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2024-10-11 17:29:56 +0200

    EDU-7517: tests parsing of command arguments

---
+++ intellij-plugin/features/command-line/testSrc/com/jetbrains/edu/commandLine/EduCreateCourseCommandTest.kt
@@ -0,0 +1,40 @@
+package com.jetbrains.edu.commandLine
+
+import org.junit.runners.Parameterized
+
+class EduCreateCourseCommandTest(
+  commandData: EduCommandTestData<EduCreateCourseCommand>,
+) : EduCommandTestBase<EduCreateCourseCommand>(commandData) {
+
+  override fun command(): EduCreateCourseCommand = EduCreateCourseCommand()
+
+  companion object {
+    @Parameterized.Parameters(name = "{0}")
+    @JvmStatic
+    fun data(): List<EduCommandTestData<*>> = EduCommandTestData.courseProjectCommandBaseData("createCourse") + listOf(
+      EduCommandTestData(
+        emptyList(),
+        emptyMap(),
+        """
+          Usage: createCourse [<options>] <path to course project directory>
+          
+          Options:
+            --archive=<value>      Path to course archive file
+            --marketplace=<value>  Marketplace course link. Supported formats:
+              - %course-id%
+              - %course-id%-%plugin-name%
+              - https://plugins.jetbrains.com/plugin/%course-id%
+              - https://plugins.jetbrains.com/plugin/%course-id%-%plugin-name%.
+              
+              So, for https://plugins.jetbrains.com/plugin/16630-introduction-to-python course, you can pass:
+              - 16630
+              - 16630-introduction-to-python
+              - https://plugins.jetbrains.com/plugin/16630
+              - https://plugins.jetbrains.com/plugin/16630-introduction-to-python
+            --hyperskill=<value>   Hyperskill project id
+            -h, --help             Show this message and exit
+        """
+      )
+    )
+  }
+}
