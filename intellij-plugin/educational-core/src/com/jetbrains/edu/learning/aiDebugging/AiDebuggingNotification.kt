2ca0da24a25a8f7111f2fc86f4885a7d8eebc71b
Polina.Musina <polina.musina@gmail.com>
2024-10-16 17:37:31 +0200

    fixes

--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/aiDebugging/AiDebuggingNotification.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/aiDebugging/AiDebuggingNotification.kt
@@ -4,50 +4,47 @@ import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.jetbrains.edu.learning.courseFormat.CheckStatus
 import com.jetbrains.edu.learning.courseFormat.CourseMode
-import com.jetbrains.edu.learning.courseFormat.tasks.EduTask
 import com.jetbrains.edu.learning.courseFormat.tasks.Task
 import com.jetbrains.edu.learning.messages.EduCoreBundle
 import java.awt.BorderLayout
 import javax.swing.JComponent
 import javax.swing.JPanel
 
-class AiDebuggingNotification(val actionTargetParent: JPanel? = null) {
-
-  private var aiDebuggingNotificationPanel: JComponent? = null
-
-  fun addAiDebuggingNotification(task: Task) {
-    if (isAvailable(task)) {
-      val textToShow = EduCoreBundle.message("action.Educational.AiDebuggingNotification.text")
-      val action = showDebugNotification()
-      val close: () -> Unit = {
-        aiDebuggingNotificationPanel?.let {
-          actionTargetParent?.remove(aiDebuggingNotificationPanel)
-          actionTargetParent?.revalidate()
-          actionTargetParent?.repaint()
-        }
-      }
-
-      val nextStepHintNotification =
-        AiDebuggingNotificationFrame(textToShow, action, actionTargetParent, close)
-
-      aiDebuggingNotificationPanel = nextStepHintNotification.rootPane
-      aiDebuggingNotificationPanel?.let { actionTargetParent?.add(it, BorderLayout.NORTH) }
-    }
-  }
-
-  @Suppress("DialogTitleCapitalization")
-  private fun showDebugNotification() =
-    object : AnAction(EduCoreBundle.message("action.Educational.AiDebuggingNotification.start.debugging.session")) {
-      override fun actionPerformed(p0: AnActionEvent) {
-      }
-    }
+class AiDebuggingNotification {
 
   companion object {
 
-    // Only for the Kotlin Onboarding Introduction: https://plugins.jetbrains.com/plugin/21067-kotlin-onboarding-introduction and for Edu tasks
-    fun isNextStepHintApplicable(task: Task) = task.course.id == 21067 && task is EduTask
+    private var aiDebuggingNotificationPanel: JComponent? = null
 
-    fun isAvailable(task: Task) =
-      isNextStepHintApplicable(task) && task.course.courseMode == CourseMode.STUDENT && task.status == CheckStatus.Failed // TODO: when should we show this button?
+    fun addAiDebuggingNotification(task: Task, actionTargetParent: JPanel?) {
+      if (isAvailable(task)) {
+        val textToShow = EduCoreBundle.message("action.Educational.AiDebuggingNotification.text")
+        val action = showDebugNotification()
+        val close: () -> Unit = {
+          aiDebuggingNotificationPanel?.let {
+            actionTargetParent?.remove(aiDebuggingNotificationPanel)
+            actionTargetParent?.revalidate()
+            actionTargetParent?.repaint()
+          }
+        }
+
+        val nextStepHintNotification =
+          AiDebuggingNotificationFrame(textToShow, action, actionTargetParent, close)
+
+        aiDebuggingNotificationPanel = nextStepHintNotification.rootPane
+        aiDebuggingNotificationPanel?.let { actionTargetParent?.add(it, BorderLayout.NORTH) }
+      }
+    }
+
+    @Suppress("DialogTitleCapitalization")
+    private fun showDebugNotification() =
+      object : AnAction(EduCoreBundle.message("action.Educational.AiDebuggingNotification.start.debugging.session")) {
+        override fun actionPerformed(p0: AnActionEvent) {
+          //TODO: debugging session
+        }
+      }
+
+    private fun isAvailable(task: Task) =
+      task.course.courseMode == CourseMode.STUDENT && task.status == CheckStatus.Failed // TODO: when should we show this button?
   }
 }
