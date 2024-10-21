6612bbbba31873d4c81e26c60ccada6e681a734c
anchouls <anna.potriasaeva@jetbrains.com>
2024-10-21 10:55:20 +0000

    Delete TODOs related to Unit return value

--- intellij-plugin/Edu-Cognifire/src/com/jetbrains/edu/cognifire/GeneratedCodeParser.kt
+++ intellij-plugin/Edu-Cognifire/src/com/jetbrains/edu/cognifire/GeneratedCodeParser.kt
@@ -4,19 +4,20 @@ import com.intellij.lang.Language
 import com.intellij.lang.LanguageExtension
 import com.intellij.openapi.project.Project
 import com.intellij.util.concurrency.ThreadingAssertions
+import com.jetbrains.edu.cognifire.models.FunctionSignature
 
 /**
  * Parses the generated code and returns a list of errors (error description inside TODO blocks)
  */
 interface GeneratedCodeParser {
-  fun hasErrors(project: Project, generatedCode: String): Boolean
+  fun hasErrors(project: Project, generatedCode: String, functionSignature: FunctionSignature): Boolean
 
   companion object {
     private val EP_NAME = LanguageExtension<GeneratedCodeParser>("Educational.generatedCodeParser")
 
-    fun hasErrors(project: Project, generatedCode: String, language: Language): Boolean {
+    fun hasErrors(project: Project, generatedCode: String, functionSignature: FunctionSignature, language: Language): Boolean {
       ThreadingAssertions.assertReadAccess()
-      return EP_NAME.forLanguage(language).hasErrors(project, generatedCode)
+      return EP_NAME.forLanguage(language).hasErrors(project, generatedCode, functionSignature)
     }
   }
 }
