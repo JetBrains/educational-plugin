c87484961b318366160bb273fe032fb06c2ec22a
anchouls <anna.potriasaeva@jetbrains.com>
2024-10-21 10:55:20 +0000

    Delete TODOs related to Unit return value

--- intellij-plugin/Edu-Kotlin/src/com/jetbrains/edu/kotlin/cognifire/utils/utils.kt
+++ intellij-plugin/Edu-Kotlin/src/com/jetbrains/edu/kotlin/cognifire/utils/utils.kt
@@ -1,6 +1,15 @@
 package com.jetbrains.edu.kotlin.cognifire.utils
 
+import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiFileFactory
+import org.jetbrains.kotlin.idea.KotlinLanguage
+import org.jetbrains.kotlin.psi.KtCallExpression
+
+const val UNIT_RETURN_VALUE = "Unit"
+const val TODO_MARKER = "TODO"
+const val EMPTY_TODO = ""
 
 fun findBlock(
   element: PsiElement,
@@ -12,4 +21,19 @@ fun findBlock(
     possibleBlock = step(possibleBlock)
   }
   return possibleBlock
+}
+
+fun createPsiFile(project: Project, functionSignature: String, code: String): PsiFile =
+  PsiFileFactory.getInstance(project).createFileFromText("fileName.kt", KotlinLanguage.INSTANCE,
+    """
+      $functionSignature {
+            $code
+        }
+    """.trimIndent()
+  )
+
+fun PsiElement.getTodoMessageOrNull() = if (this is KtCallExpression && calleeExpression?.text == TODO_MARKER) {
+  valueArguments.firstOrNull()?.getArgumentExpression()?.text ?: EMPTY_TODO
+} else {
+  null
 }
