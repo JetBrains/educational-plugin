0b11da5b9ea613ac0b33e4fc6e9fe45fff1a0707
Aleksey Rostovskiy <aleksey.rostovskiy@jetbrains.com>
2024-10-23 11:05:52 +0200

    EDU-7017 Refactor update task description call in the TranslationLoader
    It removes the direct calls to refresh the task description and replaces them with a subscription to a flow, where changes trigger the update.
    This change provides a more reactive way to watch for changes in the translation language code and allows the task description to be updated accordingly.

--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/ai/TranslationProjectSettings.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/ai/TranslationProjectSettings.kt
@@ -3,14 +3,21 @@ package com.jetbrains.edu.learning.ai
 import com.intellij.openapi.components.*
 import com.intellij.openapi.project.Project
 import com.jetbrains.edu.learning.ai.TranslationProjectSettings.TranslationProjectState
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.drop
 
 @Service(Service.Level.PROJECT)
 @State(name="TranslationProjectSettings", reloadable = true, storages = [Storage("edu_translation.xml")])
 class TranslationProjectSettings : SimplePersistentStateComponent<TranslationProjectState>(TranslationProjectState()) {
+  private val translationLanguageCodeChangeFlow = MutableStateFlow(currentTranslationLanguageCode)
+
   var currentTranslationLanguageCode: String?
     get() = state.currentTranslationLanguageCode
     set(value) {
+      if (value == currentTranslationLanguageCode) return
       state.currentTranslationLanguageCode = value
+      translationLanguageCodeChangeFlow.value = value
     }
 
   class TranslationProjectState : BaseState() {
@@ -25,5 +32,9 @@ class TranslationProjectSettings : SimplePersistentStateComponent<TranslationPro
     fun resetTranslation(project: Project) {
       getInstance(project).currentTranslationLanguageCode = null
     }
+
+    fun getTranslationLanguageCodeChangeFlow(project: Project): Flow<String?> =
+      // drop the very first emission due to the loading of TranslationProjectState object
+      getInstance(project).translationLanguageCodeChangeFlow.drop(1)
   }
 }
