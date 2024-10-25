b3e2c243ad8bcf8b7ef15b67cbabdfd6644f4af4
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2024-10-25 17:01:24 +0200

    EDU-7635: introduce `TestResultCollector` API
    The main goal is to decouple `EduTaskCheckerBase` from concrete test events because not all run configurations uses `SMTRunnerEventsListener.TEST_STATUS` topic to emit test events

---
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/checker/tests/TestResultCollector.kt
@@ -0,0 +1,66 @@
+package com.jetbrains.edu.learning.checker.tests
+
+import com.intellij.openapi.util.Disposer
+import com.intellij.util.messages.MessageBusConnection
+import com.jetbrains.edu.learning.checker.tests.TestResultCollector.CollectorState.*
+import java.util.concurrent.atomic.AtomicReference
+
+/**
+ * Collects test events (probably from different sources) and transforms them into unified data
+ *
+ * An instance of the class can be used only once because it holds the state of launched tests.
+ */
+abstract class TestResultCollector {
+
+  private val state: AtomicReference<CollectorState> = AtomicReference(NOT_STARTED)
+
+  /**
+   * Subscribes and collects events about finished tests.
+   * It's supposed the method can be invoked only once
+   */
+  fun startCollecting(connection: MessageBusConnection) {
+    changeState(NOT_STARTED, IN_PROGRESS)
+    Disposer.register(connection) {
+      changeState(IN_PROGRESS, FINISHED)
+    }
+
+    doStartCollecting(connection)
+  }
+
+  /**
+   * Transforms data collected by [startCollecting] into a list of [TestResultGroup].
+   *
+   * It's supposed the method can be invoked only after [startCollecting] and
+   * only after receiving all tests events
+   */
+  fun collectTestResults(): List<TestResultGroup> {
+    val currentState = state.get()
+    if (currentState != FINISHED) {
+      error("Unexpected collector state: expected: $FINISHED, actual: $currentState")
+    }
+
+    return doCollectTestResults()
+  }
+
+  /**
+   * Subscribes on necessary topics using given [connection] to receive test events.
+   */
+  protected abstract fun doStartCollecting(connection: MessageBusConnection)
+
+  /**
+   * Transforms data collected in [doStartCollecting] into a list of [TestResultGroup]
+   */
+  protected abstract fun doCollectTestResults(): List<TestResultGroup>
+
+  private fun changeState(expectedState: CollectorState, newState: CollectorState) {
+    if (!state.compareAndSet(expectedState, newState)) {
+      error("Unexpected collector state: expected: $expectedState, actual: ${state.get()}")
+    }
+  }
+
+  private enum class CollectorState {
+    NOT_STARTED,
+    IN_PROGRESS,
+    FINISHED
+  }
+}
---
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/checker/tests/TestResultGroup.kt
@@ -0,0 +1,8 @@
+package com.jetbrains.edu.learning.checker.tests
+
+import com.jetbrains.edu.learning.courseFormat.EduTestInfo
+
+/**
+ * Group of test result related to single run configuration
+ */
+data class TestResultGroup(val results: List<EduTestInfo>)
