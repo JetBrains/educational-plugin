0503ac18a52fae29e9062df45aabf4810518682e
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2024-10-29 14:55:19 +0100

    EDU-6988: migrate Android task checking to configuration-based approach
    Previously, we invoked gradle commands manually to run unit tests and instrumentation tests (including launching emulator).
    But doesn't allow us to reuse all implementation features provided by Android plugin itself (for example, the run configuration for instrumentation tests can launch an emulator itself)
    
    So, now it uses the same approach as we use for all other technologies

--- intellij-plugin/Edu-Android/src/com/jetbrains/edu/android/checker/AndroidTaskCheckerProvider.kt
+++ intellij-plugin/Edu-Android/src/com/jetbrains/edu/android/checker/AndroidTaskCheckerProvider.kt
@@ -1,10 +1,10 @@
 package com.jetbrains.edu.android.checker
 
 import com.intellij.openapi.project.Project
-import com.jetbrains.edu.jvm.gradle.checker.GradleEduTaskChecker
 import com.jetbrains.edu.jvm.gradle.checker.GradleTaskCheckerProvider
+import com.jetbrains.edu.learning.checker.TaskChecker
 import com.jetbrains.edu.learning.courseFormat.tasks.EduTask
 
 class AndroidTaskCheckerProvider : GradleTaskCheckerProvider() {
-  override fun getEduTaskChecker(task: EduTask, project: Project): GradleEduTaskChecker = AndroidChecker(task, envChecker, project)
+  override fun getEduTaskChecker(task: EduTask, project: Project): TaskChecker<EduTask> = AndroidEduTaskChecker(task, envChecker, project)
 }
