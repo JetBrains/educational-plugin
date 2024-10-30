79cb22e741ef6f71d6b49418b59d99fb28a6d044
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2024-10-30 01:41:12 +0100

    Don't do redundant redelcration of `codeExecutor` in `KtTaskCheckerProvider`
    `GradleCodeExecutor` is default implementation anyway

--- intellij-plugin/Edu-Kotlin/src/com/jetbrains/edu/kotlin/checker/KtTaskCheckerProvider.kt
+++ intellij-plugin/Edu-Kotlin/src/com/jetbrains/edu/kotlin/checker/KtTaskCheckerProvider.kt
@@ -1,16 +1,11 @@
 package com.jetbrains.edu.kotlin.checker
 
 import com.intellij.openapi.project.Project
-import com.jetbrains.edu.jvm.gradle.checker.GradleCodeExecutor
 import com.jetbrains.edu.jvm.gradle.checker.GradleTaskCheckerProvider
-import com.jetbrains.edu.learning.checker.CodeExecutor
 import com.jetbrains.edu.learning.checker.TaskChecker
 import com.jetbrains.edu.learning.courseFormat.tasks.EduTask
 
 class KtTaskCheckerProvider : GradleTaskCheckerProvider() {
-  override val codeExecutor: CodeExecutor
-    get() = GradleCodeExecutor()
-
   override fun getEduTaskChecker(task: EduTask, project: Project): TaskChecker<EduTask> {
     return KtNewGradleTaskChecker(task, envChecker, project)
   }
