34749f62d78a5dbc195b2e25f00cbaea6981625f
Gleb Oborin <gleb.oborin@jetbrains.com>
2024-10-30 17:05:38 +0100

    EDU-7646: Handle environmental error when sdk is not 21
    (cherry picked from commit 4c9a104d29ec2c6ab12b2f9c17c6c95593c32069)

--- intellij-plugin/jvm-core/src/com/jetbrains/edu/jvm/gradle/checker/GradleEnvironmentChecker.kt
+++ intellij-plugin/jvm-core/src/com/jetbrains/edu/jvm/gradle/checker/GradleEnvironmentChecker.kt
@@ -3,9 +3,12 @@ package com.jetbrains.edu.jvm.gradle.checker
 import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil
 import com.intellij.openapi.module.ModuleUtil
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.projectRoots.JavaSdkVersionUtil
 import com.intellij.openapi.roots.ProjectRootManager
+import com.intellij.openapi.roots.ui.configuration.ProjectSettingsService
 import com.jetbrains.edu.jvm.gradle.GradleCourseRefresher
 import com.jetbrains.edu.jvm.gradle.generation.EduGradleUtils
+import com.jetbrains.edu.jvm.hyperskillJdkVersion
 import com.jetbrains.edu.jvm.messages.EduJVMBundle
 import com.jetbrains.edu.learning.EduNames.ENVIRONMENT_CONFIGURATION_LINK_GRADLE
 import com.jetbrains.edu.learning.RefreshCause
@@ -15,13 +18,19 @@ import com.jetbrains.edu.learning.courseFormat.CheckResult
 import com.jetbrains.edu.learning.courseFormat.CheckStatus
 import com.jetbrains.edu.learning.courseFormat.EduFormatNames
 import com.jetbrains.edu.learning.courseFormat.ext.getDir
+import com.jetbrains.edu.learning.courseFormat.hyperskill.HyperskillCourse
 import com.jetbrains.edu.learning.courseFormat.tasks.Task
 import com.jetbrains.edu.learning.messages.EduCoreBundle
 import org.jetbrains.plugins.gradle.settings.GradleSettings
 
 class GradleEnvironmentChecker : EnvironmentChecker() {
   override fun getEnvironmentError(project: Project, task: Task): CheckResult? {
-    ProjectRootManager.getInstance(project).projectSdk ?: return noSdkConfiguredResult
+    val sdk = ProjectRootManager.getInstance(project).projectSdk ?: return noSdkConfiguredResult
+
+    if (task.course is HyperskillCourse && JavaSdkVersionUtil.getJavaSdkVersion(sdk) != hyperskillJdkVersion) {
+      return getIncorrectHyperskillJDKResult(project)
+    }
+
     val taskDir = task.getDir(project.courseDir) ?: return getFailedToLaunchCheckingResult(project)
     val module = ModuleUtil.findModuleForFile(taskDir, project) ?: return getFailedToLaunchCheckingResult(project)
 
@@ -53,6 +62,20 @@ class GradleEnvironmentChecker : EnvironmentChecker() {
       return CheckResult(CheckStatus.Unchecked,
                          EduJVMBundle.message("error.gradle.not.imported", RELOAD_GRADLE_LINK, EduFormatNames.NO_TESTS_URL),
                          hyperlinkAction = { reloadGradle(project) })
+    }
+
+    private const val OPEN_MODULE_SETTINGS = "open_module_settings"
+
+    private fun getIncorrectHyperskillJDKResult(project: Project): CheckResult {
+      return CheckResult(
+        CheckStatus.Unchecked,
+        EduJVMBundle.message("error.hyperskill.incorrect.jdk", hyperskillJdkVersion.description, OPEN_MODULE_SETTINGS),
+        hyperlinkAction = { openModuleSettings(project) }
+      )
+    }
+
+    private fun openModuleSettings(project: Project) {
+      ProjectSettingsService.getInstance(project).openProjectSettings()
     }
   }
 }
