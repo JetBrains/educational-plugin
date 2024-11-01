ae96dcbbaabf63948088da0193986d9cc05bfa45
Aleksey Rostovskiy <aleksey.rostovskiy@jetbrains.com>
2024-11-02 00:59:22 +0100

    EDU-7592 Implement "Update Course Translation" action

---
+++ intellij-plugin/AI/src/com/jetbrains/edu/ai/translation/action/UpdateCourseTranslation.kt
@@ -0,0 +1,39 @@
+package com.jetbrains.edu.ai.translation.action
+
+import com.intellij.openapi.actionSystem.ActionUpdateThread
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.project.DumbAwareAction
+import com.jetbrains.edu.ai.translation.TranslationLoader
+import com.jetbrains.edu.learning.ai.translationSettings
+import com.jetbrains.edu.learning.course
+import com.jetbrains.edu.learning.courseFormat.EduCourse
+import org.jetbrains.annotations.NonNls
+
+@Suppress("ComponentNotRegistered")
+class UpdateCourseTranslation : DumbAwareAction() {
+  override fun actionPerformed(e: AnActionEvent) {
+    val project = e.project ?: return
+    if (project.isDisposed) return
+
+    val course = project.course as? EduCourse ?: return
+    if (!course.isMarketplaceRemote) return
+
+    val translationProperties = project.translationSettings().translationProperties.value ?: return
+    TranslationLoader.getInstance(project).updateTranslation(course, translationProperties)
+  }
+
+  override fun update(e: AnActionEvent) {
+    e.presentation.isEnabledAndVisible = false
+    val project = e.project ?: return
+    val course = project.course as? EduCourse ?: return
+    e.presentation.isEnabledAndVisible = course.isMarketplaceRemote && !TranslationLoader.isRunning(project)
+  }
+
+  override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.BGT
+
+  companion object {
+    @Suppress("unused")
+    @NonNls
+    private const val ACTION_ID: String = "Educational.UpdateCourseTranslation"
+  }
+}
