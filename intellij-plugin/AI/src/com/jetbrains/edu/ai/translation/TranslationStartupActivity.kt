53851646b1f1581ff3229b7a339acfcaf90187c8
Aleksey Rostovskiy <aleksey.rostovskiy@jetbrains.com>
2024-11-02 14:47:44 +0100

    EDU-7592 Implement TranslationStartupActivity

---
+++ intellij-plugin/AI/src/com/jetbrains/edu/ai/translation/TranslationStartupActivity.kt
@@ -0,0 +1,19 @@
+package com.jetbrains.edu.ai.translation
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.startup.ProjectActivity
+import com.jetbrains.edu.ai.translation.updater.TranslationUpdateChecker.Companion.launchTranslationUpdateChecker
+import com.jetbrains.edu.learning.EduUtilsKt.isStudentProject
+import com.jetbrains.edu.learning.course
+import com.jetbrains.edu.learning.courseFormat.EduCourse
+
+class TranslationStartupActivity : ProjectActivity {
+  override suspend fun execute(project: Project) {
+    if (!project.isStudentProject()) return
+
+    val course = project.course as? EduCourse ?: return
+    if (!course.isMarketplaceRemote) return
+
+    project.launchTranslationUpdateChecker(course)
+  }
+}
