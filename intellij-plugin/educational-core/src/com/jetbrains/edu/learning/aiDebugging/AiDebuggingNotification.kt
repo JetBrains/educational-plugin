316b965342f419b5676856481b38818813cd2624
Daniil.Karol <daniil.karol@jetbrains.com>
2024-11-08 01:10:18 +0100

    Fixed PRs comments.

--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/aiDebugging/AiDebuggingNotification.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/aiDebugging/AiDebuggingNotification.kt
@@ -4,11 +4,12 @@ import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.components.service
 import com.intellij.openapi.vfs.readText
-import com.jetbrains.edu.learning.aiDebugging.session.LaunchDebugSessionService
+import com.jetbrains.edu.learning.aiDebugging.session.AIDebugSessionService
 import com.jetbrains.edu.learning.courseFormat.CheckResult
 import com.jetbrains.edu.learning.courseFormat.CheckStatus
 import com.jetbrains.edu.learning.courseFormat.CourseMode
 import com.jetbrains.edu.learning.courseFormat.ext.getDescriptionFile
+import com.jetbrains.edu.learning.courseFormat.ext.getVirtualFile
 import com.jetbrains.edu.learning.courseFormat.ext.project
 import com.jetbrains.edu.learning.courseFormat.tasks.Task
 import com.jetbrains.edu.learning.messages.EduCoreBundle
@@ -22,39 +23,39 @@ object AiDebuggingNotification {
   private var aiDebuggingNotificationPanel: JComponent? = null
 
   fun addAiDebuggingNotification(task: Task, actionTargetParent: JPanel?, checkResult: CheckResult) {
-    if (isAvailable(task)) {
-      val textToShow = EduCoreBundle.message("action.Educational.AiDebuggingNotification.text")
-      val testDescription = checkResult.details ?: checkResult.message
-      println(testDescription)
-      val action = showDebugNotification(task, testDescription)
-      val close: () -> Unit = {
-        aiDebuggingNotificationPanel?.let {
-          actionTargetParent?.remove(aiDebuggingNotificationPanel)
-          actionTargetParent?.revalidate()
-          actionTargetParent?.repaint()
-        }
+    if (!isAvailable(task)) return
+    val textToShow = EduCoreBundle.message("action.Educational.AiDebuggingNotification.text")
+    val testDescription = checkResult.details ?: checkResult.message
+    val action = showDebugNotification(task, testDescription)
+    val closeCallback: () -> Unit = {
+      aiDebuggingNotificationPanel?.let {
+        actionTargetParent?.remove(aiDebuggingNotificationPanel)
+        actionTargetParent?.revalidate()
+        actionTargetParent?.repaint()
       }
-
-      val aiDebuggingNotification =
-        AiDebuggingNotificationFrame(textToShow, action, actionTargetParent, close)
-
-      aiDebuggingNotificationPanel = aiDebuggingNotification.rootPane
-      aiDebuggingNotificationPanel?.let { actionTargetParent?.add(it, BorderLayout.NORTH) }
     }
+
+    val aiDebuggingNotification =
+      AiDebuggingNotificationFrame(textToShow, action, actionTargetParent, closeCallback)
+
+    aiDebuggingNotificationPanel = aiDebuggingNotification.rootPane
+    aiDebuggingNotificationPanel?.let { actionTargetParent?.add(it, BorderLayout.NORTH) }
   }
 
+  // TODO replace testDescription to some data class with test information
   @Suppress("DialogTitleCapitalization")
   private fun showDebugNotification(task: Task, testDescription: String) =
     object : AnAction(EduCoreBundle.message("action.Educational.AiDebuggingNotification.start.debugging.session")) {
 
       override fun actionPerformed(p0: AnActionEvent) {
-        println("qwer")
-        println(testDescription)
         val project = task.project ?: error("Project is missing")
-        val selectedTaskFile = project.selectedTaskFile ?:error("There are no task file with code to use")
+        val virtualFiles = listOf(
+          project.selectedTaskFile?.getVirtualFile(project)
+          ?: error("There are no virtual file for the selected task`")
+        ) // TODO take all task files
         val description = task.getDescriptionFile(project)?.readText() ?: error("There are no description for the task")
-        val service = project.service<LaunchDebugSessionService>()
-        service.startSession(description, selectedTaskFile, testDescription)
+        val service = project.service<AIDebugSessionService>()
+        service.runDebuggingSession(description, virtualFiles, testDescription)
       }
     }
 
