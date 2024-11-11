0807315d0dbe49c5b511d03931465142a0187d92
Stanislav.Sandler <stanislav.sandler@jetbrains.com>
2024-11-11 17:17:06 +0100

    Update to support new ML library interface.

--- intellij-plugin/Edu-Kotlin/src/com/jetbrains/edu/kotlin/cognifire/inspection/processing/InspectionProcessing.kt
+++ intellij-plugin/Edu-Kotlin/src/com/jetbrains/edu/kotlin/cognifire/inspection/processing/InspectionProcessing.kt
@@ -1,10 +1,10 @@
 package com.jetbrains.edu.kotlin.cognifire.inspection.processing
 
 import com.intellij.psi.PsiFile
-import com.jetbrains.educational.ml.cognifire.responses.PromptToCodeResponse
+import com.jetbrains.educational.ml.cognifire.responses.PromptToCodeContent
 
 interface InspectionProcessing {
   fun isApplicable(): Boolean
   fun apply()
-  fun applyInspection(promptToCode: PromptToCodeResponse, psiFile: PsiFile): PromptToCodeResponse
+  fun applyInspection(promptToCode: PromptToCodeContent, psiFile: PsiFile): PromptToCodeContent
 }
