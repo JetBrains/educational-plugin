25a907a1e38599ca7f3d717c8ab33f2060dbff01
Stanislav.Sandler <stanislav.sandler@jetbrains.com>
2024-11-11 17:17:06 +0100

    Update to support new ML library interface.

--- intellij-plugin/Edu-Kotlin/src/com/jetbrains/edu/kotlin/cognifire/inspection/processing/InspectionProcessingUtils.kt
+++ intellij-plugin/Edu-Kotlin/src/com/jetbrains/edu/kotlin/cognifire/inspection/processing/InspectionProcessingUtils.kt
@@ -3,12 +3,12 @@ package com.jetbrains.edu.kotlin.cognifire.inspection.processing
 import com.intellij.openapi.application.runReadAction
 import com.intellij.psi.PsiFile
 import com.intellij.psi.util.PsiTreeUtil
-import com.jetbrains.educational.ml.cognifire.responses.PromptToCodeResponse
+import com.jetbrains.educational.ml.cognifire.responses.PromptToCodeContent
 import org.jetbrains.kotlin.psi.KtExpression
 import org.jetbrains.kotlin.psi.KtFunction
 import org.jetbrains.kotlin.psi.KtIfExpression
 
-fun updatePromptToCodeWithoutChangingLines(promptToCode: PromptToCodeResponse, psiFile: PsiFile) = runReadAction {
+fun updatePromptToCodeWithoutChangingLines(promptToCode: PromptToCodeContent, psiFile: PsiFile) = runReadAction {
   val function = PsiTreeUtil.collectElementsOfType(psiFile, KtFunction::class.java).firstOrNull() ?: error("Cannot find function")
   val functionLines = function.bodyExpression?.children?.joinToString(System.lineSeparator()) { it.text }?.lines() ?: error("Cannot find function body")
   promptToCode.mapIndexed { index, generatedCodeLine ->
@@ -16,26 +16,26 @@ fun updatePromptToCodeWithoutChangingLines(promptToCode: PromptToCodeResponse, p
   }
 }
 
-fun KtIfExpression.findIfStatementInResponse(promptToCode: PromptToCodeResponse) = runReadAction {
+fun KtIfExpression.findIfStatementInResponse(promptToCode: PromptToCodeContent) = runReadAction {
   val ifStatement = text.split(System.lineSeparator())
   ifStatement.mapNotNull { ifLine ->
     promptToCode.find { it.generatedCodeLine.contains(ifLine) }
   }.distinctBy { it.promptLineNumber }
 }
 
-fun KtExpression.getGeneratedCodeLine(promptToCode: PromptToCodeResponse) = runReadAction {
+fun KtExpression.getGeneratedCodeLine(promptToCode: PromptToCodeContent) = runReadAction {
   promptToCode.firstOrNull { it.generatedCodeLine.contains(text) }
 }
 
-fun KtIfExpression.getFirstPromptLineNumber(promptToCode: PromptToCodeResponse) = runReadAction {
+fun KtIfExpression.getFirstPromptLineNumber(promptToCode: PromptToCodeContent) = runReadAction {
   findIfStatementInResponse(promptToCode).minOfOrNull { it.promptLineNumber }
 }
 
-fun KtIfExpression.getLastPromptLineNumber(promptToCode: PromptToCodeResponse): Int? = runReadAction {
+fun KtIfExpression.getLastPromptLineNumber(promptToCode: PromptToCodeContent): Int? = runReadAction {
   findIfStatementInResponse(promptToCode).maxOfOrNull { it.promptLineNumber }
 }
 
-fun KtIfExpression.getFirstCodeLineNumber(promptToCode: PromptToCodeResponse) = runReadAction {
+fun KtIfExpression.getFirstCodeLineNumber(promptToCode: PromptToCodeContent) = runReadAction {
   findIfStatementInResponse(promptToCode).minOfOrNull { it.codeLineNumber }
 }
 
