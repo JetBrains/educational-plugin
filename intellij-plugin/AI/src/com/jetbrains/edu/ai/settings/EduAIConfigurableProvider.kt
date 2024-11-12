03c7993e898e069936b687c1d87d618747c4d45d
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2024-11-12 19:27:49 +0100

    Revert "Get rid of "edu.ai" experimental feature flag"
    This reverts commit 57b12ee9bd6c29162068cad0314cf06b7ef86e08.

--- intellij-plugin/AI/src/com/jetbrains/edu/ai/settings/EduAIConfigurableProvider.kt
+++ intellij-plugin/AI/src/com/jetbrains/edu/ai/settings/EduAIConfigurableProvider.kt
@@ -1,11 +1,12 @@
 package com.jetbrains.edu.ai.settings
 
-import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.options.Configurable
 import com.intellij.openapi.options.ConfigurableProvider
+import com.jetbrains.edu.learning.EduExperimentalFeatures
+import com.jetbrains.edu.learning.isFeatureEnabled
 
 class EduAIConfigurableProvider : ConfigurableProvider(), Configurable.Beta {
   override fun createConfigurable(): Configurable = EduAIConfigurable()
 
-  override fun canCreateConfigurable(): Boolean = ApplicationManager.getApplication().isInternal
+  override fun canCreateConfigurable(): Boolean = isFeatureEnabled(EduExperimentalFeatures.AI)
 }
