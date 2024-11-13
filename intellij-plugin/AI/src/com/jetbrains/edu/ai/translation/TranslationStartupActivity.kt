993fc7bdfb65e7fd2ec8d5f5b0bea7b1274a815b
SonyaBurg <sofia.biurganovskaia@jetbrains.com>
2024-11-13 22:20:49 +0000

    EDU-7680: Fetch course translation on project opening
    Implement fetching translation on project opening based on `Preferred language` option

--- intellij-plugin/AI/src/com/jetbrains/edu/ai/translation/TranslationStartupActivity.kt
+++ intellij-plugin/AI/src/com/jetbrains/edu/ai/translation/TranslationStartupActivity.kt
@@ -2,8 +2,11 @@ package com.jetbrains.edu.ai.translation
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
+import com.intellij.util.application
+import com.jetbrains.edu.ai.translation.settings.translationSettings
 import com.jetbrains.edu.ai.translation.updater.TranslationUpdateChecker.Companion.launchTranslationUpdateChecker
 import com.jetbrains.edu.learning.EduUtilsKt.isStudentProject
+import com.jetbrains.edu.learning.ai.TranslationProjectSettings
 import com.jetbrains.edu.learning.course
 import com.jetbrains.edu.learning.courseFormat.EduCourse
 
@@ -14,6 +17,11 @@ class TranslationStartupActivity : ProjectActivity {
     val course = project.course as? EduCourse ?: return
     if (!course.isMarketplaceRemote) return
 
+    val settings = application.translationSettings()
+    val language = settings.preferableLanguage
+    if (!TranslationProjectSettings.isCourseTranslated(project) && language != null) {
+      TranslationLoader.getInstance(project).fetchAndApplyTranslation(course, language)
+    }
     project.launchTranslationUpdateChecker(course)
   }
 }
