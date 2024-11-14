aa867f3ca725834adbd379c0ac955afb313c6be1
Stanislav.Sandler <stanislav.sandler@jetbrains.com>
2024-11-14 20:54:50 +0100

    Update tutorial texts and code prompts
    Added introductory section headers and explanatory text to various tutorial markdown files to improve readability and guide users. Simplified code prompts by removing excessive indent formatting. Removed unnecessary `@Suppress` annotation from `TutorialGenerator` file.

--- intellij-plugin/Edu-Cognifire/src/com/jetbrains/edu/cognifire/tutorial/TutorialGenerator.kt
+++ intellij-plugin/Edu-Cognifire/src/com/jetbrains/edu/cognifire/tutorial/TutorialGenerator.kt
@@ -34,7 +34,6 @@ import com.jetbrains.edu.learning.yaml.YamlFormatSynchronizer
  * The tutorial section can be accessed by right-clicking on the project root directory (main folder) and navigating to New -> Tutorial.
  */
 // TODO: change the icon
-@Suppress("ComponentNotRegistered")
 class TutorialGenerator : DumbAwareAction(EduCognifireBundle.message("item.tutorial.title"), "", Section) {
   override fun actionPerformed(e: AnActionEvent) {
     val project = e.project ?: return
@@ -187,7 +186,8 @@ class TutorialGenerator : DumbAwareAction(EduCognifireBundle.message("item.tutor
           TutorialTask("IfExpression", EDU_TASK),
           TutorialTask("Run", EDU_TASK),
           TutorialTask("Correction", EDU_TASK),
-          TutorialTask("AcceptanceOfCode", EDU_TASK)
+          TutorialTask("AcceptanceOfCode", EDU_TASK),
+          TutorialTask("Verification", THEORY_TASK),
         )
       )
     )
