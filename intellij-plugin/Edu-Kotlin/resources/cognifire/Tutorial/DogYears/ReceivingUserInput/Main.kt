621b5f037f2c8982ecc621cfc73b1ba49f4a303a
Stanislav.Sandler <stanislav.sandler@jetbrains.com>
2024-11-14 20:54:50 +0100

    Update tutorial texts and code prompts
    Added introductory section headers and explanatory text to various tutorial markdown files to improve readability and guide users. Simplified code prompts by removing excessive indent formatting. Removed unnecessary `@Suppress` annotation from `TutorialGenerator` file.

--- intellij-plugin/Edu-Kotlin/resources/cognifire/Tutorial/DogYears/ReceivingUserInput/Main.kt
+++ intellij-plugin/Edu-Kotlin/resources/cognifire/Tutorial/DogYears/ReceivingUserInput/Main.kt
@@ -5,7 +5,7 @@ import org.jetbrains.academy.cognifire.dsl.*
 fun calculateDogAgeInDogYears() {
   prompt("""
         Get the user input and save the result to a variable named humanYears.
-    """.trimIndent())
+    """)
 }
 
 fun verifyHumanYearsInput(humanYears: Int): Boolean {
