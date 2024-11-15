85cf23ab7620f49becc471e7ac72b4cbd2e90e71
Gleb Oborin <gleb.oborin@jetbrains.com>
2024-11-15 14:58:37 +0100

    [refactoring] Get rid of Lemmatizer & merge terms manager and storage

--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/taskToolWindow/ui/TaskToolWindowViewImpl.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/taskToolWindow/ui/TaskToolWindowViewImpl.kt
@@ -48,13 +48,13 @@ import com.jetbrains.edu.learning.taskToolWindow.ui.navigationMap.NavigationMapT
 import com.jetbrains.edu.learning.taskToolWindow.ui.tab.TabManager
 import com.jetbrains.edu.learning.taskToolWindow.ui.tab.TabType
 import com.jetbrains.edu.learning.taskToolWindow.ui.tab.TabType.SUBMISSIONS_TAB
+import com.jetbrains.edu.learning.theoryLookup.TheoryLookupTermsManager
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import com.jetbrains.edu.learning.theoryLookup.TermsListener
-import com.jetbrains.edu.learning.theoryLookup.TermsManager
 import java.awt.Component
 import java.awt.Dimension
 import javax.swing.*
@@ -285,7 +285,7 @@ class TaskToolWindowViewImpl(project: Project, scope: CoroutineScope) : TaskTool
         updateTab(SUBMISSIONS_TAB)
       }
     })
-    connection.subscribe(TermsManager.TOPIC, TermsListener { task ->
+    connection.subscribe(TheoryLookupTermsManager.TOPIC, TheoryLookupTermsManager.TermsListener { task ->
       invokeLater {
         if (task == project.getCurrentTask()) {
           updateTaskDescription()
