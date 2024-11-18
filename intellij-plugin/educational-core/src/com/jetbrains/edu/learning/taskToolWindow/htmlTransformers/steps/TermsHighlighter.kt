f27ea7549500c9864138af55b403f1d7965502c2
Gleb Oborin <gleb.oborin@jetbrains.com>
2024-11-18 11:30:20 +0100

    [refactoring] Refactor TermsHighlighter

--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/taskToolWindow/htmlTransformers/steps/TermsHighlighter.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/taskToolWindow/htmlTransformers/steps/TermsHighlighter.kt
@@ -6,6 +6,7 @@ import com.jetbrains.edu.learning.taskToolWindow.htmlTransformers.HtmlTransforme
 import com.jetbrains.edu.learning.theoryLookup.TheoryLookupTermsManager
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
+import org.jsoup.nodes.TextNode
 
 /**
  * Highlights terms in an HTML document by adding dashed underline style to the occurrences of the terms.
@@ -16,34 +17,45 @@ import org.jsoup.nodes.Element
  */
 object TermsHighlighter : HtmlTransformer {
   // TODO: filter code blocks and other tags
-  private val INVALID_TAGS = listOf(CODE_TAG, A_TAG, IMG_TAG)
+  private val INVALID_TAGS = setOf(CODE_TAG, A_TAG, IMG_TAG)
 
   private fun Element.isValidTag(): Boolean = tagName() !in INVALID_TAGS
 
   override fun transform(html: Document, context: HtmlTransformerContext): Document {
-    TheoryLookupTermsManager.getInstance(context.project).getTaskTerms(context.task)?.map { it.value }?.forEach { termTitle ->
-      html.getElementsContainingOwnText(termTitle)
-        .flatMap { element ->
-          if (!element.isValidTag()) return@flatMap emptyList()
-          element.textNodes().filter { textNode -> textNode.text().contains(termTitle) }
-        }
-        .forEach { node ->
-          val termElement = getDashedUnderlineElement(html, termTitle)
-          var currentNode = node
-          var text = currentNode.text()
-          while (text.contains(termTitle)) {
-            val startIdx = text.indexOf(termTitle)
-            val endIdx = startIdx + termTitle.length
-            val tail = if (endIdx < text.length) currentNode.splitText(endIdx) else null
-            val middle = currentNode.splitText(startIdx)
-            middle.after(termElement.clone())
-            middle.remove()
-            currentNode = tail ?: break
-            text = currentNode.text()
-          }
-        }
+    val terms = TheoryLookupTermsManager.getInstance(context.project).getTaskTerms(context.task)?.map { it.value }
+    if (terms.isNullOrEmpty()) return html
+    for (termTitle in terms) {
+      formatTermOccurrences(html, termTitle)
     }
-
     return html
+  }
+
+  private fun formatTermOccurrences(html: Document, termTitle: String) {
+    val nodes = html.getElementsContainingOwnText(termTitle)
+      .asSequence()
+      .filter { it.isValidTag() }
+      .flatMap { it.textNodes() }
+      .filter { it.text().contains(termTitle) }
+      .toList()
+    for (node in nodes) {
+      val termElement = getDashedUnderlineElement(html, termTitle)
+      var currentNode = node
+      do {
+        currentNode = formatNextOccurrence(currentNode, termTitle, termElement)
+      }
+      while (currentNode != null)
+    }
+  }
+
+  private fun formatNextOccurrence(textNode: TextNode, termTitle: String, dashedTermElement: Element): TextNode? {
+    val text = textNode.text()
+    if (!text.contains(termTitle)) return null
+    val startIdx = text.indexOf(termTitle)
+    val endIdx = startIdx + termTitle.length
+    val tail = if (endIdx < text.length) textNode.splitText(endIdx) else null
+    val middle = textNode.splitText(startIdx)
+    middle.after(dashedTermElement.clone())
+    middle.remove()
+    return tail
   }
 }
