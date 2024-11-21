varsy/edu-for-kotlin

EDU-7052: Simplify AI-Hints Feature Files Structure

intellij-plugin/educational-core/src/com/jetbrains/edu/learning/hints/context/TaskFileHintsDataHolder.kt
@@ -1,0 +1,40 @@
+package com.jetbrains.edu.learning.hints.context
+
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.components.service
+import com.intellij.openapi.project.Project
+import com.jetbrains.edu.learning.courseFormat.TaskFile
+import com.jetbrains.edu.learning.courseFormat.ext.project
+
+@Service(Service.Level.PROJECT)
+class TaskFileHintsDataHolder {
+  private val data = mutableMapOf<TaskFile, TaskFileHintData>()
+
+  private fun getOrCreate(taskFile: TaskFile): TaskFileHintData = data.getOrPut(taskFile) { TaskFileHintData() }
+
+  data class TaskFileHintData(
+    val functionSignatures: FunctionSignatures = FunctionSignatures(),
+    var snapshotFileHash: Int? = null,
+    val usedStrings: UsedStrings = UsedStrings(),
+  )
+
+  data class FunctionSignatures(
+    var value: List<FunctionSignature>? = null,
+    var snapshotHash: Int? = null,
+  )
+
+  data class UsedStrings(
+    var value: List<String>? = null,
+    var snapshotHash: Int? = null
+  )
+
+  companion object {
+    fun getInstance(project: Project): TaskFileHintsDataHolder = project.service()
+
+    val TaskFile.hintData: TaskFileHintData?
+      get() {
+        val project = task.project ?: return null
+        return getInstance(project).getOrCreate(this)
+      }
+  }
+}

intellij-plugin/educational-core/src/com/jetbrains/edu/learning/hints/context/TaskHintsDataHolder.kt
@@ -1,0 +1,29 @@
+package com.jetbrains.edu.learning.hints.context
+
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.components.service
+import com.intellij.openapi.project.Project
+import com.jetbrains.edu.learning.courseFormat.ext.project
+import com.jetbrains.edu.learning.courseFormat.tasks.Task
+
+@Service(Service.Level.PROJECT)
+class TaskHintsDataHolder {
+  private val data = mutableMapOf<Task, TaskHintData>()
+
+  private fun getOrCreate(task: Task): TaskHintData = data.getOrPut(task) { TaskHintData() }
+
+  data class TaskHintData(
+    var authorSolutionContext: AuthorSolutionContext? = null,
+    var taskFilesWithChangedFunctions: Map<String, List<String>>? = null
+  )
+
+  companion object {
+    fun getInstance(project: Project): TaskHintsDataHolder = project.service()
+
+    val Task.hintData: TaskHintData?
+      get() {
+        val project = project ?: return null
+        return getInstance(project).getOrCreate(this)
+      }
+  }
+}

JetBrains/educational-plugin

intellij-plugin/educational-core/src/com/jetbrains/edu/learning/hints/context/TaskFileHintsDataHolder.kt
@@ -1,0 +1,41 @@
+package com.jetbrains.edu.learning.hints.context
+
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.components.service
+import com.intellij.openapi.project.Project
+import com.jetbrains.edu.learning.courseFormat.TaskFile
+import com.jetbrains.edu.learning.courseFormat.ext.project
+import com.jetbrains.rd.util.ConcurrentHashMap
+
+@Service(Service.Level.PROJECT)
+class TaskFileHintsDataHolder {
+  private val data = ConcurrentHashMap<TaskFile, TaskFileHintData>()
+
+  private fun getOrCreate(taskFile: TaskFile): TaskFileHintData = data.getOrPut(taskFile) { TaskFileHintData() }
+
+  data class TaskFileHintData(
+    val functionSignatures: FunctionSignatures = FunctionSignatures(),
+    var snapshotFileHash: Int? = null,
+    val usedStrings: UsedStrings = UsedStrings(),
+  )
+
+  data class FunctionSignatures(
+    var value: List<FunctionSignature>? = null,
+    var snapshotHash: Int? = null,
+  )
+
+  data class UsedStrings(
+    var value: List<String>? = null,
+    var snapshotHash: Int? = null
+  )
+
+  companion object {
+    fun getInstance(project: Project): TaskFileHintsDataHolder = project.service()
+
+    val TaskFile.hintData: TaskFileHintData?
+      get() {
+        val project = task.project ?: return null
+        return getInstance(project).getOrCreate(this)
+      }
+  }
+}

intellij-plugin/educational-core/src/com/jetbrains/edu/learning/hints/context/TaskHintsDataHolder.kt
@@ -1,0 +1,30 @@
+package com.jetbrains.edu.learning.hints.context
+
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.components.service
+import com.intellij.openapi.project.Project
+import com.jetbrains.edu.learning.courseFormat.ext.project
+import com.jetbrains.edu.learning.courseFormat.tasks.Task
+import com.jetbrains.rd.util.ConcurrentHashMap
+
+@Service(Service.Level.PROJECT)
+class TaskHintsDataHolder {
+  private val data = ConcurrentHashMap<Task, TaskHintData>()
+
+  private fun getOrCreate(task: Task): TaskHintData = data.getOrPut(task) { TaskHintData() }
+
+  data class TaskHintData(
+    var authorSolutionContext: AuthorSolutionContext? = null,
+    var taskFilesWithChangedFunctions: Map<String, List<String>>? = null
+  )
+
+  companion object {
+    fun getInstance(project: Project): TaskHintsDataHolder = project.service()
+
+    val Task.hintData: TaskHintData?
+      get() {
+        val project = project ?: return null
+        return getInstance(project).getOrCreate(this)
+      }
+  }
+}