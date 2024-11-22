6d754995ce6ca22f3118449be065cd06cc5f63f0
SonyaBurg <sofia.biurganovskaia@jetbrains.com>
2024-11-22 16:20:02 +0200

    Checker for Unity projects

--- intellij-plugin/Edu-CSharp/src/com/jetbrains/edu/csharp/hyperskill/HyperskillUnityCourseRootProvider.kt
+++ intellij-plugin/Edu-CSharp/src/com/jetbrains/edu/csharp/hyperskill/HyperskillUnityCourseRootProvider.kt
@@ -5,19 +5,24 @@ import com.intellij.openapi.project.guessProjectDir
 import com.intellij.openapi.vfs.VirtualFile
 import com.jetbrains.edu.learning.CourseRootProvider
 import com.jetbrains.rider.plugins.unity.isUnityProject
+import com.intellij.openapi.diagnostic.Logger
 
 class HyperskillUnityCourseRootProvider : CourseRootProvider {
   override fun Project.findCourseRootIfNeeded(): VirtualFile? {
     if (isUnityProject.value) {
-      return guessProjectDir()?.findFileByRelativePath("$PACKAGES_DIR/$HYPERSKILL_UNITY_NAME/$TESTS_DIR")
-             ?: error("Invalid structure for Unity-based course")
+      val root = guessProjectDir()?.findFileByRelativePath("$PACKAGES_DIR/$HYPERSKILL_UNITY_NAME/$TESTS_DIR")
+             ?: error("Invalid structure for Unity-based course: ${guessProjectDir()}")
+      LOG.info("Unity project root found: $root")
+      return root
     }
     return null
   }
 
   companion object {
+    private val LOG = Logger.getInstance(HyperskillUnityCourseRootProvider::class.java)
+
     private const val PACKAGES_DIR = "Packages"
-    private const val HYPERSKILL_UNITY_NAME: String = "HyperskillUnityTests"
+    const val HYPERSKILL_UNITY_NAME: String = "HyperskillUnityTests"
     private const val TESTS_DIR = "Tests"
   }
 }
