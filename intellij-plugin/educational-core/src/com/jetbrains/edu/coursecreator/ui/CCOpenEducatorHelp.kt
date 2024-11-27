099b6e8c6ee0f2ca32f95d7565cac678343c2f29
Valentina Kiryushkina <valentina.kiryushkina@jetbrains.com>
2024-11-27 15:58:23 +0100

    EDU-7648 Support dark theme

--- intellij-plugin/educational-core/src/com/jetbrains/edu/coursecreator/ui/CCOpenEducatorHelp.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/coursecreator/ui/CCOpenEducatorHelp.kt
@@ -4,9 +4,9 @@ import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.fileEditor.impl.HTMLEditorProvider
-import com.intellij.openapi.fileEditor.impl.HTMLEditorProvider.Request.Companion.url
 import com.intellij.openapi.project.Project
-import com.intellij.util.Urls.newFromEncoded
+import com.intellij.ui.jcef.JBCefApp
+import com.intellij.util.ui.StartupUiUtil
 import com.jetbrains.edu.learning.EduExperimentalFeatures
 import com.jetbrains.edu.learning.actions.ActionWithButtonCustomComponent
 import com.jetbrains.edu.learning.isFeatureEnabled
@@ -26,20 +26,34 @@ class CCOpenEducatorHelp : ActionWithButtonCustomComponent() {
   }
 
   companion object {
-    private const val DOCUMENTATION_URL = "https://jetbrains-academy.github.io/educators-guide-test/"
+    private const val HELP_URL = "/educatorsHelpPage/educatorsHelpPage.html"
+    private const val THEME_KEY = "\$__VISION_PAGE_SETTINGS_THEME__\$"
+    private const val DARK_THEME = "dark"
+    private const val LIGHT_THEME = "light"
 
     fun doOpen(project: Project) {
+      require(JBCefApp.isSupported()) { "JCEF is not supported on this system" }
+
+      val welcomeScreenHtml = loadResourceAsString()?.replace(THEME_KEY, if (StartupUiUtil.isDarkTheme) DARK_THEME else LIGHT_THEME)
+      welcomeScreenHtml ?: error("Couldn't load help file $HELP_URL")
+
       if (isFeatureEnabled(EduExperimentalFeatures.EDUCATOR_HELP)) {
-        val request = url(newFromEncoded(DOCUMENTATION_URL).toExternalForm())
         val openFiles = FileEditorManager.getInstance(project).openFiles
         val educatorHelpFile = openFiles.find { it.url.endsWith(EduCoreBundle.message("course.creator.docs.file")) }
         if (educatorHelpFile != null) {
           FileEditorManager.getInstance(project).openFile(educatorHelpFile)
         }
         else {
+          val request = HTMLEditorProvider.Request.html(welcomeScreenHtml)
           openEditor(project, EduCoreBundle.message("course.creator.docs.file"), request)
         }
       }
+    }
+
+    private fun loadResourceAsString(): String? {
+      val stream = this::class.java.getResourceAsStream(HELP_URL)
+      val content = stream?.bufferedReader().use { it?.readText() }
+      return content
     }
 
     private fun openEditor(project: Project, title: String, request: HTMLEditorProvider.Request) {
