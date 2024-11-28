eac3b09ab661262d48cce14e261c8e33f0c7ebb1
anchouls <anna.potriasaeva@jetbrains.com>
2024-11-28 12:32:53 +0000

    Syntax highlighting
    Co-authored-by: Stanislav Sandler <stanislav.sandler@jetbrains.com>
    
    Merge-request: EDU-MR-3832
    Merged-by: Anna Potriasaeva <Anna.Potriasaeva@jetbrains.com>

--- intellij-plugin/Edu-Cognifire/src/com/jetbrains/edu/cognifire/highlighting/undefinedidentifier/AnnotatorRuleMatch.kt
+++ intellij-plugin/Edu-Cognifire/src/com/jetbrains/edu/cognifire/highlighting/undefinedidentifier/AnnotatorRuleMatch.kt
@@ -5,6 +5,16 @@ package com.jetbrains.edu.cognifire.highlighting.undefinedidentifier
  * @property rule the associated [AnnotatorRule].
  * @property identifier the [MatchGroup] representing the matched identifier (e.g., function name).
  * @property arguments the substring with function arguments.
+ * @property keywords representing matching keywords (e.g., create, set, store)
+ * @property values representing matching values (e.g., 3, true)
+ * @property strings representing matching strings (e.g., "Hello", "string")
  * Can be `null` if there is no match with the `rule`.
  */
-data class AnnotatorRuleMatch(val rule: AnnotatorRule, val identifier: MatchGroup, val arguments: String?)
+data class AnnotatorRuleMatch(
+  val rule: AnnotatorRule,
+  val identifier: MatchGroup,
+  val arguments: String? = null,
+  val keywords: List<MatchGroup> = emptyList(),
+  val values: List<MatchGroup> = emptyList(),
+  val strings: List<MatchGroup> = emptyList()
+)
