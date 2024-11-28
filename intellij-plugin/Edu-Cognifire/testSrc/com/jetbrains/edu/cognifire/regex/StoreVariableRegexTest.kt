e9e1ca34f53933691d0357c814d8ec3be8cf54fc
anchouls <anna.potriasaeva@jetbrains.com>
2024-11-28 12:32:53 +0000

    Syntax highlighting
    Co-authored-by: Stanislav Sandler <stanislav.sandler@jetbrains.com>
    
    Merge-request: EDU-MR-3832
    Merged-by: Anna Potriasaeva <Anna.Potriasaeva@jetbrains.com>

--- intellij-plugin/Edu-Cognifire/testSrc/com/jetbrains/edu/cognifire/regex/StoreVariableRegexTest.kt
+++ intellij-plugin/Edu-Cognifire/testSrc/com/jetbrains/edu/cognifire/regex/StoreVariableRegexTest.kt
@@ -10,37 +10,37 @@ class StoreVariableRegexTest : RegexTest, EduTestCase() {
 
   override fun shouldMatch() =
     listOf(
-      TestAnswer("store 3 in `foo`", listOf("foo")), // test the `store` verb
-      TestAnswer("stored 3 in `foo`", listOf("foo")), // test the `stored` verb
+      TestAnswer("store 3 in `foo`", listOf("store", "3", "in", "foo")), // test the `store` verb
+      TestAnswer("stored 3 in `foo`", listOf("stored", "3", "in", "foo")), // test the `stored` verb
 
-      TestAnswer("store result in `foo`", listOf("foo")), // test the `result` word
-      TestAnswer("stored 3321 in `foo`", listOf("foo")), // test the number
-      TestAnswer("store \"hello\" in `greeting`", listOf("greeting")), // test the string value
-      TestAnswer("store `myVar` in `foo`", listOf("foo")), // test the value wrapped in backticks
-      TestAnswer("store true in `foo`", listOf("foo")), // test true value
-      TestAnswer("store false in `foo`", listOf("foo")), // test false value
+      TestAnswer("store result in `foo`", listOf("store", "result", "in", "foo")), // test the `result` word
+      TestAnswer("stored 3321 in `foo`", listOf("stored", "3321", "in", "foo")), // test the number
+      TestAnswer("store \"hello\" in `greeting`", listOf("store", "\"hello\"", "in", "greeting")), // test the string value
+      TestAnswer("store `myVar` in `foo`", listOf("store", "`myVar`", "in", "foo")), // test the value wrapped in backticks
+      TestAnswer("store true in `foo`", listOf("store", "true", "in", "foo")), // test true value
+      TestAnswer("store false in `foo`", listOf("store", "false", "in", "foo")), // test false value
 
-      TestAnswer("store the result in `buzz`", listOf("buzz")), // test the `the` article, before the value
-      TestAnswer("store a `pear` in `buzz`", listOf("buzz")), // test the `a` article, before the value
-      TestAnswer("store an `apple` in `buzz`", listOf("buzz")), // test the `an` article, before the value
-      TestAnswer("store 3 in the `buzz`", listOf("buzz")), // test the `the` article, before the variable name
-      TestAnswer("store 3 in a `buzz`", listOf("buzz")), // test the `a` article, before the variable name
-      TestAnswer("store 3 in an `apple`", listOf("apple")), // test the `an` article, before the variable name
+      TestAnswer("store the result in `buzz`", listOf("store", "result", "in", "buzz")), // test the `the` article, before the value
+      TestAnswer("store a `pear` in `buzz`", listOf("store", "`pear`", "in", "buzz")), // test the `a` article, before the value
+      TestAnswer("store an `apple` in `buzz`", listOf("store", "`apple`", "in", "buzz")), // test the `an` article, before the value
+      TestAnswer("store 3 in the `buzz`", listOf("store", "3", "in", "buzz")), // test the `the` article, before the variable name
+      TestAnswer("store 3 in a `buzz`", listOf("store", "3", "in", "buzz")), // test the `a` article, before the variable name
+      TestAnswer("store 3 in an `apple`", listOf("store", "3", "in", "apple")), // test the `an` article, before the variable name
 
-      TestAnswer("store the value 3 in `buzz`", listOf("buzz")), // test the optional `value` word
-      TestAnswer("store 3 in the variable `buzz`", listOf("buzz")), // test the optional `variable` word
+      TestAnswer("store the value 3 in `buzz`", listOf("store", "3", "in", "buzz")), // test the optional `value` word
+      TestAnswer("store 3 in the variable `buzz`", listOf("store", "3", "in", "buzz")), // test the optional `variable` word
 
-      TestAnswer("store `foo` multiplied by 3 in `buzz`", listOf("buzz")), // test arbitrary text
-      TestAnswer("store the result divided by `divisor` in `foo`", listOf("foo")), // test arbitrary text
+      TestAnswer("store `foo` multiplied by 3 in `buzz`", listOf("store", "`foo`", "in", "buzz")), // test arbitrary text
+      TestAnswer("store the result divided by `divisor` in `foo`", listOf("store", "result", "in", "foo")), // test arbitrary text
 
-      TestAnswer("SToRe tHe ResUlT iN `foo`", listOf("foo")), // test case-insensitive
-      TestAnswer("sToRE THE VALUe 3 IN `buzz`", listOf("buzz")), // test case-insensitive
-      TestAnswer("STOred 3 In `foo`", listOf("foo")), // test case-insensitive
-      TestAnswer("sTORED tHE REsUlT DIVideD bY `divisor` IN `foo`", listOf("foo")), // test case-insensitive
+      TestAnswer("SToRe tHe ResUlT iN `foo`", listOf("SToRe", "ResUlT", "iN", "foo")), // test case-insensitive
+      TestAnswer("sToRE THE VALUe 3 IN `buzz`", listOf("sToRE", "3", "IN", "buzz")), // test case-insensitive
+      TestAnswer("STOred 3 In `foo`", listOf("STOred", "3", "In", "foo")), // test case-insensitive
+      TestAnswer("sTORED tHE REsUlT DIVideD bY `divisor` IN `foo`", listOf("sTORED", "REsUlT", "IN", "foo")), // test case-insensitive
 
-      TestAnswer("store     `foo`   multiplied    by  3    in     `buzz`", listOf("buzz")), // test spacing
-      TestAnswer("STOred     3      In    `foo`", listOf("foo")), // test spacing
-      TestAnswer("store     an  `apple`   in  `buzz`", listOf("buzz")), // test spacing
+      TestAnswer("store     `foo`   multiplied    by  3    in     `buzz`", listOf("store", "`foo`", "in", "buzz")), // test spacing
+      TestAnswer("STOred     3      In    `foo`", listOf("STOred", "3", "In", "foo")), // test spacing
+      TestAnswer("store     an  `apple`   in  `buzz`", listOf("store", "`apple`", "in", "buzz")), // test spacing
     )
 
   override fun shouldNotMatch() =
