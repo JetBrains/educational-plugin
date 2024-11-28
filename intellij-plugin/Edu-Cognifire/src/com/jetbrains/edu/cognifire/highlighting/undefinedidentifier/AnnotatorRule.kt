2703c66d3577c3416db661bae1016b600c080ad7
anchouls <anna.potriasaeva@jetbrains.com>
2024-11-28 12:32:53 +0000

    Syntax highlighting
    Co-authored-by: Stanislav Sandler <stanislav.sandler@jetbrains.com>
    
    Merge-request: EDU-MR-3832
    Merged-by: Anna Potriasaeva <Anna.Potriasaeva@jetbrains.com>

--- intellij-plugin/Edu-Cognifire/src/com/jetbrains/edu/cognifire/highlighting/undefinedidentifier/AnnotatorRule.kt
+++ intellij-plugin/Edu-Cognifire/src/com/jetbrains/edu/cognifire/highlighting/undefinedidentifier/AnnotatorRule.kt
@@ -7,12 +7,16 @@ import com.jetbrains.edu.cognifire.highlighting.undefinedidentifier.GrammarRegex
 import com.jetbrains.edu.cognifire.highlighting.undefinedidentifier.GrammarRegex.saveVariable
 import com.jetbrains.edu.cognifire.highlighting.undefinedidentifier.GrammarRegex.setVariable
 import com.jetbrains.edu.cognifire.highlighting.undefinedidentifier.GrammarRegex.storeVariable
+import com.jetbrains.edu.cognifire.highlighting.undefinedidentifier.GrammarRegex.valueRegex
+import com.jetbrains.edu.cognifire.highlighting.undefinedidentifier.GrammarRegex.keyword
+import com.jetbrains.edu.cognifire.highlighting.undefinedidentifier.GrammarRegex.stringRegex
 
 /**
  * Represents a rule based on which the [AnnotatorError] can be found.
  * These rules are dictated with the regular expressions.
  */
 enum class AnnotatorRule(val regex: Regex) {
+  STRING(stringRegex),
   STORE_VARIABLE(storeVariable),
   CREATE_VARIABLE(createVariable),
   SET_VARIABLE(setVariable),
@@ -20,4 +24,6 @@ enum class AnnotatorRule(val regex: Regex) {
   CALL_FUNCTION(callFunction),
   LOOP_EXPRESSION(loopExpression),
   ISOLATED_CODE(isolatedCode),
+  KEYWORD(keyword),
+  VALUE(valueRegex)
 }
