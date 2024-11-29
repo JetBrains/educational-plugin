e8b908850a2e5f197a29ce0ea2288b7cc483f041
Stanislav.Sandler <stanislav.sandler@jetbrains.com>
2024-11-29 15:24:24 +0100

    Refactor highlighter management to use ProdeHighlighter interface
    Replaced specific highlighter lists with a unified `ProdeHighlighter` collection to simplify highlighter management. Introduced new highlighter classes such as `GrammarHighlighter`, `UncommitedChangesHighlighter`, and `LinkingHighlighter`. Renamed and adjusted interfaces and methods for better clarity and alignment with the Prode convention.

--- intellij-plugin/Edu-Cognifire/src/com/jetbrains/edu/cognifire/highlighting/grammar/GrammarHighlighterProcessor.kt
+++ intellij-plugin/Edu-Cognifire/src/com/jetbrains/edu/cognifire/highlighting/grammar/GrammarHighlighterProcessor.kt
@@ -3,24 +3,19 @@ package com.jetbrains.edu.cognifire.highlighting.grammar
 import com.intellij.openapi.project.Project
 import com.jetbrains.edu.cognifire.grammar.OffsetSentence
 import com.jetbrains.edu.cognifire.highlighting.HighlighterManager
-import com.intellij.openapi.editor.colors.CodeInsightColors
-import com.intellij.openapi.editor.colors.EditorColorsManager
-
-private val attributes = EditorColorsManager.getInstance().globalScheme.getAttributes(CodeInsightColors.WARNINGS_ATTRIBUTES)
+import com.jetbrains.edu.cognifire.highlighting.highlighers.GrammarHighlighter
 
 /**
  * Represents a class that highlights sentences that didn't pass the grammar.
  */
-object GrammarHighlighter {
+object GrammarHighlighterProcessor {
   fun highlightAll(project: Project, unparsableSentences: List<OffsetSentence>) {
     unparsableSentences.forEach { highlightSentence(project, it) }
   }
 
   private fun highlightSentence(project: Project, sentence: OffsetSentence) {
-    HighlighterManager.getInstance(project).addGrammarHighlighter(
-      sentence.startOffset,
-      sentence.endOffset,
-      attributes
+    HighlighterManager.getInstance(project).addProdeHighlighter(
+      GrammarHighlighter(sentence.startOffset, sentence.endOffset)
     )
   }
 }
---
+++ intellij-plugin/Edu-Cognifire/src/com/jetbrains/edu/cognifire/highlighting/highlighers/LinkingHighlighter.kt
@@ -0,0 +1,19 @@
+package com.jetbrains.edu.cognifire.highlighting.highlighers
+
+import com.intellij.openapi.editor.markup.HighlighterLayer
+import com.intellij.openapi.editor.markup.MarkupModel
+import com.intellij.openapi.editor.markup.RangeHighlighter
+import com.intellij.openapi.editor.markup.TextAttributes
+import com.intellij.ui.JBColor
+
+class LinkingHighlighter(val lineNumber: Int) : ProdeHighlighter {
+  override val attributes = TextAttributes().apply { backgroundColor = JBColor.LIGHT_GRAY }
+  override var markupHighlighter: RangeHighlighter? = null
+  override fun addMarkupHighlighter(markupModel: MarkupModel?): RangeHighlighter? {
+    return markupModel?.addLineHighlighter(
+      lineNumber, HighlighterLayer.LAST, attributes
+    ).also {
+      markupHighlighter = it
+    }
+  }
+}
