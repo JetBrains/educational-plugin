5abfc547de17a0c54a81a9a817130f7e4dc335c8
anchouls <anna.potriasaeva@jetbrains.com>
2024-11-29 15:22:50 +0000

    Implementation of prompt synchronisation with code
    Merge-request: EDU-MR-3820
    Merged-by: Anna Potriasaeva <Anna.Potriasaeva@jetbrains.com>

--- intellij-plugin/Edu-Kotlin/src/com/jetbrains/edu/kotlin/cognifire/utils/utils.kt
+++ intellij-plugin/Edu-Kotlin/src/com/jetbrains/edu/kotlin/cognifire/utils/utils.kt
@@ -6,10 +6,12 @@ import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiFileFactory
 import org.jetbrains.kotlin.idea.KotlinLanguage
 import org.jetbrains.kotlin.psi.KtCallExpression
+import org.jetbrains.kotlin.psi.KtValueArgument
 
 const val UNIT_RETURN_VALUE = "Unit"
 const val TODO_MARKER = "TODO"
 const val EMPTY_TODO = ""
+const val QUOTE_CHAR = '"'
 
 fun findBlock(
   element: PsiElement,
@@ -36,4 +38,10 @@ fun PsiElement.getTodoMessageOrNull() = if (this is KtCallExpression && calleeEx
   valueArguments.firstOrNull()?.getArgumentExpression()?.text ?: EMPTY_TODO
 } else {
   null
+}
+
+fun KtValueArgument.getBaseContentOffset(): Int {
+  val trimmedPromptPromptText = text.trimStart(QUOTE_CHAR).trimStart()
+  val trimmedOffset = text.length - trimmedPromptPromptText.length
+  return textOffset + trimmedOffset
 }
