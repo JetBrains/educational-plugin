839e84ce9da28074b75f893eec0035104ba35b0c
Stanislav.Sandler <stanislav.sandler@jetbrains.com>
2024-11-29 15:24:24 +0100

    Refactor highlighter management to use ProdeHighlighter interface
    Replaced specific highlighter lists with a unified `ProdeHighlighter` collection to simplify highlighter management. Introduced new highlighter classes such as `GrammarHighlighter`, `UncommitedChangesHighlighter`, and `LinkingHighlighter`. Renamed and adjusted interfaces and methods for better clarity and alignment with the Prode convention.

---
+++ intellij-plugin/Edu-Cognifire/src/com/jetbrains/edu/cognifire/highlighting/highlighers/UncommitedChangesHighlighter.kt
@@ -0,0 +1,26 @@
+package com.jetbrains.edu.cognifire.highlighting.highlighers
+
+import com.intellij.openapi.editor.markup.HighlighterLayer
+import com.intellij.openapi.editor.markup.HighlighterTargetArea
+import com.intellij.openapi.editor.markup.MarkupModel
+import com.intellij.openapi.editor.markup.RangeHighlighter
+import com.intellij.openapi.editor.markup.TextAttributes
+import com.intellij.ui.JBColor
+
+class UncommitedChangesHighlighter(val startOffset: Int, val endOffset: Int): ProdeHighlighter {
+  override val attributes = TextAttributes().apply { backgroundColor = JBColor.green }
+
+  override var markupHighlighter: RangeHighlighter? = null
+
+  override fun addMarkupHighlighter(markupModel: MarkupModel?): RangeHighlighter? {
+    return markupModel?.addRangeHighlighter(
+      startOffset,
+      endOffset,
+      HighlighterLayer.SELECTION,
+      attributes,
+      HighlighterTargetArea.EXACT_RANGE
+    )?.also {
+      markupHighlighter = it
+    }
+  }
+}
