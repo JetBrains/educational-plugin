a85a21b27afc1f40a98d3a63ac37a5d38d9e0836
Mark Prudnikov <mark.prudnikov@jetbrains.com>
2024-12-02 15:12:17 +0100

    EDU-7630: Parse Common Errors from AI Service

--- intellij-plugin/features/ai-hints-core/src/com/jetbrains/edu/aiHints/core/connector/HintsServiceConnector.kt
+++ intellij-plugin/features/ai-hints-core/src/com/jetbrains/edu/aiHints/core/connector/HintsServiceConnector.kt
@@ -6,13 +6,21 @@ import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.diagnostic.logger
 import com.intellij.ui.JBAccountInfoService
 import com.jetbrains.edu.ai.host.EduAIServiceHost
+import com.jetbrains.edu.aiHints.core.messages.EduAIHintsCoreBundle
 import com.jetbrains.edu.aiHints.core.service.HintsService
+import com.jetbrains.edu.learning.Err
+import com.jetbrains.edu.learning.Ok
+import com.jetbrains.edu.learning.Result
+import com.jetbrains.edu.learning.network.HTTP_UNAVAILABLE_FOR_LEGAL_REASONS
 import com.jetbrains.edu.learning.network.createRetrofitBuilder
+import com.jetbrains.edu.learning.onError
 import com.jetbrains.educational.ml.hints.context.CodeHintContext
 import com.jetbrains.educational.ml.hints.context.TextHintContext
 import com.jetbrains.educational.ml.hints.hint.CodeHint
 import com.jetbrains.educational.ml.hints.hint.TextHint
 import okhttp3.ConnectionPool
+import retrofit2.Response
+import java.net.HttpURLConnection.*
 
 @Service(Service.Level.APP)
 class HintsServiceConnector {
@@ -36,14 +44,46 @@ class HintsServiceConnector {
       .create(HintsService::class.java)
   }
 
+  /**
+   * We throw the [Err]or with bundled string further because we catch them on the `educational-ml-library` side.
+   * This is a bad design, and we must reimplement this ([EDU-7696](https://youtrack.jetbrains.com/issue/EDU-7696)).
+   */
   suspend fun getCodeHint(context: CodeHintContext): CodeHint {
-    val response = service.getCodeHint(context).body() ?: error("Response body is null")
+    val response = service.getCodeHint(context).handleResponse().onError {
+      error(it)
+    }
     return response.asCodeHint()
   }
 
+  /**
+   * We throw the [Err]or with bundled string further because we catch them on the `educational-ml-library` side.
+   * This is a bad design, and we must reimplement this ([EDU-7696](https://youtrack.jetbrains.com/issue/EDU-7696)).
+   */
   suspend fun getTextHint(context: TextHintContext): TextHint {
-    val response = service.getTextHint(context).body() ?: error("Response body is null")
+    val response = service.getTextHint(context).handleResponse().onError {
+      error(it)
+    }
     return response.asTextHint()
+  }
+
+  /**
+   * We put strings from bundle to the [Err]ors, because we then show them to user.
+   *
+   * @see [com.jetbrains.edu.aiHints.core.HintsLoader.getHint]
+   */
+  private fun <T> Response<List<T>>.handleResponse(): Result<List<T>, String> {
+    val code = code()
+    if (!isSuccessful) {
+      LOG.warn("Request failed. Status code: $code. Error message: ${errorBody()?.string().toString()}")
+    }
+    val body = body()
+    return when {
+      code == HTTP_OK && body != null -> Ok(body)
+      body == null -> Err(EduAIHintsCoreBundle.message("hints.error.failed.to.generate"))
+      code == HTTP_UNAVAILABLE_FOR_LEGAL_REASONS -> Err(EduAIHintsCoreBundle.message("hints.error.unavailable.for.legal.reasons"))
+      code == HTTP_UNAVAILABLE || code == HTTP_BAD_GATEWAY -> Err(EduAIHintsCoreBundle.message("hints.error.service.unavailable"))
+      else -> Err(EduAIHintsCoreBundle.message("hints.error.unknown"))
+    }
   }
 
   companion object {
