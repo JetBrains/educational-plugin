fb1642dada19b1c7f7f93e441ecd1c42a9737bf2
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2024-12-05 12:51:17 +0100

    Add basic test for course translation
    It provides basic infrastructure for checking course translation feature

---
+++ intellij-plugin/AI/testSrc/com/jetbrains/edu/ai/translation/TranslationTest.kt
@@ -0,0 +1,128 @@
+package com.jetbrains.edu.ai.translation
+
+import com.intellij.testFramework.PlatformTestUtil
+import com.intellij.testFramework.replaceService
+import com.intellij.util.application
+import com.jetbrains.edu.ai.translation.connector.TranslationServiceConnector
+import com.jetbrains.edu.learning.EduTestCase
+import com.jetbrains.edu.learning.Ok
+import com.jetbrains.edu.learning.ai.TranslationProjectSettings
+import com.jetbrains.edu.learning.ai.TranslationProperties
+import com.jetbrains.edu.learning.courseFormat.EduCourse
+import com.jetbrains.educational.core.format.enum.TranslationLanguage
+import com.jetbrains.educational.translation.format.CourseTranslationResponse
+import com.jetbrains.educational.translation.format.TranslatedText
+import com.jetbrains.educational.translation.format.domain.TranslationVersion
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.spyk
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import org.junit.Test
+import java.util.concurrent.atomic.AtomicBoolean
+
+class TranslationTest : EduTestCase() {
+
+  @Test
+  fun `test translation loading`() {
+    // given
+    val course = courseWithFiles(id = 1) {
+      lesson("lesson1", id = 2) {
+        eduTask("task1", stepId = 3)
+        eduTask("task2", stepId = 4)
+      }
+    } as EduCourse
+
+    val translationLanguage = TranslationLanguage.RUSSIAN
+    val translationVersion = 1
+
+    val response = CourseTranslationResponse(
+      TranslationVersion(translationVersion),
+      translationLanguage,
+      mapOf(
+        "3" to TranslatedText(translationLanguage, "description 1 translated"),
+        "4" to TranslatedText(translationLanguage, "description 2 translated")
+      ),
+      mapOf(
+        "2" to "lesson1 translated",
+        "3" to "task1 translated",
+        "4" to "task2 translated",
+      )
+    )
+
+    val mockedService = mockTranslationServiceConnector()
+    coEvery { mockedService.getTranslatedCourse(course.id, course.marketplaceCourseVersion, translationLanguage) } returns Ok(response)
+
+    // when
+    performAndWait(expectedVersion = translationVersion) {
+      TranslationLoader.getInstance(project).fetchAndApplyTranslation(course, translationLanguage)
+    }
+
+    // then
+    coVerify(exactly = 1) { mockedService.getTranslatedCourse(course.id, course.marketplaceCourseVersion, translationLanguage) }
+
+    val translationProperties = TranslationProjectSettings.getInstance(project).translationProperties.value
+    val expectedTranslationProperties = TranslationProperties(
+      language = translationLanguage,
+      structureTranslation = mapOf(
+        "2" to "lesson1 translated",
+        "3" to "task1 translated",
+        "4" to "task2 translated"
+      ),
+      version = TranslationVersion(translationVersion)
+    )
+    assertEquals(expectedTranslationProperties, translationProperties)
+
+    checkFileTree {
+      dir("lesson1") {
+        dir("task1") {
+          file("task.md")
+          file("task_ru.md", "description 1 translated")
+        }
+        dir("task2") {
+          file("task.md")
+          file("task_ru.md", "description 2 translated")
+        }
+      }
+    }
+  }
+
+  private fun mockTranslationServiceConnector(): TranslationServiceConnector {
+    val service = TranslationServiceConnector.getInstance()
+    val mockedService = spyk(service)
+    application.replaceService(TranslationServiceConnector::class.java, mockedService, testRootDisposable)
+    return mockedService
+  }
+
+  private fun performAndWait(expectedVersion: Int, action: () -> Unit) {
+    val updated = AtomicBoolean(false)
+
+    @OptIn(DelicateCoroutinesApi::class)
+    val job = GlobalScope.launch {
+      TranslationProjectSettings.getInstance(project).translationProperties.collectLatest {
+        if (it?.version?.value == expectedVersion) {
+          updated.set(true)
+        }
+      }
+    }
+    try {
+      action()
+      waitFor { updated.get() }
+    }
+    finally {
+      job.cancel()
+    }
+  }
+
+  private fun waitFor(condition: () -> Boolean) {
+    repeat(100) {
+      if (condition()) return
+      Thread.sleep(50)
+      PlatformTestUtil.dispatchAllEventsInIdeEventQueue()
+    }
+
+    error("Too long waiting")
+  }
+}
