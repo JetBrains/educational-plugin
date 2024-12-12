0e178a9df8d69a4a0ca77f72cd21d851ed80fa9c
anchouls <anna.potriasaeva@jetbrains.com>
2024-12-12 16:19:32 +0100

    Added startup logic to initialize highlighters, guarded blocks, and listeners for Prode expressions

--- intellij-plugin/Edu-Cognifire/src/com/jetbrains/edu/cognifire/highlighting/HighlighterManager.kt
+++ intellij-plugin/Edu-Cognifire/src/com/jetbrains/edu/cognifire/highlighting/HighlighterManager.kt
@@ -1,44 +1,55 @@
 package com.jetbrains.edu.cognifire.highlighting
 
-import com.intellij.openapi.components.Service
+import com.intellij.openapi.components.PersistentStateComponent
+import com.intellij.openapi.components.State
+import com.intellij.openapi.components.Storage
 import com.intellij.openapi.components.service
-import com.intellij.openapi.editor.markup.MarkupModel
 import com.intellij.openapi.project.Project
 import com.jetbrains.edu.cognifire.highlighting.highlighers.ProdeHighlighter
+import com.jetbrains.edu.cognifire.highlighting.highlighers.UncommitedChangesHighlighter
 import com.jetbrains.edu.learning.selectedEditor
 
 /**
  * Manages the highlighters in the editor. Allows adding and clearing highlighters for specific ranges and lines of code.
- *
- * @param project The project in which the currently edited file is located.
  */
-@Service(Service.Level.PROJECT)
-class HighlighterManager(private val project: Project) {
-  val highlighters = mutableListOf<ProdeHighlighter>()
+@State(name = "HighlighterManager", storages = [Storage("storage.xml")])
+class HighlighterManager : PersistentStateComponent<HighlighterManager> {
+  val highlighters = mutableMapOf<String, MutableList<ProdeHighlighter>>()
 
-  private val markupModel: MarkupModel?
-    get() = project.selectedEditor?.markupModel
-
-  fun clearAll() {
-    highlighters.forEach {
+  fun clearAll(id: String) {
+    highlighters[id]?.forEach {
       it.markupHighlighter?.dispose()
     }
-    highlighters.clear()
+    highlighters.remove(id)
   }
 
-  inline fun <reified T : ProdeHighlighter> clearProdeHighlighters() {
-    highlighters.filterIsInstance<T>().forEach {
+  inline fun<reified T: ProdeHighlighter> clearProdeHighlighters(id: String) {
+    highlighters[id]?.filterIsInstance<T>()?.forEach {
       it.markupHighlighter?.dispose()
     }
-    highlighters.removeAll { it is T }
+    highlighters[id]?.removeAll { it is T }
   }
 
-  fun addProdeHighlighter(highlighter: ProdeHighlighter) {
-    highlighter.addMarkupHighlighter(markupModel)
-    highlighters.add(highlighter)
+  fun addProdeHighlighter(highlighter: ProdeHighlighter, id: String, project: Project) {
+    highlighter.addMarkupHighlighter(project.selectedEditor?.markupModel)
+    highlighters.getOrPut(id) { mutableListOf() }.add(highlighter)
+  }
+
+  fun highlightAllUncommitedChanges(id: String, project: Project) {
+    highlighters[id]?.filterIsInstance<UncommitedChangesHighlighter>()?.forEach { highlighter ->
+      project.selectedEditor?.let {
+        highlighter.addHighlighter(it)
+      }
+    }
+  }
+
+  override fun getState() = this
+
+  override fun loadState(state: HighlighterManager) {
+    highlighters.putAll(state.highlighters)
   }
 
   companion object {
-    fun getInstance(project: Project): HighlighterManager = project.service()
+    fun getInstance(): HighlighterManager = service()
   }
 }
