26bd55ed621f377a3a88f48bcee7a671906542c6
Daniil.Karol <daniil.karol@jetbrains.com>
2024-12-17 13:42:08 +0100

    created class helper for custom breakpoints. Kotlin and Scala support added

--- intellij-plugin/features/ai-debugging-java/src/com/jetbrains/edu/aiDebugging/java/JAIDebuggerBreakpointType.kt
+++ intellij-plugin/features/ai-debugging-java/src/com/jetbrains/edu/aiDebugging/java/JAIDebuggerBreakpointType.kt
@@ -1,11 +1,11 @@
 package com.jetbrains.edu.aiDebugging.java
 
 import com.intellij.debugger.ui.breakpoints.JavaLineBreakpointType
+import com.jetbrains.edu.aiDebugging.core.breakpoint.BreakPointTypeHelper
 import com.jetbrains.edu.aiDebugging.core.messages.EduAIDebuggingCoreBundle
-import com.jetbrains.edu.aiDebugging.core.ui.AIDebuggingIcons
 import javax.swing.Icon
 
 class JAIDebuggerBreakpointType : JavaLineBreakpointType("java-line-ai", EduAIDebuggingCoreBundle.message("ai.debugger.java.breakpoint.description")) {
   // TODO change to real image
-  override fun getEnabledIcon(): Icon = AIDebuggingIcons.AIBug
+  override fun getEnabledIcon(): Icon = BreakPointTypeHelper.getEnabledIcon()
 }
