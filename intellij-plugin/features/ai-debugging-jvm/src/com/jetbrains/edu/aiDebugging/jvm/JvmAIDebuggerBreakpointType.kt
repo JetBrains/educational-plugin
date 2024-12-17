e36534bc6348be092f14a2b417e171e59ed2cfe6
Daniil.Karol <daniil.karol@jetbrains.com>
2024-12-17 15:50:07 +0100

    Changed java module to jvm

---
+++ intellij-plugin/features/ai-debugging-jvm/src/com/jetbrains/edu/aiDebugging/jvm/JvmAIDebuggerBreakpointType.kt
@@ -0,0 +1,11 @@
+package com.jetbrains.edu.aiDebugging.jvm
+
+import com.intellij.debugger.ui.breakpoints.JavaLineBreakpointType
+import com.jetbrains.edu.aiDebugging.core.messages.EduAIDebuggingCoreBundle
+import com.jetbrains.edu.aiDebugging.core.ui.AIDebuggingIcons
+import javax.swing.Icon
+
+class JvmAIDebuggerBreakpointType : JavaLineBreakpointType("java-line-ai", EduAIDebuggingCoreBundle.message("ai.debugger.java.breakpoint.description")) {
+  // TODO change to real image
+  override fun getEnabledIcon(): Icon = AIDebuggingIcons.AIBug
+}
