61485f808d5141fa9a3540dfde2ab1edcd97c0de
Daniil.Karol <daniil.karol@jetbrains.com>
2024-12-17 15:50:07 +0100

    Changed java module to jvm

--- intellij-plugin/features/ai-debugging-jvm/src/com/jetbrains/edu/aiDebugging/jvm/JvmBreakpointTypeManager.kt
+++ intellij-plugin/features/ai-debugging-jvm/src/com/jetbrains/edu/aiDebugging/jvm/JvmBreakpointTypeManager.kt
@@ -1,11 +1,11 @@
-package com.jetbrains.edu.aiDebugging.java
+package com.jetbrains.edu.aiDebugging.jvm
 
 import com.intellij.xdebugger.breakpoints.XBreakpointProperties
 import com.intellij.xdebugger.breakpoints.XLineBreakpointType
 import com.jetbrains.edu.aiDebugging.core.breakpoint.BreakpointTypeManager
 
 @Suppress("UNCHECKED_CAST")
-class JBreakpointTypeManager : BreakpointTypeManager {
+class JvmBreakpointTypeManager : BreakpointTypeManager {
   override fun getBreakPointType(): XLineBreakpointType<XBreakpointProperties<*>> =
-    JAIDebuggerBreakpointType() as XLineBreakpointType<XBreakpointProperties<*>>
+    JvmAIDebuggerBreakpointType() as XLineBreakpointType<XBreakpointProperties<*>>
 }
