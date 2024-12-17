bf7030a585d9c7546a0377dc2c11b89485278696
anchouls <anna.potriasaeva@jetbrains.com>
2024-12-17 16:53:08 +0100

    Remove unused dsl package check

--- intellij-plugin/Edu-Kotlin/src/com/jetbrains/edu/kotlin/cognifire/PromptRunLineMarkerContributor.kt
+++ intellij-plugin/Edu-Kotlin/src/com/jetbrains/edu/kotlin/cognifire/PromptRunLineMarkerContributor.kt
@@ -1,16 +1,15 @@
 package com.jetbrains.edu.kotlin.cognifire
 
 import com.intellij.execution.lineMarker.RunLineMarkerContributor
-import com.intellij.openapi.application.runReadAction
 import com.intellij.psi.PsiElement
 import com.intellij.psi.impl.source.tree.LeafPsiElement
 import com.intellij.psi.util.PsiTreeUtil
-import com.jetbrains.edu.cognifire.CognifireDslPackageCallChecker
 import com.jetbrains.edu.cognifire.actions.PromptExecutorAction
 import com.jetbrains.edu.cognifire.manager.PromptActionManager
 import com.jetbrains.edu.cognifire.manager.PromptCodeState
 import com.jetbrains.edu.cognifire.ui.CognifireIcons
 import com.jetbrains.edu.cognifire.utils.PROMPT
+import com.jetbrains.edu.cognifire.utils.isPromptBlock
 import com.jetbrains.edu.kotlin.messages.EduKotlinBundle
 import com.jetbrains.edu.learning.actions.EduActionUtils.getCurrentTask
 import com.jetbrains.edu.learning.taskToolWindow.ui.TaskToolWindowView
@@ -22,12 +21,7 @@ class PromptRunLineMarkerContributor : RunLineMarkerContributor() {
     val project = element.project
     val task = project.getCurrentTask() ?: return null
     val targetElement = element.parent.parent
-    if (element is LeafPsiElement &&
-        targetElement is KtCallExpression &&
-        element.text == PROMPT && runReadAction {
-          CognifireDslPackageCallChecker.isCallFromCognifireDslPackage(element.parent.parent, element.language)
-        }
-    ) {
+    if (element is LeafPsiElement && targetElement is KtCallExpression && element.text == PROMPT && targetElement.isPromptBlock()) {
       val function = PsiTreeUtil.getParentOfType(targetElement, KtNamedFunction::class.java) ?: return null
       val uniqueId = "${function.fqName}:${function.valueParameters.size}"
       val action = PromptExecutorAction(targetElement, uniqueId, task)
