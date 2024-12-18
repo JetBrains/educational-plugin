41dfad858dbe2c1ad010a26a984066a084270f23
anchouls <anna.potriasaeva@jetbrains.com>
2024-12-18 21:00:07 +0100

    Refactor highlighter application logic.
    Replaced `highlightAllUncommitedChanges` with a streamlined `applyHighlighters` method, enhancing clarity and reusability. Moved highlighter application logic to `ProdeHighlighter` to centralize responsibility and eliminate redundancy. Removed unused `UncommittedChangesHighlighter` logic for cleaner code.

--- intellij-plugin/Edu-Cognifire/src/com/jetbrains/edu/cognifire/highlighting/highlighers/ProdeHighlighter.kt
+++ intellij-plugin/Edu-Cognifire/src/com/jetbrains/edu/cognifire/highlighting/highlighers/ProdeHighlighter.kt
@@ -1,5 +1,6 @@
 package com.jetbrains.edu.cognifire.highlighting.highlighers
 
+import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.markup.MarkupModel
 import com.intellij.openapi.editor.markup.RangeHighlighter
 import com.intellij.openapi.editor.markup.TextAttributes
@@ -8,4 +9,18 @@ interface ProdeHighlighter {
   val attributes: TextAttributes
   var markupHighlighter: RangeHighlighter?
   fun addMarkupHighlighter(markupModel: MarkupModel?): RangeHighlighter?
+
+  fun applyExistingHighlighter(editor: Editor) {
+    markupHighlighter?.let {
+      it.apply {
+        editor.markupModel.addRangeHighlighter(
+          startOffset,
+          endOffset,
+          layer,
+          attributes,
+          targetArea
+        )
+      }
+    }
+  }
 }
