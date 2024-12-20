ad83391945c44895718b388259e2375d59f44492
Ilia Posov <ilia.posov@jetbrains.com>
2024-12-20 12:46:29 +0100

    EDU-7770: Do not show excluded folders. Only files may be excluded
    Only files are listed in the list of additional files, so there is currently no concept of an excluded folder.

--- intellij-plugin/educational-core/src/com/jetbrains/edu/coursecreator/projectView/CCNode.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/coursecreator/projectView/CCNode.kt
@@ -1,6 +1,5 @@
 package com.jetbrains.edu.coursecreator.projectView
 
-import com.intellij.ide.projectView.PresentationData
 import com.intellij.ide.projectView.ViewSettings
 import com.intellij.ide.projectView.impl.nodes.PsiDirectoryNode
 import com.intellij.ide.projectView.impl.nodes.PsiFileNode
@@ -9,15 +8,12 @@ import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiDirectory
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
-import com.intellij.ui.SimpleTextAttributes
 import com.jetbrains.edu.coursecreator.CCUtils.isCourseCreator
-import com.jetbrains.edu.coursecreator.courseignore.CourseIgnoreRules
 import com.jetbrains.edu.learning.EduUtilsKt
 import com.jetbrains.edu.learning.StudyTaskManager
 import com.jetbrains.edu.learning.courseFormat.ext.configurator
 import com.jetbrains.edu.learning.courseFormat.tasks.Task
 import com.jetbrains.edu.learning.isTestsFile
-import com.jetbrains.edu.learning.messages.EduCoreBundle
 import com.jetbrains.edu.learning.projectView.DirectoryNode
 
 class CCNode(
@@ -55,16 +51,6 @@ class CCNode(
       }
     }
     return null
-  }
-
-  override fun updateImpl(data: PresentationData) {
-    super.updateImpl(data)
-
-    // show node as excluded, if it is not inside a task folder, but is in the .courseignore
-    if (item == null && CourseIgnoreRules.loadFromCourseIgnoreFile(project).isIgnored(value.virtualFile)) {
-      data.clearText()
-      data.addText(EduCoreBundle.message("course.creator.course.view.excluded", value.name), SimpleTextAttributes.GRAY_ATTRIBUTES)
-    }
   }
 
   override fun createChildDirectoryNode(value: PsiDirectory): PsiDirectoryNode {
