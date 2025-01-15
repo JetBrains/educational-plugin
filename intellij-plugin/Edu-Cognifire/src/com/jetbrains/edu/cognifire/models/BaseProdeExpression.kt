0596c95eaf38757ff0dcdd7434eb7693f322eb79
Stanislav.Sandler <stanislav.sandler@jetbrains.com>
2025-01-15 15:18:29 +0000

    Refactor offset handling to use SmartPsiElementPointer.
    Replaced manual offset calculations and dynamic shifts with `SmartPsiElementPointer` for improved stability and maintainability. This change simplifies code logic, removes unnecessary offset-shifting methods, and ensures robust tracking of PSI elements.
    
    
    Merge-request: EDU-MR-4003
    Merged-by: Stanislav Sandler <stanislav.sandler@jetbrains.com>

--- intellij-plugin/Edu-Cognifire/src/com/jetbrains/edu/cognifire/models/BaseProdeExpression.kt
+++ intellij-plugin/Edu-Cognifire/src/com/jetbrains/edu/cognifire/models/BaseProdeExpression.kt
@@ -1,11 +1,7 @@
 package com.jetbrains.edu.cognifire.models
 
 interface BaseProdeExpression {
-  var dynamicStartOffset: Int
-  var dynamicEndOffset: Int
   val contentOffset: Int
   val startOffset: Int
   val endOffset: Int
-  fun shiftStartOffset(delta: Int)
-  fun shiftEndOffset(delta: Int)
 }
--- intellij-plugin/Edu-Kotlin/src/com/jetbrains/edu/kotlin/cognifire/psi/KtCodeExpressionParser.kt
+++ intellij-plugin/Edu-Kotlin/src/com/jetbrains/edu/kotlin/cognifire/psi/KtCodeExpressionParser.kt
@@ -1,21 +1,19 @@
 package com.jetbrains.edu.kotlin.cognifire.psi
 
 import com.intellij.psi.PsiElement
-import com.jetbrains.edu.cognifire.parsers.CodeExpressionParser
+import com.intellij.psi.SmartPointerManager
 import com.jetbrains.edu.cognifire.models.CodeExpression
-import org.jetbrains.kotlin.psi.psiUtil.endOffset
-import org.jetbrains.kotlin.psi.psiUtil.startOffset
+import com.jetbrains.edu.cognifire.parsers.CodeExpressionParser
 
 class KtCodeExpressionParser : CodeExpressionParser {
   override fun getExpression(element: PsiElement): CodeExpression? {
-    val codeElement = ElementSearch.findCodeElement(element) { it.nextSibling } ?: return null
-    val code = ElementSearch.findCodeElement(element) { it.nextSibling }?.lambdaArguments?.firstOrNull()?.getLambdaExpression()
-      ?.bodyExpression ?: return null
+    val codeExpressionElement = ElementSearch.findCodeElement(element) { it.nextSibling } ?: return null
+    val codeContentElement = codeExpressionElement.lambdaArguments.firstOrNull()?.getLambdaExpression()
+                               ?.bodyExpression ?: return null
     return CodeExpression(
-      code.text,
-      code.textOffset,
-      codeElement.startOffset,
-      codeElement.endOffset
+      SmartPointerManager.createPointer(codeExpressionElement),
+      SmartPointerManager.createPointer(codeContentElement),
+      codeContentElement.text
     )
   }
 }
