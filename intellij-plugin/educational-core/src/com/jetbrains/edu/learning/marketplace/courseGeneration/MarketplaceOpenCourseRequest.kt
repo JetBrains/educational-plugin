678f538e5f58eec41531b96ee45c530f7a0efc05
Ilia Posov <ilia.posov@jetbrains.com>
2025-01-18 09:45:41 +0100

    EDU-7774: Support study item id instead of task id

--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/marketplace/courseGeneration/MarketplaceOpenCourseRequest.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/marketplace/courseGeneration/MarketplaceOpenCourseRequest.kt
@@ -4,8 +4,8 @@ import com.jetbrains.edu.learning.courseGeneration.OpenInIdeRequest
 import com.jetbrains.edu.learning.marketplace.lti.LTISettingsDTO
 
 /**
- * If `taskId` is not present in the request, its value should be set to `-1`
+ * If [studyItemId] is not present in the request, its value should be set to `-1`
  */
-class MarketplaceOpenCourseRequest(val courseId: Int, val taskId: Int, val ltiSettingsDTO: LTISettingsDTO? = null) : OpenInIdeRequest {
-  override fun toString(): String = "courseId=$courseId taskId=$taskId ltiLaunchId=${ltiSettingsDTO?.launchId}"
+class MarketplaceOpenCourseRequest(val courseId: Int, val studyItemId: Int, val ltiSettingsDTO: LTISettingsDTO? = null) : OpenInIdeRequest {
+  override fun toString(): String = "courseId=$courseId studyItemId=$studyItemId ltiLaunchId=${ltiSettingsDTO?.launchId}"
 }
