16cbc11e084d61061c3aa3b05a02cf0e98bfd9cb
Mark Prudnikov <mark.prudnikov@jetbrains.com>
2025-01-22 00:45:37 +0100

    EDU-7486: Support Nullable Type in `FunctionParameter`

--- intellij-plugin/features/ai-hints-core/src/com/jetbrains/edu/aiHints/core/context/FunctionParameter.kt
+++ intellij-plugin/features/ai-hints-core/src/com/jetbrains/edu/aiHints/core/context/FunctionParameter.kt
@@ -2,7 +2,7 @@ package com.jetbrains.edu.aiHints.core.context
 
 data class FunctionParameter(
   val name: String,
-  val type: String
+  val type: String?
 ) {
-  override fun toString() = "$name: $type"
+  override fun toString(): String = if (type != null) "$name: $type" else name
 }
