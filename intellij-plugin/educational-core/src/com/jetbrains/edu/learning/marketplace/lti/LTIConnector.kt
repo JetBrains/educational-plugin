8def7f14d0e34308e16e1f97bf735df675f7c98a
Ilia Posov <ilia.posov@jetbrains.com>
2025-01-21 18:27:16 +0100

    EDU-7774: Extract LTI.xml with idea-plugin settings

--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/marketplace/lti/LTIConnector.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/marketplace/lti/LTIConnector.kt
@@ -1,35 +1,13 @@
 package com.jetbrains.edu.learning.marketplace.lti
 
-import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.service
-import com.jetbrains.edu.learning.network.createRetrofitBuilder
-import com.jetbrains.edu.learning.network.executeCall
-import com.jetbrains.edu.learning.onError
-import okhttp3.ConnectionPool
 
-@Service(Service.Level.APP)
-class LTIConnector {
-
-  private val connectionPool: ConnectionPool = ConnectionPool()
+interface LTIConnector {
 
   /**
    * returns error message or null, if request was successful
    */
-  fun postTaskChecked(onlineService: LTIOnlineService, launchId: String, courseEduId: Int, taskEduId: Int, solved: Boolean): PostTaskSolvedStatus {
-    val retrofit = createRetrofitBuilder(onlineService.serviceURL, connectionPool, LTIAuthBundle.value("ltiServiceToken")).build()
-    val ltiEndpoints = retrofit.create(LTIEndpoints::class.java)
-
-    val response = ltiEndpoints.reportTaskSolved(launchId, courseEduId, taskEduId, solved).executeCall(omitErrors = true).onError {
-      return ConnectionError(it)
-    }
-
-    return when (response.code()) {
-      200 -> NoLineItem
-      204 -> Success
-      404 -> UnknownLaunchId
-      else -> return ServerError // 500 or other statuses
-    }
-  }
+  fun postTaskChecked(onlineService: LTIOnlineService, launchId: String, courseEduId: Int, taskEduId: Int, solved: Boolean): PostTaskSolvedStatus
 
   companion object {
     fun getInstance(): LTIConnector = service()
