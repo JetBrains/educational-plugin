2771b2c02d3bde2d5bf4df3a1d4f59a9af5b562e
Anna Potriasaeva <Anna.Potriasaeva@jetbrains.com>
2025-01-27 14:31:10 +0000

    fixed MR comments, added tests, docs

--- intellij-plugin/features/ai-debugging-kotlin/src/com/jetbrains/edu/aiDebugging/kotlin/slice/KSliceManager.kt
+++ intellij-plugin/features/ai-debugging-kotlin/src/com/jetbrains/edu/aiDebugging/kotlin/slice/KSliceManager.kt
@@ -3,13 +3,17 @@ package com.jetbrains.edu.aiDebugging.kotlin.slice
 import com.intellij.openapi.editor.Document
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
+import com.intellij.psi.util.PsiTreeUtil
 import com.jetbrains.edu.aiDebugging.core.slicing.SliceManager
+import org.jetbrains.kotlin.psi.KtFunction
 
 class KSliceManager : SliceManager {
 
   @Suppress("unused")
   override fun processSlice(psiElement: PsiElement, document: Document, psiFile: PsiFile) {
-    val controlDependency = ControlDependency(psiElement).dependenciesForward
+    // TODO make logic for finding KtFunction
+    val dependencies = PsiTreeUtil.findChildrenOfType(psiElement, KtFunction::class.java)
+      .map { FunctionControlDependency(it) }
     // TODO()
   }
 
