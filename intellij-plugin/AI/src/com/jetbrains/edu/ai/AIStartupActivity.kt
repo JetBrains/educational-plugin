c9c14d8ffe89bbab2ecd49ae58a9b5d6de82ed20
Gleb Oborin <gleb.oborin@jetbrains.com>
2025-02-02 22:22:41 +0100

    Implement TermsUpdateChecker

--- intellij-plugin/AI/src/com/jetbrains/edu/ai/AIStartupActivity.kt
+++ intellij-plugin/AI/src/com/jetbrains/edu/ai/AIStartupActivity.kt
@@ -1,18 +1,18 @@
-package com.jetbrains.edu.ai.translation
+package com.jetbrains.edu.ai
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
-import com.jetbrains.edu.ai.translation.updater.TranslationUpdateChecker.Companion.launchTranslationUpdateChecker
+import com.jetbrains.edu.ai.AIServiceUpdateChecker.Companion.launchUpdateChecker
 import com.jetbrains.edu.learning.EduUtilsKt.isStudentProject
 import com.jetbrains.edu.learning.course
 import com.jetbrains.edu.learning.courseFormat.EduCourse
 
-class TranslationStartupActivity : ProjectActivity {
+class AIStartupActivity : ProjectActivity {
   override suspend fun execute(project: Project) {
     if (!project.isStudentProject()) return
 
     val course = project.course as? EduCourse ?: return
     if (!course.isMarketplaceRemote) return
-    project.launchTranslationUpdateChecker(course)
+    project.launchUpdateChecker(course)
   }
 }
