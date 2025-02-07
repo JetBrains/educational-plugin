2e11f33d43e664cd072a49fcb4208875dfb56004
Aleksey Rostovskiy <aleksey.rostovskiy@jetbrains.com>
2025-02-07 22:28:18 +0100

    Refactor template folder structure and update providers.
    Centralized template folder paths in providers to ensure consistent usage and improve maintainability.
    Adjusted resource folder structures accordingly for better organization and clarity.

--- intellij-plugin/features/ai-learner-feedback/src/com/jetbrains/edu/ai/learner/feedback/prompts/AIFeedbackPromptProvider.kt
+++ intellij-plugin/features/ai-learner-feedback/src/com/jetbrains/edu/ai/learner/feedback/prompts/AIFeedbackPromptProvider.kt
@@ -3,7 +3,9 @@ package com.jetbrains.edu.ai.learner.feedback.prompts
 import com.jetbrains.edu.ai.clippy.assistant.settings.AIClippyProperties
 import com.jetbrains.educational.ml.core.prompt.PromptProvider
 
-object AIFeedbackPromptProvider : PromptProvider() {
+private const val TEMPLATES_FOLDER: String = "/templates/learnerFeedback"
+
+object AIFeedbackPromptProvider : PromptProvider(TEMPLATES_FOLDER) {
   fun buildSystemPrompt(): String = AIFeedbackPromptTemplate.FEEDBACK_SYSTEM_PROMPT.process()
 
   fun buildUserPrompt(clippyProperties: AIClippyProperties, successful: Boolean): String =
