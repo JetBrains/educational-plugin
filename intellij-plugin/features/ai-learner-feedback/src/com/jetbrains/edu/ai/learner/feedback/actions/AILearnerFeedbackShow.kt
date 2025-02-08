fcff4db3af52c25f8ebbe90d298e4b2a33c27a4c
Aleksey Rostovskiy <aleksey.rostovskiy@jetbrains.com>
2025-02-08 01:09:32 +0100

    Remove unused language variable from AILearnerFeedbackShow

--- intellij-plugin/features/ai-learner-feedback/src/com/jetbrains/edu/ai/learner/feedback/actions/AILearnerFeedbackShow.kt
+++ intellij-plugin/features/ai-learner-feedback/src/com/jetbrains/edu/ai/learner/feedback/actions/AILearnerFeedbackShow.kt
@@ -16,7 +16,6 @@ class AILearnerFeedbackShow : DumbAwareAction() {
     val project = e.project ?: return
     currentThreadCoroutineScope().launch {
       val clippyProperties = AIClippySettings.getInstance().getClippySettings()
-      val language = AIClippySettings.getInstance().language
       val feedback = AILearnerFeedbackGrazieClient.generateFeedback(clippyProperties, true).also { println(it) }
       AIClippyService.getInstance(project).showWithText(feedback)
     }
