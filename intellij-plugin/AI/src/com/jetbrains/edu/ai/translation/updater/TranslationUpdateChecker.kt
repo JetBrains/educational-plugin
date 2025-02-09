21fcaca29e7aa408ab2bb3b9c7c5543fcd9ba3b6
Gleb Oborin <gleb.oborin@jetbrains.com>
2025-02-09 23:25:01 +0100

    Separate some parts of TranslationUpdateChecker into a base class

--- intellij-plugin/AI/src/com/jetbrains/edu/ai/translation/updater/TranslationUpdateChecker.kt
+++ intellij-plugin/AI/src/com/jetbrains/edu/ai/translation/updater/TranslationUpdateChecker.kt
@@ -4,7 +4,6 @@ import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.util.registry.Registry
 import com.jetbrains.edu.ai.messages.EduAIBundle
 import com.jetbrains.edu.ai.translation.TranslationLoader
 import com.jetbrains.edu.ai.translation.connector.TranslationServiceConnector
@@ -15,42 +14,15 @@ import com.jetbrains.edu.learning.ai.TranslationProperties
 import com.jetbrains.edu.learning.courseFormat.EduCourse
 import com.jetbrains.edu.learning.messages.EduCoreBundle
 import com.jetbrains.edu.learning.onError
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import java.util.concurrent.atomic.AtomicBoolean
-import kotlin.time.Duration
-import kotlin.time.Duration.Companion.seconds
 
 @Service(Service.Level.PROJECT)
-class TranslationUpdateChecker(private val project: Project, private val scope: CoroutineScope) {
-  private val lock = AtomicBoolean(false)
-
-  private val isRunning: Boolean
-    get() = lock.get()
-
-  private val checkInterval: Duration
-    get() {
-      // Default value is 3600 seconds (1 hour), set in AI.xml
-      return Registry.intValue(REGISTRY_KEY).seconds
-    }
-
-  private fun launch(course: EduCourse) {
-    if (lock.compareAndSet(false, true)) {
-      scope.launch {
-        while (true) {
-          val translationProperties = TranslationProjectSettings.getInstance(project).translationProperties.value
-          if (translationProperties != null && isTranslationOutdated(course, translationProperties)) {
-            showUpdateAvailableNotification {
-              TranslationLoader.getInstance(project).updateTranslation(course, translationProperties)
-            }
-          }
-          delay(checkInterval)
-        }
+class TranslationUpdateChecker(private val project: Project) {
+  suspend fun checkUpdate(course: EduCourse) {
+    val translationProperties = TranslationProjectSettings.getInstance(project).translationProperties.value
+    if (translationProperties != null && isTranslationOutdated(course, translationProperties)) {
+      showUpdateAvailableNotification {
+        TranslationLoader.getInstance(project).updateTranslation(course, translationProperties)
       }
-    }
-    else {
-      LOG.error("Translation update checker is already running")
     }
   }
 
@@ -80,13 +52,6 @@ class TranslationUpdateChecker(private val project: Project, private val scope: 
   companion object {
     private val LOG = Logger.getInstance(TranslationUpdateChecker::class.java)
 
-    private const val REGISTRY_KEY: String = "edu.ai.translation.update.check.interval"
-
-    fun Project.launchTranslationUpdateChecker(course: EduCourse) {
-      val checker = service<TranslationUpdateChecker>()
-      if (!checker.isRunning) {
-        checker.launch(course)
-      }
-    }
+    fun getInstance(project: Project): TranslationUpdateChecker = project.service()
   }
 }
