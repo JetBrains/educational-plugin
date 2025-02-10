391c015473a2566c9bdc1cbdd175b3dbeb7500a6
Gleb Oborin <gleb.oborin@jetbrains.com>
2025-02-10 03:10:52 +0100

    Use language code instead of TranslationLanguage and listen it's updates

--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/ai/terms/TermsProjectSettings.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/ai/terms/TermsProjectSettings.kt
@@ -8,7 +8,6 @@ import com.intellij.util.xmlb.annotations.XCollection
 import com.jetbrains.edu.learning.EduTestAware
 import com.jetbrains.edu.learning.courseFormat.tasks.Task
 import com.jetbrains.edu.learning.ai.terms.TermsProjectSettings.TermsProjectState
-import com.jetbrains.educational.core.format.enum.TranslationLanguage
 import com.jetbrains.educational.terms.format.Term
 import com.jetbrains.educational.terms.format.domain.TermsVersion
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -23,8 +22,8 @@ import kotlin.collections.set
 class TermsProjectSettings : PersistentStateComponent<TermsProjectState>, EduTestAware {
   private val _termsProperties = MutableStateFlow<TermsProperties?>(null)
   val termsProperties = _termsProperties.asStateFlow()
-  private val versionsByLanguage = ConcurrentHashMap<TranslationLanguage, TermsVersion>()
-  private val termsByLanguage = ConcurrentHashMap<TranslationLanguage, Map<Int, List<Term>>>()
+  private val versionsByLanguage = ConcurrentHashMap<String, TermsVersion>()
+  private val termsByLanguage = ConcurrentHashMap<String, Map<Int, List<Term>>>()
 
   fun getTaskTerms(task: Task): List<Term>? = termsProperties.value?.terms?.get(task.id)
 
@@ -33,16 +32,16 @@ class TermsProjectSettings : PersistentStateComponent<TermsProjectState>, EduTes
       _termsProperties.value = null
       return
     }
-    val language = termsProperties.language
+    val language = termsProperties.languageCode
     termsByLanguage[language] = termsProperties.terms
     versionsByLanguage[language] = termsProperties.version
     _termsProperties.value = termsProperties
   }
 
-  fun getTermsByLanguage(language: TranslationLanguage): TermsProperties? {
-    val terms = termsByLanguage[language] ?: return null
-    val version = versionsByLanguage[language] ?: return null
-    return TermsProperties(language, terms, version)
+  fun getTermsByLanguage(languageCode: String): TermsProperties? {
+    val terms = termsByLanguage[languageCode] ?: return null
+    val version = versionsByLanguage[languageCode] ?: return null
+    return TermsProperties(languageCode, terms, version)
   }
 
   override fun getState(): TermsProjectState {
@@ -53,7 +52,7 @@ class TermsProjectSettings : PersistentStateComponent<TermsProjectState>, EduTes
       }
     }
     state.termsVersions = versionsByLanguage.mapValuesTo(mutableMapOf()) { it.value.value }
-    state.currentTermsLanguage = termsProperties.value?.language
+    state.currentTermsLanguage = termsProperties.value?.languageCode
     return state
   }
 
@@ -98,18 +97,22 @@ class TermsProjectSettings : PersistentStateComponent<TermsProjectState>, EduTes
 
   class TermsProjectState : BaseState() {
     @get:XCollection(style = XCollection.Style.v2)
-    var currentTermsLanguage by enum<TranslationLanguage>()
+    var currentTermsLanguage by string()
 
     @get:XCollection(style = XCollection.Style.v2)
-    var terms by map<TranslationLanguage, Map<Int, List<StoredTerm>>>()
+    var terms by map<String, Map<Int, List<StoredTerm>>>()
 
     @get:XCollection(style = XCollection.Style.v2)
-    var termsVersions by map<TranslationLanguage, Int>()
+    var termsVersions by map<String, Int>()
   }
 
   companion object {
     fun getInstance(project: Project): TermsProjectSettings = project.service()
 
     fun areCourseTermsLoaded(project: Project): Boolean = getInstance(project).termsProperties.value != null
+
+    fun areCourseTermsLoaded(project: Project, languageCode: String): Boolean {
+      return getInstance(project).termsProperties.value?.languageCode == languageCode
+    }
   }
 }
