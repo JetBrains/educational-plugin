e5b39b73ea12a11ee65753dbc0e0d75fe44c4ced
Gleb Oborin <gleb.oborin@jetbrains.com>
2025-02-10 16:13:59 +0100

    fixup! Use separate notifications for terms

--- intellij-plugin/AI/src/com/jetbrains/edu/ai/terms/ui/AITermsNotificationManager.kt
+++ intellij-plugin/AI/src/com/jetbrains/edu/ai/terms/ui/AITermsNotificationManager.kt
@@ -2,21 +2,20 @@ package com.jetbrains.edu.ai.terms.ui
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.NlsContexts.NotificationContent
-import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowManager
 import com.intellij.ui.EditorNotificationPanel
 import com.jetbrains.edu.ai.ui.AINotification.ActionLabel
+import com.jetbrains.edu.ai.ui.AINotificationManager
 import com.jetbrains.edu.learning.taskToolWindow.ui.TaskToolWindowFactory
-import javax.swing.BoxLayout
 
-object AITermsNotificationManager {
+object AITermsNotificationManager : AINotificationManager<AITermsNotification>() {
   fun showInfoNotification(project: Project, message: @NotificationContent String, actionLabel: ActionLabel? = null) {
     val toolWindow = ToolWindowManager.getInstance(project).getToolWindow(TaskToolWindowFactory.STUDY_TOOL_WINDOW) ?: return
     val notification = AITermsNotification(EditorNotificationPanel.Status.Info, message, toolWindow.component)
     if (actionLabel != null) {
       notification.addActionLabel(actionLabel)
     }
-    closeExistingNotifications(toolWindow)
+    closeExistingNotifications<AITermsNotification>(toolWindow)
     showNotification(toolWindow, notification)
   }
 
@@ -26,21 +25,7 @@ object AITermsNotificationManager {
     if (actionLabel != null) {
       notification.addActionLabel(actionLabel)
     }
-    closeExistingNotifications(toolWindow)
+    closeExistingNotifications<AITermsNotification>(toolWindow)
     showNotification(toolWindow, notification)
-  }
-
-  fun closeExistingNotifications(project: Project) {
-    val toolWindow = ToolWindowManager.getInstance(project).getToolWindow(TaskToolWindowFactory.STUDY_TOOL_WINDOW) ?: return
-    closeExistingNotifications(toolWindow)
-  }
-
-  private fun showNotification(toolWindow: ToolWindow, notification: AITermsNotification) {
-    toolWindow.component.add(notification, BoxLayout.Y_AXIS)
-  }
-
-  private fun closeExistingNotifications(toolWindow: ToolWindow) {
-    val existingNotifications = toolWindow.component.components.filterIsInstance<AITermsNotification>()
-    existingNotifications.forEach { it.close() }
   }
 }
--- intellij-plugin/AI/src/com/jetbrains/edu/ai/translation/ui/AITranslationNotificationManager.kt
+++ intellij-plugin/AI/src/com/jetbrains/edu/ai/translation/ui/AITranslationNotificationManager.kt
@@ -2,22 +2,20 @@ package com.jetbrains.edu.ai.translation.ui
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.NlsContexts.NotificationContent
-import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowManager
 import com.intellij.ui.EditorNotificationPanel
 import com.jetbrains.edu.ai.ui.AINotification.ActionLabel
+import com.jetbrains.edu.ai.ui.AINotificationManager
 import com.jetbrains.edu.learning.taskToolWindow.ui.TaskToolWindowFactory
-import javax.swing.BoxLayout
 
-object AITranslationNotificationManager {
+object AITranslationNotificationManager : AINotificationManager<AITranslationNotification>() {
   fun showInfoNotification(project: Project, message: @NotificationContent String, actionLabel: ActionLabel? = null) {
     val toolWindow = ToolWindowManager.getInstance(project).getToolWindow(TaskToolWindowFactory.STUDY_TOOL_WINDOW) ?: return
     val notification = AITranslationNotification(EditorNotificationPanel.Status.Info, message, toolWindow.component)
     if (actionLabel != null) {
       notification.addActionLabel(actionLabel)
     }
-    closeExistingNotifications(toolWindow)
-    showNotification(toolWindow, notification)
+    closeExistingNotifications<AITranslationNotification>(toolWindow)
   }
 
   fun showErrorNotification(project: Project, message: @NotificationContent String, actionLabel: ActionLabel? = null) {
@@ -26,21 +24,7 @@ object AITranslationNotificationManager {
     if (actionLabel != null) {
       notification.addActionLabel(actionLabel)
     }
-    closeExistingNotifications(toolWindow)
+    closeExistingNotifications<AITranslationNotification>(toolWindow)
     showNotification(toolWindow, notification)
-  }
-
-  fun closeExistingNotifications(project: Project) {
-    val toolWindow = ToolWindowManager.getInstance(project).getToolWindow(TaskToolWindowFactory.STUDY_TOOL_WINDOW) ?: return
-    closeExistingNotifications(toolWindow)
-  }
-
-  private fun showNotification(toolWindow: ToolWindow, notification: AITranslationNotification) {
-    toolWindow.component.add(notification, BoxLayout.Y_AXIS)
-  }
-
-  private fun closeExistingNotifications(toolWindow: ToolWindow) {
-    val existingNotifications = toolWindow.component.components.filterIsInstance<AITranslationNotification>()
-    existingNotifications.forEach { it.close() }
   }
 }
