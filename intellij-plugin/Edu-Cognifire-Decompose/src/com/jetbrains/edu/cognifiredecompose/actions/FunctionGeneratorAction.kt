6bed40c4de0453dc9df9cecb15b2dad1a4fe2964
Arnav.Chopra <arnav.chopra@jetbrains.com>
2025-02-16 14:04:58 +0100

    removes runBackgroundableTask and replaces invokeLater

--- intellij-plugin/Edu-Cognifire-Decompose/src/com/jetbrains/edu/cognifiredecompose/actions/FunctionGeneratorAction.kt
+++ intellij-plugin/Edu-Cognifire-Decompose/src/com/jetbrains/edu/cognifiredecompose/actions/FunctionGeneratorAction.kt
@@ -2,28 +2,21 @@ package com.jetbrains.edu.cognifiredecompose.actions
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.application.invokeLater
-import com.intellij.openapi.progress.runBackgroundableTask
+import com.intellij.openapi.application.runWriteAction
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
-import com.jetbrains.edu.cognifiredecompose.messages.EduCognifireDecomposeBundle
 import com.jetbrains.edu.cognifiredecompose.writers.FunctionWriter
 
 class FunctionGeneratorAction(private val element: PsiElement) : AnAction() {
   override fun actionPerformed(e: AnActionEvent) {
     val project = e.project ?: error("Project was not found")
-    executeAction(project)
+    handleNewFunction(project)
   }
-
-  private fun executeAction(project: Project) = runBackgroundableTask(
-    EduCognifireDecomposeBundle.message("action.progress.bar.message"),
-    project
-  ) { _ -> handleNewFunction(project) }
 
   private fun handleNewFunction(
     project: Project
   ) {
-      invokeLater {
+      runWriteAction {
         FunctionWriter.addFunction(project, element, element.language)
       }
     }
