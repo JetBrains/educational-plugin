6fae9e56191a1ca9ef19218cedfebf9ea78875ed
anchouls <anna.potriasaeva@jetbrains.com>
2025-02-18 10:58:39 +0000

    Refactor tutorial generation to generate a section or a lesson depending on the course structure.

---
+++ intellij-plugin/Edu-Kotlin/resources/cognifire/Tutorial/PromptBlock/Tests.kt
@@ -0,0 +1,32 @@
+import org.jetbrains.academy.test.system.kotlin.test.BaseIjTestClass
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import java.io.File
+
+
+class Test : BaseIjTestClass() {
+    companion object {
+
+        private lateinit var sourceText: String
+
+        @JvmStatic
+        @BeforeAll
+        fun initialize() {
+            val taskDirectoryPath = System.getProperty("user.dir")
+            val sourceCodeFile =
+                File("$taskDirectoryPath/src/jetbrains/course/tutorial/Main.kt")
+            sourceText = sourceCodeFile.readText()
+        }
+    }
+
+    @Test
+    fun testPromptBlock() {
+        setUp()
+        myFixture.configureByText("Task.kt", sourceText)
+
+        Assertions.assertNotNull(getMethodCallArguments("prompt")) {
+            "Please, create a prompt block."
+        }
+    }
+}
---
+++ intellij-plugin/Edu-Kotlin/resources/cognifire/Tutorial/Run/Main.kt
@@ -0,0 +1,28 @@
+package jetbrains.course.tutorial
+
+import org.jetbrains.academy.cognifire.dsl.*
+
+fun calculateDogAgeInDogYears() {
+  prompt("""
+        Get the user input and save the result to a variable named `humanYears`.
+        Call the function `verifyHumanYearsInput` with `humanYears`. 
+        If the function returns true, set `dogYears` equal to `humanYears` multiplied by 7 and print the message "Your dog's age in dog years is: dogYears".
+    """)
+  code {
+    val humanYears = readlnOrNull()?.toIntOrNull()
+    if (humanYears != null && verifyHumanYearsInput(humanYears)) {
+      val dogYears = humanYears * 7
+      println("Your dog's age in dog years is: $dogYears")
+    } else {
+      TODO("Specify what to do if the function `verifyHumanYearsInput` returns false")
+    }
+  }
+}
+
+fun verifyHumanYearsInput(humanYears: Int): Boolean {
+  TODO("Not implemented yet")
+}
+
+fun main() {
+  calculateDogAgeInDogYears()
+}
