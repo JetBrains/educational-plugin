741da4d65232536d18b2ce2e36ab55e69bf3928c
anchouls <anna.potriasaeva@jetbrains.com>
2025-02-18 10:58:39 +0000

    Refactor tutorial generation to generate a section or a lesson depending on the course structure.

---
+++ intellij-plugin/Edu-Kotlin/resources/cognifire/Tutorial/Function/Main.kt
@@ -0,0 +1,13 @@
+package jetbrains.course.tutorial
+
+fun calculateDogAgeInDogYears() {
+  TODO("Not implemented yet")
+}
+
+fun verifyHumanYearsInput(humanYears: Int): Boolean {
+  TODO("Not implemented yet")
+}
+
+fun main() {
+  calculateDogAgeInDogYears()
+}
---
+++ intellij-plugin/Edu-Kotlin/resources/cognifire/Tutorial/FunctionCall/Main.kt
@@ -0,0 +1,18 @@
+package jetbrains.course.tutorial
+
+import org.jetbrains.academy.cognifire.dsl.*
+
+fun calculateDogAgeInDogYears() {
+  prompt("""
+        Get the user input and save the result to a variable named `humanYears`.
+        Call the function `verifyHumanYearsInput` with `humanYears`. 
+    """)
+}
+
+fun verifyHumanYearsInput(humanYears: Int): Boolean {
+  TODO("Not implemented yet")
+}
+
+fun main() {
+  calculateDogAgeInDogYears()
+}
