9be9826cb538bf3d1ba126e60d448542ab05288e
José Carlos Serize Portela <94478868+joseserize0222@users.noreply.github.com>
2025-02-20 15:05:39 +0000

    Refactored the method, no need to traverse the whole psi tree
    Merge-request: EDU-MR-4108
    Merged-by: José Carlos Serize Portela <jose.serize.portela@jetbrains.com>

--- intellij-plugin/Edu-Kotlin/src/com/jetbrains/edu/kotlin/cognifire/psi/KtGeneratedCodeParser.kt
+++ intellij-plugin/Edu-Kotlin/src/com/jetbrains/edu/kotlin/cognifire/psi/KtGeneratedCodeParser.kt
@@ -2,7 +2,7 @@ package com.jetbrains.edu.kotlin.cognifire.psi
 
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiRecursiveElementVisitor
+import com.intellij.psi.util.descendantsOfType
 import com.jetbrains.edu.cognifire.parsers.GeneratedCodeParser
 import com.jetbrains.edu.cognifire.models.FunctionSignature
 import com.jetbrains.edu.kotlin.cognifire.utils.createPsiFile
@@ -11,13 +11,6 @@ import com.jetbrains.edu.kotlin.cognifire.utils.getTodoMessageOrNull
 class KtGeneratedCodeParser : GeneratedCodeParser {
   override fun hasErrors(project: Project, generatedCode: String, functionSignature: FunctionSignature): Boolean {
     val file = createPsiFile(project, functionSignature.toString(), generatedCode)
-    val todoStrings = mutableListOf<String>()
-    file.accept(object : PsiRecursiveElementVisitor() {
-      override fun visitElement(element: PsiElement) {
-        element.getTodoMessageOrNull()?.let { todoStrings.add(it) }
-        super.visitElement(element)
-      }
-    })
-    return todoStrings.isNotEmpty()
+    return file.descendantsOfType<PsiElement>().any { it.getTodoMessageOrNull() != null }
   }
 }
