f33003e9d90db0fe88557b0e8df9e8e7dbf9abfe
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2025-03-05 13:54:44 +0100

    EDU-7963: Properly allow access to json schema files in tests
    It's possible to use resources from compiled jars instead of direct files on disk in tests. It wasn't supported before. As a result, tests failed

--- intellij-plugin/code-insight/yaml/testSrc/com/jetbrains/edu/yaml/YamlCodeInsightTest.kt
+++ intellij-plugin/code-insight/yaml/testSrc/com/jetbrains/edu/yaml/YamlCodeInsightTest.kt
@@ -4,6 +4,9 @@ import com.intellij.openapi.application.runWriteAction
 import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.vfs.newvfs.impl.VfsRootAccess
 import com.intellij.testFramework.EditorTestUtil
+import com.intellij.util.io.URLUtil
+import com.intellij.util.io.URLUtil.FILE_PROTOCOL
+import com.intellij.util.io.URLUtil.JAR_PROTOCOL
 import com.jetbrains.edu.learning.EduTestCase
 import com.jetbrains.edu.learning.courseDir
 import com.jetbrains.edu.learning.courseFormat.StudyItem
@@ -19,9 +22,18 @@ abstract class YamlCodeInsightTest : EduTestCase() {
     val factory = JsonSchemaProviderFactory.EP_NAME.findExtension(EduYamlSchemaProviderFactory::class.java)
     factory?.getProviders(project)?.forEach { provider ->
       val schemaResourcePath = provider.getSchemaResourcePath()
-      val uri = EduYamlSchemaProviderFactory::class.java.getResource(schemaResourcePath).toURI()
-      val absolutePath = Paths.get(uri).toAbsolutePath().toString()
-      VfsRootAccess.allowRootAccess(testRootDisposable, absolutePath)
+      val url = EduYamlSchemaProviderFactory::class.java.getResource(schemaResourcePath)?.toString() ?: return@forEach
+      val file = when {
+        url.startsWith(FILE_PROTOCOL) -> Paths.get(url)
+        url.startsWith(JAR_PROTOCOL) -> {
+          // If we are touching a file inside jar, it's ok to allow touching the whole jar file in tests
+          val jarPath = URLUtil.splitJarUrl(url)?.first ?: return@forEach
+          Paths.get(jarPath)
+        }
+        else -> return@forEach
+      }
+      val path = file.toAbsolutePath().toString()
+      VfsRootAccess.allowRootAccess(testRootDisposable, path)
     }
   }
 
