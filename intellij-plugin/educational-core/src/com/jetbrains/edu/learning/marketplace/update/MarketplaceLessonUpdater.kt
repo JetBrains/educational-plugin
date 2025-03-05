84f5da435608e3d84611defc1aef1f1379da9010
Ilia Posov <ilia.posov@jetbrains.com>
2025-03-05 13:17:33 +0100

    EDU-6560: Split TaskUpdater into TaskUpdater and FrameworkTaskUpdater
    Both classes now have the common base class TaskUpdaterBase

--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/marketplace/update/MarketplaceLessonUpdater.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/marketplace/update/MarketplaceLessonUpdater.kt
@@ -1,8 +1,10 @@
 package com.jetbrains.edu.learning.marketplace.update
 
 import com.intellij.openapi.project.Project
+import com.jetbrains.edu.learning.courseFormat.FrameworkLesson
 import com.jetbrains.edu.learning.courseFormat.Lesson
 import com.jetbrains.edu.learning.courseFormat.LessonContainer
+import com.jetbrains.edu.learning.update.FrameworkTaskUpdater
 import com.jetbrains.edu.learning.update.LessonUpdater
 import com.jetbrains.edu.learning.update.MarketplaceItemUpdater
 import com.jetbrains.edu.learning.update.TaskUpdater
@@ -12,4 +14,5 @@ class MarketplaceLessonUpdater(
   container: LessonContainer
 ) : LessonUpdater(project, container), MarketplaceItemUpdater<Lesson> {
   override fun createTaskUpdater(lesson: Lesson): TaskUpdater = MarketplaceTaskUpdater(project, lesson)
+  override fun createFrameworkTaskUpdater(lesson: FrameworkLesson): FrameworkTaskUpdater = MarketplaceFrameworkTaskUpdater(project, lesson)
 }
