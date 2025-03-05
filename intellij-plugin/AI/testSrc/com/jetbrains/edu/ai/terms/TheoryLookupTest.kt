7168c05cce2995fafa4a2d01de517bb0d8c085dc
Gleb Oborin <gleb.oborin@jetbrains.com>
2025-03-05 19:32:14 +0100

    Fix TranslationConnectorTest and add TheoryLookupTest

--- intellij-plugin/AI/testSrc/com/jetbrains/edu/ai/terms/TheoryLookupTest.kt
+++ intellij-plugin/AI/testSrc/com/jetbrains/edu/ai/terms/TheoryLookupTest.kt
@@ -1,5 +1,6 @@
 package com.jetbrains.edu.ai.terms
 
+import com.intellij.testFramework.PlatformTestUtil
 import com.intellij.util.application
 import com.jetbrains.edu.ai.terms.connector.TermsServiceConnector
 import com.jetbrains.edu.learning.EduTestCase
@@ -8,7 +9,6 @@ import com.jetbrains.edu.learning.ai.terms.TermsProjectSettings
 import com.jetbrains.edu.learning.ai.terms.TermsProperties
 import com.jetbrains.edu.learning.courseFormat.EduCourse
 import com.jetbrains.edu.learning.mockService
-import com.jetbrains.edu.learning.waitFor
 import com.jetbrains.educational.core.format.enum.TranslationLanguage
 import com.jetbrains.educational.terms.format.CourseTermsResponse
 import com.jetbrains.educational.terms.format.Term
@@ -82,7 +82,7 @@ class TheoryLookupTest : EduTestCase() {
     }
     try {
       action()
-      waitFor { updated.get() }
+      PlatformTestUtil.waitWhileBusy { !updated.get() }
     }
     finally {
       job.cancel()
