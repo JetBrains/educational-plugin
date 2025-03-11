8b40494e3d5b7e78102860c04b8cfe17709a7b88
Gleb Oborin <gleb.oborin@jetbrains.com>
2025-03-11 16:32:22 +0100

    Fix bug cannot serialize placeholder in TranslationOption combobox

--- intellij-plugin/AI/src/com/jetbrains/edu/ai/translation/ui/TranslationLanguageComboBoxModel.kt
+++ intellij-plugin/AI/src/com/jetbrains/edu/ai/translation/ui/TranslationLanguageComboBoxModel.kt
@@ -1,10 +1,21 @@
 package com.jetbrains.edu.ai.translation.ui
 
+import com.intellij.ui.dsl.listCellRenderer.textListCellRenderer
 import com.jetbrains.edu.ai.messages.EduAIBundle
 import com.jetbrains.edu.ai.translation.isSameLanguage
 import com.jetbrains.edu.learning.courseFormat.EduCourse
 import com.jetbrains.educational.core.format.enum.TranslationLanguage
 import javax.swing.DefaultComboBoxModel
+import javax.swing.ListCellRenderer
+
+fun translationComboBoxRenderer(course: EduCourse? = null): ListCellRenderer<TranslationLanguage?> = textListCellRenderer { language ->
+  if (language == null || course.isSameLanguage(language)) {
+    EduAIBundle.message("ai.translation.choose.language")
+  }
+  else {
+    language.toString()
+  }
+}
 
 class TranslationLanguageComboBoxModel(private val course: EduCourse? = null) : DefaultComboBoxModel<TranslationLanguage>() {
   init {
@@ -13,16 +24,5 @@ class TranslationLanguageComboBoxModel(private val course: EduCourse? = null) : 
       .filter { language -> !course.isSameLanguage(language) }
       .sortedBy { it.label }
     addAll(languages)
-  }
-
-  override fun setSelectedItem(anObject: Any?) {
-    val language = anObject as? TranslationLanguage
-    val objectToSelect = if (language == null || course.isSameLanguage(language)) {
-      EduAIBundle.message("ai.translation.choose.language")
-    }
-    else {
-      anObject
-    }
-    super.setSelectedItem(objectToSelect)
   }
 }
