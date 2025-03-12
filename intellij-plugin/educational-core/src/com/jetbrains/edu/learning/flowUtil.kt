8e59c026a724c01d032bd27ad4c1665d52ed6f54
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2025-03-12 20:10:51 +0000

    fixup! Do not show `The theory lookup is already in progress` banner

--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/flowUtil.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/flowUtil.kt
@@ -6,9 +6,8 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.stateIn
 
-fun <T1, T2> combineStateFlow(
-  scope: CoroutineScope,
+fun <T1, T2> CoroutineScope.combineStateFlow(
   state1: StateFlow<T1>,
   state2: StateFlow<T2>,
 ): StateFlow<Pair<T1, T2>> = combine(state1, state2) { t1, t2 -> t1 to t2 }
-  .stateIn(scope, SharingStarted.Eagerly, state1.value to state2.value)
+  .stateIn(this, SharingStarted.Eagerly, state1.value to state2.value)
