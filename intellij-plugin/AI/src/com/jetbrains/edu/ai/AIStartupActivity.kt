45142fcc7f15e4140be11a75a6bb3c991c505c92
Gleb Oborin <gleb.oborin@jetbrains.com>
2025-03-13 23:42:48 +0100

    Move launching flow listener from TermsLoader init
    Since the light service is not initialized when opening the project, but initialized on-demand, a situation may occur when the user changes the translation language / enables the theory lookup, and terms will not be fetched.
    
    (cherry picked from commit 16fe840638a91e023e8b5dd54dfdafb91cfb6e77)

--- intellij-plugin/AI/src/com/jetbrains/edu/ai/AIStartupActivity.kt
+++ intellij-plugin/AI/src/com/jetbrains/edu/ai/AIStartupActivity.kt
@@ -3,16 +3,20 @@ package com.jetbrains.edu.ai
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
 import com.jetbrains.edu.ai.AIServiceUpdateChecker.Companion.launchUpdateChecker
+import com.jetbrains.edu.ai.terms.observeAndLoadCourseTerms
 import com.jetbrains.edu.learning.EduUtilsKt.isStudentProject
 import com.jetbrains.edu.learning.course
 import com.jetbrains.edu.learning.courseFormat.EduCourse
+import kotlinx.coroutines.CoroutineScope
 
-class AIStartupActivity : ProjectActivity {
+class AIStartupActivity(private val scope: CoroutineScope) : ProjectActivity {
   override suspend fun execute(project: Project) {
     if (!project.isStudentProject()) return
 
     val course = project.course as? EduCourse ?: return
     if (!course.isMarketplaceRemote) return
     project.launchUpdateChecker(course)
+
+    scope.observeAndLoadCourseTerms(project)
   }
 }
