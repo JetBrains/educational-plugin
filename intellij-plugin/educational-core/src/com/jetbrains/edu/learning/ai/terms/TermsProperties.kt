1d9e25a216e58414b19901d92e91b09d843f94f4
Gleb Oborin <gleb.oborin@jetbrains.com>
2025-03-14 05:55:28 +0100

    Use one map instead of two in TermsProjectSettings
    (cherry picked from commit 3eae2703d6c6848c7bbf3de48402d2bb5a7e988a)

--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/ai/terms/TermsProperties.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/ai/terms/TermsProperties.kt
@@ -3,6 +3,13 @@ package com.jetbrains.edu.learning.ai.terms
 import com.jetbrains.educational.terms.format.Term
 import com.jetbrains.educational.terms.format.domain.TermsVersion
 
+/**
+ * Represents properties related to terms in a project, including the language, terms, and version information.
+ *
+ * @property languageCode The code of the language this set of terms applies to.
+ * @property terms A map where each key is a task ID, and the value is a list of terms associated with that task.
+ * @property version The version metadata for the terms.
+ */
 data class TermsProperties(
   val languageCode: String,
   val terms: Map<Int, List<Term>>,
