varsy/edu-for-kotlin

Implement connector for theory lookup & refactor errors
Since some of the translation errors are common with theory lookup, I've separated it into a common part.
The problem occurred when interacting with statistics, as the query requires an enum. In order not to spoil the statistics, I kept the old translation error format for interacting with statistics.

intellij-plugin/AI/src/com/jetbrains/edu/ai/connector/AIServiceConnector.kt
@@ -1,0 +1,51 @@
+package com.jetbrains.edu.ai.connector
+
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.diagnostic.Logger
+import com.jetbrains.edu.ai.error.AIServiceError
+import com.jetbrains.edu.ai.host.EduAIServiceHost
+import com.jetbrains.edu.learning.Result
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import okhttp3.ConnectionPool
+import retrofit2.Response
+import java.net.SocketException
+
+abstract class AIServiceConnector : Disposable {
+  protected val aiServiceUrl: String
+    get() = EduAIServiceHost.getSelectedUrl()
+
+  protected val connectionPool = ConnectionPool()
+
+  abstract val service: AIService
+
+  override fun dispose() {
+    connectionPool.evictAll()
+  }
+
+  protected fun <T> Response<T>.handleResponse(): Result<T, AIServiceError> {
+    val code = code()
+    val errorMessage = errorBody()?.string()
+    if (!errorMessage.isNullOrEmpty()) {
+      LOG.error("Request failed. Status code: $code. Error message: $errorMessage")
+    }
+    val result = body()
+    return parseResponseCode(code, result)
+  }
+
+  abstract fun <T> parseResponseCode(code: Int, result: T?): Result<T, AIServiceError>
+
+  protected suspend fun <T> networkCall(call: suspend () -> Result<T, AIServiceError>): Result<T, AIServiceError> =
+    try {
+      withContext(Dispatchers.IO) {
+        call()
+      }
+    }
+    catch (exception: SocketException) {
+      AIServiceError.ConnectionError.asErr()
+    }
+
+  companion object {
+    private val LOG: Logger = Logger.getInstance(AIServiceConnector::class.java)
+  }
+}

intellij-plugin/AI/src/com/jetbrains/edu/ai/terms/connector/TermsServiceConnector.kt
@@ -1,55 +1,55 @@
-package com.jetbrains.edu.ai.translation.connector
+package com.jetbrains.edu.ai.terms.connector
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.service
 import com.jetbrains.edu.ai.connector.AIServiceConnector
 import com.jetbrains.edu.ai.error.AIServiceError
-import com.jetbrains.edu.ai.error.TranslationError
-import com.jetbrains.edu.ai.translation.service.TranslationService
+import com.jetbrains.edu.ai.error.TermsServiceError
+import com.jetbrains.edu.ai.terms.service.TermsService
 import com.jetbrains.edu.learning.Ok
 import com.jetbrains.edu.learning.Result
 import com.jetbrains.edu.learning.network.HTTP_UNAVAILABLE_FOR_LEGAL_REASONS
 import com.jetbrains.edu.learning.network.createRetrofitBuilder
 import com.jetbrains.educational.core.format.enum.TranslationLanguage
-import com.jetbrains.educational.translation.format.CourseTranslationResponse
-import com.jetbrains.educational.translation.format.domain.TranslationVersion
+import com.jetbrains.educational.terms.format.CourseTermsResponse
+import com.jetbrains.educational.terms.format.domain.TermsVersion
 import retrofit2.converter.jackson.JacksonConverterFactory
 import java.net.HttpURLConnection.*
 
 @Service(Service.Level.APP)
-class TranslationServiceConnector : AIServiceConnector() {
-  override val service: TranslationService
-    get() = translationService()
+class TermsServiceConnector : AIServiceConnector() {
+  override val service: TermsService
+    get() = termsService()
 
-  private fun translationService(): TranslationService {
+  private fun termsService(): TermsService {
     val objectMapper = jacksonObjectMapper()
     val factory = JacksonConverterFactory.create(objectMapper)
     return createRetrofitBuilder(aiServiceUrl, connectionPool)
       .addConverterFactory(factory)
       .build()
-      .create(TranslationService::class.java)
+      .create(TermsService::class.java)
   }
 
-  suspend fun getLatestTranslationVersion(
+  suspend fun getLatestTermsVersion(
     marketplaceId: Int,
     updateVersion: Int,
     language: TranslationLanguage
-  ): Result<TranslationVersion, AIServiceError> =
+  ): Result<TermsVersion, AIServiceError> =
     networkCall {
       service
-        .getLatestCourseTranslationVersion(marketplaceId, updateVersion, language.name)
+        .getLatestCourseTermsVersion(marketplaceId, updateVersion, language.name)
         .handleResponse()
     }
 
-  suspend fun getTranslatedCourse(
+  suspend fun getCourseTerms(
     marketplaceId: Int,
     updateVersion: Int,
     language: TranslationLanguage
-  ): Result<CourseTranslationResponse, AIServiceError> =
+  ): Result<CourseTermsResponse, AIServiceError> =
     networkCall {
       service
-        .getTranslatedCourse(marketplaceId, updateVersion, language.name)
+        .getCourseTerms(marketplaceId, updateVersion, language.name)
         .handleResponse()
     }
 
@@ -57,13 +57,13 @@ class TranslationServiceConnector : AIServiceConnector() {
     return when {
       code == HTTP_OK && result != null -> Ok(result)
       code == HTTP_UNAVAILABLE || result == null -> AIServiceError.ServiceUnavailable.asErr()
-      code == HTTP_NOT_FOUND -> TranslationError.NoTranslation.asErr()
-      code == HTTP_UNAVAILABLE_FOR_LEGAL_REASONS -> TranslationError.TranslationUnavailableForLegalReasons.asErr()
-      else -> AIServiceError.ConnectionError.asErr()
+      code == HTTP_NOT_FOUND -> TermsServiceError.NoTerms.asErr()
+      code == HTTP_UNAVAILABLE_FOR_LEGAL_REASONS -> TermsServiceError.TermsUnavailableForLegalReason.asErr()
+      else -> AIServiceError.ConnectionError.asErr ()
     }
   }
 
   companion object {
-    fun getInstance(): TranslationServiceConnector = service()
+    fun getInstance(): TermsServiceConnector = service()
   }
 }

JetBrains/educational-plugin

intellij-plugin/AI/src/com/jetbrains/edu/ai/connector/AIServiceConnector.kt
@@ -1,0 +1,50 @@
+package com.jetbrains.edu.ai.connector
+
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.diagnostic.Logger
+import com.jetbrains.edu.ai.error.AIServiceError
+import com.jetbrains.edu.ai.error.CommonAIServiceError
+import com.jetbrains.edu.ai.host.EduAIServiceHost
+import com.jetbrains.edu.learning.Result
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import okhttp3.ConnectionPool
+import retrofit2.Response
+import java.io.IOException
+
+abstract class AIServiceConnector : Disposable {
+  protected val aiServiceUrl: String
+    get() = EduAIServiceHost.getSelectedUrl()
+
+  protected val connectionPool = ConnectionPool()
+
+  override fun dispose() {
+    connectionPool.evictAll()
+  }
+
+  protected fun <T> Response<T>.handleResponse(): Result<T, AIServiceError> {
+    val code = code()
+    val errorMessage = errorBody()?.string()
+    if (!errorMessage.isNullOrEmpty()) {
+      LOG.error("Request failed. Status code: $code. Error message: $errorMessage")
+    }
+    val result = body()
+    return parseResponseCode(code, result)
+  }
+
+  abstract fun <T> parseResponseCode(code: Int, result: T?): Result<T, AIServiceError>
+
+  protected suspend fun <T> networkCall(call: suspend () -> Result<T, AIServiceError>): Result<T, AIServiceError> =
+    try {
+      withContext(Dispatchers.IO) {
+        call()
+      }
+    }
+    catch (exception: IOException) {
+      CommonAIServiceError.CONNECTION_ERROR.asErr()
+    }
+
+  companion object {
+    private val LOG: Logger = Logger.getInstance(AIServiceConnector::class.java)
+  }
+}

intellij-plugin/AI/src/com/jetbrains/edu/ai/terms/connector/TermsServiceConnector.kt
@@ -1,4 +1,4 @@
-package com.jetbrains.edu.ai.translation.connector
+package com.jetbrains.edu.ai.terms.connector
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.intellij.openapi.components.Service
@@ -6,51 +6,51 @@ import com.intellij.openapi.components.service
 import com.jetbrains.edu.ai.connector.AIServiceConnector
 import com.jetbrains.edu.ai.error.AIServiceError
 import com.jetbrains.edu.ai.error.CommonAIServiceError
-import com.jetbrains.edu.ai.translation.TranslationError
-import com.jetbrains.edu.ai.translation.service.TranslationService
+import com.jetbrains.edu.ai.terms.TermsError
+import com.jetbrains.edu.ai.terms.service.TermsService
 import com.jetbrains.edu.learning.Ok
 import com.jetbrains.edu.learning.Result
 import com.jetbrains.edu.learning.network.HTTP_UNAVAILABLE_FOR_LEGAL_REASONS
 import com.jetbrains.edu.learning.network.createRetrofitBuilder
 import com.jetbrains.educational.core.format.enum.TranslationLanguage
-import com.jetbrains.educational.translation.format.CourseTranslationResponse
-import com.jetbrains.educational.translation.format.domain.TranslationVersion
+import com.jetbrains.educational.terms.format.CourseTermsResponse
+import com.jetbrains.educational.terms.format.domain.TermsVersion
 import retrofit2.converter.jackson.JacksonConverterFactory
 import java.net.HttpURLConnection.*
 
 @Service(Service.Level.APP)
-class TranslationServiceConnector : AIServiceConnector() {
-  private val service: TranslationService
-    get() = translationService()
+class TermsServiceConnector : AIServiceConnector() {
+  private val service: TermsService
+    get() = termsService()
 
-  private fun translationService(): TranslationService {
+  private fun termsService(): TermsService {
     val objectMapper = jacksonObjectMapper()
     val factory = JacksonConverterFactory.create(objectMapper)
     return createRetrofitBuilder(aiServiceUrl, connectionPool)
       .addConverterFactory(factory)
       .build()
-      .create(TranslationService::class.java)
+      .create(TermsService::class.java)
   }
 
-  suspend fun getLatestTranslationVersion(
+  suspend fun getLatestTermsVersion(
     marketplaceId: Int,
     updateVersion: Int,
     language: TranslationLanguage
-  ): Result<TranslationVersion, AIServiceError> =
+  ): Result<TermsVersion, AIServiceError> =
     networkCall {
       service
-        .getLatestCourseTranslationVersion(marketplaceId, updateVersion, language.name)
+        .getLatestCourseTermsVersion(marketplaceId, updateVersion, language.name)
         .handleResponse()
     }
 
-  suspend fun getTranslatedCourse(
+  suspend fun getCourseTerms(
     marketplaceId: Int,
     updateVersion: Int,
     language: TranslationLanguage
-  ): Result<CourseTranslationResponse, AIServiceError> =
+  ): Result<CourseTermsResponse, AIServiceError> =
     networkCall {
       service
-        .getTranslatedCourse(marketplaceId, updateVersion, language.name)
+        .getCourseTerms(marketplaceId, updateVersion, language.name)
         .handleResponse()
     }
 
@@ -58,13 +58,13 @@ class TranslationServiceConnector : AIServiceConnector() {
     return when {
       code == HTTP_OK && result != null -> Ok(result)
       code == HTTP_UNAVAILABLE || result == null -> CommonAIServiceError.SERVICE_UNAVAILABLE.asErr()
-      code == HTTP_NOT_FOUND -> TranslationError.NO_TRANSLATION.asErr()
-      code == HTTP_UNAVAILABLE_FOR_LEGAL_REASONS -> TranslationError.TRANSLATION_UNAVAILABLE_FOR_LEGAL_REASONS.asErr()
-      else -> CommonAIServiceError.CONNECTION_ERROR.asErr()
+      code == HTTP_NOT_FOUND -> TermsError.NO_TERMS.asErr()
+      code == HTTP_UNAVAILABLE_FOR_LEGAL_REASONS -> TermsError.TERMS_UNAVAILABLE_FOR_LEGAL_REASON.asErr()
+      else -> CommonAIServiceError.CONNECTION_ERROR.asErr ()
     }
   }
 
   companion object {
-    fun getInstance(): TranslationServiceConnector = service()
+    fun getInstance(): TermsServiceConnector = service()
   }
 }