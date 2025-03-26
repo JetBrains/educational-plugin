018dc35ea8b2c43bea6227115e6e26d5447a284f
Mark Prudnikov <mark.prudnikov@jetbrains.com>
2025-03-26 14:40:20 +0100

    EDU-7486: Fix Finding Changed Functions in `PyFilesDiffer`
    The introduced `fqName` was not unique within the temporary PSI Files in the `TaskProcessorImpl` and therefore it failed to extract the real list of functions that were changed between two files.

--- intellij-plugin/features/ai-hints-python/src/com/jetbrains/edu/aiHints/python/impl/PyFilesDiffer.kt
+++ intellij-plugin/features/ai-hints-python/src/com/jetbrains/edu/aiHints/python/impl/PyFilesDiffer.kt
@@ -5,28 +5,16 @@ import com.jetbrains.edu.aiHints.core.api.FilesDiffer
 import com.jetbrains.edu.aiHints.python.impl.PyHintsUtils.functions
 import com.jetbrains.edu.aiHints.python.impl.PyHintsUtils.hasSameBodyAs
 import com.jetbrains.edu.aiHints.python.impl.PyHintsUtils.hasSameParametersAs
-import com.jetbrains.python.psi.PyFunction
 
 object PyFilesDiffer : FilesDiffer {
   override fun findChangedMethods(before: PsiFile, after: PsiFile, considerParameters: Boolean): List<String> {
-    val beforeFunctions = before.functions().associateBy { it.fqName }
-    val afterFunctions = after.functions().associateBy { it.fqName }
+    val beforeFunctions = before.functions().associateBy { it.name }
+    val afterFunctions = after.functions().associateBy { it.name }
 
     return afterFunctions.values.filterNot { function ->
       // Include function in the result if there's no corresponding function from the original file
-      val originalFunction = beforeFunctions[function.fqName] ?: return@filterNot false
+      val originalFunction = beforeFunctions[function.name] ?: return@filterNot false
       function hasSameBodyAs originalFunction && (!considerParameters || function hasSameParametersAs originalFunction)
     }.mapNotNull { it.name }
   }
-
-  private val PyFunction.fqName: String
-    get() = buildString {
-      append(containingFile)
-      append(":")
-      if (containingClass != null) {
-        append(containingClass)
-        append(":")
-      }
-      append(name)
-    }
 }
