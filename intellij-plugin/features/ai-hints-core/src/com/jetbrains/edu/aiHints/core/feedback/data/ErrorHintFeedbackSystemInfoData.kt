53b72c132e277b43b9326c8a842ba4c14db126ab
Mark Prudnikov <mark.prudnikov@jetbrains.com>
2025-04-02 14:43:35 +0200

    EDU-7906: Fix Feedback Serialization

--- intellij-plugin/features/ai-hints-core/src/com/jetbrains/edu/aiHints/core/feedback/data/ErrorHintFeedbackSystemInfoData.kt
+++ intellij-plugin/features/ai-hints-core/src/com/jetbrains/edu/aiHints/core/feedback/data/ErrorHintFeedbackSystemInfoData.kt
@@ -1,10 +1,16 @@
 package com.jetbrains.edu.aiHints.core.feedback.data
 
 import com.intellij.platform.feedback.dialog.CommonFeedbackSystemData
+import com.intellij.platform.feedback.dialog.SystemDataJsonSerializable
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.encodeToJsonElement
 
 @Serializable
 data class ErrorHintFeedbackSystemInfoData(
-  override val commonSystemInfo: CommonFeedbackSystemData,
-  override val hintFeedbackInfo: ErrorHintFeedbackInfoData
-) : HintFeedbackSystemInfoData<ErrorHintFeedbackInfoData>()
+  val commonSystemInfo: CommonFeedbackSystemData,
+  val hintFeedbackInfo: ErrorHintFeedbackInfoData
+) : SystemDataJsonSerializable {
+  override fun serializeToJson(json: Json): JsonElement = json.encodeToJsonElement(this)
+}
