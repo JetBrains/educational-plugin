208f95ff6b2be78eabe97ef9cf5eb3fe5e13089f
Mark Prudnikov <mark.prudnikov@jetbrains.com>
2025-04-02 15:44:26 +0200

    [marketplace] Move Report Community Solution Action Tests out of MarketplaceSubmissionsTest

--- intellij-plugin/educational-core/testSrc/com/jetbrains/edu/learning/marketplace/actions/ReportCommunitySolutionActionTest.kt
+++ intellij-plugin/educational-core/testSrc/com/jetbrains/edu/learning/marketplace/actions/ReportCommunitySolutionActionTest.kt
@@ -2,22 +2,49 @@ package com.jetbrains.edu.learning.marketplace.actions
 
 import com.intellij.diff.chains.SimpleDiffRequestChain
 import com.intellij.diff.editor.ChainDiffVirtualFile
-import com.intellij.openapi.util.Disposer
-import com.jetbrains.edu.learning.EduActionTestCase
-import com.jetbrains.edu.learning.course
-import com.jetbrains.edu.learning.simpleDiffRequestChain
-import com.jetbrains.edu.learning.testAction
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.openapi.actionSystem.impl.SimpleDataContext
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.TestDialog
+import com.intellij.util.application
+import com.jetbrains.edu.learning.*
+import com.jetbrains.edu.learning.configurators.FakeGradleBasedLanguage
+import com.jetbrains.edu.learning.courseFormat.CheckStatus
+import com.jetbrains.edu.learning.courseFormat.EduCourse
+import com.jetbrains.edu.learning.marketplace.api.MarketplaceSubmission
+import com.jetbrains.edu.learning.marketplace.api.MarketplaceSubmissionsConnector
+import com.jetbrains.edu.learning.submissions.Submission
+import com.jetbrains.edu.learning.submissions.SubmissionsManager
+import io.mockk.every
 import org.junit.Test
+import kotlin.random.Random
 
 class ReportCommunitySolutionActionTest : EduActionTestCase() {
-
   override fun setUp() {
     super.setUp()
-    project.course?.isMarketplace = true
 
-    Disposer.register(testRootDisposable) {
-      project.course?.isMarketplace = false
-    }
+    courseWithFiles(
+      language = FakeGradleBasedLanguage,
+      courseProducer = ::EduCourse,
+      id = 1
+    ) {
+      lesson("lesson1") {
+        eduTask(name = "task1", stepId = 1) {
+          kotlinTaskFile("src/Task1.kt", "fun task1() {}")
+          kotlinTaskFile("test/Tests1.kt", "fun tests1() {}")
+        }
+        eduTask(name = "task2", stepId = 2) {
+          kotlinTaskFile("src/Task2.kt", "fun task2() {}")
+          kotlinTaskFile("test/Tests2.kt", "fun tests2() {}")
+        }
+      }
+    }.apply {
+      isMarketplace = true
+      marketplaceCourseVersion = 1
+    } as EduCourse
+
+    mockService<MarketplaceSubmissionsConnector>(application)
   }
 
   @Test
@@ -42,10 +69,96 @@ class ReportCommunitySolutionActionTest : EduActionTestCase() {
     testAction(ReportCommunitySolutionAction.ACTION_ID, dataContext(diffVirtualFile), shouldBeEnabled = true, runAction = false)
   }
 
-  companion object {
-    fun SimpleDiffRequestChain.putCommunityData(taskId: Int, solutionId: Int) {
-      putUserData(ReportCommunitySolutionAction.TASK_ID_KEY, taskId)
-      putUserData(ReportCommunitySolutionAction.SUBMISSION_ID_KEY, solutionId)
+  @Test
+  fun `test report community solution action success`() {
+    // setup
+    val mockedService = MarketplaceSubmissionsConnector.getInstance()
+    every { mockedService.reportSolution(any()) } returns true
+
+    // add solution from the community
+    val taskId = Random.nextInt()
+    val communitySolutions = communitySolutions(taskId)
+    val submissionsManager = SubmissionsManager.getInstance(project)
+    val communityIds = communitySolutions.map { it.id as Int }
+    val solutionToReport = communitySolutions.random()
+    val solutionToReportId = solutionToReport.id as Int
+    submissionsManager.addCommunitySolutions(taskId, communitySolutions)
+    checkCommunitySolutionsPresented(taskId, communityIds)
+
+    // when
+    withTestDialog(TestDialog.YES) {
+      testAction(ReportCommunitySolutionAction.ACTION_ID, reportActionDataContext(project, solutionToReport, solutionToReportId))
     }
+
+    // then
+    checkCommunitySolutionsPresented(taskId, communityIds.filter { it != solutionToReportId })
+    checkCommunitySolutionNotPresented(taskId, solutionToReportId)
+  }
+
+  @Test
+  fun `test report community solution action failed`() {
+    // setup
+    val mockedService = MarketplaceSubmissionsConnector.getInstance()
+    every { mockedService.reportSolution(any()) } returns false
+
+    // add solution from the community
+    val taskId = Random.nextInt()
+    val communitySolutions = communitySolutions(taskId)
+    val submissionsManager = SubmissionsManager.getInstance(project)
+    val communityIds = communitySolutions.map { it.id as Int }
+    val solutionToReport = communitySolutions.random()
+    val solutionToReportId = solutionToReport.id as Int
+    submissionsManager.addCommunitySolutions(taskId, communitySolutions)
+    checkCommunitySolutionsPresented(taskId, communityIds)
+
+    // when
+    withTestDialog(TestDialog.YES) {
+      testAction(ReportCommunitySolutionAction.ACTION_ID, reportActionDataContext(project, solutionToReport, solutionToReportId))
+    }
+
+    // then
+    checkCommunitySolutionsPresented(taskId, communityIds)
+  }
+
+  private fun checkCommunitySolutionsPresented(taskId: Int, solutionsIds: List<Int>) {
+    val solutionsIdsFromMemory =
+      SubmissionsManager.getInstance(project).getCommunitySubmissionsFromMemory(taskId)?.mapNotNull { it.id }?.toSet()
+    checkNotNull(solutionsIdsFromMemory)
+    assertTrue(solutionsIds.all { solutionsIdsFromMemory.contains(it) })
+  }
+
+  private fun checkCommunitySolutionNotPresented(taskId: Int, solutionId: Int) {
+    val solutionsIdsFromMemory =
+      SubmissionsManager.getInstance(project).getCommunitySubmissionsFromMemory(taskId)?.mapNotNull { it.id }?.toSet()
+    checkNotNull(solutionsIdsFromMemory)
+    assertTrue(solutionId !in solutionsIdsFromMemory)
+  }
+
+  private fun reportActionDataContext(project: Project, solution: Submission, solutionId: Int): DataContext {
+    val diffChain = simpleDiffRequestChain(project)
+    diffChain.putCommunityData(solution.taskId, solutionId)
+
+    val diffVirtualFile = ChainDiffVirtualFile(diffChain, "")
+    return SimpleDataContext.builder()
+      .add(CommonDataKeys.VIRTUAL_FILE, diffVirtualFile)
+      .add(CommonDataKeys.PROJECT, project)
+      .build()
+  }
+
+  private fun communitySolutions(taskId: Int): MutableList<Submission> = mutableListOf(
+    MarketplaceSubmission(taskId, CheckStatus.Solved, "some solution", null, 1).apply {
+      id = Random.nextInt()
+    },
+    MarketplaceSubmission(taskId, CheckStatus.Solved, "some solution 2", null, 1).apply {
+      id = Random.nextInt()
+    },
+    MarketplaceSubmission(taskId, CheckStatus.Solved, "some solution 3", null, 1).apply {
+      id = Random.nextInt()
+    }
+  )
+
+  private fun SimpleDiffRequestChain.putCommunityData(taskId: Int, solutionId: Int) {
+    putUserData(ReportCommunitySolutionAction.TASK_ID_KEY, taskId)
+    putUserData(ReportCommunitySolutionAction.SUBMISSION_ID_KEY, solutionId)
   }
 }
