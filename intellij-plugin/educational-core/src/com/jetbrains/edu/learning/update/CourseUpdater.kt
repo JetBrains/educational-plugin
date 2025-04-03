aca93d4196597ebab5ae0f0797b278b7fd2b06a3
Ilia Posov <ilia.posov@jetbrains.com>
2025-04-03 18:46:39 +0200

    EDU-8006: Add actions after the new update finished
    Show notification, report the COURSE_UPDATE event

--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/update/CourseUpdater.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/update/CourseUpdater.kt
@@ -1,12 +1,23 @@
 package com.jetbrains.edu.learning.update
 
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.application.EDT
+import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.project.Project
+import com.jetbrains.edu.coursecreator.AdditionalFilesUtils.getChangeNotesVirtualFile
+import com.jetbrains.edu.coursecreator.CCNotificationUtils
+import com.jetbrains.edu.learning.CourseUpdateListener
+import com.jetbrains.edu.learning.course
 import com.jetbrains.edu.learning.courseFormat.Course
 import com.jetbrains.edu.learning.courseFormat.LessonContainer
 import com.jetbrains.edu.learning.courseFormat.StudyItem
+import com.jetbrains.edu.learning.messages.EduCoreBundle
 import com.jetbrains.edu.learning.update.comparators.EduFileComparator.Companion.areNotEqual
 import com.jetbrains.edu.learning.update.elements.CourseUpdate
 import com.jetbrains.edu.learning.update.elements.StudyItemUpdate
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 abstract class CourseUpdater<T : Course>(val project: Project, private val localCourse: T) : ItemUpdater<T> {
   protected abstract fun createLessonUpdater(container: LessonContainer): LessonUpdater
@@ -36,6 +47,28 @@ abstract class CourseUpdater<T : Course>(val project: Project, private val local
     updates.forEach {
       it.update(project)
     }
+
+    showUpdateNotification()
+    withContext(Dispatchers.EDT) {
+      if (project.isDisposed) return@withContext
+      val course = project.course ?: return@withContext
+      project.messageBus.syncPublisher(CourseUpdateListener.COURSE_UPDATE).courseUpdated(project, course)
+    }
+  }
+
+  private fun showUpdateNotification() {
+    val changeNotesVirtualFile = getChangeNotesVirtualFile(project)
+    val openChangeNotesAction = if (changeNotesVirtualFile == null || changeNotesVirtualFile.length <= 0) {
+      null
+    }
+    else {
+      object : AnAction(EduCoreBundle.message("marketplace.see.whats.new")) {
+        override fun actionPerformed(e: AnActionEvent) {
+          FileEditorManager.getInstance(project).openFile(changeNotesVirtualFile, true)
+        }
+      }
+    }
+    CCNotificationUtils.showInfoNotification(project, EduCoreBundle.message("action.course.updated"), action = openChangeNotesAction)
   }
 
   abstract fun isCourseChanged(localCourse: T, remoteCourse: T): Boolean
