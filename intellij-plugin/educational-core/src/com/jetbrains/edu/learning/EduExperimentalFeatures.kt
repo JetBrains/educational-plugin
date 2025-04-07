f37cfd9fa595526bcd2efff7e24f37648f195f93
Gleb Oborin <gleb.oborin@jetbrains.com>
2025-04-07 18:35:42 +0200

    Add error explanation to edu experimental features

--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/EduExperimentalFeatures.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/EduExperimentalFeatures.kt
@@ -2,6 +2,7 @@ package com.jetbrains.edu.learning
 
 object EduExperimentalFeatures {
   const val AI_HINTS = "edu.learning.ai.get.hint"
+  const val ERROR_EXPLANATION = "edu.learning.ai.error.explanation"
   const val ANDROID_COURSES = "com.jetbrains.edu.android"
   const val SPLIT_EDITOR = "edu.course.creator.split.editor"
   const val CC_HYPERSKILL = "edu.course.hyperskill"
