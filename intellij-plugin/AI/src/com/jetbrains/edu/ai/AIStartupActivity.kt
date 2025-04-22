62a9d14c75716fe75a3912c038b2ac3588383f2b
Gleb Oborin <gleb.oborin@jetbrains.com>
2025-04-22 18:35:22 +0200

    Revert "EDU-8099: Show AI news notification on project startup"
    This reverts commit 9bd5e195

--- intellij-plugin/AI/src/com/jetbrains/edu/ai/AIStartupActivity.kt
+++ intellij-plugin/AI/src/com/jetbrains/edu/ai/AIStartupActivity.kt
@@ -1,17 +1,14 @@
 package com.jetbrains.edu.ai
 
-import com.intellij.ide.util.PropertiesComponent
 import com.intellij.openapi.application.EDT
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
 import com.jetbrains.edu.ai.AIServiceUpdateChecker.Companion.launchUpdateChecker
-import com.jetbrains.edu.ai.messages.EduAIBundle
 import com.jetbrains.edu.ai.terms.observeAndLoadCourseTerms
 import com.jetbrains.edu.ai.terms.ui.TermsGotItTooltipService
 import com.jetbrains.edu.learning.EduUtilsKt.isStudentProject
 import com.jetbrains.edu.learning.course
 import com.jetbrains.edu.learning.courseFormat.EduCourse
-import com.jetbrains.edu.learning.notification.EduNotificationManager
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -27,23 +24,7 @@ class AIStartupActivity(private val scope: CoroutineScope) : ProjectActivity {
     scope.observeAndLoadCourseTerms(project)
 
     withContext(Dispatchers.EDT) {
-      showAINewsNotification(project)
       TermsGotItTooltipService.getInstance(project).showTermsGotItTooltip()
     }
-  }
-
-  private fun showAINewsNotification(project: Project) {
-    if (PropertiesComponent.getInstance().getBoolean(EDU_AI_NEWS_NOTIFICATION_SHOWN)) return
-    EduNotificationManager.showInfoNotification(
-      project,
-      @Suppress("DialogTitleCapitalization") // incorrect capitalization of the title is intended
-      EduAIBundle.message("ai.notification.news.title"),
-      EduAIBundle.message("ai.notification.news.content")
-    )
-    PropertiesComponent.getInstance().setValue(EDU_AI_NEWS_NOTIFICATION_SHOWN, true)
-  }
-
-  companion object {
-    private const val EDU_AI_NEWS_NOTIFICATION_SHOWN = "edu.ai.news.notification.shown"
   }
 }
