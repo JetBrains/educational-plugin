dc85650f086cff6f26ccc83f143b01c714aea47f
Sofia Biurganovskaia <sofia.biurganovskaia@jetbrains.com>
2025-04-24 19:13:48 +0300

    EDU-8115: Unity-specific adjustments in `TaskChecker` and `CourseView`
    * Specified the Unity folders to be shown in `CourseView`
    * Adjusted the path to tests in `UnityEduTaskChecker`
    * Added a GotItToolTip in case a student tries to launch tests without connecting to Unity Editor

--- intellij-plugin/Edu-CSharp/src/com/jetbrains/edu/csharp/checker/CSharpTaskCheckerProvider.kt
+++ intellij-plugin/Edu-CSharp/src/com/jetbrains/edu/csharp/checker/CSharpTaskCheckerProvider.kt
@@ -1,16 +1,14 @@
 package com.jetbrains.edu.csharp.checker
 
 import com.intellij.openapi.project.Project
+import com.jetbrains.edu.learning.checker.EnvironmentChecker
 import com.jetbrains.edu.learning.checker.TaskCheckerProvider
 import com.jetbrains.edu.learning.courseFormat.tasks.EduTask
-import com.jetbrains.rider.plugins.unity.isUnityProject
 
 class CSharpTaskCheckerProvider : TaskCheckerProvider {
-  override fun getEduTaskChecker(task: EduTask, project: Project): CSharpEduTaskCheckerBase =
-    if (!project.isUnityProject.value) {
-      CSharpEduTaskChecker(task, envChecker, project)
-    }
-    else {
-      UnityEduTaskChecker(task, envChecker, project)
-    }
+  override fun getEduTaskChecker(task: EduTask, project: Project): CSharpEduTaskChecker =
+    CSharpEduTaskChecker(task, envChecker, project)
+
+  override val envChecker: EnvironmentChecker
+    get() = CSharpEnvironmentChecker()
 }
--- intellij-plugin/Edu-CSharp/src/com/jetbrains/edu/csharp/hyperskill/CSharpHyperskillConfigurator.kt
+++ intellij-plugin/Edu-CSharp/src/com/jetbrains/edu/csharp/hyperskill/CSharpHyperskillConfigurator.kt
@@ -4,11 +4,23 @@ import com.intellij.openapi.vfs.VirtualFile
 import com.jetbrains.edu.csharp.CSharpConfigurator
 import com.jetbrains.edu.csharp.CSharpProjectSettings
 import com.jetbrains.edu.learning.EduCourseBuilder
+import com.jetbrains.edu.learning.configuration.ArchiveInclusionPolicy
+import com.jetbrains.edu.learning.configuration.attributesEvaluator.AttributesEvaluator
 import com.jetbrains.edu.learning.stepik.hyperskill.HyperskillConfigurator
 
+/**
+ * For now, we assume that Hyperskill courses can only be Unity-based
+ */
 class CSharpHyperskillConfigurator : HyperskillConfigurator<CSharpProjectSettings>(CSharpConfigurator()) {
   override val courseBuilder: EduCourseBuilder<CSharpProjectSettings>
     get() = CSharpHyperskillCourseBuilder()
+
+  override val courseFileAttributesEvaluator: AttributesEvaluator = AttributesEvaluator(super.courseFileAttributesEvaluator) {
+    extension("meta") {
+      excludeFromArchive()
+      archiveInclusionPolicy(ArchiveInclusionPolicy.MUST_EXCLUDE)
+    }
+  }
 
   override fun shouldFileBeVisibleToStudent(virtualFile: VirtualFile): Boolean =
     virtualFile.name == "Packages" || virtualFile.name == "ProjectSettings" || virtualFile.path.contains("/Assets/")
