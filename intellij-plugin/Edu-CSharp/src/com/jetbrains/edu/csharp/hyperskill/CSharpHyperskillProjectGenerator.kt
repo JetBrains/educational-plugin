7923b44c5eb36e3d394e9977403664afeb316cee
Sofia Biurganovskaia <sofia.biurganovskaia@jetbrains.com>
2025-04-24 19:13:48 +0300

    EDU-8115: Unity-specific adjustments in `TaskChecker` and `CourseView`
    * Specified the Unity folders to be shown in `CourseView`
    * Adjusted the path to tests in `UnityEduTaskChecker`
    * Added a GotItToolTip in case a student tries to launch tests without connecting to Unity Editor

--- intellij-plugin/Edu-CSharp/src/com/jetbrains/edu/csharp/hyperskill/CSharpHyperskillProjectGenerator.kt
+++ intellij-plugin/Edu-CSharp/src/com/jetbrains/edu/csharp/hyperskill/CSharpHyperskillProjectGenerator.kt
@@ -4,14 +4,14 @@ import com.jetbrains.edu.csharp.CSharpProjectSettings
 import com.jetbrains.edu.learning.courseFormat.Course
 import com.jetbrains.edu.learning.newproject.CourseProjectGenerator
 import com.jetbrains.rd.ide.model.RdOpenSolution
-import com.jetbrains.rd.ide.model.RdTemporarySolution
+import com.jetbrains.rider.projectView.SolutionDescriptionFactory
 import com.jetbrains.rider.projectView.SolutionInitializer
 import java.nio.file.Path
 
 class CSharpHyperskillProjectGenerator(builder: CSharpHyperskillCourseBuilder, course: Course) :
   CourseProjectGenerator<CSharpProjectSettings>(builder, course) {
   override fun beforeInitHandler(location: Path): BeforeInitHandler = BeforeInitHandler {
-    val solutionDescription = RdTemporarySolution(course.name, location.toString(), null)
+    val solutionDescription = SolutionDescriptionFactory.virtual(course.name, location.toString(), listOf())
     val strategy = RdOpenSolution(solutionDescription, false)
     SolutionInitializer.initSolution(it, strategy)
   }
