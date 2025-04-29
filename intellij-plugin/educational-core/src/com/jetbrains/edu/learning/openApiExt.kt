c525f75b72c45166a85030a74590894f583f73e0
Sofia Biurganovskaia <sofia.biurganovskaia@jetbrains.com>
2025-04-29 15:49:05 +0300

    Remove `CourseRootProvider` extension point (redundant)

--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/openApiExt.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/openApiExt.kt
@@ -33,7 +33,6 @@ import com.intellij.psi.PsiFile
 import com.intellij.util.PathUtil
 import com.intellij.util.messages.Topic
 import com.intellij.util.messages.Topic.BroadcastDirection
-import com.jetbrains.edu.learning.CourseRootProvider.Companion.getCourseRootDir
 import com.jetbrains.edu.learning.courseFormat.Course
 import com.jetbrains.edu.learning.courseFormat.TaskFile
 import com.jetbrains.edu.learning.courseFormat.isBinary
@@ -123,7 +122,7 @@ val Project.courseDir: VirtualFile
   }
 
 fun Project.guessCourseDir(): VirtualFile? {
-  val projectDir = getCourseRootDir() ?: guessProjectDir() ?: return null
+  val projectDir = guessProjectDir() ?: return null
   return if (projectDir.name == Project.DIRECTORY_STORE_FOLDER) {
     projectDir.parent
   }
