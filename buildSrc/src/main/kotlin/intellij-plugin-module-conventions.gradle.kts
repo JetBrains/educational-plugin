86046abf86a35b301e0340cd95de70c316bdc589
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2025-05-03 23:52:28 +0200

    EDU-8130: introduce `intellij-plugin-module-conventions` plugin
    It's supposed to be used for any non-root plugin module

---
+++ buildSrc/src/main/kotlin/intellij-plugin-module-conventions.gradle.kts
@@ -0,0 +1,46 @@
+import org.jetbrains.intellij.platform.gradle.TestFrameworkType
+import org.jetbrains.intellij.platform.gradle.extensions.intellijPlatform
+
+plugins {
+  id("intellij-plugin-common-conventions")
+  id("org.jetbrains.intellij.platform.module")
+}
+
+repositories {
+  intellijPlatform {
+    defaultRepositories()
+    jetbrainsRuntime()
+  }
+}
+
+intellijPlatform {
+  instrumentCode = false
+}
+
+tasks {
+  prepareSandbox { enabled = false }
+  test {
+    // Needed for both `:intellij-plugin:features:ai-hints-kotlin` and `:intellij-plugin:Edu-Kotlin`
+    // Does nothing otherwise because Kotlin does not exist in the classpath
+    jvmArgumentProviders += CommandLineArgumentProvider {
+      listOf(
+        if (isAtLeast251) {
+          "-Didea.kotlin.plugin.use.k2=true"
+        }
+        else {
+          "-Didea.kotlin.plugin.use.k2=false"
+        }
+      )
+    }
+  }
+}
+
+dependencies {
+  val testOutput = configurations.create("testOutput")
+  testOutput(sourceSets.test.get().output.classesDirs)
+
+  intellijPlatform {
+    testIntellijPlugins(commonTestPlugins)
+    testFramework(TestFrameworkType.Bundled)
+  }
+}
