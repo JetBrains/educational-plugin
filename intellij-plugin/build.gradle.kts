08c0e47847a15340383e43bf84f6e86230bcae4e
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2025-05-04 00:23:08 +0200

    EDU-8130: extract build script for `Edu-Php` module

--- intellij-plugin/build.gradle.kts
+++ intellij-plugin/build.gradle.kts
@@ -265,20 +265,6 @@ fun IntelliJPlatformTestingExtension.customRunIdeTask(
   }
 }
 
-project("Edu-Php") {
-  dependencies {
-    intellijPlatform {
-      intellijIde(ideaVersion)
-
-      intellijPlugins(phpPlugin)
-    }
-
-    implementation(project(":intellij-plugin:educational-core"))
-
-    testImplementation(project(":intellij-plugin:educational-core", "testOutput"))
-  }
-}
-
 project("Edu-Shell") {
   dependencies {
     intellijPlatform {
