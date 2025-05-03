aa59d510b8e7ab586c1547da4f1865f141ff2864
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2025-05-03 23:51:27 +0200

    EDU-8130: introduce `intellij-plugin-common-conventions` plugin
    and apply it for `intellij-plugin` module

---
+++ buildSrc/src/main/kotlin/intellij-plugin-common-conventions.gradle.kts
@@ -0,0 +1,153 @@
+import groovy.util.Node
+import groovy.xml.XmlParser
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+  id("common-conventions")
+  id("net.saliman.properties")
+}
+
+java {
+  sourceSets {
+    main {
+      java.srcDirs("branches/$environmentName/src")
+      resources.srcDirs("branches/$environmentName/resources")
+    }
+
+    test {
+      java.srcDirs("branches/$environmentName/testSrc")
+      resources.srcDirs("branches/$environmentName/testResources")
+    }
+  }
+}
+
+kotlin {
+  sourceSets {
+    main {
+      kotlin.srcDirs("branches/$environmentName/src")
+    }
+    test {
+      kotlin.srcDirs("branches/$environmentName/testSrc")
+    }
+  }
+}
+
+// It's not possible to use version catalogs in convenrion plugin as usual,
+// so we have to get the catalog itself and libraries manually
+// See https://docs.gradle.org/current/userguide/version_catalogs.html#sec:buildsrc-version-catalog
+val libs = the<VersionCatalogsExtension>().named("libs")
+
+// TODO: move dependencies into particular module `build.gradle.kts`.
+//  Most modules don't need all (or even any) these dependencies
+dependencies {
+  implementationWithoutKotlin(libs.findLibrary("twitter4j.core").get())
+  implementationWithoutKotlin(libs.findLibrary("twitter4j.v2").get())
+  implementationWithoutKotlin(libs.findLibrary("jsoup").get())
+  implementationWithoutKotlin(libs.findLibrary("jackson.dataformat.yaml").get())
+  implementationWithoutKotlin(libs.findLibrary("jackson.datatype.jsr310").get())
+  implementationWithoutKotlin(libs.findLibrary("jackson.module.kotlin").get())
+  implementationWithoutKotlin(libs.findLibrary("okhttp").get())
+  implementationWithoutKotlin(libs.findLibrary("logging.interceptor").get())
+  implementationWithoutKotlin(libs.findLibrary("retrofit").get())
+  implementationWithoutKotlin(libs.findLibrary("converter.jackson").get())
+  implementationWithoutKotlin(libs.findLibrary("kotlin.css.jvm").get())
+  
+  testImplementation(libs.findLibrary("junit").get())
+  testImplementation(libs.findLibrary("openTest4J").get())
+  testImplementation(libs.findLibrary("classgraph").get())
+  testImplementationWithoutKotlin(libs.findLibrary("kotlin.test.junit").get())
+  testImplementationWithoutKotlin(libs.findLibrary("mockwebserver").get())
+  testImplementationWithoutKotlin(libs.findLibrary("mockk").get())
+}
+
+tasks {
+  withType<JavaCompile> {
+    // Prevents unexpected incremental compilation errors after changing value of `environmentName` property
+    inputs.property("environmentName", providers.gradleProperty("environmentName"))
+  }
+  withType<KotlinCompile> {
+    // Prevents unexpected incremental compilation errors after changing value of `environmentName` property
+    inputs.property("environmentName", providers.gradleProperty("environmentName"))
+  }
+
+  register(VERIFY_CLASSES_TASK_NAME) {
+    dependsOn(jar)
+    // `verifyClasses` relies on resources from the current and `intellij-plugin` modules.
+    // So, we need to be sure that all necessary recourses are already located in expected placed
+    dependsOn(project(":intellij-plugin").tasks.jar)
+    doLast {
+      verifyClasses(project)
+    }
+  }
+}
+
+private fun parseManifest(file: File): Node {
+  val node = XmlParser().parse(file)
+  check(node.name() == "idea-plugin") {
+    "Manifest file `$file` doesn't contain top-level `idea-plugin` attribute"
+  }
+  return node
+}
+
+private fun manifestFile(project: Project): File? {
+  var filePath: String? = null
+  // Some gradle projects are not modules from IDEA plugin point of view
+  // because we use `include` for them inside manifests, i.e. they just a part of another module.
+  // That's why we delegate manifest search to other projects in some cases
+  when (project.path) {
+    ":intellij-plugin" -> {
+      filePath = "META-INF/plugin.xml"
+    }
+    ":intellij-plugin:educational-core", ":intellij-plugin:code-insight",
+    ":intellij-plugin:Edu-Python:Idea", ":intellij-plugin:Edu-Python:PyCharm" -> return manifestFile(project.parent!!)
+    // Localization module is not supposed to have a plugin manifest.
+    // Since it also is not supposed to have any code, only resources, no need to verify anything for it
+    ":intellij-plugin:localization" -> return null
+  }
+
+  val mainOutput = project.sourceSets.main.get().output
+  val resourcesDir = mainOutput.resourcesDir ?: error("Failed to find resources dir for ${project.name}")
+
+  if (filePath != null) {
+    return resourcesDir.resolve(filePath).takeIf { it.exists() } ?: error("Failed to find manifest file for ${project.name} module")
+  }
+  val rootManifestFile = manifestFile(project(":intellij-plugin")) ?: error("Failed to find manifest file for :intellij-plugin module")
+  val rootManifest = parseManifest(rootManifestFile)
+  val children = ((rootManifest["content"] as? List<*>)?.single() as? Node)?.children()
+                 ?: error("Failed to find module declarations in root manifest")
+  return children.filterIsInstance<Node>()
+           .flatMap { node ->
+             if (node.name() != "module") return@flatMap emptyList()
+             val name = node.attribute("name") as? String ?: return@flatMap emptyList()
+             listOfNotNull(resourcesDir.resolve("$name.xml").takeIf { it.exists() })
+           }.firstOrNull() ?: error("Failed to find manifest file for ${project.name} module")
+}
+
+private fun findModulePackage(project: Project): String? {
+  val moduleManifest = manifestFile(project) ?: return null
+  val node = parseManifest(moduleManifest)
+  return node.attribute("package") as? String ?: error("Failed to find package for ${project.name}")
+}
+
+private fun verifyClasses(project: Project) {
+  val pkg = findModulePackage(project) ?: return
+  val expectedDir = pkg.replace('.', '/')
+
+  var hasErrors = false
+  for (classesDir in project.sourceSets.main.get().output.classesDirs) {
+    val basePath = classesDir.toPath()
+    for (file in classesDir.walk()) {
+      if (file.isFile && file.extension == "class") {
+        val relativePath = basePath.relativize(file.toPath())
+        if (!relativePath.startsWith(expectedDir)) {
+          logger.error("Wrong package of `${relativePath.joinToString(".").removeSuffix(".class")}` class. Expected `$pkg`")
+          hasErrors = true
+        }
+      }
+    }
+  }
+
+  if (hasErrors) {
+    throw GradleException("Classes with wrong package were found. See https://docs.google.com/document/d/1pOy-qNlGOJe6wftHVYHkH8sZOoAfav1fdGDPJgkQWJo")
+  }
+}
---
+++ buildSrc/src/main/kotlin/intellijUtils.kt
@@ -0,0 +1,198 @@
+import org.gradle.api.Project
+import org.gradle.process.JavaForkOptions
+import org.jetbrains.intellij.platform.gradle.IntelliJPlatformType
+import org.jetbrains.intellij.platform.gradle.extensions.IntelliJPlatformDependenciesExtension
+import kotlin.reflect.KProperty
+
+const val VERIFY_CLASSES_TASK_NAME = "verifyClasses"
+
+val Project.environmentName: String by Properties
+val Project.isAtLeast251: Boolean get() = environmentName.toInt() >= 251
+
+val Project.pluginVersion: String by Properties
+val Project.platformVersion: String get() = "20${StringBuilder(environmentName).insert(environmentName.length - 1, '.')}"
+val Project.baseIDE: String by Properties
+val Project.isJvmCenteredIDE: Boolean get() = baseIDE in listOf("idea", "studio")
+
+val Project.ideaVersion: String by Properties
+val Project.clionVersion: String by Properties
+val Project.pycharmVersion: String by Properties
+val Project.studioVersion: String by Properties
+val Project.riderVersion: String by Properties
+
+val Project.isIdeaIDE: Boolean get() = baseIDE == "idea"
+val Project.isClionIDE: Boolean get() = baseIDE == "clion"
+val Project.isPycharmIDE: Boolean get() = baseIDE == "pycharm"
+val Project.isStudioIDE: Boolean get() = baseIDE == "studio"
+val Project.isRiderIDE: Boolean get() = baseIDE == "rider"
+
+val Project.baseVersion: String get() = when {
+  isIdeaIDE -> ideaVersion
+  isClionIDE -> clionVersion
+  isPycharmIDE -> pycharmVersion
+  isStudioIDE -> studioVersion
+  isRiderIDE -> riderVersion
+  else -> error("Unexpected IDE name = `$baseIDE`")
+}
+
+val Project.pythonProPlugin: String by Properties
+val Project.pythonCommunityPlugin: String by Properties
+
+val Project.pythonPlugin: String get() = when {
+  // Since 2024.2 Python Community plugin is available in paid products (like IU) together with Python Pro as its base dependency.
+  // But all necessary code that we need is inside Python Community plugin, so we need only it from compilation POV
+  isIdeaIDE -> pythonCommunityPlugin
+  isClionIDE -> "PythonCore"
+  isPycharmIDE -> "PythonCore"
+  isStudioIDE -> pythonCommunityPlugin
+  isRiderIDE -> pythonCommunityPlugin
+  else -> error("Unexpected IDE name = `$baseIDE`")
+}
+val Project.javaPlugin: String get() = "com.intellij.java"
+val Project.kotlinPlugin: String get() = "org.jetbrains.kotlin"
+val Project.scalaPlugin: String by Properties
+val Project.rustPlugin: String by Properties
+val Project.tomlPlugin: String get() = "org.toml.lang"
+val Project.goPlugin: String by Properties
+val Project.sqlPlugin: String get() = "com.intellij.database"
+val Project.shellScriptPlugin: String get() = "com.jetbrains.sh"
+val Project.markdownPlugin: String get() = "org.intellij.plugins.markdown"
+val Project.githubPlugin: String get() = "org.jetbrains.plugins.github"
+val Project.psiViewerPlugin: String by Properties
+val Project.phpPlugin: String by Properties
+val Project.intelliLangPlugin: String get() = "org.intellij.intelliLang"
+val Project.javaScriptPlugin: String get() = "JavaScript"
+val Project.nodeJsPlugin: String get() = "NodeJS"
+val Project.jsonPlugin: String get() = "com.intellij.modules.json"
+val Project.yamlPlugin: String get() = "org.jetbrains.plugins.yaml"
+val Project.androidPlugin: String get() = "org.jetbrains.android"
+val Project.codeWithMePlugin: String get() = "com.jetbrains.codeWithMe"
+val Project.radlerPlugin: String get() = "org.jetbrains.plugins.clion.radler"
+val Project.imagesPlugin: String get() = "com.intellij.platform.images"
+
+
+val Project.jvmPlugins: List<String> get() = listOf(
+  javaPlugin,
+  "JUnit",
+  "org.jetbrains.plugins.gradle"
+)
+
+val Project.javaScriptPlugins: List<String> get() = listOf(
+  javaScriptPlugin,
+  nodeJsPlugin
+)
+
+val Project.rustPlugins: List<String> get() = listOf(
+  rustPlugin,
+  tomlPlugin
+)
+
+val Project.cppPlugins: List<String> get() = listOf(
+  "com.intellij.cidr.lang",
+  "com.intellij.clion",
+  "com.intellij.clion.runFile",
+  "com.intellij.nativeDebug",
+  "org.jetbrains.plugins.clion.test.google",
+  "org.jetbrains.plugins.clion.test.catch"
+)
+
+val Project.sqlPlugins: List<String> get() = listOfNotNull(
+  sqlPlugin,
+  // https://github.com/JetBrains/intellij-platform-gradle-plugin/issues/1791
+  "intellij.charts",
+  "intellij.grid.plugin".takeIf { isAtLeast251 }
+)
+
+val Project.csharpPlugins: List<String> get() = listOf(
+  "com.intellij.resharper.unity"
+)
+
+// Plugins which we add to tests for all modules.
+// It's the most common plugins which affect the behavior of the plugin code
+val Project.commonTestPlugins: List<String> get() = listOf(
+  imagesPlugin, // adds `svg` file type and makes IDE consider .svg files as text ones
+  yamlPlugin,   // makes IDE consider .yaml files as text ones and affects formatting of yaml files
+  jsonPlugin,   // dependency of a lot of other bundled plugin
+)
+
+
+data class TypeWithVersion(val type: IntelliJPlatformType, val version: String)
+
+fun String.toTypeWithVersion(): TypeWithVersion {
+  val (code, version) = split("-", limit = 2)
+  return TypeWithVersion(IntelliJPlatformType.fromCode(code), version)
+}
+
+fun IntelliJPlatformDependenciesExtension.intellijIde(versionWithCode: String) {
+  val (type, version) = versionWithCode.toTypeWithVersion()
+  create(type, version, useInstaller = false)
+  // JetBrains runtime is necessary not only for running IDE but for tests as well
+  jetbrainsRuntime()
+}
+
+fun IntelliJPlatformDependenciesExtension.intellijPlugins(vararg notations: String) {
+  for (notation in notations) {
+    if (notation.contains(":")) {
+      plugin(notation)
+    }
+    else {
+      bundledPlugin(notation)
+    }
+  }
+}
+
+fun IntelliJPlatformDependenciesExtension.intellijPlugins(notations: List<String>) {
+  intellijPlugins(*notations.toTypedArray())
+}
+
+fun IntelliJPlatformDependenciesExtension.testIntellijPlugins(vararg notations: String) {
+  for (notation in notations) {
+    if (notation.contains(":")) {
+      testPlugin(notation)
+    }
+    else {
+      testBundledPlugin(notation)
+    }
+  }
+}
+
+fun IntelliJPlatformDependenciesExtension.testIntellijPlugins(notations: List<String>) {
+  testIntellijPlugins(*notations.toTypedArray())
+}
+
+// Since 2024.1 CLion has two sets of incompatible plugins: based on classic language engine and new one (AKA Radler).
+// Platform uses `idea.suppressed.plugins.set.selector` system property to choose which plugins should be disabled.
+// But there aren't `idea.suppressed.plugins.set.selector`, `idea.suppressed.plugins.set.classic`
+// and `idea.suppressed.plugins.set.radler` properties in tests,
+// as a result, the platform tries to load all plugins and fails because of duplicate definitions.
+// Here is a workaround to make test work with CLion by defining proper values for necessary properties
+fun JavaForkOptions.setClionSystemProperties(withRadler: Boolean = false) {
+  val (mode, suppressedPlugins) = if (withRadler) {
+    val radlerSuppressedPlugins = listOfNotNull(
+      "com.intellij.cidr.lang",
+      "com.intellij.cidr.lang.clangdBridge",
+      "com.intellij.c.performanceTesting",
+      "org.jetbrains.plugins.cidr-intelliLang",
+      "com.intellij.cidr.grazie",
+      "com.intellij.cidr.markdown",
+    )
+    "radler" to radlerSuppressedPlugins
+  }
+  else {
+    val classicSuppressedPlugins = listOf(
+      "org.jetbrains.plugins.clion.radler",
+      "intellij.rider.cpp.debugger",
+      "intellij.rider.plugins.clion.radler.cwm"
+    )
+    "classic" to classicSuppressedPlugins
+  }
+  systemProperty("idea.suppressed.plugins.set.selector", mode) // possible values: `classic` and `radler`
+  systemProperty("idea.suppressed.plugins.set.$mode", suppressedPlugins.joinToString(","))
+}
+
+// There isn't an implicit `project` object here, so
+// this is a minor workaround to use delegation for properties almost like in a regular plugin
+// and not to duplicate property name twice: one time in Kotlin property and the second time in `prop` call
+private object Properties {
+  operator fun getValue(thisRef: Project, property: KProperty<*>): String = thisRef.prop(property.name)
+}
