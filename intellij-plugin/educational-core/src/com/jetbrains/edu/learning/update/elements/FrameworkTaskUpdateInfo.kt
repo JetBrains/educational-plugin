368ed1678b80538f1d88042790550db0fb9b4fd7
Ilia Posov <ilia.posov@jetbrains.com>
2025-05-06 18:53:29 +0200

    EDU-8134: Support renamed tasks on update of framework lessons
    The update is done sequentially for tasks, so the renaming also goes task folder by task folder. It means that some renaming scenarios are impossible, for example, when names of two tasks are exchanged.

--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/update/elements/FrameworkTaskUpdateInfo.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/update/elements/FrameworkTaskUpdateInfo.kt
@@ -1,8 +1,10 @@
 package com.jetbrains.edu.learning.update.elements
 
 import com.intellij.openapi.application.writeAction
+import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.progress.blockingContext
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.util.io.ReadOnlyAttributeUtil
 import com.jetbrains.edu.learning.courseDir
@@ -10,13 +12,14 @@ import com.jetbrains.edu.learning.courseFormat.CheckStatus
 import com.jetbrains.edu.learning.courseFormat.FileContents
 import com.jetbrains.edu.learning.courseFormat.FrameworkLesson
 import com.jetbrains.edu.learning.courseFormat.ext.getDir
+import com.jetbrains.edu.learning.courseFormat.ext.getTaskDirectory
 import com.jetbrains.edu.learning.courseFormat.tasks.Task
 import com.jetbrains.edu.learning.courseGeneration.GeneratorUtils
 import com.jetbrains.edu.learning.framework.FrameworkLessonManager
 import com.jetbrains.edu.learning.toCourseInfoHolder
 import com.jetbrains.edu.learning.update.FrameworkLessonHistory
-import com.jetbrains.edu.learning.update.UpdateUtils.updateTaskDescription
 import com.jetbrains.edu.learning.yaml.YamlFormatSynchronizer
+import java.io.IOException
 
 data class FrameworkTaskUpdateInfo(
   override val localItem: Task,
@@ -38,8 +41,12 @@ data class FrameworkTaskUpdateInfo(
       remoteItem.status = CheckStatus.Solved
     }
 
-    blockingContext {
-      updateTaskDescription(project, localItem, remoteItem)
+    try {
+      updateTaskDirectory(project, localLesson)
+    }
+    catch (e: Exception) {
+      thisLogger().error("Failed to update task directory", e)
+      return
     }
 
     remoteItem.record = localItem.record
@@ -65,6 +72,37 @@ data class FrameworkTaskUpdateInfo(
     blockingContext {
       YamlFormatSynchronizer.saveItemWithRemoteInfo(remoteItem)
     }
+  }
+
+  @Throws(IOException::class, IllegalStateException::class)
+  private suspend fun updateTaskDirectory(project: Project, localLesson: FrameworkLesson) {
+    val newTaskDir = if (localItem.name != remoteItem.name) {
+      createTaskDirectoryWithNewName(project, localLesson)
+    }
+    else {
+      localItem.getTaskDirectory(project) ?: error("Failed to find local task dir: ${localLesson.name}")
+    }
+
+    blockingContext {
+      GeneratorUtils.createDescriptionFile(project, newTaskDir, remoteItem)
+    }
+  }
+
+  @Throws(IOException::class, IllegalStateException::class)
+  private suspend fun createTaskDirectoryWithNewName(project: Project, localLesson: FrameworkLesson): VirtualFile {
+    val localTaskDir = localItem.getTaskDirectory(project) ?: error("Failed to find local task dir on update: ${localLesson.name}")
+
+    writeAction {
+      localTaskDir.delete(FrameworkTaskUpdateInfo::class)
+    }
+
+    val lessonDir = localLesson.getDir(project.courseDir) ?: error("Failed to find local lesson dir on update: ${localLesson.name}")
+
+    val newTaskDir = writeAction {
+      VfsUtil.createDirectoryIfMissing(lessonDir, remoteItem.name)
+    }
+
+    return newTaskDir
   }
 
   private suspend fun removeFile(taskDir: VirtualFile, fileName: String) = writeAction {
