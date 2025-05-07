4944f80af91b702745c3a917e54d9fd5d3e14e15
Aleksey Rostovskiy <aleksey.rostovskiy@jetbrains.com>
2025-05-07 22:06:48 +0200

    Add dialog and logic for changing AI Debugger service URL
    Introduced a new dialog to allow users to change the AI Debugger service URL.
    Added corresponding action, properties, enums, and tests to support this functionality.
    Updated resource bundles and plugin configurations accordingly.

---
+++ intellij-plugin/features/ai-debugger-core/src/com/jetbrains/edu/ai/debugger/core/host/EduAIDebuggerServiceHost.kt
@@ -0,0 +1,27 @@
+package com.jetbrains.edu.ai.debugger.core.host
+
+import com.intellij.ide.util.PropertiesComponent
+import com.jetbrains.edu.ai.debugger.core.EDU_AI_DEBUGGER_SERVICE_HOST_PROPERTY
+import com.jetbrains.edu.ai.debugger.core.EDU_AI_DEBUGGER_SERVICE_URL
+import com.jetbrains.edu.ai.debugger.core.messages.EduAIDebuggerCoreBundle
+import org.jetbrains.annotations.Nls
+import java.util.function.Supplier
+
+// TODO: add STAGING host
+enum class EduAIDebuggerServiceHost(private val visibleName: Supplier<@Nls String>, val url: String) {
+  PRODUCTION(EduAIDebuggerCoreBundle.lazyMessage("ai.debugger.service.production.server"), EDU_AI_DEBUGGER_SERVICE_URL),
+  OTHER(EduAIDebuggerCoreBundle.lazyMessage("ai.debugger.service.other"), "http://localhost:8080");
+
+  override fun toString(): String = visibleName.get()
+
+  companion object {
+    @JvmStatic
+    fun getSelectedHost(): EduAIDebuggerServiceHost {
+      val selectedUrl = getSelectedUrl()
+      return EduAIDebuggerServiceHost.values().firstOrNull { it.url == selectedUrl } ?: OTHER
+    }
+
+    @JvmStatic
+    fun getSelectedUrl(): String = PropertiesComponent.getInstance().getValue(EDU_AI_DEBUGGER_SERVICE_HOST_PROPERTY, PRODUCTION.url)
+  }
+}
