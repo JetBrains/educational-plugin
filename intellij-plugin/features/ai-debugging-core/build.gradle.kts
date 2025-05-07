1b08bbf3ca7f3b42f55827f9b3548cd62b9f2e38
Aleksey Rostovskiy <aleksey.rostovskiy@jetbrains.com>
2025-05-07 19:11:51 +0200

    Add build scripts for AI debugging modules in IntelliJ plugin

---
+++ intellij-plugin/features/ai-debugging-core/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+  id("intellij-plugin-module-conventions")
+  alias(libs.plugins.kotlinSerializationPlugin)
+}
+
+dependencies {
+  intellijPlatform {
+    intellijIde(baseVersion)
+  }
+
+  implementation(project(":intellij-plugin:educational-core"))
+  implementation(project(":intellij-plugin:AI"))
+  api(rootProject.libs.educational.ml.library.core) {
+    excludeKotlinDeps()
+    excludeKotlinSerializationDeps()
+    exclude(group = "net.java.dev.jna")
+  }
+  api(rootProject.libs.educational.ml.library.ai.debugger) {
+    excludeKotlinDeps()
+    excludeKotlinSerializationDeps()
+    exclude(group = "net.java.dev.jna")
+  }
+
+  testImplementation(project(":intellij-plugin:educational-core", "testOutput"))
+}
