79072f201419f904995ee0ed7e006db885d4a6d7
Aleksey Rostovskiy <aleksey.rostovskiy@jetbrains.com>
2025-05-09 02:41:53 +0200

    Add AI Debugger support including new plugin modules, resources, and utilities

---
+++ intellij-plugin/features/ai-debugger-core/src/com/jetbrains/edu/ai/debugger/core/ui/AIDebuggerColors.kt
@@ -0,0 +1,8 @@
+package com.jetbrains.edu.ai.debugger.core.ui
+
+import com.intellij.ui.JBColor
+import java.awt.Color
+
+object AIDebuggerColors {
+  val HIGHLIGHTING_COLOR: Color = JBColor(Color(109, 0, 247, 77), Color(135, 49, 247, 77))
+}
