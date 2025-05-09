0132a152a6d8be7f107693f7b6580c57a4265a79
Aleksey Rostovskiy <aleksey.rostovskiy@jetbrains.com>
2025-05-09 02:41:53 +0200

    Add AI Debugger support including new plugin modules, resources, and utilities

---
+++ intellij-plugin/features/ai-debugger-jvm/src/com/jetbrains/edu/ai/debugger/jvm/JvmAIDebuggerBreakpointHandlerFactory.kt
@@ -0,0 +1,10 @@
+package com.jetbrains.edu.ai.debugger.jvm
+
+import com.intellij.debugger.engine.DebugProcessImpl
+import com.intellij.debugger.engine.JavaBreakpointHandler
+import com.intellij.debugger.engine.JavaBreakpointHandlerFactory
+
+class JvmAIDebuggerBreakpointHandlerFactory : JavaBreakpointHandlerFactory {
+  override fun createHandler(process: DebugProcessImpl): JavaBreakpointHandler =
+    JavaBreakpointHandler(JvmAIDebuggerBreakpointType::class.java, process)
+}
