d4c7db67032556fb3aac4fe7fef071d814c98762
anchouls <anna.potriasaeva@jetbrains.com>
2025-05-13 13:39:11 +0000

    Add a custom icon for verified breakpoints
    Merge-request: EDU-MR-4328
    Merged-by: Anna Potriasaeva <Anna.Potriasaeva@jetbrains.com>

--- intellij-plugin/features/ai-debugger-jvm/src/com/jetbrains/edu/ai/debugger/jvm/JvmAIDebuggerBreakpointType.kt
+++ intellij-plugin/features/ai-debugger-jvm/src/com/jetbrains/edu/ai/debugger/jvm/JvmAIDebuggerBreakpointType.kt
@@ -1,6 +1,9 @@
 package com.jetbrains.edu.ai.debugger.jvm
 
+import com.intellij.debugger.ui.breakpoints.Breakpoint
 import com.intellij.debugger.ui.breakpoints.JavaLineBreakpointType
+import com.intellij.openapi.project.Project
+import com.intellij.xdebugger.breakpoints.XBreakpoint
 import com.intellij.xdebugger.breakpoints.XLineBreakpoint
 import com.jetbrains.edu.ai.debugger.core.messages.EduAIDebuggerCoreBundle
 import com.jetbrains.edu.ai.debugger.core.ui.AIDebuggerIcons
@@ -8,8 +11,14 @@ import org.jetbrains.java.debugger.breakpoints.properties.JavaLineBreakpointProp
 import javax.swing.Icon
 
 class JvmAIDebuggerBreakpointType : JavaLineBreakpointType("jvm-line-ai", EduAIDebuggerCoreBundle.message("ai.debugger.jvm.breakpoint.description")) {
-  // TODO change to real image
   override fun getEnabledIcon(): Icon = AIDebuggerIcons.AIBug
 
   override fun getGeneralDescription(breakpoint: XLineBreakpoint<JavaLineBreakpointProperties>?): String? = null
+
+  override fun createJavaBreakpoint(
+    project: Project?,
+    breakpoint: XBreakpoint<JavaLineBreakpointProperties>?
+  ): Breakpoint<JavaLineBreakpointProperties> {
+    return JvmAIDebuggerLineBreakpoint(project, breakpoint)
+  }
 }
