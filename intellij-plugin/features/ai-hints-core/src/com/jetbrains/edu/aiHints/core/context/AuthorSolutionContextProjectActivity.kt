063fb3e16c5fde0df2c80b53d900611a1cbe2a4f
Mark Prudnikov <mark.prudnikov@jetbrains.com>
2025-05-14 16:29:32 +0200

    EDU-7703: Gather `AuthorSolutionContext` Only For `EduTask`s

--- intellij-plugin/features/ai-hints-core/src/com/jetbrains/edu/aiHints/core/context/AuthorSolutionContextProjectActivity.kt
+++ intellij-plugin/features/ai-hints-core/src/com/jetbrains/edu/aiHints/core/context/AuthorSolutionContextProjectActivity.kt
@@ -7,13 +7,15 @@ import com.jetbrains.edu.aiHints.core.EduAIHintsProcessor
 import com.jetbrains.edu.learning.course
 import com.jetbrains.edu.learning.courseFormat.EduCourse
 import com.jetbrains.edu.learning.courseFormat.ext.allTasks
+import com.jetbrains.edu.learning.courseFormat.tasks.EduTask
 
 class AuthorSolutionContextProjectActivity : ProjectActivity {
   override suspend fun execute(project: Project) {
     val course = project.course?.asSafely<EduCourse>() ?: return
     // AI Hints are available only for Marketplace courses in the Student mode, and for a subset of programming languages only
     if (!course.isStudy || !course.isMarketplace || EduAIHintsProcessor.forCourse(course) == null) return
-    for (task in course.allTasks) {
+    val eduTasks = course.allTasks.filterIsInstance<EduTask>() // AI Hints available only for EduTasks
+    for (task in eduTasks) {
       task.authorSolutionContext = AuthorSolutionContext.create(project, task)
     }
   }
