cb7b269f6157b705c800202625b64f6484e4e9a7
Ilia Posov <ilia.posov@jetbrains.com>
2025-05-19 17:53:58 +0200

    EDU-8162: Add Tode to menu
    (cherry picked from commit 817bf610ea50b848aee69fa5d50f642c77218076)

---
+++ intellij-plugin/features/ide-onboarding/src/com/jetbrains/edu/uiOnboarding/EduUiOnboardingIcons.kt
@@ -0,0 +1,10 @@
+package com.jetbrains.edu.uiOnboarding
+
+import com.intellij.util.ui.JBFont
+import com.jetbrains.edu.learning.taskToolWindow.ui.navigationMap.EduTextIcon
+import javax.swing.Icon
+
+object EduUiOnboardingIcons {
+  @JvmField
+  val ToadIcon: Icon = EduTextIcon("\uD83D\uDC38", JBFont.label())
+}
