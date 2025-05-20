902fe2dd927d15a4e97a089c10f8411babb72fb1
anchouls <anna.potriasaeva@jetbrains.com>
2025-05-20 14:13:09 +0000

    Refactor AI Debugger to use updated breakpoint API
    Fixed file names
    
    Update library versions and refactor DebuggerHintRequest defaults
    
    Upgraded the educational-ml-library version to 1.0.57 and adjusted version references for related libraries.
    Removed unnecessary properties in AIDebuggerServiceConnector, and set default values for optional fields in DebuggerHintRequest to simplify initialization.
    
    Add Jackson support for JSON serialization in AI Debugger
    
    Integrated Jackson `ObjectMapper` and `JacksonConverterFactory` to handle JSON serialization/deserialization in the AI Debugger service connector. This improves compatibility and maintains consistency in processing JSON data throughout the service.
    
    Updated context
    
    Updated the version of `educational-ml-library-debugger` to `1.0.55`
    
    Refactor debugger service requests and data models
    
    Reorganized and modularized service requests by introducing new data classes like `DebuggerHintRequest`, `BreakpointHintRequest`, and `TestInfo`.
    Updated related method signatures and imports to align with the new structure, ensuring improved clarity and maintainability.
    
    Refactor AI Debugger to use updated breakpoint API
    
    
    Co-authored-by: Aleksei Rostovskii <aleksei.rostovskii@jetbrains.com>
    
    Merge-request: EDU-MR-4375
    Merged-by: Anna Potriasaeva <Anna.Potriasaeva@jetbrains.com>

--- intellij-plugin/features/ai-debugger-core/src/com/jetbrains/edu/ai/debugger/core/service/AIDebuggerService.kt
+++ intellij-plugin/features/ai-debugger-core/src/com/jetbrains/edu/ai/debugger/core/service/AIDebuggerService.kt
@@ -1,17 +1,15 @@
 package com.jetbrains.edu.ai.debugger.core.service
 
-import com.jetbrains.educational.ml.debugger.dto.BreakpointHintRequest
-import com.jetbrains.educational.ml.debugger.dto.BreakpointHintResponse
-import com.jetbrains.educational.ml.debugger.dto.CodeFixRequest
-import com.jetbrains.educational.ml.debugger.dto.CodeFixResponse
+import com.jetbrains.educational.ml.debugger.dto.Breakpoint
+import com.jetbrains.educational.ml.debugger.response.BreakpointHintDetails
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface AIDebuggerService{
   @POST("/api/get-breakpoint-hint")
-  suspend fun getBreakpointHint(@Body request: BreakpointHintRequest): Response<BreakpointHintResponse>
+  suspend fun getBreakpointHint(@Body request: BreakpointHintRequest): Response<List<BreakpointHintDetails>>
 
-  @POST("/get-code-fix")
-  suspend fun getCodeFix(@Body request: CodeFixRequest): Response<CodeFixResponse>
+  @POST("/api/get-breakpoints")
+  suspend fun getBreakpoints(@Body request: DebuggerHintRequest): Response<List<Breakpoint>>
 }
