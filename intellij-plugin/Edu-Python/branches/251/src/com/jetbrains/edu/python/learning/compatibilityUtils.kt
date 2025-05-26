4fd5ee07194430a72a7a3203717b52e4b8f288c5
Mark Prudnikov <mark.prudnikov@jetbrains.com>
2025-05-26 20:42:20 +0200

    EDU-8136: Support 2025.2 API in `PyEduUtils`
    (cherry picked from commit 9c9a4b3c392c5607120d14c1c15dafa30d457a31)

--- intellij-plugin/Edu-Python/branches/251/src/com/jetbrains/edu/python/learning/compatibilityUtils.kt
+++ intellij-plugin/Edu-Python/branches/251/src/com/jetbrains/edu/python/learning/compatibilityUtils.kt
@@ -2,6 +2,8 @@ package com.jetbrains.edu.python.learning
 
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.projectRoots.Sdk
+import com.intellij.platform.util.progress.SequentialProgressReporter
+import com.jetbrains.python.packaging.PyRequirement
 import com.jetbrains.python.packaging.common.PythonSimplePackageSpecification
 import com.jetbrains.python.packaging.management.PythonPackageManager
 import com.jetbrains.python.psi.LanguageLevel
@@ -11,21 +13,36 @@ import com.jetbrains.python.sdk.setAssociationToModule
 import kotlin.reflect.full.callSuspend
 import kotlin.reflect.full.functions
 
-// BACKCOMPAT: 2024.3. inline it
-internal suspend fun installRequiredPackage(packageManager: PythonPackageManager, spec: PythonSimplePackageSpecification) {
-  try {
-    packageManager.installPackage(spec, emptyList(), withBackgroundProgress = false)
-  }
-  catch (_: NoSuchMethodError) {
-    // For versions of the py plugin before 251.25410. The installPackage() method didn't have the last boolean argument
+// BACKCOMPAT: 2024.3. Inline it.
+internal suspend fun installRequiredPackages(
+  reporter: SequentialProgressReporter,
+  packageManager: PythonPackageManager,
+  requirements: List<PyRequirement>
+) {
+  for (requirement in requirements) {
+    val spec = PythonSimplePackageSpecification(
+      requirement.installOptions.joinToString(" "),
+      version = null,
+      repository = null,
+      relation = null
+    )
 
-    val method = packageManager::class.functions.find { function ->
-      // Match name and parameter types
-      function.name == "installPackage" &&
-      function.parameters.size == 3 // 1 receiver + 2 arguments
-    } ?: throw NoSuchMethodError("installPackage() method not found")
+    reporter.itemStep(requirement.name) {
+      try {
+        packageManager.installPackage(spec, emptyList(), withBackgroundProgress = false)
+      }
+      catch (_: NoSuchMethodError) {
+        // For versions of the py plugin before 251.25410. The installPackage() method didn't have the last boolean argument
 
-    method.callSuspend(packageManager, spec, emptyList<String>())
+        val method = packageManager::class.functions.find { function ->
+          // Match name and parameter types
+          function.name == "installPackage" &&
+          function.parameters.size == 3 // 1 receiver + 2 arguments
+        } ?: throw NoSuchMethodError("installPackage() method not found")
+
+        method.callSuspend(packageManager, spec, emptyList<String>())
+      }
+    }
   }
 }
 
