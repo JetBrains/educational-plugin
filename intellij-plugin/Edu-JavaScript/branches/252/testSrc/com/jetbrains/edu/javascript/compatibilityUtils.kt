a7df0209b3d560fa711bbb605631fa183e145316
Mark Prudnikov <mark.prudnikov@jetbrains.com>
2025-05-26 20:21:47 +0200

    EDU-8136: Copy Existing `**/branches/251/**` to `**/branches/252/**`
    (cherry picked from commit 1e290214bfedd758a6ac4cba6789c8fd95bdbb78)

---
+++ intellij-plugin/Edu-JavaScript/branches/252/testSrc/com/jetbrains/edu/javascript/compatibilityUtils.kt
@@ -0,0 +1,4 @@
+package com.jetbrains.edu.javascript
+
+// BACKCOMPAT: 2024.3. inline it
+val JavascriptLanguage: com.intellij.lang.javascript.JavascriptLanguage get() = com.intellij.lang.javascript.JavascriptLanguage
