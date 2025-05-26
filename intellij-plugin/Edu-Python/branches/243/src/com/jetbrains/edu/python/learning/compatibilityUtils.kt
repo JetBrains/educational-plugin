8d23634636eb4c37f29e347d21859b6c08d319b6
Mark Prudnikov <mark.prudnikov@jetbrains.com>
2025-05-26 20:30:38 +0200

    EDU-8136: Support 2025.2 API in `PyCourseProjectGenerator`

--- intellij-plugin/Edu-Python/branches/243/src/com/jetbrains/edu/python/learning/compatibilityUtils.kt
+++ intellij-plugin/Edu-Python/branches/243/src/com/jetbrains/edu/python/learning/compatibilityUtils.kt
@@ -1,8 +1,15 @@
 package com.jetbrains.edu.python.learning
 
+import com.intellij.openapi.module.Module
+import com.intellij.openapi.projectRoots.Sdk
 import com.jetbrains.python.packaging.common.PythonSimplePackageSpecification
 import com.jetbrains.python.packaging.management.PythonPackageManager
+import com.jetbrains.python.sdk.setAssociationToModule
 
 internal suspend fun installRequiredPackage(packageManager: PythonPackageManager, spec: PythonSimplePackageSpecification) {
   packageManager.installPackage(spec, emptyList())
+}
+
+internal fun setAssociationToModule(sdk: Sdk, module: Module) {
+  sdk.setAssociationToModule(module)
 }
