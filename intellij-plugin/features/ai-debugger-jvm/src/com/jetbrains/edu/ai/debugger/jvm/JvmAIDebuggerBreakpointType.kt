429ece2bb064cf5387f2e0ec7227b08c563fa783
anchouls <anna.potriasaeva@jetbrains.com>
2025-05-30 14:41:54 +0000

    Bug fixes in AI-Debugger
    Replace `getInvisibleTestFiles` with `getAllTestFiles` to include all test files
    
    Remove unused method and redundant import in breakpoint type
    
    Refine failed test name extraction in AIDebugUtils.
    
    Updated the  function to remove additional details from the test name by extracting only the part before the first parenthesis. This ensures cleaner and more concise test names are processed.
    
    Refactor removeHighlighter to use invokeLater
    
    Encapsulated the highlighter removal logic within an invokeLater block to ensure UI updates occur on the Event Dispatch Thread.
    
    Refactor test lookup logic to ensure read action safety
    
    
    Co-authored-by: Daniil Karol <daniil.karol@jetbrains.com>
    
    Merge-request: EDU-MR-4409
    Merged-by: Anna Potriasaeva <Anna.Potriasaeva@jetbrains.com>

--- intellij-plugin/features/ai-debugger-jvm/src/com/jetbrains/edu/ai/debugger/jvm/JvmAIDebuggerBreakpointType.kt
+++ intellij-plugin/features/ai-debugger-jvm/src/com/jetbrains/edu/ai/debugger/jvm/JvmAIDebuggerBreakpointType.kt
@@ -4,7 +4,6 @@ import com.intellij.debugger.ui.breakpoints.Breakpoint
 import com.intellij.debugger.ui.breakpoints.JavaLineBreakpointType
 import com.intellij.openapi.project.Project
 import com.intellij.xdebugger.breakpoints.XBreakpoint
-import com.intellij.xdebugger.breakpoints.XLineBreakpoint
 import com.jetbrains.edu.ai.debugger.core.messages.EduAIDebuggerCoreBundle
 import com.jetbrains.edu.ai.debugger.core.ui.AIDebuggerIcons
 import org.jetbrains.java.debugger.breakpoints.properties.JavaLineBreakpointProperties
@@ -12,8 +11,6 @@ import javax.swing.Icon
 
 class JvmAIDebuggerBreakpointType : JavaLineBreakpointType(BREAKPOINT_TYPE_ID, EduAIDebuggerCoreBundle.message("ai.debugger.jvm.breakpoint.description")) {
   override fun getEnabledIcon(): Icon = AIDebuggerIcons.AIBug
-
-  override fun getGeneralDescription(breakpoint: XLineBreakpoint<JavaLineBreakpointProperties>?): String? = null
 
   override fun createJavaBreakpoint(
     project: Project?,
