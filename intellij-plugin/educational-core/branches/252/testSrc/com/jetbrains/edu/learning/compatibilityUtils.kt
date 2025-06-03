fa578fbe4cad22aea326dd8315f72f6c3bb637a7
Mark Prudnikov <mark.prudnikov@jetbrains.com>
2025-06-03 19:58:35 +0200

    EDU-8136: Support 2025.2 API in `testUtils`

--- intellij-plugin/educational-core/branches/252/testSrc/com/jetbrains/edu/learning/compatibilityUtils.kt
+++ intellij-plugin/educational-core/branches/252/testSrc/com/jetbrains/edu/learning/compatibilityUtils.kt
@@ -3,6 +3,9 @@ package com.jetbrains.edu.learning
 import com.intellij.ide.plugins.IdeaPluginDescriptorImpl
 import com.intellij.ide.plugins.contentModules
 import com.intellij.ide.plugins.getMainDescriptor
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.ex.ActionUtil
 
 internal fun collectFromModules(
   pluginDescriptor: IdeaPluginDescriptorImpl,
@@ -11,4 +14,9 @@ internal fun collectFromModules(
   for (module in pluginDescriptor.contentModules) {
     collect(module.getMainDescriptor())
   }
+}
+
+// BACKCOMPAT: 2025.1. Inline it.
+internal fun updateAction(action: AnAction, e: AnActionEvent) {
+  ActionUtil.updateAction(action, e)
 }
