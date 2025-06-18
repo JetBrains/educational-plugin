38ccd09b7666d5be4f09bbc5e3a096b73b554bac
Ilia Posov <ilia.posov@jetbrains.com>
2025-06-18 13:23:44 +0200

    EDU-8236: Service to add information next to the Check button

---
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/taskToolWindow/ui/check/AdditionalInformationPanel.kt
@@ -0,0 +1,92 @@
+package com.jetbrains.edu.learning.taskToolWindow.ui.check
+
+import com.intellij.openapi.project.Project
+import com.intellij.ui.components.JBPanel
+import com.intellij.util.ui.JBUI
+import com.intellij.util.ui.launchOnShow
+import com.jetbrains.edu.learning.taskToolWindow.ui.EduBrowserHyperlinkListener
+import kotlinx.coroutines.flow.collectLatest
+import java.awt.BorderLayout
+import javax.swing.JEditorPane
+import javax.swing.UIManager
+import javax.swing.text.html.HTMLEditorKit
+
+class AdditionalInformationPanel private constructor(
+  private val project: Project
+) : JBPanel<AdditionalInformationPanel>(BorderLayout()) {
+
+  private lateinit var informationText: JEditorPane
+
+  init {
+    border = JBUI.Borders.empty(8, 0)
+
+    createInformationText()
+  }
+
+  fun createInformationText() {
+    informationText = JEditorPane()
+
+    with(informationText) {
+      isEditable = false
+      contentType = "text/html"
+      isFocusable = false
+      border = null
+      background = null
+      margin = JBUI.emptyInsets()
+
+      setupInfoFont()
+
+      addHyperlinkListener(EduBrowserHyperlinkListener.INSTANCE)
+    }
+
+    add(informationText, BorderLayout.CENTER)
+  }
+
+  fun updateSize() {
+    remove(informationText)
+    createInformationText()
+    updateText()
+  }
+
+  private fun JEditorPane.setupInfoFont() {
+    val labelFont = UIManager.getFont("Label.font") ?: return
+    val leftMargin = JBUI.scale(2)
+
+    editorKit = HTMLEditorKit().apply {
+      styleSheet.addRule(
+        """
+        body {
+          font-family: ${labelFont.family};
+          font-size: ${labelFont.size}pt;
+          margin-left: ${leftMargin}px;
+        }
+        """
+      )
+    }
+  }
+
+  private fun updateText() {
+    val texts = CheckButtonAdditionalInformationManager.getInstance(project).additionalInformation.value.allTexts
+    updateText(texts)
+  }
+
+  private fun updateText(texts: String?) {
+    isVisible = texts != null
+    if (texts == null) return
+    informationText.text = "<html>$texts</html>"
+  }
+
+  companion object {
+    fun create(project: Project): AdditionalInformationPanel {
+      val panel = AdditionalInformationPanel(project)
+
+      panel.launchOnShow("AdditionalInformationForCheckButtonWatcher") {
+        CheckButtonAdditionalInformationManager.getInstance(project).additionalInformation.collectLatest {
+          panel.updateText(it.allTexts)
+        }
+      }
+
+      return panel
+    }
+  }
+}
