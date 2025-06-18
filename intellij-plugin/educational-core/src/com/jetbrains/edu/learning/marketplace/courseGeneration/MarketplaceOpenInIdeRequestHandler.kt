753eb9fc1dd329600127648f062db87a85337036
Ilia Posov <ilia.posov@jetbrains.com>
2025-06-18 12:20:05 +0200

    EDU-8236: Notify on LTI Settings changes

--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/marketplace/courseGeneration/MarketplaceOpenInIdeRequestHandler.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/marketplace/courseGeneration/MarketplaceOpenInIdeRequestHandler.kt
@@ -51,10 +51,7 @@ object MarketplaceOpenInIdeRequestHandler : OpenInIdeRequestHandler<MarketplaceO
 
     val ltiSettings = request.ltiSettingsDTO
     if (ltiSettings != null) {
-      val settingsState = LTISettingsManager.instance(project).state
-      settingsState.launchId = ltiSettings.launchId
-      settingsState.lmsDescription = ltiSettings.lmsDescription
-      settingsState.onlineService = ltiSettings.onlineService
+      LTISettingsManager.instance(project).settings = ltiSettings
     }
   }
 
--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/marketplace/lti/LTISettingsManager.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/marketplace/lti/LTISettingsManager.kt
@@ -2,6 +2,9 @@ package com.jetbrains.edu.learning.marketplace.lti
 
 import com.intellij.openapi.components.*
 import com.intellij.openapi.project.Project
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 /**
  * Provides access to LTI settings.
@@ -9,6 +12,41 @@ import com.intellij.openapi.project.Project
 @Service(Service.Level.PROJECT)
 @State(name="LTISettings", reloadable = true, storages = [Storage("lti.xml")])
 class LTISettingsManager : SimplePersistentStateComponent<LTISettings>(LTISettings()) {
+
+  private val _settingsFlow: MutableStateFlow<LTISettingsDTO?> = MutableStateFlow(null)
+
+  val settingsFlow: StateFlow<LTISettingsDTO?>
+    get() = _settingsFlow.asStateFlow()
+
+  var settings: LTISettingsDTO?
+    get() = settingsFlow.value
+    set(value) {
+      with(state) {
+        launchId = value?.launchId
+        lmsDescription = value?.lmsDescription
+        onlineService = value?.onlineService ?: LTIOnlineService.STANDALONE
+      }
+
+      _settingsFlow.value = value
+    }
+
+  override fun loadState(state: LTISettings) {
+    super.loadState(state)
+
+    // update settings flow with the new value
+    val launchId = state.launchId
+    _settingsFlow.value = if (launchId.isNullOrEmpty()) {
+      null
+    }
+    else {
+      LTISettingsDTO(
+        launchId,
+        state.lmsDescription,
+        state.onlineService
+      )
+    }
+  }
+
   companion object {
     fun instance(project: Project) = project.service<LTISettingsManager>()
   }
