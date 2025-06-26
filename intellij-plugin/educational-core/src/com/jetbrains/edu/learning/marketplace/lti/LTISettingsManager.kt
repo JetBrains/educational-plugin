710914f05ce3e9bbcd169145d90a853ef0c22a6f
Ilia Posov <ilia.posov@jetbrains.com>
2025-06-26 13:14:35 +0200

    EDU-8236: Load LTI service when course is set

--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/marketplace/lti/LTISettingsManager.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/marketplace/lti/LTISettingsManager.kt
@@ -2,24 +2,32 @@ package com.jetbrains.edu.learning.marketplace.lti
 
 import com.intellij.openapi.components.*
 import com.intellij.openapi.project.Project
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import com.jetbrains.edu.learning.messages.EduCoreBundle
+import com.jetbrains.edu.learning.taskToolWindow.ui.check.CheckButtonAdditionalInformationManager
+import com.jetbrains.edu.learning.xmlEscaped
 
 /**
  * Provides access to LTI settings.
  */
 @Service(Service.Level.PROJECT)
 @State(name="LTISettings", reloadable = true, storages = [Storage("lti.xml")])
-class LTISettingsManager : SimplePersistentStateComponent<LTISettings>(LTISettings()) {
-
-  private val _settingsFlow: MutableStateFlow<LTISettingsDTO?> = MutableStateFlow(null)
-
-  val settingsFlow: StateFlow<LTISettingsDTO?>
-    get() = _settingsFlow.asStateFlow()
+class LTISettingsManager(private val project: Project) : SimplePersistentStateComponent<LTISettings>(LTISettings()) {
 
   var settings: LTISettingsDTO?
-    get() = settingsFlow.value
+    get() {
+      val launchId = state.launchId
+
+      if (launchId.isNullOrEmpty()) {
+        return null
+      }
+
+      return LTISettingsDTO(
+        launchId,
+        state.lmsDescription,
+        state.onlineService,
+        state.returnLink
+      )
+    }
     set(value) {
       with(state) {
         launchId = value?.launchId
@@ -28,28 +36,38 @@ class LTISettingsManager : SimplePersistentStateComponent<LTISettings>(LTISettin
         returnLink = value?.returnLink
       }
 
-      _settingsFlow.value = value
+      settingsUpdated(value)
     }
 
   override fun loadState(state: LTISettings) {
     super.loadState(state)
 
-    // update settings flow with the new value
-    val launchId = state.launchId
-    _settingsFlow.value = if (launchId.isNullOrEmpty()) {
-      null
+    settingsUpdated(settings)
+  }
+
+  private fun settingsUpdated(settings: LTISettingsDTO?) {
+    val additionalInformationManager = CheckButtonAdditionalInformationManager.getInstance(project)
+
+    if (settings == null) {
+      additionalInformationManager.removeInformation(LTI_ADDITIONAL_INFORMATION_KEY)
+      return
     }
-    else {
-      LTISettingsDTO(
-        launchId,
-        state.lmsDescription,
-        state.onlineService,
-        state.returnLink
-      )
+
+    val lmsDescription = settings.lmsDescription?.xmlEscaped
+    val returnLink = settings.returnLink
+
+    val message = when {
+      lmsDescription.isNullOrEmpty() && !returnLink.isNullOrEmpty() -> EduCoreBundle.message("lti.check.button.hint.link", returnLink)
+      lmsDescription.isNullOrEmpty() -> EduCoreBundle.message("lti.check.button.hint")
+      !returnLink.isNullOrEmpty() -> EduCoreBundle.message("lti.check.button.hint.with.lms.link", returnLink, lmsDescription)
+      else -> EduCoreBundle.message("lti.check.button.hint.with.lms", lmsDescription)
     }
+
+    additionalInformationManager.setInformation(LTI_ADDITIONAL_INFORMATION_KEY, message)
   }
 
   companion object {
-    fun getInstance(project: Project): LTISettingsManager = project.service()
+    fun getInstance(project: Project): LTISettingsManager = project.service<LTISettingsManager>()
+    private const val LTI_ADDITIONAL_INFORMATION_KEY = "lti.check.button.additional.information"
   }
 }
