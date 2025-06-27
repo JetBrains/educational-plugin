b8aa48d84b8710d7018b53b1435c7137fe652d0a
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2025-06-27 14:08:06 +0200

    Make some tests work on 252
    Temporarily add `bundledModule("com.intellij.modules.ultimate")` dependency as a workaround to make tests in corresponding modules work

--- intellij-plugin/Edu-Php/build.gradle.kts
+++ intellij-plugin/Edu-Php/build.gradle.kts
@@ -7,6 +7,11 @@ dependencies {
     intellijIde(ideaVersion)
 
     intellijPlugins(phpPlugin)
+    // Temporary workaround to make test work as expected
+    // For some reason, the corresponding module is not loaded automatically
+    if (isAtLeast252) {
+      bundledModule("com.intellij.modules.ultimate")
+    }
   }
 
   implementation(project(":intellij-plugin:educational-core"))
