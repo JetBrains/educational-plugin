7e8273ee64695375a5ba4e610658811a390d6164
Sofia Biurganovskaia <sofia.biurganovskaia@jetbrains.com>
2025-08-25 16:58:40 +0300

    EDU-8313: switch to the action button instead of hyperlinks

--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/statistics/FeedbackSender.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/statistics/FeedbackSender.kt
@@ -1,24 +1,16 @@
 package com.jetbrains.edu.learning.statistics
 
 import com.intellij.ide.util.PropertiesComponent
-import com.intellij.notification.Notification
 import com.intellij.notification.NotificationType.INFORMATION
 import com.intellij.openapi.actionSystem.ActionManager
-import com.intellij.openapi.actionSystem.ActionUiKind
-import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.actionSystem.CommonDataKeys
-import com.intellij.openapi.actionSystem.ex.ActionUtil
-import com.intellij.openapi.actionSystem.impl.SimpleDataContext
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.NlsContexts.NotificationContent
 import com.intellij.openapi.util.NlsContexts.NotificationTitle
 import com.jetbrains.edu.learning.actions.LeaveInIdeFeedbackAction
 import com.jetbrains.edu.learning.capitalize
 import com.jetbrains.edu.learning.courseFormat.Course
-import com.jetbrains.edu.learning.invokeLater
 import com.jetbrains.edu.learning.messages.EduCoreBundle
 import com.jetbrains.edu.learning.notification.EduNotificationManager
-import javax.swing.event.HyperlinkEvent
 
 fun showCCPostFeedbackNotification(course: Course, project: Project) {
   val language = course.languageId.lowercase().capitalize()
@@ -53,36 +45,9 @@ private fun showFeedbackNotification(
   @NotificationContent content: String,
 ) {
   EduNotificationManager.create(INFORMATION, title, content).apply {
-    setListener { notification, event ->
-      handleSurveyNotificationLinkClick(notification, event, project)
-    }
+    addAction(ActionManager.getInstance().getAction(LeaveInIdeFeedbackAction.ACTION_ID))
+    isSuggestionType = true
   }.notify(project)
-}
-
-private fun handleSurveyNotificationLinkClick(
-  notification: Notification,
-  event: HyperlinkEvent,
-  project: Project
-) {
-  if (event.eventType != HyperlinkEvent.EventType.ACTIVATED || event.description != "action://survey") {
-    return
-  }
-  val actionManager = ActionManager.getInstance()
-  val action = actionManager.getAction(LeaveInIdeFeedbackAction.ACTION_ID)
-
-  if (action != null) {
-    val actionEvent = AnActionEvent.createEvent(
-      SimpleDataContext.builder().add(CommonDataKeys.PROJECT, project).build(),
-      action.templatePresentation.clone(),
-      "FeedbackNotification",
-      ActionUiKind.NONE,
-      null
-    )
-    project.invokeLater {
-      ActionUtil.invokeAction(action, actionEvent) {}
-    }
-  }
-  notification.expire()
 }
 
 fun isLeaveFeedbackPrompted(): Boolean = PropertiesComponent.getInstance().getBoolean(LEAVE_FEEDBACK_NOTIFICATION_SHOWN)
