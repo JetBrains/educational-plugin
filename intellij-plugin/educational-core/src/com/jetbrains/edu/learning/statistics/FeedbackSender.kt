91f740295dd41c80621655404f34354c1fe8c724
Sofia Biurganovskaia <sofia.biurganovskaia@jetbrains.com>
2025-08-26 10:18:24 +0000

    EDU-8313: Refactor the feedback forms available from plugin
    This review refactors the notification shown to users, prompting them to open a web feedback form. It was decided that we do not want to lead the users out of the IDE and instead would rather direct them to the in-IDE form
    
    Merge-request: EDU-MR-4538
    Merged-by: Sofia Biurganovskaia <sofia.biurganovskaia@jetbrains.com>

--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/statistics/FeedbackSender.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/statistics/FeedbackSender.kt
@@ -1,88 +1,45 @@
 package com.jetbrains.edu.learning.statistics
 
 import com.intellij.ide.util.PropertiesComponent
-import com.intellij.notification.Notification
-import com.intellij.notification.NotificationListener
 import com.intellij.notification.NotificationType.INFORMATION
+import com.intellij.openapi.actionSystem.ActionManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.NlsContexts.NotificationContent
 import com.intellij.openapi.util.NlsContexts.NotificationTitle
-import com.intellij.util.PlatformUtils
-import com.jetbrains.edu.learning.EduBrowser
+import com.jetbrains.edu.learning.actions.LeaveInIdeFeedbackAction
 import com.jetbrains.edu.learning.capitalize
 import com.jetbrains.edu.learning.courseFormat.Course
 import com.jetbrains.edu.learning.messages.EduCoreBundle
 import com.jetbrains.edu.learning.notification.EduNotificationManager
-import javax.swing.event.HyperlinkEvent
 
 fun showCCPostFeedbackNotification(course: Course, project: Project) {
   val language = course.languageId.lowercase().capitalize()
-  val feedbackUrl = getFeedbackUrl(course.name, "Educator")
-  showPostFeedbackNotification(project, EduCoreBundle.message("check.correct.solution.title"), EduCoreBundle.message("feedback.template.creator", feedbackUrl, language), feedbackUrl)
-}
-
-fun showStudentPostFeedbackNotification(courseName: String, project: Project) {
-  val feedbackUrl = getFeedbackUrl(courseName, "Learner")
-  showPostFeedbackNotification(project, EduCoreBundle.message("feedback.template.student.title"), EduCoreBundle.message("feedback.template.student", feedbackUrl), feedbackUrl)
-}
-
-private fun getFeedbackUrl(courseName: String, courseMode: String): String {
-  return FEEDBACK_URL_TEMPLATE
-    .replace(
-      "\$PRODUCT", productMap[PlatformUtils.getPlatformPrefix()] ?: PlatformUtils.getPlatformPrefix()
-    )
-    .replace("\$COURSE", courseName)
-    .replace("\$MODE", courseMode)
-}
-
-private fun showPostFeedbackNotification(project: Project, notificationTitle: String, notificationText: String, feedbackUrl: String) {
-  PropertiesComponent.getInstance().setValue(LEAVE_FEEDBACK_NOTIFICATION_SHOWN, true)
-  showFeedbackNotification(
+  showPostFeedbackNotification(
     project,
-    notificationTitle,
-    notificationText,
-    feedbackUrl
+    EduCoreBundle.message("check.correct.solution.title"),
+    EduCoreBundle.message("feedback.template.creator", language)
   )
 }
 
-@Suppress("DEPRECATION")
-private fun showFeedbackNotification(
+fun showStudentPostFeedbackNotification(project: Project) = showPostFeedbackNotification(
+  project,
+  EduCoreBundle.message("feedback.template.student.title"),
+  EduCoreBundle.message("feedback.template.student")
+)
+
+private fun showPostFeedbackNotification(
   project: Project,
   @NotificationTitle title: String,
   @NotificationContent content: String,
-  feedbackUrl: String
 ) {
-  EduNotificationManager
-    .create(INFORMATION, title, content)
-    .setListener(object : NotificationListener.Adapter() {
-      override fun hyperlinkActivated(notification: Notification, e: HyperlinkEvent) {
-        EduBrowser.getInstance().browse(feedbackUrl)
-      }
-    })
+  PropertiesComponent.getInstance().setValue(LEAVE_FEEDBACK_NOTIFICATION_SHOWN, true)
+  EduNotificationManager.create(INFORMATION, title, content)
+    .addAction(ActionManager.getInstance().getAction(LeaveInIdeFeedbackAction.ACTION_ID))
+    .setSuggestionType(true)
     .notify(project)
 }
 
-fun isLeaveFeedbackPrompted() : Boolean = PropertiesComponent.getInstance().getBoolean(LEAVE_FEEDBACK_NOTIFICATION_SHOWN)
+fun isLeaveFeedbackPrompted(): Boolean = PropertiesComponent.getInstance().getBoolean(LEAVE_FEEDBACK_NOTIFICATION_SHOWN)
 
 private const val LEAVE_FEEDBACK_NOTIFICATION_SHOWN = "questionnaireAdvertisingNotification"
 
-private const val FEEDBACK_URL_TEMPLATE = "https://www.jetbrains.com/feedback/feedback.jsp?product=EduTools&ide=\$PRODUCT&course=\$COURSE&mode=\$MODE"
-
-@Suppress("UnstableApiUsage")
-private val productMap = hashMapOf(
-  PlatformUtils.PYCHARM_CE_PREFIX to "PCC",
-  PlatformUtils.PYCHARM_PREFIX to "PCP",
-  PlatformUtils.PYCHARM_EDU_PREFIX to "PCE",
-  PlatformUtils.IDEA_CE_PREFIX to "IIC",
-  PlatformUtils.IDEA_PREFIX to "IIU",
-  "AndroidStudio" to "AI",
-  PlatformUtils.WEB_PREFIX to "WS",
-  PlatformUtils.PHP_PREFIX to "PS",
-  PlatformUtils.APPCODE_PREFIX to "AC",
-  PlatformUtils.CLION_PREFIX to "CL",
-  PlatformUtils.DBE_PREFIX to "DG",
-  PlatformUtils.GOIDE_PREFIX to "GO",
-  PlatformUtils.RIDER_PREFIX to "RD",
-  PlatformUtils.RUBY_PREFIX to "RM",
-  PlatformUtils.DATASPELL_PREFIX to "DS"
-)
