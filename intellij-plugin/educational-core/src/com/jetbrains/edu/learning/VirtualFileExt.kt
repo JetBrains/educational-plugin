cb7920cb2ea51fe808917423d64974c6462ee033
Ilia Posov <ilia.posov@jetbrains.com>
2025-09-04 17:04:43 +0200

    EDU-7821: Get rid of excludeFromArchive() usages

--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/VirtualFileExt.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/VirtualFileExt.kt
@@ -31,7 +31,8 @@ import com.intellij.util.ui.UIUtil
 import com.jetbrains.edu.coursecreator.actions.BinaryContentsFromDisk
 import com.jetbrains.edu.coursecreator.archive.FailedToProcessEduFileAsTextualException
 import com.jetbrains.edu.learning.EduDocumentListener.Companion.runWithListener
-import com.jetbrains.edu.learning.configuration.excludeFromArchive
+import com.jetbrains.edu.learning.configuration.ArchiveInclusionPolicy
+import com.jetbrains.edu.learning.configuration.courseFileAttributes
 import com.jetbrains.edu.learning.courseFormat.*
 import com.jetbrains.edu.learning.courseFormat.EduFormatNames.TASK
 import com.jetbrains.edu.learning.courseFormat.ext.configurator
@@ -171,7 +172,7 @@ fun VirtualFile.canBeAddedToTask(project: Project): Boolean {
   if (isDirectory) return false
   val course = getContainingTask(project)?.course ?: return false
   val configurator = course.configurator ?: return false
-  return if (configurator.excludeFromArchive(project, this)) false else !belongsToTask(project)
+  return if (configurator.courseFileAttributes(project, this).archiveInclusionPolicy == ArchiveInclusionPolicy.MUST_EXCLUDE) false else !belongsToTask(project)
 }
 
 /**
