f097ad8bdfde1ff796c359be174018afbcb8f14e
Ilia Posov <ilia.posov@jetbrains.com>
2025-10-15 09:58:25 +0200

    EDU-8173: Interrupt Zhaba on resize
    On frame resize, zoom, interrupt Zhaba's animation. Then make Zhaba rerun the current step.
    Effectively, if Zhaba is showing step N, after the resize zhaba finished its animation and then prepares to run the step N again. By reruning the step, it again computes its position in a resized window and moves there.

--- intellij-plugin/features/ide-onboarding/src/com/jetbrains/edu/uiOnboarding/EduUiOnboardingExecutor.kt
+++ intellij-plugin/features/ide-onboarding/src/com/jetbrains/edu/uiOnboarding/EduUiOnboardingExecutor.kt
@@ -1,11 +1,13 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.jetbrains.edu.uiOnboarding
 
+import com.intellij.ide.ui.UISettingsListener
 import com.intellij.notification.NotificationGroupManager
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.actionSystem.ActionManager
 import com.intellij.openapi.actionSystem.IdeActions
 import com.intellij.openapi.application.EDT
+import com.intellij.openapi.observable.util.addComponentListener
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.MessageType
 import com.intellij.openapi.util.Disposer
@@ -19,6 +21,8 @@ import com.jetbrains.edu.uiOnboarding.transitions.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.awt.event.ComponentAdapter
+import java.awt.event.ComponentEvent
 import javax.swing.JComponent
 import javax.swing.JLayeredPane
 
@@ -33,14 +37,34 @@ class EduUiOnboardingExecutor(
   private val disposable = Disposer.newCheckedDisposable()
 
   private var curStepId: String? = null
-  private var curStepStartMillis: Long? = null
+
+  private var currentZhabaComponent: ZhabaComponent? = null
 
   init {
     Disposer.register(parentDisposable, disposable)
+
+    // Listen to IDE resize
+    WindowManager.getInstance().getFrame(project)?.addComponentListener(parentDisposable = parentDisposable, object : ComponentAdapter() {
+      override fun componentResized(e: ComponentEvent?) {
+        changeZhabaLocation()
+      }
+    })
+
+    // Listen to IDE Zoom changes and other changes that might affect UI components positions and sizes
+    project.messageBus.connect(parentDisposable).subscribe(UISettingsListener.TOPIC, UISettingsListener {
+      changeZhabaLocation()
+    })
   }
 
   suspend fun start() {
     runStep(0)
+  }
+
+  /**
+   * Notify zhaba that it has to change its location because of movements of UI components.
+   */
+  private fun changeZhabaLocation() {
+    currentZhabaComponent?.stop()
   }
 
   private suspend fun runStep(ind: Int, previousStepId: String? = null, previousData: EduUiOnboardingStepData? = null) {
@@ -48,21 +72,53 @@ class EduUiOnboardingExecutor(
       return
     }
 
-    val (stepId, step) = steps[ind]
     val stepDisposable = Disposer.newCheckedDisposable()
     Disposer.register(disposable, stepDisposable)
 
-    val stepStartMillis = System.currentTimeMillis()
+    fun finishStep() {
+      Disposer.dispose(stepDisposable)
+    }
+
+    val (stepId, step) = steps[ind]
+
     curStepId = stepId
-    curStepStartMillis = stepStartMillis
 
     val gotItData = step.performStep(project, animationData, stepDisposable)
     if (gotItData == null || !gotItData.tooltipPoint.component.isShowing) {
+      finishStep()
       runStep(ind + 1, previousStepId, previousData)
       return
     }
+
+    /**
+     * If component positions have changes, Zhaba may sit in the wrong place.
+     * Rerunning the current step makes Zhaba to find the correct position and to move there.
+     */
+    suspend fun rerunCurrentStep() {
+      finishStep()
+      runStep(ind, stepId, gotItData)
+    }
+
+    fun proceedToStep(ind: Int, previousStepId: String?, previousData: EduUiOnboardingStepData?) {
+      finishStep()
+      cs.launch(Dispatchers.EDT) {
+        runStep(ind, previousStepId, previousData)
+      }
+    }
+
+    fun proceedToFinishOnboarding(data: EduUiOnboardingStepData, isHappy: Boolean) {
+      finishStep()
+      cs.launch(Dispatchers.EDT) {
+        finishOnboarding(data, isHappy)
+      }
+    }
+
     if (previousData != null && previousStepId != null) {
-      animateTransitionBetweenSteps(previousStepId, stepId, previousData, gotItData)
+      val completed = animateTransitionBetweenSteps(previousStepId, stepId, previousData, gotItData)
+      if (!completed) {
+        rerunCurrentStep()
+        return
+      }
     }
 
     val showInCenter = gotItData.position == null
@@ -74,10 +130,7 @@ class EduUiOnboardingExecutor(
 
     builder.onEscapePressed {
       EduCounterUsageCollector.uiOnboardingSkipped(ind, stepId)
-      Disposer.dispose(stepDisposable)
-      cs.launch(Dispatchers.EDT) {
-        finishOnboarding(gotItData, isHappy = false)
-      }
+      proceedToFinishOnboarding(gotItData, isHappy = false)
     }.requestFocus(true)
 
     if (ind < steps.lastIndex) {
@@ -85,31 +138,19 @@ class EduUiOnboardingExecutor(
         if (ind == 0) {
           EduCounterUsageCollector.uiOnboardingStarted()
         }
-        Disposer.dispose(stepDisposable)
-        cs.launch(Dispatchers.EDT) {
-          runStep(ind + 1, curStepId, gotItData)
-        }
+        proceedToStep(ind + 1, curStepId, gotItData)
       }.withSecondaryButton(EduUiOnboardingBundle.message("gotIt.button.skipAll")) {
         EduCounterUsageCollector.uiOnboardingSkipped(ind, stepId)
-        Disposer.dispose(stepDisposable)
-        cs.launch(Dispatchers.EDT) {
-          finishOnboarding(gotItData, isHappy = false)
-        }
+        proceedToFinishOnboarding(gotItData, isHappy = false)
       }
     }
     else {
       builder.withButtonLabel(EduUiOnboardingBundle.message("gotIt.button.finish")).withContrastButton(true).onButtonClick {
         EduCounterUsageCollector.uiOnboardingFinished()
-        cs.launch(Dispatchers.EDT) {
-          Disposer.dispose(stepDisposable)
-          finishOnboarding(gotItData, isHappy = true)
-        }
+        proceedToFinishOnboarding(gotItData, isHappy = true)
       }.withSecondaryButton(EduUiOnboardingBundle.message("gotIt.button.restart")) {
         EduCounterUsageCollector.uiOnboardingRelaunched(UiOnboardingRelaunchLocation.TOOLTIP_RESTART_BUTTON)
-        Disposer.dispose(stepDisposable)
-        cs.launch(Dispatchers.EDT) {
-          runStep(1, curStepId, gotItData)
-        }
+        proceedToStep(1, curStepId, gotItData)
       }
     }
 
@@ -125,20 +166,24 @@ class EduUiOnboardingExecutor(
       balloon.show(gotItData.tooltipPoint, gotItData.position)
     }
 
-    showZhaba(gotItData.zhaba)
+    val completed = showZhaba(gotItData.zhaba)
+    if (!completed) {
+      rerunCurrentStep()
+    }
   }
 
-  private suspend fun showZhaba(zhaba: ZhabaComponent) {
+  private suspend fun showZhaba(zhaba: ZhabaComponent): Boolean {
     val frame = WindowManager.getInstance().getFrame(project)!!
     frame.layeredPane.add(zhaba, JLayeredPane.PALETTE_LAYER, -1)
     zhaba.setBounds(0, 0, frame.width, frame.height)
-    zhaba.start()
+    currentZhabaComponent = zhaba
+    return zhaba.start()
   }
 
   private suspend fun animateTransitionBetweenSteps(
     fromStepId: String, toStepId: String, fromData: EduUiOnboardingStepData, toData: EduUiOnboardingStepData
-  ) {
-    val frame = WindowManager.getInstance().getFrame(project) ?: return
+  ): Boolean {
+    val frame = WindowManager.getInstance().getFrame(project) ?: return true
 
     val fromPoint = fromData.zhabaPoint
     val toPoint = toData.zhabaPoint
@@ -151,8 +196,10 @@ class EduUiOnboardingExecutor(
     zhaba.setBounds(0, 0, frame.width, frame.height)
     zhaba.animation = transitionAnimation
 
-    zhaba.start()
+    currentZhabaComponent = zhaba
+    val success = zhaba.start()
     Disposer.dispose(zhaba)
+    return success
   }
 
   private fun getTransition(
