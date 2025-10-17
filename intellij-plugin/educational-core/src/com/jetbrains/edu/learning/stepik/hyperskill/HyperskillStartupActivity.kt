ed6941e29a66895267d206792337a90dacb2a7a0
Gleb Oborin <gleb.oborin@jetbrains.com>
2025-10-17 15:59:02 +0200

    EDU-8468: Add new hyperskill plugin info notification

--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/stepik/hyperskill/HyperskillStartupActivity.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/stepik/hyperskill/HyperskillStartupActivity.kt
@@ -1,8 +1,14 @@
 package com.jetbrains.edu.learning.stepik.hyperskill
 
+import com.intellij.ide.util.RunOnceUtil
+import com.intellij.notification.Notification
+import com.intellij.notification.NotificationType
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.StartupActivity
+import com.jetbrains.edu.learning.EduBrowser
 import com.jetbrains.edu.learning.EduLogInListener
 import com.jetbrains.edu.learning.EduUtilsKt.isStudentProject
 import com.jetbrains.edu.learning.StudyTaskManager
@@ -10,6 +16,8 @@ import com.jetbrains.edu.learning.isUnitTestMode
 import com.jetbrains.edu.learning.stepik.hyperskill.api.HyperskillConnector
 import com.jetbrains.edu.learning.stepik.hyperskill.api.HyperskillSolutionLoader
 import com.jetbrains.edu.learning.courseFormat.hyperskill.HyperskillCourse
+import com.jetbrains.edu.learning.messages.EduCoreBundle
+import com.jetbrains.edu.learning.notification.EduNotificationManager
 import com.jetbrains.edu.learning.stepik.hyperskill.update.HyperskillCourseUpdateChecker
 import com.jetbrains.edu.learning.submissions.SubmissionsManager
 import com.jetbrains.edu.learning.taskToolWindow.ui.TaskToolWindowView
@@ -43,9 +51,29 @@ class HyperskillStartupActivity : StartupActivity {
 
     synchronizeTopics(project, course)
     HyperskillCourseUpdateChecker.getInstance(project).check()
+
+    showNewHyperskillPluginInfoNotification(project)
+  }
+
+  private fun showNewHyperskillPluginInfoNotification(project: Project) {
+    RunOnceUtil.runOnceForApp(NEW_HYPERSKILL_PLUGIN_NOTIFICATION_KEY) {
+      EduNotificationManager.create(
+        type = NotificationType.WARNING,
+        title = EduCoreBundle.message("hyperskill.new.plugin.notification.title"),
+        content = EduCoreBundle.message("hyperskill.new.plugin.notification.text"),
+      ).addAction(object : AnAction(EduCoreBundle.message("hyperskill.new.plugin.notification.action.text")) {
+        override fun actionPerformed(e: AnActionEvent) {
+          EduBrowser.getInstance().browse(NEW_HYPERSKILL_PLUGIN_INFO_LINK)
+          e.getData(Notification.KEY)?.expire()
+        }
+      }).notify(project)
+    }
   }
 
   companion object {
+    private const val NEW_HYPERSKILL_PLUGIN_NOTIFICATION_KEY = "edu.new.hyperskill.plugin.notification"
+    private const val NEW_HYPERSKILL_PLUGIN_INFO_LINK = "https://jb.gg/academy/hyperskill/plugin"
+
     fun synchronizeTopics(project: Project, hyperskillCourse: HyperskillCourse) {
       ApplicationManager.getApplication().executeOnPooledThread {
         HyperskillConnector.getInstance().fillTopics(project, hyperskillCourse)
