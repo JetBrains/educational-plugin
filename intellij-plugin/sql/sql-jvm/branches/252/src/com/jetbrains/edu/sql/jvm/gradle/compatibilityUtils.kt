bec6e230ed878616bab9cd169f6f6297b7e2c73f
Gleb Oborin <gleb.oborin@jetbrains.com>
2025-10-22 06:54:44 +0200

    Use default context for database artifact loader
    In EAP6, `DatabaseArtifactContext.getInstance(project, driver)` was removed.
    In corresponding code in `DataSourceScriptInterpreter`, default context is used now.
    
    (cherry picked from commit 3ec368c24b6b4e1ec8d38309c595e0a964ff7db6)

--- intellij-plugin/sql/sql-jvm/branches/252/src/com/jetbrains/edu/sql/jvm/gradle/compatibilityUtils.kt
+++ intellij-plugin/sql/sql-jvm/branches/252/src/com/jetbrains/edu/sql/jvm/gradle/compatibilityUtils.kt
@@ -1,10 +1,10 @@
 package com.jetbrains.edu.sql.jvm.gradle
 
-import com.intellij.database.dataSource.DatabaseDriver
 import com.intellij.database.dataSource.artifacts.DatabaseArtifactList
 import com.intellij.database.dataSource.artifacts.DatabaseArtifactLoader
-import com.intellij.openapi.project.Project
 
-fun DatabaseArtifactLoader.isValid(project: Project, version: DatabaseArtifactList.ArtifactVersion, driver: DatabaseDriver): Boolean = isValid(version)
+fun isValid(loader: DatabaseArtifactLoader, version: DatabaseArtifactList.ArtifactVersion): Boolean = loader.isValid(version)
 
-fun DatabaseArtifactLoader.downloadArtifact(project: Project, artifact: DatabaseArtifactList.ArtifactVersion, driver: DatabaseDriver) = downloadArtifact(artifact)
+fun downloadArtifact(loader: DatabaseArtifactLoader, artifact: DatabaseArtifactList.ArtifactVersion) {
+  loader.downloadArtifact(artifact)
+}
