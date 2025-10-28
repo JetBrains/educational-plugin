6d217d9040fbcf3f5c61d458af38ab5d43072e2c
Ilia Posov <ilia.posov@jetbrains.com>
2025-10-28 07:21:07 +0100

    fix BottomToTopAppearance.kt

--- intellij-plugin/features/ide-onboarding/src/com/jetbrains/edu/uiOnboarding/transitions/BottomToTopAppearance.kt
+++ intellij-plugin/features/ide-onboarding/src/com/jetbrains/edu/uiOnboarding/transitions/BottomToTopAppearance.kt
@@ -2,6 +2,7 @@ package com.jetbrains.edu.uiOnboarding.transitions
 
 import com.intellij.ui.awt.RelativePoint
 import com.jetbrains.edu.uiOnboarding.EduUiOnboardingAnimation
+import com.jetbrains.edu.uiOnboarding.EduUiOnboardingAnimationData.Companion.zhabaScale
 import com.jetbrains.edu.uiOnboarding.EduUiOnboardingAnimationData.Companion.zhabaScaleWithoutIDEScale
 import com.jetbrains.edu.uiOnboarding.EduUiOnboardingAnimationStep
 import com.jetbrains.edu.uiOnboarding.TransitionType
@@ -13,7 +14,8 @@ class BottomToTopAppearance(nextAnimation: EduUiOnboardingAnimation?, toPoint: R
   override val steps: List<EduUiOnboardingAnimationStep> = listOf(computeStep(nextAnimation, toPoint))
 
   private fun computeStep(animation: EduUiOnboardingAnimation?, toPoint: RelativePoint): EduUiOnboardingAnimationStep {
-    val firstFrame = animation?.steps?.firstOrNull()?.image ?: error("No animation frame on Zhaba appearance")
+    val firstStep = animation?.steps?.firstOrNull() ?: error("No animation frame on Zhaba appearance")
+    val firstFrame = firstStep.image
 
     val width = zhabaScaleWithoutIDEScale(firstFrame.getWidth(null))
     val height = zhabaScaleWithoutIDEScale(firstFrame.getHeight(null))
@@ -26,7 +28,13 @@ class BottomToTopAppearance(nextAnimation: EduUiOnboardingAnimation?, toPoint: R
       toPoint,
       200,
       transitionType = TransitionType.EASE_OUT,
-      visibleBounds = Rectangle(0, 0, width, height)
+      visibleBounds = Rectangle(
+        -zhabaScale(firstStep.imageShift.x),
+        -zhabaScale(firstStep.imageShift.y),
+        width,
+        height
+      ),
+      imageShift = firstStep.imageShift
     )
   }
 
