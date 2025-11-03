df5dcae4caf77215235baff04d32324c56b272be
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2025-11-03 20:43:26 +0200

    EDU-8566: update the list of courses to accept them in statistics validation

--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/statistics/EduFields.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/statistics/EduFields.kt
@@ -25,7 +25,10 @@ object EduFields {
     17654,  // https://plugins.jetbrains.com/plugin/17654-atomickotlin
     18302,  // https://plugins.jetbrains.com/plugin/18302-python-libraries--numpy
     18392,  // https://plugins.jetbrains.com/plugin/18392-machine-learning-101
+    18905,  // https://plugins.jetbrains.com/plugin/18905-java-for-beginners
+    19933,  // https://plugins.jetbrains.com/plugin/19933-liu-course-java-oop
     20995,  // https://plugins.jetbrains.com/plugin/20995-c-basics
+    21005,  // https://plugins.jetbrains.com/plugin/21005-java-programming-basics
     21067,  // https://plugins.jetbrains.com/plugin/21067-kotlin-onboarding-introduction
     21188,  // https://plugins.jetbrains.com/plugin/21188-reinforcement-learning-maze-solver
     21913,  // https://plugins.jetbrains.com/plugin/21913-kotlin-onboarding-object-oriented-programming
@@ -34,9 +37,18 @@ object EduFields {
     23048,  // https://plugins.jetbrains.com/plugin/23048-practical-ide-code-refactoring-in-kotlin
     23135,  // https://plugins.jetbrains.com/plugin/23135-tour-of-go
     23312,  // https://plugins.jetbrains.com/plugin/23312-coroutines-and-channels
+    23369,  // https://plugins.jetbrains.com/plugin/23369-kotlin-onboarding-collections
     23536,  // https://plugins.jetbrains.com/plugin/23536-ui-test-automation-with-selenium-and-python
     23833,  // https://plugins.jetbrains.com/plugin/23833-functional-programming-in-scala
-    24051,  // https://plugins.jetbrains.com/plugin/24051-introduction-to-ide-code-refactoring-in-java
+    24051,  // https://plugins.jetbrains.com/plugin/24051-introduction-to-ide-code-refactoring-in-java,
+    25097,  // https://plugins.jetbrains.com/plugin/25097-building-a-multicomponent-flask-app
+    25212,  // https://plugins.jetbrains.com/plugin/25212-100-days-of-code--the-complete-python-pro-bootcamp
+    25398,  // https://plugins.jetbrains.com/plugin/25398-ide-plugin-development-course
+    26697,  // https://plugins.jetbrains.com/plugin/26697-introduction-to-javascript-programming
+    27682,  // https://plugins.jetbrains.com/plugin/27682-learning-python-in-fragmented-time-basics-
+    27687,  // https://plugins.jetbrains.com/plugin/27687-full-stack-javascript-for-beginners
+    27805,  // https://plugins.jetbrains.com/plugin/27805-100-exercises-to-learn-rust
+    27941,  // https://plugins.jetbrains.com/plugin/27941-data-visualization-in-python
   ))
   val TASK_ID_FIELD = EventFields.Int(TASK_ID)
   val COURSE_UPDATE_VERSION_FIELD = EventFields.Int(MARKETPLACE_COURSE_VERSION)
