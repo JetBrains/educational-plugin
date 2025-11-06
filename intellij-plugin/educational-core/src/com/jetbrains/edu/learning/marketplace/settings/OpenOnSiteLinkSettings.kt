52f8d56f3cfb24bd793d93284132a1516c13847b
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2025-11-06 13:42:13 +0200

    EDU-8571: add internal LC staging host into trusted hosts

--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/marketplace/settings/OpenOnSiteLinkSettings.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/marketplace/settings/OpenOnSiteLinkSettings.kt
@@ -29,6 +29,7 @@ class OpenOnSiteLinkSettings : SimplePersistentStateComponent<EduTrackLinkSettin
       HYPERSKILL_DEFAULT_HOST to EduCoreBundle.lazyMessage("action.open.on.text", EduNames.JBA),
       "academy.jetbrains.com" to EduCoreBundle.lazyMessage("action.open.in.course.catalog"),
       "jetbrains-academy-staging-external.labs.jb.gg" to EduCoreBundle.lazyMessage("action.open.in.course.catalog"),
+      "staging.academy.labs.jb.gg" to EduCoreBundle.lazyMessage("action.open.in.course.catalog"),
     )
 
     fun getInstance(project: Project): OpenOnSiteLinkSettings = project.service<OpenOnSiteLinkSettings>()
