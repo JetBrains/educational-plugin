66577b7f96ccf49285ebadb8aa576088fe7565dd
Gleb Oborin <gleb.oborin@jetbrains.com>
2025-11-10 18:57:54 +0100

    EDU-8576: Use legacy Gradle templates for learner

--- intellij-plugin/Edu-Scala/testSrc/com/jetbrains/edu/scala/hyperskill/ScalaHyperskillCourseGenerationTest.kt
+++ intellij-plugin/Edu-Scala/testSrc/com/jetbrains/edu/scala/hyperskill/ScalaHyperskillCourseGenerationTest.kt
@@ -1,6 +1,7 @@
 package com.jetbrains.edu.scala.hyperskill
 
 import com.jetbrains.edu.jvm.gradle.GradleCourseBuilderBase.Companion.HYPERSKILL_SETTINGS_GRADLE_TEMPLATE_NAME
+import com.jetbrains.edu.jvm.gradle.GradleCourseBuilderBase.Companion.LEGACY_TEMPLATE_PREFIX
 import com.jetbrains.edu.learning.EduTestCase
 import com.jetbrains.edu.learning.courseFormat.hyperskill.HyperskillCourse
 import com.jetbrains.edu.learning.courseGeneration.GeneratorUtils
@@ -19,7 +20,8 @@ class ScalaHyperskillCourseGenerationTest : EduTestCase() {
       language = ScalaLanguage.INSTANCE
     ) {}
     val actualBuildGradleContent = findFile(DEFAULT_SCRIPT_NAME).document.text
-    val expectedBuildGradleContent = GeneratorUtils.getInternalTemplateText(SCALA_HYPERSKILL_BUILD_GRADLE_TEMPLATE_NAME)
+    val templateName = LEGACY_TEMPLATE_PREFIX + SCALA_HYPERSKILL_BUILD_GRADLE_TEMPLATE_NAME
+    val expectedBuildGradleContent = GeneratorUtils.getInternalTemplateText(templateName)
 
     assertEquals(expectedBuildGradleContent, actualBuildGradleContent)
   }
@@ -31,7 +33,8 @@ class ScalaHyperskillCourseGenerationTest : EduTestCase() {
       language = ScalaLanguage.INSTANCE
     ) {}
     val actualSettingsGradleContent = findFile(SETTINGS_FILE_NAME).document.text
-    val expectedSettingsGradleContent = GeneratorUtils.getInternalTemplateText(HYPERSKILL_SETTINGS_GRADLE_TEMPLATE_NAME)
+    val templateName = LEGACY_TEMPLATE_PREFIX + HYPERSKILL_SETTINGS_GRADLE_TEMPLATE_NAME
+    val expectedSettingsGradleContent = GeneratorUtils.getInternalTemplateText(templateName)
 
     assertEquals(expectedSettingsGradleContent, actualSettingsGradleContent)
   }
