05952f08504807c3f78d72329178660c37d98df0
Ilia Posov <ilia.posov@jetbrains.com>
2025-11-14 12:34:03 +0100

    EDU-8472: Introduce GotItBalloonGraphData

---
+++ intellij-plugin/features/ide-onboarding/src/com/jetbrains/edu/uiOnboarding/GotItBalloonGraphData.kt
@@ -0,0 +1,8 @@
+package com.jetbrains.edu.uiOnboarding
+
+import com.jetbrains.edu.uiOnboarding.stepsGraph.GraphData
+
+open class GotItBalloonGraphData(
+  val stepIndex: Int?,
+  val totalSteps: Int
+): GraphData
