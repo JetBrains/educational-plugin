021d4d0b4923ea406950a5457c213d46a3a63b0c
Ilia Posov <ilia.posov@jetbrains.com>
2025-11-14 16:12:34 +0100

    EDU-8472: Unify animations to jump down at the end
    The Toad could jump away either with a sad image or with a happy one. Previously, we had separate ZhabaStep objects for both situations, separate ZhabaData objects for both situations, and two separate checks in Transition Animator. We are going to add one more jumping away, with the image where the Toad has a hat.
    
     The fix removes the duplication. Now there is only one ZhabaData class for jumping away. This data contains the image, either sad or happy.

---
+++ intellij-plugin/features/ide-onboarding/src/com/jetbrains/edu/uiOnboarding/stepsGraph/commonZhabaData.kt
@@ -0,0 +1,6 @@
+package com.jetbrains.edu.uiOnboarding.stepsGraph
+
+import com.jetbrains.edu.uiOnboarding.ZhabaImage
+
+object StartZhabaData : ZhabaData
+data class JumpingAwayZhabaData(val zhabaImage: ZhabaImage) : ZhabaData
