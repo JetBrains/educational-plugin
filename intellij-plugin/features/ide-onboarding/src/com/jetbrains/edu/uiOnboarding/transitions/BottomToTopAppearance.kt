8ad0fafdadfabb920727bac83e47fdb0ae609c2d
Ilia Posov <ilia.posov@jetbrains.com>
2025-11-14 14:08:35 +0100

    EDU-8472: Introduce ZhabaImage
    It stores the image-shift logic that has been previously stored in animation steps. And it also stores all the image sizes, so that there is no need to scale lengthes when using the image.

--- intellij-plugin/features/ide-onboarding/src/com/jetbrains/edu/uiOnboarding/transitions/BottomToTopAppearance.kt
+++ intellij-plugin/features/ide-onboarding/src/com/jetbrains/edu/uiOnboarding/transitions/BottomToTopAppearance.kt
@@ -2,8 +2,6 @@ package com.jetbrains.edu.uiOnboarding.transitions
 
 import com.intellij.ui.awt.RelativePoint
 import com.jetbrains.edu.uiOnboarding.EduUiOnboardingAnimation
-import com.jetbrains.edu.uiOnboarding.EduUiOnboardingAnimationData.Companion.zhabaScale
-import com.jetbrains.edu.uiOnboarding.EduUiOnboardingAnimationData.Companion.zhabaScaleWithoutIDEScale
 import com.jetbrains.edu.uiOnboarding.EduUiOnboardingAnimationStep
 import com.jetbrains.edu.uiOnboarding.TransitionType
 import java.awt.Point
@@ -17,10 +15,9 @@ class BottomToTopAppearance(nextAnimation: EduUiOnboardingAnimation?, toPoint: R
     val firstStep = animation?.steps?.firstOrNull() ?: error("No animation frame on Zhaba appearance")
     val firstFrame = firstStep.image
 
-    val width = zhabaScaleWithoutIDEScale(firstFrame.getWidth(null))
-    val height = zhabaScaleWithoutIDEScale(firstFrame.getHeight(null))
+    val imageScreenSize = firstFrame.screenSize
 
-    val shiftedPoint = Point(toPoint.originalPoint.x, toPoint.originalPoint.y + height)
+    val shiftedPoint = Point(toPoint.originalPoint.x, toPoint.originalPoint.y + imageScreenSize.height)
 
     return EduUiOnboardingAnimationStep(
       firstFrame,
@@ -29,12 +26,11 @@ class BottomToTopAppearance(nextAnimation: EduUiOnboardingAnimation?, toPoint: R
       200,
       transitionType = TransitionType.EASE_OUT,
       visibleBounds = Rectangle(
-        -zhabaScale(firstStep.imageShift.x),
-        -zhabaScale(firstStep.imageShift.y),
-        width,
-        height
-      ),
-      imageShift = firstStep.imageShift
+        -firstStep.image.screenShiftX,
+        -firstStep.image.screenShiftY,
+        imageScreenSize.width,
+        imageScreenSize.height
+      )
     )
   }
 
--- intellij-plugin/features/ide-onboarding/src/com/jetbrains/edu/uiOnboarding/transitions/JumpLeft.kt
+++ intellij-plugin/features/ide-onboarding/src/com/jetbrains/edu/uiOnboarding/transitions/JumpLeft.kt
@@ -6,12 +6,11 @@ import com.jetbrains.edu.uiOnboarding.EduUiOnboardingAnimationData
 import com.jetbrains.edu.uiOnboarding.EduUiOnboardingAnimationData.Companion.JUMP_DURATION
 import com.jetbrains.edu.uiOnboarding.EduUiOnboardingAnimationStep
 import com.jetbrains.edu.uiOnboarding.TransitionType
-import java.awt.Point
 
 class JumpLeft(data: EduUiOnboardingAnimationData, fromPoint: RelativePoint, toPoint: RelativePoint) : EduUiOnboardingAnimation {
   override val steps: List<EduUiOnboardingAnimationStep> = listOf(
     EduUiOnboardingAnimationStep(data.jumpLeft1, fromPoint, fromPoint, 500),
-    EduUiOnboardingAnimationStep(data.jumpLeft2, fromPoint, toPoint, JUMP_DURATION, Point(0, 100), TransitionType.EASE_OUT),
+    EduUiOnboardingAnimationStep(data.jumpLeft2, fromPoint, toPoint, JUMP_DURATION, TransitionType.EASE_OUT),
     EduUiOnboardingAnimationStep(data.lookUp, toPoint, toPoint, 500)
   )
 
