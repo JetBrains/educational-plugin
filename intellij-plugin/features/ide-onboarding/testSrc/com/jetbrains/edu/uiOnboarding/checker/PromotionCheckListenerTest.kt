22adb767ca6fdd70b1222b0ed6058367d3486fea
Ilia Posov <ilia.posov@jetbrains.com>
2025-11-18 13:10:50 +0100

    EDU-8472: Test Student Pack Promotion by the Toad

--- intellij-plugin/features/ide-onboarding/testSrc/com/jetbrains/edu/uiOnboarding/checker/PromotionCheckListenerTest.kt
+++ intellij-plugin/features/ide-onboarding/testSrc/com/jetbrains/edu/uiOnboarding/checker/PromotionCheckListenerTest.kt
@@ -1,31 +1,45 @@
 package com.jetbrains.edu.uiOnboarding.checker
 
 import com.intellij.ide.util.PropertiesComponent
-import com.intellij.notification.Notification
-import com.intellij.openapi.actionSystem.AnAction
-import com.intellij.openapi.actionSystem.impl.SimpleDataContext
 import com.intellij.util.application
 import com.intellij.util.asSafely
-import com.jetbrains.edu.learning.EduBrowser
-import com.jetbrains.edu.learning.EduTestCase
-import com.jetbrains.edu.learning.MockEduBrowser
+import com.jetbrains.edu.learning.*
 import com.jetbrains.edu.learning.actions.CheckAction
-import com.jetbrains.edu.learning.findTask
-import com.jetbrains.edu.learning.mockService
-import com.jetbrains.edu.learning.notification.EduNotificationManager
-import com.jetbrains.edu.learning.testAction
 import com.jetbrains.edu.learning.ui.getUICheckLabel
-import io.mockk.clearAllMocks
-import io.mockk.every
-import io.mockk.justRun
-import io.mockk.mockk
-import io.mockk.mockkObject
-import io.mockk.slot
-import io.mockk.verify
-import org.junit.Ignore
+import com.jetbrains.edu.uiOnboarding.GotItBalloonGraphData
+import com.jetbrains.edu.uiOnboarding.TransitionAnimator
+import com.jetbrains.edu.uiOnboarding.steps.ZhabaStepFactory
+import com.jetbrains.edu.uiOnboarding.stepsGraph.ZhabaStep.Companion.FINISH_TRANSITION
+import io.mockk.*
 import org.junit.Test
 
 class PromotionCheckListenerTest : EduTestCase() {
+
+  override fun setUp() {
+    super.setUp()
+
+    mockkObject(TransitionAnimator)
+    every { TransitionAnimator.animateTransition(any(), any(), any(), any()) } returns null
+
+    mockkObject(ZhabaStepFactory)
+
+    val studentPackPromotionStep = ZhabaStepFactory.studentPackPromotionStep()
+    val mockedStudentPackPromotionStep = spyk(studentPackPromotionStep)
+
+    // when the step is performed, it should return a non-empty result so that the executor does not skip it
+    every { mockedStudentPackPromotionStep.performStep(any(), any()) } returns mockk {
+      every { zhaba } returns mockk()
+    }
+
+    // We are not going to wait when a user clicks the "Apply Now" button, so we pretend that when the step is executed,
+    // the button is pressed immediately:
+    coEvery { mockedStudentPackPromotionStep.executeStep(any(), any(), any(), any()) } answers {
+      mockedStudentPackPromotionStep.onPrimaryButton(GotItBalloonGraphData(null, 1))
+      return@answers FINISH_TRANSITION
+    }
+
+    every { ZhabaStepFactory.studentPackPromotionStep() } returns mockedStudentPackPromotionStep
+  }
 
   override fun tearDown() {
     try {
@@ -40,9 +54,11 @@ class PromotionCheckListenerTest : EduTestCase() {
   }
 
   @Test
-  @Ignore("Test is temporarily disabled")
   fun `show promotion notification for the first time`() {
     // given
+
+    // mocks for ZhabaGraph from the setUp() method
+
     val course = courseWithFiles {
       lesson("lesson1") {
         eduTask("task1") {
@@ -55,37 +71,21 @@ class PromotionCheckListenerTest : EduTestCase() {
     every { properties.updateValue(PROPERTY_KEY, any()) } returns true
     justRun { properties.setValue(PROPERTY_KEY, any<Boolean>()) }
 
-    mockkObject(EduNotificationManager)
-
-    val notificationAction = slot<AnAction>()
-    val notification = mockk<Notification> {
-      every { addAction(capture(notificationAction)) } returns this
-      justRun { notify(any()) }
-      justRun { hideBalloon() }
-    }
-    every { EduNotificationManager.create(any(), any(), any()) } returns notification
-
     // when
     val task = course.findTask("lesson1", "task1")
     task.openTaskFileInEditor("foo.txt")
     testAction(CheckAction(task.getUICheckLabel()))
 
-    if (notificationAction.isCaptured) {
-      val dataContext = SimpleDataContext.builder().add(Notification.KEY, notification).build()
-      testAction(notificationAction.captured, dataContext)
-    }
-
     // then
-    verify { EduNotificationManager.create(any(), any(), any()) }
-    verify { notification.notify(any()) }
-    verify { notification.hideBalloon() }
     assertEquals(STUDENT_PACK_LINK, EduBrowser.getInstance().asSafely<MockEduBrowser>()?.lastVisitedUrl)
   }
 
   @Test
-  @Ignore("Test is temporarily disabled")
   fun `do not show promotion notification for the second time`() {
     // given
+
+    // mocks for ZhabaGraph from the setUp() method
+
     val course = courseWithFiles {
       lesson("lesson1") {
         eduTask("task1") {
@@ -98,16 +98,13 @@ class PromotionCheckListenerTest : EduTestCase() {
     every { properties.updateValue(PROPERTY_KEY, any()) } returns false
     justRun { properties.setValue(PROPERTY_KEY, any<Boolean>()) }
 
-    mockkObject(EduNotificationManager)
-    every { EduNotificationManager.create(any(), any(), any()) } returns mockk<Notification>()
-
     // when
     val task = course.findTask("lesson1", "task1")
     task.openTaskFileInEditor("foo.txt")
     testAction(CheckAction(task.getUICheckLabel()))
 
     // then
-    verify(exactly = 0) { EduNotificationManager.create(any(), any(), any()) }
+    assertEquals(null, EduBrowser.getInstance().asSafely<MockEduBrowser>()!!.lastVisitedUrl)
   }
 
   companion object {
