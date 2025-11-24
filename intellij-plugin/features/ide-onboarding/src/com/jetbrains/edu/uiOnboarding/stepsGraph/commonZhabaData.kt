f97751950b0aff7b4afba4f8bf2d6dea3e5d5e2e
Ilia Posov <ilia.posov@jetbrains.com>
2025-11-24 13:41:19 +0100

    EDU-8472: Allow different animations for Zhaba appearance

--- intellij-plugin/features/ide-onboarding/src/com/jetbrains/edu/uiOnboarding/stepsGraph/commonZhabaData.kt
+++ intellij-plugin/features/ide-onboarding/src/com/jetbrains/edu/uiOnboarding/stepsGraph/commonZhabaData.kt
@@ -1,6 +1,24 @@
 package com.jetbrains.edu.uiOnboarding.stepsGraph
 
+import com.jetbrains.edu.uiOnboarding.EduUiOnboardingAnimation
+import com.jetbrains.edu.uiOnboarding.EduUiOnboardingAnimationData
+import com.jetbrains.edu.uiOnboarding.EduUiOnboardingAnimationStep
 import com.jetbrains.edu.uiOnboarding.ZhabaImage
+import com.jetbrains.edu.uiOnboarding.transitions.BottomToTopAppearance
 
-object StartZhabaData : ZhabaData
+/**
+ * Is able to compute the transition animation to the next [ZhabaStep], given its [ZhabaData].
+ */
+interface StartZhabaData : ZhabaData {
+  fun transitionAnimation(nextData: ZhabaDataWithComponent, animationData: EduUiOnboardingAnimationData): EduUiOnboardingAnimation
+}
+
+/**
+ * In start onboarding, the Toad simply appears from bottom to up.
+ */
+object StartOnboardingZhabaData: StartZhabaData {
+  override fun transitionAnimation(nextData: ZhabaDataWithComponent, animationData: EduUiOnboardingAnimationData): EduUiOnboardingAnimation =
+    BottomToTopAppearance(nextData.zhaba.animation, nextData.zhabaPoint)
+}
+
 data class JumpingAwayZhabaData(val zhabaImage: ZhabaImage) : ZhabaData
