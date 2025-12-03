e7377d5542fc140b50233b737f411e5140497f41
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2025-12-03 17:36:55 +0100

    EDU-8644: fix deadlock inside `StudyTaskManager` initialization on dynamic plugin loading

--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/StudyTaskManager.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/StudyTaskManager.kt
@@ -54,32 +54,42 @@ class StudyTaskManager(private val project: Project) : DumbAware, Disposable, Ed
   private fun initializeCourse() {
     if (!needToLoadCourse(project)) return
 
-    var loadedCourse: Course? = null
+    // Read action is necessary for the `loadCourse` call.
+    // At the same time, ordering of lock acquiring is important here:
+    // read/write action lock should be taken first, `courseLoadingLock` after that.
+    //
+    // Otherwise, it can lead to deadlock if:
+    // - 1st thread: takes write action and waits for `courseLoadingLock` taken by 2nd thread
+    // - 2nd thread: takes `courseLoadingLock` and waits for write action from 1st thread to finish to take read lock
+    // Described case may happen when the plugin is dynamically loaded with an already opened project
+    val (loadedCourse, courseLoadingHappened) = runReadAction {
+      courseLoadingLock.withLock {
+        if (!needToLoadCourse(project)) return@runReadAction null to false
 
-    courseLoadingLock.withLock {
-      if (!needToLoadCourse(project)) return
-
-      loadedCourse = runReadAction {
-        try {
+        val loadedCourse = try {
           loadCourse(project)
         }
         catch (th: Throwable) {
           LOG.error("Error while loading course", th)
           null
         }
-      }
-      courseLoadedWithError = loadedCourse == null
-      if (loadedCourse != null) {
-        logger<StudyTaskManager>().info("Loaded course corresponding to the project: ${loadedCourse.name}")
-        _course = loadedCourse
-      }
-      else {
-        logger<StudyTaskManager>().info("Course corresponding to the project loaded with errors")
+
+        courseLoadedWithError = loadedCourse == null
+        if (loadedCourse != null) {
+          logger<StudyTaskManager>().info("Loaded course corresponding to the project: ${loadedCourse.name}")
+          _course = loadedCourse
+        }
+        else {
+          logger<StudyTaskManager>().info("Course corresponding to the project loaded with errors")
+        }
+        loadedCourse to true
       }
     }
 
-    loadedCourse?.fireCourseSetEvent()
-    startSynchronization(project)
+    if (courseLoadingHappened) {
+      loadedCourse?.fireCourseSetEvent()
+      startSynchronization(project)
+    }
   }
 
   override fun dispose() {}
