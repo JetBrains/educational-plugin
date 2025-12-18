402961a30503ebef85fc1421823022f000b96968
Ilia Posov <ilia.posov@jetbrains.com>
2025-12-18 23:07:11 +0100

    EDU-8682: New actions: Call Tode, Start onboarding, promote student pack
    And Hide Tode

---
+++ intellij-plugin/features/ide-onboarding/src/com/jetbrains/edu/uiOnboarding/actions/PromoteStudentPackAction.kt
@@ -0,0 +1,17 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.jetbrains.edu.uiOnboarding.actions
+
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.jetbrains.edu.uiOnboarding.EduUiOnboardingService
+
+class PromoteStudentPackAction : ZhabaActionBase() {
+
+  override fun actionPerformed(e: AnActionEvent) {
+    val project = e.project ?: return
+    EduUiOnboardingService.getInstance(project).promoteStudentPack()
+  }
+
+  companion object {
+    const val ACTION_ID: String = "Educational.PromoteStudentPackAction"
+  }
+}
