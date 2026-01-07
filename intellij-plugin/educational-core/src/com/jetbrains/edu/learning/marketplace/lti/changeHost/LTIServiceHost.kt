2309a2ea50bd2271d38cc6050111c5a31b0fcbf5
Ilia Posov <ilia.posov@jetbrains.com>
2026-01-07 17:52:26 +0100

    EDU-7851: "Change LTI Service URL" action

---
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/marketplace/lti/changeHost/LTIServiceHost.kt
@@ -0,0 +1,25 @@
+package com.jetbrains.edu.learning.marketplace.lti.changeHost
+
+import com.intellij.openapi.util.NlsContexts
+import com.jetbrains.edu.learning.actions.changeHost.ServiceHostEnum
+import com.jetbrains.edu.learning.actions.changeHost.ServiceHostManager
+import com.jetbrains.edu.learning.messages.BUNDLE
+import com.jetbrains.edu.learning.messages.EduCoreBundle
+import org.jetbrains.annotations.PropertyKey
+
+@Suppress("unused") // All enum values ar used in UI
+enum class LTIServiceHost(
+  override val url: String,
+  @param:PropertyKey(resourceBundle = BUNDLE) private val visibleNameKey: String
+) : ServiceHostEnum {
+  PRODUCTION("https://lti-tool-production.labs.jb.gg/", "change.service.host.production"),
+  STAGING("https://lti-tool-staging.labs.jb.gg/", "change.service.host.staging"),
+  OTHER("http://localhost:8083", "change.service.host.other");
+
+  override fun visibleName(): @NlsContexts.ListItem String = EduCoreBundle.message(visibleNameKey)
+
+  companion object : ServiceHostManager<LTIServiceHost>("Submissions service", LTIServiceHost::class.java) {
+    override val default: LTIServiceHost = PRODUCTION
+    override val other: LTIServiceHost = OTHER
+  }
+}
