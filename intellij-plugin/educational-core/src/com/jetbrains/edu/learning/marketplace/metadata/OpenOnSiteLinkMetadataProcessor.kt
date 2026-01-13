d4321c5d8fe35ca7858629913c21fcc011a2ae45
Gleb Oborin <gleb.oborin@jetbrains.com>
2026-01-13 03:05:23 +0100

    EDU-8667: Move `TRUSTED_OPEN_ON_SITE_HOSTS` out of `OpenOnSiteLinkSettings`
    It will be used for `LicenseLinkSettings`

--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/marketplace/metadata/OpenOnSiteLinkMetadataProcessor.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/marketplace/metadata/OpenOnSiteLinkMetadataProcessor.kt
@@ -3,11 +3,8 @@ package com.jetbrains.edu.learning.marketplace.metadata
 import com.intellij.openapi.project.Project
 import com.jetbrains.edu.learning.courseFormat.Course
 import com.jetbrains.edu.learning.marketplace.settings.OpenOnSiteLinkSettings
-import com.jetbrains.edu.learning.marketplace.settings.OpenOnSiteLinkSettings.Companion.TRUSTED_OPEN_ON_SITE_HOSTS
 import com.jetbrains.edu.learning.newproject.CourseMetadataProcessor
 import com.jetbrains.edu.learning.newproject.CourseProjectState
-import java.net.URI
-import java.net.URISyntaxException
 
 class OpenOnSiteLinkMetadataProcessor : CourseMetadataProcessor<String> {
   override fun findApplicableMetadata(rawMetadata: Map<String, String>): String? {
@@ -25,13 +22,5 @@ class OpenOnSiteLinkMetadataProcessor : CourseMetadataProcessor<String> {
     courseProjectState: CourseProjectState
   ) {
     OpenOnSiteLinkSettings.getInstance(project).link = metadata
-  }
-
-  private fun String.isValidAndAllowedUrl(): Boolean = try {
-    val uri = URI(this)
-    uri.scheme == "https" && uri.host in TRUSTED_OPEN_ON_SITE_HOSTS
-  }
-  catch (_: URISyntaxException) {
-    false
   }
 }
--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/marketplace/settings/OpenOnSiteLinkSettings.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/marketplace/settings/OpenOnSiteLinkSettings.kt
@@ -3,11 +3,7 @@ package com.jetbrains.edu.learning.marketplace.settings
 
 import com.intellij.openapi.components.*
 import com.intellij.openapi.project.Project
-import com.jetbrains.edu.learning.EduNames
 import com.jetbrains.edu.learning.EduTestAware
-import com.jetbrains.edu.learning.messages.EduCoreBundle
-import com.jetbrains.edu.learning.stepik.hyperskill.HYPERSKILL_DEFAULT_HOST
-import java.util.function.Supplier
 
 @Service(Service.Level.PROJECT)
 @State(name = "OpenOnSiteLinkSettings", storages = [Storage(StoragePathMacros.WORKSPACE_FILE, roamingType = RoamingType.DISABLED)])
@@ -20,18 +16,6 @@ class OpenOnSiteLinkSettings : SimplePersistentStateComponent<EduTrackLinkSettin
     }
 
   companion object {
-
-    /**
-     * Contains a set of trusted hosts for links to the course on Task Description view.
-     * Each host is associated with message of the corresponding link
-     */
-    val TRUSTED_OPEN_ON_SITE_HOSTS: Map<String, Supplier<String>> = mapOf(
-      HYPERSKILL_DEFAULT_HOST to EduCoreBundle.lazyMessage("action.open.on.text", EduNames.JBA),
-      "academy.jetbrains.com" to EduCoreBundle.lazyMessage("action.open.in.course.catalog"),
-      "jetbrains-academy-staging-external.labs.jb.gg" to EduCoreBundle.lazyMessage("action.open.in.course.catalog"),
-      "staging.academy.labs.jb.gg" to EduCoreBundle.lazyMessage("action.open.in.course.catalog"),
-    )
-
     fun getInstance(project: Project): OpenOnSiteLinkSettings = project.service<OpenOnSiteLinkSettings>()
   }
 }
