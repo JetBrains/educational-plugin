695730f0e30037e2dcb3990543ead89a96da713d
Gleb Oborin <gleb.oborin@jetbrains.com>
2026-01-13 14:39:07 +0100

    EDU-8700: Drop 252 related code for Edu-Cpp module

--- intellij-plugin/Edu-Cpp/CLion-Nova/src/com/jetbrains/edu/cpp/radler/checker/RadlerCppRunConfigurationHelper.kt
+++ intellij-plugin/Edu-Cpp/CLion-Nova/src/com/jetbrains/edu/cpp/radler/checker/RadlerCppRunConfigurationHelper.kt
@@ -4,8 +4,8 @@ import com.intellij.clion.radler.core.symbols.RadMainPsiElement
 import com.intellij.psi.PsiElement
 import com.jetbrains.edu.cpp.checker.CppRunConfigurationHelper
 
-// BACKCOMPAT: 2025.2. Inline it
 class RadlerCppRunConfigurationHelper : CppRunConfigurationHelper {
-  override fun prepareEntryPointForRunConfiguration(entryPoint: PsiElement): PsiElement =
-    RadMainPsiElement(entryPoint.containingFile, entryPoint.textRange)
+  override fun prepareEntryPointForRunConfiguration(entryPoint: PsiElement): PsiElement {
+    return RadMainPsiElement(entryPoint.containingFile, entryPoint.textRange)
+  }
 }
