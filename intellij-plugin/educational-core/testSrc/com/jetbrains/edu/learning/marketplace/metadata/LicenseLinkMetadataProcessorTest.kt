237fc164bba8b02c55d2e3ed632813a536963ed4
Gleb Oborin <gleb.oborin@jetbrains.com>
2026-01-20 16:55:46 +0100

    EDU-8667: Decode license url on link processor & refactor processor tests
    `license_url` link is encoded with `URLEncoder(link, StandardCharsets.UTF_8)` on learning center side
    Decoupled `OpenOnSite` and `LicenseLink` tests, because `LicenseLink` tests require decoding now
    
    (cherry picked from commit 41f2f1a6d23d222339f410295069efbc5feab5db)

--- intellij-plugin/educational-core/testSrc/com/jetbrains/edu/learning/marketplace/metadata/LicenseLinkMetadataProcessorTest.kt
+++ intellij-plugin/educational-core/testSrc/com/jetbrains/edu/learning/marketplace/metadata/LicenseLinkMetadataProcessorTest.kt
@@ -1,10 +1,75 @@
 package com.jetbrains.edu.learning.marketplace.metadata
 
 import com.jetbrains.edu.learning.marketplace.settings.LicenseLinkSettings
+import com.jetbrains.edu.learning.newproject.CourseMetadataProcessorTestBase
+import kotlin.test.Test
 
-class LicenseLinkMetadataProcessorTest : LinkMetadataProcessorTestBase() {
-  override val linkParameterName: String = LicenseLinkMetadataProcessor.LICENSE_URL_PARAMETER_NAME
+class LicenseLinkMetadataProcessorTest : CourseMetadataProcessorTestBase() {
+  private val linkParameterName: String = LicenseLinkMetadataProcessor.LICENSE_URL_PARAMETER_NAME
 
-  override val link: String?
+  private val link: String?
     get() = LicenseLinkSettings.getInstance(project).link
+
+  @Test
+  fun `test license link metadata decodes encoded url and saves it`() {
+    // given
+    val encodedLink = "https%3A%2F%2Facademy%2Ejetbrains%2Ecom%2Fapi%2Fedu-track%2Faws%2Fplugin%2Flicense%3FtrackRef%3Dabc%26courseRef%3Dedf%26moduleRef%3Dhij"
+    val expectedLink = "https://academy.jetbrains.com/api/edu-track/aws/plugin/license?trackRef=abc&courseRef=edf&moduleRef=hij"
+    val params = mapOf(linkParameterName to encodedLink)
+
+    // when
+    createCourseWithMetadata(params)
+
+    // then
+    assertNotNull(link)
+    assertEquals(expectedLink, link)
+  }
+
+  @Test
+  fun `test link with untrusted host is not saved`() {
+    // given
+    val params = mapOf(linkParameterName to "https%3A%2F%2Fgoogle.com")
+
+    // when
+    createCourseWithMetadata(params)
+
+    // then
+    assertNull(link)
+  }
+
+  @Test
+  fun `test link with invalid protocol is not saved`() {
+    // given
+    val params = mapOf(linkParameterName to "ftp%3A%2F%2Fgoogle.com")
+
+    // when
+    createCourseWithMetadata(params)
+
+    // then
+    assertNull(link)
+  }
+
+  @Test
+  fun `test link with absent protocol is not saved`() {
+    // given
+    val params = mapOf(linkParameterName to "google.com")
+
+    // when
+    createCourseWithMetadata(params)
+
+    // then
+    assertNull(link)
+  }
+
+  @Test
+  fun `test missing link parameter`() {
+    // given
+    val params = emptyMap<String, String>()
+
+    // when
+    createCourseWithMetadata(params)
+
+    // then
+    assertNull(link)
+  }
 }
--- intellij-plugin/educational-core/testSrc/com/jetbrains/edu/learning/marketplace/metadata/OpenOnSiteLinkMetadataProcessorTest.kt
+++ intellij-plugin/educational-core/testSrc/com/jetbrains/edu/learning/marketplace/metadata/OpenOnSiteLinkMetadataProcessorTest.kt
@@ -1,10 +1,73 @@
 package com.jetbrains.edu.learning.marketplace.metadata
 
 import com.jetbrains.edu.learning.marketplace.settings.OpenOnSiteLinkSettings
+import com.jetbrains.edu.learning.newproject.CourseMetadataProcessorTestBase
+import org.junit.Test
 
-class OpenOnSiteLinkMetadataProcessorTest : LinkMetadataProcessorTestBase() {
-  override val linkParameterName: String = OpenOnSiteLinkMetadataProcessor.OPEN_ON_SITE_URL_PARAMETER_NAME
+class OpenOnSiteLinkMetadataProcessorTest : CourseMetadataProcessorTestBase() {
+  private val linkParameterName: String = OpenOnSiteLinkMetadataProcessor.OPEN_ON_SITE_URL_PARAMETER_NAME
 
-  override val link: String?
+  private val link: String?
     get() = OpenOnSiteLinkSettings.getInstance(project).link
+
+  @Test
+  fun `test link is saved`() {
+    // given
+    val params = mapOf(linkParameterName to getRandomTrustedUrl())
+
+    // when
+    createCourseWithMetadata(params)
+
+    //then
+    assertNotNull(link)
+    assertEquals(params[linkParameterName], link)
+  }
+
+  @Test
+  fun `test link with untrusted host is not saved`() {
+    // given
+    val params = mapOf(linkParameterName to "https://google.com")
+
+    // when
+    createCourseWithMetadata(params)
+
+    // then
+    assertNull(link)
+  }
+
+  @Test
+  fun `test link with invalid protocol is not saved`() {
+    // given
+    val params = mapOf(linkParameterName to "ftp://google.com")
+
+    // when
+    createCourseWithMetadata(params)
+
+    // then
+    assertNull(link)
+  }
+
+  @Test
+  fun `test link with absent protocol is not saved`() {
+    // given
+    val params = mapOf(linkParameterName to "google.com")
+
+    // when
+    createCourseWithMetadata(params)
+
+    // then
+    assertNull(link)
+  }
+
+  @Test
+  fun `test missing link parameter`() {
+    // given
+    val params = emptyMap<String, String>()
+
+    // when
+    createCourseWithMetadata(params)
+
+    // then
+    assertNull(link)
+  }
 }
