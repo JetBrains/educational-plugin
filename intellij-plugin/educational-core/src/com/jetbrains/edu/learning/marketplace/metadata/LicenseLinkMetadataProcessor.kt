41f2f1a6d23d222339f410295069efbc5feab5db
Gleb Oborin <gleb.oborin@jetbrains.com>
2026-01-20 16:55:46 +0100

    EDU-8667: Decode license url on link processor & refactor processor tests
    `license_url` link is encoded with `URLEncoder(link, StandardCharsets.UTF_8)` on learning center side
    Decoupled `OpenOnSite` and `LicenseLink` tests, because `LicenseLink` tests require decoding now

--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/marketplace/metadata/LicenseLinkMetadataProcessor.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/marketplace/metadata/LicenseLinkMetadataProcessor.kt
@@ -5,14 +5,17 @@ import com.jetbrains.edu.learning.courseFormat.Course
 import com.jetbrains.edu.learning.marketplace.settings.LicenseLinkSettings
 import com.jetbrains.edu.learning.newproject.CourseMetadataProcessor
 import com.jetbrains.edu.learning.newproject.CourseProjectState
+import java.net.URLDecoder
+import java.nio.charset.StandardCharsets
 
 class LicenseLinkMetadataProcessor : CourseMetadataProcessor<String> {
   override fun findApplicableMetadata(rawMetadata: Map<String, String>): String? {
     val link = rawMetadata[LICENSE_URL_PARAMETER_NAME] ?: return null
-    if (!link.isValidAndAllowedUrl()) {
+    val decodedLink = URLDecoder.decode(link, StandardCharsets.UTF_8)
+    if (!decodedLink.isValidAndAllowedUrl()) {
       return null
     }
-    return link
+    return decodedLink
   }
 
   override fun processMetadata(
