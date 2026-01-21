ef3140a87820001eaeed02f56280f9586e280bfb
Gleb Oborin <gleb.oborin@jetbrains.com>
2026-01-21 19:16:27 +0100

    EDU-8667: Add retry action link to expired license editor notification
    (cherry picked from commit 50e197af650cffab7c026151d0a8f672db288682)

--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/marketplace/license/CourseLicenseEditorNotificationProvider.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/marketplace/license/CourseLicenseEditorNotificationProvider.kt
@@ -3,16 +3,20 @@ package com.jetbrains.edu.learning.marketplace.license
 import com.intellij.openapi.fileEditor.FileEditor
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.NlsContexts
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.ui.EditorNotificationPanel
 import com.intellij.ui.EditorNotificationProvider
 import com.jetbrains.edu.learning.EduBrowser
 import com.jetbrains.edu.learning.EduUtilsKt.isEduProject
 import com.jetbrains.edu.learning.actions.CheckLicenseAction
+import com.jetbrains.edu.learning.marketplace.license.CourseLicenseEditorNotificationProvider.LicenseNotificationTexts.ERROR
+import com.jetbrains.edu.learning.marketplace.license.CourseLicenseEditorNotificationProvider.LicenseNotificationTexts.EXPIRED
 import com.jetbrains.edu.learning.marketplace.settings.LicenseLinkSettings
 import com.jetbrains.edu.learning.marketplace.settings.OpenOnSiteLinkSettings
 import com.jetbrains.edu.learning.messages.EduCoreBundle
 import java.util.function.Function
+import java.util.function.Supplier
 import javax.swing.JComponent
 
 class CourseLicenseEditorNotificationProvider : EditorNotificationProvider, DumbAware {
@@ -21,30 +25,44 @@ class CourseLicenseEditorNotificationProvider : EditorNotificationProvider, Dumb
     if (!LicenseLinkSettings.isLicenseRequired(project)) return null
     val licenseState = LicenseChecker.getInstance(project).licenseState.value ?: return null
 
-    val notificationText = when (licenseState) {
-      // license is valid -> no notification
-      LicenseState.VALID -> return null
-      LicenseState.EXPIRED -> EduCoreBundle.message("license.notification.invalid.text")
-      LicenseState.ERROR -> EduCoreBundle.message("license.notification.error.text")
-    }
+    val (notificationText, retryActionText) = licenseState.toNotificationTexts() ?: return null
     return Function {
       EditorNotificationPanel(EditorNotificationPanel.Status.Error).apply {
-        text = notificationText
-        when (licenseState) {
+        text = notificationText.get()
+        if (licenseState == LicenseState.EXPIRED) {
           // if the license is expired -> show a link to a learning center
-          LicenseState.EXPIRED -> {
-            val link = OpenOnSiteLinkSettings.getInstance(project).link ?: return@apply
-            createActionLabel(EduCoreBundle.message("course.dialog.learn.more")) {
-              EduBrowser.getInstance().browse(link)
-            }
+          val link = OpenOnSiteLinkSettings.getInstance(project).link ?: return@apply
+          createActionLabel(EduCoreBundle.message("course.dialog.learn.more")) {
+            EduBrowser.getInstance().browse(link)
           }
-          // if there was an error during license check -> add a link to a retry license check action
-          LicenseState.ERROR -> {
-            createActionLabel(EduCoreBundle.message("retry"), CheckLicenseAction.ACTION_ID)
-          }
-          else -> {}
         }
+        // if license expired or error happened -> give an option to retry the check
+        createActionLabel(retryActionText.get(), CheckLicenseAction.ACTION_ID)
       }
     }
+  }
+
+  private enum class LicenseNotificationTexts(
+    @NlsContexts.Label val text: Supplier<String>,
+    @NlsContexts.LinkLabel val retryActionText: Supplier<String>
+  ) {
+    ERROR(
+      EduCoreBundle.lazyMessage("license.notification.error.text"),
+      EduCoreBundle.lazyMessage("retry"),
+    ),
+
+    EXPIRED(
+      EduCoreBundle.lazyMessage("license.notification.invalid.text"),
+      EduCoreBundle.lazyMessage("license.notification.invalid.check.license.text"),
+    );
+
+    operator fun component1() = text
+    operator fun component2() = retryActionText
+  }
+
+  private fun LicenseState.toNotificationTexts(): LicenseNotificationTexts? = when (this) {
+    LicenseState.VALID -> null
+    LicenseState.ERROR -> ERROR
+    LicenseState.EXPIRED -> EXPIRED
   }
 }
