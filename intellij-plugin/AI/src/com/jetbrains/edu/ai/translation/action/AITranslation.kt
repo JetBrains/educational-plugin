f216788e3058aad63727e2ace741fc430facca12
Ilia Posov <ilia.posov@jetbrains.com>
2026-01-22 17:49:53 +0100

    EDU-8634: Remove Got-It tooltip for AI translation

--- intellij-plugin/AI/src/com/jetbrains/edu/ai/translation/action/AITranslation.kt
+++ intellij-plugin/AI/src/com/jetbrains/edu/ai/translation/action/AITranslation.kt
@@ -1,13 +1,9 @@
 package com.jetbrains.edu.ai.translation.action
 
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.ui.popup.Balloon
 import com.intellij.openapi.ui.popup.JBPopupFactory
-import com.intellij.ui.GotItTooltip
-import com.jetbrains.edu.ai.messages.EduAIBundle
 import com.jetbrains.edu.ai.translation.TRANSLATION_NOTIFICATION_ID
 import com.jetbrains.edu.ai.translation.TranslationLoader
-import com.jetbrains.edu.ai.translation.connector.TranslationServiceConnector
 import com.jetbrains.edu.ai.translation.ui.CourseTranslationPopup
 import com.jetbrains.edu.ai.ui.EducationalAIIcons
 import com.jetbrains.edu.learning.ai.TranslationProjectSettings
@@ -15,16 +11,12 @@ import com.jetbrains.edu.learning.course
 import com.jetbrains.edu.learning.courseFormat.EduCourse
 import com.jetbrains.edu.learning.taskToolWindow.ui.TaskToolWindowView
 
-@Suppress("ComponentNotRegistered")
 class AITranslation : AITranslationActionBase() {
-  private val tooltip: GotItTooltip = createTooltip()
 
   init {
     templatePresentation.icon = EducationalAIIcons.Translation
     templatePresentation.hoveredIcon = EducationalAIIcons.TranslationHovered
     templatePresentation.selectedIcon = EducationalAIIcons.TranslationPressed
-
-    tooltip.assignTo(templatePresentation, GotItTooltip.BOTTOM_MIDDLE)
   }
 
   override fun actionPerformed(e: AnActionEvent) {
@@ -56,17 +48,8 @@ class AITranslation : AITranslationActionBase() {
     e.presentation.isEnabledAndVisible = !TranslationLoader.isRunning(project)
   }
 
-  private fun createTooltip(): GotItTooltip {
-    val translationServiceConnector = TranslationServiceConnector.getInstance()
-    return GotItTooltip(TOOLTIP_ID, EduAIBundle.message("ai.translation.tooltip.text"), translationServiceConnector)
-      .withHeader(EduAIBundle.message("ai.translation.tooltip.header"))
-      .withPosition(Balloon.Position.below)
-  }
-
   companion object {
     @Suppress("unused")
     const val ACTION_ID: String = "Educational.AITranslation"
-
-    private const val TOOLTIP_ID: String = "ai.translation.tooltip"
   }
 }
