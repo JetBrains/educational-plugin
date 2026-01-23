0c7427f83c1ddf9885448989b2d1d5801fe06c4e
Ilia Posov <ilia.posov@jetbrains.com>
2026-01-23 11:52:12 +0100

    EDU-8634: Remove Got-It tooltip for Theory Lookup

--- intellij-plugin/AI/src/com/jetbrains/edu/ai/AIStartupActivity.kt
+++ intellij-plugin/AI/src/com/jetbrains/edu/ai/AIStartupActivity.kt
@@ -1,18 +1,14 @@
 package com.jetbrains.edu.ai
 
-import com.intellij.openapi.application.EDT
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
 import com.jetbrains.edu.ai.AIServiceUpdateChecker.Companion.launchUpdateChecker
 import com.jetbrains.edu.ai.terms.observeAndLoadCourseTerms
-import com.jetbrains.edu.ai.terms.ui.TermsGotItTooltipService
 import com.jetbrains.edu.learning.EduUtilsKt.isStudentProject
 import com.jetbrains.edu.learning.course
 import com.jetbrains.edu.learning.courseFormat.EduCourse
 import com.jetbrains.edu.learning.marketplace.isFromCourseStorage
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 
 class AIStartupActivity(private val scope: CoroutineScope) : ProjectActivity {
   override suspend fun execute(project: Project) {
@@ -24,9 +20,5 @@ class AIStartupActivity(private val scope: CoroutineScope) : ProjectActivity {
     project.launchUpdateChecker(course)
 
     scope.observeAndLoadCourseTerms(project)
-
-    withContext(Dispatchers.EDT) {
-      TermsGotItTooltipService.getInstance(project).showTermsGotItTooltip()
-    }
   }
 }
