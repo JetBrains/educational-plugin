e434acd9fb82acd7c66804b4064926d28587632e
Gleb Oborin <gleb.oborin@jetbrains.com>
2026-02-09 16:22:05 +0100

    EDU-8699: Fix construction of `RadMainPsiElement` in CLion-Nova

--- intellij-plugin/Edu-Cpp/CLion-Classic/src/com/jetbrains/edu/cpp/classic/checker/ClassicCppRunConfigurationHelper.kt
+++ intellij-plugin/Edu-Cpp/CLion-Classic/src/com/jetbrains/edu/cpp/classic/checker/ClassicCppRunConfigurationHelper.kt
@@ -1,8 +1,9 @@
 package com.jetbrains.edu.cpp.classic.checker
 
+import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
 import com.jetbrains.edu.cpp.checker.CppRunConfigurationHelper
 
 class ClassicCppRunConfigurationHelper : CppRunConfigurationHelper {
-  override fun prepareEntryPointForRunConfiguration(entryPoint: PsiElement): PsiElement = entryPoint
+  override fun prepareEntryPointForRunConfiguration(project: Project, entryPoint: PsiElement): PsiElement = entryPoint
 }
