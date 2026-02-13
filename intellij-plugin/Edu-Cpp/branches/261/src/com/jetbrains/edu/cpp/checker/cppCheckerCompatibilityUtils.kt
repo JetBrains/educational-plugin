61740acd37b0a7aac4ba33ddb1c398ddc3ebf406
Gleb Oborin <gleb.oborin@jetbrains.com>
2026-02-13 19:20:41 +0100

    fixup! EDU-8699: Fix Catch and Google test compatibility issues

--- intellij-plugin/Edu-Cpp/branches/261/src/com/jetbrains/edu/cpp/checker/cppCheckerCompatibilityUtils.kt
+++ intellij-plugin/Edu-Cpp/branches/261/src/com/jetbrains/edu/cpp/checker/cppCheckerCompatibilityUtils.kt
@@ -4,10 +4,12 @@ import com.intellij.execution.configurations.ConfigurationFactory
 import com.jetbrains.cidr.execution.testing.google.CidrGoogleTestRunConfigurationType
 import com.jetbrains.cidr.execution.testing.tcatch.CidrCatchTestRunConfigurationType
 
+// BACKCOMPAT 2025.3. Inline it.
 fun getGoogleTestConfigurationFactory(): ConfigurationFactory {
   return CidrGoogleTestRunConfigurationType.getInstance().getFactory()
 }
 
+// BACKCOMPAT 2025.3. Inline it.
 fun getCatchTestConfigurationFactory(): ConfigurationFactory {
   return CidrCatchTestRunConfigurationType.getInstance().getFactory()
 }
