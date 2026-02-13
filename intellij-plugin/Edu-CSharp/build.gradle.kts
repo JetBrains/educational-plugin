cefa82049b3a925868af01027608c71a8509ec51
Gleb Oborin <gleb.oborin@jetbrains.com>
2026-02-13 21:01:44 +0100

    EDU-8699: Add rider testFramework dependency

--- intellij-plugin/Edu-CSharp/build.gradle.kts
+++ intellij-plugin/Edu-CSharp/build.gradle.kts
@@ -1,3 +1,6 @@
+import org.jetbrains.intellij.platform.gradle.extensions.IntelliJPlatformDependenciesExtension
+import org.jetbrains.intellij.platform.gradle.models.Coordinates
+
 plugins {
   id("intellij-plugin-module-conventions")
 }
@@ -10,9 +13,24 @@ dependencies {
     bundledModule("intellij.rider")
     if (isAtLeast261) {
       bundledModule("intellij.rider.debugger.shared")
+      // TODO: remove it and use testFramework(TestFrameworkType.Plugins.Rider) when new version of IPGP will be available
+      riderTestFramework()
     }
   }
 
   implementation(project(":intellij-plugin:educational-core"))
   testImplementation(project(":intellij-plugin:educational-core", "testOutput"))
+}
+
+// See: RIDER-131602
+// Implementation from a related commit in issue
+fun IntelliJPlatformDependenciesExtension.riderTestFramework() {
+  val (_, riderVersionNumber) = riderVersion.toTypeWithVersion()
+  val testFrameworkVersion = "RIDER-$riderVersionNumber"
+  fun riderTestPlatformDependency(artifact: String) {
+    testPlatformDependency(Coordinates("com.jetbrains.intellij.rider", artifact), testFrameworkVersion)
+  }
+  riderTestPlatformDependency("rider-test-framework")
+  riderTestPlatformDependency("rider-test-framework-core")
+  riderTestPlatformDependency("rider-test-framework-testng")
 }
