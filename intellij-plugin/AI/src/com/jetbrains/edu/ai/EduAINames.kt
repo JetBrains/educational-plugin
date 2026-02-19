954049d449f818c8b6c3f07f04a46655934253f2
Gleb Oborin <gleb.oborin@jetbrains.com>
2026-02-19 14:46:08 +0100

    EDU-8456: Use special url for china users for translation service

--- intellij-plugin/AI/src/com/jetbrains/edu/ai/EduAINames.kt
+++ intellij-plugin/AI/src/com/jetbrains/edu/ai/EduAINames.kt
@@ -2,4 +2,5 @@ package com.jetbrains.edu.ai
 
 const val EDU_AI_SERVICE_HOST_PROPERTY: String = "edu.ai.service.host"
 const val EDU_AI_SERVICE_PRODUCTION_URL: String = "https://educational-ai-service.labs.jb.gg/"
+const val EDU_AI_SERVICE_PRODUCTION_CHINA_URL: String = "https://educational-ai-service-china.labs.jb.gg/"
 const val EDU_AI_SERVICE_STAGING_URL: String = "https://educational-ai-staging.labs.jb.gg/"
