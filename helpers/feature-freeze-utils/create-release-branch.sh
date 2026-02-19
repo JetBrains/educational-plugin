bf360984898d766374a666b6e0523c52c13d67d1
TeamCity <teamcity@jetbrains.com>
2026-02-19 15:54:19 +0100

    EDU-5768: Added token processing in the create-release-branch.sh
    script now changes remote url to https if necessary
    then it adds an authorization header to the url during branch creation to have a proper authorization for Space

--- helpers/feature-freeze-utils/create-release-branch.sh
+++ helpers/feature-freeze-utils/create-release-branch.sh
@@ -2,11 +2,47 @@
 set -e
 
 RELEASE_BRANCH="$1"
+TOKEN="$2"
+ORIGINAL_REMOTE="$3"  # Pass %vcsroot.url% from TeamCity
 
 if [ -z "RELEASE_BRANCH" ]; then
-    echo "Error: RELEASE_BRANCH argument is required"
+    echo "Error: RELEASE_BRANCH argument is required. Please pass the name of branch to be created as a first argument"
     exit 1
 fi
+
+if [ -z "TOKEN" ]; then
+    echo "Error: TOKEN argument is required. Please use a Space Authorization Token as a second argument"
+    exit 1
+fi
+
+if [ -z "$ORIGINAL_REMOTE" ]; then
+    echo "Error: ORIGINAL_REMOTE argument is required. "
+    exit 1
+fi
+
+# Convert SSH URL to HTTPS URL
+# Handles formats like:
+#   ssh://git@git.jetbrains.team/repo.git
+#   git@github.com:user/repo.git
+convert_to_https() {
+    local url="$1"
+
+    if [[ "$url" == ssh://* ]]; then
+        # ssh://git@host/path -> https://host/path
+        echo "$url" | sed -E 's|ssh://git@([^/]+)/|https://\1/|'
+    elif [[ "$url" == git@* ]]; then
+        # git@host:path -> https://host/path
+        echo "$url" | sed -E 's|git@([^:]+):|https://\1/|'
+    elif [[ "$url" == https://* ]]; then
+        # Already HTTPS
+        echo "$url"
+    else
+        echo "Error: Unsupported URL format: $url" >&2
+        exit 1
+    fi
+}
+
+HTTPS_REMOTE=$(convert_to_https "$ORIGINAL_REMOTE")
 
 # Check if branch already exists (locally or remotely)
 if git show-ref --verify --quiet "refs/heads/$RELEASE_BRANCH" || \
@@ -15,8 +51,16 @@ if git show-ref --verify --quiet "refs/heads/$RELEASE_BRANCH" || \
     exit 1
 fi
 
+
 git config user.email "teamcity@jetbrains.com"
 git config user.name "TeamCity"
 
 git checkout -b "$RELEASE_BRANCH"
-git push origin "$RELEASE_BRANCH"
+
+git remote set-url origin "$HTTPS_REMOTE"
+
+temp="${HTTPS_REMOTE#https://}"
+HOSTNAME="${temp%%/*}"
+git -c "http.https://${HOSTNAME}/.extraheader=Authorization: Bearer $TOKEN" push origin "$RELEASE_BRANCH"
+
+git remote set-url origin "$ORIGINAL_REMOTE"
