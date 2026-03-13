13dad7e72cb8060f630f96204b51efd3eb92c185
Ilia Posov <ilia.posov@jetbrains.com>
2026-03-13 16:52:23 +0100

    EDU-8793: Tests

--- intellij-plugin/jvm-core/testSrc/com/jetbrains/edu/jvm/gradle/generation/EduGradleUtilsSetupGradleJvmTest.kt
+++ intellij-plugin/jvm-core/testSrc/com/jetbrains/edu/jvm/gradle/generation/EduGradleUtilsSetupGradleJvmTest.kt
@@ -1,22 +1,25 @@
 package com.jetbrains.edu.jvm.gradle.generation
 
+import com.intellij.openapi.externalSystem.service.execution.ExternalSystemJdkUtil
 import com.intellij.openapi.externalSystem.service.execution.ExternalSystemJdkUtil.USE_PROJECT_JDK
 import com.intellij.openapi.projectRoots.JavaSdk
 import com.intellij.openapi.projectRoots.JavaSdkVersion
 import com.intellij.openapi.projectRoots.Sdk
 import com.intellij.openapi.projectRoots.SdkType
 import com.intellij.openapi.projectRoots.impl.jdkDownloader.JdkDownloadUtil
+import com.intellij.openapi.projectRoots.impl.jdkDownloader.JdkItem
+import com.intellij.openapi.roots.ui.configuration.projectRoot.SdkDownloadTask
 import com.intellij.util.lang.JavaVersion
 import com.jetbrains.edu.jvm.gradle.generation.EduGradleUtils.setUpGradleJvm
 import com.jetbrains.edu.jvm.lookupJdkByPath
 import com.jetbrains.edu.learning.EduTestCase
 import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.mockkObject
 import io.mockk.mockkStatic
 import io.mockk.spyk
-import io.mockk.unmockkAll
 import org.gradle.util.GradleVersion
 import org.jetbrains.plugins.gradle.jvmcompat.GradleJvmSupportMatrix
 import org.jetbrains.plugins.gradle.settings.GradleProjectSettings
@@ -39,7 +42,7 @@ class EduGradleUtilsSetupGradleJvmTest : EduTestCase() {
 
     javaSdk = JavaSdk.getInstance()
     mockkObject(javaSdk)
-    every { javaSdk.collectSdkEntries(null) } returns emptyList<SdkType.SdkEntry>()
+    every { javaSdk.collectSdkEntries(null) } returns emptyList()
     every { javaSdk.getVersion(any()) } returns null
 
     gradleProjectSettings = spyk(GradleProjectSettings())
@@ -51,92 +54,65 @@ class EduGradleUtilsSetupGradleJvmTest : EduTestCase() {
 
   @Test
   fun `project sdk is used when compatible`() {
+    // Given
     val projectSdk = mockSdk(name = "Project 21", homePath = "/project/jdk", version = "21")
     every { javaSdk.getVersion(projectSdk) } returns JavaSdkVersion.JDK_21
     every { GradleJvmSupportMatrix.isSupported(any(), java21) } returns true
 
+    // When
     setUpGradleJvm(project, gradleProjectSettings, projectSdk)
 
+    // Then
     assertEquals(USE_PROJECT_JDK, gradleProjectSettings.gradleJvm)
   }
 
   @Test
-  fun `compatible auto detected sdk is used when project sdk is incompatible`() {
-    val projectSdk = mockSdk(name = "Project 17", homePath = "/project/jdk", version = "17")
+  fun `compatible auto detected or already configured sdk is used when project sdk is incompatible`() {
+    // Given
+    val projectSdk = mockSdk(name = "Project 25", homePath = "/project/jdk", version = "25")
     val autoDetectedEntry = sdkEntry(homePath = validJdkHome, version = "21")
     val lookedUpSdk = mockSdk(name = "Auto Detected 21", homePath = validJdkHome, version = "21")
 
-    every { javaSdk.getVersion(projectSdk) } returns JavaSdkVersion.JDK_17
+    mockkStatic(ExternalSystemJdkUtil::class)
+
+    every { javaSdk.getVersion(projectSdk) } returns JavaSdkVersion.JDK_25
     every { javaSdk.collectSdkEntries(null) } returns listOf(autoDetectedEntry)
+    every { ExternalSystemJdkUtil.isValidJdk(validJdkHome) } returns true
     every { lookupJdkByPath(project, validJdkHome) } returns lookedUpSdk
     every { GradleJvmSupportMatrix.isSupported(any(), java21) } returns true
 
+    // When
     setUpGradleJvm(project, gradleProjectSettings, projectSdk)
 
+    // Then
     assertEquals("Auto Detected 21", gradleProjectSettings.gradleJvm)
   }
 
   @Test
-  fun `test compatible configured sdk is used when project sdk is incompatible`() {
-    val projectSdk = mockSdk(name = "Project 17", homePath = "/project/jdk", version = "17")
-    val configuredEntry = sdkEntry(homePath = validJdkHome, version = "21")
-    val configuredSdk = mockSdk(name = "Configured 21", homePath = validJdkHome, version = "21")
+  fun `downloadable sdk is used when no compatible sdk exists`() {
+    // Given
+    val projectSdk = mockSdk(name = "Project 25", homePath = "/project/jdk", version = "25")
+    val downloadableSdk = mockSdk(name = "Downloadable 21", homePath = "/home/user/.jdks/jdk-21", version = "21")
 
-    every { javaSdk.getVersion(projectSdk) } returns JavaSdkVersion.JDK_17
-    every { javaSdk.collectSdkEntries(null) } returns listOf(configuredEntry)
-    every { lookupJdkByPath(project, validJdkHome) } returns configuredSdk
-    every { GradleJvmSupportMatrix.isSupported(any(), java21) } returns true
+    every { javaSdk.getVersion(projectSdk) } returns JavaSdkVersion.JDK_25
 
-    setUpGradleJvm(project, gradleProjectSettings, projectSdk)
-
-    assertEquals("Configured 21", gradleProjectSettings.gradleJvm)
-  }
-
-  @Test
-  fun `test downloadable sdk is used when no compatible sdk exists`() {
-    val projectSdk = mockSdk(name = "Project 17", homePath = "/project/jdk", version = "17")
-    val downloadedSdk = mockSdk(name = "Downloaded 21", homePath = "/downloaded/jdk", version = "21")
-
-    every { javaSdk.getVersion(projectSdk) } returns JavaSdkVersion.JDK_17
-
-    val jdkItem = mockk<com.intellij.openapi.projectRoots.impl.jdkDownloader.JdkItem>(relaxed = true)
-    val downloadTask = mockk<com.intellij.openapi.roots.ui.configuration.projectRoot.SdkDownloadTask>(relaxed = true)
+    val jdkItem = mockk<JdkItem>(relaxed = true)
+    val downloadTask = mockk<SdkDownloadTask>(relaxed = true)
 
     coEvery {
       JdkDownloadUtil.pickJdkItemAndPath(project, any())
-    } returns (jdkItem to Path.of("/downloaded/jdk"))
+    } returns (jdkItem to Path.of("/home/user/.jdks/jdk-21"))
 
-    coEvery { JdkDownloadUtil.createDownloadTask(project, jdkItem, Path.of("/downloaded/jdk")) } returns downloadTask
-    coEvery { JdkDownloadUtil.createDownloadSdk(any(), downloadTask) } returns downloadedSdk
-    coEvery { JdkDownloadUtil.downloadSdk(downloadedSdk) } returns true
+    coEvery { JdkDownloadUtil.createDownloadTask(project, jdkItem, Path.of("/home/user/.jdks/jdk-21")) } returns downloadTask
+    coEvery { JdkDownloadUtil.createDownloadSdk(any(), downloadTask) } returns downloadableSdk
+    coEvery { JdkDownloadUtil.downloadSdk(downloadableSdk) } returns true
 
+    // When
     setUpGradleJvm(project, gradleProjectSettings, projectSdk)
 
-    assertEquals("Downloaded 21", gradleProjectSettings.gradleJvm)
-  }
-
-  @Test
-  fun `test no compatible sdk entries means download branch is used`() {
-    val projectSdk = mockSdk(name = "Project 17", homePath = "/project/jdk", version = "17")
-    val downloadedSdk = mockSdk(name = "Downloaded 21", homePath = "/downloaded/jdk", version = "21")
-
-    every { javaSdk.getVersion(projectSdk) } returns JavaSdkVersion.JDK_17
-    every { javaSdk.collectSdkEntries(null) } returns emptyList()
-
-    val jdkItem = mockk<com.intellij.openapi.projectRoots.impl.jdkDownloader.JdkItem>(relaxed = true)
-    val downloadTask = mockk<com.intellij.openapi.roots.ui.configuration.projectRoot.SdkDownloadTask>(relaxed = true)
-
-    coEvery {
-      JdkDownloadUtil.pickJdkItemAndPath(project, any())
-    } returns (jdkItem to Path.of("/downloaded/jdk"))
-
-    coEvery { JdkDownloadUtil.createDownloadTask(project, jdkItem, Path.of("/downloaded/jdk")) } returns downloadTask
-    coEvery { JdkDownloadUtil.createDownloadSdk(any(), downloadTask) } returns downloadedSdk
-    coEvery { JdkDownloadUtil.downloadSdk(downloadedSdk) } returns true
-
-    setUpGradleJvm(project, gradleProjectSettings, projectSdk)
-
-    assertEquals("Downloaded 21", gradleProjectSettings.gradleJvm)
+    // Then
+    assertEquals("Downloadable 21", gradleProjectSettings.gradleJvm)
+    coVerify(exactly = 1) { JdkDownloadUtil.downloadSdk(downloadableSdk) }
   }
 
   private fun mockSdk(name: String, homePath: String, version: String): Sdk {
