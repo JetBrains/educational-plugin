8d4cd3e642abb43b4a072f2fc1ce98da6e8450a8
Gleb Oborin <gleb.oborin@jetbrains.com>
2026-03-25 14:28:16 +0100

    EDU-8819: Remove unnecessary navigation during project creation
    What happened:
    1) Navigation on project creation opened edu toolWindow and some files from first task (in `CourseProjectGenerator`)
    2) Gradlew toolwindow was opened after project is linked (in `ExternalToolWindowManager.onProjectsLinked`)
    3) After waiting for smart mode, we try to navigate to first task again, but we cannot because we opened some files on step 1 (in `EduProjectActivity`)
    
    Solution: Remove navigation on project creation step because navigation to first task will happen anyway on project openning stage
    (cherry picked from commit 2e5feb9736c645d0c8a0e3cccc82c8073916e368)

--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/newproject/CourseProjectGenerator.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/newproject/CourseProjectGenerator.kt
@@ -92,10 +92,6 @@ abstract class CourseProjectGenerator<S : EduProjectSettings>(
     setUpPluginDependencies(project, course)
     initializeFeatureManagement(project, course)
 
-    if (!SubmissionSettings.getInstance(project).stateOnClose) {
-      NavigationUtils.openFirstTask(course, project)
-    }
-
     CourseMetadataProcessor.applyProcessors(project, course, openCourseParams, CourseProjectState.CREATED_PROJECT)
 
     YamlFormatSynchronizer.saveAll(project)
