7907edfe3d67634867a266e9fe43e3c7faa346f2
Ilia Posov <ilia.posov@jetbrains.com>
2026-04-15 11:07:24 +0200

    EDU-8820: Generate ScalaSBT run configurations for test files (not dirs)
    When IDE generates configurations for Scala files, it better detects the used test framework. For directories, it always creates "Scala Test" configurations

--- intellij-plugin/Edu-Scala/src/com/jetbrains/edu/scala/sbt/checker/ScalaSbtEduTaskChecker.kt
+++ intellij-plugin/Edu-Scala/src/com/jetbrains/edu/scala/sbt/checker/ScalaSbtEduTaskChecker.kt
@@ -1,18 +1,11 @@
 package com.jetbrains.edu.scala.sbt.checker
 
-import com.intellij.execution.RunManager
 import com.intellij.execution.RunnerAndConfigurationSettings
-import com.intellij.openapi.module.ModuleUtilCore
 import com.intellij.openapi.project.Project
-import com.intellij.psi.PsiDirectory
 import com.jetbrains.edu.learning.checker.EduTaskCheckerBase
 import com.jetbrains.edu.learning.checker.EnvironmentChecker
 import com.jetbrains.edu.learning.checker.tests.TestResultCollector
-import com.jetbrains.edu.learning.courseFormat.ext.getAllTestDirectories
 import com.jetbrains.edu.learning.courseFormat.tasks.EduTask
-import org.jetbrains.plugins.scala.testingSupport.test.scalatest.ScalaTestConfigurationType
-import org.jetbrains.plugins.scala.testingSupport.test.scalatest.ScalaTestRunConfiguration
-import org.jetbrains.plugins.scala.testingSupport.test.testdata.AllInPackageTestData
 
 class ScalaSbtEduTaskChecker(
   task: EduTask,
@@ -20,25 +13,15 @@ class ScalaSbtEduTaskChecker(
   project: Project
 ) : EduTaskCheckerBase(task, envChecker, project) {
   override fun createDefaultTestConfigurations(): List<RunnerAndConfigurationSettings> {
-    val configurations = createTestConfigurationsForTestDirectories().filter { it.configuration.type == preferredConfigurationType }
-    return if (configurations.isEmpty()) {
-      task.getAllTestDirectories(project).map { createConfiguration(it) }
-    }
-    else {
-      configurations
-    }
-  }
+    // IDE always suggests "Scala Test" configurations for directories, even if files inside use the "MUnit" framework
 
-  @Suppress("UnstableApiUsage")
-  private fun createConfiguration(testDirectory: PsiDirectory): RunnerAndConfigurationSettings {
-    val settings = RunManager.getInstance(project).createConfiguration("Scala tests", ScalaTestConfigurationType().confFactory())
-    val configuration = settings.configuration as ScalaTestRunConfiguration
-    val packageTestData = AllInPackageTestData(configuration)
-    packageTestData.workingDirectory = testDirectory.project.basePath
-    configuration.`testConfigurationData_$eq`(packageTestData)
-    configuration.testKind = packageTestData.kind
-    configuration.module = ModuleUtilCore.findModuleForPsiElement(testDirectory)
-    return settings
+    val fileConfigurations = createTestConfigurationsForTestFiles()
+    if (fileConfigurations.isNotEmpty()) {
+      return fileConfigurations
+    }
+
+    val directoryConfigurations = createTestConfigurationsForTestDirectories()
+    return directoryConfigurations
   }
 
   override fun createTestResultCollector(): TestResultCollector = ScalaTestResultCollector()
