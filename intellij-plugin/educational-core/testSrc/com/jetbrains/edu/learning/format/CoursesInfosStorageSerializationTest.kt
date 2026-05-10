60fe32898d3fb7dc54ccc7e089b09b4008775b50
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2026-05-10 20:43:37 +0200

    EDU-8874: refactor CoursesInfosStorage tests

---
+++ intellij-plugin/educational-core/testSrc/com/jetbrains/edu/learning/format/CoursesInfosStorageSerializationTest.kt
@@ -0,0 +1,212 @@
+package com.jetbrains.edu.learning.format
+
+import com.jetbrains.edu.learning.EduSettingsServiceTestBase
+import com.jetbrains.edu.learning.course
+import com.jetbrains.edu.learning.courseFormat.EduFormatNames
+import com.jetbrains.edu.learning.newproject.coursesStorage.CoursesStorage
+import org.junit.Test
+
+class CoursesInfosStorageSerializationTest : EduSettingsServiceTestBase() {
+
+  @Test
+  fun `test deserialize first version courses storage`() {
+    val coursesStorage = CoursesStorage()
+    coursesStorage.loadStateAndCheck($$"""
+      <UserCoursesState>
+        <courses>
+          <course>
+            <option name="courseMode" value="Study" />
+            <option name="description" value="Introduction course to Python." />
+            <option name="humanLanguage" value="English" />
+            <option name="id" value="238" />
+            <option name="location" value="$USER_HOME$/IdeaProjects/Introduction to Python" />
+            <option name="name" value="Introduction to Python" />
+            <option name="programmingLanguageId" value="Python" />
+            <option name="programmingLanguageVersion" value="2.7" />
+            <option name="type" value="PyCharm" />
+          </course>
+        </courses>
+      </UserCoursesState>
+    """, $$"""
+      <UserCoursesState>
+        <courses>
+          <course>
+            <option name="description" value="Introduction course to Python." />
+            <option name="id" value="238" />
+            <option name="location" value="$USER_HOME$/IdeaProjects/Introduction to Python" />
+            <option name="name" value="Introduction to Python" />
+            <option name="type" value="PyCharm" />
+            <option name="programmingLanguageVersion" value="2.7" />
+            <option name="programmingLanguageId" value="Python" />
+          </course>
+        </courses>
+      </UserCoursesState>
+    """)
+  }
+
+  @Test
+  fun `test deserialize course with default parameters`() {
+    val coursesStorage = CoursesStorage()
+    coursesStorage.loadStateAndCheck($$"""
+      <UserCoursesState>
+        <courses>
+          <course>
+            <option name="description" value="The examples and exercises accompanying the AtomicKotlin book" />
+            <option name="id" value="20403" />
+            <option name="location" value="$USER_HOME$/IdeaProjects/AtomicKotlin" />
+            <option name="name" value="AtomicKotlin" />
+            <option name="type" value="PyCharm" />
+            <option name="programmingLanguageId" value="kotlin" />
+          </course>
+        </courses>
+      </UserCoursesState>
+    """)
+  }
+
+  @Test
+  fun `test deserialize old language version`() {
+    val coursesStorage = CoursesStorage()
+    coursesStorage.loadStateAndCheck($$"""
+      <UserCoursesState>
+        <courses>
+          <course>
+            <option name="description" value="The examples and exercises accompanying the AtomicKotlin book" />
+            <option name="id" value="20403" />
+            <option name="location" value="$USER_HOME$/IdeaProjects/AtomicKotlin" />
+            <option name="name" value="AtomicKotlin" />
+            <option name="type" value="PyCharm" />
+            <option name="programmingLanguage" value="Python 3.7" />
+          </course>
+        </courses>
+      </UserCoursesState>
+    """, $$"""
+      <UserCoursesState>
+        <courses>
+          <course>
+            <option name="description" value="The examples and exercises accompanying the AtomicKotlin book" />
+            <option name="id" value="20403" />
+            <option name="location" value="$USER_HOME$/IdeaProjects/AtomicKotlin" />
+            <option name="name" value="AtomicKotlin" />
+            <option name="type" value="PyCharm" />
+            <option name="programmingLanguageVersion" value="3.7" />
+            <option name="programmingLanguageId" value="Python" />
+          </course>
+        </courses>
+      </UserCoursesState>
+    """)
+  }
+
+  @Test
+  fun `test deserialize new language version`() {
+    val coursesStorage = CoursesStorage()
+    coursesStorage.loadStateAndCheck($$"""
+      <UserCoursesState>
+        <courses>
+          <course>
+            <option name="description" value="The examples and exercises accompanying the AtomicKotlin book" />
+            <option name="id" value="20403" />
+            <option name="location" value="$USER_HOME$/IdeaProjects/AtomicKotlin" />
+            <option name="name" value="AtomicKotlin" />
+            <option name="type" value="PyCharm" />
+            <option name="programmingLanguageVersion" value="3.7" />
+            <option name="programmingLanguageId" value="Python" />
+          </course>
+        </courses>
+      </UserCoursesState>
+    """)
+  }
+
+  @Test
+  fun `test deserialize new and old language version`() {
+    val coursesStorage = CoursesStorage()
+    coursesStorage.loadStateAndCheck($$"""
+      <UserCoursesState>
+        <courses>
+          <course>
+            <option name="description" value="The examples and exercises accompanying the AtomicKotlin book" />
+            <option name="id" value="20403" />
+            <option name="location" value="$USER_HOME$/IdeaProjects/AtomicKotlin" />
+            <option name="name" value="AtomicKotlin" />
+            <option name="type" value="PyCharm" />
+            <option name="programmingLanguage" value="Python 3.6" />
+            <option name="programmingLanguageVersion" value="3.7" />
+          </course>
+        </courses>
+      </UserCoursesState>
+    """, $$"""
+      <UserCoursesState>
+        <courses>
+          <course>
+            <option name="description" value="The examples and exercises accompanying the AtomicKotlin book" />
+            <option name="id" value="20403" />
+            <option name="location" value="$USER_HOME$/IdeaProjects/AtomicKotlin" />
+            <option name="name" value="AtomicKotlin" />
+            <option name="type" value="PyCharm" />
+            <option name="programmingLanguageVersion" value="3.7" />
+            <option name="programmingLanguageId" value="Python" />
+          </course>
+        </courses>
+      </UserCoursesState>
+    """)
+  }
+
+  @Test
+  fun `test serialize course with default parameters`() {
+    val coursesStorage = CoursesStorage()
+    val course = course(
+      "AtomicKotlin",
+      description = "The examples and exercises accompanying the AtomicKotlin book"
+    ) { }.apply {
+      id = 20403
+      languageId = EduFormatNames.PYTHON
+    }
+    coursesStorage.addCourse(course, $$"$USER_HOME$/IdeaProjects/AtomicKotlin")
+
+    coursesStorage.checkState($$"""
+      <UserCoursesState>
+        <courses>
+          <course>
+            <option name="description" value="The examples and exercises accompanying the AtomicKotlin book" />
+            <option name="id" value="20403" />
+            <option name="location" value="$USER_HOME$/IdeaProjects/AtomicKotlin" />
+            <option name="marketplace" value="false" />
+            <option name="name" value="AtomicKotlin" />
+            <option name="type" value="PyCharm" />
+            <option name="programmingLanguageId" value="Python" />
+          </course>
+        </courses>
+      </UserCoursesState>
+    """)
+  }
+
+  @Test
+  fun `test serialize language version`() {
+    val coursesStorage = CoursesStorage()
+    val course = course(
+      "AtomicKotlin",
+      description = "The examples and exercises accompanying the AtomicKotlin book"
+    ) { }.apply {
+      id = 20403
+      languageId = EduFormatNames.PYTHON
+      languageVersion = "3.7"
+    }
+    coursesStorage.addCourse(course, $$"$USER_HOME$/IdeaProjects/AtomicKotlin")
+
+    coursesStorage.checkState($$"""
+      <UserCoursesState>
+        <courses>
+          <course>
+            <option name="description" value="The examples and exercises accompanying the AtomicKotlin book" />
+            <option name="id" value="20403" />
+            <option name="location" value="$USER_HOME$/IdeaProjects/AtomicKotlin" />
+            <option name="marketplace" value="false" />
+            <option name="name" value="AtomicKotlin" />
+            <option name="type" value="PyCharm" />
+            <option name="programmingLanguageVersion" value="3.7" />
+            <option name="programmingLanguageId" value="Python" />
+          </course>
+        </courses>
+      </UserCoursesState>
+    """)
+  }
+}
--- intellij-plugin/educational-core/testSrc/com/jetbrains/edu/learning/format/CoursesInfosStorageTest.kt
+++ intellij-plugin/educational-core/testSrc/com/jetbrains/edu/learning/format/CoursesInfosStorageTest.kt
@@ -1,35 +1,117 @@
 package com.jetbrains.edu.learning.format
 
+import com.jetbrains.edu.learning.EduTestCase
+import com.jetbrains.edu.learning.configuration.PlainTextConfigurator
 import com.jetbrains.edu.learning.course
+import com.jetbrains.edu.learning.courseFormat.CourseMode
 import com.jetbrains.edu.learning.courseFormat.EduFormatNames
+import com.jetbrains.edu.learning.courseFormat.ext.configurator
+import com.jetbrains.edu.learning.messages.EduCoreBundle
+import com.jetbrains.edu.learning.newproject.coursesStorage.CoursesStorage
 import org.junit.Test
+import kotlin.test.assertIs
 
-class CoursesInfosStorageTest : CoursesInfosStorageTestBase() {
+class CoursesInfosStorageTest : EduTestCase() {
 
   @Test
-  fun testSerializeCourseWithDefaultParameters() {
-    val course = course(
-      "AtomicKotlin",
-      description = "The examples and exercises accompanying the AtomicKotlin book"
-    ) { }.apply {
-      id = 20403
-      languageId = EduFormatNames.PYTHON
-    }
+  fun `test correct configurator found for courses in storage`() {
+    val coursesStorage = CoursesStorage.getInstance()
 
-    doSerializationTest(course)
+    val eduCourse = course {}
+    coursesStorage.addCourse(eduCourse, "location", 0, 0)
+    assertIs<PlainTextConfigurator>(coursesStorage.getCourseMetaInfo(eduCourse)?.toCourse()?.configurator)
   }
 
   @Test
-  fun testSerializeLanguageVersion() {
-    val course = course(
-      "AtomicKotlin",
-      description = "The examples and exercises accompanying the AtomicKotlin book"
-    ) { }.apply {
-      id = 20403
-      languageId = EduFormatNames.PYTHON
-      languageVersion = "3.7"
-    }
+  fun `test course mode respected`() {
+    val coursesStorage = CoursesStorage.getInstance()
+    val educatorCourse = course(courseMode = CourseMode.EDUCATOR) {}
+    coursesStorage.addCourse(educatorCourse, "", 0, 0)
+    val studentCourse = course {}
+    assertFalse(coursesStorage.hasCourse(studentCourse))
+  }
 
-    doSerializationTest(course)
+  @Test
+  fun `test course id respected`() {
+    val coursesStorage = CoursesStorage.getInstance()
+    val courseWithDefaultId = course {}
+    coursesStorage.addCourse(courseWithDefaultId, "", 0, 0)
+    val studentCourse = course {}.apply { id = 1234 }
+    assertFalse(coursesStorage.hasCourse(studentCourse))
+  }
+
+  @Test
+  fun `test language respected`() {
+    val coursesStorage = CoursesStorage.getInstance()
+    val courseWithDefaultId = course {}
+    coursesStorage.addCourse(courseWithDefaultId, "", 0, 0)
+    val courseWithLanguage = course {}.apply { languageId = EduFormatNames.PYTHON }
+    assertFalse(coursesStorage.hasCourse(courseWithLanguage))
+  }
+
+  @Test
+  fun `test empty courses group`() {
+    val coursesStorage = CoursesStorage.getInstance()
+    assertEmpty(coursesStorage.coursesInGroups())
+  }
+
+  @Test
+  fun `test in progress courses group`() {
+    val coursesStorage = CoursesStorage.getInstance()
+    val course = course {}
+    coursesStorage.addCourse(course, "", 1, 10)
+    val coursesInGroups = coursesStorage.coursesInGroups()
+    assertSize(1, coursesInGroups)
+    assertEquals(EduCoreBundle.message("course.dialog.in.progress"), coursesInGroups.first().name)
+  }
+
+  @Test
+  fun `test completed courses group`() {
+    val coursesStorage = CoursesStorage.getInstance()
+    val course = course {}
+    coursesStorage.addCourse(course, "", 10, 10)
+    val coursesInGroups = coursesStorage.coursesInGroups()
+    assertSize(1, coursesInGroups)
+    assertEquals(EduCoreBundle.message("course.dialog.completed"), coursesInGroups.first().name)
+  }
+
+  @Test
+  fun `test untouched course`() {
+    val coursesStorage = CoursesStorage.getInstance()
+    val course = course {}
+    coursesStorage.addCourse(course, "", 0, 0)
+    val coursesInGroups = coursesStorage.coursesInGroups()
+    assertSize(1, coursesInGroups)
+    assertEquals(EduCoreBundle.message("course.dialog.in.progress"), coursesInGroups.first().name)
+  }
+
+  @Test
+  fun `test cc group`() {
+    val coursesStorage = CoursesStorage.getInstance()
+    val educatorCourse = course(courseMode = CourseMode.EDUCATOR) {}
+    coursesStorage.addCourse(educatorCourse, "", 0, 0)
+    val coursesInGroups = coursesStorage.coursesInGroups()
+    assertSize(1, coursesInGroups)
+    assertEquals(EduCoreBundle.message("course.dialog.my.courses.course.creation"), coursesInGroups.first().name)
+  }
+
+  @Test
+  fun `test all courses groups`() {
+    val coursesStorage = CoursesStorage.getInstance()
+
+    val educatorCourse = course(name = "CC course", courseMode = CourseMode.EDUCATOR) {}
+    coursesStorage.addCourse(educatorCourse, "/CC course", 0, 0)
+
+    val inProgressCourse = course(name = "In Progress") {}
+    coursesStorage.addCourse(inProgressCourse, "/in_progress", 1, 10)
+
+    val completedCourse = course(name = "Completed") {}
+    coursesStorage.addCourse(completedCourse, "/completed", 10, 10)
+
+    val coursesInGroups = coursesStorage.coursesInGroups()
+    assertSize(3, coursesInGroups)
+    assertEquals(EduCoreBundle.message("course.dialog.my.courses.course.creation"), coursesInGroups.first().name)
+    assertEquals(EduCoreBundle.message("course.dialog.in.progress"), coursesInGroups[1].name)
+    assertEquals(EduCoreBundle.message("course.dialog.completed"), coursesInGroups[2].name)
   }
 }
