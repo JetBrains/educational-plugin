b8d62bcf7f18da554ae10f9160f153442601835f
Gleb Oborin <gleb.oborin@jetbrains.com>
2026-05-13 14:25:15 +0200

    EDU-8551: Try to guess default gradle version and find lower bound for JDK for it

--- intellij-plugin/jvm-core/src/com/jetbrains/edu/jvm/JdkEnvironmentSettings.kt
+++ intellij-plugin/jvm-core/src/com/jetbrains/edu/jvm/JdkEnvironmentSettings.kt
@@ -6,13 +6,15 @@ import com.intellij.openapi.roots.LanguageLevelProjectExtension
 import com.jetbrains.edu.learning.courseFormat.Course
 import com.jetbrains.edu.learning.courseFormat.CourseMode
 import com.jetbrains.edu.learning.courseFormat.hyperskill.HyperskillCourse
+import org.gradle.util.GradleVersion
+import org.jetbrains.plugins.gradle.jvmcompat.GradleJvmSupportMatrix
 
 const val JVM_LANGUAGE_LEVEL = "jvm_language_level"
 
 // TODO(refactor this), this is a temporary solution
 // All JVM-based Hyperskill courses now require JDK version 23
 val hyperskillJdkVersion: JavaSdkVersion = JavaSdkVersion.JDK_23
-val minCCJdkVersion: JavaSdkVersion = JavaSdkVersion.JDK_17
+val minCCJdkVersion: JavaSdkVersion by lazy { guessCCMinJdkVersion() }
 
 val Course.minJvmSdkVersion: ParsedJavaVersion
   get() = when {
@@ -24,3 +26,9 @@ val Course.minJvmSdkVersion: ParsedJavaVersion
 fun jvmEnvironmentSettings(project: Project): Map<String, String> = mapOf(
   JVM_LANGUAGE_LEVEL to LanguageLevelProjectExtension.getInstance(project).languageLevel.toString()
 )
+
+private fun guessCCMinJdkVersion(): JavaSdkVersion {
+  val defaultGradleVersion = GradleVersion.current()
+  val defaultJavaVersion = GradleJvmSupportMatrix.suggestOldestSupportedJavaVersion(defaultGradleVersion) ?: return JavaSdkVersion.JDK_17
+  return JavaSdkVersion.fromJavaVersion(defaultJavaVersion) ?: JavaSdkVersion.JDK_17
+}
