68ea29f0af51825860ea433995a785874124658c
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2026-05-16 01:59:44 +0200

    EDU-8582: step 2 - drop Hyperskill code in Edu-Shell

--- intellij-plugin/Edu-Shell/src/com/jetbrains/edu/shell/hyperskill/ShellHyperskillConfigurator.kt
+++
@@ -1,7 +0,0 @@
-package com.jetbrains.edu.shell.hyperskill
-
-import com.jetbrains.edu.learning.newproject.EmptyProjectSettings
-import com.jetbrains.edu.learning.stepik.hyperskill.HyperskillConfigurator
-import com.jetbrains.edu.shell.ShellConfigurator
-
-class ShellHyperskillConfigurator : HyperskillConfigurator<EmptyProjectSettings>(ShellConfigurator())
