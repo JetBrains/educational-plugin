14d53f9a80f08d2933294ba81325e046ae9791c3
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2026-05-16 02:12:14 +0200

    EDU-8582: step 11 - drop Hyperskill code in jvm-core

--- intellij-plugin/jvm-core/src/com/jetbrains/edu/jvm/JdkEnvironmentSettings.kt
+++ intellij-plugin/jvm-core/src/com/jetbrains/edu/jvm/JdkEnvironmentSettings.kt
@@ -5,20 +5,15 @@ import com.intellij.openapi.projectRoots.JavaSdkVersion
 import com.intellij.openapi.roots.LanguageLevelProjectExtension
 import com.jetbrains.edu.learning.courseFormat.Course
 import com.jetbrains.edu.learning.courseFormat.CourseMode
-import com.jetbrains.edu.learning.courseFormat.hyperskill.HyperskillCourse
 import org.gradle.util.GradleVersion
 import org.jetbrains.plugins.gradle.jvmcompat.GradleJvmSupportMatrix
 
 const val JVM_LANGUAGE_LEVEL = "jvm_language_level"
 
-// TODO(refactor this), this is a temporary solution
-// All JVM-based Hyperskill courses now require JDK version 23
-val hyperskillJdkVersion: JavaSdkVersion = JavaSdkVersion.JDK_23
 val minCCJdkVersion: JavaSdkVersion by lazy { guessCCMinJdkVersion() }
 
 val Course.minJvmSdkVersion: ParsedJavaVersion
   get() = when {
-    this is HyperskillCourse -> JavaVersionParseSuccess(hyperskillJdkVersion)
     courseMode == CourseMode.EDUCATOR -> JavaVersionParseSuccess(minCCJdkVersion)
     else -> ParsedJavaVersion.fromStringLanguageLevel(environmentSettings[JVM_LANGUAGE_LEVEL])
   }
--- intellij-plugin/jvm-core/src/com/jetbrains/edu/jvm/gradle/checker/GradleEnvironmentChecker.kt
+++ intellij-plugin/jvm-core/src/com/jetbrains/edu/jvm/gradle/checker/GradleEnvironmentChecker.kt
@@ -3,13 +3,10 @@ package com.jetbrains.edu.jvm.gradle.checker
 import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil
 import com.intellij.openapi.module.ModuleUtil
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.projectRoots.JavaSdkVersionUtil
 import com.intellij.openapi.roots.ProjectRootManager
-import com.intellij.openapi.roots.ui.configuration.ProjectSettingsService
 import com.intellij.util.concurrency.annotations.RequiresEdt
 import com.jetbrains.edu.jvm.gradle.GradleCourseRefresher
 import com.jetbrains.edu.jvm.gradle.generation.EduGradleUtils
-import com.jetbrains.edu.jvm.hyperskillJdkVersion
 import com.jetbrains.edu.jvm.messages.EduJVMBundle
 import com.jetbrains.edu.learning.EduNames.ENVIRONMENT_CONFIGURATION_LINK_GRADLE
 import com.jetbrains.edu.learning.RefreshCause
@@ -19,18 +16,13 @@ import com.jetbrains.edu.learning.courseFormat.CheckResult
 import com.jetbrains.edu.learning.courseFormat.CheckStatus
 import com.jetbrains.edu.learning.courseFormat.EduFormatNames
 import com.jetbrains.edu.learning.courseFormat.ext.getDir
-import com.jetbrains.edu.learning.courseFormat.hyperskill.HyperskillCourse
 import com.jetbrains.edu.learning.courseFormat.tasks.Task
 import com.jetbrains.edu.learning.messages.EduCoreBundle
 import org.jetbrains.plugins.gradle.settings.GradleSettings
 
 open class GradleEnvironmentChecker : EnvironmentChecker() {
   override fun getEnvironmentError(project: Project, task: Task): CheckResult? {
-    val sdk = ProjectRootManager.getInstance(project).projectSdk ?: return noSdkConfiguredResult
-
-    if (task.course is HyperskillCourse && JavaSdkVersionUtil.getJavaSdkVersion(sdk) != hyperskillJdkVersion) {
-      return getIncorrectHyperskillJDKResult(project)
-    }
+    ProjectRootManager.getInstance(project).projectSdk ?: return noSdkConfiguredResult
 
     val taskDir = task.getDir(project.courseDir) ?: return getFailedToLaunchCheckingResult(project)
     val module = ModuleUtil.findModuleForFile(taskDir, project) ?: return getFailedToLaunchCheckingResult(project)
@@ -66,18 +58,5 @@ open class GradleEnvironmentChecker : EnvironmentChecker() {
                          hyperlinkAction = { reloadGradle(project) })
     }
 
-    private const val OPEN_MODULE_SETTINGS = "open_module_settings"
-
-    private fun getIncorrectHyperskillJDKResult(project: Project): CheckResult {
-      return CheckResult(
-        CheckStatus.Unchecked,
-        EduJVMBundle.message("error.hyperskill.incorrect.jdk", hyperskillJdkVersion.description, OPEN_MODULE_SETTINGS),
-        hyperlinkAction = { openModuleSettings(project) }
-      )
-    }
-
-    private fun openModuleSettings(project: Project) {
-      ProjectSettingsService.getInstance(project).openProjectSettings()
-    }
   }
 }
