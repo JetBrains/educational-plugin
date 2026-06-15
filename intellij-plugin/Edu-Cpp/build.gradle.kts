0b1a029d0b6ee502b799fce6f17ffede04594b50
Gleb Oborin <gleb.oborin@jetbrains.com>
2026-06-15 18:02:50 +0200

    EDU-8805: Ignore Edu-Cpp tests for now

--- intellij-plugin/Edu-Cpp/build.gradle.kts
+++ intellij-plugin/Edu-Cpp/build.gradle.kts
@@ -18,3 +18,13 @@ dependencies {
 
   testImplementation(project(":intellij-plugin:educational-core", "testOutput"))
 }
+
+// At the moment, the tests for the Edu-Cpp module don't work correctly.
+// This started after migrating the tests from the classic engine to the CLion Nova engine.
+// The first test to run fails (`CppMoveHandlerTest` and a few other tests consistently fail).
+// We should consider using the test base classes from the CLion test framework.
+
+// TODO(re-enable the tests once they are fixed as part of EDU-8917)
+tasks.withType<Test> {
+  enabled = false
+}
