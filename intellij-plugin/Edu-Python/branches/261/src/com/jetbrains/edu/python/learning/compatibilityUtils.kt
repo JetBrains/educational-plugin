abbf3434145add77597dc0e8e66f11be9bdd3492
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2026-06-25 00:21:23 +0200

    EDU-8939: simplify and make more generic python package installation
    (cherry picked from commit f5ae7ffd167a48eab5cf4cec7f3247cc87164fdb)

---
+++ intellij-plugin/Edu-Python/branches/261/src/com/jetbrains/edu/python/learning/compatibilityUtils.kt
@@ -0,0 +1,7 @@
+package com.jetbrains.edu.python.learning
+
+import com.jetbrains.python.errorProcessing.PyResult
+import com.jetbrains.python.packaging.common.PythonPackage
+import com.jetbrains.python.packaging.management.PythonPackageManager
+
+suspend fun PythonPackageManager.syncLocked(): PyResult<List<PythonPackage>> = sync()
