e139d601387f59e989f8ed30d52973685f14a083
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2026-07-02 23:01:51 +0200

    Make `validateCourse` command create output file together with all missing parent directories if needed

--- intellij-plugin/features/command-line/src/com/jetbrains/edu/commandLine/validation/FileValidationOutputConsumer.kt
+++ intellij-plugin/features/command-line/src/com/jetbrains/edu/commandLine/validation/FileValidationOutputConsumer.kt
@@ -3,10 +3,11 @@ package com.jetbrains.edu.commandLine.validation
 import java.io.PrintWriter
 import java.nio.file.Path
 import kotlin.io.path.bufferedWriter
+import kotlin.io.path.createParentDirectories
 
 class FileValidationOutputConsumer(path: Path) : ValidationOutputConsumer {
 
-  private val writer: PrintWriter = PrintWriter(path.bufferedWriter())
+  private val writer: PrintWriter = PrintWriter(path.createParentDirectories().bufferedWriter())
 
   override fun consume(output: String) {
     writer.println(output)
