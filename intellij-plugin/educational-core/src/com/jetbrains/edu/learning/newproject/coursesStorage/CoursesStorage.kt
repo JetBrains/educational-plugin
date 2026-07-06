8a5ea285d1f775748ed357ca8c9d39ab70d17b74
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2026-07-06 23:56:57 +0200

    Cleanup `CourseStorage`
    Merge it with `CourseStorageBase` and drop unused methods

--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/newproject/coursesStorage/CoursesStorage.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/newproject/coursesStorage/CoursesStorage.kt
@@ -1,34 +1,44 @@
 package com.jetbrains.edu.learning.newproject.coursesStorage
 
+import com.intellij.ide.RecentProjectsManager
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.components.*
+import com.intellij.openapi.util.io.FileUtilRt
 import com.intellij.openapi.wm.impl.welcomeScreen.learnIde.coursesInProgress.CourseDataStorage
 import com.intellij.openapi.wm.impl.welcomeScreen.learnIde.coursesInProgress.CourseInfo
+import com.intellij.util.messages.Topic
+import com.intellij.util.xmlb.annotations.XCollection
 import com.jetbrains.edu.learning.EduTestAware
 import com.jetbrains.edu.learning.courseFormat.Course
 import com.jetbrains.edu.learning.courseFormat.CourseMode
+import com.jetbrains.edu.learning.courseFormat.EduFormatNames
+import com.jetbrains.edu.learning.messages.EduCoreBundle
+import com.jetbrains.edu.learning.newproject.ui.coursePanel.groups.CoursesGroup
 import com.jetbrains.edu.learning.newproject.ui.welcomeScreen.JBACourseFromStorage
 import org.jetbrains.annotations.TestOnly
 
 @State(name = "CoursesStorage", storages = [Storage("coursesStorage.xml", roamingType = RoamingType.DISABLED)])
 @Service
-class CoursesStorage : CourseDataStorage, CoursesStorageBase(), EduTestAware {
+class CoursesStorage :
+  SimplePersistentStateComponent<UserCoursesState>(UserCoursesState()),
+  CourseDataStorage,
+  EduTestAware {
 
-  override fun getCoursePath(course: Course): String? = getCoursePath(JBACourseFromStorage().apply {
-    this.name = course.name
-    this.id = course.id
-    this.courseMode = course.courseMode
-    this.languageId = course.languageId
-    this.languageVersion = course.languageVersion
-  })
-
-  private fun getCourseMetaInfo(name: String, id: Int, courseMode: CourseMode, languageId: String): JBACourseFromStorage? {
-    return state.courses.find {
-      it.name == name
-      && it.id == id
-      && it.courseMode == courseMode
-      && it.languageId == languageId
-    }
+  override fun loadState(state: UserCoursesState) {
+    // We do not support Hyperskill and Stepik courses anymore.
+    // But we still can have records with such types in the storage.
+    // So, let's manually filter them out here not to expose them to other places.
+    // Also, after the next state serialization, we won't have such records in storage anymore.
+    state.courses.removeAll { it.type == EduFormatNames.HYPERSKILL || it.type == EduFormatNames.STEPIK }
+    super.loadState(state)
   }
+
+  fun addCourse(course: Course, location: String, tasksSolved: Int = 0, tasksTotal: Int = 0) {
+    state.addCourse(course, location, tasksSolved, tasksTotal)
+    ApplicationManager.getApplication().messageBus.syncPublisher(COURSE_ADDED).courseAdded(course)
+  }
+
+  fun getCoursePath(course: Course): String? = getCourseMetaInfo(course)?.location
 
   override fun getCoursePath(courseInfo: CourseInfo): String? {
     return if (courseInfo is JBACourseFromStorage) {
@@ -39,9 +49,49 @@ class CoursesStorage : CourseDataStorage, CoursesStorageBase(), EduTestAware {
     }
   }
 
+  fun hasCourse(course: Course): Boolean = getCoursePath(course) != null
+
   override fun removeCourseByLocation(location: String): Boolean {
-    return super.doRemoveCourseByLocation(location)
+    val deletedCourse = state.removeCourseByLocation(location) ?: return false
+    ApplicationManager.getApplication().messageBus.syncPublisher(COURSE_DELETED).courseDeleted(deletedCourse)
+    RecentProjectsManager.getInstance().removePath(location)
+
+    return true
   }
+
+  fun getCourseMetaInfo(course: Course): JBACourseFromStorage? =
+    getCourseMetaInfo(course.name, course.id, course.courseMode, course.languageId)
+
+  private fun getCourseMetaInfo(name: String, id: Int, courseMode: CourseMode, languageId: String): JBACourseFromStorage? {
+    return state.courses.find {
+      it.name == name
+      && it.id == id
+      && it.courseMode == courseMode
+      && it.languageId == languageId
+    }
+  }
+
+  fun updateCourseProgress(course: Course, location: String, tasksSolved: Int, tasksTotal: Int) {
+    state.updateCourseProgress(course, location, tasksSolved, tasksTotal)
+  }
+
+  fun coursesInGroups(): List<CoursesGroup> {
+    val courses = state.courses
+    val solvedCourses = courses.filter { it.isStudy && it.tasksSolved != 0 && it.tasksSolved == it.tasksTotal }.map { it.toCourse() }
+    val solvedCoursesGroup = CoursesGroup(EduCoreBundle.message("course.dialog.completed"), solvedCourses)
+
+    val courseCreatorCoursesGroup = CoursesGroup(
+      EduCoreBundle.message("course.dialog.my.courses.course.creation"),
+      courses.filter { !it.isStudy }.map { it.toCourse() }
+    )
+
+    val inProgressCourses = courses.filter { it.isStudy && (it.tasksSolved == 0 || it.tasksSolved != it.tasksTotal) }.map { it.toCourse() }
+    val inProgressCoursesGroup = CoursesGroup(EduCoreBundle.message("course.dialog.in.progress"), inProgressCourses)
+
+    return listOf(courseCreatorCoursesGroup, inProgressCoursesGroup, solvedCoursesGroup).filter { it.courses.isNotEmpty() }
+  }
+
+  fun isNotEmpty(): Boolean = state.courses.isNotEmpty()
 
   override fun getAllCourses(): List<CourseInfo> {
     return state.courses
@@ -53,6 +103,41 @@ class CoursesStorage : CourseDataStorage, CoursesStorageBase(), EduTestAware {
   }
 
   companion object {
+    val COURSE_DELETED = Topic.create("Edu.courseDeletedFromStorage", CourseDeletedListener::class.java)
+    val COURSE_ADDED = Topic.create("Edu.courseAddedToStorage", CourseAddedListener::class.java)
+
     fun getInstance(): CoursesStorage = service()
+  }
+}
+
+class UserCoursesState : BaseState() {
+  //  courses list is not updated on course removal and could contain removed courses.
+  @get:XCollection(style = XCollection.Style.v2)
+  val courses by list<JBACourseFromStorage>()
+
+  fun addCourse(course: Course, location: String, tasksSolved: Int = 0, tasksTotal: Int = 0) {
+    val systemIndependentLocation = FileUtilRt.toSystemIndependentName(location)
+    courses.removeIf { it.location == systemIndependentLocation }
+    val courseMetaInfo = JBACourseFromStorage(systemIndependentLocation, course, tasksTotal, tasksSolved)
+    courses.add(courseMetaInfo)
+  }
+
+  fun removeCourseByLocation(location: String): JBACourseFromStorage? {
+    val courseMetaInfo = courses.find { it.location == location }
+    courses.remove(courseMetaInfo)
+    return courseMetaInfo
+  }
+
+  fun updateCourseProgress(course: Course, location: String, tasksSolved: Int, tasksTotal: Int) {
+    val systemIndependentLocation = FileUtilRt.toSystemIndependentName(location)
+    val courseMetaInfo = courses.find { it.location == systemIndependentLocation }
+    if (courseMetaInfo != null) {
+      courseMetaInfo.tasksSolved = tasksSolved
+      courseMetaInfo.tasksTotal = tasksTotal
+      intIncrementModificationCount()
+    }
+    else {
+      courses.add(JBACourseFromStorage(systemIndependentLocation, course, tasksTotal, tasksSolved))
+    }
   }
 }
