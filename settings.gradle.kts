5d1b0c33962604adf5ce91683356e84aa92d2e6a
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2026-07-25 01:37:26 +0200

    Update missing `org.jetbrains.intellij.platform.settings` Gradle plugin to 2.18.1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -152,7 +152,7 @@ plugins {
   // Version kept in sync with plugins.intelliJPlatformPlugin in gradle/libs.versions.toml.
   // Gradle forbids version-catalog references inside settings `plugins { }` blocks, so we can't use it here directly.
   // See https://github.com/gradle/gradle/issues/36437
-  id("org.jetbrains.intellij.platform.settings") version "2.16.0"
+  id("org.jetbrains.intellij.platform.settings") version "2.18.1"
   id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
