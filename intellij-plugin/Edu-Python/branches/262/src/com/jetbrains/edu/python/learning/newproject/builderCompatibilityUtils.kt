e7a1f946bb34eb9758a588f43821f33e0986942b
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2026-08-19 13:31:26 +0200

    EDU-8416: properly look for default python
    `Files.isSameFile` properly compares paths including symlinks, special path segments, etc. It will find proper system python even if provided path has different string representation but still referes to the same file

--- intellij-plugin/Edu-Python/branches/262/src/com/jetbrains/edu/python/learning/newproject/builderCompatibilityUtils.kt
+++ intellij-plugin/Edu-Python/branches/262/src/com/jetbrains/edu/python/learning/newproject/builderCompatibilityUtils.kt
@@ -5,14 +5,13 @@ import com.jetbrains.edu.learning.Err
 import com.jetbrains.edu.learning.Ok
 import com.jetbrains.edu.learning.Result
 import com.jetbrains.edu.python.learning.environment.PyLanguageEnvironment
+import java.nio.file.Files
 import kotlin.io.path.Path
 
-suspend fun createDefaultSettings(sdkPath: String): Result<PyLanguageEnvironment, String> {
+suspend fun createDefaultSettings(sdkLocation: String): Result<PyLanguageEnvironment, String> {
+  val sdkPath = Path(sdkLocation)
   val sdk = SystemPythonService().findSystemPythons(forceRefresh = true).firstOrNull {
-    it.pythonBinary == Path(sdkPath)
+    Files.isSameFile(it.pythonBinary, sdkPath)
   }
-  if (sdk == null) {
-    return Err("No system python found")
-  }
-  return Ok(sdk.toExisting())
+  return if (sdk == null) Err("No system python found") else Ok(sdk.toExisting())
 }
