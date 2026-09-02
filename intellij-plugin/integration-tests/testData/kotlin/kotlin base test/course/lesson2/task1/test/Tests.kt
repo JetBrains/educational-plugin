53549bce30391fccbc7f255d95d71cb82fa6d863
jamilya.shurukhova <jamilya.shurukhova@jetbrains.com>
2026-09-02 11:56:07 +0200

    EDU-8416 - fixed code review comments

--- intellij-plugin/integration-tests/testData/kotlin/kotlin base test/course/lesson2/task1/test/Tests.kt
+++ intellij-plugin/integration-tests/testData/kotlin/kotlin base test/course/lesson2/task1/test/Tests.kt
@@ -3,7 +3,6 @@ import org.junit.Test
 
 class Test {
     @Test fun testSolution() {
-        //TODO: implement your test here
-        Assert.assertTrue("Tests not implemented for the task", false)
+        Assert.assertTrue("Tests not implemented for the task", true)
     }
 }
